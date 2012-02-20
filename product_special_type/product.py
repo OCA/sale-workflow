cc683068037406eceaa2152b0e19aa70b3d6706b
unknown <nicolas.bessi@camptocamp.com>
2012-02-20 14:45:30 +0100

    [MRG] 6.1 (lp:c2c-addons/6.1  rev 28.1.5)

---
+++ product_special_type/product.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2011-2012 Camptocamp SA (http://www.camptocamp.com)
+#   @author Guewen Baconnier
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import osv, fields
+
+
+class product_template(osv.osv):
+
+    _inherit = 'product.template'
+
+    _columns = {
+        'special_type': fields.selection([('discount', 'Global Discount'),
+                                          ('advance', 'Advance'),
+                                          ('delivery', 'Delivery Costs'),
+                                          ('', '')],
+                                         string='Special Type',
+                                         help='Special products will not be displayed on invoices printed reports but will be summed in the totals.'),
+    }
+
+product_template()
