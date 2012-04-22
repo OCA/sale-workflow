4d0ffd76f28e703d86a51a52ca342f9939def8ec
sebastien beau <sebastien.beau@akretion.com.br>
2012-04-22 14:49:21 +0200

    [ADD] add sale_automatic_workflow, a module to automatized the sale order workflow

---
+++ sale_automatic_workflow/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+#################################################################################
+#                                                                               #
+#    sale_automatic_workflow for OpenERP                                        #
+#    Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>   #
+#                                                                               #
+#    This program is free software: you can redistribute it and/or modify       #
+#    it under the terms of the GNU Affero General Public License as             #
+#    published by the Free Software Foundation, either version 3 of the         #
+#    License, or (at your option) any later version.                            #
+#                                                                               #
+#    This program is distributed in the hope that it will be useful,            #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#    GNU Affero General Public License for more details.                        #
+#                                                                               #
+#    You should have received a copy of the GNU Affero General Public License   #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                               #
+#################################################################################
+
+
+{
+    'name': 'sale_automatic_workflow',
+    'version': '0.1',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'description': """empty""",
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com/',
+    'depends': ['sale_quick_payment'], 
+    'init_xml': [],
+    'update_xml': [ 
+            'sale_view.xml',
+            'sale_workflow.xml',
+#            'invoice_workflow.xml',
+#            'stock_workflow.xml',
+            'sale_workflow_process_view.xml',
+            'payment_method_view.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
