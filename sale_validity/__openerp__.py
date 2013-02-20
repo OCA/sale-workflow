a913b698e4f055a6ff4ac33582bbf96cf126e0bd
Jacques-Etienne Baudoux <je@bcim.be>
2013-02-20 12:27:22 +0100

    [ADD] module sale_validity : Add validity date on Sales Quotation (../trunk-generic/ rev 1)

---
+++ sale_validity/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sales Quotation Validity Date",
+    "version": "7.0.0",
+    "depends": ["sale"],
+    "author": "Jacques-Etienne Baudoux",
+    "category": "Sales",
+    "description": "Add a validity date on the sales quotation defining until when the quotation is valid",
+    'data': [
+        "view/sale_order.xml",
+        ],
+    'installable': True,
+    'active': False,
+}
