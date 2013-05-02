930223b0c0fb601d1aa27bc03425aec4f398df12
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-05-02 15:11:15 +0200

    [MIG] portage of crm_department by OpenERP

--- crm_department/__openerp__.py
+++ crm_department/__openerp__.py
@@ -1,47 +1,53 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Joël Grand-guillaume (Camptocamp) 
-#    Contributor: Yannick Vaucher (Camptocamp)
-#    Copyright 2011 Camptocamp SA
-#    Donors:
+# Copyright (c) 2011 Camptocamp SA (http://www.camptocamp.com)
+# All Right Reserved
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# Author : Joël Grand-guillaume (Camptocamp)
+# Contribution : Yannick Vaucher (Camptocamp)
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
+
 {
-    "name" : "CRM with Department Categorization",
-    "version" : "1.0",
-    "author" : "Camptocamp",
-    "category" : "Generic Modules/CRM & SRM",
-    "description": """
-    
+    'name' : 'CRM with Department Categorization',
+    'version' : '1.0',
+    'category' : 'Generic Modules/CRM & SRM',
+    'description': '''
+
 Add the department on Sales Team (with related field on Lead & Opportunities)
 as well as the related filter and button in the search form.
 
-    
-""",
-    "website": "http://camptocamp.com",
-    "depends" : [
-            "crm",
-            "hr"
-                ],
-    "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml" : [
-           "crm_view.xml",
-                   ],
-    "active": False,
-    'installable': False
+''',
+    'author' : 'Camptocamp',
+    'website': 'http://camptocamp.com',
+    'depends' : ['crm', 'hr'],
+    'data' : ['crm_view.xml'],
+    'demo_xml' : [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
 }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- crm_department/crm.py
+++ crm_department/crm.py
@@ -1,59 +1,78 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Joël Grand-guillaume (Camptocamp) 
-#    Contributor: Yannick Vaucher (Camptocamp)
-#    Copyright 2011 Camptocamp SA
-#    Donors:
+# Copyright (c) 2011 Camptocamp SA (http://www.camptocamp.com)
+# All Right Reserved
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# Author : Joel Grand-guillaume (Camptocamp)
+# Contribution : Yannick Vaucher (Camptocamp)
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
+
 from osv import osv
 from osv import fields
 
 class crm_sales_team(osv.osv):
     _inherit = "crm.case.section"
-
     _columns = {
         'department_id': fields.many2one('hr.department', 'Department'),
     }
+
+    def _get_department(self, cr, uid, ids, context=None):
+        employee_obj = self.pool.get('hr.employee')
+        department_id = False
+        employee_ids = employee_obj.search(cr, uid, [('user_id','=', uid)])
+        if employee_ids:
+            department_id = employee_obj.browse(cr, uid, employee_ids[0], context=context).department_id.id
+        return department_id
+
     _defaults = {
-        'department_id': lambda s,cr,uid,c: s.pool.get('res.users').browse(cr,uid,uid).context_department_id.id,
+        'department_id': _get_department,
     }
 
 crm_sales_team()
 
 class crm_lead(osv.osv):
     _inherit = "crm.lead"
-    
+
     def onchange_section_id(self, cr, uid, ids, section_id=False, context=None):
-        """ Updates res dictionary with the department corresponding to the section
-        """
+        print "onchange_section_id"
+        """ Updates res dictionary with the department corresponding to the section """
+        if context is None:
+            context = {}
         res = {}
         if section_id:
             section = self.pool.get('crm.case.section').browse(cr, uid, section_id, context=context)
             if section.department_id.id:
                 res.update({'department_id': section.department_id.id})
-        return {
-            'value':res
-        }
-    
-    
+
+        return {'value':res}
+
     _columns = {
         'department_id': fields.many2one('hr.department', 'Department'),
     }
 
 crm_lead()
 
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
