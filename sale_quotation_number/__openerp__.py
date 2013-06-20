83d596db0345554430564a2fa957f51dbe2c67a4
Nhomar Hernandez <nhomar@gmail.com>
2013-06-19 22:45:22 -0530

    [IMP] Documentation and PEP8 applied

--- sale_quotation_number/__openerp__.py
+++ sale_quotation_number/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#  
+#
 #    Copyright (c) 2010-2012 Elico Corp. All Rights Reserved.
 #    Author:            Andy Lu <andy.lu@elico-corp.com>
 #    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
@@ -28,16 +28,60 @@
     'category': 'Sales Management',
     'description': """
 This module adds a sale quotation sequence.
-When you create e quotation, it is numbered using the 'sale.quotation' sequence.
-When you confirm a quotation, its orginal number is saved in the 'origin' field and the sale order gets a new number, retrieving it from 'sale.order' sequence.
-    """,
+===========================================
+
+Defining.
+
+*Sale Quotation:* Sale process in draft stage just informing prices and element
+of communication.
+*Sale Order:* Sale process confirmed, the customer already have a compromise with
+us in terms of pay an invoice and receive our product or service.
+
+Originally OpenERP manage only 1 sequence for this 2 documents, then the sales
+order won and lost manage the same sequence losting almost all lost quotations
+in terms of sequences, making so difficult understand qith a quick view if we
+are good or bad in terms of logistic and sale process already confirmed.
+
+Technical Explanation:
+
+When you create a quotation, it is numbered using the 'sale.quotation'
+sequence.  When you confirm a quotation, its orginal number is saved in the
+'origin' field and the sale order gets a new number, retrieving it from
+'sale.order' sequence.
+
+With Openerp Base.
+
+Sale Quotation 1 Number = SO001
+Sale Quotation 2 Number = SO002
+Sale Quotation 3 Number = SO003
+Sale Quotation 4 Number = SO004
+
+Sale Quotation 2 Confirmed = Number for Sale Order SO004
+
+With Openerp + This Module.
+
+
+Sale Quotation 1 Number = SQ001
+Sale Quotation 2 Number = SQ002
+Sale Quotation 3 Number = SQ003
+Sale Quotation 4 Number = SQ004
+
+Sale Quotation 2 Confirmed = Number for Sale Order SO001 from Sale Quotation
+SQ002
+
+Sale Quotation 1 Confirmed = Number for Sale Order SO002 from Sale Quotation
+SQ001
+
+Sale Quotation 4 Confirmed = Number for Sale Order SO003 from Sale Quotation
+SQ004
+""",
     'author': ['Elico Corp', 'Agile Business Group'],
     'website': 'http://www.openerp.net.cn',
     'depends': ['sale'],
     'data': [
         'sale_quotation_view.xml',
     ],
-    'demo': [], 
+    'demo': [],
     'test': [],
     'installable': True,
     'active': False,
