abb674056923b0b867ba114062fa3ed5ec7684df
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-07-18 15:43:33 +0200

    [CHG] partner_prepayment: the 'Use prepayment' option is delegated to the commercial entity, never set on contacts. (../trunk-generic/ rev 15.3.2)

--- partner_prepayment/model/res_partner.py
+++ partner_prepayment/model/res_partner.py
@@ -26,5 +26,13 @@ class res_partner(orm.Model):
     _inherit = 'res.partner'
 
     _columns = {
-        'use_prepayment': fields.boolean('Use prepayment'),
+        'use_prepayment': fields.boolean(
+            'Use prepayment',
+            help="When prepayment is activated, the sales order policy "
+                 "will be automatically defined to pay before delivery."),
     }
+
+    def _commercial_fields(self, cr, uid, context=None):
+        fields =  super(res_partner, self)._commercial_fields(cr, uid, context=context)
+        fields.append('use_prepayment')
+        return fields
