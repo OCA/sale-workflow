502fb579d645dd7caf47b44f17c9bc8e60b0b8cf
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-10-30 08:43:03 +0100

    [ADD] Camptocamp as author alongside Akretion

--- sale_automatic_workflow/__openerp__.py
+++ sale_automatic_workflow/__openerp__.py
@@ -1,24 +1,24 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-#    sale_automatic_workflow for OpenERP                                        #
-#    Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>   #
-#                                                                               #
-#    This program is free software: you can redistribute it and/or modify       #
-#    it under the terms of the GNU Affero General Public License as             #
-#    published by the Free Software Foundation, either version 3 of the         #
-#    License, or (at your option) any later version.                            #
-#                                                                               #
-#    This program is distributed in the hope that it will be useful,            #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#    GNU Affero General Public License for more details.                        #
-#                                                                               #
-#    You should have received a copy of the GNU Affero General Public License   #
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                               #
-#################################################################################
-
+##############################################################################
+#
+#   sale_automatic_workflow for OpenERP
+#   Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+#   Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': 'Sale Automatic Workflow',
@@ -49,7 +49,7 @@ A workflow can:
 This module is used by Magentoerpconnect and Prestashoperpconnect.
 It is well suited for other E-Commerce connectors as well.
 """,
-    'author': 'Akretion',
+    'author': 'Akretion,Camptocamp',
     'website': 'http://www.akretion.com/',
     'depends': [
         'sale_payment_method',
