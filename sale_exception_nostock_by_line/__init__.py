d3fd0d4f2220cee440f0af1a9ed3efd5cfd9444c
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-11-01 10:06:17 +0100

    [FIX] Correct author (../trunk-generic/ rev 29.1.22)

--- sale_exception_nostock_by_line/__init__.py
+++ sale_exception_nostock_by_line/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Guewen Baconnier
+#    Author: Joel Grand-Guillaume
 #    Copyright 2013 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
