1c0c3eb404724ec957052b124ccd06c934228ea2
unknown <nicolas.bessi@camptocamp.com>
2013-11-08 09:35:37 +0100

    [FIX] sale_validity: import style + licence + manifest description + PEP8

--- sale_validity/model/sale_order.py
+++ sale_validity/model/sale_order.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
-
+##############################################################################
+#
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from osv import fields, osv
-from tools.translate import _
-from openerp import SUPERUSER_ID
 
-"""
-* Pre-book stock while sale order is not yet confirmed.
-    Create a stock move (without picking and procurement) to decrease virtual stock. That reservation gets updated with the sale order line.
-    If a reservation is existing at order confirmation, use it in the generated picking.
-"""
 
 class sale_order(osv.osv):
     _inherit = "sale.order"
 
-    _columns = {
-        'date_validity': fields.date("Valid Until", help="Define date until when quotation is valid",
-                                     readonly=True,
-                                     states={
-                                        'draft':[('readonly',False)],
-                                        'sent':[('readonly',True)], #don't allow to modify validity date when quotation has been send
-                                     },
-                                     track_visibility='onchange',
-                                     ),
-    }
-
+    _columns = {'date_validity': fields.date("Valid Until",
+                                             help="Define date until when quotation is valid",
+                                             readonly=True,
+                                             states={
+                                                 'draft': [('readonly', False)],
+                                                 'sent': [('readonly', True)],
+                                             },
+                                             track_visibility='onchange')}
