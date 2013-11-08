cbb4337abaff777359792af121f0406cd02f25d3
unknown <nicolas.bessi@camptocamp.com>
2013-11-08 10:04:46 +0100

    [PEP8]

--- sale_stock_prebook/__openerp__.py
+++ sale_stock_prebook/__openerp__.py
@@ -19,18 +19,15 @@
 #
 ##############################################################################
 
-{
-    "name": "Sales Quotation Pre-booking of stock",
-    "version": "7.0.0",
-    "depends": ["sale_validity","sale_stock"],
-    "author": "Camptocamp",
-    "website": "http://www.camptocamp.com",
-    "category": "Sales",
-    "description": "Allow to reserve stock (virtual quantity) during quotation",
-    'data': [
-        "wizard/sale_stock_prebook.xml",
-        "view/sale_order.xml",
-        ],
-    'installable': True,
-    'active': False,
-}
+{"name": "Sales Quotation Pre-booking of stock",
+ "version": "7.0.0",
+ "depends": ["sale_validity", "sale_stock"],
+ "author": "Camptocamp",
+ "website": "http://www.camptocamp.com",
+ "category": "Sales",
+ "description": "Allow to reserve stock (virtual quantity) during quotation",
+ 'data': ["wizard/sale_stock_prebook.xml",
+          "view/sale_order.xml"],
+ 'installable': True,
+ 'active': False,
+ }
