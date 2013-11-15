f91be6d0870fd18567f0735f3325a58237b58011
unknown <nicolas.bessi@camptocamp.com>
2013-11-15 14:37:40 +0100

    [TYPO]

--- sale_exception_nostock/__openerp__.py
+++ sale_exception_nostock/__openerp__.py
@@ -31,7 +31,7 @@ Sale stock exception
 
 This addon adds two new sales exceptions to be used by sale_exception addon.
 The first one will ensure that an order line can be delivered on the delivery
-date if it is in MTS. Validation is done by using the order line location via
+$date if it is in MTS. Validation is done by using the order line location via
 related shop and using the line delay.
 
 The second one will create a sales exception if the current SO will break a
@@ -44,7 +44,7 @@ they must pass by the location related to the SO line, else they will be ignored
 
 The second test is a workaround to compensate the lack of
 stock reservation process in OpenERP. This can be a performance killer
-and should not be be used if you have hundreds of simultanous open SO.
+and should not be be used if you have hundreds of simultaneous open SO.
 """,
  'website': 'http://www.camptocamp.com',
  'data': ["data/data.xml"],
