94cd1d986f435219660dabce9370d067c1124966
unknown <alexandre.fayolle@camptocamp.com>
2014-02-06 11:45:51 +0100

    [REF] wrap long lines

--- purchase_order_reorder_lines/purchase.py
+++ purchase_order_reorder_lines/purchase.py
@@ -21,27 +21,37 @@
 
 from openerp.osv import orm, fields
 
+
 class purchase_order_line(orm.Model):
     _inherit = 'purchase.order.line'
     _columns = {
-        'sequence': fields.integer('Sequence', help="Gives the sequence of this line when displaying the purchase order."),
+        'sequence': fields.integer('Sequence',
+                                   help="Gives the sequence of this line when "
+                                        "displaying the purchase order."),
         }
     _order = 'order_id desc, sequence, id'
     _defaults = {'sequence': 10,
                  }
 
+
 class purchase_order(orm.Model):
     _inherit = 'purchase.order'
 
     def _prepare_inv_line(self, cr, uid, account_id, order_line, context=None):
-        res = super(purchase_order, self)._prepare_inv_line(cr, uid, account_id, order_line, context)
+        res = super(purchase_order, self)._prepare_inv_line(cr, uid,
+                                                            account_id,
+                                                            order_line, context)
         res['sequence'] = order_line.sequence
         return res
 
     def _prepare_order_line_move(self, cr, uid, order, line, picking_id, context=None):
-        res = super(purchase_order, self)._prepare_order_line_move(cr, uid, order, line, picking_id)
+        res = super(purchase_order, self)._prepare_order_line_move(cr, uid,
+                                                                   order,
+                                                                   line,
+                                                                   picking_id)
         res['sequence'] = line.sequence
         return res
+
 
 class purchase_line_invoice(orm.TransientModel):
     _inherit = 'purchase.order.line_invoice'
@@ -50,13 +60,23 @@ class purchase_line_invoice(orm.TransientModel):
         invoice_line_obj = self.pool.get('account.invoice.line')
         purchase_line_obj = self.pool.get('purchase.order.line')
         res = super(purchase_line_invoice, self).makeInvoices(cr, uid, ids, context)
-        invoice_ids = eval(res['domain'])[0][-1] # OMG :-(
-        invoice_line_ids = invoice_line_obj.search(cr, uid, [('invoice_id', 'in', invoice_ids)], context=context)
-        for invoice_line in invoice_line_obj.browse(cr, uid, invoice_line_ids, context=context):
-            order_line_ids = purchase_line_obj.search(cr, uid, [('invoice_lines', '=', invoice_line.id)], context=context)
+        invoice_ids = eval(res['domain'])[0][-1]  # OMG :-(
+        invoice_line_ids = invoice_line_obj.search(cr, uid,
+                                                   [('invoice_id', 'in', invoice_ids)],
+                                                   context=context)
+        for invoice_line in invoice_line_obj.browse(cr, uid,
+                                                    invoice_line_ids,
+                                                    context=context):
+            order_line_ids = purchase_line_obj.search(cr, uid,
+                                                      [('invoice_lines', '=', invoice_line.id)],
+                                                      context=context)
             if not order_line_ids:
                 continue
-            order_line_seq = purchase_line_obj.read(cr, uid, order_line_ids[0], ['sequence'], context=context)['sequence']
+            record_data = purchase_line_obj.read(cr, uid,
+                                                 order_line_ids[0],
+                                                 ['sequence'],
+                                                 context=context)
+            order_line_seq = record_data['sequence']
             if not invoice_line.sequence:
                 invoice_line.write({'sequence': order_line_seq})
         return res
--- stock_picking_reorder_lines/stock.py
+++ stock_picking_reorder_lines/stock.py
@@ -25,7 +25,9 @@ from openerp.osv import orm, fields
 class stock_move(orm.Model):
     _inherit = 'stock.move'
     _columns = {
-        'sequence': fields.integer('Sequence', help="Gives the sequence of this line when displaying the picking."),
+        'sequence': fields.integer('Sequence',
+                                   help="Gives the sequence of this line when "
+                                        "displaying the picking."),
         }
     _order = 'date_expected desc, sequence, id'
     _defaults = {'sequence': 10,
@@ -37,8 +39,13 @@ class stock_picking(orm.Model):
 
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id,
                               invoice_vals, context=None):
-        res = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id,
-                                                          invoice_vals, context)
+        res = super(stock_picking, self)._prepare_invoice_line(cr, uid,
+                                                               group,
+                                                               picking,
+                                                               move_line,
+                                                               invoice_id,
+                                                               invoice_vals,
+                                                               context)
         res['sequence'] = move_line.sequence
         return res
 
@@ -47,6 +54,11 @@ class sale_order(orm.Model):
     _inherit = 'sale.order'
 
     def _prepare_order_line_move(self, cr, uid, order, line, picking_id, date_planned, context=None):
-        res = super(sale_order, self)._prepare_order_line_move(cr, uid, order, line, picking_id, date_planned, context)
+        res = super(sale_order, self)._prepare_order_line_move(cr, uid,
+                                                               order,
+                                                               line,
+                                                               picking_id,
+                                                               date_planned,
+                                                               context)
         res['sequence'] = line.sequence
         return res
