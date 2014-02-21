8601c7e030921ad330e3bc8d2516c14b2454b7d5
Romain Deheele <romain.deheele@camptocamp.com>
2014-02-21 12:39:15 +0100

    [ADD] add product_procurement_type_dropshipping addon

---
+++ product_procurement_type_dropshipping/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Romain Deheele
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Product Procurement Type Dropshipping",
+    "version": "1.0",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "description":
+    """
+    Add a choice named 'dropshipping' in procurement_type selection
+    introduced by product_procurement_type addon.
+    Choose "Dropshipping" changes automatically
+    procurement method as "make to order" and supply method as "buy".
+    It checks if at least one supplier is associated on supplier list
+    in product form.
+
+    """,
+    "website": "http://camptocamp.com",
+    "depends": ['product_procurement_type',
+                'sale_dropshipping'],
+    "data": ['product_view.xml'],
+    "demo": [],
+    "test": ['test/test_onchange_procurement_type.yml'],
+    "active": False,
+    "installable": True,
+}
---
+++ product_procurement_type_dropshipping/product.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Romain Deheele
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+
+class product_template(orm.Model):
+
+    _inherit = 'product.template'
+
+    def get_procurement_type_selection(self, cr, uid, context=None):
+        """
+        Adds type dropshipping in procurement_type selection
+        """
+        res = super(product_template, self).get_procurement_type_selection(
+            cr, uid, context=context)
+        res.append(('direct_delivery', 'Drop Shipping'))
+        return res
+
+    def _compute_procurement_vals(self, vals):
+        vals = super(product_template, self)._compute_procurement_vals(vals)
+        if vals['procurement_type'] == 'direct_delivery':
+            vals.update({'procure_method': 'make_to_order',
+                         'supply_method': 'buy',
+                         })
+        return vals
+
+    def _check_sellers(self, cr, uid, ids, procurement_type, context=None):
+        dd_flag = procurement_type == 'direct_delivery'
+        seller_pool = self.pool['product.supplierinfo']
+        seller_ids = seller_pool.search(cr, uid, [('product_id', 'in', ids)],
+                                        context=context)
+        if seller_ids:
+            seller_pool.write(cr, uid, seller_ids,
+                              {'direct_delivery_flag': dd_flag},
+                              context=context)
+        else:
+            if dd_flag:
+                raise orm.except_orm(
+                    _('Warning!'),
+                    _('No suppliers defined'))
+
+    _columns = {
+        'procurement_type': fields.selection(
+            get_procurement_type_selection,
+            'Procurement Type',
+            required=True,
+            help='Standard: Procurement Method: Make to Stock, '
+                 'Supply Method: Buy.\n'
+                 'Bill of Materials: Procurement Method: Make to Order, '
+                 'Supply Method: Produce.\n'
+                 'DropShipping: Procurement Method: Make to Order, '
+                 'Supply Method: Buy.'),
+    }
+
+    def onchange_procurement_type(self, cr, uid, ids, type,
+                                  procurement_type, context=None):
+        res = super(product_template, self). \
+            onchange_procurement_type(cr,
+                                      uid,
+                                      ids,
+                                      type,
+                                      procurement_type)
+        if type != 'service':
+            res['value'].update(
+                self._compute_procurement_vals(
+                    {'procurement_type': procurement_type})
+                )
+            self._check_sellers(cr, uid, ids, procurement_type,
+                                context=context)
+        return res
+
+    def write(self, cr, uid, ids, vals, context=None):
+        for product in self.read(cr, uid, ids, ['type'], context=context):
+            if product['type'] != 'service' and \
+                    vals.get('procurement_type'):
+                vals = self._compute_procurement_vals(vals)
+                self._check_sellers(cr, uid, ids,
+                                    vals['procurement_type'],
+                                    context=context)
+        res = super(product_template, self).write(cr, uid, ids,
+                                                  vals, context=context)
+        return res
+
+    def create(self, cr, uid, vals, context=None):
+        if vals['type'] != 'service' and vals.get('procurement_type'):
+            vals = self._compute_procurement_vals(vals)
+        return super(product_template, self).create(cr, uid, vals,
+                                                    context=context)
