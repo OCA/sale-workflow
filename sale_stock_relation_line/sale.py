a3a6aae8b1a898397bd6d3ffc475037d46837c3b
chafique delli <chafique.delli@akretion.com>
2014-04-01 16:15:08 +0200

    [ADD]:module that creates links between parent lines orders and child in stock.move

---
+++ sale_stock_relation_line/sale.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Chafique Delli
+#    Copyright 2014 Akretion SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm
+from openerp.osv import fields
+
+
+class sale_order_line(orm.Model):
+    _inherit = 'sale.order.line'
+
+    _columns = {
+        'line_parent_id': fields.many2one('sale.order.line', 'Parent Line'),
+        'line_child_id': fields.one2many('sale.order.line', 'line_parent_id', 'Children Line'),
+        }
