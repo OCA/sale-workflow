e41fa07bd9e7fe55235e4be29f0daf002e7db539
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-06-19 13:12:42 +0200

    English correction typo and improvement of description

--- last_sale_price/__openerp__.py
+++ last_sale_price/__openerp__.py
@@ -29,14 +29,16 @@ Last Sale Price
 Add price, quantity and date of a product of the last time it was sold to
 a partner.
 
-In order to let the sellerman know if a customer already ordrered a product
-to give him hint of what price he should propose, those information will be
-shown next to the price in sale order line Form.
+In order to let the salesman know if a customer already ordered a product.
+And to give him hint about what price he should propose.
+That information is shown next to the price in Sale Order's line Form.
 
-Only confirmed and done sale orders line are considered.
+Only Sale Orders' lines in state Confirmed and Done are considered to
+compute those fields.
 
-If multiple sale order lines for the same partner where made on the same
-date, the sum of all qty and the mean of price will be displayed.
+If multiple Sale Order lines for the same partner where made on the same
+date for the same product, the sum of all qty and the average price will be
+displayed.
 """,
  'author': 'Camptocamp',
  'maintainer': 'Camptocamp',
