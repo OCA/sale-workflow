ac115962636be9b8c0cdb1258e91a7c6a83dc962
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-07-17 17:48:52 +0200

    [add] new module sale_default_discount

---
+++ sale_default_discount/__openerp__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Leonardo Pistone
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Default Discount',
+    'version': '1.0',
+    'category': 'Generic Modules/Sale',
+    'description': """
+Sale Default Discount
+=====================
+
+This module allows to set a default discount per customer and per sale order.
+
+The customer discount is used by default when creating an order, and the
+order default is used when creating new order lines.
+
+The defaults can be left empty, keeping the default behaviour.
+
+Contributors
+------------
+
+  * Leonardo Pistone <leonardo.pistone@camptocamp.com>
+
+""",
+    'author': 'Camptocamp',
+    'depends': ['sale'],
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        'view/partner.xml',
+        'view/sale.xml',
+    ],
+    'test': [
+    ],
+    'demo': [],
+    'installable': True,
+    'active': False,
+}
