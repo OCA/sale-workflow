f8c1c844d871cfaa5b65db0151f17ba5aa007640
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-07 12:23:36 +0200

    [FIX] repository PEP8

--- sale_quotation_number/sale_quotation.py
+++ sale_quotation_number/sale_quotation.py
@@ -1,4 +1,4 @@
-##############################################################################
+#
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 from openerp.osv import fields, orm
 
