220795ab74c0903db08eb670ac3bf23c9d356c5d
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-07 16:03:32 +0200

    [IMP] on_change with web_context_tunnel

--- product_price_properties_based/sale.py
+++ product_price_properties_based/sale.py
@@ -26,19 +26,22 @@ from openerp.tools.translate import _
 class SaleOrderLine(orm.Model):
     _inherit = 'sale.order.line'
 
-    def product_id_change_with_properties(
+    def product_id_change(
         self, cr, uid, ids, pricelist, product, qty=0,
         uom=False, qty_uos=0, uos=False, name='', partner_id=False,
         lang=False, update_tax=True, date_order=False, packaging=False,
-        fiscal_position=False, flag=False, context=None, properties=False
+        fiscal_position=False, flag=False, context=None
     ):
-        res = self.product_id_change(
+        res = super(SaleOrderLine, self).product_id_change(
             cr, uid, ids, pricelist, product, qty=qty,
             uom=uom, qty_uos=qty_uos, uos=uos,
             name=name, partner_id=partner_id,
             lang=lang, update_tax=update_tax,
             date_order=date_order, packaging=packaging,
             fiscal_position=fiscal_position, flag=flag, context=context)
+        if context is None:
+            context = {}
+        properties = context.get('property_ids')
         if properties and product:
             prop_dict = {}
             prop_pool = self.pool['mrp.property']
