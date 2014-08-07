09d9131eed98395a9c07bf6cb200ba577d3f1d25
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-07 12:23:36 +0200

    [FIX] repository PEP8

--- sale_cancel_reason/__init__.py
+++ sale_cancel_reason/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+#
 #
 #    Author: Guewen Baconnier
 #    Copyright 2013 Camptocamp SA
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 from . import wizard
 from . import model
