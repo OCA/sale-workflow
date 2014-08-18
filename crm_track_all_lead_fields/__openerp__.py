027e3d2bb027c258786ec680a420c7a902e78bd8
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2014-08-18 10:02:13 -0400

    [ADD] crm_track_all_lead_leads

---
+++ crm_track_all_lead_fields/__openerp__.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'CRM Track All Lead Fields',
+    'version': '0.1',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'category': 'Others',
+    'summary': 'Track all lead fields for changes',
+    'description': """
+CRM Track All Lead Fields
+=========================
+
+This modules makes all fields trackable for changes in leads.
+
+Contributors
+------------
+* Vincent Vinet (vincent.vinet@savoirfairelinux.com)
+""",
+    'depends': [
+        'crm',
+    ],
+    'external_dependencies': {
+        'python': [],
+    },
+    'data': [
+        'crm_data.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ crm_track_all_lead_fields/crm_lead.py
@@ -0,0 +1,48 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm
+
+from openerp.addons.crm.crm_lead import crm_lead
+
+
+new_tracked_fields = []
+for name, column in crm_lead._columns.items():
+    if name == 'date_action_last':
+        # This one causes double track updates
+        continue
+
+    track = getattr(column, "track_visibility", False)
+    if track not in ("always", "onchange"):
+        new_tracked_fields.append(name)
+        column.track_visibility = "onchange"
+
+
+class CRMLead(orm.Model):
+    _name = 'crm.lead'
+    _inherit = 'crm.lead'
+
+    _track = dict(
+        (field, {
+            'crm_track_all_fields.mt_field_updated': lambda self, cr, uid, obj, ctx=None: True,
+        }) for field in new_tracked_fields
+    )
