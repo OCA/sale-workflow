0109689e21235faf8dd119d24e6da42e552a83e4
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-08-25 17:17:49 +0200

    add tests

--- sale_validity/__openerp__.py
+++ sale_validity/__openerp__.py
@@ -39,5 +39,8 @@ A default validity duration (in days) can be configured on the company.
      "view/sale_order.xml",
      "view/company_view.xml",
  ],
+ 'test': [
+     'test/sale_validity.yml',
+ ],
  'installable': True,
  }
