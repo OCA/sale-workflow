63fd1484b07c05e9dd7b636682cf14aab396546f
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-08-25 16:50:05 +0200

    [FIX] s/nsale/sale/

--- sale_properties_dynamic_fields/mrp_property_group.py
+++ sale_properties_dynamic_fields/mrp_property_group.py
@@ -27,7 +27,7 @@ class MrpPropertyGroup(orm.Model):
     _inherit = 'mrp.property.group'
     _columns = {
         'draw_dynamically': fields.boolean(
-            'Draw dynamically', help="In nsale order line, draw this property "
+            'Draw dynamically', help="In sale order line, draw this property "
                                      "dynamically, as text field"),
         'field_id': fields.many2one('ir.model.fields', 'Field', readonly=True),
         }
