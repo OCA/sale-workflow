a5b105b497100444ef5c43d94a592c23fa4b263a
EL HADJI DEM <elhadji.dem@savoirfairelinux.com>
2014-09-05 10:37:53 -0400

    [ADD] Add a new column amount_shipped; add a predefine filter (amount_shipped <>0)

---
+++ sale_order_amount_shipped/__openerp__.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sale Order Amount Shipped',
+    'version': '0.1',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'category': 'Sale',
+    'summary': 'Show amount left to ship in sale order tree',
+    'description': """
+Sale Order Amount Shipped
+=========================
+
+This module adds a field "amount_shipped" on sale orders and displays
+it in the tree view. The field is calculated as
+   to ship = (total - sum(total_amount for each invoice_id))
+based on 'draft', 'proformat', 'open' and 'done' invoices.
+
+Contributors
+------------
+* El Hadji Dem (elhadji.dem@savoirfairelinux.com)
+""",
+    'depends': [
+        'sale_order_amount_to_invoice',
+    ],
+    'external_dependencies': {
+        'python': [],
+    },
+    'data': [
+        'sale_order_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
