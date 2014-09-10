f1f2784ed8cfcd3fcd85ec3144b57acdca7d858b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2014-09-10 16:00:40 +0200

    scaffold new addon

---
+++ sale_quotation_sourcing/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+#    Author: Alexandre Fayolle
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    'name': "sale_quotation_sourcing",
+
+    'summary': "manual sourcing of sale quotations",
+    'description': """
+    This module implements manual sourcing of sale order lines from purchase order lines.
+
+    Instead of having a the confirmation of a SO generate procurements which in
+    turn may generate PO, we invert the process: in order to generate a quote
+    for a customer, it can be necessary to ask our suppliers from some
+    quotes. Once the quote is accepted by the customer and the user confirms
+    the SO, a wizard is presented which enables choosing the differente PO to
+    use to source the SO lines. """,
+
+    'author': "Camptocamp",
+    'website': "http://www.camptocamp.com",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'depends': ['sale_stock', 'purchase'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'test': [
+
+        ]
+}
---
+++ sale_quotation_sourcing/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class sale_quotation_sourcing(models.Model):
+#     _name = 'sale_quotation_sourcing.sale_quotation_sourcing'
+
+#     name = fields.Char()
