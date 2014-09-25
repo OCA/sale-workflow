992c868cc27e9b590ea98ca3a8daa24c997e407d
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-09-25 10:48:34 +0200

    say my name

--- sale_quotation_sourcing/model/procurement.py
+++ sale_quotation_sourcing/model/procurement.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#    Author: Alexandre Fayolle
+#    Author: Alexandre Fayolle, Leonardo Pistone
 #    Copyright 2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
