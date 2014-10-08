413d5cff168ea7a2ccc335732301a55f5d080637
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-08 12:19:30 +0200

    check for consistency between procurement and PO
    This check is done when the procurement is processed, which happens when
    the Sale Order is validated. We cannot easily check before validation
    because we need to know what destination location has been chosen in the
    procurement. Just checking the route selected in the Sale Order Line is
    too restrictive because the Route can be chosen in many other places.

--- sale_quotation_sourcing/model/procurement.py
+++ sale_quotation_sourcing/model/procurement.py
@@ -19,6 +19,8 @@
 #
 from openerp import models, api, _
 
+from openerp.exceptions import Warning
+
 
 class ProcurementOrder(models.Model):
     _inherit = 'procurement.order'
@@ -42,6 +44,21 @@ class ProcurementOrder(models.Model):
 
             if sale_line and sale_line.manually_sourced:
                 po_line = sale_line.sourced_by
+
+                if po_line.order_id.location_id != procurement.location_id:
+                    raise Warning(_(
+                        'The manually sourced Purchase Order has Destination '
+                        'location {}, while the Procurement was generated '
+                        'with destination {}. To solve the problem, please '
+                        'source a Sale Order Line with a Purchase Order '
+                        'consistent with the active Route. For example, if '
+                        'the active route is Drop Shipping, the chosen PO '
+                        'should have destination location Customers.'.format(
+                            po_line.order_id.location_id.name,
+                            procurement.location_id.name
+                        )
+                    ))
+
                 res[procurement.id] = po_line.id
                 procurement.purchase_line_id = po_line
                 procurement.message_post(body=_('Manually sourced'))
