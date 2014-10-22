0a6b4d1adfc01c64079ba247938f89fa875aa8b1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-10-22 10:16:53 +0200

    Added security accesses for the sales users and managers

--- sale_condition_template/__openerp__.py
+++ sale_condition_template/__openerp__.py
@@ -27,6 +27,7 @@
  "author": "Camptocamp",
  "data": ["sale_order_view.xml",
           'base_condition_template_view.xml',
+          'security/ir.model.access.csv',
           ],
  "category": "Sale",
  "installable": True,
