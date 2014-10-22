e98a3dd3677b049b3e56f05715b8f9d3d2c275f9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-10-22 09:53:28 +0200

    Extract the module description in README.rst

--- sale_condition_template/__openerp__.py
+++ sale_condition_template/__openerp__.py
@@ -24,35 +24,6 @@
  "depends": ["sale",
              "invoice_condition_template",
              ],
- "description": """
-Sale Conditions
-===============
-
-Adds template condition texts on sales orders and invoices.
-
-Templates are categorized by their position on the document.
-
-Two positions are available:
-
-- before sale order/invoice lines
-- after sale order/invoice lines
-
-Texts are copied on the invoice when you will create invoice from sale order.
-
-Dependencies
-------------
-
-The modules `invoice_condition_template` and `base_condition_template` are
-required. They live in the OCA project `account-invoice-reporting`_.
-
-.. _`account-invoice-reporting`: https://github.com/OCA/account-invoice-reporting
-
-Contributors
-------------
-
-* Nicolas Bessi <nicolas.bessi@camptocamp.com>
-* Yannick Vaucher <yannick.vaucher@camptocamp.com>
-""",
  "author": "Camptocamp",
  "data": ["sale_order_view.xml",
           ],
