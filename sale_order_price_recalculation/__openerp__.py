ad4fde262d67817b44e166ffe3762d8b63f5f43a
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-10-31 14:40:46 +0100

    [ADD] sale_order_recalculate_prices: Module to allow to recalculate on demand sale order line prices
    ==================================================
    Recalculation of sale order lines prices on demand
    ==================================================
    
    This module adds a button on sale orders below pricelist that recalculates the
    prices of the order lines that contain a product in them.
    
    It is launched manually as a button to get the user decide if he/she wants to
    recalculate prices when pricelist is changed.
    
    Usage
    =====
    
    Inside a sale order, you can click in any moment a button called
    "(Recalculate prices)", that is next to the pricelist selection, to launch
    a recalculation of all the prices of the lines, losing previous custom prices.
    
    Known issues / Roadmap
    ======================
    
    * In a sale order with lot of lines, the recalculation may slow down, because
      sale general data (amount untaxed, amount taxed...) are recalculated for
      each line.

---
+++ sale_order_price_recalculation/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# (c) 2014 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
+# (c) 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# (c) 2015 Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Price recalculation in sales orders",
+    "version": "8.0.1.0.0",
+    "depends": [
+        "sale",
+    ],
+    'license': 'AGPL-3',
+    "author": "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Grupo Vermon,"
+              "Odoo Community Association (OCA)",
+    "category": "Sales Management",
+    "website": "http://github.com/OCA/sale-workflow",
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
