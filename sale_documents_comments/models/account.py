308fbacd058fec743394d6970119a1a217ba3b5f
oihane <oihanecruce@gmail.com>
2014-11-03 12:00:45 +0100

    [MOD] Added translations
    [FIX] Pylint fixing <sale_documents_comments>
    
    [FIX] Pylint fixing <warning_log>
    
    [MOD] Translated module <sale_documents_comments>

--- sale_documents_comments/models/account.py
+++ sale_documents_comments/models/account.py
@@ -26,12 +26,12 @@ class AccountInvoice(orm.Model):
         'sale_comment': fields.text('Internal comments'),
     }
 
-    def onchange_partner_id(self, cr, uid, ids, type, partner_id,
+    def onchange_partner_id(self, cr, uid, ids, invoice_type, partner_id,
                             date_invoice=False, payment_term=False,
                             partner_bank_id=False, company_id=False,
                             context=None):
         val = super(AccountInvoice, self).onchange_partner_id(
-            cr, uid, ids, type, partner_id, date_invoice=date_invoice,
+            cr, uid, ids, invoice_type, partner_id, date_invoice=date_invoice,
             payment_term=payment_term, partner_bank_id=partner_bank_id,
             company_id=company_id, context=context)
         if partner_id:
