229d4311efdec052c0162ee2b125a20b08a94e2d
Leonardo Donelli <learts92@gmail.com>
2014-11-10 16:23:20 +0100

    Change description to reflect implementation changes, small xml fixes.

--- sale_order_authorized_users/__openerp__.py
+++ sale_order_authorized_users/__openerp__.py
@@ -27,14 +27,27 @@
     'summary': 'Sale orders, Security, Permissions, Users',
     'description': """
 Sale Order Authorized Users
-======================================
+===========================
+This module allows you to implement "secret" sale orders.
+Users in the 'Sale / Secret orders' group will see a new field on sale orders,
+"Allowed Users", that can be used to restrict access to that sale order to
+selected users.
 
-Let Admin (or any user in the base.group_erp_manager) choose,
-for each sale order, which users will be able to access and see it.
-Any other user won't be able to see it.
-If no users are set, the sale order has normal permissions.
-The field to set allowed users will be visibile only to admin, which makes it
-possible to make the users unaware of this feature.
+Details
+-------
+This acts as an **additional restriction**, access rules are still considered:
+for example, an user in the "Own leads only" group won't be able to see an order
+that it's not his, even if he's selected as allowed.
+
+On the other hand, by setting some users as allowed, all other
+users, even if they are "See all leads", won't be able to see the order.
+
+*When you add allowed users to a sale order you should include yourself, or you
+won't be able to access it anymore!*
+
+If no users are set (the default), all users are considered as allowed,
+so the sale order follows the "normal" access rules with no additional
+restrictions.
 """,
     'author': 'Creativi Quadrati',
     'website': 'http://www.creativiquadrati.it',
