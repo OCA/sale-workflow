fd85a442d7fcf832c6c7c9bae0b6e144a58c0978
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-20 16:02:31 +0100

    New API for sale.workflow.process

--- sale_automatic_workflow/sale_workflow_process.py
+++ sale_automatic_workflow/sale_workflow_process.py
@@ -3,6 +3,8 @@
 #
 #    sale_automatic_workflow for OpenERP
 #    Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+#    Author: Guewen Baconnier
+#    Copyright 2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,12 +21,11 @@
 #
 ###############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class sale_workflow_process(orm.Model):
-    """
-    A workflow process is the setup of the automation of a sales order.
+class SaleWorkflowProcess(models.Model):
+    """ A workflow process is the setup of the automation of a sales order.
 
     Each sales order can be linked to a workflow process.
     Then, the options of the workflow will change how the sales order
@@ -36,42 +37,42 @@ class sale_workflow_process(orm.Model):
     _name = "sale.workflow.process"
     _description = "Sale Workflow Process"
 
-    _columns = {
-        'name': fields.char('Name', size=64),
-        'picking_policy': fields.selection(
-            [('direct', 'Deliver each product when available'),
-             ('one', 'Deliver all products at once')],
-            string='Shipping Policy'),
-        'order_policy': fields.selection([('prepaid', 'Before Delivery'),
-                                          ('manual', 'On Demand'),
-                                          ('picking', 'On Delivery Order')],
-                                         string='Invoice Policy'),
-        'invoice_quantity': fields.selection(
-            [('order', 'Ordered Quantities'),
-             ('procurement', 'Shipped Quantities')],
-            string='Invoice on'),
-        'validate_order': fields.boolean('Validate Order'),
-        'create_invoice_on': fields.selection(
-            [('manual', 'No automatic invoice'),
-             ('on_order_confirm', 'On confirmation of Sale Order'),
-             ('on_picking_done', 'After Delivery')],
-            required=True,
-            string='Create Invoice'),
-        'validate_invoice': fields.boolean('Validate Invoice'),
-        'validate_picking': fields.boolean('Confirm and Close Picking'),
-        'invoice_date_is_order_date': fields.boolean(
-            'Force Invoice Date',
-            help="When checked, the invoice date will be "
-                 "the same than the order's date"),
-        'warning': fields.text('Warning Message', translate=True,
-                               help='if set, display the message when a '
-                               'user selects the process on a sale order'),
-    }
-
-    _defaults = {
-        'picking_policy': 'direct',
-        'order_policy': 'manual',
-        'create_invoice_on': 'manual',
-        'invoice_quantity': 'order',
-        'validate_invoice': False,
-    }
+    name = fields.Char()
+    picking_policy = fields.Selection(
+        selection=[('direct', 'Deliver each product when available'),
+                   ('one', 'Deliver all products at once')],
+        string='Shipping Policy',
+        default='direct',
+    )
+    order_policy = fields.Selection(
+        selection=[('prepaid', 'Before Delivery'),
+                   ('manual', 'On Demand'),
+                   ('picking', 'On Delivery Order')],
+        string='Invoice Policy',
+        default='manual',
+    )
+    invoice_quantity = fields.Selection(
+        selection=[('order', 'Ordered Quantities'),
+                   ('procurement', 'Shipped Quantities')],
+        string='Invoice on',
+        default='order',
+    )
+    validate_order = fields.Boolean(string='Validate Order')
+    create_invoice_on = fields.Selection(
+        selection=[('manual', 'No automatic invoice'),
+                   ('on_order_confirm', 'On confirmation of Sale Order'),
+                   ('on_picking_done', 'After Delivery')],
+        required=True,
+        string='Create Invoice',
+        default='manual',
+    )
+    validate_invoice = fields.Boolean(string='Validate Invoice')
+    validate_picking = fields.Boolean(string='Confirm and Close Picking')
+    invoice_date_is_order_date = fields.Boolean(
+        string='Force Invoice Date',
+        help="When checked, the invoice date will be "
+             "the same than the order's date"
+    )
+    warning = fields.Text('Warning Message', translate=True,
+                          help='if set, display the message when a '
+                               'user selects the process on a sale order')
