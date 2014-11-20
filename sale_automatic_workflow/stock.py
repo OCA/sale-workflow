afb6509a07408dbf3aa68c30d49f3ddde29178a8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-20 14:13:07 +0100

    stock.picking.out has passed away and methods have changed

--- sale_automatic_workflow/stock.py
+++ sale_automatic_workflow/stock.py
@@ -26,32 +26,21 @@ class stock_picking(orm.Model):
     _inherit = "stock.picking"
 
     _columns = {
-        # if we add the column in stock.picking.out, we can't write it
         'workflow_process_id': fields.many2one('sale.workflow.process',
                                                'Sale Workflow Process'),
     }
 
-    def _prepare_invoice(self, cr, uid, picking, partner, inv_type, journal_id,
-                         context=None):
-        invoice_vals = super(stock_picking, self)._prepare_invoice(
-            cr, uid, picking, partner, inv_type, journal_id, context=context)
-        invoice_vals['workflow_process_id'] = picking.workflow_process_id.id
+    def _create_invoice_from_picking(self, cr, uid, picking, vals,
+                                     context=None):
+        vals['workflow_process_id'] = picking.workflow_process_id.id
         if picking.workflow_process_id.invoice_date_is_order_date:
-            invoice_vals['date_invoice'] = picking.sale_id.date_order
-        return invoice_vals
+            vals['date_invoice'] = picking.sale_id.date_order
 
-
-class stock_picking_out(orm.Model):
-    _inherit = "stock.picking.out"
+        _super = super(stock_picking, self)
+        return _super._prepare_invoice(cr, uid, picking, vals, context=context)
 
     def validate_picking(self, cr, uid, ids, context=None):
         for picking in self.browse(cr, uid, ids, context=context):
-            self.force_assign(cr, uid, [picking.id])
-            partial_data = {}
-            for move in picking.move_lines:
-                partial_data["move" + str(move.id)] = {
-                    'product_qty': move.product_qty,
-                    'product_uom': move.product_uom.id,
-                }
-            self.do_partial(cr, uid, [picking.id], partial_data)
+            self.force_assign(cr, uid, [picking.id], context=context)
+            self.do_transfer(cr, uid, [picking.id], context=context)
         return True
