2986e9dc5a17bee78d410b2b33cce02e19571c16
Adrien Chaussende <chaussende.adrien@gmail.com>
2014-11-24 15:08:08 +0100

    [IMP] Implements some checks on vals before cancel, better cancel log management and fixes some Flake8 on sale_cancel_purchase

--- sale_cancel_purchase/sale.py
+++ sale_cancel_purchase/sale.py
@@ -10,49 +10,42 @@
 
 from osv import orm
 
+
 class SaleOrder(orm.Model):
     _inherit = 'sale.order'
 
     def action_cancel(self, cr, uid, ids, context=None):
         picking_obj = self.pool.get('stock.picking')
         po_line_obj = self.pool.get('purchase.order.line')
-        for order_id in ids:
-            order = self.browse(cr, uid, [order_id], context=context)[0]
+        for order in self.browse(cr, uid, ids, context=context):
             po_line_ids = po_line_obj.search(
-                cr, uid, [('sale_order_id','=',order_id)], context=context
+                cr, uid, [('sale_order_id', '=', order.id)], context=context
             )
             for po_line in po_line_obj.browse(cr, uid, po_line_ids,
-                                                context=context):
-                if not order.cancel_logs:
-                    cancel_logs = ""
-                else:
-                    cancel_logs = order.cancel_logs
-                if po_line.order_id.state in ['draft']:
-                    po_line_obj.unlink(cr, uid, po_line_ids, context=context)
-                    self.write(
-                        cr, uid, order_id,
-                        {'cancel_logs':cancel_logs + "<p>Number of deleted \
-                            lines in Purchase Orders : %d" % len(po_line_ids)},
-                        context=context
-                    )
+                                              context=context):
+                if po_line.state in ['draft']:
+                    po_line_obj.unlink(cr, uid, po_line.id, context=context)
+                    log_line = "<p>Number of deleted lines in Purchase \
+                        Orders: %d</p>" % len(po_line_ids)
                     picking_ids = picking_obj.search(
-                        cr, uid, [('purchase_id','=',order_id)], context=context)
-                    picking_obj.action_cancel(cr, uid, picking_ids, context=context)
-                    self.write(
-                        cr, uid, order_id,
-                        {'cancel_logs':cancel_logs + "<p>Cancel %d incoming \
-                            pickings" % len(picking_ids)},
+                        cr, uid, [('purchase_id', '=', po_line.order_id)],
                         context=context
                     )
+                    for picking in picking_obj.browse(cr, uid, picking_ids,
+                                                      context=context):
+                        if picking.state in ['assigned', 'confirmed', 'draft']:
+                            picking.action_cancel()
+                            log = "<p>Canceled picking in: %s</p>" % \
+                                picking.name
+                        else:
+                            log = "<p>Can't cancel picking in: %s</p>" \
+                                % picking.name
+                        order.add_logs(log)
                 else:
-                    self.write(
-                        cr, uid, order_id,
-                        {'cancel_logs': cancel_logs + "<p>Impossible to cancel Sale\
-                        Order. State is %s" % order.state},
-                        context=context
-                    )
+                    log_line = "<p>Impossible to cancel Purchase Order Line \
+                        for product %s because Line's state is in %s" % \
+                        (po_line.product_id, po_line.state)
+                order.add_logs(log_line)
         return super(SaleOrder, self).action_cancel(
             cr, uid, ids, context=context
         )
-
-
