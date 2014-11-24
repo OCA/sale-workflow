2d89c4252dec01bbbe1904563ecd6f8549adc383
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-24 15:26:56 +0100

    Unused import

--- sale_automatic_workflow/sale.py
+++ sale_automatic_workflow/sale.py
@@ -19,7 +19,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api, _
 
 
 class sale_order(models.Model):
