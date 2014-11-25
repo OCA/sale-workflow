5d17a5090fd702d7b340bb9d14c07fec8284c253
Adrien Chaussende <chaussende.adrien@gmail.com>
2014-11-25 17:29:25 +0100

    [ADD] Buttons on Sale Order form view to directly access to list view of related Purchase Orders (not lines). Kept element on dropdown menu too.

--- sale_purchase_link/sale_purchase_link.py
+++ sale_purchase_link/sale_purchase_link.py
@@ -10,6 +10,7 @@
 
 from osv import orm, fields
 
+
 class ProcurementOrder(orm.Model):
     _inherit = 'procurement.order'
 
@@ -25,8 +26,33 @@ class ProcurementOrder(orm.Model):
         return super(ProcurementOrder, self).create_procurement_purchase_order(
             cr, uid, procurement, po_vals, line_vals, context=context)
 
+
 class PurchaseOrderLine(orm.Model):
     _inherit = 'purchase.order.line'
     _columns = {
         'sale_order_id': fields.many2one('sale.order', 'Source Sale Order'),
     }
+
+
+class SaleOrder(orm.Model):
+    _inherit = 'sale.order'
+
+    def action_view_purchase_order(self, cr, uid, ids, context=None):
+        mod_obj = self.pool.get('ir.model.data')
+        act_obj = self.pool.get('ir.actions.act_window')
+        po_line_obj = self.pool.get('purchase.order.line')
+
+        result = mod_obj.get_object_reference(
+            cr, uid, 'purchase', 'purchase_form_action'
+        )
+        id = result and result[1] or False
+        result = act_obj.read(cr, uid, [id], context=context)[0]
+        line_ids = po_line_obj.search(
+            cr, uid, [('sale_order_id', 'in', ids)], context=context
+        )
+        po_ids = []
+        for line in po_line_obj.browse(cr, uid, line_ids, context=context):
+            po_ids.append(line.order_id.id)
+        result['domain'] = "[('id', 'in', [" + \
+            ','.join(map(str, po_ids)) + "])]"
+        return result
