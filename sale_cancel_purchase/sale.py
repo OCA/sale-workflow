5f241b870e231edfd70014322dd55a3a71eeb338
Adrien Chaussende <chaussende.adrien@gmail.com>
2014-11-28 12:40:56 +0100

    [IMP] Translation on sale_cancel_purchase, better management of exceptions to keep logs written on sale order

--- sale_cancel_purchase/sale.py
+++ sale_cancel_purchase/sale.py
@@ -8,7 +8,8 @@
 #
 ##############################################################################
 
-from osv import orm
+from openerp.osv import orm
+from openerp.tools.translate import _
 
 
 class SaleOrder(orm.Model):
@@ -17,16 +18,18 @@ class SaleOrder(orm.Model):
     def action_cancel(self, cr, uid, ids, context=None):
         picking_obj = self.pool.get('stock.picking')
         po_line_obj = self.pool.get('purchase.order.line')
+        cancel_ids = []
         for order in self.browse(cr, uid, ids, context=context):
             po_line_ids = po_line_obj.search(
                 cr, uid, [('sale_order_id', '=', order.id)], context=context
             )
+            cancel = True
             for po_line in po_line_obj.browse(cr, uid, po_line_ids,
                                               context=context):
                 if po_line.state in ['draft']:
                     po_line_obj.unlink(cr, uid, [po_line.id], context=context)
-                    log_line = "<p>Number of deleted lines in Purchase \
-                        Orders: %d</p>" % len(po_line_ids)
+                    log_line = _("<p>Number of deleted lines in Purchase "
+                                 "Order: %s</p>") % len(po_line_ids)
                     picking_ids = picking_obj.search(
                         cr, uid, [('purchase_id', '=', po_line.order_id.id)],
                         context=context
@@ -35,17 +38,21 @@ class SaleOrder(orm.Model):
                                                       context=context):
                         if picking.state in ['assigned', 'confirmed', 'draft']:
                             picking.action_cancel()
-                            log = "<p>Canceled picking in: %s</p>" % \
-                                picking.name
+                            log = _("<p>Canceled picking in: %s</p>")
                         else:
-                            log = "<p>Can't cancel picking in: %s</p>" \
-                                % picking.name
+                            cancel = False
+                            log = _("<p>Can't cancel picking in: %s</p>")
+                        log %= picking.name
                         order.add_logs(log)
                 else:
-                    log_line = "<p>Impossible to cancel Purchase Order Line \
-                        for product %s because Line's state is in %s" % \
-                        (po_line.product_id, po_line.state)
+                    cancel = False
+                    log_line = _("<p>Impossible to cancel Purchase Order Line "
+                                 "for product %s because Line's state is in %s"
+                                 "</p>")
+                    log_line %= (po_line.product_id.name, po_line.state)
                 order.add_logs(log_line)
+            if cancel:
+                cancel_ids.append(order.id)
         return super(SaleOrder, self).action_cancel(
-            cr, uid, ids, context=context
+            cr, uid, cancel_ids, context=context
         )
