7cebd1fe25ba75bbae012cbff3c3274ef6a2f0a3
Adrien Chaussende <chaussende.adrien@gmail.com>
2014-12-01 16:21:52 +0100

    [IMP] text coloration for errors and update on translations. Some fixes on messages for the cancellation logs

--- sale_cancel_purchase/sale.py
+++ sale_cancel_purchase/sale.py
@@ -24,12 +24,11 @@ class SaleOrder(orm.Model):
                 cr, uid, [('sale_order_id', '=', order.id)], context=context
             )
             cancel = True
+            line_to_cancel = []
             for po_line in po_line_obj.browse(cr, uid, po_line_ids,
                                               context=context):
                 if po_line.state in ['draft']:
-                    po_line_obj.unlink(cr, uid, [po_line.id], context=context)
-                    log_line = _("<p>Number of deleted lines in Purchase "
-                                 "Order: %s</p>") % len(po_line_ids)
+                    line_to_cancel.append(po_line.id)
                     picking_ids = picking_obj.search(
                         cr, uid, [('purchase_id', '=', po_line.order_id.id)],
                         context=context
@@ -38,19 +37,22 @@ class SaleOrder(orm.Model):
                                                       context=context):
                         if picking.state in ['assigned', 'confirmed', 'draft']:
                             picking.action_cancel()
-                            log = _("<p>Canceled picking in: %s</p>")
+                            log = _("Canceled picking in: %s")
                         else:
                             cancel = False
-                            log = _("<p>Can't cancel picking in: %s</p>")
+                            log = _("Can't cancel picking in: %s")
                         log %= picking.name
-                        order.add_logs(log)
+                        order.add_logs(log, cancel)
                 else:
-                    cancel = False
-                    log_line = _("<p>Impossible to cancel Purchase Order Line "
-                                 "for product %s because Line's state is in %s"
-                                 "</p>")
-                    log_line %= (po_line.product_id.name, po_line.state)
-                order.add_logs(log_line)
+                    log = _("Impossible to cancel Purchase Order Line for"
+                            " product %s because Line's state is in %s")
+                    log %= (po_line.product_id.name, po_line.state)
+                    order.add_logs(log, False)
+            if line_to_cancel:
+                po_line_obj.unlink(cr, uid, line_to_cancel, context=context)
+                log = _("Number of deleted lines in Purchase Order: "
+                        "%s") % len(po_line_ids)
+                order.add_logs(log, True)
             if cancel:
                 cancel_ids.append(order.id)
         return super(SaleOrder, self).action_cancel(
