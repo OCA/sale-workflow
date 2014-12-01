d65a10953b8b9d509a514a4a4213e08ee8297bbe
Sébastien BEAU <sebastien.beau@akretion.com>
2014-12-01 23:47:04 +0100

    [REF] refactor sale_cancel_mrp in order to give the posibility to inherit the behaviour easily

--- sale_cancel_mrp/sale.py
+++ sale_cancel_mrp/sale.py
@@ -15,33 +15,74 @@ from openerp.tools.translate import _
 class SaleOrder(orm.Model):
     _inherit = 'sale.order'
 
-    def action_cancel(self, cr, uid, ids, context=None):
+    def _can_cancel_mo_internal_move(self, cr, uid, mo, context=None):
+        """
+        Method that return if it's possible or not to cancel the internal
+        move linked to the manufacturing order
+
+        :param mo: Manufacturing Order
+        :type items: browse_record
+        :return: tuple that contain the following value
+            (able_to_cancel, message, important)
+        """
+        able_to_cancel = False
+        important = False
+        message = ""
+
+        if mo.picking_id.state == 'cancel':
+            pass
+        elif mo.picking_id.state != 'done':
+            able_to_cancel = True
+            message = _("Canceled the internal move %s for the MO %s") \
+                      % (mo.picking_id.name, mo.name)
+        else:
+            important = True
+            message = _("Fail to cancel the internal move %s for the "
+                        "MO %s as it's in the done state") \
+                      % (mo.picking_id.name, mo.name)
+        return able_to_cancel, message, important
+
+    def _can_cancel_mo(self, cr, uid, mo, context=None):
+        """
+        Method that return if it's possible or not to cancel the
+        manufacturing order
+
+        :param mo: Manufacturing Order
+        :type items: browse_record
+        :return: tuple that contain the following value
+            (able_to_cancel, message, important)
+        """
+        able_to_cancel = False
+        important = False
+        message = ""
+
+        if mo.state == 'cancel':
+            pass
+        if mo.state != 'done':
+            able_to_cancel = True
+            message = _("MO %s canceled")
+        else:
+            important = True
+            message = _("Fail to cancel the Manufacturing Order %s as it's"
+                        " already done" % mo.name)
+        return able_to_cancel, message, important
+
+    def _cancel_linked_record(self, cr, uid, order, context=None):
         mrp_prod_obj = self.pool.get('mrp.production')
-        mo_ids = mrp_prod_obj.search(
-            cr, uid, [('sale_order_id', 'in', ids)], context=context
-        )
-        cancel_ids = []
-        for order in self.browse(cr, uid, ids, context=context):
-            cancel = True
-            for mo in mrp_prod_obj.browse(cr, uid, mo_ids, context=context):
-                if mo.picking_id.state in ['assigned', 'confirmed', 'draft']:
-                    mo.picking_id.action_cancel()
-                    log = _("Canceled int picking on MO %s: %s")
-                else:
-                    cancel = False
-                    log = _("Can't cancel int picking on MO %s: %s")
-                log %= (mo.name, mo.picking_id.name)
-                order.add_logs(log, cancel)
-                if mo.state in ['draft', 'confirmed', 'ready']:
-                    mo.action_cancel()
-                    log = _("MO %s canceled")
-                else:
-                    cancel = False
-                    log = _("Can't cancel MO: %s")
-                log %= mo.name
-                order.add_logs(log, cancel)
-            if cancel:
-                cancel_ids.append(order.id)
-        return super(SaleOrder, self).action_cancel(
-            cr, uid, cancel_ids, context=context
-        )
+        mo_ids = mrp_prod_obj.search(cr, uid, [
+            ('sale_order_id', '=', order.id),
+            ], context=context)
+        for mo in mrp_prod_obj.browse(cr, uid, mo_ids, context=context):
+            able_to_cancel, message, important =\
+                self._can_cancel_mo_internal_move(cr, uid, mo, context=context)
+            if able_to_cancel:
+                mo.picking_id.action_cancel()
+            order.add_cancel_log(message, important=important)
+
+            able_to_cancel, message, important =\
+                self._can_cancel_mo(cr, uid, mo, context=context)
+            if able_to_cancel:
+                mo.action_cancel()
+            order.add_cancel_log(message, important=important)
+        return super(SaleOrder, self)._cancel_linked_record(
+            cr, uid, order, context=context)
