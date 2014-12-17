d67358cec13cd49d808ec5902e4c5dfbf4923d07
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-12-17 13:24:39 +0100

    [ADD] module sale_owner_stock_sourcing
    This modules allows to choose stock ownership on sale order line

---
+++ sale_owner_stock_sourcing/README.rst
@@ -0,0 +1,16 @@
+Sale Owner Stock Sourcing
+=========================
+
+This module allow you to choose for every sale order line the owner of the stock that will be dispatched.
+By choosing an owner, the generated deliveries will then look for stock of the specified partner.
+If no one is available, then the picking will be in waiting availability until stocks of the proper ownership
+are replenished.
+
+Specifying an owner on a line, forces to use the stock of this owner.
+Letting the owner empty allows to use stock (quant) without owner.
+
+Contributors
+------------
+
+* Yannick Vaucher <yannick.vaucher@camptocamp.com>
+
---
+++ sale_owner_stock_sourcing/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Author: Yannick Vaucher
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{"name": "Sale Owner Stock Sourcing",
+ "summary": "Manage stock ownership on sale order lines",
+ "version": "0.0.1",
+ "author": "Camptocamp",
+ "license": "AGPL-3",
+ "category": "Purchase Management",
+ 'complexity': "normal",
+ "images": [],
+ "website": "http://www.camptocamp.com",
+ "depends": ['sale_stock'
+             #'purchase_vendor_consignment_stock',
+             ],
+ "demo": [],
+ "data": ['view/sale_order.xml',
+          ],
+ "test": [ ],
+ 'installable': True,
+ "auto_install": False,
+ }
