b0bb6fcc7475c8445c3becb5d0de10d9b3e44631
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-01-11 11:21:41 +0100

    [REN] sale_order_type: Make module name singular

---
+++ sale_order_type/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#                                                                            #
+#  OpenERP, Open Source Management Solution.                                 #
+#                                                                            #
+#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
+#                                                                            #
+#  This program is free software: you can redistribute it and/or modify      #
+#  it under the terms of the GNU Affero General Public License as            #
+#  published by the Free Software Foundation, either version 3 of the        #
+#  License, or (at your option) any later version.                           #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful,           #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+#  GNU Affero General Public License for more details.                       #
+#                                                                            #
+#  You should have received a copy of the GNU Affero General Public License  #
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                            #
+##############################################################################
+
+{
+    'name': 'Sale Order Types',
+    'version': '1.0',
+    'category': 'Sales Management',
+    'description': """This module adds a typology for the sale orders.""",
+    'author': 'OdooMRP team',
+    'website': 'www.odoomrp.com',
+    'license': 'AGPL-3',
+    'depends': ['sale', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_view.xml',
+        'views/sale_order_type_view.xml',
+        'data/default_type.xml',
+        'demo/sale_order_demo.xml',
+    ],
+    'installable': True,
+}
