8e7b6b6ac7a553e8ed0f3d01e664bac8acd5fb67
Adrien Chaussende <chaussende.adrien@gmail.com>
2015-01-13 10:50:49 +0100

    [FIX] Error in License terms

--- sale_cancel_picking/sale.py
+++ sale_cancel_picking/sale.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#  licence AGPL version 3 or later
-#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  License AGPL version 3 or later
+#  See license in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
 #  Copyright (C) 2014 Akretion (http://www.akretion.com).
 #  @author Adrien CHAUSSENDE <adrien.chaussende@akretion.com>
 #
