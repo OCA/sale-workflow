a9d9c3a264ed89b705ee282c5304f8adb9381f05
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-01-29 16:58:53 +0300

    Added account_invoice.py to add visible_discount to account_invoice_line

---
+++ product_visible_discount_extension/account_invoice.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2010 - 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import (
+    models,
+    fields,
+    api,
+)
+
+
+class AccountInvoiceLine(models.Model):
+
+    """
+    Subclass account.invoice.line.
+
+    This class adds an invoice_line_number
+    that should match the order_line_number from
+    the sale_order model.
+    """
+
+    _name = 'account.invoice.line'
+    _inherit = 'account.invoice.line'
+
+    visible_discount = fields.Float("Customer Discount (%)")
