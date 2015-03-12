5c85769bf058299218588fecb4e55912b5df4576
Benoit Guillot <benoit.guillot@akretion.com>
2015-03-12 18:07:12 +0100

    [FIX] unused import

--- sale_datetime_confirm/report/sale.py
+++ sale_datetime_confirm/report/sale.py
@@ -19,7 +19,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class SaleReport(models.Model):
