fef44b6506d53f3c6dfc12430b1f0f1efdf1773b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-03-17 14:30:13 +0100

    start sale_addresses

---
+++ sale_addresses/README.rst
@@ -0,0 +1,36 @@
+Sale Address Management
+=======================
+
+Description
+===========
+
+This module (together with stock_addresses and purchase_addresses) manages in a
+coherent way the following addresses in the standard Odoo workflows:
+
+* origin address
+* destination address
+* consignee
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
---
+++ sale_addresses/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Author: Alexandre Fayolle
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+{'name': 'Sale Addresses',
+ 'summary': 'Manage origin / destination / consignee addresses on sales',
+ 'version': '1.0',
+ 'author': "Camptocamp,Odoo Community Association (OCA)",
+ 'category': 'Warehouse',
+ 'license': 'AGPL-3',
+ 'complexity': 'expert',
+ 'images': [],
+ 'website': "http://www.camptocamp.com",
+ 'depends': ['sale_stock',
+             'stock_addresses',
+             ],
+ 'demo': [],
+ 'data': ['view/sale.xml',
+          ],
+ 'auto_install': False,
+ 'installable': True,
+ }
