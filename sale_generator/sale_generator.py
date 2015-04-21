d6fee085c8ac195a5c55574ccae2556828986c77
Abdessamad HILALI <abdessamad.hilali@akretion.com>
2015-04-21 12:01:09 +0200

    Sale Generator

--- sale_generator/sale_generator.py
+++ sale_generator/sale_generator.py
@@ -3,8 +3,10 @@ from openerp import fields, models , api
 class SaleLineGenerator(models.Model):
 	_name='sale.line.generator' 
 
-	generator_id = fields.Many2one('sale.generator', string="Generator" ,required=True)
-	product_id = fields.Many2one('product.product', string="Product" , required=True)
+	generator_id = fields.Many2one('sale.generator', string="Generator"
+							,required=True)
+	product_id = fields.Many2one('product.product', string="Product"
+							, required=True)
 	product_qty= fields.Float('quantity' , required=True ,default=1)
 	
 
@@ -17,11 +19,13 @@ class SaleGenerator(models.Model):
     _name='sale.generator'
 
 
-    line_ids = fields.One2many('sale.line.generator','generator_id', string="lines")
+    line_ids = fields.One2many('sale.line.generator','generator_id',
+						string="lines")
     partner_ids = fields.Many2many('res.partner', string="Partner")
     sale_ids = fields.One2many('sale.order','generator_id', string="Sales")
     date = fields.Date('Date')
-    state = fields.Selection([('draft','Draft'),('confirmed','Confirmed')],'State',readonly=True, default='draft')
+    state = fields.Selection([('draft','Draft'),('confirmed','Confirmed')],
+				'State',readonly=True, default='draft')
 
     def _prepare_generator_sale_vals(self, partner):
         return {
@@ -52,17 +56,15 @@ class SaleGenerator(models.Model):
                 vals = self._prepare_generator_sale_vals(partner)
                 vals['order_line'] = []
                 for gline in self.line_ids:
-	            vals['order_line'].append((0, 0, self._prepare_sale_order_line(gline)))
+	            vals['order_line'].append((0,
+				 0, self._prepare_sale_order_line(gline)))
 		    #import pdb; pdb.set_trace()
 		    sale_order_obj.create(vals)
 	 
     @api.one
     def button_confirm(self):
         return self.write({'state': 'confirmed'})
-    #@api.one
-    #def write(self,vals):
-	
-    #@api.depends('partner_ids')
+
     def order_test(self):
 	sale_order_obj = self.env['sale.order']
 	partner_make_order = []
@@ -74,7 +76,8 @@ class SaleGenerator(models.Model):
 	            vals = self._prepare_generator_sale_vals(partner)
 		    vals['order_line'] = []
 		    for gline in self.line_ids:
-		        vals['order_line'].append((0, 0, self._prepare_sale_order_line(gline)))
+		        vals['order_line'].append((0,
+				0, self._prepare_sale_order_line(gline)))
 		        sale_order_obj.create(vals)
 	self._search_and_delete_partner()
 	
@@ -104,7 +107,6 @@ class SaleGenerator(models.Model):
                  "target": "new",
 	}
 	self.ensure_one()
-	#pdb.set_trace()
 	return res
 
 
