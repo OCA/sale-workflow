24f4cd697befc2a9ca0591df75692990c77f63c1
hurrinico <nicola.malcontenti@agilebg.com>
2015-04-22 12:49:59 +0200

    [FIX] add restriction on sale confirm changed icon and minor fix
    [FIX] Fix product_id on test
    
    [FIX] Fix product on test

--- sale_order_lot_selection/tests/test_sale_order_lot_selection.py
+++ sale_order_lot_selection/tests/test_sale_order_lot_selection.py
@@ -23,17 +23,22 @@ import openerp.tests.common as test_common
 
 class TestSaleOrderLotSelection(test_common.SingleTransactionCase):
     def setUp(self):
-        """Set up a sale order a particular lot.
+        """
+        Set up a sale order a particular lot.
 
         I confirm it, transfer the delivery order and check lot on picking
 
+        Set up a sale order with two lines with different products and lots.
+
+        I confirm it, transfer the delivery order and check lots on picking
+
         """
         super(TestSaleOrderLotSelection, self).setUp()
-        product_11 = self.env.ref('product.product_product_11')
+        self.product_11 = self.env.ref('product.product_product_11')
         self.lot = self.env['stock.production.lot'].create(
             {
                 'name': "0000010",
-                'product_id': product_11.id
+                'product_id': self.product_11.id
             })
         self.order = self.env['sale.order'].create(
             {
@@ -43,10 +48,38 @@ class TestSaleOrderLotSelection(test_common.SingleTransactionCase):
             'name': 'sol1',
             'order_id': self.order.id,
             'lot_id': self.lot.id,
-            'product_id': product_11.id,
+            'product_id': self.product_11.id,
+        })
+        self.product_13 = self.env.ref('product.product_product_13')
+        self.lot13 = self.env['stock.production.lot'].create(
+            {
+                'name': "0000011",
+                'product_id': self.product_13.id
+            })
+        self.product_12 = self.env.ref('product.product_product_12')
+        self.lot12 = self.env['stock.production.lot'].create(
+            {
+                'name': "0000012",
+                'product_id': self.product_12.id
+            })
+        self.order2 = self.env['sale.order'].create(
+            {
+                'partner_id': self.env.ref('base.res_partner_1').id,
+            })
+        self.sol2a = self.env['sale.order.line'].create({
+            'name': 'sol2a',
+            'order_id': self.order2.id,
+            'lot_id': self.lot13.id,
+            'product_id': self.product_13.id,
+        })
+        self.sol2b = self.env['sale.order.line'].create({
+            'name': 'sol2b',
+            'order_id': self.order2.id,
+            'lot_id': self.lot12.id,
+            'product_id': self.product_12.id,
         })
 
-    def test_order_confirm_and_picking_transfer(self):
+    def test_order_confirm_and_picking_transfer_one_lot(self):
         self.order.action_button_confirm()
         picking = self.order.picking_ids
         picking.action_assign()
@@ -54,5 +87,20 @@ class TestSaleOrderLotSelection(test_common.SingleTransactionCase):
         wiz = self.env['stock.transfer_details'].search([
             ['picking_id', '=', picking.id]])
         wiz.do_detailed_transfer()
-        self.assertEqual(self.lot,
-                         picking.move_lines.lot_ids)
+        for pack in picking.pack_operation_ids:
+            if pack.product_id.id == self.product_11.id:
+                self.assertEqual(pack.lot_id, self.lot)
+
+    def test_order_confirm_and_picking_transfer_2_products_2_lots(self):
+        self.order2.action_button_confirm()
+        picking = self.order2.picking_ids
+        picking.action_assign()
+        picking.do_enter_transfer_details()
+        wiz = self.env['stock.transfer_details'].search([
+            ['picking_id', '=', picking.id]])
+        wiz.do_detailed_transfer()
+        for pack in picking.pack_operation_ids:
+            if pack.product_id.id == self.product_13.id:
+                self.assertEqual(pack.lot_id, self.lot13)
+            else:
+                self.assertEqual(pack.lot_id, self.lot12)
