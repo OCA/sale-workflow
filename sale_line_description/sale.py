8fdcfa36fc042dfa84d3284e8415603436f5b8a8
Alex Comba <alex.comba@agilebg.com>
2015-04-30 18:05:15 +0200

    Check description_sale instead of description

--- sale_line_description/sale.py
+++ sale_line_description/sale.py
@@ -52,10 +52,6 @@ class SaleOrderLine(orm.Model):
                     product_obj = self.pool.get('product.product')
                     product = product_obj.browse(
                         cr, uid, product_id, context=context)
-                    if (
-                        product and
-                        product.description and
-                        'value' in res
-                    ):
+                    if (product.description_sale and 'value' in res):
                         res['value']['name'] = product.description_sale
         return res
