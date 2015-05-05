e929194c73508e4cb2f0953e3a38b0accaea7692
Alex Comba <alex.comba@agilebg.com>
2015-05-05 12:24:22 +0200

    Update copyright year

--- sale_properties_easy_creation/__openerp__.py
+++ sale_properties_easy_creation/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2014 Agile Business Group sagl
+#    Copyright (C) 2014-15 Agile Business Group sagl
 #    (<http://www.agilebg.com>)
 #    @author Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 #
