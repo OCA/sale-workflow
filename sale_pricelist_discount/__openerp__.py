b873c4b239fb60dd8afedefcd2bd637c589591c2
Jacob Hicks <jacob.hicks@credativ.co.uk>
2015-05-06 17:49:56 +0100

    [ADD] Module sale_pricelist_discount

---
+++ sale_pricelist_discount/__openerp__.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2015 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sale Pricelist Discount',
+    'version':'1.0',
+    'category' : 'Sale',
+    'description': """
+This module allows specifying a default percentage discount to apply to sale order lines.
+This ensures the pricelist discount is visible on quotations and sale orders.
+    """,
+    'author' : 'credativ',
+    'website' : 'http://www.credativ.co.uk',
+    'depends':[
+               'base',
+               'sale',
+               'product',
+               ],
+    'update_xml' : [
+        'view/pricelist_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
