5efca3e73887c3e633dd2c9b33ad90cd1fe32928
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-05-06 14:43:30 +0200

    [REF] fix incorrect description in manifest file;

--- sale_order_mass_action/__openerp__.py
+++ sale_order_mass_action/__openerp__.py
@@ -29,17 +29,41 @@
     'category': 'Sales',
     'description': """
 Sale Orders Mass Action
-========================
+=======================
 
-Facilities to realize mass action on Sale Orders:
+Facilities to perform mass action on Sale Orders:
 
 * A wizard which allows on multiple Sale Orders at a time to:
     * confirm Sale Orders;
 
-Possible Improvements:
-----------------------
-    * Possibility to create invoices for many Sale Orders at a time;
- """,
+Known issues / Roadmap
+======================
+
+* Possibility to create invoices for many Sale Orders at a time;
+
+Credits
+=======
+
+Contributors
+------------
+
+* Sylvain LE GAL (https://twitter.com/legalsylvain)
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+""",
     'depends': [
         'sale',
     ],
