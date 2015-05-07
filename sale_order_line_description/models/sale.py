1de8615d8c7bd258cda96e85ae2fa5d21b49b540
Alex Comba <alex.comba@agilebg.com>
2015-05-07 11:46:36 +0200

    Update po files due to change name of module

--- sale_order_line_description/models/sale.py
+++ sale_order_line_description/models/sale.py
@@ -27,11 +27,11 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def product_id_change(
-        self, pricelist, product, qty=0, uom=False,
-        qty_uos=0, uos=False,
-        name='', partner_id=False, lang=False, update_tax=True,
-        date_order=False, packaging=False, fiscal_position=False,
-            flag=False):
+            self, pricelist, product, qty=0, uom=False, qty_uos=0,
+            uos=False, name='', partner_id=False, lang=False,
+            update_tax=True, date_order=False, packaging=False,
+            fiscal_position=False, flag=False
+    ):
         res = super(SaleOrderLine, self).product_id_change(
             pricelist, product, qty, uom, qty_uos, uos, name,
             partner_id, lang, update_tax, date_order, packaging,
