1358d966b3d95e98e2235e76894deda8c129dfb9
Alexis de Lattre <alexis@via.ecp.fr>
2015-05-25 02:43:43 +0200

    Port to v8 and new API with several enhancements

--- sale_start_end_dates/__openerp__.py
+++ sale_start_end_dates/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Sale Start End Dates module for OpenERP
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
+#    Sale Start End Dates module for Odoo
+#    Copyright (C) 2014-2015 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -23,24 +23,14 @@
 
 {
     'name': 'Sale Start End Dates',
-    'version': '0.1',
+    'version': '8.0.0.1.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Adds start date and end date on sale order lines',
-    'description': """
-Sale Start End Dates
-====================
-
-This module adds start date and end dates on sale order lines. The value of these field is copied to the start date and end date of invoice lines. This module is designed to be used with other modules such as sale_rental.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
-    """,
-    'author': 'Akretion',
+    'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
-    'depends': ['account_cutoff_prepaid', 'sale'],
-    'data': [
-        'sale_view.xml',
-    ],
+    'depends': ['account_cutoff_prepaid', 'sale', 'web_context_tunnel'],
+    'data': ['sale_view.xml'],
+    'demo': ['sale_demo.xml'],
     'installable': True,
-    'active': False,
 }
