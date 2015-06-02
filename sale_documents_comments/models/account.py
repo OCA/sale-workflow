40671d5c143910dd0894c7be458c95b11781cacc
oihane <oihanecruce@gmail.com>
2015-06-02 10:20:56 +0200

    [IMP] <sale_documents_comments> Code cleaning, fixes #848
    [IMP] <sale_documents_comments> Just in case, take order partner_invoice_id
    if partner_id is not present

--- sale_documents_comments/models/account.py
+++ sale_documents_comments/models/account.py
@@ -12,18 +12,14 @@ class AccountInvoice(models.Model):
     sale_comment = fields.Text(string='Internal comments')
 
     @api.multi
-    def onchange_partner_id(self, invoice_type, partner_id,
-                            date_invoice=False, payment_term=False,
-                            partner_bank_id=False, company_id=False):
+    def onchange_partner_id(
+            self, invoice_type, partner_id, date_invoice=False,
+            payment_term=False, partner_bank_id=False, company_id=False):
         val = super(AccountInvoice, self).onchange_partner_id(
             invoice_type, partner_id, date_invoice=date_invoice,
             payment_term=payment_term, partner_bank_id=partner_bank_id,
             company_id=company_id)
         if partner_id:
-            partner_obj = self.env['res.partner']
-            partner = partner_obj.browse(partner_id)
-            comment = partner.invoice_comment or ''
-            if partner.parent_id:
-                comment += '\n%s' % (partner.parent_id.invoice_comment or '')
-            val['value']['sale_comment'] = comment
+            partner = self.env['res.partner'].browse(partner_id)
+            val['value']['sale_comment'] = partner._get_invoice_comments()
         return val
