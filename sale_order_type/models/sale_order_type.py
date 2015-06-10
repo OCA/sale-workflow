cce919e43f185e68912cb4eb73d10a39122ec6fe
oihane <oihanecruce@gmail.com>
2015-06-10 17:50:57 +0200

    [IMP] <sale_order_type> Added readme and default journal made False

--- sale_order_type/models/sale_order_type.py
+++ sale_order_type/models/sale_order_type.py
@@ -1,43 +1,22 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#                                                                            #
-#  OpenERP, Open Source Management Solution.                                 #
-#                                                                            #
-#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
-#                                                                            #
-#  This program is free software: you can redistribute it and/or modify      #
-#  it under the terms of the GNU Affero General Public License as            #
-#  published by the Free Software Foundation, either version 3 of the        #
-#  License, or (at your option) any later version.                           #
-#                                                                            #
-#  This program is distributed in the hope that it will be useful,           #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
-#  GNU Affero General Public License for more details.                       #
-#                                                                            #
-#  You should have received a copy of the GNU Affero General Public License  #
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                            #
+# For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 
-from openerp.osv import fields, orm
+from openerp import fields, models
 
 
-class SaleOrderTypology(orm.Model):
-
+class SaleOrderTypology(models.Model):
     _name = 'sale.order.type'
-
     _description = 'Type of sale order'
 
-    _columns = {
-        'refund_journal_id': fields.many2one('account.journal',
-                                             'Refund Billing Journal'),
-        'description': fields.text('Description'),
-        'journal_id': fields.many2one('account.journal',
-                                      'Billing Journal'),
-        'name': fields.char('Name', required=True),
-        'sequence_id': fields.many2one('ir.sequence', 'Entry Sequence',
-                                       copy=False),
-        'warehouse_id': fields.many2one('stock.warehouse',
-                                        'Warehouse', required=True),
-    }
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    sequence_id = fields.Many2one(
+        comodel_name='ir.sequence', string='Entry Sequence', copy=False)
+    journal_id = fields.Many2one(
+        comodel_name='account.journal', string='Billing Journal')
+    refund_journal_id = fields.Many2one(
+        comodel_name='account.journal', string='Refund Billing Journal')
+    warehouse_id = fields.Many2one(
+        comodel_name='stock.warehouse', string='Warehouse', required=True)
