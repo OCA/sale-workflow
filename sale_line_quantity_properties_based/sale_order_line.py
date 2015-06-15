ad23bd2df437d3f2311b8b1f53f02e98c671feaa
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-06-15 14:42:21 +0200

    [FIX] using new API for property_ids onchange

--- sale_line_quantity_properties_based/sale_order_line.py
+++ sale_line_quantity_properties_based/sale_order_line.py
@@ -19,85 +19,66 @@
 #
 ##############################################################################
 
-from openerp.osv import orm
+from openerp import models, api
 from openerp.tools.translate import _
 from openerp.tools import float_round
 import logging
+from openerp.exceptions import except_orm
 
 _logger = logging.getLogger(__name__)
 
 
-class SaleOrderLine(orm.Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    def product_id_change(
-        self, cr, uid, ids, pricelist, product_id, qty=0,
-        uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-        lang=False, update_tax=True, date_order=False, packaging=False,
-        fiscal_position=False, flag=False, context=None
-    ):
-        res = super(SaleOrderLine, self).product_id_change(
-            cr, uid, ids, pricelist, product_id, qty=qty,
-            uom=uom, qty_uos=qty_uos, uos=uos,
-            name=name, partner_id=partner_id,
-            lang=lang, update_tax=update_tax,
-            date_order=date_order, packaging=packaging,
-            fiscal_position=fiscal_position, flag=flag, context=context)
-        if context is None:
-            context = {}
-        property_ids = context.get('property_ids')
-        prop_ctx = context.copy()
+    @api.onchange(
+        'property_ids', 'product_id', 'product_uos_qty'
+    )
+    def property_ids_changed(self):
+        prop_ctx = self.env.context.copy()
         if 'lang' in prop_ctx:
             del prop_ctx['lang']
-        if product_id and property_ids and qty_uos:
-            product = self.pool['product.product'].browse(
-                cr, uid, product_id, context=context)
-            if product.quantity_formula_id:
+        if self.product_id and self.property_ids and self.product_uos_qty:
+            if self.product_id.quantity_formula_id:
                 prop_dict = {}
-                prop_pool = self.pool['mrp.property']
-                precision_pool = self.pool['decimal.precision']
-                uom_precision = precision_pool.precision_get(
-                    cr, uid, 'Product UoS')
-                for m2m_tup in property_ids:
-                    for prop in prop_pool.browse(
-                        cr, uid, m2m_tup[2], context=prop_ctx
-                    ):
-                        if prop.group_id.name in prop_dict:
-                            raise orm.except_orm(
-                                _('Error'),
-                                _('Property of group %s already present')
-                                % prop.group_id.name)
-                        prop_dict[prop.group_id.name] = prop.value
+                uom_precision = self.pool['decimal.precision'].precision_get(
+                    self._cr, self._uid, 'Product UoS')
+                for prop in self.env['mrp.property'].with_context(
+                    prop_ctx
+                ).browse([p.id for p in self.property_ids]):
+                    if prop.group_id.name in prop_dict:
+                        raise except_orm(
+                            _('Error'),
+                            _('Property of group %s already present')
+                            % prop.group_id.name)
+                    prop_dict[prop.group_id.name] = prop.value
 
                 localdict = {
                     'self': self,
-                    'cr': cr,
-                    'uid': uid,
+                    'cr': self._cr,
+                    'uid': self._uid,
                     'properties': prop_dict,
-                    'qty_uos': qty_uos,
-                    'product_id': product_id,
+                    'qty_uos': self.product_uos_qty,
+                    'product_id': self.product_id.id,
                 }
                 try:
-                    exec product.quantity_formula_id.formula_text in localdict
+                    exec (
+                        self.product_id.quantity_formula_id.formula_text
+                        in localdict
+                    )
                     try:
                         amount = localdict['result']
                     except KeyError:
-                        raise orm.except_orm(
+                        raise except_orm(
                             _('Error'),
                             _("Formula must contain 'result' variable"))
                     # rounding because decimal values, different from what
                     # displayed on interface, could generate infinite
                     # on_change loop
-                    res['value']['product_uom_qty'] = float_round(
+                    self.product_uom_qty = float_round(
                         amount, precision_digits=uom_precision)
                 except KeyError:
                     _logger.warning(
                         "KeyError for formula '%s' and prop_dict '%s'"
-                        % (product.quantity_formula_id.formula_text,
+                        % (self.product_id.quantity_formula_id.formula_text,
                             prop_dict))
-        # Removing product_uos_qty is needed because it can now be used to
-        # compute the real quantity. Otherwise, it would be recomputed after
-        # the quantity changed. See the automated test for the use case.
-        if 'value' in res and 'product_uos_qty' in res['value']:
-            del res['value']['product_uos_qty']
-        return res
