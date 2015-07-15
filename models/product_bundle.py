890a6a56692b97ed64e8871d6c613460809a744e
Clovis NZOUENDJOU <clovis@anybox.fr>
2015-07-15 17:06:29 +0200

    Relation between product_bundle and product_bunfle_line

--- models/product_bundle.py
+++ models/product_bundle.py
@@ -17,11 +17,14 @@ class product_bundle(models.Model):
 class product_bundle_line(models.Model):
     _name = 'product.bundle.line'
     _description = 'Product bundle line'
+    _rec_name = 'product_id'
 
     product_id = fields.Many2one(
         'product.product', domain=[('sale_ok', '=', True)], string=_('Product'), required=True)
     quantity = fields.Float(
         string=_('Quantity'), digits=dp.get_precision('Product Unit of Measure'),
         required=True, default=1)
+    product_bundle_id = fields.Many2one(
+        'product.bundle', _('Bundle reference'), ondelete='cascade')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
