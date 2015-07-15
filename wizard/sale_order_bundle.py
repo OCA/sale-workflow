58f6e1a5f162a2085ddac0cc1f78b284d29c4aad
Clovis NZOUENDJOU <clovis@anybox.fr>
2015-07-15 17:07:37 +0200

    Wizard to manage bundle on sale order

---
+++ wizard/sale_order_bundle.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv
+from openerp import fields, api, _
+
+import openerp.addons.decimal_precision as dp
+
+
+class sale_order_bundle(osv.osv_memory):
+    _name = 'sale.order.bundle'
+    _rec_name = 'product_bundle_id'
+
+    product_bundle_id = fields.Many2one(
+        'product.bundle', _('Product bundle'), required=True)
+    quantity = fields.Float(
+        string=_('Quantity'), digits=dp.get_precision('Product Unit of Measure'),
+        required=True, default=1)
+
+    @api.multi
+    def add_bundle(self):
+        """ Add product bundle, multiplied by quantity in sale order line """
+        so_id = self._context['active_id']
+        if not so_id:
+            return
+        sol = self.env['sale.order.line']
+        for bundle in self.product_bundle_id.bundle_line_ids:
+            sol_data = {
+                'order_id': so_id,
+                'description': bundle.product_id.name,
+                'product_id': bundle.product_id.id,
+                'price_unit': bundle.product_id.list_price,
+                'product_uom_qty': bundle.quantity*self.quantity,
+            }
+            sol.create(sol_data)
