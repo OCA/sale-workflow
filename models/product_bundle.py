e0e45ee2da44cd5040ee399aca6d17a953efc214
Clovis NZOUENDJOU <clovis@anybox.fr>
2015-07-16 16:48:48 +0200

    update bundle and bundle line relation from Many2many to One2many

--- models/product_bundle.py
+++ models/product_bundle.py
@@ -7,8 +7,7 @@ class ProductBundle(models.Model):
     _description = 'Product bundle'
 
     name = fields.Char(_('Name'), help=_('Product bundle name'), required=True)
-    bundle_line_ids = fields.Many2many(
-        'product.bundle.line', 'product_bundle_product_bundle_line',
-        'product_bundle_id', 'product_bundle_line_id', string=_('Bundle lines'))
+    bundle_line_ids = fields.One2many(
+        'product.bundle.line', 'product_bundle_id', _(u"Products"))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
