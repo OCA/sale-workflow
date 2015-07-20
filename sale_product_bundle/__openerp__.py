55f3f9ec91c3d9a42686eb139e33e6d391f8094f
Pierre Verkest <pverkest@anybox.fr>
2015-07-20 22:59:11 +0200

    unused remove product bundle line views

--- sale_product_bundle/__openerp__.py
+++ sale_product_bundle/__openerp__.py
@@ -1,39 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'product_bundle',
-    'category': 'Product',
+    'name': 'sale_product_bundle',
+    'category': 'Sale',
     'author': 'Anybox',
     'version': '1.0',
     'sequence': 150,
     'website': 'http://anybox.fr',
-    'summary': "Product bundle management",
-    'description': """
-Product bundle management
-=========================
-        """,
+    'summary': "Sale product bundle",
     'depends': [
         'sale',
     ],
     'data': [
         'views/product_bundle.xml',
-        'wizard/sale_order_bundle.xml',
         'views/sale_order.xml',
-        'views/product_bundle_line.xml',
+        'wizard/sale_order_bundle.xml',
         'security/ir.model.access.csv',
-    ],
-    'init': [
-
-    ],
-    'test': [
-    ],
-    'demo': [
-    ],
-    'js': [
-    ],
-    'qweb': [
-    ],
-    'css': [
     ],
     'installable': True,
     'application': False,
