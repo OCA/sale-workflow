554ca7e4b1fe79336759f1fd2c6054633ab144f7
Pierre Verkest <pverkest@anybox.fr>
2015-08-06 09:27:32 +0200

    Rename Bundle to Set

--- sale_product_set_layout/wizard/sale_order_set.py
+++ sale_product_set_layout/wizard/sale_order_set.py
@@ -2,12 +2,12 @@
 from openerp import models
 
 
-class SaleOrderBundle(models.TransientModel):
+class SaleOrderSet(models.TransientModel):
     _inherit = 'sale.order.set'
 
     def prepare_sale_order_line_data(self, sale_order_id, set, set_line,
                                      *args, **kwargs):
-        sol_data = super(SaleOrderBundle, self).prepare_sale_order_line_data(
+        sol_data = super(SaleOrderSet, self).prepare_sale_order_line_data(
             sale_order_id, set, set_line, *args, **kwargs)
         sol_data.update({'sale_layout_cat_id': set.section_id.id})
         return sol_data
