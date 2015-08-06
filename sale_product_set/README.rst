617582b07513460755f5221a41ef10f5fab33352
Pierre Verkest <pverkest@anybox.fr>
2015-08-06 08:25:52 +0200

    [add] sale_product_set

---
+++ sale_product_set/README.rst
@@ -0,0 +1,86 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Sale Product set
+================
+
+A **product set** is a list of products which end customers aren't aware, this
+list is defined by sales manager.
+
+This module aims to help salesman to quickly create several sale order lines
+at once in a quotation.
+
+After a *product set* is added to the sale order, each line can be update or
+remove as any other sale order lines.
+
+This is differ to packing products as you don't follow *product set* after it
+was added to the sale order.
+
+Usage
+=====
+
+To use this module, you need to:
+
+* Define a *product set* as sale manager:
+    - choose products
+    - for each products, define a quantity.
+    - Sort *set* lines, this order will be the default when added into the
+      quotation
+
+.. image:: /sale_product_set/static/description/product_set.png
+    :alt: Set a product set
+
+* On quotation any salesman can click on "Add set" button
+  which will open wizard where users can chose a *product set* and quantity
+  to add at the end of sale order lines. Order defined in *product set* is
+  preserved in sale order.
+
+.. image:: /sale_product_set/static/description/add_set.png
+    :alt: Add set to sale order
+
+* Then you can remove or update added lines as any other sale order lines.
+
+.. image:: /sale_product_set/static/description/sale_order.png
+    :alt: Sale order
+
+For further information, please visit:
+
+* https://www.odoo.com/forum/help-1
+
+Known issues / Roadmap
+======================
+
+*
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/sale-workflow/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
+`here <https://github.com/OCA/sale-workflow/issues/new?body=module:%20sale_product_set%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Clovis Nzouendjou <clovis@anybox.fr>
+* Pierre Verkest <pverkest@anybox.fr>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
---
+++ sale_product_set/tests/test_product_set.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from openerp.tests import common
+
+
+class test_product_set(common.TransactionCase):
+    """ Test Product set"""
+
+    def setUp(self):
+        super(test_product_set, self).setUp()
+        self.sale_order = self.env['sale.order']
+        self.product_set_add = self.env['product.set.add']
+
+    def test_add_set(self):
+        so = self.env.ref('sale.sale_order_6')
+        count_lines = len(so.order_line)
+        untaxed_amount = so.amount_untaxed
+        tax_amount = so.amount_tax
+        total_amount = so.amount_total
+
+        product_set = self.env.ref(
+            'sale_product_set.product_set_i5_computer')
+        # Simulation the opening of the wizard and adding a set on the
+        # current sale order
+        so_set = self.product_set_add.with_context(
+            active_id=so.id).create({'product_set_id': product_set.id,
+                                     'quantity': 2})
+        so_set.add_set()
+        # checking our sale order
+        self.assertEquals(len(so.order_line), count_lines + 4)
+        # untaxed_amount + ((147*1)+(2100*1)+(2000*1)+(85*2)) * 2
+        self.assertEquals(so.amount_untaxed, untaxed_amount + 8834.0)
+        self.assertEquals(so.amount_tax, tax_amount + 0)  # without tax
+        self.assertEquals(so.amount_total, total_amount + 8834.0)
+        sequence = {}
+        for line in so.order_line:
+            sequence[line.product_id.id] = line.sequence
+            for set_line in product_set.set_line_ids:
+                if line.product_id.id == set_line.product_id.id:
+                    self.assertEquals(line.product_id.name,
+                                      set_line.product_id.name)
+        # make sure sale order line sequence keep sequence set on set
+        seq_line1 = sequence.pop(
+            self.env.ref(
+                "sale_product_set.product_set_line_computer_2"
+            ).product_id.id)
+        seq_line2 = sequence.pop(
+            self.env.ref(
+                "sale_product_set.product_set_line_computer_4"
+            ).product_id.id)
+        seq_line3 = sequence.pop(
+            self.env.ref(
+                "sale_product_set.product_set_line_computer_1"
+            ).product_id.id)
+        seq_line4 = sequence.pop(
+            self.env.ref(
+                "sale_product_set.product_set_line_computer_3"
+            ).product_id.id)
+        self.assertTrue(max([v for k, v in sequence.iteritems()]) <
+                        seq_line1 < seq_line2 < seq_line3 < seq_line4)
+
+    def test_add_set_on_empty_so(self):
+        so = self.sale_order.create({
+            'partner_id': self.ref('base.res_partner_1')})
+        product_set = self.env.ref(
+            'sale_product_set.product_set_i5_computer')
+        so_set = self.product_set_add.with_context(
+            active_id=so.id).create({'product_set_id': product_set.id,
+                                     'quantity': 2})
+        so_set.add_set()
+        self.assertEquals(len(so.order_line), 4)
