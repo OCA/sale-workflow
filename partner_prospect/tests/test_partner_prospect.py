5837a77637097b8e897cbf7e2fd43e5d9d4e6286
oihane <oihanecruce@gmail.com>
2015-08-13 09:08:20 +0200

    [IMP] <partner_prospect> Upgraded tests

--- partner_prospect/tests/test_partner_prospect.py
+++ partner_prospect/tests/test_partner_prospect.py
@@ -13,22 +13,47 @@ class TestPartnerProspect(TransactionCase):
         self.sale_order_model = self.env['sale.order']
         self.partner_model = self.env['res.partner']
         self.partner1 = self.partner_model.create({'name': 'Partner1'})
-        self.partner2 = self.partner_model.create({'name': 'Partner2'})
+        self.partner2 = self.partner_model.create({
+            'name': 'Partner2',
+            'parent_id': self.partner1.id,
+        })
+        self.partner3 = self.partner_model.create({
+            'name': 'Partner3',
+            'parent_id': self.partner1.id,
+        })
+        self.partner4 = self.partner_model.create({'name': 'Partner4'})
         self.product = self.env.ref('product.product_product_4')
-        self.sale_order_line_model = self.env['sale.order.line']
-        self.sale_order_partner1 = self.sale_order_model.create({
+        self.sale_order1 = self.sale_order_model.create({
             'partner_id': self.partner1.id,
             'order_policy': 'manual',
             'order_line': [(0, 0, {'product_id': self.product.id, })],
         })
-        self.sale_order_partner1.action_button_confirm()
-        self.sale_order_partner2 = self.sale_order_model.create({
+        self.sale_order2 = self.sale_order_model.create({
             'partner_id': self.partner2.id,
             'order_policy': 'manual',
             'order_line': [(0, 0, {'product_id': self.product.id, })],
         })
-        self.sale_order_partner2.action_quotation_send()
+        self.sale_order3 = self.sale_order_model.create({
+            'partner_id': self.partner4.id,
+            'order_policy': 'manual',
+            'order_line': [(0, 0, {'product_id': self.product.id, })],
+        })
 
-    def test_partner_prospect_check(self):
+    def test_partner_child_check(self):
+        self.sale_order2.action_button_confirm()
         self.assertFalse(self.partner1.prospect, 'Partner1 is a prospect')
-        self.assertTrue(self.partner2.prospect, 'Partner2 is not a prospect')
+        self.assertFalse(self.partner2.prospect, 'Partner2 is a prospect')
+        self.assertFalse(self.partner3.prospect, 'Partner3 is a prospect')
+
+    def test_partner_parent_check(self):
+        self.sale_order1.action_button_confirm()
+        self.assertFalse(self.partner1.prospect, 'Partner1 is a prospect')
+        self.assertFalse(self.partner2.prospect, 'Partner2 is a prospect')
+        self.assertFalse(self.partner3.prospect, 'Partner3 is a prospect')
+
+    def test_partner_prospect(self):
+        self.assertTrue(self.partner4.prospect, 'Partner4 is not a prospect')
+        self.sale_order3.action_button_confirm()
+        self.assertFalse(self.partner4.prospect, 'Partner4 is a prospect')
+        self.sale_order3.action_cancel()
+        self.assertTrue(self.partner4.prospect, 'Partner4 is not a prospect')
