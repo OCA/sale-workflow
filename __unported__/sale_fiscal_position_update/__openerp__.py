b26e85266925883d3bea73068f9a65f30179c2d8
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-08-28 16:14:05 +0200

    [REF] new OCA convention;

--- __unported__/sale_fiscal_position_update/__openerp__.py
+++ __unported__/sale_fiscal_position_update/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Sale Fiscal Position Update',
-    'version': '1.0',
+    'version': '8.0.1.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Changing the fiscal position of a sale order will auto-update '
