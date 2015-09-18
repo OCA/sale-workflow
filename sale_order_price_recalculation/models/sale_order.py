22df976870299847c517c906bdff47814b04a96e
Pierre Verkest <pverkest@anybox.fr>
2015-09-18 18:56:57 +0200

    [IMP] sale_order_price_recalculation
    * add choice to recompute price only
      This commit add the capability to reset sale order line
      without renaming sale order lines
    * upgrade translation
    * Split functionnalities reset prices/reset desciptions
    * Move button under sale order lines

--- sale_order_price_recalculation/models/sale_order.py
+++ sale_order_price_recalculation/models/sale_order.py
@@ -12,6 +12,19 @@ class SaleOrder(models.Model):
 
     @api.multi
     def recalculate_prices(self):
+        return self.reset_lines(price=True)
+
+    @api.multi
+    def recalculate_names(self):
+        return self.reset_lines(price=False)
+
+    @api.multi
+    def reset_lines(self, price=False):
+        """
+        Reset lines according informations on products and price list
+        :param price: boolean to indicate if we are resetting price or
+                      descriptions
+        """
         for line in self.mapped('order_line'):
             order = line.order_id
             res = line.product_id_change(
@@ -20,6 +33,10 @@ class SaleOrder(models.Model):
                 qty_uos=line.product_uos_qty, uos=line.product_uos.id,
                 name=line.name, partner_id=order.partner_id.id, lang=False,
                 update_tax=True, date_order=order.date_order, packaging=False,
-                fiscal_position=order.fiscal_position.id, flag=False)
-            line.write(res['value'])
+                fiscal_position=order.fiscal_position.id, flag=price)
+            if price:
+                line.write(res['value'])
+            else:
+                if 'name' in res['value']:
+                    line.write({'name': res['value']['name']})
         return True
