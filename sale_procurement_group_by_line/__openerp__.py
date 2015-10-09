893535fb905bb01ac6df32d80e011738ae51e979
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:07 +0200

    [UPD] prefix versions with 8.0

--- sale_procurement_group_by_line/__openerp__.py
+++ sale_procurement_group_by_line/__openerp__.py
@@ -21,7 +21,7 @@
 
 {'name': 'Sale Procurement Group by Line',
  'summary': 'Base module for multiple procurement group by Sale order',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Warehouse',
  'license': 'AGPL-3',
