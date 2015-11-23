0a6b91502b11fc10a67eb81ffa681981a7b6e710
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-11-23 16:41:54 +0100

    test pricelist propagation

--- sale_team_default_values/tests/test_default_values.py
+++ sale_team_default_values/tests/test_default_values.py
@@ -10,6 +10,7 @@ class TestDefaultValues(TransactionCase):
         self.so.action_button_confirm()
         self.assertEqual(self.so.project_id, self.team.project_id)
         self.assertEqual(self.so.fiscal_position, self.position)
+        self.assertEqual(self.so.pricelist_id, self.pricelist)
         self.so.action_invoice_create()
         invoice = self.so.invoice_ids
         self.assertEqual(1, len(invoice))
@@ -22,11 +23,15 @@ class TestDefaultValues(TransactionCase):
         self.position = self.env['account.fiscal.position'].create({
             'name': 'more tax',
         })
+        self.pricelist = self.env['product.pricelist'].create({
+            'name': 'super expensive',
+            'type': 'sale',
+        })
         self.team.write({
             'payment_term':
             self.env.ref('account.account_payment_term_15days').id,
             'fiscal_position': self.position.id,
-            # 'pricelist_id',
+            'pricelist_id': self.pricelist.id,
             'warehouse_id': self.env.ref('stock.stock_warehouse_shop0').id,
             'project_id': self.env.ref('account.analytic_nebula').id,
             'journal_id': self.env.ref('account.miscellaneous_journal').id
