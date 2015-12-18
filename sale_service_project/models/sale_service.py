9be58ee31ca3ddf0e50c940608c1480d035c96ef
Carlos Incaser <carlos@incaser.es>
2015-12-18 04:52:17 +0100

    [FIX] sale_service_project: PEP8

--- sale_service_project/models/sale_service.py
+++ sale_service_project/models/sale_service.py
@@ -3,7 +3,7 @@
 # (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api, _
+from openerp import models, api
 
 
 class ProcurementOrder(models.Model):
