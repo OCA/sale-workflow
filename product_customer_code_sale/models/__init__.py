0de54b022d10236840707c3e63c78578a0d20cfd
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2015-12-22 16:41:19 +0100

    [MIGR] Porting product_customer_code_sale to the new API

--- product_customer_code_sale/models/__init__.py
+++ product_customer_code_sale/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Nicola Malcontenti - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import models
+from . import sale
