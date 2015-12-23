f67e56fbed223aabe473ead8ac80988d5d69629f
jbeficent <jordi.ballester@eficent.com>
2015-12-23 14:15:51 +0100

    [ADD] [sale_partner_contact]

---
+++ sale_partner_contact/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services, S.L. -
+# Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale partner contact",
+    "summary": "Add contact person to the quotation or sales order",
+    "version": "8.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://odoo-community.org/",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale"
+    ],
+    "data": [
+        "view/sale_view.xml",
+    ],
+}
