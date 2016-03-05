7636c8681dd681e5342b0e72a92dda32ce04ef95
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-05 03:18:10 +0100

    [ADD] sales_team_security
    ==============================
    Security rules for sales teams
    ==============================
    
    This module sets different permissions levels for accessing sales and CRM
    records based on the sales team: customers, sales orders, leads, opportunities,
    phone calls and sales teams.
    
    It also handles the propagation of the sales team from commercial partners to
    the contacts, which standard doesn't make.
    
    Installation
    ============
    
    At installation time, this module sets int all the contacts that have the sales
    team empty the sales team of the parent. If you have a lot of contacts, this
    operation can take a while.
    
    Usage
    =====
    
    On the user configuration (Configuration > Users > Users), select in the
    *Sales Team* section the option "See only own team". Then, the documents
    mentioned before will be filtered out to have only those belonging to the
    teams the user belongs to.
    
    This is complementary to the Sales level access, but sometimes can be
    incoherent depending on the combination chosen. If you chose "See Own Leads"
    on _Sales_ section, marking on unmarking the sales team check will be
    irrelevant, because the most restricting level, which the sales one, will
    prevail.
    
    Known issues/Roadmap
    ====================
    
    * This module is designed for support only sales part, so someone that has
      access to other Odoo parts (for example, an accountant), shouldn't have
      this new permission, or some access errors will be found when seeing invoices
      and other documents. A _sales_team_security_account_ bridge module can be
      done for fixing this case, but not in the case of for example warehouse.

---
+++ sales_team_security/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import test_sales_team_security
