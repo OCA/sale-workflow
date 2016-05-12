00b4365efa9ad5de111fa31cdd1a95c68c882efc
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-05-12 13:08:04 +0530

    [FIX] separated payment portion

--- sale_automatic_workflow/models/automatic_workflow_job.py
+++ sale_automatic_workflow/models/automatic_workflow_job.py
@@ -6,7 +6,7 @@
 
 import logging
 from contextlib import contextmanager
-from openerp import models, api, fields
+from openerp import models, api
 from openerp.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
@@ -64,34 +64,6 @@ class AutomaticWorkflowJob(models.Model):
             with commit(self.env.cr):
                 invoice.signal_workflow('invoice_open')
 
-    # TODO:
-    # use account_payment_mode to get the payment information
-    @api.model
-    def _register_payments(self, payment_filter):
-        invoice_obj = self.env['account.invoice']
-        invoices = invoice_obj.search(payment_filter)
-        _logger.debug('Invoices to Register Payment: %s', invoices.ids)
-        for invoice in invoices:
-            partner_type = invoice.type in ('out_invoice', 'out_refund') and \
-                'customer' or 'supplier'
-            payment_type = invoice.type in ('out_invoice', 'in_refund') and \
-                'inbound' or 'outbound'
-            payment_method = 1  # TODO
-#             self.env.ref('account.account_payment_method_manual_in')
-            with commit(self.env.cr):
-                payment = self.env['account.payment'].create({
-                    'invoice_ids': [(6, 0, invoice.ids)],
-                    'amount': invoice.residual,
-                    'payment_date': fields.Date.context_today(self),
-                    'communication': invoice.reference or invoice.number,
-                    'partner_id': invoice.partner_id.id,
-                    'partner_type': partner_type,
-                    'payment_type': payment_type,
-                    'payment_method_id': payment_method,
-                })
-                payment.post()
-        return
-
     @api.model
     def _validate_pickings(self, picking_filter):
         picking_obj = self.env['stock.picking']
@@ -111,36 +83,35 @@ class AutomaticWorkflowJob(models.Model):
                 sale.action_done()
 
     @api.model
+    def run_with_workflow(self, sale_workflow):
+        workflow_domain = [('workflow_process_id', '=', sale_workflow.id)]
+        if sale_workflow.validate_order:
+            self._validate_sale_orders(
+                safe_eval(sale_workflow.order_filter_id.domain) +
+                workflow_domain)
+        if sale_workflow.validate_picking:
+            self._validate_pickings(
+                safe_eval(sale_workflow.picking_filter_id.domain) +
+                workflow_domain)
+        if sale_workflow.create_invoice:
+            self._create_invoices(
+                safe_eval(sale_workflow.create_invoice_filter_id.domain) +
+                workflow_domain)
+        if sale_workflow.validate_invoice:
+            self._validate_invoices(
+                safe_eval(
+                    sale_workflow.validate_invoice_filter_id.domain) +
+                workflow_domain)
+        if sale_workflow.sale_done:
+            self._sale_done(
+                safe_eval(
+                    sale_workflow.sale_done_filter_id.domain) +
+                workflow_domain)
+
+    @api.model
     def run(self):
         """ Must be called from ir.cron """
         sale_workflow_process = self.env['sale.workflow.process']
         for sale_workflow in sale_workflow_process.search([]):
-            workflow_domain = [('workflow_process_id', '=', sale_workflow.id)]
-            if sale_workflow.validate_order:
-                self._validate_sale_orders(
-                    safe_eval(sale_workflow.order_filter_id.domain) +
-                    workflow_domain)
-            if sale_workflow.validate_picking:
-                self._validate_pickings(
-                    safe_eval(sale_workflow.picking_filter_id.domain) +
-                    workflow_domain)
-            if sale_workflow.create_invoice:
-                self._create_invoices(
-                    safe_eval(sale_workflow.create_invoice_filter_id.domain) +
-                    workflow_domain)
-            if sale_workflow.validate_invoice:
-                self._validate_invoices(
-                    safe_eval(
-                        sale_workflow.validate_invoice_filter_id.domain) +
-                    workflow_domain)
-            # TODO
-#             if sale_workflow.register_payment:
-#                 self._register_payments(
-#                     safe_eval(sale_workflow.payment_filter_id.domain) +
-#                     workflow_domain)
-            if sale_workflow.sale_done:
-                self._sale_done(
-                    safe_eval(
-                        sale_workflow.sale_done_filter_id.domain) +
-                    workflow_domain)
+            self.run_with_workflow(sale_workflow)
         return True
