10c14aebf3ba6d2de2cc51a30148d3eb9e7fe083
Francesco Apruzzese <cescoap@gmail.com>
2016-06-07 15:04:30 +0200

    [FIX] Reference to old module name

--- sale_order_unified_menu/__openerp__.py
+++ sale_order_unified_menu/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Sale Unified Menu",
+    "name": "Sale Order Unified Menu",
     "summary": "Add a menu to show draft and confirmed orders",
     "version": "8.0.1.0.0",
     "category": "Sale",
