50bf693674400f6f0769471cead709b0b3a31fa3
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-07-11 17:36:27 +0200

    [ADD] new module 'product_margin_classification';

---
+++ product_margin_classification/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Product Margin Classification',
+    'version': '8.0.1.0.0',
+    'category': 'Account',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'security/ir_rule.xml',
+        'security/ir.model.access.csv',
+        'views/action.xml',
+        'views/menu.xml',
+        'views/view_product_template.xml',
+        'views/view_product_margin_classification.xml',
+    ],
+    'demo': [
+        'demo/res_groups.yml',
+        'demo/product_margin_classification.xml',
+        'demo/product_template.xml',
+    ],
+    'installable': True,
+}
