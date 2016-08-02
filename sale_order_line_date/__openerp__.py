635df44e62f2987f9e070f48c03651c962f0e205
aheficent <ahenriquez@eficent.com>
2016-08-02 16:19:53 +0200

    Update code
    [FIX] Update readme to latest template

---
+++ sale_order_line_date/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 OdooMRP team
+# © 2016 AvanzOSC
+# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Line Date",
+    "version": "8.0.1.0.0",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow/",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_dates",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
