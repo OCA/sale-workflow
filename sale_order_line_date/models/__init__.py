217efa1d5de232fccfc2e8d644375fd906fd1e8a
aheficent <ahenriquez@eficent.com>
2016-08-02 16:19:53 +0200

    Update code
    [FIX] Update readme to latest template

---
+++ sale_order_line_date/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2016 OdooMRP team
+# © 2016 AvanzOSC
+# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import sale_order
