a95b622a451d8add467a160b0a974327ec1ed124
ralwafaie <rami.alwafaie@initos.de>
2016-08-05 12:51:58 +0200

    add module sale_payment_method_auto_pay_selection

---
+++ sale_payment_method_auto_pay_selection/sale.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © initOS GmbH 2016
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def automatic_payment(self, amount=None):
+        """ Add some limitations to the creation of automatic_payments.
+        """
+        self.ensure_one()
+        # do nothing when it isn't allow
+        if self.payment_method_id\
+                and self.payment_method_id.allow_automatic_payment == 'never':
+            return True
+        return super(SaleOrder, self).automatic_payment(amount=amount)
