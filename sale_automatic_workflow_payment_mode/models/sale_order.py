7fe33746b7548c9c6c6dbe558ad5f503176ad808
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-08-23 09:06:12 +0200

    [WIP][9.0] Add sale_automatic_workflow_payment_mode (#297)
    [ADD] sale_automatic_workflow_payment_mode

---
+++ sale_automatic_workflow_payment_mode/models/sale_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from openerp import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('payment_mode_id')
+    def onchange_payment_mode_set_workflow(self):
+        if not self.payment_mode_id:
+            return
+        workflow = self.payment_mode_id.workflow_process_id
+        if workflow:
+            self.workflow_process_id = workflow
