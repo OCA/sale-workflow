c760bbde211b20e7731987cf71ae484e2d787452
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-08-23 09:06:12 +0200

    [WIP][9.0] Add sale_automatic_workflow_payment_mode (#297)
    [ADD] sale_automatic_workflow_payment_mode

---
+++ sale_automatic_workflow_payment_mode/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA, Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import account_payment_mode
+from . import sale_order
+from . import sale_workflow_process
+from . import automatic_workflow_job
