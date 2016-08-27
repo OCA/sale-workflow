355b4ab05976fa960eb95b8afe2f969210ffa417
Alexis de Lattre <alexis.delattre@akretion.com>
2016-08-28 00:14:42 +0200

    Initial check-in of module sale_order_import, sale_stock_order_import, sale_order_import_ubl, sale_order_import_csv, sale_order_ubl, sale_commercial_partner

---
+++ sale_commercial_partner/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Commercial Partner',
+    'version': '8.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'summary': "Add stored related field 'Commercial Customer' on sale orders",
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['sale'],
+    'data': [
+        'views/sale.xml',
+    ],
+    'installable': True,
+}
