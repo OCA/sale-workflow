30aee23f173cefa6e9bc557d725ffd821051a86e
eLBati <lorenzo.battistini@agilebg.com>
2016-09-05 15:04:08 +0200

    ADD sale type for invoices
    Conflicts:
    	sale_order_type/README.rst
    	sale_order_type/models/account_invoice.py
    	sale_order_type/tests/test_sale_order_type.py

--- sale_order_type/README.rst
+++ sale_order_type/README.rst
@@ -39,6 +39,8 @@ Contributors
 * Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 * Ana Juaristi <anajuaristi@avanzosc.es>
 * Daniel Campos <danielcampos@avanzosc.es>
+* Ainara Galdona <ainaragaldona@avanzosc.es>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 
 Maintainer
 ----------
--- sale_order_type/tests/test_sale_order_type.py
+++ sale_order_type/tests/test_sale_order_type.py
@@ -54,3 +54,10 @@ class TestSaleOrderType(common.TransactionCase):
         sale_order.onchange_type_id()
         sale_order.onchange_partner_id()
         sale_order.action_confirm()
+
+    def test_invoice_onchange_type(self):
+        invoice = self.invoice_model.new({'sale_type_id': self.sale_type.id})
+        invoice.onchange_sale_type_id()
+        self.assertEqual(self.sale_type.payment_term_id,
+                         invoice.payment_term)
+        self.assertEqual(self.sale_type.journal_id, invoice.journal_id)
