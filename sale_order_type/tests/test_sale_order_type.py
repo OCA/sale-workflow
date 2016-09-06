fba77920342df8ffd828a17c822bf4fc0ee21101
eLBati <lorenzo.battistini@agilebg.com>
2016-09-06 08:32:46 +0200

    ADD tests for onchange_partner_id

--- sale_order_type/tests/test_sale_order_type.py
+++ sale_order_type/tests/test_sale_order_type.py
@@ -93,3 +93,9 @@ class TestSaleOrderType(common.TransactionCase):
         self.assertEqual(self.sale_type.payment_term_id,
                          invoice.payment_term)
         self.assertEqual(self.sale_type.journal_id, invoice.journal_id)
+
+    def test_invoice_onchange_partner(self):
+        onchange_partner = self.invoice_model.onchange_partner_id(
+            'out_invoice', self.partner.id)
+        self.assertEqual(self.sale_type.id,
+                         onchange_partner['value']['type_id'])
