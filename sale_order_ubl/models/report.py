624e847c0d88ee4412169c8ee2f7e922b74b50c7
Alexis de Lattre <alexis.delattre@akretion.com>
2016-09-12 01:57:38 +0200

    Handle context key no_embedded_ubl_xml

--- sale_order_ubl/models/report.py
+++ sale_order_ubl/models/report.py
@@ -18,9 +18,14 @@ class Report(models.Model):
         """We go through that method when the PDF is generated for the 1st
         time and also when it is read from the attachment.
         This method is specific to QWeb"""
+        if context is None:
+            context = {}
         pdf_content = super(Report, self).get_pdf(
             cr, uid, ids, report_name, html=html, data=data, context=context)
-        if report_name == 'sale.report_saleorder' and len(ids) == 1:
+        if (
+                report_name == 'sale.report_saleorder' and
+                len(ids) == 1 and
+                not context.get('no_embedded_ubl_xml')):
             order = self.pool['sale.order'].browse(
                 cr, uid, ids[0], context=context)
             pdf_content = order.embed_ubl_xml_in_pdf(
