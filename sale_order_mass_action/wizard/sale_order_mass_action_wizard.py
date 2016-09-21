f8842850674ffc41a1bd67b35e93f55b0ce0b2cb
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-09-21 12:45:29 +0200

    [FIX] pylint (new tests)

--- sale_order_mass_action/wizard/sale_order_mass_action_wizard.py
+++ sale_order_mass_action/wizard/sale_order_mass_action_wizard.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Sale - Sale Order Mass Action module for Odoo
