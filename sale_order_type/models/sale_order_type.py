3e928f285f480b75ffbed6347d6c63721a77eb1b
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-09-23 11:53:15 -0300

    sale_order_type
    FIX case of tests
    
    DEL refund journal field on sale order type, now same invoice journal should be used for refunds
    
    FIX case of test
    
    FIX sale order test

--- sale_order_type/models/sale_order_type.py
+++ sale_order_type/models/sale_order_type.py
@@ -12,7 +12,7 @@ class SaleOrderTypology(models.Model):
 
     @api.model
     def _get_domain_sequence_id(self):
-        seq_type = self.env.ref('sale.seq_type_sale_order')
+        seq_type = self.env.ref('sale.seq_sale_order')
         domain = [('code', '=', seq_type.code)]
         return domain
 
@@ -25,24 +25,6 @@ class SaleOrderTypology(models.Model):
         default_dict = self.env['sale.order'].default_get(['picking_policy'])
         return default_dict.get('picking_policy')
 
-    @api.model
-    def _get_selection_order_policy(self):
-        return self.env['sale.order'].fields_get(
-            allfields=['order_policy'])['order_policy']['selection']
-
-    def default_order_policy(self):
-        default_dict = self.env['sale.order'].default_get(['order_policy'])
-        return default_dict.get('order_policy')
-
-    @api.model
-    def _get_selection_invoice_state(self):
-        return self.env['stock.picking'].fields_get(
-            allfields=['invoice_state'])['invoice_state']['selection']
-
-    def default_invoice_state(self):
-        default_dict = self.env['stock.picking'].default_get(['invoice_state'])
-        return default_dict.get('invoice_state')
-
     name = fields.Char(string='Name', required=True, translate=True)
     description = fields.Text(string='Description', translate=True)
     sequence_id = fields.Many2one(
@@ -51,17 +33,11 @@ class SaleOrderTypology(models.Model):
     journal_id = fields.Many2one(
         comodel_name='account.journal', string='Billing Journal',
         domain=[('type', '=', 'sale')])
-    refund_journal_id = fields.Many2one(
-        comodel_name='account.journal', string='Refund Billing Journal',
-        domain=[('type', '=', 'sale_refund')])
     warehouse_id = fields.Many2one(
         comodel_name='stock.warehouse', string='Warehouse', required=True)
     picking_policy = fields.Selection(
         selection='_get_selection_picking_policy', string='Shipping Policy',
         required=True, default=default_picking_policy)
-    order_policy = fields.Selection(
-        selection='_get_selection_order_policy', string='Create Invoice',
-        required=True, default=default_order_policy)
-    invoice_state = fields.Selection(
-        selection='_get_selection_invoice_state', string='Invoice Control',
-        required=True, default=default_invoice_state)
+    company_id = fields.Many2one(
+        'res.company',
+        related='warehouse_id.company_id', store=True, readonly=True)
