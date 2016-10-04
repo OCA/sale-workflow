02dcd17e3b9247a6e0bf397e4d9fbb3af8297d01
eLBati <lorenzo.battistini@agilebg.com>
2016-10-04 12:12:35 +0200

    FIX sale_order_type: propagate sale type to invoice
    Conflicts:
    	sale_order_type/models/sale_order.py
    	sale_order_type/models/stock_picking.py
    	sale_order_type/tests/test_sale_order_type.py
    
    ADD Payment Term, Pricelist and Incoterm to sale order type
    
    Conflicts:
    	sale_order_type/models/sale_order.py
    	sale_order_type/tests/test_sale_order_type.py
    	sale_order_type/views/sale_order_type_view.xml
    
    ADD sale type for invoices
    
    Conflicts:
    	sale_order_type/README.rst
    	sale_order_type/models/account_invoice.py
    	sale_order_type/tests/test_sale_order_type.py
    
    IMP description

---
+++ sale_order_type/models/account_invoice.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from openerp import api, models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    def _get_order_type(self):
+        return self.env['sale.order.type'].search([], limit=1)
+
+    sale_type_id = fields.Many2one(
+        comodel_name='sale.order.type', string='Type', default=_get_order_type)
+
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        super(AccountInvoice, self)._onchange_partner_id()
+        if self.partner_id:
+            if self.partner_id.sale_type:
+                print 'asdasdasd'
+                self.sale_type_id = self.partner_id.sale_type.id
+
+    @api.onchange('sale_type_id')
+    def onchange_sale_type_id(self):
+        if self.sale_type_id.payment_term_id:
+            self.payment_term = self.sale_type_id.payment_term_id.id
+        if self.sale_type_id.journal_id:
+            self.journal_id = self.sale_type_id.journal_id.id
