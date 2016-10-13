ca293e647ef3344b247e5f009decdabb11a98e5b
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-13 15:15:33 +0200

    sale_order_type: Update authors

--- sale_order_type/__manifest__.py
+++ sale_order_type/__manifest__.py
@@ -1,33 +1,18 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#                                                                            #
-#  OpenERP, Open Source Management Solution.                                 #
-#                                                                            #
-#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
-#                                                                            #
-#  This program is free software: you can redistribute it and/or modify      #
-#  it under the terms of the GNU Affero General Public License as            #
-#  published by the Free Software Foundation, either version 3 of the        #
-#  License, or (at your option) any later version.                           #
-#                                                                            #
-#  This program is distributed in the hope that it will be useful,           #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
-#  GNU Affero General Public License for more details.                       #
-#                                                                            #
-#  You should have received a copy of the GNU Affero General Public License  #
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                            #
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2015 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
+# Copyright 2015-2016 Oihane Crucelaegui <oihane@avanzosc.com>
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Lorenzo Battistini
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sale Order Types",
     "version": "9.0.1.0.0",
     "category": "Sales Management",
-    "author": "OdooMRP team, "
-              "Grupo Vermon, "
-              "AvanzOSC, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "author": "Grupo Vermon,"
+              "AvanzOSC,"
+              "Tecnativa,"
+              "Agile Business Group,"
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
     "license": "AGPL-3",
