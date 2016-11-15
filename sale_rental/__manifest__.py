fd67fe0d86daeb1d379c48b28f2d07b1cf8181b9
Dhinesh, Sodexis <dhinesh@sodexis.com>
2016-11-15 12:37:47 +0530

    [10.0][MIG] sale_start_end_dates and sale_rental

--- sale_rental/__manifest__.py
+++ sale_rental/__manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (http://www.akretion.com)
+# Copyright 2014-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2016 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Rental',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'summary': 'Manage Rental of Products',
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    'author': 'Akretion, Sodexis, Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
-    'depends': ['sale_start_end_dates', 'sale_stock'],
+    'depends': ['sale_start_end_dates', 'sale_stock', 'sales_team'],
     'data': [
         'data/rental_data.xml',
         'views/sale_order.xml',
