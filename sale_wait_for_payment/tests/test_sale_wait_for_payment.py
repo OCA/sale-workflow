2997d2ace20542196ca623a659387b859d1f1347
dreambits <admin@dreambits.in>
2016-12-15 01:35:45 +0530

    Fixed lint error of test file

--- sale_wait_for_payment/tests/test_sale_wait_for_payment.py
+++ sale_wait_for_payment/tests/test_sale_wait_for_payment.py
@@ -13,20 +13,20 @@ class TestSaleWaitForPayment(TransactionCase):
         self.sale_line_obj = self.env['sale.order.line']
 
     def test_no_procurement_control(self):
-        #lets set both the settings to False
+        # lets set both the settings to False
         self.env["sale.config.settings"].create({
             'default_procurement_product': 0,
             'default_procurement_customer': 0,
         }).execute()
 
-        #case 1 : product and customer are not blacklisted
+        # Case 1 : product and customer are not blacklisted
         self.createProduct(False)
         self.createCustomer(False)
         self.createSO()
         self.so.action_confirm()
         self.assertTrue(self.so.picking_ids)
 
-        #case 2 : product and customer are  blacklisted
+        # Case 2 : product and customer are  blacklisted
         self.createProduct(True)
         self.createCustomer(True)
         self.createSO()
@@ -34,34 +34,34 @@ class TestSaleWaitForPayment(TransactionCase):
         self.assertTrue(self.so.picking_ids)
 
     def test_product_procurement_control(self):
-        #lets set appropriate settings
+        # Lets set appropriate settings
         self.env["sale.config.settings"].create({
             'default_procurement_product': 1,
             'default_procurement_customer': 0,
         }).execute()
 
-        #case 1 : product and customer are not blacklisted
+        # Case 1 : product and customer are not blacklisted
         self.createProduct(False)
         self.createCustomer(False)
         self.createSO()
         self.so.action_confirm()
         self.assertTrue(self.so.picking_ids)
 
-        #case 2 : product is not blacklisted and customer is blacklisted
+        # Case 2 : product is not blacklisted and customer is blacklisted
         self.createProduct(False)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertTrue(self.so.picking_ids)
 
-        #case 3 : product and customer are  blacklisted
+        # Case 3 : product and customer are  blacklisted
         self.createProduct(True)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertFalse(self.so.picking_ids)
 
-        #case 4 : product is blacklisted , customer is not  blacklisted
+        # Case 4 : product is blacklisted , customer is not  blacklisted
         self.createProduct(True)
         self.createCustomer(False)
         self.createSO()
@@ -69,34 +69,34 @@ class TestSaleWaitForPayment(TransactionCase):
         self.assertFalse(self.so.picking_ids)
 
     def test_customer_procurement_control(self):
-        #lets set appropriate settings
+        # Lets set appropriate settings
         self.env["sale.config.settings"].create({
             'default_procurement_product': 0,
             'default_procurement_customer': 1,
         }).execute()
 
-        #case 1 : product and customer are not blacklisted
+        # Case 1 : product and customer are not blacklisted
         self.createProduct(False)
         self.createCustomer(False)
         self.createSO()
         self.so.action_confirm()
         self.assertTrue(self.so.picking_ids)
 
-        #case 2 : product is not blacklisted and customer is blacklisted
+        # Case 2 : product is not blacklisted and customer is blacklisted
         self.createProduct(False)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertFalse(self.so.picking_ids)
 
-        #case 3 : product and customer are  blacklisted
+        # Case 3 : product and customer are  blacklisted
         self.createProduct(True)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertFalse(self.so.picking_ids)
 
-        #case 4 : product is blacklisted , customer is not  blacklisted
+        # Case 4 : product is blacklisted , customer is not  blacklisted
         self.createProduct(True)
         self.createCustomer(False)
         self.createSO()
@@ -104,34 +104,34 @@ class TestSaleWaitForPayment(TransactionCase):
         self.assertTrue(self.so.picking_ids)
 
     def test_both_procurement_control(self):
-        #lets set appropriate settings
+        # lets set appropriate settings
         self.env["sale.config.settings"].create({
             'default_procurement_product': 1,
             'default_procurement_customer': 1,
         }).execute()
 
-        #case 1 : product and customer are not blacklisted
+        # Case 1 : product and customer are not blacklisted
         self.createProduct(False)
         self.createCustomer(False)
         self.createSO()
         self.so.action_confirm()
         self.assertTrue(self.so.picking_ids)
 
-        #case 2 : product is not blacklisted and customer is blacklisted
+        # Case 2 : product is not blacklisted and customer is blacklisted
         self.createProduct(False)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertFalse(self.so.picking_ids)
 
-        #case 3 : product and customer are  blacklisted
+        # Case 3 : product and customer are  blacklisted
         self.createProduct(True)
         self.createCustomer(True)
         self.createSO()
         self.so.action_confirm()
         self.assertFalse(self.so.picking_ids)
 
-        #case 4 : product is blacklisted , customer is not  blacklisted
+        # Case 4 : product is blacklisted , customer is not  blacklisted
         self.createProduct(True)
         self.createCustomer(False)
         self.createSO()
@@ -139,7 +139,6 @@ class TestSaleWaitForPayment(TransactionCase):
         self.assertFalse(self.so.picking_ids)
 
     def createSO(self):
-        #orderline
         orderline_values = {
             'product_id': self.product.id,
             'product_uom_qty': 1,
@@ -148,14 +147,14 @@ class TestSaleWaitForPayment(TransactionCase):
         }
 
         values = {
-            'partner_id':self.customer.id,
-            'partner_invoice_id':self.customer.id,
-            'partner_shipping_id':self.customer.id,
-            'order_line':[(0,0,orderline_values)]
+            'partner_id': self.customer.id,
+            'partner_invoice_id': self.customer.id,
+            'partner_shipping_id': self.customer.id,
+            'order_line': [(0, 0, orderline_values)]
         }
         self.so = self.sale_obj.create(values)
 
-    def createProduct(self,blacklist):
+    def createProduct(self, blacklist):
         values = {
             'name': 'Test Product',
             'type': 'product',
@@ -165,14 +164,10 @@ class TestSaleWaitForPayment(TransactionCase):
         }
         self.product = self.product_obj.create(values)
 
-    def createCustomer(self,blacklist):
+    def createCustomer(self, blacklist):
         values = {
             'name': 'Test Customer',
             'customer': True,
             'x_whitelist_blacklist': blacklist,
         }
         self.customer = self.customer_obj.create(values)
-
-
-
-
