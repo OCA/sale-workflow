8977b1bf1614ad04fc98510f8d07094bee9f6735
cubells <vicent@vcubells.net>
2016-12-16 19:56:42 +0100

    sale_order_price_recalculation module

--- sale_order_price_recalculation/__openerp__.py
+++ sale_order_price_recalculation/__openerp__.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
-# (c) 2014 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
-# (c) 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# (c) 2015 Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
+# Copyright 2014 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# Copyright 2015 Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
+# Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Price recalculation in sales orders",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
+    "category": "Sales Management",
+    "website": "http://github.com/OCA/sale-workflow",
+    "author": "AvanzOSC,"
+              "Grupo Vermon,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
     "depends": [
         "sale",
     ],
-    'license': 'AGPL-3',
-    "author": "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
-              "Grupo Vermon,"
-              "Odoo Community Association (OCA)",
-    "category": "Sales Management",
-    "website": "http://github.com/OCA/sale-workflow",
     "data": [
         "views/sale_order_view.xml",
     ],
-    "installable": True,
 }
