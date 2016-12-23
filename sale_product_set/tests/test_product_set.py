4cfc8125fd173093b766ad072588a5233bf08917
Denis Leemann <denis.leemann@camptocamp.com>
2016-12-23 11:31:39 +0100

    [10.0][MIG] sale_product_set

--- sale_product_set/tests/test_product_set.py
+++ sale_product_set/tests/test_product_set.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp.tests import common
+from odoo.tests import common
 
 
 class test_product_set(common.TransactionCase):
@@ -26,11 +26,11 @@ class test_product_set(common.TransactionCase):
                                      'quantity': 2})
         so_set.add_set()
         # checking our sale order
-        self.assertEquals(len(so.order_line), count_lines + 4)
-        # untaxed_amount + ((147*1)+(2100*1)+(2000*1)+(85*2)) * 2
-        self.assertEquals(so.amount_untaxed, untaxed_amount + 8834.0)
+        self.assertEquals(len(so.order_line), count_lines + 3)
+        # untaxed_amount + ((147*1)+(2100*1)+(85*2)) * 2
+        self.assertEquals(so.amount_untaxed, untaxed_amount + 4834.0)
         self.assertEquals(so.amount_tax, tax_amount + 0)  # without tax
-        self.assertEquals(so.amount_total, total_amount + 8834.0)
+        self.assertEquals(so.amount_total, total_amount + 4834.0)
         sequence = {}
         for line in so.order_line:
             sequence[line.product_id.id] = line.sequence
@@ -41,22 +41,18 @@ class test_product_set(common.TransactionCase):
         # make sure sale order line sequence keep sequence set on set
         seq_line1 = sequence.pop(
             self.env.ref(
-                "sale_product_set.product_set_line_computer_2"
+                "sale_product_set.product_set_line_computer_4"
             ).product_id.id)
         seq_line2 = sequence.pop(
             self.env.ref(
-                "sale_product_set.product_set_line_computer_4"
-            ).product_id.id)
-        seq_line3 = sequence.pop(
-            self.env.ref(
                 "sale_product_set.product_set_line_computer_1"
             ).product_id.id)
-        seq_line4 = sequence.pop(
+        seq_line3 = sequence.pop(
             self.env.ref(
                 "sale_product_set.product_set_line_computer_3"
             ).product_id.id)
         self.assertTrue(max([v for k, v in sequence.iteritems()]) <
-                        seq_line1 < seq_line2 < seq_line3 < seq_line4)
+                        seq_line1 < seq_line2 < seq_line3)
 
     def test_add_set_on_empty_so(self):
         so = self.sale_order.create({
@@ -67,4 +63,4 @@ class test_product_set(common.TransactionCase):
             active_id=so.id).create({'product_set_id': product_set.id,
                                      'quantity': 2})
         so_set.add_set()
-        self.assertEquals(len(so.order_line), 4)
+        self.assertEquals(len(so.order_line), 3)
