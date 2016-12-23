267e23f151ee5e4b146675c58281a2b8c467fd5a
Denis Leemann <denis.leemann@camptocamp.com>
2016-12-23 11:31:39 +0100

    [10.0][MIG] sale_product_set

--- sale_product_set/models/product_set.py
+++ sale_product_set/models/product_set.py
@@ -1,11 +1,21 @@
-# -*- encoding: utf-8 -*-
-from openerp import fields, models, _
+# -*- coding: utf-8 -*-
+# Copyright 2015 Anybox S.A.S
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
 
 
 class ProductSet(models.Model):
     _name = 'product.set'
     _description = 'Product set'
 
-    name = fields.Char(_('Name'), help=_('Product set name'), required=True)
+    name = fields.Char(
+        string='Name',
+        help='Product set name',
+        required=True
+    )
     set_line_ids = fields.One2many(
-        'product.set.line', 'product_set_id', _(u"Products"))
+        'product.set.line',
+        'product_set_id',
+        string="Products"
+    )
