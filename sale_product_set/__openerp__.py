aa46ed243637597fa7323a31f081ca9413f741ed
Denis Leemann <denis.leemann@camptocamp.com>
2016-12-23 11:31:39 +0100

    [10.0][MIG] sale_product_set

--- sale_product_set/__openerp__.py
+++ sale_product_set/__openerp__.py
@@ -4,7 +4,7 @@
     'name': 'Sale product set',
     'category': 'Sale',
     'author': 'Anybox, Odoo Community Association (OCA)',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'sequence': 150,
     'website': 'http://anybox.fr',
     'summary': "Sale product set",
