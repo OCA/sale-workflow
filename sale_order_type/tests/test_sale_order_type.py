ed24ce1937e09ad4a8329f34770f3526ab048709
Pierre Faniel <pierre@niboo.be>
2017-01-04 11:28:49 +0100

    [MIG] sale_order_type: Migrated to 10.0

--- sale_order_type/tests/test_sale_order_type.py
+++ sale_order_type/tests/test_sale_order_type.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# © 2015 Oihane Crucelaegui - AvanzOSC
+# © 2017 Pierre Faniel - Niboo SPRL (<https://www.niboo.be/>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import openerp.tests.common as common
+import odoo.tests.common as common
 
 
 class TestSaleOrderType(common.TransactionCase):
@@ -39,22 +40,51 @@ class TestSaleOrderType(common.TransactionCase):
         })
         self.partner.sale_type = self.sale_type
 
-    def test_sale_order_confirm(self):
+    def get_sale_order_vals(self):
         sale_line_dict = {
             'product_id': self.product.id,
             'name': self.product.name,
             'product_uom_qty': 1.0,
             'price_unit': self.product.lst_price,
         }
-        vals = {
+        return {
             'partner_id': self.partner.id,
             'order_line': [(0, 0, sale_line_dict)]
         }
-        sale_order = self.sale_order_model.create(vals)
-        sale_order.onchange_type_id()
-        sale_order.onchange_partner_id()
-        sale_order.action_confirm()
+
+    def test_sale_order_confirm(self):
+        sale_type = self.sale_type
+        order_vals = self.get_sale_order_vals()
+        order = self.sale_order_model.create(order_vals)
+        order.onchange_partner_id()
+        self.assertTrue(order.type_id == sale_type)
+
+        order.onchange_type_id()
+        self.assertTrue(order.warehouse_id == sale_type.warehouse_id)
+        self.assertTrue(order.picking_policy == sale_type.picking_policy)
+        self.assertTrue(order.payment_term_id == sale_type.payment_term_id)
+        self.assertTrue(order.pricelist_id == sale_type.pricelist_id)
+        self.assertTrue(order.incoterm == sale_type.incoterm_id)
+
+        order.action_confirm()
 
     def test_invoice_onchange_type(self):
-        invoice = self.invoice_model.new({'sale_type_id': self.sale_type.id})
+        sale_type = self.sale_type
+        invoice = self.invoice_model.new({'sale_type_id': sale_type.id})
         invoice.onchange_sale_type_id()
+        self.assertTrue(invoice.payment_term == sale_type.payment_term_id.id)
+        self.assertTrue(invoice.journal_id == sale_type.journal_id)
+
+    def test_invoice_onchange_partner(self):
+        invoice = self.invoice_model.create({'partner_id': self.partner.id})
+        invoice._onchange_partner_id()
+        self.assertTrue(invoice.sale_type_id == self.sale_type)
+
+    def test_prepare_invoice(self):
+        sale_type = self.sale_type
+        order_vals = self.get_sale_order_vals()
+        order_vals['type_id'] = sale_type.id
+        order = self.sale_order_model.create(order_vals)
+        invoice_vals = order._prepare_invoice()
+        self.assertTrue(invoice_vals['sale_type_id'] == sale_type.id)
+        self.assertTrue(invoice_vals['journal_id'] == sale_type.journal_id.id)
