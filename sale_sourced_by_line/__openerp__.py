493e61eab12015184ac76302dbf14a0755d9c611
SerpentCS <support@serpentcs.com>
2017-01-05 12:22:53 +0530

    [IMP] author name

--- sale_sourced_by_line/__openerp__.py
+++ sale_sourced_by_line/__openerp__.py
@@ -10,7 +10,7 @@
     'version': '9.0.1.0.0',
     "author": "Camptocamp,"
               "Eficent,"
-              "Serpent,"
+              "SerpentCS,"
               "Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'license': 'AGPL-3',
