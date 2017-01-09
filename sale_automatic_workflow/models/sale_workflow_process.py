7f9b2f2c60d1876f6054aa90ce88558e04b59a64
Dave Lasley <dave@laslabs.com>
2017-01-09 14:00:01 -0800

    [MIG] sale_automatic_workflow: Upgrade to v10 * Bump version * Change openerp references to odoo * Fix license headers * Move menu * Alphabetize imports * Remove data key from XML * Change deprecated invoice validation workflow call to replacement action method

--- sale_automatic_workflow/models/sale_workflow_process.py
+++ sale_automatic_workflow/models/sale_workflow_process.py
@@ -4,7 +4,7 @@
 # © 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleWorkflowProcess(models.Model):
