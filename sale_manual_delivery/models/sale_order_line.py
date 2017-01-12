06c08eb2bdbfd8bc9ec86a1dda64816fbf9217fa
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-12 18:39:30 +0100

    base module & sales_team
    Add base for wizard
    
    wizard for manual procurements
    
    Update base computation
    
    update to OCA guidelines

---
+++ sale_manual_delivery/models/sale_order_line.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def _action_procurement_create(self):
+        for line in self:
+            if (line.order_id.manual_procurement and
+                    line.product_id.type != 'service'):
+                return self.env['procurement.order'].browse()
+            else:
+                return super(SaleOrderLine, self)._action_procurement_create()
+
+    @api.multi
+    def _action_manual_procurement_create(self, product_qty, date_planned,
+                                          carrier):
+
+        new_procs = self.env['procurement.order']  # Empty recordset
+        for line in self:
+            # if line.state != 'sale' or not
+                # line.product_id._need_procurement():
+            #     continue
+            qty = 0.0
+            for proc in line.procurement_ids:
+                qty += proc.product_qty
+
+            if not line.order_id.procurement_group_id:
+                vals = line.order_id._prepare_procurement_group()
+                line.order_id.procurement_group_id = self.env[
+                    "procurement.group"].create(vals)
+
+            vals = line._prepare_order_line_procurement(
+                group_id=line.order_id.procurement_group_id.id)
+            vals['date_planned'] = date_planned  # line added
+            vals['product_qty'] = product_qty  # line added
+            vals['carrier_id'] = carrier.id  # line added
+            new_proc = self.env["procurement.order"].create(vals)
+            new_proc.message_post_with_view('mail.message_origin_link',
+                                            values={'self': new_proc,
+                                                    'origin': line.order_id},
+                                            subtype_id=self.env.ref(
+                                                'mail.mt_note').id)
+            new_procs += new_proc
+        new_procs.run()
+        return new_procs
---
+++ sale_manual_delivery/wizard/line.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Denis Leemann, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class ManualLine(models.TransientModel):
+    _name = "manual.line"
+
+    manual_proc_id = fields.Many2one(
+        'manual.procurement',
+        string='Wizard manual procurement',
+    )
+    order_line_id = fields.Many2one(
+        'sale.order.line',
+        string='Sale Order Line',
+    )
+    ordered_qty = fields.Float(
+        'Ordered Quantity',
+        readonly=True,
+    )
+    remaining = fields.Float(
+        'Remaining',
+        compute='compute_remaining',
+        readonly=True,
+    )
+    product_qty = fields.Float(
+        'Quantity to Ship',
+    )
+
+    @api.multi
+    @api.depends('product_qty')
+    def compute_remaining(self):
+        for line in self:
+            line.remaining = line.ordered_qty - line.product_qty
