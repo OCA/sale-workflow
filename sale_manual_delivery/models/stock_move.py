fcc3ae88b995a241db36e1fa847fe548c50817dc
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-30 18:15:07 +0100

    wizard for manual procurements

--- sale_manual_delivery/models/stock_move.py
+++ sale_manual_delivery/models/stock_move.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import models, api
 
 
@@ -10,23 +11,20 @@ class StockMove(models.Model):
     def _get_new_picking_values(self):
         res = super(StockMove, self)._get_new_picking_values()
         if self.procurement_id.carrier_id:
-            res['carrier_id'] = self.procurement_id.carrier_id
+            res['carrier_id'] = self.procurement_id.carrier_id.id
+        res = super(StockMove, self)._get_new_picking_values()
         return res
 
     # Below comment is from Odoo sources
     # TDE DECORATOR: remove that api.multi when action_confirm is migrated
     @api.multi
     def assign_picking(self):
-        import pdb
-        pdb.set_trace()
+
         Picking = self.env['stock.picking']
+        self.recompute()
 
         for move in self:
-            ## ---> Set BreakPoint
-            import pdb;
-            pdb.set_trace()
             picking = Picking.search([
-                ('carrier_id')
                 ('group_id', '=', move.group_id.id),
                 ('location_id', '=', move.location_id.id),
                 ('location_dest_id', '=', move.location_dest_id.id),
@@ -40,7 +38,8 @@ class StockMove(models.Model):
             if not picking:
                 picking = Picking.create(move._get_new_picking_values())
             move.write({'picking_id': picking.id})
-
         return True
         # return super(StockMove, self).assign_picking()
         # ELSE return super
+        #
+# move.procurement_id.sale_line_id.order_id.carrier_id
--- sale_manual_delivery/wizard/manual_proc.py
+++ sale_manual_delivery/wizard/manual_proc.py
@@ -3,23 +3,43 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
+from odoo.tools import float_compare
 
 
 class ManualProcurement(models.TransientModel):
     """Creates procurements manually"""
     _name = "manual.procurement"
 
+    def _set_order_id(self):
+        assert 'active_ids' in self._context
+        return self.env['sale.order'].browse(self._context['active_ids'])
+
+    def _set_order_line_ids(self):
+        so = self._set_order_id()
+        ManualLine = self.env['manual.line']
+        lines = self.env['manual.line']
+        for line in so.order_line:
+            vals = {
+                'order_line_id': line.id,
+                'ordered_qty': line.product_uom_qty
+            }
+            manual_line = ManualLine.create(vals)
+            lines |= manual_line
+        return lines
+
     date_planned = fields.Date(
         string='Date Planned'
     )
     order_id = fields.Many2one(
         'sale.order',
-        string='Sale Order TO HIDE',  # TODO HIDE
+        string='Sale Order',  # TODO HIDE
+        default=_set_order_id
     )
     line_ids = fields.One2many(
         'manual.line',
         'manual_proc_id',
         string='Lines to validate',
+        default=_set_order_line_ids
     )
     carrier_id = fields.Many2one(
         'delivery.carrier',
@@ -29,8 +49,10 @@ class ManualProcurement(models.TransientModel):
     @api.multi
     def record_picking(self):
         for wizard in self:
-            ## ---> Set BreakPoint
-            import pdb;
-            pdb.set_trace()
             carrier_id = wizard.carrier_id
             date_planned = wizard.date_planned
+            for line in wizard.line_ids:
+                if float_compare(line.product_qty, 0, 2):
+                    product_qty = line.product_qty
+                    line.order_line_id._action_manual_procurement_create(
+                        product_qty, date_planned, carrier_id)
