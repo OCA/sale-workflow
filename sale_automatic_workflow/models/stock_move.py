55cc9a2904398f5c04fbb847582cdda9d8bd1ed8
Dave Lasley <dave@laslabs.com>
2017-01-30 11:14:25 -0800

    Fix API type & method name for picking values

--- sale_automatic_workflow/models/stock_move.py
+++ sale_automatic_workflow/models/stock_move.py
@@ -10,10 +10,10 @@ from odoo import api, models
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    @api.model
-    def _prepare_picking_assign(self, move):
-        values = super(StockMove, self)._prepare_picking_assign(move)
-        if move.procurement_id.sale_line_id:
-            sale = move.procurement_id.sale_line_id.order_id
+    @api.multi
+    def _get_new_picking_values(self):
+        values = super(StockMove, self)._get_new_picking_values()
+        if self.procurement_id.sale_line_id:
+            sale = self.procurement_id.sale_line_id.order_id
             values['workflow_process_id'] = sale.workflow_process_id.id
         return values
