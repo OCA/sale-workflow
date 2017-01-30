88e6ba08b4a4d838dd0eb9380cc07a6729b6ef00
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-30 18:15:07 +0100

    wizard for manual procurements

--- sale_manual_delivery/wizard/line.py
+++ sale_manual_delivery/wizard/line.py
@@ -2,10 +2,10 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
-class ManualProcurement(models.TransientModel):
+class ManualLine(models.TransientModel):
     _name = "manual.line"
 
     manual_proc_id = fields.Many2one(
@@ -15,14 +15,21 @@ class ManualProcurement(models.TransientModel):
     order_line_id = fields.Many2one(
         'sale.order.line',
         string='Sale Order Line',
-        readonly=True,
+        readonly=False,
     )
     ordered_qty = fields.Float(
         'Ordered Quantity',
     )
     remaining = fields.Float(
-        'Computed remaining to ship',
+        'Remaining',
+        compute='compute_remaining'
     )
     product_qty = fields.Float(
         'Quantity to Ship',
     )
+
+    @api.multi
+    @api.depends('ordered_qty', 'product_qty')
+    def compute_remaining(self):
+        for line in self:
+            line.remaining = line.ordered_qty - line.product_qty
