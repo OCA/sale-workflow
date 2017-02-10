f663f3ddda3fe29d26f1dad8b1c2feb453fd836e
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-10 17:38:00 +0100

    update to OCA guidelines

--- sale_manual_delivery/__manifest__.py
+++ sale_manual_delivery/__manifest__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Sale Manual Delivery',
     'category': 'Sale',
     'author': 'Camptocamp SA, Odoo Community Association (OCA)',
     'version': '10.0.1.0.0',
-    'sequence': 150,
     'website': 'http://camptocamp.com',
     'summary': "Sale product set",
     'depends': [
@@ -17,8 +16,6 @@
         'views/crm_team_view.xml',
         'views/sale_order_view.xml',
         'wizard/manual_proc_view.xml',
-    ],
-    'demo': [
     ],
     'installable': True,
     'application': False,
