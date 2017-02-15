783358fec6b90a634fdeeef2bc825df805f54182
Joël Grand-Guillaume <joel.grandguillaume@gmail.com>
2017-02-15 12:56:17 +0100

    [TYP] Change manual proc for manual delivery

--- sale_manual_delivery/models/sale_order.py
+++ sale_manual_delivery/models/sale_order.py
@@ -7,25 +7,25 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    manual_procurement = fields.Boolean(
-        string='Manual Procurement',
+    manual_delivery = fields.Boolean(
+        string='Manual Delivery',
         default=False
     )
 
     @api.onchange('team_id')
     def _onchange_team_id(self):
-        self.manual_procurement = self.team_id.manual_procurement \
+        self.manual_delivery = self.team_id.manual_delivery \
             if self.team_id else False
 
     @api.multi
-    def action_manual_procurement_wizard(self):
+    def action_manual_delivery_wizard(self):
         self.ensure_one()
-        wizard = self.env['manual.procurement'].create({
+        wizard = self.env['manual.delivery'].create({
             'order_id': self.id,
         })
         wizard.onchange_order_id()
         action = self.env.ref(
-            'sale_manual_delivery.action_wizard_manual_procurement'
+            'sale_manual_delivery.action_wizard_manual_delivery'
         ).read()[0]
         action['res_id'] = wizard.id
         return action
