60f4f1a42edc6542c7ee8ffcfd7bb893ba38eb02
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-02-15 11:50:36 +0530

    [ADD] Added Sale_order_revision v10

--- sale_order_revision/__manifest__.py
+++ sale_order_revision/__manifest__.py
@@ -1,42 +1,25 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
-#    @author Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Sale order revisions",
-    'version': '8.0.0.1.0',
-    'category': 'Sale Management',
-    'author': 'Agile Business Group,'
-              'Camptocamp,'
-              'Akretion,'
-              'Odoo Community Association (OCA)',
-    'website': 'http://www.agilebg.com',
-    'license': 'AGPL-3',
-    "depends": ['sale'],
+    "name": "Sale order revisions",
+    "version": "10.0.1.0.0",
+    "category": "Sale Management",
+    "author": "Agile Business Group,"
+              "Camptocamp,"
+              "Akretion,"
+              "Odoo Community Association (OCA), "
+              "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.agilebg.com",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
     "data": [
-        'view/sale_order.xml',
-        ],
-    "test": [
-        'test/sale_order.yml',
-        ],
-    "active": False,
-    'installable': False,
-    "post_init_hook": 'populate_unrevisioned_name',
+        "view/sale_order.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "populate_unrevisioned_name",
 }
