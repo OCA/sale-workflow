6b77a5dc710d823581001e942ded6e62b2105538
Joël Grand-Guillaume <joel.grandguillaume@gmail.com>
2017-02-15 14:00:43 +0100

    [IMP] Toggle manual,standard as a button in SO + UI
    [IMP] Update translation files
    
    [IMP] lot's of functionnal improvments
    
    Don't add already planned or shipped lines in wizard
    Ensure you have one picking per manual delivery
    Forbid to add lines or ship more than ordered
    Planned date is required
    Improve views, naming and buttons
    Better Readme
    Default Carrier set from SO
    
    [FIX] Do not add product of type 'service' in manual delivery creation

--- sale_manual_delivery/models/crm_team.py
+++ sale_manual_delivery/models/crm_team.py
@@ -9,5 +9,8 @@ class CrmTeam(models.Model):
 
     manual_delivery = fields.Boolean(
         string='Manual Delivery',
-        default=False
+        default=False,
+        help="If Manual, the deliveries are not created at SO confirmation.\
+        You need to use the Create Delivery button in order to reserve and \
+        ship the goods."
     )
