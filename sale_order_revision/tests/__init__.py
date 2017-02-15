dea1b1abc1b01e4830c0365706c1d0d19a940a84
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-02-15 11:50:36 +0530

    [MIG] sale_order_revision: Migration to v10

--- sale_order_revision/tests/__init__.py
+++ sale_order_revision/tests/__init__.py
@@ -2,10 +2,4 @@
 # Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
-
-
-def populate_unrevisioned_name(cr, registry):
-    cr.execute('UPDATE sale_order '
-               'SET unrevisioned_name = name '
-               'WHERE unrevisioned_name is NULL')
+from . import test_sale_order_revision
