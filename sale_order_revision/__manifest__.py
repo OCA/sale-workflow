dcd9f9e88db2474518adaf1ec8d3354cb6d37251
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-02-15 11:50:36 +0530

    [MIG] sale_order_revision: Migration to v10

---
+++ sale_order_revision/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale order revisions",
+    "version": "10.0.1.0.0",
+    "category": "Sale Management",
+    "author": "Agile Business Group,"
+              "Camptocamp,"
+              "Akretion,"
+              "Odoo Community Association (OCA), "
+              "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.agilebg.com",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "view/sale_order.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "populate_unrevisioned_name",
+}
