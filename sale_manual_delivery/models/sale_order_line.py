36a8d4b4f9afa007626bc3e66b03d2fe44410234
Joël Grand-Guillaume <joel.grandguillaume@gmail.com>
2017-02-15 16:46:45 +0100

    [IMP] lot's of functionnal improvments
    Don't add already planned or shipped lines in wizard
    Ensure you have one picking per manual delivery
    Forbid to add lines or ship more than ordered
    Planned date is required
    Improve views, naming and buttons
    Better Readme
    Default Carrier set from SO

--- sale_manual_delivery/models/sale_order_line.py
+++ sale_manual_delivery/models/sale_order_line.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models, api
+from odoo import fields, models, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    existing_qty = fields.Float(
+        'Existing quantity',
+        compute='_get_existing_qty',
+        help = "Quantity already planned or shipped (stock movements \
+            already created)",
+        readonly=True,
+    )
 
     @api.multi
     def _action_procurement_create(self):
@@ -17,14 +25,10 @@ class SaleOrderLine(models.Model):
                 return super(SaleOrderLine, self)._action_procurement_create()
 
     @api.multi
-    def _action_manual_delivery_create(self, product_qty, date_planned,
+    def _action_manual_procurement_create(self, product_qty, date_planned,
                                           carrier):
-
         new_procs = self.env['procurement.order']  # Empty recordset
         for line in self:
-            # if line.state != 'sale' or not
-                # line.product_id._need_procurement():
-            #     continue
             qty = 0.0
             for proc in line.procurement_ids:
                 qty += proc.product_qty
@@ -48,3 +52,24 @@ class SaleOrderLine(models.Model):
             new_procs += new_proc
         new_procs.run()
         return new_procs
+
+    @api.multi
+    def _get_existing_qty(self):
+        """Computes the remai quantity on sale order lines, based on done 
+        stock moves related to its procurements
+        """
+        for line in self:
+            qty = 0.0
+            for move in line.procurement_ids.mapped('move_ids').filtered(
+                    lambda r: r.state not in ('draft', 'cancel')
+                    and not r.scrapped):
+                if move.location_dest_id.usage == "customer":
+                    qty += move.product_uom._compute_quantity(
+                        move.product_uom_qty, 
+                        line.product_uom)
+                elif move.location_dest_id.usage == "internal" \
+                    and move.to_refund_so:
+                    qty -= move.product_uom._compute_quantity(
+                        move.product_uom_qty,
+                        line.product_uom)
+            line.existing_qty = qty
