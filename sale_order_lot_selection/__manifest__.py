41d425715a5307cd8ed24122668f8f014eb05ff6
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-02-16 10:24:01 +0100

    [MIG] sale_order_lot_selection: Migrated to 10.0

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -1,36 +1,18 @@
 # -*- coding: utf-8 -*-
-#########################################################################
-#                                                                       #
-# Copyright (C) 2015  Agile Business Group                              #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU Affero General Public License as        #
-# published by the Free Software Foundation, either version 3 of the    #
-# License, or (at your option) any later version.                       #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU Affero General Public Licensefor more details.                    #
-#                                                                       #
-# You should have received a copy of the                                #
-# GNU Affero General Public License                                     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#                                                                       #
-#########################################################################
+# © 2015 Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
 {
     'name': 'Sale Order Lot Selection',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Sales Management',
     'author': "Odoo Community Association (OCA), Agile Business Group",
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
     'depends': ['sale_stock', 'procurement'],
     'data': ['view/sale_view.xml'],
-    'installable': False,
-    'active': False,
+    'installable': True,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
