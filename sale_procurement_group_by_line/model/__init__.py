19f459a4d033a5355049ff8265a08bea6191c682
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-02-23 17:55:30 +0530

    9.0 sale_procurement_group_by_line

--- sale_procurement_group_by_line/model/__init__.py
+++ sale_procurement_group_by_line/model/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 Serpent Consulting Services Pvt. Ltd.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
 from . import sale
