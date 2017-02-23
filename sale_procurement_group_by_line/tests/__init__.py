cc91354187bb21a26ecc3032ee3dbaaabfd64734
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-02-23 17:55:30 +0530

    9.0 sale_procurement_group_by_line

--- sale_procurement_group_by_line/tests/__init__.py
+++ sale_procurement_group_by_line/tests/__init__.py
@@ -4,4 +4,4 @@
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import model
+from . import test_sale_procurement_group_by_line
