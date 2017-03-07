ec2f700226b70b80ba1ebb1e57d1f6e35729311b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-03-07 10:19:06 +0100

    [NEW] sale_double_validation

---
+++ sale_double_validation/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{'name': 'Double validation for Sales',
+ 'summary': "",
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'maintainer': 'Camptocamp',
+ 'license': 'AGPL-3',
+ 'category': 'category',
+ 'complexity': "complexity",  # easy, normal, expert
+ 'depends': ['sale'],
+ 'website': 'www.camptocamp.com',
+ 'data': ['views/company.xml',
+          'views/sale.xml'],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ }
