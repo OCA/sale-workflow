f5aff98725c66ef520bf9bdf810c0121c6d9846a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-03-07 10:19:06 +0100

    [NEW] sale_double_validation

---
+++ sale_double_validation/models/company.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from openerp import fields, models
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    so_double_validation = fields.Selection([
+        ('one_step', 'Confirm sale orders in one step'),
+        ('two_step', 'Get 2 levels of approvals to confirm a sale order')
+        ], string="Levels of Approvals", default='one_step',
+        help="Provide a double validation mechanism for purchases")
+    so_double_validation_amount = fields.Monetary(
+        string='Double validation amount', default=5000,
+        help="Minimum amount for which a double validation is required")
