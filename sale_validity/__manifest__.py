62bb233059ebcf5e296f803eb6ede8df5e8e4dc9
Alexis de Lattre <alexis.delattre@akretion.com>
2017-03-09 14:48:15 +0100

    [MIG] sale_validity to v10.0
    Convert YAML tests to unittests
    Add README.rst

---
+++ sale_validity/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2013-2017 Camptocamp SA (Jacques-Etienne Baudoux)
+# © 2014-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+
+
+{
+    'name': 'Default Quotation Validity',
+    'version': '10.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Set a default validity delay on quotations',
+    'depends': ['sale'],
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'website': 'http://www.camptocamp.com',
+    'data': ['views/sale_config_settings.xml'],
+    'installable': True,
+}
---
+++ sale_validity/models/sale_order.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# © 2013-2017 Camptocamp SA
+# © 2014-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    validity_date = fields.Date(
+        track_visibility='onchange',
+        default=lambda rec: rec._default_validity_date())
+
+    @api.model
+    def _default_validity_date(self):
+        validity_date_str = False
+        company_pool = self.env['res.company']
+        company = company_pool._company_default_get('sale.order')
+        if company.default_sale_order_validity_days:
+            today_str = fields.Date.context_today(self)
+            today = fields.Date.from_string(today_str)
+            validity_date = today + relativedelta(
+                days=company.default_sale_order_validity_days)
+            validity_date_str = fields.Date.to_string(validity_date)
+        return validity_date_str
+
+    @api.onchange('date_order')
+    def _onchange_date_order(self):
+        company_pool = self.env['res.company']
+        if self.date_order:
+            company = self.company_id or company_pool._company_default_get(
+                'sale.order')
+            if company.default_sale_order_validity_days:
+                date_order = fields.Datetime.from_string(self.date_order)
+                validity_date = date_order + relativedelta(
+                    days=company.default_sale_order_validity_days)
+                self.validity_date = fields.Date.to_string(validity_date)
