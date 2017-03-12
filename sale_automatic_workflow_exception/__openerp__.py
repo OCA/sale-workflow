cb42332505350dc900ff20796c43e8fe12ed063d
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-03-12 11:28:30 +0100

    fix website url in manifest

--- sale_automatic_workflow_exception/__openerp__.py
+++ sale_automatic_workflow_exception/__openerp__.py
@@ -7,7 +7,7 @@
 {'name': 'Sale Automatic Workflow Exception',
  'version': '8.0.1.0.0',
  'author': "Akretion,Odoo Community Association (OCA)",
- 'website': 'www.akretion.com',
+ 'website': 'http://www.akretion.com/',
  'license': 'AGPL-3',
  'category': 'Generic Modules',
  'depends': [
