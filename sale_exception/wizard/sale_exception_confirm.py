c425cb903daf1f7ba76cbca00db4e26e53de3357
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-13 11:16:23 +0100

    [FIX] sale_exception misc
    * [FIX] sale_exception confirm wizard
    * [FIX] add test of test_all_draft_orders & flake8 & readme.rst
    * [FIX] improve codecov test
    * [FIX] delete odl translation
    * [FIX] add Camptocamp to author
    * [FIX] readme.rst

--- sale_exception/wizard/sale_exception_confirm.py
+++ sale_exception/wizard/sale_exception_confirm.py
@@ -1,35 +1,20 @@
 # -*- coding: utf-8 -*-
 # © 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
+# © 2017 Mourad EL HADJ MIMOUNE, Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleExceptionConfirm(models.TransientModel):
-
     _name = 'sale.exception.confirm'
+    _inherit = ['exception.rule.confirm']
 
-    sale_id = fields.Many2one('sale.order', 'Sale')
-    exception_ids = fields.Many2many('sale.exception',
-                                     string='Exceptions to resolve',
-                                     readonly=True)
-    ignore = fields.Boolean('Ignore Exceptions')
+    related_model_id = fields.Many2one('sale.order', 'Sale')
 
-    @api.model
-    def default_get(self, field_list):
-        res = super(SaleExceptionConfirm, self).default_get(field_list)
-        order_obj = self.env['sale.order']
-        sale_id = self._context.get('active_ids')
-        assert len(sale_id) == 1, "Only 1 ID accepted, got %r" % sale_id
-        sale_id = sale_id[0]
-        sale = order_obj.browse(sale_id)
-        exception_ids = [e.id for e in sale.exception_ids]
-        res.update({'exception_ids': [(6, 0, exception_ids)]})
-        res.update({'sale_id': sale_id})
-        return res
-
-    @api.one
+    @api.multi
     def action_confirm(self):
+        self.ensure_one()
         if self.ignore:
-            self.sale_id.ignore_exception = True
-        return {'type': 'ir.actions.act_window_close'}
+            self.related_model_id.ignore_exception = True
+        return super(SaleExceptionConfirm, self).action_confirm()
