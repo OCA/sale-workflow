5aa99620dd0ec121d4b839a29f800ff526c1ed9f
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-04-03 15:02:36 +0200

    [FIX] sale_order_revision: 2 things:
    * Fix create revision and improve view invisibility
    * Fix unrevisioned name and improve test

--- sale_order_revision/tests/test_sale_order_revision.py
+++ sale_order_revision/tests/test_sale_order_revision.py
@@ -14,6 +14,8 @@ class TestSaleOrderRevision(common.TransactionCase):
         self.partner_id = self.env.ref('base.res_partner_2').id
         self.product_id1 = self.env.ref('product.product_product_1').id
         self.sale_order_1 = self._create_sale_order()
+        self.action = self._revision_sale_order(self.sale_order_1)
+        self.revision_1 = self.sale_order_1.current_revision_id
 
     def _create_sale_order(self):
         # Creating a sale order
@@ -24,16 +26,46 @@ class TestSaleOrderRevision(common.TransactionCase):
                 'product_uom_qty': '15.0'
             })]
         })
-        # Cancel the sale order
-        new_sale.action_cancel()
-        # Create a new revision
-        self.action = new_sale.copy_quotation()
         return new_sale
 
+    def _revision_sale_order(self, sale_order):
+        # Cancel the sale order
+        sale_order.action_cancel()
+        # Create a new revision
+        return sale_order.create_revision()
+
     def test_order_revision(self):
-        """Check whether revision number is 1 or not."""
-        self.assertEquals(self.sale_order_1.id, self.action['res_id'])
-        if self.sale_order_1.old_revision_ids:
-            self.sale_order_1.active = False
-        self.assertEquals(self.sale_order_1.revision_number, 1)
-        self.assertEquals(self.sale_order_1.name.endswith('-01'), True)
+        """Check revision process"""
+        self.assertEquals(self.sale_order_1.unrevisioned_name,
+                          self.sale_order_1.name)
+        self.assertEquals(self.sale_order_1.state, 'cancel')
+        self.assertFalse(self.sale_order_1.active)
+        self.assertEquals(self.revision_1.unrevisioned_name,
+                          self.sale_order_1.name)
+        self.assertEquals(self.revision_1.state, 'draft')
+        self.assertTrue(self.revision_1.active)
+        self.assertEquals(self.revision_1.old_revision_ids,
+                          self.sale_order_1)
+        self.assertEquals(self.revision_1.revision_number, 1)
+        self.assertEquals(self.revision_1.name.endswith('-01'), True)
+
+        self._revision_sale_order(self.revision_1)
+        revision_2 = self.revision_1.current_revision_id
+
+        self.assertEquals(revision_2, self.sale_order_1.current_revision_id)
+        self.assertEquals(self.revision_1.state, 'cancel')
+        self.assertFalse(self.revision_1.active)
+        self.assertEquals(revision_2.unrevisioned_name,
+                          self.sale_order_1.name)
+        self.assertEquals(revision_2.state, 'draft')
+        self.assertTrue(revision_2.active)
+        self.assertEquals(revision_2.old_revision_ids,
+                          self.sale_order_1 + self.revision_1)
+        self.assertEquals(revision_2.revision_number, 2)
+        self.assertEquals(revision_2.name.endswith('-02'), True)
+
+    def test_simple_copy(self):
+        sale_order_2 = self._create_sale_order()
+        self.assertEquals(sale_order_2.name, sale_order_2.unrevisioned_name)
+        sale_order_3 = sale_order_2.copy()
+        self.assertEquals(sale_order_3.name, sale_order_3.unrevisioned_name)
