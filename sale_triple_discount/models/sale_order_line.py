f577e1964da4190c09b59f6c23c4b6e100f01fa7
Alex Comba <alex.comba@agilebg.com>
2017-04-10 17:10:13 +0200

    [FIX] sale_triple_discount * manage default in the right way * override _onchange_discount on sale order line to change three discounts values coherently

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -10,25 +10,55 @@ import openerp.addons.decimal_precision as dp
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    @api.model
+    def _default_discount1(self):
+        default_discount1 = 0.0
+        partner_id = self.env.context.get('partner_id', False)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            default_discount1 = partner.default_discount1
+        return default_discount1
+
+    @api.model
+    def _default_discount2(self):
+        default_discount2 = 0.0
+        partner_id = self.env.context.get('partner_id', False)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            default_discount2 = partner.default_discount2
+        return default_discount2
+
+    @api.model
+    def _default_discount3(self):
+        default_discount3 = 0.0
+        partner_id = self.env.context.get('partner_id', False)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            default_discount3 = partner.default_discount3
+        return default_discount3
+
     discount1 = fields.Float(
         'Discount 1 (%)',
         digits=dp.get_precision('Discount'),
         readonly=True,
+        default=_default_discount1,
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
     )
     discount2 = fields.Float(
         'Discount 2 (%)',
         digits=dp.get_precision('Discount'),
         readonly=True,
+        default=_default_discount2,
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
     )
     discount3 = fields.Float(
         'Discount 3 (%)',
         digits=dp.get_precision('Discount'),
         readonly=True,
+        default=_default_discount3,
         states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
     )
-    discount = fields.Float(compute='get_discount')
+    discount = fields.Float(compute='get_discount', store=True)
 
     @api.multi
     @api.depends('discount1', 'discount2', 'discount3')
@@ -40,6 +70,20 @@ class SaleOrderLine(models.Model):
                     discount_factor * ((100.0 - discount) / 100.0))
             line.discount = 100.0 - (discount_factor * 100.0)
 
+    @api.onchange(
+        'product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
+    def _onchange_discount(self):
+        super(SaleOrderLine, self)._onchange_discount()
+        if self.discount != 0:
+            # to keep discount value calculated by pricelist
+            self.discount1 = self.discount
+            self.discount2 = 0
+            self.discount3 = 0
+        else:
+            # to recalculate discount value based on three discounts
+            # when these latter are filled by their default
+            self.get_discount()
+
     @api.multi
     def _prepare_invoice_line(self, qty):
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
@@ -48,10 +92,3 @@ class SaleOrderLine(models.Model):
             'discount2': self.discount2,
             'discount3': self.discount3})
         return res
-
-    @api.onchange('product_id')
-    def onchange_product_id(self):
-        partner = self.order_id.partner_id
-        self.discount1 = partner.default_discount1
-        self.discount2 = partner.default_discount2
-        self.discount3 = partner.default_discount3
