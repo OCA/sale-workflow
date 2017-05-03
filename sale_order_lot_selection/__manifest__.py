81fe1c95b0526f2a979b42183c0060f02c30e4ad
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-05-03 22:37:09 +0800

    [FIX] flake8

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
 {
     'name': 'Sale Order Lot Selection',
     'version': '10.0.1.0.1',
@@ -17,5 +15,3 @@
     ],
     'installable': True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sale_order_lot_selection/model/stock_move.py
+++ sale_order_lot_selection/model/stock_move.py
@@ -3,7 +3,7 @@
 # @author Valentin CHEMIERE <valentin.chemiere@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockMove(models.Model):
