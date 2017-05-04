376514f917f099182220fb6f1a86b509ebc225cb
Roberto Onnis <roberto.onnis@innoviu.com>
2017-05-04 18:25:20 +0200

    [MIG] sale_order_line_variant_description: Migrated to 10.0

--- sale_order_line_variant_description/__manifest__.py
+++ sale_order_line_variant_description/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Sale order line variant description",
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Sales Management',
     'author': "Agile Business Group, Odoo Community Association (OCA)",
     'website': 'http://www.agilebg.com',
--- sale_order_line_variant_description/models/sale_order_line.py
+++ sale_order_line_variant_description/models/sale_order_line.py
@@ -2,31 +2,21 @@
 # © 2015 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.multi
-    def product_id_change(
-            self, pricelist, product, qty=0, uom=False, qty_uos=0,
-            uos=False, name='', partner_id=False, lang=False,
-            update_tax=True, date_order=False, packaging=False,
-            fiscal_position=False, flag=False
-    ):
-        res = super(SaleOrderLine, self).product_id_change(
-            pricelist=pricelist, product=product, qty=qty, uom=uom,
-            qty_uos=qty_uos, uos=uos, name=name,
-            partner_id=partner_id, lang=lang, update_tax=update_tax,
-            date_order=date_order, packaging=packaging,
-            fiscal_position=fiscal_position, flag=flag)
-        if product:
-            product_obj = self.env['product.product']
-            lang = self.env['res.partner'].browse(partner_id).lang
-            product = product_obj.with_context(lang=lang).browse(product)
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        if self.product_id:
+            product = self.product_id.with_context(
+                lang=self.order_id.partner_id.lang,
+                partner=self.order_id.partner_id.id
+            )
             if product.variant_description_sale:
-                if 'value' not in res:
-                    res['value'] = {}
-                res['value']['name'] = product.variant_description_sale
+                self.update({'name': product.variant_description_sale})
         return res
