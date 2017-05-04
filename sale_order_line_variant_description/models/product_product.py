8b37e306f93381f155ecb2b32a11821891c5a7e2
Roberto Onnis <roberto.onnis@innoviu.com>
2017-05-04 18:25:20 +0200

    [MIG] sale_order_line_variant_description: Migrated to 10.0

--- sale_order_line_variant_description/models/product_product.py
+++ sale_order_line_variant_description/models/product_product.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class ProductProduct(models.Model):
