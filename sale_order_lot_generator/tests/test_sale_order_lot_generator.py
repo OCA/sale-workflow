a6a5e11c15fc7a574cbd5adc7892b35b096321c0
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-05-04 17:42:37 +0200

    [FIX] manage lot generation for order lines created after confirmation

--- sale_order_lot_generator/tests/test_sale_order_lot_generator.py
+++ sale_order_lot_generator/tests/test_sale_order_lot_generator.py
@@ -10,7 +10,11 @@ class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
     def setUp(self):
         super(TestSaleOrderLotGenerator, self).setUp()
         self.product_5b = self.env.ref('product.product_product_5b')
+        self.product_6 = self.env.ref('product.product_product_6')
         self.product_5b.write({
+            'tracking': 'lot', 'type': 'product',
+            'auto_generate_prodlot': True})
+        self.product_6.write({
             'tracking': 'lot', 'type': 'product',
             'auto_generate_prodlot': True})
 
@@ -30,7 +34,20 @@ class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
         self.order1.action_confirm()
         lot_number = "%s-%03d" % (self.order1.name, 1)
         self.assertEqual(self.sol1.lot_id.name, lot_number)
+        # add second line after order confirm
+        self.sol2 = self.env['sale.order.line'].create({
+            'name': 'sol1',
+            'order_id': self.order1.id,
+            'product_id': self.product_6.id,
+            'product_uom_qty': 1,
+        })
+        lot_number = "%s-%03d" % (self.order1.name, 2)
+        self.assertEqual(self.sol2.lot_id.name, lot_number)
         picking = self.order1.picking_ids
         for move in picking.move_lines:
             if move.product_id.id == self.product_5b.id:
-                self.assertEqual(move.restrict_lot_id, self.sol1.lot_id)
+                self.assertEqual(
+                    move.restrict_lot_id, self.sol1.lot_id)
+            if move.product_id.id == self.product_6.id:
+                self.assertEqual(
+                    move.restrict_lot_id, self.sol2.lot_id)
