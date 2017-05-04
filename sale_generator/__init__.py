ff15ad13e6f3257d6b0e6c2fe9d075624ee63cc4
MonsieurB <contact@ebii.fr>
2017-05-04 15:05:15 +0200

    git mv files to mig 10
    git mv files to mig 10
    
     mig v10 with git mv
    
    next view
    
    add view for partner wizard
    
    line size
    
    WIP for partner wizard
    
    partner wizard rocks!
    
    lint!
    
    add group to security/ir.rule.csv for non global rules
    
    optimise with new api
    
    line lenth
    
    travis test alert on default=vals

--- sale_generator/__init__.py
+++ sale_generator/__init__.py
@@ -1,12 +1,2 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#  licence AGPL version 3 or later
-#  see license in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
-#  Copyright (C) 2015 Akretion (http://www.akretion.com).
-#  @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-#
-##############################################################################
 
-from . import sale_generator
-from . import partner
+from . import models
