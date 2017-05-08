a56d37b24e20ba09c692f620215467981c840be1
Dave Lasley <dave@laslabs.com>
2017-05-08 16:21:25 -0700

    Use related fields for domains & char_domain where applicable

--- sale_automatic_workflow/models/__init__.py
+++ sale_automatic_workflow/models/__init__.py
@@ -6,7 +6,7 @@
 
 from . import automatic_workflow_job
 from . import account_invoice
-from . import sale
+from . import sale_order
 from . import stock_move
 from . import stock_picking
 from . import sale_workflow_process
