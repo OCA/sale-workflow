6eb512f44f81e594ed46178634a104e973ab256b
Roberto Onnis <roberto.onnis@innoviu.com>
2017-05-08 15:37:29 +0200

    Changes copyright header in __init__.py file

--- sale_order_line_variant_description/models/__init__.py
+++ sale_order_line_variant_description/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Alex Comba - Agile Business Group
+# Copyright 2015-2017 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import product_product
