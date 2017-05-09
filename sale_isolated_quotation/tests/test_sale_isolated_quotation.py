ede108188db320d3827ef436ea0a132cb0d77d10
Kitti U <kittiu@gmail.com>
2017-05-09 23:47:12 +0700

    Code Review,
    * Add README.rst

--- sale_isolated_quotation/tests/test_sale_isolated_quotation.py
+++ sale_isolated_quotation/tests/test_sale_isolated_quotation.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2012 Ecosoft (<http://www.ecosoft.co.th>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# © 2017 Ecosoft (ecosoft.co.th).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp.tests.common import TransactionCase
 
 
@@ -26,13 +10,13 @@ class TestSaleIsolatedQuotation(TransactionCase):
         """
         - When quotation is converted to order
           - Status chagned to 'done'
-          - New sale.order of order_type = 'sale_order' created
+          - New sale.order of is_order = True created
         - Quotation can refer to Order and Order can refer to Quotation
         """
         self.quotation.action_convert_to_order()
         self.assertEqual(self.quotation.state, 'done')
         self.sale_order = self.quotation.order_id
-        self.assertEqual(self.sale_order.order_type, 'sale_order')
+        self.assertTrue(self.sale_order.is_order)
         self.assertEqual(self.sale_order.state, 'draft')
         self.assertEqual(self.sale_order.partner_id, self.partner)
         self.assertEqual(self.sale_order.quote_id, self.quotation)
@@ -41,6 +25,6 @@ class TestSaleIsolatedQuotation(TransactionCase):
         super(TestSaleIsolatedQuotation, self).setUp()
         self.partner = self.env.ref('base.res_partner_2')
         vals = {'partner_id': self.partner.id,
-                'order_type': 'quotation',
+                'is_order': False,
                 }
         self.quotation = self.env['sale.order'].create(vals)
