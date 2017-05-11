ced3c7606b1be1a1141f8acc6fa34d602ab6d8ed
Kitti U <kittiu@gmail.com>
2017-05-11 09:52:41 +0700

    Add uninstall_hook to remove the unused context and domain

--- sale_isolated_quotation/hooks.py
+++ sale_isolated_quotation/hooks.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 # © 2017 Ecosoft (ecosoft.co.th).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import ast
+from openerp import api, SUPERUSER_ID
 
 
 def post_init_hook(cr, registry):
-    # Set is_order for old records
+    """ Set value for is_order on old records """
     cr.execute("""
         update sale_order
         set is_order = true
         where state not in ('draft', 'cancel')
     """)
+
+
+def uninstall_hook(cr, registry):
+    """ Restore sale.order action, remove context value """
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        for action_id in ['sale.action_quotations', 'sale.action_orders']:
+            action = env.ref(action_id)
+            ctx = ast.literal_eval(action.context)
+            print ctx
+            del ctx['is_order']
+            dom = ast.literal_eval(action.domain)
+            print dom
+            dom = [x for x in dom if x[0] != 'is_order']
+            action.write({'context': ctx, 'domain': dom})
