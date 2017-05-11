3edd9f957c641b80151ce2a1651b7db4240051c2
Kitti U <kittiu@gmail.com>
2017-05-11 17:21:19 +0700

    Code review

--- sale_isolated_quotation/tests/test_sale_isolated_quotation.py
+++ sale_isolated_quotation/tests/test_sale_isolated_quotation.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2017 Ecosoft (ecosoft.co.th).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestSaleIsolatedQuotation(TransactionCase):
