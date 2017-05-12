475d7593d26b7786e4334db6e117e86f14e46f2a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-05-12 17:59:33 +0200

    [MIG] sale_automatic_workflow_payment_mode: Upgrade to v10

--- sale_automatic_workflow_payment_mode/models/account_payment_mode.py
+++ sale_automatic_workflow_payment_mode/models/account_payment_mode.py
@@ -2,7 +2,7 @@
 # © 2016 Camptocamp SA, Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountPaymentMode(models.Model):
--- sale_automatic_workflow_payment_mode/models/sale_order.py
+++ sale_automatic_workflow_payment_mode/models/sale_order.py
@@ -2,7 +2,7 @@
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import api, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
