77c12b4f685ff202290b32bb8b2ddc74b4371cdb
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-05-12 15:13:12 +0200

    [10.0][MIG] port sale_product_multi_add to 10.0

--- sale_product_multi_add/__manifest__.py
+++ sale_product_multi_add/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Cédric Pigeon, ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 Cédric Pigeon, ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Sale Product Multi Add",
@@ -8,12 +8,13 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Sale Management',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'sale',
     ],
-    'data': ['wizard/sale_import_products_view.xml',
-             'views/sale_view.xml',
-             ]
+    'data': [
+        'wizards/sale_import_products_view.xml',
+        'views/sale_view.xml',
+    ],
 }
--- sale_product_multi_add/tests/test_sale.py
+++ sale_product_multi_add/tests/test_sale.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 # © 2016  Cédric Pigeon, Acsone SA/NV (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import openerp.tests.common as common
+import odoo.tests.common as common
 
 
 class TestSale(common.TransactionCase):
 
     def setUp(self):
-        common.TransactionCase.setUp(self)
+        super(TestSale, self).setUp()
 
-        self.product_35 = self.env.ref("product.product_product_35")
-        self.product_36 = self.env.ref("product.product_product_36")
+        self.product_9 = self.env.ref("product.product_product_9")
+        self.product_11 = self.env.ref("product.product_product_11")
 
     def test_import_product(self):
         """ Create SO
@@ -19,14 +19,13 @@ class TestSale(common.TransactionCase):
         """
 
         so = self.env["sale.order"].create(
-            {"partner_id": self.env.ref("base.res_partner_2").id,
-             })
+            {"partner_id": self.env.ref("base.res_partner_2").id})
 
         wiz_obj = self.env['sale.import.products']
         wizard = wiz_obj.with_context(active_id=so.id,
                                       active_model='sale.order')
 
-        products = [(6, 0, [self.product_35.id, self.product_36.id])]
+        products = [(6, 0, [self.product_9.id, self.product_11.id])]
 
         wizard_id = wizard.create({'products': products})
         wizard_id.create_items()
@@ -37,7 +36,7 @@ class TestSale(common.TransactionCase):
         self.assertEqual(len(so.order_line), 2)
 
         for line in so.order_line:
-            if line.product_id.id == self.product_35.id:
+            if line.product_id.id == self.product_9.id:
                 self.assertEqual(line.product_uom_qty, 4)
             else:
                 self.assertEqual(line.product_uom_qty, 6)
