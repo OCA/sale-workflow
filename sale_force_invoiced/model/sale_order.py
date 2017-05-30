c4bc03055bd5531327d94d022ad289335d15a8c4
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-05-30 10:25:26 +0200

    [9.0][ADD] module 'sale_force_invoiced' (#439)
    * [ADD] module 'sale_force_invoiced' for 9.0
    
    * [FIX] Travis

---
+++ sale_force_invoiced/model/sale_order.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    force_invoiced = fields.Boolean(string='Force invoiced',
+                                    help='When you set this field, the sales '
+                                         'order will be considered as fully '
+                                         'invoiced, even when there may be '
+                                         'ordered or delivered quantities '
+                                         'pending to invoice.',
+                                    readonly=True,
+                                    states={'done': [('readonly', False)]},
+                                    default=False)
+
+    @api.depends('force_invoiced')
+    def _get_invoiced(self):
+        super(SaleOrder, self)._get_invoiced()
+        for order in self:
+            if order.force_invoiced and order.invoice_status == 'to invoice':
+                order.invoice_status = 'invoiced'
