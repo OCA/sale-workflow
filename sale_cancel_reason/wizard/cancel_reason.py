f5875454d83fd4cf2974a0e7ad06ddf9d302f8e9
Kitti U <kittiu@gmail.com>
2017-05-30 17:13:19 +0700

    [MIG] sale_cancel_reason Migration to 10.0 (#477)
    * [MIG] sale_cancel_reason Migration to 10.0
    
    * Follow coding standard
    
    * Convert test to unittest2

--- sale_cancel_reason/wizard/cancel_reason.py
+++ sale_cancel_reason/wizard/cancel_reason.py
@@ -1,27 +1,7 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Author: Guewen Baconnier
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-
-from openerp import models, fields, api
-
-QUOTATION_STATES = ['draft', 'sent']
+# © 2013 Guewen Baconnier, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, api
 
 
 class SaleOrderCancel(models.TransientModel):
@@ -35,8 +15,9 @@ class SaleOrderCancel(models.TransientModel):
         string='Reason',
         required=True)
 
-    @api.one
+    @api.multi
     def confirm_cancel(self):
+        self.ensure_one()
         act_close = {'type': 'ir.actions.act_window_close'}
         sale_ids = self._context.get('active_ids')
         if sale_ids is None:
@@ -44,10 +25,5 @@ class SaleOrderCancel(models.TransientModel):
         assert len(sale_ids) == 1, "Only 1 sale ID expected"
         sale = self.env['sale.order'].browse(sale_ids)
         sale.cancel_reason_id = self.reason_id.id
-        # in the official addons, they call the signal on quotations
-        # but directly call action_cancel on sales orders
-        if sale.state in QUOTATION_STATES:
-            sale.signal_workflow('cancel')
-        else:
-            sale.action_cancel()
+        sale.action_cancel()
         return act_close
