0217046f67543525c6af669fa32142feab6c4215
Voodoo <voodoo@fake.com>
2017-06-01 13:48:21 +0000

    add group to security/ir.rule.csv for non global rules

--- sale_generator/models/__init__.py
+++ sale_generator/models/__init__.py
@@ -1,8 +1,3 @@
-# coding: utf-8
-#  @author Sébastien BEAU <sebastien.beau@akretion.com>
-#  @author Abdessamad HILALI <abdessamad.hilali@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 
 from . import sale_generator
 from . import sale_order
