ad9c8900252e18464ea0d30c8dde25e9d18b2782
mreficent <miquel.raich@eficent.com>
2017-06-22 13:47:18 +0200

    [MIG] sale_procurement_group_by_line: Migration to 10.0

--- sale_procurement_group_by_line/tests/__init__.py
+++ sale_procurement_group_by_line/tests/__init__.py
@@ -2,6 +2,6 @@
 # Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_sale_procurement_group_by_line
