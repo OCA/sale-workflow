58c6c3293830e323435c587a9653284bd468a7b6
mreficent <miquel.raich@eficent.com>
2017-06-22 13:47:18 +0200

    [MIG] sale_procurement_group_by_line: Migration to 10.0

--- sale_stock_reference_by_line/__manifest__.py
+++ sale_stock_reference_by_line/__manifest__.py
@@ -2,22 +2,20 @@
 # Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Procurement Group by Line',
     'summary': 'Base module for multiple procurement group by Sale order',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     "author": "Camptocamp,"
               "Eficent,"
               "Serpent,"
               "Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'license': 'AGPL-3',
-    'complexity': 'expert',
     'website': "https://github.com/OCA/sale-workflow",
     'depends': ['sale_stock',
                 ],
-    'auto_install': False,
     'installable': True,
 }
