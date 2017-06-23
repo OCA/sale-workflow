a800d1c686010447371722b8e028d8a56981e12b
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-23 15:58:35 +0530

    [MIG] sale_order_line_sequence v9

---
+++ sale_order_line_sequence/model/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import sale_order
