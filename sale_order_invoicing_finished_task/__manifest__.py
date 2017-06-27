1856f118c907cb96f29d14ec1214b1a964beb17c
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-06-27 14:46:44 +0200

    [10.0][ADD] sale_order_invoicing_finished_task: New module

---
+++ sale_order_invoicing_finished_task/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Sale Order Invoicing Finished Task",
+    "summary": "Allow invoice order lines if his task has been finished",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [
+        "views/product_view.xml",
+    ],
+}
