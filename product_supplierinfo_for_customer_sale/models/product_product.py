dbc28aaab8dd3dff2f693d7b0492cc39c1c7869e
Yennifer Santiago <yennifer@vauxoo.com>
2017-06-30 04:19:33 +0000

    product_supplierinfo_for_customer_sale: Return all result in name search, products found by default code and customer code
    [REF] product_supplierinfo_for_customer_sale: Add test to find more than one product in name_search
    
    [REF] product_supplierinfo_for_customer_sale: In test find one product from customer code and other one from product default code.
    
    [REF] product_supplierinfo_for_customer_sale: Changes in name_search to work exactly with number of limit defined.
    
    [REF] product_supplierinfo_for_customer_sale: Changes in name_search

--- product_supplierinfo_for_customer_sale/models/product_product.py
+++ product_supplierinfo_for_customer_sale/models/product_product.py
@@ -13,14 +13,22 @@ class ProductProduct(models.Model):
     def name_search(self, name='', args=None, operator='ilike', limit=80):
         res = super(ProductProduct, self).name_search(
             name, args=args, operator=operator, limit=limit)
-        if res or not self._context.get('partner_id'):
+        if (not name or not self._context.get('partner_id') or
+                len(res) >= limit):
             return res
         partner_id = self._context['partner_id']
+        limit -= len(res)
         supplierinfo = self.env['product.supplierinfo'].search(
             [('name', '=', partner_id), '|', ('product_code', operator, name),
-             ('product_name', operator, name)])
+             ('product_name', operator, name)], limit=limit)
+        if not supplierinfo:
+            return res
+        res_template_ids = self.browse(
+            [product_id for product_id, _name in res]).mapped(
+                'product_tmpl_id')
+        product_tmpl_ids = (supplierinfo.mapped('product_tmpl_id') -
+                            res_template_ids)
         product = self.search(
-            [('product_tmpl_id', '=', supplierinfo.product_tmpl_id.id)],
-            limit=limit)
-        res = product.name_get()
+            [('product_tmpl_id', 'in', product_tmpl_ids.ids)], limit=limit)
+        res.extend(product.name_get())
         return res
