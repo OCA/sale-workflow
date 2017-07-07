a333be8f32f53c40627b1ff07707299548631fc3
Graeme Gellatly <gdgellatly@gmail.com>
2017-07-07 16:07:43 +1200

    [IMP] Update sale_isolated_quotation
    - allow to print, email and publish on website_portal
    - attempt to at least make website_portal_sale make sense
    - override action_confirm behaviour for website_quote
    - update backend header to v10 classes as used in view_order_form.

--- sale_isolated_quotation/tests/test_sale_isolated_quotation.py
+++ sale_isolated_quotation/tests/test_sale_isolated_quotation.py
@@ -6,14 +6,14 @@ from odoo.tests.common import TransactionCase
 
 class TestSaleIsolatedQuotation(TransactionCase):
 
-    def test_quotation_convert_to_order(self):
-        """
-        - When quotation is converted to order
-          - Status chagned to 'done'
-          - New sale.order of is_order = True created
-        - Quotation can refer to Order and Order can refer to Quotation
-        """
-        self.quotation.action_convert_to_order()
+    def create_orders(self):
+        vals = {'partner_id': self.partner.id,
+                'is_order': False}
+        self.quotation = self.env['sale.order'].create(vals)
+        vals.update({'is_order': True})
+        self.sale_order = self.env['sale.order'].create(vals)
+
+    def quotation_assertions(self):
         self.assertEqual(self.quotation.state, 'done')
         self.sale_order = self.quotation.order_id
         self.assertTrue(self.sale_order.is_order)
@@ -21,10 +21,78 @@ class TestSaleIsolatedQuotation(TransactionCase):
         self.assertEqual(self.sale_order.partner_id, self.partner)
         self.assertEqual(self.sale_order.quote_id, self.quotation)
 
+    def test_quotation_convert_to_order(self):
+        """
+        - When quotation is converted to order
+          - Status chagned to 'done'
+          - New sale.order of is_order = True created
+        - Quotation can refer to Order and Order can refer to Quotation
+        """
+        self.create_orders()
+        self.quotation.action_convert_to_order()
+        self.quotation_assertions()
+
+    def test_quotation_confirm_order(self):
+        """
+        When confirm order is called on a quotation we need to ensure
+        that the quotation is converted to an order.
+        :return:
+        """
+        self.create_orders()
+        self.quotation.action_confirm()
+        self.quotation_assertions()
+
+    def test_sale_confirm_order(self):
+        """
+        When a sales order is converted to an order we need to ensure that
+        behaviour of sale module is unchanged
+        """
+        self.create_orders()
+        self.quotation.is_order = True
+        self.quotation.action_confirm()
+        self.assertFalse(self.quotation.quote_id)
+        self.assertFalse(self.quotation.order_id)
+        self.assertFalse(self.quotation.state in ('draft', 'sent', 'cancel'))
+
+    def test_quotation_search(self):
+        """
+        Because the amendment to search is highly dependent on hardcoded
+        upstream function we test fully for any change to its behaviour.
+        """
+        # At start of search both orders are in draft state.
+        self.create_orders()
+        sale_obj = self.env['sale.order']
+        search = sale_obj.search
+        web_search = self.env['sale.order'].with_context(website_id=1).search
+        orders = self.quotation + self.sale_order
+        self.assertEqual(len(search([('id', '=', self.quotation.id)])), 1)
+        # A typical backend search
+        self.assertEqual(
+            len(search([('id', 'in', orders.ids),
+                       ('state', 'in', ['sent', 'cancel'])])), 0)
+        self.assertEqual(
+            len(web_search([('id', 'in', orders.ids),
+                           ('state', 'in', ['sent', 'cancel'])])), 0)
+        # Test sent state - 2 should appear in backend, only 1 in website
+        orders.write({'state': 'sent'})
+        self.assertEqual(
+            len(search([('id', 'in', orders.ids),
+                       ('state', 'in', ['sent', 'cancel'])])), 2)
+        web_quotes = web_search([('id', 'in', orders.ids),
+                                ('state', 'in', ['sent', 'cancel'])])
+        self.assertEqual(len(web_quotes), 1)
+        self.assertTrue(web_quotes.id == self.quotation.id)
+
+        # Test sale, done state
+        orders.action_confirm()
+        self.assertEqual(
+            len(search([('id', 'in', orders.ids),
+                       ('state', 'in', ['sale', 'done'])])), 2)
+        web_orders = web_search([('id', 'in', orders.ids),
+                                ('state', 'in', ['sale', 'done'])])
+        self.assertEqual(len(web_orders), 1)
+        self.assertTrue(web_orders.id == self.sale_order.id)
+
     def setUp(self):
         super(TestSaleIsolatedQuotation, self).setUp()
         self.partner = self.env.ref('base.res_partner_2')
-        vals = {'partner_id': self.partner.id,
-                'is_order': False,
-                }
-        self.quotation = self.env['sale.order'].create(vals)
