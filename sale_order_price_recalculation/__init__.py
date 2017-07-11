7a6328852451004dd14052ece1d723fbac4d797d
David Vidal <chienandalu@gmail.com>
2017-07-11 13:28:18 +0200

    [MIG] sale_order_price_recalculation: Migration to 10.0

--- sale_order_price_recalculation/__init__.py
+++ sale_order_price_recalculation/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
-# Copyright 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# Copyright 2015 Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
-# Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
