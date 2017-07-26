87525f16e85fe044c60ca2aeb9841e7a3599afef
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-26 13:51:00 +0530

    [MIG] sale_order_line_date v10

--- sale_order_line_date/__manifest__.py
+++ sale_order_line_date/__manifest__.py
@@ -3,11 +3,12 @@
 # © 2016 AvanzOSC
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Order Line Date",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
