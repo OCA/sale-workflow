df2f63a402ae7b66e0fd72a413848f2940bcf5e9
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-26 13:51:00 +0530

    [MIG] sale_order_line_date v10

--- sale_order_line_date/models/__init__.py
+++ sale_order_line_date/models/__init__.py
@@ -3,6 +3,8 @@
 # © 2016 AvanzOSC
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import sale_order
+from . import sale_order_line
