547440e5a4003addf9607af1b2ae2a9c9c2365ba
mreficent <miquel.raich@eficent.com>
2017-08-18 15:55:36 +0200

    [10.0][MIG] sale_sourced_by_line

--- sale_sourced_by_line/__manifest__.py
+++ sale_sourced_by_line/__manifest__.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016-17 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Sale Sourced by Line',
     'summary': 'Multiple warehouse source locations for Sale order',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     "author": "Camptocamp,"
               "Eficent,"
               "SerpentCS,"
               "Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'license': 'AGPL-3',
-    'website': "http://www.camptocamp.com",
+    'website': "https://github.com/OCA/sale-workflow",
     'depends': ['sale_stock',
                 'sale_procurement_group_by_line',
                 ],
     'data': [
         'view/sale_view.xml'
     ],
-    'auto_install': False,
     'installable': True,
 }
