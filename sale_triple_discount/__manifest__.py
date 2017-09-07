971cdf01fc0ba93973d9be9d2f3df74f4f1fe187
David Vidal <chienandalu@gmail.com>
2017-09-07 11:44:55 +0200

    [MIG] sale_triple_discount: Migration to 10.0
    OCA Transbot updated translations from Transifex
    
    [UPD] Update sale_triple_discount.pot

--- sale_triple_discount/__manifest__.py
+++ sale_triple_discount/__manifest__.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # Copyright 2017 Alex Comba - Agile Business Group
+# Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Sale Triple Discount',
     'version': '10.0.1.0.0',
     'category': 'Sales Management',
-    'author': 'ADHOC SA, Agile Business Group, '
+    'author': 'ADHOC SA, '
+              'Agile Business Group, '
+              'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'www.adhoc.com.ar',
+    'website': 'https://odoo-community.org',
     'license': 'AGPL-3',
-    'summary': 'Manage triple discount on sale order lines '
-               'and on invoice lines',
+    'summary': 'Manage triple discount on sale order lines',
     'depends': [
         'sale',
-        'account'
+        'account_invoice_triple_discount',
     ],
     'data': [
         'views/sale_order_view.xml',
-        'views/account_invoice_view.xml',
-        'views/res_partner_view.xml',
     ],
     'installable': True,
 }
