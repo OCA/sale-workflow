71665918ff9f3b6b8abde39da34e7fbceff7139c
David Vidal <chienandalu@gmail.com>
2017-09-07 11:44:55 +0200

    [MIG] sale_triple_discount: Migration to 10.0

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -1,94 +1,65 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # Copyright 2017 Alex Comba - Agile Business Group
+# Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-import openerp.addons.decimal_precision as dp
+import odoo.addons.decimal_precision as dp
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.model
-    def _default_discount1(self):
-        default_discount1 = 0.0
-        partner_id = self.env.context.get('partner_id', False)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            default_discount1 = partner.default_discount1
-        return default_discount1
+    @api.depends('discount2', 'discount3')
+    def _compute_amount(self):
+        for line in self:
+            prev_price_unit = line.price_unit
+            prev_discount = line.discount
+            price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            price_unit *= (1 - (line.discount2 or 0.0) / 100.0)
+            price_unit *= (1 - (line.discount3 or 0.0) / 100.0)
+            line.update({
+                'price_unit': price_unit,
+                'discount': 0.0,
+            })
+            super(SaleOrderLine, line)._compute_amount()
+            line.update({
+                'price_unit': prev_price_unit,
+                'discount': prev_discount,
+            })
 
-    @api.model
-    def _default_discount2(self):
-        default_discount2 = 0.0
-        partner_id = self.env.context.get('partner_id', False)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            default_discount2 = partner.default_discount2
-        return default_discount2
-
-    @api.model
-    def _default_discount3(self):
-        default_discount3 = 0.0
-        partner_id = self.env.context.get('partner_id', False)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            default_discount3 = partner.default_discount3
-        return default_discount3
-
-    discount1 = fields.Float(
-        'Discount 1 (%)',
-        digits=dp.get_precision('Discount'),
-        readonly=True,
-        default=_default_discount1,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
-    )
     discount2 = fields.Float(
-        'Discount 2 (%)',
+        'Disc. 2 (%)',
         digits=dp.get_precision('Discount'),
-        readonly=True,
-        default=_default_discount2,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
+        default=0.0,
     )
     discount3 = fields.Float(
-        'Discount 3 (%)',
+        'Disc. 3 (%)',
         digits=dp.get_precision('Discount'),
-        readonly=True,
-        default=_default_discount3,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}
+        default=0.0,
     )
-    discount = fields.Float(compute='get_discount', store=True)
 
-    @api.multi
-    @api.depends('discount1', 'discount2', 'discount3')
-    def get_discount(self):
-        for line in self:
-            discount_factor = 1.0
-            for discount in [line.discount1, line.discount2, line.discount3]:
-                discount_factor = (
-                    discount_factor * ((100.0 - discount) / 100.0))
-            line.discount = 100.0 - (discount_factor * 100.0)
+    _sql_constraints = [
+        ('discount2_limit', 'CHECK (discount2 <= 100.0)',
+         'Discount 2 must be lower than 100%.'),
+        ('discount3_limit', 'CHECK (discount3 <= 100.0)',
+         'Discount 3 must be lower than 100%.'),
+    ]
 
-    @api.onchange(
-        'product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
-    def _onchange_discount(self):
-        super(SaleOrderLine, self)._onchange_discount()
-        if self.discount != 0:
-            # to keep discount value calculated by pricelist
-            self.discount1 = self.discount
-            self.discount2 = 0
-            self.discount3 = 0
-        else:
-            # to recalculate discount value based on three discounts
-            # when these latter are filled by their default
-            self.get_discount()
-
-    @api.multi
     def _prepare_invoice_line(self, qty):
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
         res.update({
-            'discount1': self.discount1,
             'discount2': self.discount2,
-            'discount3': self.discount3})
+            'discount3': self.discount3,
+        })
         return res
+
+    @api.depends('discount2', 'discount3')
+    def _get_price_reduce(self):
+        for line in self:
+            super(SaleOrderLine, line)._get_price_reduce()
+            if line.discount2:
+                line.price_reduce *= (1 - line.discount2 / 100)
+            if line.discount3:
+                line.price_reduce *= (1 - line.discount3 / 100)
