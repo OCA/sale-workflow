12410c93c67ae62173d7a05e910ccf4869ad9cf6
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-09-13 16:46:26 +0200

    [ADD] sale_automatic_workflow_payment_ref: Propagates your payment reference from your sale order to your payment

--- sale_automatic_workflow_payment_mode/models/automatic_workflow_job.py
+++ sale_automatic_workflow_payment_mode/models/automatic_workflow_job.py
@@ -25,15 +25,32 @@ class AutomaticWorkflowJob(models.Model):
         return res
 
     @api.model
+    def _prepare_invoice_account_payment_vals(self, invoice):
+        partner_type = (
+            invoice.type in ('out_invoice', 'out_refund') and 'customer' or
+            'supplier'
+        )
+        payment_mode = invoice.payment_mode_id
+        return {
+            'invoice_ids': [(6, 0, invoice.ids)],
+            'amount': invoice.residual,
+            'payment_date': fields.Date.context_today(self),
+            'communication': invoice.reference or invoice.number,
+            'partner_id': invoice.partner_id.id,
+            'partner_type': partner_type,
+            'payment_type': payment_mode.payment_type,
+            'payment_method_id': payment_mode.payment_method_id.id,
+            'journal_id': payment_mode.fixed_journal_id.id,
+            'currency_id': invoice.currency_id.id,
+        }
+
+    @api.model
     def _register_payments(self, payment_filter):
         invoice_obj = self.env['account.invoice']
         invoices = invoice_obj.search(payment_filter)
         _logger.debug('Invoices to Register Payment: %s', invoices.ids)
         for invoice in invoices:
-            partner_type = invoice.type in ('out_invoice', 'out_refund') and \
-                'customer' or 'supplier'
             payment_mode = invoice.payment_mode_id
-
             if not payment_mode.fixed_journal_id:
                 _logger.debug('Unable to Register Payment for invoice %s: '
                               'Payment mode %s must have fixed journal',
@@ -41,17 +58,7 @@ class AutomaticWorkflowJob(models.Model):
                 continue
 
             with savepoint(self.env.cr):
-                payment = self.env['account.payment'].create({
-                    'invoice_ids': [(6, 0, invoice.ids)],
-                    'amount': invoice.residual,
-                    'payment_date': fields.Date.context_today(self),
-                    'communication': invoice.reference or invoice.number,
-                    'partner_id': invoice.partner_id.id,
-                    'partner_type': partner_type,
-                    'payment_type': payment_mode.payment_type,
-                    'payment_method_id': payment_mode.payment_method_id.id,
-                    'journal_id': payment_mode.fixed_journal_id.id,
-                    'currency_id': invoice.currency_id.id,
-                })
+                vals = self._prepare_invoice_account_payment_vals(invoice)
+                payment = self.env['account.payment'].create(vals)
                 payment.post()
         return
---
+++ sale_automatic_workflow_payment_ref/tests/test_automatic_workflow_job.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# Copyright 2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.sale_automatic_workflow.tests.test_automatic_workflow_base \
+    import TestAutomaticWorkflowBase
+
+
+class TestAutomaticWorkflowJob(TestAutomaticWorkflowBase):
+
+    def create_sale_order(self, workflow, override=None):
+        new_order = super(TestAutomaticWorkflowJob, self).\
+            create_sale_order(workflow, override)
+        new_order.transaction_id = 'transaction_id'
+        self.pay_method = self.env['account.payment.method'].create({
+            'name': 'default inbound',
+            'code': 'definb',
+            'payment_type': 'inbound'})
+        self.acc_journ = self.env['account.journal'].create({
+            'name': 'Bank US',
+            'type': 'bank',
+            'code': 'BNK68'})
+        self.pay_mode = self.env['account.payment.mode'].create({
+            'name': "Julius Caesare payment",
+            'bank_account_link': 'fixed',
+            'fixed_journal_id': self.acc_journ.id,
+            'payment_method_id': self.pay_method.id,
+            'workflow_process_id': workflow.id})
+        new_order.payment_mode_id = self.pay_mode
+        new_order.payment_mode_id.workflow_process_id = \
+            new_order.workflow_process_id.id
+        return new_order
+
+    def create_full_automatic(self, override=None):
+        values = super(TestAutomaticWorkflowJob, self).\
+            create_full_automatic(override)
+        reg_pay_dict = {'register_payment': True}
+        values.update(reg_pay_dict)
+        return values
+
+    def test_full_automatic(self):
+        workflow = self.create_full_automatic()
+        sale = self.create_sale_order(workflow)
+        sale._onchange_workflow_process_id()
+        sale.onchange_payment_mode_set_workflow()
+        self.assertEqual(sale.state, 'draft')
+        self.assertEqual(sale.workflow_process_id, workflow)
+        self.progress()
+        self.assertEqual(sale.state, 'sale')
+        self.assertTrue(sale.picking_ids)
+        self.assertTrue(sale.invoice_ids)
+        invoice = sale.invoice_ids
+        self.assertEqual(invoice.state, 'paid')
+        self.assertEqual(invoice.payment_ids.transaction_id,
+                         sale.transaction_id)
