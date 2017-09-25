6e56616c1ca0f946dc396613b3025eb952fd3b05
Simone Rubino <simone.rubino@agilebg.com>
2017-09-25 18:05:29 +0200

    [MIG] sale_order_line_description: Migration to 10.0

--- sale_order_line_description/models/sale.py
+++ sale_order_line_description/models/sale.py
@@ -19,34 +19,25 @@
 #
 #
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.multi
-    def product_id_change(
-            self, pricelist, product, qty=0, uom=False, qty_uos=0,
-            uos=False, name='', partner_id=False, lang=False,
-            update_tax=True, date_order=False, packaging=False,
-            fiscal_position=False, flag=False
-    ):
-        res = super(SaleOrderLine, self).product_id_change(
-            pricelist=pricelist, product=product, qty=qty, uom=uom,
-            qty_uos=qty_uos, uos=uos, name=name,
-            partner_id=partner_id, lang=lang, update_tax=update_tax,
-            date_order=date_order, packaging=packaging,
-            fiscal_position=fiscal_position, flag=flag)
-        if product:
-            product_obj = self.env['product.product']
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
+        if self.product_id:
             if self.user_has_groups(
                     'sale_order_line_description.'
                     'group_use_product_description_per_so_line'):
-                lang = self.env['res.partner'].browse(partner_id).lang
-                product = product_obj.with_context(lang=lang).browse(product)
+                product = self.product_id.with_context(
+                    lang=self.order_id.partner_id.lang,
+                )
                 if product.description_sale:
-                    if 'value' not in res:
-                        res['value'] = {}
-                    res['value']['name'] = product.description_sale
+                    vals = {'name': product.description_sale}
+                    self.update(vals)
+
         return res
