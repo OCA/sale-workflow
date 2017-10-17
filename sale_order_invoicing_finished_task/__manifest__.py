feccc0975e5352b2f8b2f1113b2b0d04635dc203
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-10-17 04:11:08 +0200

    [10.0][ADD] sale_order_invoicing_finished_task: New module (#500)
    * [10.0][ADD] sale_order_invoicing_finished_task: New module
    
    * [10.0][IMP] sale_order_invoicing_finished_task: Toggle invoiceable flag in task.
    
    * [10.0][FIX] sale_order_invoicing_finished_task: Not invoiceable status
    
    * [10.0][IMP] sale_order_invoicing_finished_task: check invoice policy
    
    * Add files for README
    
    * Update README.rst
    
    * Change some format issues in README
    
    * Change license to AGPL and improve readme
    
    * Update __manifest__.py
    
    Add Camptocamp as author
    
    * [10.0][FIX] sale_order_invoicing_finished_task: Overwrite create and write methods because the stage_id field onchange not trigger in a form view with statusbar widget
    
    * [10.0][IMP] sale_order_invoicing_finished_task: Add tests

---
+++ sale_order_invoicing_finished_task/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Sale Order Invoicing Finished Task",
+    "summary": "Control invoice order lines if his task has been finished",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Tecnativa, "
+              "Camptocamp, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [
+        "views/product_view.xml",
+        "views/project_view.xml",
+    ],
+}
