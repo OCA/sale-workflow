1765c5fee1b482176e46f39d53fc6dd31f74eba7
rgarnau <rgarnau20@gmail.com>
2017-10-27 11:13:32 +0200

    [ADD] Module Sale Invoice Group Method

---
+++ sale_invoice_group_method/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import test_sale_invoice_group_method
