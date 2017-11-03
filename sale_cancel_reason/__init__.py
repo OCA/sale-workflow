bb0e870242bc8f9b7eadb88625e19fa1af7c4e52
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-11-03 16:11:50 +0530

    [ADD] Migrated sale_cancel_reason module in v11.

---
+++ sale_cancel_reason/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Guewen Baconnier, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import wizard
+from . import model
