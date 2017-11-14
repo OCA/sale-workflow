9d444a49990372b0b779da9fc0355da1678ebdf8
rgarnau <rgarnau20@gmail.com>
2017-11-14 10:22:51 +0100

    [MIG] sale_order_action_invoice_create_hook: Migration to 11.0

--- sale_order_action_invoice_create_hook/__manifest__.py
+++ sale_order_action_invoice_create_hook/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Sale Order Action Invoice Create Hook",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Sale Workflow",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": [
         'sale',
     ],
     "license": 'LGPL-3',
+    "post_load": "post_load_hook",
     "installable": True
 }
