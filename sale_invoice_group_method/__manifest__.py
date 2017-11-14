e7db872aa718ce578b703c2c29674a18cd2a70cb
rgarnau <rgarnau20@gmail.com>
2017-11-14 11:38:08 +0100

    [MIG] sale_invoice_group_method: Migration to 11.0

--- sale_invoice_group_method/__manifest__.py
+++ sale_invoice_group_method/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Sale Invoice Group Method",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Sale Workflow",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": [
