e845d5db319c43fdcefa6ea2b031910329eafda0
rgarnau <rgarnau20@gmail.com>
2017-11-14 11:38:08 +0100

    [MIG] sale_invoice_group_method: Migration to 11.0

--- sale_invoice_group_method/model/__init__.py
+++ sale_invoice_group_method/model/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import sale_invoice_group_method
 from . import sale_order
