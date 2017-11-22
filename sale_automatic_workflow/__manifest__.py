b3555d689cb7ddb425614d11328dcecf82bec8fa
jaredkipe <jared@jaredkipe.com>
2017-11-22 09:24:30 +0100

    [FIX] sale_automatic_workflow: multi-record aware on validation (#570)
    * FIX automatic validation of pickings if the domain returns more than one
    
    * Change picking validation to be transactionally similar to other jobs.

--- sale_automatic_workflow/__manifest__.py
+++ sale_automatic_workflow/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Sale Automatic Workflow',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'author': "Akretion,Camptocamp,Sodexis,Odoo Community Association (OCA)",
