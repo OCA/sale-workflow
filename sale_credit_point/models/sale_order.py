8e62f53144a8360a8d213901f491dec5375f8110
Simone Orsi <simahawk@gmail.com>
2017-12-07 13:45:47 +0100

    [ADD] sale_credit_point

---
+++ sale_credit_point/models/sale_order.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def credit_point_check(self):
+        """Verify order amount against credit point budget."""
+        self.ensure_one()
+        if not self.partner_id.credit_point_bypass_check():
+            if self.amount_total > self.partner_id.credit_point:
+                raise UserError(self.credit_point_check_failed_msg)
+
+    @property
+    def credit_point_check_failed_msg(self):
+        return _(
+            "Sale Order amount total (%s) "
+            "is higher than your available credit (%s)."
+        ) % (self.amount_total, self.partner_id.credit_point)
+
+    @property
+    def credit_point_decrease_msg(self):
+        return _('SO %s') % self.name
+
+    @api.multi
+    def action_confirm(self):
+        """Check credit before confirmation, update credit if check passed."""
+        for sale in self:
+            sale.credit_point_check()
+            sale.partner_id.credit_point_decrease(
+                sale.amount_total, comment=self.credit_point_decrease_msg)
+        return super(SaleOrder, self).action_confirm()
---
+++ sale_credit_point/tests/test_credit.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import SavepointCase
+from odoo import exceptions
+
+
+class TestCredit(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCredit, cls).setUpClass()
+        cls.partner = cls.env['res.partner'].create({
+            'name': 'John Credit',
+        })
+        cls.currency = cls.env.ref('sale_credit_point.res_currency_pt')
+
+    def test_default_currency(self):
+        self.assertEqual(self.partner.credit_point_currency_id, self.currency)
+
+    def test_default_credit(self):
+        self.assertEqual(self.partner.credit_point, 0)
+
+    def test_credit_replace(self):
+        self.partner.credit_point = 10
+        self.partner.credit_point_replace(100)
+        self.assertEqual(self.partner.credit_point, 100)
+
+    def test_credit_increase(self):
+        self.partner.credit_point = 10
+        self.partner.credit_point_increase(25)
+        self.assertEqual(self.partner.credit_point, 35)
+
+    def test_credit_decrease(self):
+        self.partner.credit_point = 10
+        self.partner.credit_point_decrease(5)
+        self.assertEqual(self.partner.credit_point, 5)
+
+    def _test_message(self, msg):
+        self.assertIn(msg, self.partner.message_ids[0].body)
+
+    def test_credit_replace_with_message(self):
+        self.partner.credit_point = 10
+        msg = 'Wrong amount dude!'
+        self.partner.credit_point_replace(100, comment=msg)
+        self.assertEqual(self.partner.credit_point, 100)
+        self._test_message(msg)
+
+    def test_credit_increase_with_message(self):
+        self.partner.credit_point = 10
+        msg = 'I have money dude!'
+        self.partner.credit_point_increase(25, comment=msg)
+        self.assertEqual(self.partner.credit_point, 35)
+        self._test_message(msg)
+
+    def test_credit_decrease_with_message(self):
+        self.partner.credit_point = 10
+        msg = 'I less money dude!'
+        self.partner.credit_point_decrease(5, comment=msg)
+        self.assertEqual(self.partner.credit_point, 5)
+        self._test_message(msg)
+
+    def test_credit_cannot_be_negative(self):
+        self.partner.credit_point = 10
+        with self.assertRaises(exceptions.ValidationError):
+            self.partner.credit_point_decrease(20)
+
+    def test_credit_negative_bypass_flag(self):
+        self.partner.credit_point = 10
+        self.partner.with_context(
+            skip_credit_check=True).credit_point_decrease(20)
+        self.assertEqual(self.partner.credit_point, -10)
+
+    def test_credit_negative_bypass_group(self):
+        self.env.user.groups_id |= \
+            self.env.ref('sale_credit_point.group_manage_credit_point')
+        self.partner.credit_point = 10
+        self.partner.credit_point_decrease(20)
+        self.assertEqual(self.partner.credit_point, -10)
