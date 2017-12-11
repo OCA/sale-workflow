12ad31446f9e305851d5c336415aabb1bcd29ef4
Graeme Gellatly <gdgellatly@gmail.com>
2017-12-11 22:10:29 +1300

    [PORT] Sale Automatic Workflow to v11.0

--- sale_automatic_workflow/models/stock_move.py
+++ sale_automatic_workflow/models/stock_move.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
-# © 2013 Camptocamp SA (author: Guewen Baconnier)
-# © 2016 Sodexis
+# Copyright 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
@@ -13,7 +12,7 @@ class StockMove(models.Model):
     @api.multi
     def _get_new_picking_values(self):
         values = super(StockMove, self)._get_new_picking_values()
-        if self.procurement_id.sale_line_id:
-            sale = self.procurement_id.sale_line_id.order_id
+        sale = self.group_id.sale_id
+        if sale:
             values['workflow_process_id'] = sale.workflow_process_id.id
         return values
