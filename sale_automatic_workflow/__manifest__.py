a1a764ecfb12ce270362d2f0fdf79721f4d7a5f9
Graeme Gellatly <gdgellatly@gmail.com>
2017-12-11 22:10:29 +1300

    [PORT] Sale Automatic Workflow to v11.0

--- sale_automatic_workflow/__manifest__.py
+++ sale_automatic_workflow/__manifest__.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-# © 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
-# © 2013 Camptocamp SA (author: Guewen Baconnier)
-# © 2016 Sodexis
+# Copyright 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Automatic Workflow',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'author': "Akretion,Camptocamp,Sodexis,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
-    'depends': ['sale_stock', 'sales_team',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'depends': ['sale_stock',
+                'sales_team',
                 ],
     'data': ['views/sale_view.xml',
              'views/sale_workflow_process_view.xml',
--- sale_automatic_workflow/models/account_invoice.py
+++ sale_automatic_workflow/models/account_invoice.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
-# © 2013 Camptocamp SA (author: Guewen Baconnier)
-# © 2016 Sodexis
+# Copyright 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
