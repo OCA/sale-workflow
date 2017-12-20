e1b6102d77c76cfcbf66712bea9a75c4b1d6dbcc
Duc Dao <ducdd@komit-consulting.com>
2017-12-21 01:33:31 +0700

    [MIG] sale_order_price_recalculation: Migration to 11.0

--- sale_order_price_recalculation/tests/test_sale_order_price_recalculation.py
+++ sale_order_price_recalculation/tests/test_sale_order_price_recalculation.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
+# Copyright 2018 Duc Dao Dong <duc.dd@komit-consulting.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo.tests import common
 
 
@@ -11,22 +10,21 @@ class TestSaleOrderPriceRecalculation(common.TransactionCase):
 
     def setUp(self):
         super(TestSaleOrderPriceRecalculation, self).setUp()
-        self.sale_order_model = self.env['sale.order']
-        self.sale_order_line_model = self.env['sale.order.line']
         self.partner = self.env['res.partner'].create({
             'name': 'Test partner',
         })
-        self.product = self.env.ref('product.product_product_4')
+        uom_id = self.env.ref('product.product_uom_kgm')
         self.product = self.env['product.product'].create({
             'name': 'Jacket - Color: Black - Size: XL',
+            'uom_id': uom_id.id,
+            'uom_po_id': uom_id.id,
         })
-        self.sale_order = self.sale_order_model.create({
+        self.sale_order = self.env['sale.order'].create({
             'partner_id': self.partner.id,
             'partner_invoice_id': self.partner.id,
             'partner_shipping_id': self.partner.id,
             'pricelist_id': self.env.ref('product.list0').id,
         })
-        self.product.uos_id = self.env.ref('product.product_uom_kgm')
         line_vals = {
             'product_id': self.product.id,
             'name': self.product.name,
@@ -35,11 +33,11 @@ class TestSaleOrderPriceRecalculation(common.TransactionCase):
             'price_unit': self.product.lst_price,
             'order_id': self.sale_order.id,
         }
-        self.sale_order_line = self.sale_order_line_model.create(line_vals)
+        self.sale_order_line = self.env['sale.order.line'].create(line_vals)
 
     def test_price_recalculation(self):
         # Check current price
-        self.sale_order_line.name = u"My product description"
+        self.sale_order_line.name = "My product description"
         self.assertEqual(
             self.sale_order_line.price_unit, self.product.lst_price)
         # Change price
@@ -52,7 +50,7 @@ class TestSaleOrderPriceRecalculation(common.TransactionCase):
         # Check if quantities have changed
         self.assertEqual(self.sale_order_line.product_uom_qty, 1.0)
         # Check the description still unchanged
-        self.assertEqual(self.sale_order_line.name, u"My product description")
+        self.assertEqual(self.sale_order_line.name, "My product description")
 
     def test_name_recalculation(self):
         self.sale_order_line.price_unit = 150.0
@@ -62,8 +60,7 @@ class TestSaleOrderPriceRecalculation(common.TransactionCase):
         )
         self.sale_order_line.name = 'Custom Jacket'
         self.sale_order.recalculate_names()
-        self.assertEquals(u"Jacket - Color: Black - Size: XL",
-                          self.sale_order_line.name)
+        self.assertEqual("Jacket - Color: Black - Size: XL",
+                         self.sale_order_line.name)
         # Check the price wasn't reset
-        self.assertEquals(initial_price,
-                          self.sale_order_line.price_unit)
+        self.assertEqual(initial_price, self.sale_order_line.price_unit)
