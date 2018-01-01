7f2ab31cebb0a0434669623364c672baeeda0e91
Hamza Talibi <htalibi.alami@gmail.com>
2018-01-01 12:50:10 +0000

    Migration to version 11.0

---
+++ sale_automatic_workflow/models/stock_picking.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+# © 2013 Camptocamp SA (author: Guewen Baconnier)
+# © 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    workflow_process_id = fields.Many2one(
+        comodel_name='sale.workflow.process',
+        string='Sale Workflow Process'
+    )
+
+    @api.multi
+    def validate_picking(self):
+        for picking in self:
+            picking.force_assign()
+            picking.do_transfer()
+        return True
