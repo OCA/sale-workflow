35261727dedb29bb8ff396589704a723ad234462
Swapnesh <s.shah.serpentcs@gmail.com>
2018-02-06 15:50:16 +0530

    [MIG] sale_last_price_info: Migration to 11.0

--- sale_last_price_info/models/product.py
+++ sale_last_price_info/models/product.py
@@ -1,29 +1,25 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    @api.one
-    def _get_last_sale(self):
+    def _compute_last_sale(self):
         """ Get last sale price, last sale date and last customer """
         lines = self.env['sale.order.line'].search(
             [('product_id', '=', self.id),
-             ('state', 'in', ['confirmed', 'done'])]).sorted(
+             ('state', 'in', ['sale', 'done'])]).sorted(
             key=lambda l: l.order_id.date_order, reverse=True)
         self.last_sale_date = lines[:1].order_id.date_order
         self.last_sale_price = lines[:1].price_unit
         self.last_customer_id = lines[:1].order_id.partner_id
 
     last_sale_price = fields.Float(
-        string='Last Sale Price', compute='_get_last_sale')
+        string='Last Sale Price', compute='_compute_last_sale')
     last_sale_date = fields.Date(
-        string='Last Sale Date', compute='_get_last_sale')
+        string='Last Sale Date', compute='_compute_last_sale')
     last_customer_id = fields.Many2one(
         comodel_name='res.partner', string='Last Customer',
-        compute='_get_last_sale')
+        compute='_compute_last_sale')
