9a15fde69bf8065d1aeaa888576683f0a2426b89
Swapnesh <s.shah.serpentcs@gmail.com>
2018-02-07 20:03:03 +0530

    [MIG] sale_commercial_partner: Migration to 11.0
    [UPD] Update sale_commercial_partner.pot

--- sale_commercial_partner/__manifest__.py
+++ sale_commercial_partner/__manifest__.py
@@ -1,16 +1,21 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
 {
     'name': 'Sale Commercial Partner',
-    'version': '10.0.1.0.0',
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
     'summary': "Add stored related field 'Commercial Entity' on sale orders",
+    'version': '11.0.1.0.0',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
-    'depends': ['sale'],
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    "contributors": [
+        "Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>",
+    ],
+    'depends': [
+        'sale'
+    ],
     'data': [
         'views/sale.xml',
     ],
