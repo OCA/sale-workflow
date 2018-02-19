9ad8803907f418c39b8a064004d2a2d6e761196a
benjamin.henquet@noviat.com <benjamin.henquet@noviat.com>
2018-02-19 10:48:34 +0100

    check pylint

--- sale_order_cancel_invoice/models/sale_order.py
+++ sale_order_cancel_invoice/models/sale_order.py
@@ -16,7 +16,8 @@ class SaleOrder(models.Model):
 
     def cancel_draft_invoice(self):
         for invoice in self.invoice_ids:
-            if not invoice.number and (invoice.state == 'draft' or invoice.state == 'proforma' or invoice.state == 'proforma2'):
+            if not invoice.number and (
+                        invoice.state == 'draft' or invoice.state == 'proforma' or invoice.state == 'proforma2'):
                 invoice.signal_workflow('invoice_cancel')
 
     @api.multi
