7b773a1e00c5f2e3e9e251edb066835c580ae8f8
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-04-04 20:09:24 +0200

    [MIG] sale_order_invoicing_finished_task: Migration to 11.0

--- sale_order_invoicing_finished_task/__manifest__.py
+++ sale_order_invoicing_finished_task/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
-# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2017-2018 Carlos Dauden <carlos.dauden@tecnativa.com>
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Sale Order Invoicing Finished Task",
-    "summary": "Control invoice order lines if his task has been finished",
-    "version": "10.0.1.0.3",
+    "summary": "Control invoice order lines if their related task has been "
+               "set to invoiceable",
+    "version": "11.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, "
