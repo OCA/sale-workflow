5bdf5c2f28a02b7654fe958e0f1eb957eb239fce
aheficent <ahenriquez@eficent.com>
2018-04-04 16:53:40 +0200

    [UPT]rename module product_customer_code

--- product_supplierinfo_for_customer_sale/__manifest__.py
+++ product_supplierinfo_for_customer_sale/__manifest__.py
@@ -3,7 +3,7 @@
 #     (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Product Customer code on sale",
+    "name": "Product Supplierinfo for customer sale",
     "version": "10.0.1.0.0",
     "summary": """Based on product_supplierinfo_for_customer,this module loads
          in every sale order line the customer code defined in the product,""",
