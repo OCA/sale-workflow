d89c4f35f2a584257309d8abd6ed2a863f9ff890
aheficent <ahenriquez@eficent.com>
2018-04-09 10:26:46 +0200

    [FIX]module rename. sale_delivery_block -> sale_stock_picking_blocking

---
+++ sale_stock_picking_blocking/models/sale_stock_picking_blocking_reason.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleDeliveryBlockReason(models.Model):
+    _name = 'sale.delivery.block.reason'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    sale_order_ids = fields.One2many(comodel_name='sale.order',
+                                     inverse_name='delivery_block_id',
+                                     string='Sale Orders',
+                                     readonly=True)
