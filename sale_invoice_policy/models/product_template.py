c2fc08a73eac870a0bc9f2b49e4f10bda7146d4e
Denis Roussel <denis.roussel@acsone.eu>
2018-05-04 11:19:51 +0200

    [10.0][ADD] sale_invoice_policy

---
+++ sale_invoice_policy/models/product_template.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    default_invoice_policy = fields.Selection(
+        [('order', 'Ordered quantities'),
+         ('delivery', 'Delivered quantities')],
+        string='Invoicing Policy',
+        help='Ordered Quantity: Invoice based on the quantity the customer '
+             'ordered.\n'
+             'Delivered Quantity: Invoiced based on the quantity the vendor '
+             'delivered (time or deliveries).',
+        default='order')
+
+    invoice_policy = fields.Selection(compute="_compute_invoice_policy",
+                                      store=False)
+
+    def _compute_invoice_policy(self):
+        """
+        Apply the invoice_policy given by context (if exist) otherwise use the
+        default invoice policy given by the field with this same name.
+        If the product is type = 'service', we don't have to apply the invoice
+        policy given by the context.
+        :return:
+        """
+        invoice_policy = self.env.context.get('invoice_policy')
+        for tmpl in self:
+            if tmpl.type != 'service' and invoice_policy:
+                tmpl.invoice_policy = invoice_policy
+            else:
+                tmpl.invoice_policy = tmpl.default_invoice_policy
