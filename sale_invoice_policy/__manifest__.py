a622795cb299f066e4280030a6fcd2584e55f15d
Denis Roussel <denis.roussel@acsone.eu>
2018-05-04 11:19:51 +0200

    [10.0][ADD] sale_invoice_policy

---
+++ sale_invoice_policy/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Sale invoice Policy",
+    'description': """
+        Sales Management: let the user choose the invoice policy on the
+        order""",
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'category': 'Sales Management',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'sale_stock',
+    ],
+    'data': [
+        'views/product_template_view.xml',
+        'views/sale_view.xml',
+    ],
+}
