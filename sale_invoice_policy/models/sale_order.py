79e4ac98226731644139c44a75a5b5b14a1eed69
Denis Roussel <denis.roussel@acsone.eu>
2018-05-04 11:19:51 +0200

    [10.0][ADD] sale_invoice_policy

---
+++ sale_invoice_policy/models/sale_order.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    invoice_policy = fields.Selection(
+        [('order', 'Ordered quantities'),
+         ('delivery', 'Delivered quantities')],
+        default='delivery',
+        required=True,
+        readonly=True,
+        states={
+            'draft': [('readonly', False)],
+            'sent': [('readonly', False)]
+        },
+        help='Ordered Quantity: Invoice based on the quantity the customer '
+             'ordered.\n'
+             'Delivered Quantity: Invoiced based on the quantity the vendor '
+             'delivered (time or deliveries).')
---
+++ sale_invoice_policy/models/sale_order_line.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    @api.depends('qty_invoiced', 'qty_delivered', 'product_uom_qty',
+                 'order_id.state', 'order_id.invoice_policy')
+    def _get_to_invoice_qty(self):
+        invoice_policies = set(self.mapped('order_id.invoice_policy'))
+        line_by_id = {l.id: l for l in self}
+        for invoice_policy in invoice_policies:
+            so_lines = self.with_context(
+                invoice_policy=invoice_policy).filtered(
+                    lambda x, p=invoice_policy: x.order_id.invoice_policy ==
+                    p).with_prefetch(self._prefetch)
+            so_lines.mapped('product_id')
+            if so_lines:
+                super(SaleOrderLine, so_lines)._get_to_invoice_qty()
+                for line in so_lines:
+                    # due to the change of context in compute methods,
+                    # assign the value in the modified context to self
+                    line_by_id[line.id].qty_to_invoice = line.qty_to_invoice
+        return True
+
+    @api.depends('state', 'product_uom_qty', 'qty_delivered', 'qty_to_invoice',
+                 'qty_invoiced', 'order_id.invoice_policy')
+    def _compute_invoice_status(self):
+        invoice_policies = self.mapped('order_id.invoice_policy')
+        line_by_id = {l.id: l for l in self}
+        for invoice_policy in invoice_policies:
+            so_lines = self.with_context(
+                invoice_policy=invoice_policy).filtered(
+                    lambda x, p=invoice_policy: x.order_id.invoice_policy ==
+                    p).with_prefetch(self._prefetch)
+            if so_lines:
+                super(SaleOrderLine, so_lines)._compute_invoice_status()
+                for line in so_lines:
+                    # due to the change of context in compute methods,
+                    # assign the value in the modified context to self
+                    line_by_id[line.id].invoice_status = line.invoice_status
+
+        return True
