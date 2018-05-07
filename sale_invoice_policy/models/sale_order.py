e0ed1f0b4387c4e1fd0232c7cdfff49b8c6f7132
Denis Roussel <denis.roussel@acsone.eu>
2018-05-07 16:41:22 +0200

    [IMP] sale_invoice_policy: Add config for default_policy and if policy is required on Sale Orders

--- sale_invoice_policy/models/sale_order.py
+++ sale_invoice_policy/models/sale_order.py
@@ -2,7 +2,7 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -21,3 +21,27 @@ class SaleOrder(models.Model):
              'ordered.\n'
              'Delivered Quantity: Invoiced based on the quantity the vendor '
              'delivered (time or deliveries).')
+    invoice_policy_required = fields.Boolean(
+        compute='_compute_invoice_policy_required',
+        default=lambda self: self.env['ir.values'].get_default(
+            'sale.config.settings', 'sale_invoice_policy_required'),
+    )
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(SaleOrder, self).default_get(fields_list)
+        default_sale_invoice_policy = self.env['ir.values'].get_default(
+            'sale.config.settings', 'sale_default_invoice_policy')
+        if 'invoice_policy' not in res:
+            res.update({
+                'invoice_policy': default_sale_invoice_policy
+            })
+        return res
+
+    @api.multi
+    @api.depends()
+    def _compute_invoice_policy_required(self):
+        invoice_policy_required = self.env['ir.values'].get_default(
+            'sale.config.settings', 'sale_invoice_policy_required')
+        for sale in self:
+            sale.invoice_policy_required = invoice_policy_required
