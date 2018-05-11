3e319b13e07c5b87b02ff4316973731ec58b0a4e
Denis Roussel <denis.roussel@acsone.eu>
2018-05-11 11:17:25 +0200

    [10.0][ADD] sale_procurement_amendment

---
+++ sale_procurement_amendment/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Procurement Amendment',
+    'summary': """
+        Allow to reflect confirmed sale lines quantity amendments
+        to procurements""",
+    'version': '10.0.1.0.1',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'development_status': 'Beta',
+    'maintainers': ['rousseldenis'],
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'depends': [
+        'sale',
+        'sale_stock',
+    ],
+}
---
+++ sale_procurement_amendment/readme/USAGE.rst
@@ -0,0 +1,6 @@
+To use this module, you need to:
+
+#. Create a Sale Order with at least one product and confirm it
+#. Don't modify any picking
+#. Decrease the product quantity in Sale Order and save
+#. The quantities in pickings are changed
