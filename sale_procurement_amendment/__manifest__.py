372cc5eff0b658b7f7d3e755300d5b9247f739c0
Denis Roussel <denis.roussel@acsone.eu>
2018-05-11 11:17:25 +0200

    [10.0][ADD] sale_procurement_amendment

---
+++ sale_procurement_amendment/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Procurement Amendment',
+    'summary': """
+        Allow to reflect confirmed sale lines quantity amendments
+        to procurements""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'depends': [
+        'sale',
+        'sale_stock',
+    ],
+}
