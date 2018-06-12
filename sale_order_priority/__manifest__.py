51e0e1f57edd33fa942e5ee1886e4917909e1580
Simone Rubino <simone.rubino@agilebg.com>
2018-06-12 16:11:36 +0200

    [ADD] sale_order_priority (#657)
    * [ADD] sale_order_priority
    
    * Code improvements
    
    * [FIX] Better not to call write in inverse

---
+++ sale_order_priority/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale order priority",
+    "summary": "Define priority on sale orders",
+    "version": "10.0.1.0.0",
+    "category": "Sale Workflow",
+    "website": "https://github.com/OCA/sale-workflow/tree/10.0/"
+               "sale_order_priority",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "procurement",
+        "sale_stock"
+    ],
+    "data": [
+        "views/sale.xml"
+    ]
+}
