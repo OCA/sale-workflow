84b1c1648bcdbea0b82fe690bf0665a02d404e42
Alexis de Lattre <alexis@via.ecp.fr>
2018-06-20 22:47:40 +0200

    [10.0] sale_commercial_partner: add compute_sudo=True and readonly=True (#662)

--- sale_commercial_partner/models/sale.py
+++ sale_commercial_partner/models/sale.py
@@ -10,4 +10,5 @@ class SaleOrder(models.Model):
 
     commercial_partner_id = fields.Many2one(
         'res.partner', related='partner_id.commercial_partner_id', store=True,
-        string='Commercial Entity', index=True)
+        string='Commercial Entity', index=True, compute_sudo=True,
+        readonly=True)
