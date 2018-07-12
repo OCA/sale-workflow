09d567f03249cd11c988d6dd274966cd614c52ad
David <david.vidal@tecnativa.com>
2018-07-12 16:32:33 +0200

    [MIG] sale_order_product_recommendation: Migration to 11.0

--- sale_order_product_recommendation/tests/test_recommendation.py
+++ sale_order_product_recommendation/tests/test_recommendation.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import SavepointCase
+from odoo.tests.common import SavepointCase
 
 
 class RecommendationCase(SavepointCase):
@@ -11,8 +10,8 @@ class RecommendationCase(SavepointCase):
         super(RecommendationCase, cls).setUpClass()
         cls.camptocamp = cls.env.ref("base.res_partner_12")
         cls.product_12 = cls.env.ref('product.product_product_12')
-        cls.product_46 = cls.env.ref('product.product_product_46')
-        cls.product_57 = cls.env.ref('product.product_product_57')
+        cls.product_10 = cls.env.ref('product.product_product_10')
+        cls.product_20 = cls.env.ref('product.product_product_20')
         # Create old sale orders to have searchable history
         cls.env["sale.order"].create({
             "partner_id": cls.camptocamp.id,
@@ -24,13 +23,13 @@ class RecommendationCase(SavepointCase):
                     "qty_delivered": 25,
                 }),
                 (0, 0, {
-                    "product_id": cls.product_46.id,
-                    "name": cls.product_46.name,
+                    "product_id": cls.product_10.id,
+                    "name": cls.product_10.name,
                     "qty_delivered": 50,
                 }),
                 (0, 0, {
-                    "product_id": cls.product_57.id,
-                    "name": cls.product_57.name,
+                    "product_id": cls.product_20.id,
+                    "name": cls.product_20.name,
                     "qty_delivered": 100,
                 }),
             ],
@@ -40,8 +39,8 @@ class RecommendationCase(SavepointCase):
             "state": "done",
             "order_line": [
                 (0, 0, {
-                    "product_id": cls.product_46.id,
-                    "name": cls.product_46.name,
+                    "product_id": cls.product_10.id,
+                    "name": cls.product_10.name,
                     "qty_delivered": 50,
                 }),
             ],
@@ -75,12 +74,12 @@ class RecommendationCase(SavepointCase):
         self.assertEqual(wizard.line_ids[0].units_delivered, 25)
         self.assertEqual(wizard.line_ids[0].units_included, 3)
         # Product 46 appears second
-        self.assertEqual(wizard.line_ids[1].product_id, self.product_46)
+        self.assertEqual(wizard.line_ids[1].product_id, self.product_10)
         self.assertEqual(wizard.line_ids[1].times_delivered, 2)
         self.assertEqual(wizard.line_ids[1].units_delivered, 100)
         self.assertEqual(wizard.line_ids[1].units_included, 0)
         # Product 57 appears third
-        self.assertEqual(wizard.line_ids[2].product_id, self.product_57)
+        self.assertEqual(wizard.line_ids[2].product_id, self.product_20)
         self.assertEqual(wizard.line_ids[2].times_delivered, 1)
         self.assertEqual(wizard.line_ids[2].units_delivered, 100)
         self.assertEqual(wizard.line_ids[2].units_included, 0)
