2aa6b45e44f983fe65e64d461982c05b754abff9
David <david.vidal@tecnativa.com>
2018-07-12 16:32:33 +0200

    [MIG] sale_order_product_recommendation: Migration to 11.0

--- sale_order_product_recommendation/__manifest__.py
+++ sale_order_product_recommendation/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Order Product Recommendation",
     "summary": "Recommend products to sell to customer based on history",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Sales",
-    "website": "https://www.tecnativa.com/",
+    "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
