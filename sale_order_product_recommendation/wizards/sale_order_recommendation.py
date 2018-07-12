53748dd05d608054cb7aee00a0ffaf9afdaf0626
David <david.vidal@tecnativa.com>
2018-07-12 16:32:33 +0200

    [MIG] sale_order_product_recommendation: Migration to 11.0

--- sale_order_product_recommendation/wizards/sale_order_recommendation.py
+++ sale_order_product_recommendation/wizards/sale_order_recommendation.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2018 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime, timedelta
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderRecommendation(models.TransientModel):
