3289d7ae189a95373472d14f97476e5c68e5e680
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-07-13 12:58:38 +0200

    [IMP] partner_prospect: some code cleaning in order to meet OCA Guidelines

--- partner_prospect/__manifest__.py
+++ partner_prospect/__manifest__.py
@@ -1,41 +1,19 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Partner Prospect",
     "version": "11.0.1.0.0",
-    "license": "AGPL-3",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-workflow",
     "author": "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
               "Odoo Community Association (OCA)",
-    "website": "http://www.avanzosc.es",
-    "category": "Sales Management",
-    "contributors": [
-        "Esther Martín <esthermartin@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>",
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-    ],
+    "license": "AGPL-3",
+    "installable": True,
     "depends": [
         "sale",
     ],
     "data": [
         "views/res_partner_view.xml",
     ],
-    "installable": True,
 }
