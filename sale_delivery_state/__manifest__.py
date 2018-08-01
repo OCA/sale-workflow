a04607360d09e26bb3ae59c3414e1c7ed2833e2c
Pierrick Brun <brun.pierrick@protonmail.com>
2018-08-01 11:46:37 +0200

    [ADD] sale_delivery_state module

--- sale_delivery_state/__manifest__.py
+++ sale_delivery_state/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion (http://www.akretion.com).
 # @author Pierrick BRUN <pierrick.brun@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -5,17 +6,14 @@
 {
     "name": "Sale delivery State",
     "summary": "Show the delivery state on the sale order",
-    "version": "15.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Product",
-    "website": "https://github.com/OCA/sale-workflow",
-    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "www.akretion.com",
+    "author": " Akretion",
     "license": "AGPL-3",
-    "installable": True,
-    "depends": ["sale"],
+    "depends": ["sale_stock"],
     "data": [
         "views/sale_order_views.xml",
     ],
-    "demo": [
-        "demo/sale_demo.xml",
-    ],
+    "installable": True,
 }
--- sale_delivery_state/models/__init__.py
+++ sale_delivery_state/models/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from . import sale_order
+from . import stock_picking
