7ab9b6d18d965aae739ed3d8fcadd90890b2a5e1
Pierrick Brun <brun.pierrick@protonmail.com>
2018-08-01 11:46:37 +0200

    [ADD] sale_delivery_state module

---
+++ sale_delivery_state/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com).
+# @author Pierrick BRUN <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale delivery State",
+    "summary": "Show the delivery state on the sale order",
+    "version": "10.0.1.0.0",
+    "category": "Product",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "depends": ["sale_stock"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
