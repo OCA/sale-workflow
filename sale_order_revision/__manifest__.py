673e942b538be3577f05d0da4ed4f2bb0d7c4edd
devang-dreambits <devang@dreambits.in>
2018-08-13 20:32:40 +0530

    [MIG] sale_order_revision: Migration to 11.0

--- sale_order_revision/__manifest__.py
+++ sale_order_revision/__manifest__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2018 Dreambits Technologies Pvt. Ltd. (<http://dreambits.in>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale order revisions",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
     "category": "Sale Management",
     "author": "Agile Business Group,"
+              "Dreambits,"
               "Camptocamp,"
               "Akretion,"
               "Odoo Community Association (OCA), "
@@ -15,7 +16,7 @@
     "website": "http://www.agilebg.com",
     "license": "AGPL-3",
     "depends": [
-        "sale",
+        "sale_management",
     ],
     "data": [
         "view/sale_order.xml",
