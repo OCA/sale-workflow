cfc3c13cef092d139f2953ecdbd2cf7075947981
devang-dreambits <devang@dreambits.in>
2018-08-13 20:32:40 +0530

    [MIG] sale_order_revision: Migration to 11.0

--- sale_order_revision/tests/__init__.py
+++ sale_order_revision/tests/__init__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2018 Dreambits Technologies Pvt. Ltd. (<http://dreambits.in>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import test_sale_order_revision
