89ab7ccf01bda7cec9f39a26bcefaf0ed9da3f3a
xavierjimenez <xavier.jimenez@qubiq.es>
2018-08-16 09:22:40 +0200

    [MIG] product_supplierinfo_for_customer_sale: Migration to 11.0
     - New field name from type to supplierinfo_type
     - Remove copyright for __init__.py
     - Re-using variables from tests
     - Manifest bump version and using module url
     - Get commit history from
     https://github.com/OCA/product-attribute/tree/8.0/product_supplierinfo_for_customer_sale
        - Run 2to3 script
        - view fixing

--- product_supplierinfo_for_customer_sale/tests/test_product_name_search.py
+++ product_supplierinfo_for_customer_sale/tests/test_product_name_search.py
@@ -12,7 +12,7 @@ class TestProductNameSearch(TransactionCase):
     """
     def setUp(self):
         super(TestProductNameSearch, self).setUp()
-        self.customer = self.env.ref('base.res_partner_9')
+        self.customer = self.env.ref('base.res_partner_1')
         self.customer_1 = self.env.ref('base.res_partner_2')
         self.product = self.env['product.product'].create(
             {'name': 'Name_product',
@@ -26,7 +26,7 @@ class TestProductNameSearch(TransactionCase):
             'product_name': 'Name_test',
             'name': self.customer.id,
             'product_tmpl_id': self.product.product_tmpl_id.id,
-            'type': 'customer',
+            'supplierinfo_type': 'customer',
         }
 
     def test_10_find_product_customer_code(self):
@@ -40,24 +40,24 @@ class TestProductNameSearch(TransactionCase):
 
         # Search by product customer code
         product_names = self.product.name_search(name='code_test')
-        self.assertEquals(len(product_names), 1)
-        self.assertEquals(self.product.id, product_names[0][0])
-        self.assertEquals('[code_test] Name_test', product_names[0][1])
+        self.assertEqual(len(product_names), 1)
+        self.assertEqual(self.product.id, product_names[0][0])
+        self.assertEqual('[code_test] Name_test', product_names[0][1])
 
         # Search by product default code with the customer used in
         # configuration customer
         product_names = self.product.name_search(name='code_product')
-        self.assertEquals(len(product_names), 1)
-        self.assertEquals(self.product.id, product_names[0][0])
-        self.assertEquals('[code_test] Name_test', product_names[0][1])
+        self.assertEqual(len(product_names), 1)
+        self.assertEqual(self.product.id, product_names[0][0])
+        self.assertEqual('[code_test] Name_test', product_names[0][1])
 
         # Search by product default code with a different customer used in
         # configuration customer
         product_names = self.product.with_context(
             partner_id=self.customer_1.id).name_search(name='code_product')
-        self.assertEquals(len(product_names), 1)
-        self.assertEquals(self.product.id, product_names[0][0])
-        self.assertEquals('[code_product] Name_product', product_names[0][1])
+        self.assertEqual(len(product_names), 1)
+        self.assertEqual(self.product.id, product_names[0][0])
+        self.assertEqual('[code_product] Name_product', product_names[0][1])
 
         # Create a product_1 with default_code similar to customer_code of
         # product, then name_search must find two products
@@ -71,7 +71,7 @@ class TestProductNameSearch(TransactionCase):
 
         # Search by product customer code
         product_names = self.product.name_search(name='code_test')
-        self.assertEquals(len(product_names), 2)
+        self.assertEqual(len(product_names), 2)
         product_names = product_names[0] + product_names[1]
         self.assertIn(self.product.id, product_names)
         self.assertIn(self.product_1.id, product_names)
