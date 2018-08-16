0d13b861ec9a37f3920ba62a670b769693995580
xavierjimenez <xavier.jimenez@qubiq.es>
2018-08-16 09:22:40 +0200

    [MIG] product_supplierinfo_for_customer_sale: Migration to 11.0
     - New field name from type to supplierinfo_type
     - Remove copyright for __init__.py
     - Re-using variables from tests
     - Manifest bump version and using module url
     - Get commit history from
     https://github.com/OCA/product-attribute/tree/8.0/product_supplierinfo_for_customer_sale
        - Run 2to3 script
        - view fixing

--- product_supplierinfo_for_customer_sale/__manifest__.py
+++ product_supplierinfo_for_customer_sale/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2017 Agile Business Group sagl
 #     (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo for Customer Sale",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "summary": "Loads in every sale order line the customer code defined "
                "in the product",
     "author": "Agile Business Group,Odoo Community Association (OCA)",
-    "website": "http://www.agilebg.com",
+    "website": "https://github.com/OCA/sale-workflow/tree/11.0/product_supplierinfo_for_customer_sale", # noqa
     "category": "Sales Management",
     "license": "AGPL-3",
     "depends": [
