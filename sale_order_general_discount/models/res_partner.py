caf17067e97f161497514bbd3629dabfd49c3259
sergio.teruel <sergio.teruel@tecnativa.com>
2018-08-26 09:45:55 +0200

    [11.0][NEW] sale_order_general_discount: New module for apply a discount for all sale order lines

---
+++ sale_order_general_discount/models/res_partner.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    sale_discount = fields.Float(
+        digits=dp.get_precision('Discount'),
+        string='Discount (%)',
+        company_dependent=True,
+    )
