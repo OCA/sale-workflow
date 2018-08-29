faa33a58206d7e6179c5b6e9cd2536087a9631fc
mreficent <miquel.raich@eficent.com>
2018-08-29 18:52:53 +0200

    [MIG] sale_force_invoiced: Migration to 11.0

--- sale_force_invoiced/model/sale_order.py
+++ sale_force_invoiced/model/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -8,20 +7,19 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    force_invoiced = fields.Boolean(string='Force invoiced',
-                                    help='When you set this field, the sales '
-                                         'order will be considered as fully '
-                                         'invoiced, even when there may be '
-                                         'ordered or delivered quantities '
-                                         'pending to invoice.',
-                                    readonly=True,
-                                    states={'done': [('readonly', False)]},
-                                    copy=False,
-                                    )
+    force_invoiced = fields.Boolean(
+        string='Force invoiced',
+        help='When you set this field, the sales order will be considered as '
+             'fully invoiced, even when there may be ordered or delivered '
+             'quantities pending to invoice.',
+        readonly=True,
+        states={'done': [('readonly', False)]},
+        copy=False,
+    )
 
     @api.depends('force_invoiced')
     def _get_invoiced(self):
         super(SaleOrder, self)._get_invoiced()
-        for order in self:
-            if order.force_invoiced and order.invoice_status == 'to invoice':
-                order.invoice_status = 'invoiced'
+        for order in self.filtered(lambda so: so.force_invoiced and
+                                   so.invoice_status == 'to invoice'):
+            order.invoice_status = 'invoiced'
