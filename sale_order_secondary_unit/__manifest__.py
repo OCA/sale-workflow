099645f2bd7557702a37fd404341f76690811d71
sergio.teruel <sergio.teruel@tecnativa.com>
2018-09-05 13:47:31 +0200

    [11.0][NEW] sale_order_secondary_unit: New module for sale in a secondary product unit

---
+++ sale_order_secondary_unit/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2018 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale Order Secondary Unit',
+    'summary': 'Sale product in a secondary unit',
+    'version': '11.0.1.0.0',
+    'development_status': 'Beta',
+    'category': 'Sale',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+    'depends': [
+        'sale',
+        'product_secondary_unit',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+}
