f55f9858889858576d1be2c47cc14d70816076a7
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-09-14 16:23:37 +0200

    [ADD] add new module to display discount amount on sale orders

---
+++ sale_discount_display_amount/models/sale_order_line.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    discount_total = fields.Monetary(
+        compute='_compute_amount',
+        string='Discount Subtotal',
+        readonly=True,
+        store=True)
+    price_total_no_discount = fields.Monetary(
+        compute='_compute_amount',
+        string='Subtotal Without Discount',
+        readonly=True,
+        store=True)
+
+    def _compute_discount(self):
+        for line in self:
+            if not line.discount:
+                line.price_total_no_discount = line.price_total
+                continue
+            price = line.price_unit
+            taxes = line.tax_id.compute_all(
+                price,
+                line.order_id.currency_id,
+                line.product_uom_qty,
+                product=line.product_id,
+                partner=line.order_id.partner_shipping_id)
+
+            price_total_no_discount = taxes['total_included']
+            discount_total = price_total_no_discount - line.price_total
+
+            line.update({
+                'discount_total': discount_total,
+                'price_total_no_discount': price_total_no_discount
+            })
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        res = super(SaleOrderLine, self)._compute_amount()
+        self._compute_discount()
+        return res
---
+++ sale_discount_display_amount/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+In standard Odoo only display the rate of the discount applied, never the
+amount. It could be great to be able to tell the customer how much he spares.
+This is the goal of this addons, it will show on a sale
+order the total without the discount and the value of the discount.
