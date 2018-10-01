70990f7ba3afe43c4287ecf1d0b2af4ccb3fb260
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2018-10-01 15:48:21 +0530

    [MIG]Migrated Code

--- sale_validity/models/res_config_settings.py
+++ sale_validity/models/res_config_settings.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,8 +5,8 @@
 from odoo import models, fields
 
 
-class SaleConfigSettings(models.TransientModel):
-    _inherit = 'sale.config.settings'
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
 
     default_sale_order_validity_days = fields.Integer(
         related='company_id.default_sale_order_validity_days')
--- sale_validity/models/sale_order.py
+++ sale_validity/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2013-2017 Camptocamp SA
 # © 2014-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
