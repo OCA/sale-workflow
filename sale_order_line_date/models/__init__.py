d929dfdfad0cffb4660c568331e04c61e21f19df
mpanarin <m.panarin@mobilunity.com>
2018-10-01 18:42:24 +0300

    [MIG] 12.0 sale_order_line_date

--- sale_order_line_date/models/__init__.py
+++ sale_order_line_date/models/__init__.py
@@ -1,9 +1,2 @@
-# © 2016 OdooMRP team
-# © 2016 AvanzOSC
-# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2016 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import sale_order
 from . import sale_order_line
