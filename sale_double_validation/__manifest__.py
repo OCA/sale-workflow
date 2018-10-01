b3aa7b951a2bf745096960fd707eec83c6864526
Denis Leemann <denis.leemann@camptocamp.com>
2018-10-01 15:34:15 +0200

    fixup! Migrate sale_double_validation to 10.0

--- sale_double_validation/__manifest__.py
+++ sale_double_validation/__manifest__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
+# Copyright 2017-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {'name': 'Double validation for Sales',
  'summary': "",
  'version': '10.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'maintainer': 'Camptocamp',
+ 'author': 'Camptocamp SA, Odoo Community Association (OCA)',
+ 'maintainer': 'Camptocamp ',
  'license': 'AGPL-3',
  'category': 'sale',
  'depends': [
      'sale',
      'sales_team',
  ],
- 'website': 'www.camptocamp.com',
+ 'website': 'https://github.com/OCA/sale-workflow',
  'data': ['views/res_config.xml',
           'views/sale.xml'],
  'test': [],
--- sale_double_validation/models/__init__.py
+++ sale_double_validation/models/__init__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from . import company
 from . import sale
 from . import res_config
