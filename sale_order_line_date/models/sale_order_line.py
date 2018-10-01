30ee2ae19489161441cae08de2f9bc65af5d98d2
mpanarin <m.panarin@mobilunity.com>
2018-10-01 18:42:24 +0300

    [MIG] 12.0 sale_order_line_date

--- sale_order_line_date/models/sale_order_line.py
+++ sale_order_line_date/models/sale_order_line.py
@@ -3,7 +3,8 @@
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # © 2016 Eficent Business and IT Consulting Services, S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
 
