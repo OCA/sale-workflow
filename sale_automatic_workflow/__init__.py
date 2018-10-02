a5befdd0779acc7a0fd3f9e93cd3eb60c8bef3a1
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-10-02 12:35:39 +0300

    [12.0][MIG] sale_automatic_workflow: Migrate to 12.0

--- sale_automatic_workflow/__init__.py
+++ sale_automatic_workflow/__init__.py
@@ -1,6 +1,1 @@
-# Copyright 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
-# Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
-# Copyright 2016 Sodexis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from . import models
