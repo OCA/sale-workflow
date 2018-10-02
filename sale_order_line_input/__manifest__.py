7fa14a31d215508be8ab74071e8a5e2061aed6b1
mpanarin <m.panarin@mobilunity.com>
2018-10-02 12:29:45 +0300

    [MIG] sale_order_line_input: migration to v12.0

--- sale_order_line_input/__manifest__.py
+++ sale_order_line_input/__manifest__.py
@@ -3,15 +3,13 @@
 {
     'name': 'Sale Order Line Input',
     'summary': 'Search, create or modify directly sale order lines',
-    'version': '11.0.1.0.0',
-    'development_status': 'Beta',
+    'version': '12.0.1.0.0',
     'category': 'Sales',
     'website': 'https://github.com/OCA/sale-workflow',
     'author': 'Tecnativa, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'installable': True,
     'depends': [
-        'sale',
+        'sale_management',
     ],
     'data': [
         'views/sale_order_line_view.xml',
