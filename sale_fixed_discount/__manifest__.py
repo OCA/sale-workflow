ca29baaab1514e2d5f552d212df87d14ee9d11a5
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-10-08 16:32:35 +0200

    [11.0][mig] sale_fixed_discount

--- sale_fixed_discount/__manifest__.py
+++ sale_fixed_discount/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Fixed Discount",
     "summary": "Allows to apply fixed amount discounts in sales orders.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Eficent, Odoo Community Association (OCA)",
@@ -15,7 +14,9 @@
         "sale", "account_invoice_fixed_discount"
     ],
     "data": [
-        "views/sale_order_view.xml",
+        "views/sale_order_views.xml",
+        "views/account_invoice_views.xml",
         "reports/report_sale_order.xml",
+        "reports/report_account_invoice.xml"
     ],
 }
