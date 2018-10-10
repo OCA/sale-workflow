bc4f1a8d065f186acd24db1181d1e89d5d9375a8
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-10-10 06:37:51 +0200

    [ADD] sale_promotion_rule_assortment: Add new addon sale_promotion_rule_assortment
    [UPD] Update sale_promotion_rule_assortment.pot
    
    [UPD] README.rst

---
+++ sale_promotion_rule_assortment/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Promotion Rule Assortment',
+    'summary': """
+        This addon allows to reduce the appliance of a promotion rule to a
+        set of products""",
+    'version': '10.0.1.0.0',
+    "development_status": "Beta",
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'depends': [
+        'product_assortment',
+        'sale_promotion_rule',
+    ],
+    'data': [
+        'views/sale_promotion_rule.xml',
+    ],
+    'demo': [
+        'demo/sale_promotion_rule.xml',
+        'demo/sale_order.xml',
+    ],
+}
---
+++ sale_promotion_rule_assortment/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_promotion_rule_assortment
