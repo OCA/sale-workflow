f3b721f11a60dda0b276b10128220802e3524e60
SimoRubi <simone.rubino@agilebg.com>
2018-10-16 13:52:59 +0200

    [IMP] sale_order_type: Find sale order type based on the products of the sale order

--- sale_order_type/__manifest__.py
+++ sale_order_type/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Sale Order Type",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.1.0",
     "category": "Sales Management",
     "author": "Grupo Vermon,"
               "AvanzOSC,"
@@ -19,8 +19,7 @@
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "depends": [
-        'sale_stock',
-        'account',
+        'sale_stock'
     ],
     "demo": [
         "demo/sale_order_demo.xml",
@@ -30,6 +29,7 @@
         "security/security.xml",
         "views/sale_order_view.xml",
         "views/sale_order_type_view.xml",
+        "views/sale_order_type_rule_view.xml",
         "views/account_invoice_view.xml",
         "views/res_partner_view.xml",
         "data/default_type.xml",
---
+++ sale_order_type/readme/ROADMAP.rst
@@ -0,0 +1,1 @@
+* Manage the order of *sale.order.type.rule* similar to *product.pricelist.item* (see field *applied_on*) instead of using *sequence*.
