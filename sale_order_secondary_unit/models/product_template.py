d4ca6ebc7e006642a351e8a752d50fd0cd19c9ce
Sergio Teruel <sergio.teruel@tecnativa.com>
2018-10-26 10:40:30 +0200

    [11.0][FIX] sale_order_secondary_unit: define sale secondary uom in this module instead of product_secondary_unit

---
+++ sale_order_secondary_unit/models/product_template.py
@@ -0,0 +1,12 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    sale_secondary_uom_id = fields.Many2one(
+        comodel_name='product.secondary.unit',
+        string='Default unit sale',
+    )
