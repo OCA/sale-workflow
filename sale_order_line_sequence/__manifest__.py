b57aed5d90248dc5e5176efbc3bdb8505d488667
remihb <remi@hobo.live>
2018-10-30 11:21:00 +0100

    [MIG] sale_order_line_sequence: migration to 11.0

--- sale_order_line_sequence/__manifest__.py
+++ sale_order_line_sequence/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,12 +5,12 @@
 {
     "name": "Sale Order Line Sequence",
     "summary": "Propagates SO line sequence to invoices and stock picking.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent, "
               "Serpent CS, "
               "Odoo Community Association (OCA)",
     "category": "Sales",
-    "website": "https://www.eficent.com/",
+    "website": "https://github.com/OCA/sale-workflow/tree/11.0/sale_order_line_sequence", # noqa
     "license": "AGPL-3",
     'data': ['views/sale_view.xml',
              'views/report_saleorder.xml'],
--- sale_order_line_sequence/model/sale_order.py
+++ sale_order_line_sequence/model/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
