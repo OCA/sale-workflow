1fb64b82cc1589901c64042247e4cbf8e5ebf4bf
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-06 00:52:25 +0100

    [MIG] sales_team_security: Adaptation to v11 + enhancement

---
+++ sales_team_security/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2016-2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sales documents permissions by channels (teams)",
+    "summary": "New group for seeing only sales channel's documents",
+    "version": "11.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": [
+        "pedrobaeza",
+    ],
+    "depends": [
+        "crm",
+        "sale",
+    ],
+    "data": [
+        "security/sales_team_security.xml",
+        "views/res_partner_view.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
