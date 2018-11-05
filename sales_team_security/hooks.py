3900a84d4e4c3eaf102c08fde0d465831d6d8523
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-06 00:52:25 +0100

    [MIG] sales_team_security: Adaptation to v11 + enhancement

--- sales_team_security/hooks.py
+++ sales_team_security/hooks.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Tecnativa - Pedro M. Baeza
+# Copyright 2018-2016 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 
-def assign_contacts_team(cr, registry):
+def post_init_hook(cr, registry):
     """At installation time, propagate the parent sales team to the children
     contacts that have this field empty, as it's supposed that the intention
     is to have the same.
     """
     cr.execute(
-        """
-        UPDATE res_partner
-        SET section_id=parent.section_id
+        """UPDATE res_partner
+        SET team_id=parent.team_id
         FROM res_partner AS parent
-        WHERE parent.section_id IS NOT NULL
+        WHERE parent.team_id IS NOT NULL
         AND res_partner.parent_id = parent.id
-        AND res_partner.section_id IS NULL
-        """)
+        AND res_partner.team_id IS NULL"""
+    )
