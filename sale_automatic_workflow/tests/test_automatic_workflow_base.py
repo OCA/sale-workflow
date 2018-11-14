ffb28f267d420b674abd969885fa0fc271c81413
Graeme Gellatly <gdgellatly@gmail.com>
2018-11-14 22:44:23 +1300

    Fix some of the porting of sale_automatic_workflow [WIP]

--- sale_automatic_workflow/tests/test_automatic_workflow_base.py
+++ sale_automatic_workflow/tests/test_automatic_workflow_base.py
@@ -61,7 +61,7 @@ class TestAutomaticWorkflowMixin:
                         self.env.ref('stock.stock_location_stock').id
                     })]
                 })
-                inventory.action_done()
+                inventory.post_inventory()
         return order
 
     def create_full_automatic(self, override=None):
