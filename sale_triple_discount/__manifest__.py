dc584af2b41d97149742e314eec624c02f29eeb4
Jacques-Etienne Baudoux <je@bcim.be>
2018-11-21 16:32:19 +0100

    sale_triple_discount: Fix tax total on report: include the discount2/3 in the computation

--- sale_triple_discount/__manifest__.py
+++ sale_triple_discount/__manifest__.py
@@ -2,6 +2,7 @@
 # Copyright 2015 ADHOC SA  (http://www.adhoc.com.ar)
 # Copyright 2017 Alex Comba - Agile Business Group
 # Copyright 2017 Tecnativa - David Vidal
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
