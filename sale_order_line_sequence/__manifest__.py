5d2bf5ec0ae9252edb071d087e26ac688585a78c
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-11-28 13:53:18 +0530

    [12.0] [MIG] sale_order_line_sequence (#744)
    * sale_order_line_sequence v10
    
    * OCA Transbot updated translations from Transifex
    
    * Update sale_order_line_sequence.pot
    
    * Translated using Weblate (Portuguese)
    
    Currently translated at 100,0% (6 of 6 strings)
    
    Translation: sale-workflow-10.0/sale-workflow-10.0-sale_order_line_sequence
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-10-0/sale-workflow-10-0-sale_order_line_sequence/pt/
    
    * sale_order_line_sequence: migration to 11.0
    
    * Update sale_order_line_sequence.pot
    
    * [MIG] sale_order_line_sequence v12

---
+++ sale_order_line_sequence/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Line Sequence",
+    "summary": "Propagates SO line sequence to invoices and stock picking.",
+    "version": "12.0.1.0.0",
+    "author": "Eficent, "
+              "Serpent CS, "
+              "Odoo Community Association (OCA)",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    'data': [
+        'views/sale_view.xml',
+        'views/report_saleorder.xml'
+    ],
+    "depends": [
+        "sale",
+    ],
+    'post_init_hook': 'post_init_hook',
+    "installable": True,
+}
