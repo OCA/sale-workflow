3c9bbd4760ef7cff4c82c401d5c06ad63bae0120
Andrea <a.stirpe@onestein.nl>
2018-12-03 13:33:20 +0100

    [12.0][MIG] sale_order_archive

--- sale_order_archive/models/sale_order.py
+++ sale_order_archive/models/sale_order.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Onestein (<http://www.onestein.eu>)
+# Copyright 2017-2018 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
