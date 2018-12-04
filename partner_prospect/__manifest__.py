6e7a860bc8e9c0103639102ba0172a6fc9a4f509
Sudhir Arya <sudhir@erpharbor.com>
2018-12-04 11:44:22 +0530

    [MIG] Migrated partner_prospect to v12

--- partner_prospect/__manifest__.py
+++ partner_prospect/__manifest__.py
@@ -2,18 +2,18 @@
 
 {
     "name": "Partner Prospect",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "installable": True,
     "depends": [
         "sale",
     ],
     "data": [
         "views/res_partner_view.xml",
     ],
+    "installable": True,
 }
