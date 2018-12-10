ae52c501d64591f07be5066a43111bf75154b1eb
mreficent <miquel.raich@eficent.com>
2018-12-10 13:24:05 +0100

    [IMP] Restore super in _action_launch_procurement_rule

--- sale_procurement_group_by_line/model/sale.py
+++ sale_procurement_group_by_line/model/sale.py
@@ -40,15 +40,10 @@ class SaleOrderLine(models.Model):
         errors = []
         groups = {}
         for line in self:
-            if line.state != 'sale' or line.product_id.type not in ('consu',
-                                                                    'product'):
+            if line.state != 'sale' or line.product_id.type not in (
+                    'consu', 'product'):
                 continue
-            qty = 0.0
-            for move in line.move_ids.filtered(lambda r: r.state != 'cancel'):
-                qty += move.product_uom.\
-                    _compute_quantity(move.product_uom_qty,
-                                      line.product_uom,
-                                      rounding_method='HALF-UP')
+            qty = line._get_qty_procurement()
             if float_compare(qty, line.product_uom_qty,
                              precision_digits=precision) >= 0:
                 continue
@@ -68,7 +63,7 @@ class SaleOrderLine(models.Model):
                 vals.update({
                     'move_type': line.order_id.picking_policy,
                     'sale_id': line.order_id.id,
-                    'partner_id': line.order_id.partner_shipping_id.id
+                    'partner_id': line.order_id.partner_shipping_id.id,
                 })
                 group_id = self.env['procurement.group'].create(vals)
             else:
@@ -77,11 +72,11 @@ class SaleOrderLine(models.Model):
                 # of the group.
                 updated_vals = {}
                 if group_id.partner_id != line.order_id.partner_shipping_id:
-                    updated_vals.update({'partner_id':
-                                         line.order_id.partner_shipping_id.id})
+                    updated_vals.update(
+                        {'partner_id': line.order_id.partner_shipping_id.id})
                 if group_id.move_type != line.order_id.picking_policy:
-                    updated_vals.update({'move_type':
-                                         line.order_id.picking_policy})
+                    updated_vals.update(
+                        {'move_type': line.order_id.picking_policy})
                 if updated_vals:
                     group_id.write(updated_vals)
             line.procurement_group_id = group_id
@@ -92,24 +87,22 @@ class SaleOrderLine(models.Model):
             procurement_uom = line.product_uom
             quant_uom = line.product_id.uom_id
             get_param = self.env['ir.config_parameter'].sudo().get_param
-            if procurement_uom.id != quant_uom.id and \
-                    get_param('stock.propagate_uom') != '1':
-                product_qty = line.product_uom.\
-                    _compute_quantity(product_qty, quant_uom,
-                                      rounding_method='HALF-UP')
+            if procurement_uom.id != quant_uom.id and get_param(
+                    'stock.propagate_uom') != '1':
+                product_qty = line.product_uom._compute_quantity(
+                    product_qty, quant_uom, rounding_method='HALF-UP')
                 procurement_uom = quant_uom
 
             try:
-                self.env['procurement.group'].\
-                    run(line.product_id, product_qty, procurement_uom,
-                        line.order_id.partner_shipping_id.
-                        property_stock_customer, line.name, line.order_id.name,
-                        values)
+                self.env['procurement.group'].run(
+                    line.product_id, product_qty, procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.name, line.order_id.name, values)
             except UserError as error:
                 errors.append(error.name)
         if errors:
             raise UserError('\n'.join(errors))
-        return True
+        return super(SaleOrderLine, self)._action_launch_procurement_rule()
 
     procurement_group_id = fields.Many2one('procurement.group',
                                            'Procurement group', copy=False)
