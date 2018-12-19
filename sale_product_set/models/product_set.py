1a9d4055d99e941d6f3848da74a3b40409642ab6
sbejaoui <souheil.bejaoui@acsone.eu>
2018-12-19 12:53:26 +0100

    [ADD] - Add ref field

--- sale_product_set/models/product_set.py
+++ sale_product_set/models/product_set.py
@@ -1,20 +1,33 @@
 # Copyright 2015 Anybox S.A.S
 # Copyright 2016-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductSet(models.Model):
     _name = 'product.set'
     _description = 'Product set'
 
-    name = fields.Char(
-        string='Name',
-        help='Product set name',
-        required=True
+    name = fields.Char(string='Name', help='Product set name', required=True)
+    ref = fields.Char(
+        string='Internal Reference',
+        help='Product set internal reference',
+        copy=False,
     )
     set_line_ids = fields.One2many(
-        'product.set.line',
-        'product_set_id',
-        string="Products"
+        'product.set.line', 'product_set_id', string="Products"
     )
+
+    @api.multi
+    def name_get(self):
+        return [
+            (
+                product_set.id,
+                '%s%s'
+                % (
+                    product_set.ref and '[%s] ' % product_set.ref or '',
+                    product_set.name,
+                ),
+            )
+            for product_set in self
+        ]
