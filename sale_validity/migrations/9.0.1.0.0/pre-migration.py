99bb84887acff2706d1027764b63d407a7126baa
David Vidal <david.vidal@tecnativa.com>
2018-12-19 17:11:34 +0100

    [9.0] sale_validity: add migration script (#767)

---
+++ sale_validity/migrations/9.0.1.0.0/pre-migration.py
@@ -0,0 +1,13 @@
+# Copyright 2018 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+_field_renames = [
+    ('sale.order', 'sale_order', 'date_validity', 'validity_date'),
+]
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    openupgrade.rename_fields(env, _field_renames)
