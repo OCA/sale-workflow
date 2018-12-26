fc3d8c4b6269a146f15771af1dd19787b06d5408
Raf Ven <raf.ven@dynapps.be>
2018-12-26 16:53:48 +0100

    [MIG] sale_order_price_recalculation: Migration to 12.0

--- sale_order_price_recalculation/__manifest__.py
+++ sale_order_price_recalculation/__manifest__.py
@@ -7,7 +7,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Price recalculation in sales orders",
-    "version": "11.0.1.0.0",
+    "summary": "Recalculate prices / Reset descriptions on sale order lines",
+    "version": "12.0.1.0.0",
     "category": "Sales Management",
     "website": "http://github.com/OCA/sale-workflow",
     "author": "AvanzOSC,"
