3de103acc07a6a22276e86f469e738837afaa6cc
Laurent Bélorgey <lb@lalieutenante.com>
2019-01-06 10:43:37 +0100

    [10.0][MIG] sale_partner_incoterm: migrate to 10.0

--- sale_partner_incoterm/tests/test_sale_partner_incoterm.py
+++ sale_partner_incoterm/tests/test_sale_partner_incoterm.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Opener B.V. (<https://opener.am>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.tests.common import TransactionCase
+# Copyright 2015 Opener B.V. (<https://opener.am>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
 
 
 class TestSalePartnerIncoterm(TransactionCase):
@@ -28,5 +13,7 @@ sales order's onchange """
             [('customer', '=', True)], limit=1)
         incoterm = self.env['stock.incoterms'].search([], limit=1)
         customer.write({'sale_incoterm_id': incoterm.id})
-        res = self.env['sale.order'].onchange_partner_id(customer.id)['value']
-        self.assertEqual(res['incoterm'], incoterm.id)
+        sale_order = self.env['sale.order'].create({
+            'partner_id': customer.id})
+        sale_order.onchange_partner_id()
+        self.assertEqual(sale_order.incoterm, incoterm)
