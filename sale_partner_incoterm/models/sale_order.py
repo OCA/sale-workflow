5cb30be050b64ff0122733968ec2728e0abcc60a
Laurent Bélorgey <lb@lalieutenante.com>
2019-01-06 10:43:37 +0100

    [10.0][MIG] sale_partner_incoterm: migrate to 10.0

--- sale_partner_incoterm/models/sale_order.py
+++ sale_partner_incoterm/models/sale_order.py
@@ -1,32 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Opener B.V. (<https://opener.am>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, api
+# Copyright 2015 Opener B.V. (<https://opener.am>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
 
 
-class Sale(models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.multi
-    def onchange_partner_id(self, partner_id):
-        res = super(Sale, self).onchange_partner_id(partner_id)
-        if partner_id:
-            res['value']['incoterm'] = self.env['res.partner'].browse(
-                partner_id).sale_incoterm_id.id
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        res = super(SaleOrder, self).onchange_partner_id()
+        if not self.partner_id:
+            self.incoterm = False
+            return res
+        self.incoterm = self.partner_id.sale_incoterm_id
         return res
