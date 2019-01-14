3d870be76291b229ff1d3512a85d0b27e766183b
Denis Roussel <denis.roussel@acsone.eu>
2019-01-14 10:45:26 +0100

    [10.0][IMP] sale_product_country_restriction: Use company strategies

--- sale_product_country_restriction/__manifest__.py
+++ sale_product_country_restriction/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Sale Product Country Restriction',
     'summary': """
         Implements product country restrictions on sale workflow""",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/sale-workflow',
