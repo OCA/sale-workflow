23a6b8a2ee68a17b19ac3810d390501367f653c0
Florian da Costa <florian.dacosta@akretion.com>
2019-01-19 00:41:54 +0100

    Migration to version 12.0

--- sale_automatic_workflow_payment_mode/__manifest__.py
+++ sale_automatic_workflow_payment_mode/__manifest__.py
@@ -1,20 +1,22 @@
-# -*- coding: utf-8 -*-
 # © 2016 Camptocamp SA, Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-{'name': 'Sale Automatic Workflow - Payment Mode',
- 'version': '11.0.1.0.0',
- 'author': 'Camptocamp,Sodexis,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Sales Management',
- 'depends': ['sale_automatic_workflow',
-             'account_payment_sale',  # oca/bank-payment
-             ],
- 'website': 'http://www.camptocamp.com',
- 'data': ['data/automatic_workflow_data.xml',
-          'views/account_payment_mode_views.xml',
-          'views/sale_workflow_process_view.xml',
-          ],
- 'installable': True,
- 'auto_install': True,
- }
+{
+    'name': 'Sale Automatic Workflow - Payment Mode',
+    'version': '12.0.1.0.0',
+    'author': 'Camptocamp,Sodexis,Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'category': 'Sales Management',
+    'depends': [
+        'sale_automatic_workflow',
+        'account_payment_sale',  # oca/bank-payment
+    ],
+    'website': 'https://github.com/OCA/sale-workflow',
+    'data': [
+        'data/automatic_workflow_data.xml',
+        'views/account_payment_mode_views.xml',
+        'views/sale_workflow_process_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
