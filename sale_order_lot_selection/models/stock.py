f864c71f2c7f0e5d971a202c957c4f7b5474987d
Paul Catinean <paulcatinean@gmail.com>
2019-01-18 13:30:29 +0100

    [MIG] sale_order_lot_selection: Migration to 12.0

---
+++ sale_order_lot_selection/models/stock.py
@@ -0,0 +1,23 @@
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _update_reserved_quantity(self, need, available_quantity,
+                                  location_id, lot_id=None,
+                                  package_id=None, owner_id=None,
+                                  strict=True):
+        if self._context.get('sol_lot_id'):
+            lot_id = self.sale_line_id.lot_id
+        return super(StockMove, self)._update_reserved_quantity(
+            need, available_quantity, location_id, lot_id=lot_id,
+            package_id=package_id, owner_id=owner_id, strict=strict)
+
+    def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
+        vals = super(StockMove, self)._prepare_move_line_vals(
+            quantity=quantity, reserved_quant=reserved_quant)
+        lot = self.sale_line_id.lot_id
+        if reserved_quant and lot:
+            vals['lot_id'] = lot.id
+        return vals
