cb9391a991d2ae3ed920cbe4cb659222acfd5425
Swapnesh-SerpentCS <36026345+Swapnesh-SerpentCS@users.noreply.github.com>
2019-01-28 15:22:02 +0530

    [FIX]sale_commercial_partner: Make commercial_partner_id readonly

--- sale_commercial_partner/models/sale.py
+++ sale_commercial_partner/models/sale.py
@@ -9,4 +9,4 @@ class SaleOrder(models.Model):
 
     commercial_partner_id = fields.Many2one(
         'res.partner', related='partner_id.commercial_partner_id', store=True,
-        string='Commercial Entity', index=True)
+        string='Commercial Entity', index=True, readonly=True)
