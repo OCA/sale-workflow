c9b2d667b9fa312c805254e274822b971ec272d4
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-02-12 17:42:50 +0100

    [12.0][MIG] migrate sale_double_validation to V12

---
+++ sale_double_validation/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Double validation for Sales',
+    'summary': "",
+    'version': '12.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'sale',
+    'depends': [
+        'sale',
+        'sale_management',
+        'sales_team',
+    ],
+    'website': 'https://github.com/OCA/sale-workflow',
+    'data': [
+        'views/company.xml',
+        'views/sale.xml'
+    ],
+}
---
+++ sale_double_validation/readme/USAGE.rst
@@ -0,0 +1,4 @@
+Once configured sales continue to create Sale orders just as normal.
+If a review is required the SO will be set into the state "To Approve" when approved it will be moved into "Draft".
+
+A sale order in "To approve" state will have a button "Approve" only visible to sales manager.
