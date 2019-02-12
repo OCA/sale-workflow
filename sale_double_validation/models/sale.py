fba127e97fc1eaae623fd30b549fc582cd449b25
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-02-12 17:42:50 +0100

    [12.0][MIG] migrate sale_double_validation to V12

--- sale_double_validation/models/sale.py
+++ sale_double_validation/models/sale.py
@@ -1,44 +1,47 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from openerp import api, fields, models
+from odoo import api, models, _
+from odoo.tools import float_compare
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    state = fields.Selection(
-        selection_add=[('to_approve', 'To Approve')]
-    )
+    @api.model
+    def _setup_fields(self):
+        super(SaleOrder, self)._setup_fields()
+        selection = self._fields['state'].selection
+        exists = False
+        for idx, (state, __) in enumerate(selection):
+            if state == 'to_approve':
+                exists = True
+        if not exists:
+            selection.insert(0, ('to_approve', _('To Approve')))
 
     @api.multi
     def is_amount_to_approve(self):
-        currency = self.env.user.company_id.currency_id
+        self.ensure_one()
+        currency = self.company_id.currency_id
         limit_amount = self.company_id.so_double_validation_amount
         limit_amount = currency.compute(limit_amount, self.currency_id)
-        return self.amount_total >= limit_amount
+        return float_compare(
+            limit_amount, self.amount_total,
+            precision_rounding=self.currency_id.rounding) <= 0
 
     @api.multi
     def is_to_approve(self):
+        self.ensure_one()
         return (self.company_id.so_double_validation == 'two_step' and
                 self.is_amount_to_approve() and
-                not self.user_has_groups('base.group_sale_manager'))
+                not self.user_has_groups('sales_team.group_sale_manager'))
 
-    @api.multi
-    def action_confirm(self):
-        to_approve = self.env['sale.order']
-        to_confirm = self.env['sale.order']
-        for order in self:
-            if order.is_to_approve():
-                to_approve |= order
-            else:
-                to_confirm |= order
-        to_approve.write({'state': 'to_approve'})
-
-        if to_confirm:
-            return super(SaleOrder, to_confirm).action_confirm()
-        return True
+    @api.model
+    def create(self, vals):
+        obj = super().create(vals)
+        if obj.is_to_approve():
+            obj.state = 'to_approve'
+        return obj
 
     @api.multi
     def action_approve(self):
-        return super(SaleOrder, self).action_confirm()
+        self.write({'state': 'draft'})
