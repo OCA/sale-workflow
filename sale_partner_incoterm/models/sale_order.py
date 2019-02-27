cb9f8c5cc091be69aecd69d660e0116a9b39acb0
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-02-27 18:19:17 +0200

    [MIG] sale_partner_incoterm: Migration to 12.0

--- sale_partner_incoterm/models/sale_order.py
+++ sale_partner_incoterm/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Opener B.V. (<https://opener.am>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -11,7 +10,7 @@ class SaleOrder(models.Model):
     @api.multi
     @api.onchange('partner_id')
     def onchange_partner_id(self):
-        res = super(SaleOrder, self).onchange_partner_id()
+        res = super().onchange_partner_id()
         if not self.partner_id:
             self.incoterm = False
             return res
