6b40d9ad7c1e29ad78c68f480a7df132a299a04c
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-02-28 18:05:07 +0100

    [IMP] sale_elaboration: Some improvements

--- sale_elaboration/models/stock_picking.py
+++ sale_elaboration/models/stock_picking.py
@@ -1,7 +1,6 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, models
-from collections import defaultdict
 
 
 class StockPicking(models.Model):
@@ -10,16 +9,17 @@ class StockPicking(models.Model):
     @api.multi
     def action_done(self):
         res = super(StockPicking, self).action_done()
-        elaboration_product = defaultdict(lambda: 0.0)
-        for pick in self.filtered(lambda x: x.picking_type_code == 'outgoing'):
-            elaboration_lines = pick.move_lines.filtered(
-                lambda x: (
-                    x.sale_line_id.elaboration_id and
-                    x.sale_line_id.elaboration_id.product_id
-                ))
-            for line in elaboration_lines:
-                product = line.sale_line_id.elaboration_id.product_id
-                elaboration_product[product] += line.quantity_done
-            for product, qty in elaboration_product.items():
-                pick.sale_id._create_elaboration_line(product, qty)
+        with self.env.norecompute():
+            for pick in self.filtered(
+                lambda x: x.picking_type_code == 'outgoing'
+            ):
+                elaboration_lines = pick.move_lines.filtered(
+                    lambda x: x.sale_line_id.elaboration_id
+                )
+                for line in elaboration_lines:
+                    pick.sale_id._create_elaboration_line(
+                        line.sale_line_id.elaboration_id.product_id,
+                        line.quantity_done,
+                    )
+        self.recompute()
         return res
