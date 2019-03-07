df485649ef551aaf02463fad59ead4955ae8d748
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-03-07 18:11:05 +0100

    [11.0][IMP] sale_order_secondary_unit: add secondary unit to sale report

--- sale_order_secondary_unit/__manifest__.py
+++ sale_order_secondary_unit/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2018-2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
+<<<<<<< HEAD
     "name": "Sale Order Secondary Unit",
     "summary": "Sale product in a secondary unit",
     "version": "18.0.1.0.0",
@@ -18,5 +19,45 @@
         "views/product_views.xml",
         "views/sale_order_views.xml",
         "report/sale_report_templates.xml",
+||||||| parent of f34d90643 ([11.0][IMP] sale_order_secondary_unit: add secondary unit to sale report)
+    'name': 'Sale Order Secondary Unit',
+    'summary': 'Sale product in a secondary unit',
+    'version': '11.0.1.0.2',
+    'development_status': 'Beta',
+    'category': 'Sale',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+    'depends': [
+        'sale',
+        'product_secondary_unit',
+    ],
+    'data': [
+        'views/product_views.xml',
+        'views/sale_order_views.xml',
+=======
+    'name': 'Sale Order Secondary Unit',
+    'summary': 'Sale product in a secondary unit',
+    'version': '11.0.1.0.2',
+    'development_status': 'Beta',
+    'category': 'Sale',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+    'depends': [
+        'sale',
+        'product_secondary_unit',
+    ],
+    'data': [
+        'views/product_views.xml',
+        'views/sale_order_views.xml',
+        'report/sale_report_templates.xml',
+>>>>>>> f34d90643 ([11.0][IMP] sale_order_secondary_unit: add secondary unit to sale report)
     ],
 }
