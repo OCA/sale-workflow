07248efc02fb0bee564ac5fe540a55e657300aae
Kitti U <kittiu@gmail.com>
2019-03-08 18:57:52 +0700

    [ADD][12.0] sale_invoice_plan

---
+++ sale_invoice_plan/wizard/sale_create_invoice_plan.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class SaleCreateInvoicePlan(models.TransientModel):
+    _name = 'sale.create.invoice.plan'
+    _description = 'Fillig invoice planning criteria'
+
+    advance = fields.Boolean(
+        string='Advance on 1st Invoice',
+        default=False,
+    )
+    num_installment = fields.Integer(
+        string='Number of Installment',
+        default=0,
+        required=True,
+    )
+    installment_date = fields.Date(
+        string='Installment Date',
+        default=fields.Date.context_today,
+        required=True,
+    )
+    interval = fields.Integer(
+        string='Interval',
+        default=1,
+        required=True,
+    )
+    interval_type = fields.Selection(
+        [('day', 'Day'),
+         ('month', 'Month'),
+         ('year', 'Year')],
+        string='Interval Type',
+        default='month',
+        required=True,
+    )
+
+    @api.one
+    @api.constrains('num_installment')
+    def _check_num_installment(self):
+        if self.num_installment <= 1:
+            raise ValidationError(_('Number Installment must greater than 1'))
+
+    @api.multi
+    def sale_create_invoice_plan(self):
+        sale = self.env['sale.order'].browse(self._context.get('active_id'))
+        self.ensure_one()
+        sale.create_invoice_plan(self.num_installment,
+                                 self.installment_date,
+                                 self.interval,
+                                 self.interval_type,
+                                 self.advance)
+        return {'type': 'ir.actions.act_window_close'}
