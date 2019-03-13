3227efac5fce9b27d781e2f0d0c9e04fa8a061c5
alvaro <alvaro@domatix.com>
2019-03-13 13:24:02 +0100

    License has been add to the manifest

--- product_template_tag_sale/__manifest__.py
+++ product_template_tag_sale/__manifest__.py
@@ -4,6 +4,7 @@
     "summary": "This addon allows to manage tags of the products from sales",
     "version": "11.0.1.0.0",
     "category": "Product",
+    'license': 'AGPL-3',
     "author": "Domatix, Odoo Community Association (OCA)",
     "depends": ["sale", "product_template_tags"],
     'data': ['views/product_template_tag_sale_views.xml'],
