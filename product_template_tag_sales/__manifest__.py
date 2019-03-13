1ff0f8fbc6dc7bdc616e7678aaea5fdbb9a89947
alvaro <alvaro@domatix.com>
2019-03-13 11:14:48 +0100

    Added product_template_tag_sales module

---
+++ product_template_tag_sales/__manifest__.py
@@ -0,0 +1,11 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Template Tags For Sales",
+    "summary": "This addon allows to manage tags of the products",
+    "version": "11.0.1.0.0",
+    "category": "Product",
+    "author": "Domatix, Odoo Community Association (OCA)",
+    "depends": ["sale", "product_template_tags"],
+    'data': ['views/product_template_tag_sales.xml'],
+    "installable": True
+}
