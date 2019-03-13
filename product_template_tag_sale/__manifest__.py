6e63da5f71f579b2617db12e519569d3cfd52a80
alvaro <alvaro@domatix.com>
2019-03-13 13:16:04 +0100

    Plurals have been removed from product_template_tag_sale

--- product_template_tag_sale/__manifest__.py
+++ product_template_tag_sale/__manifest__.py
@@ -1,11 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Template Tags For Sales",
-    "summary": "This addon allows to manage tags of the products",
+    "summary": "This addon allows to manage tags of the products from sales",
     "version": "11.0.1.0.0",
     "category": "Product",
     "author": "Domatix, Odoo Community Association (OCA)",
     "depends": ["sale", "product_template_tags"],
-    'data': ['views/product_template_tag_sales.xml'],
+    'data': ['views/product_template_tag_sale_views.xml'],
     "installable": True
 }
