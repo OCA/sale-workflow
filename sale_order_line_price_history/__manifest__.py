ce79edf44436acee39a1a9cc14123e1cc6cb79b7
ernesto <ernesto.tejeda87@gmail.com>
2019-03-19 12:59:44 -0400

    sale_order_line_price_history: add price history in sale order line
    App price history of a product from a sale order line and set one of
    this old prices in the sale order line.

---
+++ sale_order_line_price_history/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale order line price history",
+    "version": "11.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow/",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "wizards/sale_orderLine_price_history.xml",
+        "views/sale_views.xml",
+    ],
+    "installable": True,
+}
