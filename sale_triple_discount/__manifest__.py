edb51ecd81e7219ea374644e12dac2f5e2db7391
Simone Rubino <simone.rubino@agilebg.com>
2019-03-22 16:46:56 +0100

    Increase module version

--- sale_triple_discount/__manifest__.py
+++ sale_triple_discount/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Sale Triple Discount',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'category': 'Sales Management',
     'author': 'ADHOC SA, '
               'Agile Business Group, '
