a8f91be2c11677c23b67d445906c3c6085acaaac
Danh Vo <danh@trobz.com>
2019-03-29 10:37:03 +0700

    sale_rental: Migration to 11.0

--- sale_rental/models/stock.py
+++ sale_rental/models/stock.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # Copyright 2016 Sodexis (http://sodexis.com)
@@ -26,13 +25,22 @@ class StockWarehouse(models.Model):
     sell_rented_product_route_id = fields.Many2one(
         'stock.location.route', string='Sell Rented Product Route')
 
+    @api.onchange('rental_allowed')
+    def _onchange_rrental_allowed(self):
+        if not self.rental_allowed:
+            self.rental_view_location_id = False
+            self.rental_in_location_id = False
+            self.rental_out_location_id = False
+            self.rental_route_id = False
+            self.sell_rented_product_route_id = False
+
     @api.multi
     def _get_rental_push_pull_rules(self):
         self.ensure_one()
         route_obj = self.env['stock.location.route']
         try:
             rental_route = self.env.ref('sale_rental.route_warehouse0_rental')
-        except:
+        except Exception:
             rental_routes = route_obj.search([('name', '=', _('Rent'))])
             rental_route = rental_routes and rental_routes[0] or False
         if not rental_route:
@@ -40,7 +48,7 @@ class StockWarehouse(models.Model):
         try:
             sell_rented_product_route = self.env.ref(
                 'sale_rental.route_warehouse0_sell_rented_product')
-        except:
+        except Exception:
             sell_rented_product_routes = route_obj.search(
                 [('name', '=', _('Sell Rented Product'))])
             sell_rented_product_route =\
@@ -49,7 +57,6 @@ class StockWarehouse(models.Model):
         if not sell_rented_product_route:
             raise UserError(_(
                 "Can't find any generic 'Sell Rented Product' route."))
-
         if not self.rental_in_location_id:
             raise UserError(_(
                 "The Rental Input stock location is not set on the "
@@ -59,9 +66,9 @@ class StockWarehouse(models.Model):
                 "The Rental Output stock location is not set on the "
                 "warehouse %s") % self.name)
         rental_pull_rule = {
-            'name': self.env['stock.warehouse']._format_rulename(
-                self, self.rental_in_location_id,
-                self.rental_out_location_id),
+            'name': self._format_rulename(
+                self.rental_in_location_id,
+                self.rental_out_location_id, ''),
             'location_id': self.rental_out_location_id.id,
             'location_src_id': self.rental_in_location_id.id,
             'route_id': rental_route.id,
@@ -70,32 +77,32 @@ class StockWarehouse(models.Model):
             'warehouse_id': self.id,
         }
         rental_push_rule = {
-            'name': self.env['stock.warehouse']._format_rulename(
-                self, self.rental_out_location_id,
-                self.rental_in_location_id),
+            'name': self._format_rulename(
+                self.rental_out_location_id,
+                self.rental_in_location_id, ''),
             'location_from_id': self.rental_out_location_id.id,
             'location_dest_id': self.rental_in_location_id.id,
             'route_id': rental_route.id,
             'picking_type_id': self.in_type_id.id,
             'warehouse_id': self.id,
-            }
+        }
         customer_loc = self.env.ref('stock.stock_location_customers')
         sell_rented_product_pull_rule = {
-            'name': self.env['stock.warehouse']._format_rulename(
-                self, self.rental_out_location_id, customer_loc),
+            'name': self._format_rulename(
+                self.rental_out_location_id, customer_loc, ''),
             'location_id': customer_loc.id,
             'location_src_id': self.rental_out_location_id.id,
             'route_id': sell_rented_product_route.id,
             'action': 'move',
             'picking_type_id': self.out_type_id.id,
             'warehouse_id': self.id,
-            }
+        }
         res = {
             'procurement.rule': [
                 rental_pull_rule,
                 sell_rented_product_pull_rule],
             'stock.location.path': [rental_push_rule],
-            }
+        }
         return res
 
     @api.multi
@@ -109,42 +116,59 @@ class StockWarehouse(models.Model):
                     # create stock locations
                     slo = self.env['stock.location']
                     if not warehouse.rental_view_location_id:
-                        view_loc = slo.create({
-                            'name': _('Rental'),
-                            'location_id': self.view_location_id.id,
-                            'usage': 'view',
+                        view_loc = slo.search(
+                            [('name', '=', 'Rental'),
+                             ('location_id', '=', self.view_location_id.id),
+                             ('usage', '=', 'view')], limit=1)
+                        if not view_loc:
+                            view_loc = slo.create({
+                                'name': _('Rental'),
+                                'location_id': self.view_location_id.id,
+                                'usage': 'view',
                             })
-                        logger.debug(
-                            'New view rental stock location created ID %d',
-                            view_loc.id)
+                            logger.debug(
+                                'New view rental stock location created ID %d',
+                                view_loc.id)
                         warehouse.rental_view_location_id = view_loc.id
                     if not warehouse.rental_in_location_id:
-                        in_loc = slo.create({
-                            'name': _('Rental In'),
-                            'location_id':
-                            warehouse.rental_view_location_id.id,
+                        in_loc = slo.search(
+                            [('name', '=', 'Rental In'),
+                             ('location_id', '=',
+                                warehouse.rental_view_location_id.id)],
+                            limit=1)
+                        if not in_loc:
+                            in_loc = slo.create({
+                                'name': _('Rental In'),
+                                'location_id':
+                                warehouse.rental_view_location_id.id,
                             })
-                        logger.debug(
-                            'New in rental stock location created ID %d',
-                            in_loc.id)
+                            logger.debug(
+                                'New in rental stock location created ID %d',
+                                in_loc.id)
                         warehouse.rental_in_location_id = in_loc.id
                     if not warehouse.rental_out_location_id:
-                        out_loc = slo.create({
-                            'name': _('Rental Out'),
-                            'location_id':
-                            warehouse.rental_view_location_id.id,
+                        out_loc = slo.search(
+                            [('name', '=', 'Rental Out'),
+                             ('location_id', '=',
+                                warehouse.rental_view_location_id.id)],
+                            limit=1)
+                        if not out_loc:
+                            out_loc = slo.create({
+                                'name': _('Rental Out'),
+                                'location_id':
+                                warehouse.rental_view_location_id.id,
                             })
-                        logger.debug(
-                            'New out rental stock location created ID %d',
-                            out_loc.id)
+                            logger.debug(
+                                'New out rental stock location created ID %d',
+                                out_loc.id)
                         warehouse.rental_out_location_id = out_loc.id
                     warehouse.write({
                         'route_ids': [(4, rental_route.id)],
                         'rental_route_id': rental_route.id,
                         'sell_rented_product_route_id': sell_rented_route.id,
-                        })
+                    })
                     for obj, rules_list in\
-                            self._get_rental_push_pull_rules().iteritems():
+                            self._get_rental_push_pull_rules().items():
                         for rule in rules_list:
                             self.env[obj].create(rule)
             else:
@@ -171,26 +195,28 @@ class StockWarehouse(models.Model):
                         'route_ids': [(3, rental_route.id)],
                         'rental_route_id': False,
                         'sell_rented_product_route_id': False,
-                        })
+                    })
         return super(StockWarehouse, self).write(vals)
 
 
 class StockLocationPath(models.Model):
     _inherit = 'stock.location.path'
 
-    @api.model
-    def _prepare_push_apply(self, rule, move):
-        '''Inherit to write the end date of the rental on the return move'''
-        vals = super(StockLocationPath, self)._prepare_push_apply(rule, move)
-        if (
-                move.procurement_id and
-                move.procurement_id.location_id ==
-                move.procurement_id.warehouse_id.rental_out_location_id and
-                move.procurement_id.sale_line_id and
-                move.procurement_id.sale_line_id.rental_type == 'new_rental'):
-            rental_end_date = move.procurement_id.sale_line_id.end_date
-            vals['date'] = vals['date_expected'] = rental_end_date
-        return vals
+    def _prepare_move_copy_values(self, move_to_copy, new_date):
+        """Inherit to write the end date of the rental on the return move"""
+        res = super(StockLocationPath, self)._prepare_move_copy_values(
+            move_to_copy, new_date)
+        location_id = res.get('location_id', False)
+        if location_id and\
+            location_id ==\
+            move_to_copy.warehouse_id.rental_out_location_id.id and\
+                move_to_copy.sale_line_id and\
+                move_to_copy.sale_line_id.rental_type == 'new_rental':
+            rental_end_date = move_to_copy.sale_line_id.end_date
+            res.update({
+                'date_expected': rental_end_date
+            })
+        return res
 
 
 class StockInventory(models.Model):
@@ -205,7 +231,7 @@ class StockInventory(models.Model):
             ('product.product_product_6', 56),
             ('product.product_product_8', 46),
             ('product.product_product_25', 2),
-            ]
+        ]
         for (product_xmlid, qty) in products:
             product = self.env.ref(product_xmlid)
             silo.create({
---
+++ sale_rental/readme/ROADMAP.rst
@@ -0,0 +1,5 @@
+This module has the following limitations:
+
+* No support for planning/agenda of the rented products
+* The unit of measure of the rental services must be *Day* (the rental per hours / per week / per month is not supported for the moment)
+* In case of selling rented product, the incoming shipment should be created manually
