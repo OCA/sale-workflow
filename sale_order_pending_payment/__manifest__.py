d73ca8a7c16dfd4ca2879424cadf4dec7891a0eb
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2019-04-05 01:49:11 +0200

    [ADD] TH190404/0024 sale_order_pending_payment: Show pending payment in sale orders

---
+++ sale_order_pending_payment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Pending Payment",
+    "version": "10.0.1.0.0",
+    "author": "PlanetaTIC",
+    "website": "https://www.planetatic.com",
+    "license": "AGPL-3",
+    "category": "Sales Management",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        'report/sale_report_templates.xml',
+        "views/sale_views.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_order_pending_payment/models/sale.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.depends('order_line.price_total', 'order_line.price_unit')
+    def _amount_deposit(self):
+        for order in self:
+            self.amount_tax = amount_deposit = 0.0
+            for line in order.order_line:
+                amount_deposit += line.price_unit*line.qty_invoiced if \
+                    line.product_id.id == self.env['ir.values'].get_default(
+                            'sale.config.settings',
+                            'deposit_product_id_setting') else 0
+            order.update({
+                'amount_deposit': amount_deposit,
+                'amount_total_pending': self.amount_total - amount_deposit,
+            })
+
+    amount_deposit = fields.Monetary(string='Deposit', readonly=True,
+                                     compute='_amount_deposit',
+                                     track_visibility='always')
+    amount_total_pending = fields.Monetary(string='Total Pending',
+                                           readonly=True,
+                                           compute='_amount_deposit',
+                                           track_visibility='always')
