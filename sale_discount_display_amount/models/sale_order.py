8722e9a6fb0ce204ee66225a6ecc8c4b2f048395
Abraham Anes <abrahamanes@gmail.com>
2019-04-04 20:57:19 +0200

    [12.0][MIG] sale_discount_display_amount

--- sale_discount_display_amount/models/sale_order.py
+++ sale_discount_display_amount/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -12,15 +11,16 @@ class SaleOrder(models.Model):
     discount_total = fields.Monetary(
         compute='_compute_discount',
         string='Discount Subtotal',
-        readonly=True,
+        currency_field='currency_id',
         store=True)
     price_total_no_discount = fields.Monetary(
         compute='_compute_discount',
         string='Subtotal Without Discount',
-        readonly=True,
+        currency_field='currency_id',
         store=True)
 
-    @api.depends('order_line.discount_total', 'order_line.discount_total')
+    @api.depends('order_line.discount_total',
+                 'order_line.price_total_no_discount')
     def _compute_discount(self):
         for order in self:
             discount_total = sum(order.order_line.mapped('discount_total'))
