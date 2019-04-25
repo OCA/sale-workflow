1a0231f71f4c6ffaab0ec3e4d175a65117bd36d8
sebalix <seb@usr-src.org>
2019-04-25 17:26:45 +0200

    [MIG] sale_exception: Migration to 12.0

--- sale_exception/tests/__init__.py
+++ sale_exception/tests/__init__.py
@@ -1,4 +1,2 @@
-# (c) 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from . import test_sale_exception
