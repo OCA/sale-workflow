6dff4f7e05198dae47b80b9969b4fecfac82eae8
Raf Ven <raf.ven@dynapps.be>
2019-05-06 09:05:44 +0200

    [MIG] sale_order_revision: Migration to 12.0

--- sale_order_revision/__init__.py
+++ sale_order_revision/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
-# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
-# Copyright 2018 Dreambits Technologies Pvt. Ltd. (<http://dreambits.in>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
 
