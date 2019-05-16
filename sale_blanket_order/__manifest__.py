7006e8a2e763a402142dfc8000b0604f698d5c5d
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-05-16 09:51:16 +0200

    [11.0][MIG] Migrate module sale_blanket_order

--- sale_blanket_order/__manifest__.py
+++ sale_blanket_order/__manifest__.py
@@ -1,12 +1,11 @@
-# coding: utf-8
 # Copyright 2018 Acsone
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Blanket Orders',
+    'name': 'Sale Blanket Orders',
     'category': 'Sale',
     'license': 'AGPL-3',
-    'author': 'Acsone SA/NV,Odoo Community Association (OCA)',
-    'version': '10.0.1.0.0',
+    'author': 'Acsone SA/NV, Odoo Community Association (OCA)',
+    'version': '11.0.1.0.0',
     'website': 'https://github.com/OCA/sale-workflow',
     'summary': "Blanket Orders",
     'depends': [
@@ -14,14 +13,14 @@
         'web_action_conditionable',
     ],
     'data': [
-        'views/sale_config_settings.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'data/sequence.xml',
         'data/ir_cron.xml',
-        'security/ir.model.access.csv',
-        'security/security.xml',
         'wizard/create_sale_orders.xml',
-        'views/blanket_orders.xml',
-        'views/sale_orders.xml',
+        'views/sale_config_settings.xml',
+        'views/sale_blanket_order_views.xml',
+        'views/sale_order_views.xml',
         'report/templates.xml',
         'report/report.xml',
     ],
