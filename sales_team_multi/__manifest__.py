e70294bc7a4c9a88db87abc2594c608ba1a3590b
Felipe Enrique Garcia Guitron <felipe.garcia@berrysoft.com>
2019-05-28 13:04:57 -0500

    Models

--- sales_team_multi/__manifest__.py
+++ sales_team_multi/__manifest__.py
@@ -12,7 +12,6 @@
     'website': "http://www.berrysoft.mx",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sales',
     'version': '12.0.1.0.0',
