9c5420aa349c70fe1f0892d11a8fa96c336c41c6
Florian da Costa <florian.dacosta@akretion.com>
2019-05-28 18:20:55 +0200

    Adapt sale_exception to base_exception simplification

---
+++ sale_exception/models/sale_order_line.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © 2019 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = ['sale.order.line', 'base.exception.method']
+    _name = 'sale.order.line'
+
+    ignore_exception = fields.Boolean(
+        related='order_id.ignore_exception',
+        store=True,
+        string="Ignore Exceptions")
+
+    @api.model
+    def _reverse_field(self):
+        return 'sale_ids'
+
+    @api.multi
+    def _detect_exceptions(self, rule):
+        records = super(SaleOrderLine, self)._detect_exceptions(rule)
+        return records.mapped('order_id')
+
+    @api.model
+    def _exception_rule_eval_context(self, rec):
+        # We keep this only for backward compatibility, because some existing
+        # rules may use the variable "sale_line". But we should remove this
+        # code during v13 migration. The record is already available in obj and
+        # object variables and it is more than enough.
+        res = super(SaleOrderLine, self)._exception_rule_eval_context(rec)
+        res['sale_line'] = rec
+        return res
