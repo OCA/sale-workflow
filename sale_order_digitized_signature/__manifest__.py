d3ab47fcd81fc6ce5f8d89d0b8f2ad76f632c5de
cubells <vicent@vcubells.net>
2019-05-28 12:22:29 +0200

    [MIG] sale_order_digitized_signature: Migration to 11.0

---
+++ sale_order_digitized_signature/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Digitized Signature",
+    "version": "11.0.1.0.0",
+    "author": "Tecnativa, "
+               "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "web_widget_digitized_signature",
+    ],
+    "data": [
+        "report/report_saleorder.xml",
+        "views/sale_views.xml",
+    ],
+    "installable": True,
+}
