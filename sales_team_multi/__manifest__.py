0a28aeee3098e6f14c06c5116524447c0d39ddad
FelipeEnriqueGG <isc_garcia.f@tesvb.edu.mx>
2019-05-29 08:56:36 -0500

    no comments

--- sales_team_multi/__manifest__.py
+++ sales_team_multi/__manifest__.py
@@ -1,18 +1,11 @@
 {
     'name': "Multiple sales teams",
-
     'summary': """
         it allows to add a user to multiple teams or sales channels.""",
-
     'author': "BerrySoft, Odoo Community Association (OCA)",
     'website': "https://github.com/oca",
-
-    # Categories can be used to filter modules in modules listing
-    # for the full list
     'category': 'Sales',
     "license": "AGPL-3",
     'version': '12.0.1.0.0',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'sales_team'],
 }
