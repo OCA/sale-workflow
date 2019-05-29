6f6c33e1988c62508805807d09537bb084a90fd4
FelipeEnriqueGG <isc_garcia.f@tesvb.edu.mx>
2019-05-29 09:05:03 -0500

    fixed website

--- sales_team_multi/__manifest__.py
+++ sales_team_multi/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': """
         it allows to add a user to multiple teams or sales channels.""",
     'author': "BerrySoft, Odoo Community Association (OCA)",
-    'website': "https://github.com/oca",
+    'website': "https://github.com/OCA/sale-workflow",
     'category': 'Sales',
     "license": "AGPL-3",
     'version': '12.0.1.0.0',
