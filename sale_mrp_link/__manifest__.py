dcfbdcf3322cdc414ce6ac363c5f933dfd9a8e19
humanesj <juan.humanes@guadaltech.es>
2019-06-03 14:14:47 +0200

    [MIG] Migration of sale_mrp_link to 12.0

--- sale_mrp_link/__manifest__.py
+++ sale_mrp_link/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Alex Comba - Agile Business Group
 # Copyright 2016-2018 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'Sale MRP Link',
     'summary': 'Show manufacturing orders generated from sales order',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'development_status': 'Production/Stable',
     'category': 'Sales Management',
     'website': 'https://github.com/OCA/sale-workflow',
