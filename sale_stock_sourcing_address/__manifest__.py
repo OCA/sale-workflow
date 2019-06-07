a99384fbbc1d052bb385f9751d9aaea32985b387
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-06-07 13:52:15 +0200

    [11.0][add][sale_stock_sourcing_address]

---
+++ sale_stock_sourcing_address/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Sale Stock Sourcing Address",
+    "version": "11.0.1.0.0",
+    "author": "Eficent,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow/",
+    "category": "Sales Management",
+    "license": "LGPL-3",
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "security/security.xml",
+        "views/sale_order_view.xml",
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_stock_sourcing_address/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module computes the sourcing address in sales order lines and customer
+invoice lines. The address is obtained in the sales order, calculating the
+potential procurement rules that would be required for each line.
