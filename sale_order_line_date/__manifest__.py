fa073997facfb655ef85c08b89ba2279cb402175
Open Net Sàrl <info@open-net.ch>
2019-06-13 13:13:27 +0200

    Update sale_order_line_date/__manifest__.py
    Changed version in __manifest__.py
    
    Co-Authored-By: Denis Roussel (ACSONE) <rousseldenis@users.noreply.github.com>

--- sale_order_line_date/__manifest__.py
+++ sale_order_line_date/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Sale Order Line Date",
-    "version": "12.0.0.0.0",
+    "version": "12.0.1.0.0",
     "author": "OdooMRP team,"
               "AvanzOSC,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
