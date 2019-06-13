09cfd47851d5ba34062867f04ef019ec62bbc02a
Open-Net Sàrl <jae@open-net.ch>
2019-06-13 11:39:25 +0200

    [MIG] sale_order_line_date: Migration to 12.0

--- sale_order_line_date/models/sale_order.py
+++ sale_order_line_date/models/sale_order.py
@@ -12,19 +12,18 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.multi
-    @api.onchange('requested_date')
-    def onchange_requested_date(self):
-        """Warn if the requested dates is sooner than the commitment date"""
-        result = super(SaleOrder, self).onchange_requested_date()
+    @api.onchange('commitment_date')
+    def _onchange_commitment_date(self):
+        """Warn if the commitment dates is sooner than the commitment date"""
+        result = super(SaleOrder, self)._onchange_commitment_date()
         if not result:
             result = {}
         if not self:
             return result
         self.ensure_one()
         if 'warning' not in result:
-            lines = []
-            for line in self.order_line:
-                lines.append((1, line.id, {'requested_date':
-                                           self.requested_date}))
-            result['value'] = {'order_line': lines}
+            result['value'] = {'order_line': [
+                (1, line.id, {'commitment_date': self.commitment_date})
+                for line in self.order_line
+            ]}
         return result
