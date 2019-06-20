055dc9dae77bb2b3836466f9da03af1a024ec345
David <david.vidal@tecnativa.com>
2019-06-20 18:07:03 +0200

    [MIG] portal_sale_personal_data_only: Migration to 12.0

--- portal_sale_personal_data_only/hooks.py
+++ portal_sale_personal_data_only/hooks.py
@@ -6,16 +6,16 @@ from odoo import api, SUPERUSER_ID
 def post_init_hook(cr, registry, vals=None):
     """Archive the ir.rules we want to override"""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env.ref('portal_sale.portal_sale_order_user_rule').active = False
-    env.ref('portal_sale.portal_sale_order_line_rule').active = False
-    env.ref('portal_sale.portal_account_invoice_user_rule').active = False
-    env.ref('portal_sale.portal_account_invoice_line_rule').active = False
+    env.ref('sale.sale_order_rule_portal').active = False
+    env.ref('sale.sale_order_line_rule_portal').active = False
+    env.ref('account.account_invoice_rule_portal').active = False
+    env.ref('account.account_invoice_line_rule_portal').active = False
 
 
 def uninstall_hook(cr, registry, vals=None):
     """Unarchive the overriden ir.rules"""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env.ref('portal_sale.portal_sale_order_user_rule').active = True
-    env.ref('portal_sale.portal_sale_order_line_rule').active = True
-    env.ref('portal_sale.portal_account_invoice_user_rule').active = True
-    env.ref('portal_sale.portal_account_invoice_line_rule').active = True
+    env.ref('sale.sale_order_rule_portal').active = True
+    env.ref('sale.sale_order_line_rule_portal').active = True
+    env.ref('account.account_invoice_rule_portal').active = True
+    env.ref('account.account_invoice_line_rule_portal').active = True
