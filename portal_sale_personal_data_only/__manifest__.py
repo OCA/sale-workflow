2ed2a8f02bcc2f04c963f8e6233ca64526bd15c9
David <david.vidal@tecnativa.com>
2019-06-20 18:07:03 +0200

    [MIG] portal_sale_personal_data_only: Migration to 12.0

--- portal_sale_personal_data_only/__manifest__.py
+++ portal_sale_personal_data_only/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Tecnativa S.L. - David Vidal
+# Copyright 2018-19 Tecnativa S.L. - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Portal Sale Personal Data Only',
     'description': 'Allow portal users to see their own documents',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Sale',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'website': 'https://www.github.com/OCA/sale-workflow',
     "license": "AGPL-3",
     'depends': [
-        'portal_sale',
+        'sale',
     ],
     'data': [
         'data/portal_sale_security.xml',
