af31f1f2e7650840a509515eb5eec8d3135d4b05
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-25 12:16:28 +0200

    [12.0][MIG] sale_merge_draft_invoice

--- sale_merge_draft_invoice/__manifest__.py
+++ sale_merge_draft_invoice/__manifest__.py
@@ -4,15 +4,15 @@
 {
     "name": "Sale Merge Draft Invoice",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Sale Workflow",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": [
         'sale_order_action_invoice_create_hook',
     ],
     "data": [
-        'wizard/sale_make_invoice_advance_views.xml',
         'security/sale_merge_draft_invoice_security.xml',
+        'wizard/sale_make_invoice_advance_views.xml',
         'view/res_config_settings_views.xml',
     ],
     "license": 'LGPL-3',
