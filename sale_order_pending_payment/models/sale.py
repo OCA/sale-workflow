dd969286c1f456cd5b16ca556654ba5f0b79c2e8
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2019-07-01 17:07:20 +0200

    [FIX] TH190404/0024 sale_order_pending_payment: Fix calculate function

--- sale_order_pending_payment/models/sale.py
+++ sale_order_pending_payment/models/sale.py
@@ -11,20 +11,41 @@ class SaleOrder(models.Model):
     @api.depends('order_line.price_total', 'order_line.price_unit')
     def _amount_deposit(self):
         for order in self:
-            order.amount_tax = amount_deposit = 0.0
+            order.amount_tax = amount_deposit = amount_invoiced = \
+                amount_residual = 0.0
             for line in order.order_line:
-                amount_deposit += line.price_unit*line.qty_invoiced if \
-                    line.product_id.id == order.env['ir.values'].get_default(
+                if line.product_id.id == order.env['ir.values'].get_default(
                             'sale.config.settings',
-                            'deposit_product_id_setting') else 0
+                            'deposit_product_id_setting') and \
+                        line.product_uom_qty == 0:
+                    for invoice_line in line.invoice_lines:
+                        if invoice_line.invoice_id.residual == 0 and \
+                            invoice_line.invoice_id.state not in \
+                                ('draft', 'cancel'):
+                            amount_deposit += \
+                                line.price_unit * line.qty_invoiced
+            for invoice in order.invoice_ids:
+                if invoice.state not in ('draft', 'cancel'):
+                    amount_invoiced += invoice.amount_total
+                    amount_residual += invoice.residual
+            if amount_invoiced == order.amount_total:
+                total_pending = amount_residual
+            else:
+                total_pending = \
+                    order.amount_total - (amount_invoiced - amount_residual)
             order.update({
                 'amount_deposit': amount_deposit,
-                'amount_total_pending': order.amount_total - amount_deposit,
+                'amount_paid':
+                    amount_invoiced - amount_residual - amount_deposit,
+                'amount_total_pending': total_pending,
             })
 
     amount_deposit = fields.Monetary(string='Deposit', readonly=True,
                                      compute='_amount_deposit',
                                      track_visibility='always')
+    amount_paid = fields.Monetary(string='Paid', readonly=True,
+                                  compute='_amount_deposit',
+                                  track_visibility='always')
     amount_total_pending = fields.Monetary(string='Total Pending',
                                            readonly=True,
                                            compute='_amount_deposit',
