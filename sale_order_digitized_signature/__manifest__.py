14ec9496a41a0e0cb84a0d3da013e3b73a8dc0c4
Mayank Gosai <31255575+mgosai@users.noreply.github.com>
2019-07-03 09:15:58 -0400

    Update manifest
    Removed Duplication

--- sale_order_digitized_signature/__manifest__.py
+++ sale_order_digitized_signature/__manifest__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
