174ed2e2da92eafa84698a9a2dc0742baa217086
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-07-03 15:25:39 +0200

    [11.0][MIG] sale_product_multi_add

--- sale_product_multi_add/__manifest__.py
+++ sale_product_multi_add/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Cédric Pigeon, ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -8,7 +7,7 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Sale Management',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'sale',
