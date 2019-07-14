0bcecbf381ae5fec7d8abb4d5ab60ad69f6f321c
Alexis de Lattre <alexis.delattre@akretion.com>
2019-07-15 01:29:07 +0200

    [MIG] sale_start_end_dates and sale_rental to v12
    Convert readme to new subdir format

--- sale_rental/hooks.py
+++ sale_rental/hooks.py
@@ -1,14 +1,13 @@
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
 
-# DONE
-def post_init_hook(cr, registry):
+def add_to_group_stock_packaging(cr, registry):
     env = Environment(cr, SUPERUSER_ID, {})
-    users = env['res.users'].search(
-        ['|', ('active', '=', True), ('active', '=', False)])
-    users.write({
-        'groups_id': [(4, env.ref('product.group_stock_packaging').id)]})
+    conf_page = env['res.config.settings'].create({})
+    conf_page.group_stock_packaging = True
+    conf_page.execute()
