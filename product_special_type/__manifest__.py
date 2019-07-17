0212d9e53c289694d842c881560e42968509c6ec
Hiren Dangar <hiren.dangar.serpentcs@gmail.com>
2019-07-17 16:00:16 +0530

    [MIG]product_special_type: Migration to 11.0

--- product_special_type/__manifest__.py
+++ product_special_type/__manifest__.py
@@ -1,27 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#
-#    Author: Guewen Baconnier
-#    Copyright 2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2012 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Product Special Types",
-    "version": "1.0",
+    "version": "11.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Sales",
@@ -39,12 +21,8 @@ These fields can be used on reports to display the amounts for
 discounts / advances / fees separately.
 
 """,
-    "website": "http://camptocamp.com",
-    "depends": ['base',
-                'product', ],
-    "init_xml": [],
-    "demo_xml": [],
-    "update_xml": ['product_view.xml'],
-    "active": False,
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ['product'],
+    "data": 'views/product_view.xml',
     'installable': False
 }
---
+++ product_special_type/models/product.py
@@ -0,0 +1,18 @@
+# Copyright 2012 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class product_template(models.Model):
+
+    _inherit = 'product.template'
+
+	special_type = fields.selection([
+		('discount', 'Global Discount'),
+      	('advance', 'Advance'),
+		('delivery', 'Delivery Costs')],
+		string='Special Type',
+		help="""Special products will not be displayed on invoices printed
+		reports but will be summed in the totals.""")
+
