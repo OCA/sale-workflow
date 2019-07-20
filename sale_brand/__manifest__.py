f2f71fb6f1828a0bff4aaa6c607396d43024bbb6
scampbell <scampbell@opensourceintegrators.com>
2019-07-20 13:38:56 -0700

    [ADD] sale_brand

---
+++ sale_brand/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Brand",
+    "summary": "Send branded sales orders",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        'account_brand',
+        'sale',
+    ],
+    "data": [
+        "views/sale_views.xml",
+        "views/report_template.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["osi-scampbell"],
+}
---
+++ sale_brand/readme/CONFIGURE.rst
@@ -0,0 +1,2 @@
+To configure this module, please refer to the documentation of
+`partner_brand <https://github.com/OCA/partner-contact/blob/12.0/partner_brand/README.rst>`_.
