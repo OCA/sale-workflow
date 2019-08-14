fb75e69eb27ef624a370969c42f45c8d79d61a4f
sbejaoui <souheil.bejaoui@acsone.eu>
2019-08-14 17:39:14 +0200

    [FIX] - set brand for down payment invoices

---
+++ sale_brand/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super()._create_invoice(order, so_line, amount)
+        invoice.brand_id = order.brand_id
+        invoice._onchange_partner_id()
+        return invoice
