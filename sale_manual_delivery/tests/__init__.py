e57fecc6c73ebf45e805fefe4c923df06c0d6f62
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-08-20 11:04:30 +0200

    [MIG][11] rm coding* + python 2to3
    [MIG][11] Update Readme + Manifest
    
    [MIG][11] Adapt procurement for V11 + rm api.one

--- sale_manual_delivery/tests/__init__.py
+++ sale_manual_delivery/tests/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 
 import test_manual_delivery
--- sale_manual_delivery/wizard/manual_proc.py
+++ sale_manual_delivery/wizard/manual_proc.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -32,7 +31,7 @@ class ManualDelivery(models.TransientModel):
 
             self.partner_id = self.order_id.partner_id
 
-    date_planned = fields.Date(
+    date_planned = fields.Datetime(
         string='Date Planned'
     )
     order_id = fields.Many2one(
@@ -69,7 +68,6 @@ class ManualDelivery(models.TransientModel):
 
     @api.multi
     def record_picking(self):
-        proc_order_obj = self.env['procurement.order']
         proc_group_obj = self.env['procurement.group']
         for wizard in self:
             carrier_id = wizard.carrier_id if wizard.carrier_id else \
@@ -77,9 +75,13 @@ class ManualDelivery(models.TransientModel):
             date_planned = wizard.date_planned
             order = wizard.order_id
             if not order.procurement_group_id:
-                vals = order._prepare_procurement_group()
+                vals = {
+                    'name': order.name,
+                    'move_type': order.picking_policy,
+                    'sale_id': order.id,
+                    'partner_id': order.partner_shipping_id.id,
+                }
                 order.procurement_group_id = proc_group_obj.create(vals)
-            new_procs = proc_order_obj
 
             for line in wizard.line_ids:
                 if line.to_ship_qty > line.ordered_qty - line.existing_qty:
@@ -87,18 +89,17 @@ class ManualDelivery(models.TransientModel):
                     'You can not deliver more than the remaining quantity. If\
                     you need to do so, please edit the sale order first.'))
                 if float_compare(line.to_ship_qty, 0, 2):
-                    vals = line.order_line_id._prepare_order_line_procurement(
-                        group_id=order.procurement_group_id.id)
+                    vals = line.order_line_id._prepare_procurement_values(
+                        group_id=order.procurement_group_id)
                     vals['date_planned'] = date_planned
-                    vals['product_qty'] = line.to_ship_qty
                     vals['carrier_id'] = carrier_id.id
                     vals['partner_dest_id'] = wizard.partner_id.id
-                    new_proc = proc_order_obj.with_context(
-                        {'manual_delivery': True}).create(vals)
-                    new_proc.message_post_with_view(
-                        'mail.message_origin_link',
-                        values={'self': new_proc, 'origin': order},
-                        subtype_id=self.env.ref('mail.mt_note').id
+                    proc_group_obj.run(
+                        line.order_line_id.product_id,
+                        line.to_ship_qty,
+                        line.order_line_id.product_uom,
+                        line.order_line_id.order_id.partner_shipping_id.property_stock_customer,
+                        line.order_line_id.name,
+                        line.order_line_id.order_id.name,
+                        vals
                     )
-                    new_procs |= new_proc
-            new_procs.run()
