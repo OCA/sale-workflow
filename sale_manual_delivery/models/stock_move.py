3293281c86fccf0afac5a23067c32351c8672c46
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-08-21 15:32:48 +0200

    [MIG][11] Run black to fix lint

--- sale_manual_delivery/models/stock_move.py
+++ sale_manual_delivery/models/stock_move.py
@@ -7,40 +7,47 @@ from odoo import models, fields, api
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    carrier_id = fields.Many2one(
-        "delivery.carrier",
-        string="Delivery Method",
-    )
+    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method")
 
     def _get_new_picking_values(self):
         res = super(StockMove, self)._get_new_picking_values()
         if self.carrier_id:
-            res['carrier_id'] = self.carrier_id.id
+            res["carrier_id"] = self.carrier_id.id
         return res
 
     @api.multi
     def assign_picking(self):
         # Redefine method to filter on date_expected (allows to group moves
         # from the same manual delivery in one delivery)
-        if 'manual_delivery' not in self.env.context:
+        if "manual_delivery" not in self.env.context:
             return super(StockMove, self).assign_picking()
-        Picking = self.env['stock.picking']
+        Picking = self.env["stock.picking"]
         self.recompute()
 
         for move in self:
             domain = [
-                ('group_id', '=', move.group_id.id),
-                ('location_id', '=', move.location_id.id),
-                ('location_dest_id', '=', move.location_dest_id.id),
-                ('picking_type_id', '=', move.picking_type_id.id),
-                ('printed', '=', False),
-                ('min_date', '=', move.date_expected),
-                ('max_date', '=', move.date_expected),
-                ('carrier_id', '=', move.carrier_id.id),
-                ('state', 'in', ['draft', 'confirmed', 'waiting',
-                                 'partially_available', 'assigned'])]
+                ("group_id", "=", move.group_id.id),
+                ("location_id", "=", move.location_id.id),
+                ("location_dest_id", "=", move.location_dest_id.id),
+                ("picking_type_id", "=", move.picking_type_id.id),
+                ("printed", "=", False),
+                ("min_date", "=", move.date_expected),
+                ("max_date", "=", move.date_expected),
+                ("carrier_id", "=", move.carrier_id.id),
+                (
+                    "state",
+                    "in",
+                    [
+                        "draft",
+                        "confirmed",
+                        "waiting",
+                        "partially_available",
+                        "assigned",
+                    ],
+                ),
+            ]
             picking = Picking.search(domain, limit=1)
             if not picking.sale_id.manual_delivery:
                 picking = Picking.create(move._get_new_picking_values())
-            move.write({'picking_id': picking.id})
+            move.write({"picking_id": picking.id})
         return True
--- sale_manual_delivery/wizard/manual_proc.py
+++ sale_manual_delivery/wizard/manual_proc.py
@@ -6,100 +6,101 @@ from odoo.tools import float_compare
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
+
 class ManualDelivery(models.TransientModel):
     """Creates procurements manually"""
+
     _name = "manual.delivery"
-    _order = 'create_date desc'
+    _order = "create_date desc"
 
     def _set_order_id(self):
-        return self.env['sale.order'].browse(self._context['active_ids'])
+        return self.env["sale.order"].browse(self._context["active_ids"])
 
-    @api.onchange('order_id')
+    @api.onchange("order_id")
     def onchange_order_id(self):
         lines = []
         if self.order_id:
             for line in self.order_id.order_line:
-                if not line.existing_qty == line.product_uom_qty and \
-                line.product_id.type != 'service':
+                if (
+                    not line.existing_qty == line.product_uom_qty
+                    and line.product_id.type != "service"
+                ):
                     vals = {
-                        'order_line_id': line.id,
-                        'ordered_qty': line.product_uom_qty,
-                        'existing_qty': line.existing_qty
+                        "order_line_id": line.id,
+                        "ordered_qty": line.product_uom_qty,
+                        "existing_qty": line.existing_qty,
                     }
                     lines.append((0, 0, vals))
-            self.update({'line_ids': lines})
+            self.update({"line_ids": lines})
 
             self.partner_id = self.order_id.partner_id
 
-    date_planned = fields.Datetime(
-        string='Date Planned'
-    )
+    date_planned = fields.Datetime(string="Date Planned")
     order_id = fields.Many2one(
-        'sale.order',
-        string='Sale Order',  # TODO HIDE
-        default=_set_order_id
+        "sale.order", string="Sale Order", default=_set_order_id  # TODO HIDE
     )
     line_ids = fields.One2many(
-        'manual.delivery.line',
-        'manual_delivery_id',
-        string='Lines to validate',
+        "manual.delivery.line",
+        "manual_delivery_id",
+        string="Lines to validate"
     )
-    carrier_id = fields.Many2one(
-        'delivery.carrier',
-        string='Delivery Method'
-    )
-    partner_id = fields.Many2one(
-        'res.partner',
-        string='Delivery Address'
-    )
+    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method")
+    partner_id = fields.Many2one("res.partner", string="Delivery Address")
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_id(self):
         return {
-            'domain': {
-                'partner_id': [
-                    '&', '|',
-                    ('id', '=', self.order_id.partner_id.id),
-                    ('parent_id', '=', self.order_id.partner_id.id),
-                    ('id', '!=', self.partner_id.id),
-                ],
-            },
+            "domain": {
+                "partner_id": [
+                    "&",
+                    "|",
+                    ("id", "=", self.order_id.partner_id.id),
+                    ("parent_id", "=", self.order_id.partner_id.id),
+                    ("id", "!=", self.partner_id.id),
+                ]
+            }
         }
 
     @api.multi
     def record_picking(self):
-        proc_group_obj = self.env['procurement.group']
+        proc_group_obj = self.env["procurement.group"]
         for wizard in self:
-            carrier_id = wizard.carrier_id if wizard.carrier_id else \
-              wizard.order_id.carrier_id
+            carrier_id = wizard.carrier_id if wizard.carrier_id \
+                else wizard.order_id.carrier_id
             date_planned = wizard.date_planned
             order = wizard.order_id
             if not order.procurement_group_id:
                 vals = {
-                    'name': order.name,
-                    'move_type': order.picking_policy,
-                    'sale_id': order.id,
-                    'partner_id': order.partner_shipping_id.id,
+                    "name": order.name,
+                    "move_type": order.picking_policy,
+                    "sale_id": order.id,
+                    "partner_id": order.partner_shipping_id.id,
                 }
                 order.procurement_group_id = proc_group_obj.create(vals)
 
             for line in wizard.line_ids:
                 if line.to_ship_qty > line.ordered_qty - line.existing_qty:
-                    raise UserError(_(
-                    'You can not deliver more than the remaining quantity. If\
-                    you need to do so, please edit the sale order first.'))
+                    raise UserError(
+                        _(
+                            "You can not deliver more than the "
+                            "remaining quantity. If you need to do "
+                            "so, please edit the sale order first."
+                        )
+                    )
                 if float_compare(line.to_ship_qty, 0, 2):
                     vals = line.order_line_id._prepare_procurement_values(
-                        group_id=order.procurement_group_id)
-                    vals['date_planned'] = date_planned
-                    vals['carrier_id'] = carrier_id.id
-                    vals['partner_dest_id'] = wizard.partner_id.id
+                        group_id=order.procurement_group_id
+                    )
+                    vals["date_planned"] = date_planned
+                    vals["carrier_id"] = carrier_id.id
+                    vals["partner_dest_id"] = wizard.partner_id.id
+                    so_id = line.order_line_id.order_id
                     proc_group_obj.run(
                         line.order_line_id.product_id,
                         line.to_ship_qty,
                         line.order_line_id.product_uom,
-                        line.order_line_id.order_id.partner_shipping_id.property_stock_customer,
+                        so_id.partner_shipping_id.property_stock_customer,
                         line.order_line_id.name,
-                        line.order_line_id.order_id.name,
-                        vals
+                        so_id.name,
+                        vals,
                     )
