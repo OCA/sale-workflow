28e14e3689a73d40bc4c96c59c3786ef34110d76
Mourad <mourad.elhadj.mimoune@akretion.com>
2019-09-20 17:19:57 +0200

    [ADD] sale_order_min_qty

---
+++ sale_order_min_qty/models/product.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    sale_min_qty = fields.Float(string='Min Qty', default=1)
+    force_sale_min_qty = fields.Boolean(string='Force Min Qty',
+            help='If force min qty is checked, the min quantity '
+                 'is only indicative value.')
+
---
+++ sale_order_min_qty/models/sale.py
@@ -0,0 +1,42 @@
+#Copyright 2018 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.constrains('order_line')
+    def check_constraint_quantity(self):
+        for order in self:
+            invaild_lines = []
+            line_to_test = order.order_line.filtered(
+                lambda l: not l.product_id.force_sale_min_qty and
+                l.product_uom_qty < l.product_id.sale_min_qty)
+            for line in line_to_test:
+                invaild_lines.append(_('Product "%s": Min Quantity %s.') % (line.product_id.name, line.product_id.sale_min_qty)) 
+
+            if invaild_lines:
+                msg = _('Check minimum order quantity for this products: * \n') +\
+                        '\n '.join(list)
+                msg += _('* If you want you can check "force min quatity" on product')
+                raise ValidationError(msg)  
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    sale_min_qty = fields.Float(string='Min Qty',
+            related='product_id.sale_min_qty', readonly=True, store=True)
+    force_sale_min_qty = fields.Boolean(
+            related='product_id.force_sale_min_qty', readonly=True, store=True)
+    is_qty_less_min_qty = fields.Boolean(string='Qty < Min Qty', compute='_get_is_qty_less_min_qty',)
+
+
+    @api.multi
+    @api.depends('product_uom_qty', 'sale_min_qty')
+    def _get_is_qty_less_min_qty(self):
+        for line in self:
+            line.is_qty_less_min_qty = line.product_uom_qty < line.sale_min_qty
+
