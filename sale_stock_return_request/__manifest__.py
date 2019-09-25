107a1281f0fa03f21718e755e7033445f0f10e70
david <david.vidal@tecnativa.com>
2019-09-25 11:20:14 +0200

    [ADD][11.0] sale_stock_return_request: New module

---
+++ sale_stock_return_request/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Stock Return Request",
+    "version": "11.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/sale-workflow",
+    "development_status": "Production/Stable",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "maintainers": [
+        "chienandalu",
+    ],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_stock",
+        "stock_return_request",
+    ],
+    "data": [
+        'views/sale_return_request_views.xml',
+    ],
+}
---
+++ sale_stock_return_request/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+  * Sergio Teruel
+  * Pedro M. Baeza
+  * David Vidal
