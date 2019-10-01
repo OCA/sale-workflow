3f2d342f6995f81d435fb4d3b151c48f31b10008
antonio <espo-tony@hotmail.it>
2019-10-01 15:33:58 +0200

    [MIG] sale_order_archive: Migration to 13.0

--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/sale-workflow',
     'category': 'Sales',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'sale',
