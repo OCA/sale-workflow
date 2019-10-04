93bccd95ade0c0863a812cbef5d6e13dc4b7bcda
Mourad <mourad.elhadj.mimoune@akretion.com>
2019-10-04 17:08:20 +0200

    [ADD] sale_substate 12.0

---
+++ sale_substate/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Akretion (<http://www.akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Sub State",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow/",
+    "license": "AGPL-3",
+    "depends": ["base_substate", "sale_management"],
+    "data": ["views/sale_views.xml"],
+    "installable": True,
+}
