79e924eea79f5bfd3658b082c8ff6fe01eb2c9fc
sudhir-erpharbor <sudhir@erpharbor.com>
2019-10-11 14:23:47 +0530

    [IMP] Improved Code.

--- sale_validity/models/sale_order.py
+++ sale_validity/models/sale_order.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
 
 
