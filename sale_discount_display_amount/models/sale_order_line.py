feefdaac4fbac41f26e3d27646cb793c542f5a77
laurent.corron <laurent.corron@acsone.eu>
2019-10-18 11:37:17 +0200

    [IMP] sale_discount_display_amount: black, isort

--- sale_discount_display_amount/models/sale_order_line.py
+++ sale_discount_display_amount/models/sale_order_line.py
@@ -6,16 +6,14 @@ from odoo import api, fields, models
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     discount_total = fields.Monetary(
-        compute='_compute_amount',
-        string='Discount Subtotal',
-        store=True)
+        compute="_compute_amount", string="Discount Subtotal", store=True
+    )
     price_total_no_discount = fields.Monetary(
-        compute='_compute_amount',
-        string='Subtotal Without Discount',
-        store=True)
+        compute="_compute_amount", string="Subtotal Without Discount", store=True
+    )
 
     def _compute_discount(self):
         for line in self:
@@ -28,17 +26,20 @@ class SaleOrderLine(models.Model):
                 line.order_id.currency_id,
                 line.product_uom_qty,
                 product=line.product_id,
-                partner=line.order_id.partner_shipping_id)
+                partner=line.order_id.partner_shipping_id,
+            )
 
-            price_total_no_discount = taxes['total_included']
+            price_total_no_discount = taxes["total_included"]
             discount_total = price_total_no_discount - line.price_total
 
-            line.update({
-                'discount_total': discount_total,
-                'price_total_no_discount': price_total_no_discount
-            })
+            line.update(
+                {
+                    "discount_total": discount_total,
+                    "price_total_no_discount": price_total_no_discount,
+                }
+            )
 
-    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    @api.depends("product_uom_qty", "discount", "price_unit", "tax_id")
     def _compute_amount(self):
         res = super(SaleOrderLine, self)._compute_amount()
         self._compute_discount()
