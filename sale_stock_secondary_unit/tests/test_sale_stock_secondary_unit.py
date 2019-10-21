89918cee8eb29935fdfa47c57c770e5b2c5b78dd
Tony Gu <tony@openerp.cn>
2019-10-21 12:01:21 +0800

    [MIG] sale_stock_secondary_unit: Migration to 12.0

--- sale_stock_secondary_unit/tests/test_sale_stock_secondary_unit.py
+++ sale_stock_secondary_unit/tests/test_sale_stock_secondary_unit.py
@@ -1,64 +1,75 @@
 # Copyright 2019 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import SavepointCase
+from odoo.tests import SavepointCase, tagged
 
 
+@tagged("post_install", "-at_install")
 class TestSaleStockOrderSecondaryUnit(SavepointCase):
-    at_install = False
-    post_install = True
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.warehouse = cls.env.ref('stock.warehouse0')
-        cls.product_uom_kg = cls.env.ref('product.product_uom_kgm')
-        cls.product_uom_gram = cls.env.ref('product.product_uom_gram')
-        cls.product_uom_unit = cls.env.ref('product.product_uom_unit')
-        cls.product = cls.env['product.product'].create({
-            'name': 'test',
-            'type': 'product',
-            'uom_id': cls.product_uom_kg.id,
-            'uom_po_id': cls.product_uom_kg.id,
-            'secondary_uom_ids': [
-                (0, 0, {
-                    'name': 'unit-700',
-                    'uom_id': cls.product_uom_unit.id,
-                    'factor': 0.7,
-                })],
-        })
-        StockQuant = cls.env['stock.quant']
-        StockQuant.create({
-            'product_id': cls.product.id,
-            'location_id': cls.warehouse.lot_stock_id.id,
-            'quantity': 2000,
-        })
-        cls.secondary_unit = cls.env['product.secondary.unit'].search([
-            ('product_tmpl_id', '=', cls.product.product_tmpl_id.id),
-        ])
+        cls.warehouse = cls.env.ref("stock.warehouse0")
+        cls.product_uom_kg = cls.env.ref("uom.product_uom_kgm")
+        cls.product_uom_gram = cls.env.ref("uom.product_uom_gram")
+        cls.product_uom_unit = cls.env.ref("uom.product_uom_unit")
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "test",
+                "type": "product",
+                "uom_id": cls.product_uom_kg.id,
+                "uom_po_id": cls.product_uom_kg.id,
+                "secondary_uom_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "unit-700",
+                            "uom_id": cls.product_uom_unit.id,
+                            "factor": 0.7,
+                        },
+                    )
+                ],
+            }
+        )
+        StockQuant = cls.env["stock.quant"]
+        StockQuant.create(
+            {
+                "product_id": cls.product.id,
+                "location_id": cls.warehouse.lot_stock_id.id,
+                "quantity": 2000,
+            }
+        )
+        cls.secondary_unit = cls.env["product.secondary.unit"].search(
+            [("product_tmpl_id", "=", cls.product.product_tmpl_id.id)]
+        )
         cls.product.sale_secondary_uom_id = cls.secondary_unit.id
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'test - partner',
-        })
-        so = cls.env['sale.order'].new({
-            'partner_id': cls.partner.id,
-            'order_line': [(0, 0, {
-                'name': cls.product.name,
-                'product_id': cls.product.id,
-                'product_uom_qty': 1,
-                'product_uom': cls.product.uom_id.id,
-                'price_unit': 1000.00,
-            })],
-            'pricelist_id': cls.env.ref('product.list0').id,
-        })
+        cls.partner = cls.env["res.partner"].create({"name": "test - partner"})
+        so = cls.env["sale.order"].new(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 1000.00,
+                        },
+                    )
+                ],
+                "pricelist_id": cls.env.ref("product.list0").id,
+            }
+        )
         so.onchange_partner_id()
-        cls.order = cls.env['sale.order'].create(
-            so._convert_to_write(so._cache))
+        cls.order = cls.env["sale.order"].create(so._convert_to_write(so._cache))
 
     def test_stock_move_line_secondary_unit(self):
-        self.order.order_line.write({
-            'secondary_uom_id': self.secondary_unit.id,
-            'secondary_uom_qty': 5,
-        })
+        self.order.order_line.write(
+            {"secondary_uom_id": self.secondary_unit.id, "secondary_uom_qty": 5}
+        )
         self.order.order_line.onchange_secondary_uom()
         self.order.action_confirm()
         picking = self.order.picking_ids
