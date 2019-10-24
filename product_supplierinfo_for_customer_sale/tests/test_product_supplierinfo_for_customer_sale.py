10ce1a2c5da20d96d1066afbf457a97b2249f16c
mreficent <miquel.raich@eficent.com>
2019-10-24 19:13:08 +0200

    [IMP] Split of 'product.supplierinfo' for 'product.customerinfo'

--- product_supplierinfo_for_customer_sale/tests/test_product_supplierinfo_for_customer_sale.py
+++ product_supplierinfo_for_customer_sale/tests/test_product_supplierinfo_for_customer_sale.py
@@ -9,28 +9,29 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
     def setUp(self):
         super(TestProductSupplierinfoForCustomerSale, self).setUp()
         self.supplierinfo_model = self.env['product.supplierinfo']
+        self.customerinfo_model = self.env['product.customerinfo']
         self.pricelist_item_model = self.env['product.pricelist.item']
         self.pricelist_model = self.env['product.pricelist']
         self.customer = self._create_customer('customer1')
         self.product = self.env.ref('product.product_product_4')
         self.product_variant_1 = self.env.ref('product.product_product_4b')
         self.product_variant_2 = self.env.ref('product.product_product_4c')
-        self.supplierinfo = self._create_supplierinfo(
+        self.customerinfo = self._create_partnerinfo(
             'customer', self.customer, self.product)
         self.pricelist = self._create_pricelist(
             'Test Pricelist', self.product)
         self.pricelist_item = self._create_pricelist_item(
             'Test Pricelist Item', self.pricelist, self.product)
         self.company = self.env.ref('base.main_company')
-        self._create_supplierinfo(
+        self._create_partnerinfo(
             'customer', self.customer, self.product_variant_1)
-        self._create_supplierinfo(
+        self._create_partnerinfo(
             'customer', self.customer, self.product_variant_2,
             empty_variant=True)
         self.product_template = self.env['product.template'].create({
             'name': 'product wo variants'
         })
-        self._create_supplierinfo(
+        self._create_partnerinfo(
             'customer', self.customer,
             self.product_template.product_variant_ids[:1],
             empty_variant=True)
@@ -47,21 +48,20 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
             'phone': 123456,
         })
 
-    def _create_supplierinfo(self, supplierinfo_type, partner, product,
-                             empty_variant=False):
+    def _create_partnerinfo(self, supplierinfo_type, partner, product,
+                            empty_variant=False):
         vals = {
             'name': partner.id,
             'product_id': product.id,
             'product_name': 'product4',
             'product_code': '00001',
-            'supplierinfo_type': supplierinfo_type,
             'price': 100.0,
             'min_qty': 15.0,
         }
         if empty_variant:
             vals.pop('product_id', None)
             vals['product_tmpl_id'] = product.product_tmpl_id.id
-        return self.supplierinfo_model.create(vals)
+        return self.env['product.' + supplierinfo_type + 'info'].create(vals)
 
     def _create_pricelist(self, name, product):
         return self.pricelist_model.create({
@@ -90,10 +90,10 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
         })
         line.product_id_change()
         self.assertEqual(
-            line.product_customer_code, self.supplierinfo.product_code,
+            line.product_customer_code, self.customerinfo.product_code,
             "Error: Customer product code was not passed to sale order line")
         self.assertEqual(
-            line.product_uom_qty, self.supplierinfo.min_qty,
+            line.product_uom_qty, self.customerinfo.min_qty,
             "Error: Min qty was not passed to the sale order line")
 
     def test_product_supplierinfo_for_customer_sale_variant(self):
@@ -107,12 +107,12 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
         })
         line.product_id_change()
         self.assertEqual(
-            line.product_customer_code, self.supplierinfo.product_code,
+            line.product_customer_code, self.customerinfo.product_code,
             "Error: Customer product code was not passed to sale order line")
 
     def test_product_supplierinfo_for_customer_sale_template(self):
-        supplierinfo = self._create_supplierinfo('customer', self.customer,
-                                                 self.product_variant_2)
+        customerinfo = self._create_partnerinfo(
+            'customer', self.customer, self.product_variant_2)
         so = self.env['sale.order'].create({
             'partner_id': self.customer.id,
             'pricelist_id': self.pricelist.id,
@@ -123,7 +123,7 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
         })
         line.product_id_change()
         self.assertEqual(
-            line.product_customer_code, supplierinfo.product_code,
+            line.product_customer_code, customerinfo.product_code,
             "Error: Customer product code was not passed to sale order line")
         # Test with product without variants
         so2 = self.env['sale.order'].create({
@@ -136,11 +136,11 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
         })
         line2.product_id_change()
         self.assertEqual(
-            line2.product_customer_code, supplierinfo.product_code,
+            line2.product_customer_code, customerinfo.product_code,
             "Error: Customer product code was not passed to sale order line")
 
     def test_product_supplierinfo_for_customer_sale_variant_wo_template(self):
-        supplierinfo = self._create_supplierinfo(
+        customerinfo = self._create_partnerinfo(
             'customer', self.customer, self.product_variant_2,
             empty_variant=True)
         so = self.env['sale.order'].create({
@@ -153,5 +153,5 @@ class TestProductSupplierinfoForCustomerSale(TransactionCase):
         })
         line.product_id_change()
         self.assertEqual(
-            line.product_customer_code, supplierinfo.product_code,
+            line.product_customer_code, customerinfo.product_code,
             "Error: Customer product code was not passed to sale order line")
