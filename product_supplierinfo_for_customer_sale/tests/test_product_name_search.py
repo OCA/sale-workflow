258404d23d349662f0edb244b73d8a6d508a08ec
mreficent <miquel.raich@eficent.com>
2019-10-24 19:13:08 +0200

    [IMP] Split of 'product.supplierinfo' for 'product.customerinfo'

--- product_supplierinfo_for_customer_sale/tests/test_product_name_search.py
+++ product_supplierinfo_for_customer_sale/tests/test_product_name_search.py
@@ -12,30 +12,28 @@ class TestProductNameSearch(TransactionCase):
 
     def setUp(self):
         super(TestProductNameSearch, self).setUp()
-        self.customer = self.env.ref('base.res_partner_1')
-        self.customer_1 = self.env.ref('base.res_partner_2')
+        self.supplier = self.env.ref('base.res_partner_1')
+        self.customer = self.env.ref('base.res_partner_2')
         self.product = self.env['product.product'].create(
             {'name': 'Name_product',
              'default_code': 'code_product'}
-        ).with_context({'partner_id': self.customer.id,
-                        'supplierinfo_type': 'customer'})
+        ).with_context({'partner_id': self.customer.id})
         self.supplierinfo = self.env['product.supplierinfo']
-
-        self.supplierinfo_dict = {
+        self.customerinfo = self.env['product.customerinfo']
+        self.customerinfo_dict = {
             'product_code': 'code_test',
             'product_name': 'Name_test',
             'name': self.customer.id,
             'product_tmpl_id': self.product.product_tmpl_id.id,
-            'supplierinfo_type': 'customer',
         }
 
     def test_10_find_product_customer_code(self):
-        """Assign a product_supplierinfo to the product and then search it
+        """Assign a product_customerinfo to the product and then search it
         using name_search
         """
         self.assertFalse(self.product.customer_ids)
 
-        self.supplierinfo.create(self.supplierinfo_dict)
+        self.customerinfo.create(self.customerinfo_dict)
         self.assertTrue(self.product.customer_ids)
 
         # Search by product customer code
@@ -54,7 +52,7 @@ class TestProductNameSearch(TransactionCase):
         # Search by product default code with a different customer used in
         # configuration customer
         product_names = self.product.with_context(
-            partner_id=self.customer_1.id).name_search(name='code_product')
+            partner_id=self.supplier.id).name_search(name='code_product')
         self.assertEqual(len(product_names), 1)
         self.assertEqual(self.product.id, product_names[0][0])
         self.assertEqual('[code_product] Name_product', product_names[0][1])
@@ -64,8 +62,7 @@ class TestProductNameSearch(TransactionCase):
         self.product_1 = self.env['product.product'].create(
             {'name': 'Name_test_1',
              'default_code': 'code_test_1'}
-        ).with_context({'partner_id': self.customer.id,
-                        'supplierinfo_type': 'customer'})
+        ).with_context({'partner_id': self.customer.id})
 
         self.assertFalse(self.product_1.customer_ids)
 
