b7d00ebf3da620a1fa12f73e1c5132b5a0eddec0
sudhir-erpharbor <sudhir@erpharbor.com>
2019-11-02 18:41:55 +0530

    [MIG] sale_order_general_discount: Migration to 13.0
    [MIG] Migrated sale_order_general_discount to v13.
    
    [IMP] Improved code as per request.
    
    [IMP][13.0] sale_order_general_discount: rebase with OCA/13.0 + apply pre-commit
    
    [UPD] sale_order_general_discount: update view to display percentage correctly

--- sale_order_general_discount/models/res_partner.py
+++ sale_order_general_discount/models/res_partner.py
@@ -3,14 +3,10 @@
 
 from odoo import fields, models
 
-from odoo.addons import decimal_precision as dp
-
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     sale_discount = fields.Float(
-        digits=dp.get_precision("Discount"),
-        string="Discount (%)",
-        company_dependent=True,
+        string="Discount", digits="Discount", company_dependent=True,
     )
