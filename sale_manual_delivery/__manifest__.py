7f921f29bdd7e48fab7dd992975ce6025b7c5d5f
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-04 13:28:25 +0100

    Remove unneeded calls to invalidate_cache
    Fix view name and improve tests
    
    IMP: Use a procurement group according to date planned
    
    Fix manifest keys

--- sale_manual_delivery/__manifest__.py
+++ sale_manual_delivery/__manifest__.py
@@ -5,10 +5,14 @@
     "category": "Sale",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "version": "11.0.1.0.0",
-    "website": "http://camptocamp.com",
+    "version": "11.0.2.0.0",
+    "website": "https://github.com/sale-workflow",
     "summary": "Create manually your deliveries",
-    "depends": ["delivery", "sale", "sales_team"],
+    "depends": [
+        "delivery",
+        "sale",
+        "sales_team"
+    ],
     "data": [
         "views/crm_team_view.xml",
         "views/sale_order_view.xml",
