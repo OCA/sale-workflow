51f47fd4dabbe9b5cde22396f5356dd088e6e9a6
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-04 13:28:25 +0100

    Remove unneeded calls to invalidate_cache
    Fix view name and improve tests
    
    IMP: Use a procurement group according to date planned
    
    Fix manifest keys

--- sale_manual_delivery/models/procurement.py
+++ sale_manual_delivery/models/procurement.py
@@ -1,10 +1,12 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import models, fields
 
 
 class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
+
+    scheduled_date = fields.Date()
 
     def _get_stock_move_values(
         self,
