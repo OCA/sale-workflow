160d82b0f58c2cf51d257df4f5d4a8b5a265f7a6
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-04 15:16:37 +0100

    IMP: Use a procurement group according to date planned

--- sale_manual_delivery/tests/test_manual_delivery.py
+++ sale_manual_delivery/tests/test_manual_delivery.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+from odoo import fields
 from odoo.addons.sale.tests.test_sale_common import TestSale
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class TestSaleStock(TestSale):
@@ -274,3 +275,100 @@ class TestSaleStock(TestSale):
             be 5.0 instead of %s after complete delivery"
             % del_qty,
         )
+
+    def test_03_sale_multi_delivery(self):
+        self.product_2 = self.env.ref('product.product_delivery_02')
+        self.env['stock.quant']._update_available_quantity(
+            self.product_2, self.stock_location, 100)
+        so_vals = {
+            'partner_id': self.partner.id,
+            'partner_invoice_id': self.partner.id,
+            'partner_shipping_id': self.partner.id,
+            'order_line': [
+                (0, 0, {
+                    'name': self.product.name,
+                    'product_id': self.product.id,
+                    'product_uom_qty': 10.0,
+                    'product_uom': self.product.uom_id.id,
+                    'price_unit': self.product.list_price
+                }), (0, 0, {
+                    'name': self.product_2.name,
+                    'product_id': self.product_2.id,
+                    'product_uom_qty': 10.0,
+                    'product_uom': self.product_2.uom_id.id,
+                    'price_unit': self.product_2.list_price
+                })
+            ],
+            'pricelist_id': self.env.ref('product.list0').id,
+            'manual_delivery': True,
+        }
+        self.so = self.env['sale.order'].create(so_vals)
+
+        # confirm our standard so, check the picking
+        self.so.action_confirm()
+        self.assertFalse(self.so.picking_ids, 'Sale Manual Delivery: no \
+            picking should be created for "manual delivery" orders')
+        # create a manual delivery for part of ordered quantity
+        date_now = datetime.now()
+        wizard = self.env['manual.delivery'].create({
+            'order_id': self.so.id,
+            'carrier_id': self.so.carrier_id.id,
+            'date_planned': date_now,
+        })
+        wizard.onchange_order_id()
+        for line in wizard.line_ids:
+            line.to_ship_qty = 2.0
+        wizard.record_picking()
+        # check picking is created
+        self.assertEqual(
+            len(self.so.picking_ids),
+            1,
+            'Sale Manual Delivery: picking should be created after "manual'
+            ' delivery" wizard call'
+        )
+        first_picking = self.so.picking_ids
+        self.assertEqual(
+            first_picking.scheduled_date, fields.Datetime.to_string(date_now)
+        )
+        # create a second manual delivery for next week
+        date_next_week = date_now + relativedelta(weeks=1)
+        wizard = self.env['manual.delivery'].create({
+            'order_id': self.so.id,
+            'carrier_id': self.so.carrier_id.id,
+            'date_planned': date_next_week,
+        })
+        wizard.onchange_order_id()
+        for line in wizard.line_ids:
+            line.to_ship_qty = 3.0
+        wizard.record_picking()
+        self.assertEqual(
+            len(self.so.picking_ids),
+            2,
+            'Sale Manual Delivery: second picking should be created after'
+            ' "manual delivery" wizard call with different date'
+        )
+        second_picking = self.so.picking_ids - first_picking
+        self.assertEqual(
+            second_picking.scheduled_date,
+            fields.Datetime.to_string(date_next_week)
+        )
+        # create a third manual delivery for today (should be mixed with first)
+        new_date_now = datetime.now()
+        wizard = self.env['manual.delivery'].create({
+            'order_id': self.so.id,
+            'carrier_id': self.so.carrier_id.id,
+            'date_planned': new_date_now,
+        })
+        wizard.onchange_order_id()
+        for line in wizard.line_ids:
+            line.to_ship_qty = 5.0
+        wizard.record_picking()
+        self.assertEqual(
+            len(self.so.picking_ids),
+            2,
+            'Sale Manual Delivery: new moves should be merged in first picking'
+            ' after "manual delivery" wizard call with same date'
+
+        )
+        for move in first_picking.move_lines:
+            self.assertEqual(move.product_uom_qty, 7)
