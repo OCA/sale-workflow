6234e5ac7f7a2939be4b70b3bbd8002dfa4e1b2d
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-04 13:37:31 +0100

    Fix view name and improve tests

--- sale_manual_delivery/tests/test_manual_delivery.py
+++ sale_manual_delivery/tests/test_manual_delivery.py
@@ -1,22 +1,23 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.addons.sale.tests.test_sale_common import TestSale
-from odoo.exceptions import UserError
 from datetime import datetime
 
+
 class TestSaleStock(TestSale):
+
+    def setUp(self):
+        super().setUp()
+        self.partner = self.env.ref('base.res_partner_1')
+        self.product = self.env.ref('product.product_delivery_01')
+        self.stock_location = self.env.ref('stock.stock_location_stock')
+        self.env['stock.quant']._update_available_quantity(
+            self.product, self.stock_location, 100)
+
     def test_00_sale_manual_delivery(self):
         """
         Test SO's manual delivery
         """
-        self.partner = self.env.ref("base.res_partner_1")
-        self.product = self.env.ref("product.product_delivery_01")
-        self.env['stock.change.product.qty'].create(
-            {
-                'product_id': self.product.id,
-                'new_quantity': 10,
-            },
-        ).change_product_qty()
         so_vals = {
             "partner_id": self.partner.id,
             "partner_invoice_id": self.partner.id,
@@ -57,14 +58,9 @@ class TestSaleStock(TestSale):
             }
         )
         wizard.onchange_order_id()
-        wiz_act = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        wiz_act["res_id"] = wizard.id
-        wiz = self.env[wiz_act["res_model"]].browse(wiz_act["res_id"])
-        for line in wiz.line_ids:
-            line.to_ship_qty=line.ordered_qty
-        wiz.record_picking()
+        for line in wizard.line_ids:
+            line.to_ship_qty = line.ordered_qty
+        wizard.record_picking()
         # check picking is created
         self.assertTrue(
             self.so.picking_ids,
@@ -81,17 +77,13 @@ class TestSaleStock(TestSale):
             }
         )
         wizard.onchange_order_id()
-        wiz_act = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        wiz_act["res_id"] = wizard.id
-        wiz = self.env[wiz_act["res_model"]].browse(wiz_act["res_id"])
         self.assertFalse(
-            wiz.line_ids,
+            wizard.line_ids,
             "Sale Manual Delivery: After picking \
             creation for all products, no lines should be left in the wizard",
         )
-        wiz.record_picking()
+
+        wizard.record_picking()
         self.assertEqual(
             len(self.so.picking_ids),
             1.0,
@@ -105,7 +97,7 @@ class TestSaleStock(TestSale):
         pick = self.so.picking_ids
         pick.action_assign()
         pick.move_line_ids.write({"qty_done": 5})
-        pick.action_done()
+        pick.button_validate()
 
         # Check quantity delivered
         del_qty = sum(sol.qty_delivered for sol in self.so.order_line)
@@ -163,7 +155,7 @@ class TestSaleStock(TestSale):
         pick = self.so.picking_ids
         pick.action_assign()
         pick.move_line_ids.write({"qty_done": 5})
-        pick.action_done()
+        pick.button_validate()
 
         # Check quantity delivered
         del_qty = sum(sol.qty_delivered for sol in self.so.order_line)
@@ -179,14 +171,6 @@ class TestSaleStock(TestSale):
         """
         Test SO's various manual delivery
         """
-        self.partner = self.env.ref("base.res_partner_1")
-        self.product = self.env.ref("product.product_delivery_01")
-        self.env['stock.change.product.qty'].create(
-            {
-                'product_id': self.product.id,
-                'new_quantity': 10,
-            },
-        ).change_product_qty()
         so_vals = {
             "partner_id": self.partner.id,
             "partner_invoice_id": self.partner.id,
@@ -226,14 +210,9 @@ class TestSaleStock(TestSale):
             }
         )
         wizard.onchange_order_id()
-        wiz_act = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        wiz_act["res_id"] = wizard.id
-        wiz = self.env[wiz_act["res_model"]].browse(wiz_act["res_id"])
-        for line in wiz.line_ids:
-            line.to_ship_qty=2.0
-        wiz.record_picking()
+        for line in wizard.line_ids:
+            line.to_ship_qty = 2.0
+        wizard.record_picking()
         # check picking is created
         self.assertTrue(
             self.so.picking_ids,
@@ -246,7 +225,7 @@ class TestSaleStock(TestSale):
         pick = self.so.picking_ids
         pick.action_assign()
         pick.move_line_ids.write({"qty_done": 2})
-        pick.action_done()
+        pick.button_validate()
 
         # Check quantity delivered
         del_qty = sum(sol.qty_delivered for sol in self.so.order_line)
@@ -267,14 +246,9 @@ class TestSaleStock(TestSale):
             }
         )
         wizard.onchange_order_id()
-        wiz_act = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        wiz_act["res_id"] = wizard.id
-        wiz = self.env[wiz_act["res_model"]].browse(wiz_act["res_id"])
-        for line in wiz.line_ids:
-            line.to_ship_qty=3.0
-        wiz.record_picking()
+        for line in wizard.line_ids:
+            line.to_ship_qty = 3.0
+        wizard.record_picking()
         self.assertEqual(
             len(self.so.picking_ids),
             2.0,
