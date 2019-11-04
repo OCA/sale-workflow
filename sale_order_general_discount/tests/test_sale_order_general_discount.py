98576776c4e43ed047e8f960b17754aabdca5365
sudhir-erpharbor <sudhir@erpharbor.com>
2019-11-04 11:23:14 +0530

    [IMP] Improved Code.

--- sale_order_general_discount/tests/test_sale_order_general_discount.py
+++ sale_order_general_discount/tests/test_sale_order_general_discount.py
@@ -9,26 +9,26 @@ class TestSaleOrderLineInput(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'Test',
-            'sale_discount': 10.0,
+        cls.partner = cls.env["res.partner"].create({
+            "name": "Test",
+            "sale_discount": 10.0,
         })
-        cls.product = cls.env['product.product'].create({
-            'name': 'test_product',
-            'type': 'service',
+        cls.product = cls.env["product.product"].create({
+            "name": "test_product",
+            "type": "service",
         })
-        cls.order = cls.env['sale.order'].create({
-            'partner_id': cls.partner.id,
-            'order_line': [(0, 0, {
-                'name': cls.product.name,
-                'product_id': cls.product.id,
-                'product_uom_qty': 1,
-                'product_uom': cls.product.uom_id.id,
-                'price_unit': 1000.00,
+        cls.order = cls.env["sale.order"].create({
+            "partner_id": cls.partner.id,
+            "order_line": [(0, 0, {
+                "name": cls.product.name,
+                "product_id": cls.product.id,
+                "product_uom_qty": 1,
+                "product_uom": cls.product.uom_id.id,
+                "price_unit": 1000.00,
             })],
-            'pricelist_id': cls.env.ref('product.list0').id,
+            "pricelist_id": cls.env.ref("product.list0").id,
         })
-        cls.View = cls.env['ir.ui.view']
+        cls.View = cls.env["ir.ui.view"]
 
     def test_default_partner_discount(self):
         self.order.onchange_partner_id()
@@ -41,7 +41,7 @@ class TestSaleOrderLineInput(SavepointCase):
         self.assertEqual(self.order.order_line.price_reduce, 900.00)
 
     def _get_ctx_from_view(self, res):
-        order_xml = etree.XML(res['arch'])
+        order_xml = etree.XML(res["arch"])
         order_line_path = "//field[@name='order_line']"
         order_line_field = order_xml.xpath(order_line_path)[0]
         return order_line_field.attrib.get("context", "{}")
@@ -49,16 +49,16 @@ class TestSaleOrderLineInput(SavepointCase):
     def test_default_line_discount_value(self):
 
         res = self.order.fields_view_get(
-            view_id=self.env.ref('sale_order_general_discount.'
-                                 'sale_order_general_discount_form_view').id,
-            view_type='form')
+            view_id=self.env.ref("sale_order_general_discount."
+                                 "sale_order_general_discount_form_view").id,
+            view_type="form")
         ctx = self._get_ctx_from_view(res)
-        self.assertTrue('default_discount' in ctx)
+        self.assertTrue("default_discount" in ctx)
         view = self.View.create({
-            'name': "test",
-            'type': "form",
-            'model': 'sale.order',
-            'arch': """
+            "name": "test",
+            "type": "form",
+            "model": "sale.order",
+            "arch": """
                 <data>
                     <field name='order_line'
                         context="{'default_product_uom_qty': 3.0}">
@@ -66,6 +66,6 @@ class TestSaleOrderLineInput(SavepointCase):
                 </data>
             """
         })
-        res = self.order.fields_view_get(view_id=view.id, view_type='form')
+        res = self.order.fields_view_get(view_id=view.id, view_type="form")
         ctx = self._get_ctx_from_view(res)
-        self.assertTrue('default_discount' in ctx)
+        self.assertTrue("default_discount" in ctx)
