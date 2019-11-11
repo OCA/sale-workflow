5a1eb39d89bad7fbdd0d17559c00da24100b44b9
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-11-11 11:39:08 +0530

    [12.0][FIX] enable sale_merge_draft_invoice boolean

--- sale_merge_draft_invoice/model/__init__.py
+++ sale_merge_draft_invoice/model/__init__.py
@@ -2,5 +2,5 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from . import sale_order
-from . import res_config_settings
 from . import res_company
+from . import res_config_settings
