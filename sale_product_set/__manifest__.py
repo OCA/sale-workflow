abc95fa4e2c117d615d0dde5f3f45105273c46b0
Simone Orsi <simahawk@gmail.com>
2019-12-11 10:34:06 +0100

    sale_product_set 12.0.1.3.0

--- sale_product_set/__manifest__.py
+++ sale_product_set/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Sale',
     'license': 'AGPL-3',
     'author': 'Anybox, Odoo Community Association (OCA)',
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.3.0',
     'website': 'https://github.com/OCA/sale-workflow',
     'summary': "Sale product set",
     'depends': [
