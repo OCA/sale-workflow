72a35ec39f7530472436059fb03e08770c0649cb
Jeroen Evens <jeroen7e@gmail.com>
2019-12-18 10:27:17 +0100

    [MIG] sale_order_revision: Migration to 13.0
    [13.0] fix pre-commit + formatting
    
    add test lines and linting xml

--- sale_order_revision/__init__.py
+++ sale_order_revision/__init__.py
@@ -3,6 +3,8 @@ from . import models
 
 
 def populate_unrevisioned_name(cr, registry):
-    cr.execute('UPDATE sale_order '
-               'SET unrevisioned_name = name '
-               'WHERE unrevisioned_name is NULL')
+    cr.execute(
+        "UPDATE sale_order "
+        "SET unrevisioned_name = name "
+        "WHERE unrevisioned_name is NULL"
+    )
