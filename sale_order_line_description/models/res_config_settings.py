0ebb8cdbbca8b181730cb7b62e588bce9b943daf
kongrattapong <rattapong.chok@gmail.com>
2019-12-19 11:50:45 +0700

    [MIG] sale_order_line_description: Migration to 13.0

--- sale_order_line_description/models/res_config_settings.py
+++ sale_order_line_description/models/res_config_settings.py
@@ -8,7 +8,8 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     group_use_product_description_per_so_line = fields.Boolean(
-        "Allow using only the product sale description on the sales order " "lines",
+        string="Allow using only the product sale description on the sales order "
+        "lines",
         implied_group="sale_order_line_description."
         "group_use_product_description_per_so_line",
         help="Allows you to use only product sale description on the "
