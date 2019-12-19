34fb1e47240210d867398a44cf802c31a87900b8
kongrattapong <rattapong.chok@gmail.com>
2019-12-19 10:02:05 +0700

    [IMP] sale_commercial_partner: black, isort

--- sale_commercial_partner/__manifest__.py
+++ sale_commercial_partner/__manifest__.py
@@ -3,21 +3,15 @@
 
 
 {
-    'name': 'Sale Commercial Partner',
-    'summary': "Add stored related field 'Commercial Entity' on sale orders",
-    'version': '12.0.1.0.0',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'category': 'Sales',
-    'license': 'AGPL-3',
-    "contributors": [
-        "Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>",
-    ],
-    'depends': [
-        'sale'
-    ],
-    'data': [
-        'views/sale.xml',
-    ],
-    'installable': True,
+    "name": "Sale Commercial Partner",
+    "summary": "Add stored related field 'Commercial Entity' on sale orders",
+    "version": "12.0.1.0.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "contributors": ["Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>"],
+    "depends": ["sale"],
+    "data": ["views/sale.xml"],
+    "installable": True,
 }
