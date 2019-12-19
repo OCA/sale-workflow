23271bc42541f788561834158cdc379e2b594e5d
kongrattapong <rattapong.chok@gmail.com>
2019-12-19 10:02:05 +0700

    [IMP] sale_commercial_partner: black, isort

--- sale_commercial_partner/models/sale.py
+++ sale_commercial_partner/models/sale.py
@@ -1,16 +1,16 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     commercial_partner_id = fields.Many2one(
-        'res.partner',
-        related='partner_id.commercial_partner_id',
+        "res.partner",
+        related="partner_id.commercial_partner_id",
         store=True,
-        string='Commercial Entity',
-        index=True
+        string="Commercial Entity",
+        index=True,
     )
