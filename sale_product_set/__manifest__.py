1ac8fc94023c55f45f383cca73996f9b0536a4bf
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-23 16:54:14 +0100

    [IMP] sale_product_set: black, isort

--- sale_product_set/__manifest__.py
+++ sale_product_set/__manifest__.py
@@ -2,26 +2,21 @@
 # Copyright 2018 Camptocamp, ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale product set',
-    'category': 'Sale',
-    'license': 'AGPL-3',
-    'author': 'Anybox, Odoo Community Association (OCA)',
-    'version': '12.0.1.3.0',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'summary': "Sale product set",
-    'depends': [
-        'sale',
+    "name": "Sale product set",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "author": "Anybox, Odoo Community Association (OCA)",
+    "version": "12.0.1.3.0",
+    "website": "https://github.com/OCA/sale-workflow",
+    "summary": "Sale product set",
+    "depends": ["sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/rule_product_set.xml",
+        "views/product_set.xml",
+        "wizard/product_set_add.xml",
+        "views/sale_order.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/rule_product_set.xml',
-        'views/product_set.xml',
-        'wizard/product_set_add.xml',
-        'views/sale_order.xml',
-    ],
-    'demo': [
-        'demo/product_set.xml',
-        'demo/product_set_line.xml',
-    ],
-    'installable': True,
+    "demo": ["demo/product_set.xml", "demo/product_set_line.xml"],
+    "installable": True,
 }
