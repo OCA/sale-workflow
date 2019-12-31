b359de2d1044434d6c12572c4a3b9e6e1a9df303
ahenriquez <ahenriquez@eficent.com>
2019-12-31 16:33:55 +0100

    [MIG]sale_invoice_policy to v12

--- sale_invoice_policy/__manifest__.py
+++ sale_invoice_policy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -9,7 +8,7 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/sale-workflow',
     'category': 'Sales Management',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'sale',
