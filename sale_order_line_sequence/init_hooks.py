469d077dc4b8f47e1749a70f94ec5987f0576f1f
kongrattapong <rattapong.chok@gmail.com>
2020-01-06 15:34:00 +0700

    [IMP] sale_order_line_sequense: black, isort

--- sale_order_line_sequence/init_hooks.py
+++ sale_order_line_sequence/init_hooks.py
@@ -11,5 +11,5 @@ def post_init_hook(cr, pool):
     Fetches all the sale order and resets the sequence of the order lines
     """
     env = Environment(cr, SUPERUSER_ID, {})
-    sale = env['sale.order'].search([])
+    sale = env["sale.order"].search([])
     sale._reset_sequence()
