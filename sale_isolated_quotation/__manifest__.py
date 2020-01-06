f487ee20b1790e8aa46ff7a7ddbae4ff517df63f
Kitti U <kittiu@gmail.com>
2020-01-06 18:30:41 +0100

    TYPO switch from simple quote to double to preserve commits

--- sale_isolated_quotation/__manifest__.py
+++ sale_isolated_quotation/__manifest__.py
@@ -1,20 +1,20 @@
 # © 2017 Ecosoft (ecosoft.co.th).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Sale Isolated Quotation',
-    'version': '12.0.1.0.0',
-    'author': 'Ecosoft,Odoo Community Association (OCA)',
-    'category': 'Sales',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'depends': ['sale_management'],
-    'license': 'AGPL-3',
-    'images': [],
-    'data': [
-        'data/ir_sequence_data.xml',
-        'views/sale_views.xml',
+    "name": "Sale Isolated Quotation",
+    "version": "12.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale_management"],
+    "license": "AGPL-3",
+    "images": [],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "views/sale_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
-    'uninstall_hook': 'uninstall_hook',
-    'post_init_hook': 'post_init_hook',
+    "installable": True,
+    "auto_install": False,
+    "uninstall_hook": "uninstall_hook",
+    "post_init_hook": "post_init_hook",
 }
--- sale_isolated_quotation/models/sale.py
+++ sale_isolated_quotation/models/sale.py
@@ -5,77 +5,77 @@ from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     is_order = fields.Boolean(
-        string='Is Order',
+        string="Is Order",
         readonly=True,
         index=True,
-        default=lambda self: self.env.context.get('is_order'),
+        default=lambda self: self.env.context.get("is_order"),
     )
     quote_id = fields.Many2one(
-        'sale.order',
-        string='Quotation',
+        "sale.order",
+        string="Quotation",
         readonly=True,
-        ondelete='restrict',
+        ondelete="restrict",
         copy=False,
         help="For Sales Order, this field references to its Quotation",
     )
     order_id = fields.Many2one(
-        'sale.order',
-        string='Order',
+        "sale.order",
+        string="Order",
         readonly=True,
-        ondelete='restrict',
+        ondelete="restrict",
         copy=False,
         help="For Quotation, this field references to its Sales Order",
     )
     state2 = fields.Selection(
-        [('draft', 'Draft'),
-         ('sent', 'Mail Sent'),
-         ('cancel', 'Cancelled'),
-         ('done', 'Done'), ],
-        string='Status',
+        [("draft", "Draft"),
+         ("sent", "Mail Sent"),
+         ("cancel", "Cancelled"),
+         ("done", "Done"), ],
+        string="Status",
         readonly=True,
-        related='state',
+        related="state",
         help="A dummy state used for quotation",
     )
 
     @api.model
     def create(self, vals):
-        is_order = vals.get('is_order') or \
-            self.env.context.get('is_order')
-        if not is_order and vals.get('name', '/') == '/':
-            Seq = self.env['ir.sequence']
-            vals['name'] = Seq.next_by_code('sale.quotation') or '/'
+        is_order = vals.get("is_order") or \
+            self.env.context.get("is_order")
+        if not is_order and vals.get("name", "/") == "/":
+            Seq = self.env["ir.sequence"]
+            vals["name"] = Seq.next_by_code("sale.quotation") or "/"
         return super().create(vals)
 
     def action_convert_to_order(self):
         self.ensure_one()
         if self.is_order:
             raise UserError(
-                _('Only quotation can convert to order'))
+                _("Only quotation can convert to order"))
         order = self.copy({
-            'name': self.env['ir.sequence'].next_by_code('sale.order') or '/',
-            'is_order': True,
-            'quote_id': self.id,
-            'client_order_ref': self.client_order_ref,
+            "name": self.env["ir.sequence"].next_by_code("sale.order") or "/",
+            "is_order": True,
+            "quote_id": self.id,
+            "client_order_ref": self.client_order_ref,
         })
         self.order_id = order.id  # Reference from this quotation to order
-        if self.state == 'draft':
+        if self.state == "draft":
             self.action_done()
         return self.open_sale_order()
 
     @api.model
     def open_sale_order(self):
         return {
-            'name': _('Sales Order'),
-            'view_mode': 'form',
-            'view_id': False,
-            'res_model': 'sale.order',
-            'context': {'is_order': True, },
-            'type': 'ir.actions.act_window',
-            'nodestroy': True,
-            'target': 'current',
-            'domain': "[('is_order', '=', True)]",
-            'res_id': self.order_id and self.order_id.id or False,
+            "name": _("Sales Order"),
+            "view_mode": "form",
+            "view_id": False,
+            "res_model": "sale.order",
+            "context": {"is_order": True, },
+            "type": "ir.actions.act_window",
+            "nodestroy": True,
+            "target": "current",
+            "domain": "[("is_order", "=", True)]",
+            "res_id": self.order_id and self.order_id.id or False,
         }
