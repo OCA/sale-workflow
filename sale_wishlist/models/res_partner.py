ef13fdfc8420900a615927bfe72c807ef65d8131
Simone Orsi <simahawk@gmail.com>
2020-01-08 16:14:44 +0100

    [REF] sale_wishlist: Black python code

--- sale_wishlist/models/res_partner.py
+++ sale_wishlist/models/res_partner.py
@@ -6,41 +6,39 @@ from odoo import api, fields, models, _
 
 class ResPartner(models.Model):
 
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     wishlists_count = fields.Integer(
-        compute='_compute_wishlists_count',
-        string="# Wishlists"
+        compute="_compute_wishlists_count", string="# Wishlists"
     )
 
     def _compute_wishlists_count(self):
         # do just one query for all records
-        data = self.env['product.set'].read_group(
-            self._wishlist_domain(), ['partner_id'], ['partner_id'])
+        data = self.env["product.set"].read_group(
+            self._wishlist_domain(), ["partner_id"], ["partner_id"]
+        )
         data_mapped = {
-            count['partner_id'][0]: count['partner_id_count']
-            for count in data
+            count["partner_id"][0]: count["partner_id_count"] for count in data
         }
         for rec in self:
             rec.wishlists_count = data_mapped.get(rec.id, 0)
 
     def _wishlist_domain(self):
-        return [
-            ('partner_id', 'in', self.ids),
-            ('typology', '=', 'wishlist'),
-        ]
+        return [("partner_id", "in", self.ids), ("typology", "=", "wishlist")]
 
     @api.multi
     def action_view_wishlists(self):
         self.ensure_one()
         action = self.env.ref("sale_product_set.act_open_product_set_view")
         res = action.read()[0]
-        res.update({
-            'name': _('Wishlists'),
-            'domain': self._wishlist_domain(),
-            'context': {
-                'default_typology': 'wishlist',
-                'default_partner_id': self.id,
-            },
-        })
+        res.update(
+            {
+                "name": _("Wishlists"),
+                "domain": self._wishlist_domain(),
+                "context": {
+                    "default_typology": "wishlist",
+                    "default_partner_id": self.id,
+                },
+            }
+        )
         return res
