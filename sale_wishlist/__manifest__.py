0edbba4976546f474758a47db4901fbb8c69dc87
Simone Orsi <simahawk@gmail.com>
2020-01-08 16:14:44 +0100

    [REF] sale_wishlist: Black python code

--- sale_wishlist/__manifest__.py
+++ sale_wishlist/__manifest__.py
@@ -2,18 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Wishlist',
-    'summary': """
+    "name": "Sale Wishlist",
+    "summary": """
         Handle sale wishlist for partners""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Camptocamp SA,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'depends': [
-        'sale_product_set',
-    ],
-    'data': [
-        'views/product_set.xml',
-        'views/partner.xml',
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale_product_set"],
+    "data": ["views/product_set.xml", "views/partner.xml"],
 }
