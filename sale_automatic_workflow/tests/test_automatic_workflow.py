d59bd29dc7011470c9f5b2d03a3932afc8d7e770
Simone Orsi <simahawk@gmail.com>
2020-01-09 11:27:50 +0100

    sale_automatic_workflow: improve tests

--- sale_automatic_workflow/tests/test_automatic_workflow.py
+++ sale_automatic_workflow/tests/test_automatic_workflow.py
@@ -3,28 +3,30 @@
 
 from datetime import timedelta
 
+import mock
+
 from odoo import fields
 from odoo.tests import tagged
 
-from .test_automatic_workflow_base import TestAutomaticWorkflowBase
+from .common import TestAutomaticWorkflowMixin, TestCommon
 
 
 @tagged("post_install", "-at_install")
-class TestAutomaticWorkflow(TestAutomaticWorkflowBase):
+class TestAutomaticWorkflow(TestCommon, TestAutomaticWorkflowMixin):
     def test_full_automatic(self):
         workflow = self.create_full_automatic()
         sale = self.create_sale_order(workflow)
         sale._onchange_workflow_process_id()
         self.assertEqual(sale.state, "draft")
         self.assertEqual(sale.workflow_process_id, workflow)
-        self.progress()
+        self.run_job()
         self.assertEqual(sale.state, "sale")
         self.assertTrue(sale.picking_ids)
         self.assertTrue(sale.invoice_ids)
         invoice = sale.invoice_ids
         self.assertEqual(invoice.state, "posted")
         picking = sale.picking_ids
-        self.progress()
+        self.run_job()
         self.assertEqual(picking.state, "done")
 
     def test_onchange(self):
@@ -46,11 +48,34 @@ class TestAutomaticWorkflow(TestAutomaticWorkflowBase):
         sale = self.create_sale_order(workflow, override=override)
         sale._onchange_workflow_process_id()
         self.assertEqual(sale.date_order, last_week_time)
-        self.progress()
+        self.run_job()
         self.assertTrue(sale.invoice_ids)
         invoice = sale.invoice_ids
         self.assertEqual(invoice.invoice_date, last_week_time.date())
         self.assertEqual(invoice.workflow_process_id, sale.workflow_process_id)
+
+    def test_create_invoice_from_sale_order(self):
+        workflow = self.create_full_automatic()
+        sale = self.create_sale_order(workflow)
+        sale._onchange_workflow_process_id()
+        line = sale.order_line[0]
+        self.assertFalse(workflow.invoice_service_delivery)
+        self.assertEqual(line.qty_delivered_method, "stock_move")
+        self.assertEqual(line.qty_delivered, 0.0)
+        # `_create_invoices` is already tested in `sale` module.
+        # Make sure this addon works properly in regards to it.
+        mock_path = "odoo.addons.sale.models.sale.SaleOrder._create_invoices"
+        with mock.patch(mock_path) as mocked:
+            sale._create_invoices()
+            mocked.assert_called()
+        self.assertEqual(line.qty_delivered, 0.0)
+
+        workflow.invoice_service_delivery = True
+        line.qty_delivered_method = "manual"
+        with mock.patch(mock_path) as mocked:
+            sale._create_invoices()
+            mocked.assert_called()
+        self.assertEqual(line.qty_delivered, 1.0)
 
     def test_invoice_from_picking_with_service_product(self):
         workflow = self.create_full_automatic()
@@ -85,7 +110,7 @@ class TestAutomaticWorkflow(TestAutomaticWorkflowBase):
         }
         sale = self.create_sale_order(workflow, override=override)
         sale._onchange_workflow_process_id()
-        self.progress()
+        self.run_job()
         self.assertFalse(sale.picking_ids)
         self.assertTrue(sale.invoice_ids)
         invoice = sale.invoice_ids
@@ -102,7 +127,7 @@ class TestAutomaticWorkflow(TestAutomaticWorkflowBase):
         workflow = self.create_full_automatic()
         sale = self.create_sale_order(workflow)
         sale._onchange_workflow_process_id()
-        self.progress()
+        self.run_job()
         self.assertTrue(sale.invoice_ids)
         invoice = sale.invoice_ids
         self.assertEqual(invoice.journal_id.id, sale_journal.id)
@@ -112,7 +137,7 @@ class TestAutomaticWorkflow(TestAutomaticWorkflowBase):
         )
         sale = self.create_sale_order(workflow)
         sale._onchange_workflow_process_id()
-        self.progress()
+        self.run_job()
         self.assertTrue(sale.invoice_ids)
         invoice = sale.invoice_ids
         self.assertEqual(invoice.journal_id.id, new_sale_journal.id)
