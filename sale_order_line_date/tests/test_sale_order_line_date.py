e76ea0a1a024f0964e7f1a0154bde9538c92f9d3
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-13 15:55:21 +0100

    [13.0][MIG] sale_order_line_date

--- sale_order_line_date/tests/test_sale_order_line_date.py
+++ sale_order_line_date/tests/test_sale_order_line_date.py
@@ -1,7 +1,7 @@
 # © 2016 OdooMRP team
 # © 2016 AvanzOSC
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# © 2016 Eficent Business and IT Consulting Services, S.L.
+# © 2016 ForgeFlow S.L. (https://forgeflow.com)
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
