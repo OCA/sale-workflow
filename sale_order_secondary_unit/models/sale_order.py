778817e5fbccbfa00d3e301356e6f4ed1c6872e2
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-01-15 14:12:57 +0100

    [MIG] migrate sale_order_secondary_unit from 12.0 to 13.0

--- sale_order_secondary_unit/models/sale_order.py
+++ sale_order_secondary_unit/models/sale_order.py
@@ -1,66 +1,77 @@
-# Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2018-2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
-from odoo.addons import decimal_precision as dp
 from odoo.tools.float_utils import float_compare, float_round
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     secondary_uom_qty = fields.Float(
-        string='Secondary Qty',
-        digits=dp.get_precision('Product Unit of Measure'),
+        string="Secondary Qty", digits="Product Unit of Measure"
     )
     secondary_uom_id = fields.Many2one(
-        comodel_name='product.secondary.unit',
-        string='Secondary uom',
-        ondelete='restrict',
+        comodel_name="product.secondary.unit",
+        string="Secondary uom",
+        ondelete="restrict",
     )
 
-    @api.onchange('secondary_uom_id', 'secondary_uom_qty')
+    @api.onchange("secondary_uom_id", "secondary_uom_qty")
     def onchange_secondary_uom(self):
         if not self.secondary_uom_id:
             return
         factor = self.secondary_uom_id.factor * self.product_uom.factor
         qty = float_round(
             self.secondary_uom_qty * factor,
-            precision_rounding=self.product_uom.rounding
+            precision_rounding=self.product_uom.rounding,
         )
-        if float_compare(
-                self.product_uom_qty, qty,
-                precision_rounding=self.product_uom.rounding) != 0:
+        if (
+            float_compare(
+                self.product_uom_qty, qty, precision_rounding=self.product_uom.rounding
+            )
+            != 0
+        ):
             self.product_uom_qty = qty
 
-    @api.onchange('product_uom_qty')
+    @api.onchange("product_uom_qty")
     def onchange_secondary_unit_product_uom_qty(self):
         if not self.secondary_uom_id:
             return
         factor = self.secondary_uom_id.factor * self.product_uom.factor
         qty = float_round(
             self.product_uom_qty / (factor or 1.0),
-            precision_rounding=self.secondary_uom_id.uom_id.rounding
+            precision_rounding=self.secondary_uom_id.uom_id.rounding,
         )
-        if float_compare(
-                self.secondary_uom_qty, qty,
-                precision_rounding=self.secondary_uom_id.uom_id.rounding) != 0:
+        if (
+            float_compare(
+                self.secondary_uom_qty,
+                qty,
+                precision_rounding=self.secondary_uom_id.uom_id.rounding,
+            )
+            != 0
+        ):
             self.secondary_uom_qty = qty
 
-    @api.onchange('product_uom')
+    @api.onchange("product_uom")
     def onchange_product_uom_for_secondary(self):
         if not self.secondary_uom_id:
             return
         factor = self.product_uom.factor * self.secondary_uom_id.factor
         qty = float_round(
             self.product_uom_qty / (factor or 1.0),
-            precision_rounding=self.product_uom.rounding
+            precision_rounding=self.product_uom.rounding,
         )
-        if float_compare(
-                self.secondary_uom_qty, qty,
-                precision_rounding=self.product_uom.rounding) != 0:
+        if (
+            float_compare(
+                self.secondary_uom_qty,
+                qty,
+                precision_rounding=self.product_uom.rounding,
+            )
+            != 0
+        ):
             self.secondary_uom_qty = qty
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         """
         If default sales secondary unit set on product, put on secondary
@@ -68,7 +79,7 @@ class SaleOrderLine(models.Model):
         that is the one that sets by default 1 on the other quantity with that
         purpose.
         """
-        res = super(SaleOrderLine, self).product_id_change()
+        res = super().product_id_change()
         self.secondary_uom_id = self.product_id.sale_secondary_uom_id
         if self.secondary_uom_id:
             self.secondary_uom_qty = 1.0
