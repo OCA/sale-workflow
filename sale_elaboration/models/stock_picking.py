f90ec2747930df021d0de7b33b0b108c2357b558
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-22 23:09:48 +0100

    [IMP] sale_elaboration: black, isort

--- sale_elaboration/models/stock_picking.py
+++ sale_elaboration/models/stock_picking.py
@@ -4,21 +4,18 @@ from odoo import models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     def action_done(self):
         res = super(StockPicking, self).action_done()
         with self.env.norecompute():
-            for pick in self.filtered(
-                lambda x: x.picking_type_code == 'outgoing'
-            ):
+            for pick in self.filtered(lambda x: x.picking_type_code == "outgoing"):
                 elaboration_lines = pick.move_lines.filtered(
                     lambda x: x.sale_line_id.elaboration_id
                 )
                 for line in elaboration_lines:
                     pick.sale_id._create_elaboration_line(
-                        line.sale_line_id.elaboration_id.product_id,
-                        line.quantity_done,
+                        line.sale_line_id.elaboration_id.product_id, line.quantity_done
                     )
         self.recompute()
         return res
