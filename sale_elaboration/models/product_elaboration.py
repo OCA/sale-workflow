6303875a589abe4d709347b644fa005cab392940
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-22 23:09:48 +0100

    [IMP] sale_elaboration: black, isort

--- sale_elaboration/models/product_elaboration.py
+++ sale_elaboration/models/product_elaboration.py
@@ -5,37 +5,32 @@ from odoo import api, fields, models
 
 
 class Elaboration(models.Model):
-    _name = 'product.elaboration'
-    _description = 'Product elaborations'
+    _name = "product.elaboration"
+    _description = "Product elaborations"
 
-    name = fields.Char(
-        required=True,
-        translate=True,
-    )
-    code = fields.Char(
-        string='Short Code',
-    )
+    name = fields.Char(required=True, translate=True)
+    code = fields.Char(string="Short Code")
     product_id = fields.Many2one(
-        comodel_name='product.product',
-        string='Product',
-        ondelete='restrict',
-        domain=[('type', '=', 'service'), ('is_elaboration', '=', True)],
+        comodel_name="product.product",
+        string="Product",
+        ondelete="restrict",
+        domain=[("type", "=", "service"), ("is_elaboration", "=", True)],
         required=True,
     )
 
     _sql_constraints = [
-        ('name_uniq', 'unique(name)', 'Name must be unique!'),
-        ('code_uniq', 'unique(code)', 'Code must be unique!'),
+        ("name_uniq", "unique(name)", "Name must be unique!"),
+        ("code_uniq", "unique(code)", "Code must be unique!"),
     ]
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         """Give preference to codes on name search, appending
         the rest of the results after.
         """
         if not args:
             args = []
-        recs = self.search([('code', operator, name)] + args, limit=limit)
+        recs = self.search([("code", operator, name)] + args, limit=limit)
         res = recs.name_get()
         if limit:
             limit_rest = limit - len(recs)
@@ -43,8 +38,8 @@ class Elaboration(models.Model):
             # limit can be 0 or None representing infinite
             limit_rest = limit
         if limit_rest or not limit:
-            args += [('id', 'not in', recs.ids)]
+            args += [("id", "not in", recs.ids)]
             res += super().name_search(
-                name, args=args, operator=operator, limit=limit_rest,
+                name, args=args, operator=operator, limit=limit_rest
             )
         return res
