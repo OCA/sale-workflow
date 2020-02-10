d239ff8a5d9e857269d521055149e57cb400803d
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-10 15:52:14 +0100

    [IMP] sale_order_product_recommendation: black, isort, prettier

--- sale_order_product_recommendation/__manifest__.py
+++ sale_order_product_recommendation/__manifest__.py
@@ -3,18 +3,13 @@
 {
     "name": "Sale Order Product Recommendation",
     "summary": "Recommend products to sell to customer based on history",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "sale",
-    ],
-    "data": [
-        "wizards/sale_order_recommendation_view.xml",
-        "views/sale_order_view.xml",
-    ],
+    "depends": ["sale"],
+    "data": ["wizards/sale_order_recommendation_view.xml", "views/sale_order_view.xml"],
 }
