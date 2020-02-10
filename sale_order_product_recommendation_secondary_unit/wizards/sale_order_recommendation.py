1c7b5d00255c2dbb576d9128e15d3d83d47b8aa0
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-10 18:25:46 +0100

    [IMP] sale_order_product_recommendation_secondary_unit: black, isort

--- sale_order_product_recommendation_secondary_unit/wizards/sale_order_recommendation.py
+++ sale_order_product_recommendation_secondary_unit/wizards/sale_order_recommendation.py
@@ -2,71 +2,85 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.addons import decimal_precision as dp
 from odoo.tools.float_utils import float_compare, float_round
+
+from odoo.addons import decimal_precision as dp
 
 
 class SaleOrderRecommendationLine(models.TransientModel):
-    _inherit = 'sale.order.recommendation.line'
+    _inherit = "sale.order.recommendation.line"
 
     secondary_uom_id = fields.Many2one(
-        comodel_name='product.secondary.unit',
-        related='product_id.sale_secondary_uom_id',
+        comodel_name="product.secondary.unit",
+        related="product_id.sale_secondary_uom_id",
     )
     secondary_uom_qty = fields.Float(
-        string='Secondary Qty',
-        digits=dp.get_precision('Product Unit of Measure'),
+        string="Secondary Qty", digits=dp.get_precision("Product Unit of Measure")
     )
     product_tmpl_id = fields.Many2one(
-        related='product_id.product_tmpl_id',
+        related="product_id.product_tmpl_id",
         readonly=True,
-        help='To filter secondary uom available',
+        help="To filter secondary uom available",
     )
 
-    @api.onchange('secondary_uom_id', 'secondary_uom_qty')
+    @api.onchange("secondary_uom_id", "secondary_uom_qty")
     def _onchange_secondary_uom(self):
         if not self.secondary_uom_id:
             return
         factor = self.secondary_uom_id.factor * self.product_id.uom_id.factor
         qty = float_round(
             self.secondary_uom_qty * factor,
-            precision_rounding=self.product_id.uom_id.rounding)
-        if float_compare(
+            precision_rounding=self.product_id.uom_id.rounding,
+        )
+        if (
+            float_compare(
                 self.units_included,
                 qty,
-                precision_rounding=self.product_id.uom_id.rounding) != 0:
+                precision_rounding=self.product_id.uom_id.rounding,
+            )
+            != 0
+        ):
             self.units_included = qty
 
-    @api.onchange('units_included')
+    @api.onchange("units_included")
     def _onchange_units_included_sale_order_secondary_unit(self):
         if not self.secondary_uom_id:
             return
         factor = self.secondary_uom_id.factor * self.product_id.uom_id.factor
         qty = float_round(
             self.units_included / (factor or 1.0),
-            precision_rounding=self.secondary_uom_id.uom_id.rounding)
-        if float_compare(
+            precision_rounding=self.secondary_uom_id.uom_id.rounding,
+        )
+        if (
+            float_compare(
                 self.secondary_uom_qty,
                 qty,
-                precision_rounding=self.secondary_uom_id.uom_id.rounding) != 0:
+                precision_rounding=self.secondary_uom_id.uom_id.rounding,
+            )
+            != 0
+        ):
             self.secondary_uom_qty = qty
 
     def _prepare_update_so_line(self):
         res = super()._prepare_update_so_line()
         if self.secondary_uom_id and self.secondary_uom_qty:
-            res.update({
-                'secondary_uom_id': self.secondary_uom_id.id,
-                'secondary_uom_qty': self.secondary_uom_qty,
-            })
+            res.update(
+                {
+                    "secondary_uom_id": self.secondary_uom_id.id,
+                    "secondary_uom_qty": self.secondary_uom_qty,
+                }
+            )
         return res
 
     def _prepare_new_so_line(self, sequence):
         res = super()._prepare_new_so_line(sequence)
         if self.secondary_uom_id and self.secondary_uom_qty:
-            res.update({
-                'secondary_uom_id': self.secondary_uom_id.id,
-                'secondary_uom_qty': self.secondary_uom_qty,
-            })
+            res.update(
+                {
+                    "secondary_uom_id": self.secondary_uom_id.id,
+                    "secondary_uom_qty": self.secondary_uom_qty,
+                }
+            )
         return res
 
     def _trigger_so_line_onchanges(self, so_line):
