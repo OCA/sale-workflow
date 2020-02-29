f1e34ed88881a208568c548e3c710e0cee4e0a15
Sudhir Arya <sudhir@erpharbor.com>
2020-02-29 16:37:26 +0530

    [IMP] sale_order_line_price_history: black, isort

--- sale_order_line_price_history/wizards/sale_order_line_price_history.py
+++ sale_order_line_price_history/wizards/sale_order_line_price_history.py
@@ -10,20 +10,16 @@ class SaleOrderLinePriceHistory(models.TransientModel):
     @api.model
     def _default_partner_id(self):
         line_id = self.env.context.get("active_id")
-        return self.env['sale.order.line'].browse(line_id).order_partner_id.id
+        return self.env["sale.order.line"].browse(line_id).order_partner_id.id
 
     sale_order_line_id = fields.Many2one(
-        comodel_name='sale.order.line',
-        string='Sale order line',
+        comodel_name="sale.order.line",
+        string="Sale order line",
         default=lambda self: self.env.context.get("active_id"),
     )
-    product_id = fields.Many2one(
-        related="sale_order_line_id.product_id",
-    )
+    product_id = fields.Many2one(related="sale_order_line_id.product_id")
     partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Customer',
-        default=_default_partner_id,
+        comodel_name="res.partner", string="Customer", default=_default_partner_id
     )
     line_ids = fields.One2many(
         comodel_name="sale.order.line.price.history.line",
@@ -32,42 +28,47 @@ class SaleOrderLinePriceHistory(models.TransientModel):
         readonly=True,
     )
     include_quotations = fields.Boolean(
-        string="Include quotations",
-        help="Include quotations lines in the sale history",
+        string="Include quotations", help="Include quotations lines in the sale history"
     )
     include_commercial_partner = fields.Boolean(
         string="Include commercial entity",
         default=True,
-        help="Include commercial entity and its contacts in the sale history"
+        help="Include commercial entity and its contacts in the sale history",
     )
 
-    @api.onchange("partner_id", "include_quotations",
-                  "include_commercial_partner")
+    @api.onchange("partner_id", "include_quotations", "include_commercial_partner")
     def _onchange_partner_id(self):
         self.line_ids = False
         states = ["sale", "done"]
         if self.include_quotations:
             states += ["draft", "sent"]
-        domain = [
-            ("product_id", "=", self.product_id.id),
-            ("state", "in", states),
-        ]
+        domain = [("product_id", "=", self.product_id.id), ("state", "in", states)]
         if self.partner_id:
             if self.include_commercial_partner:
-                domain += [("order_partner_id", "child_of",
-                            self.partner_id.commercial_partner_id.ids)]
-            else:
                 domain += [
-                    ("order_partner_id", "child_of", self.partner_id.ids)]
+                    (
+                        "order_partner_id",
+                        "child_of",
+                        self.partner_id.commercial_partner_id.ids,
+                    )
+                ]
+            else:
+                domain += [("order_partner_id", "child_of", self.partner_id.ids)]
 
         vals = []
-        order_lines = self.env['sale.order.line'].search(domain, limit=20)
+        order_lines = self.env["sale.order.line"].search(domain, limit=20)
         order_lines -= self.sale_order_line_id
         for order_line in order_lines:
-            vals.append((0, False, {
-                'sale_order_line_id': order_line.id,
-                'history_sale_order_line_id': self.sale_order_line_id,
-            }))
+            vals.append(
+                (
+                    0,
+                    False,
+                    {
+                        "sale_order_line_id": order_line.id,
+                        "history_sale_order_line_id": self.sale_order_line_id,
+                    },
+                )
+            )
         self.line_ids = vals
 
 
@@ -76,32 +77,21 @@ class SaleOrderLinePriceHistoryline(models.TransientModel):
     _description = "Sale order line price history line"
 
     history_id = fields.Many2one(
-        comodel_name="sale.order.line.price.history",
-        string="History",
+        comodel_name="sale.order.line.price.history", string="History"
     )
     history_sale_order_line_id = fields.Many2one(
-        comodel_name='sale.order.line',
-        string="history sale order line",
+        comodel_name="sale.order.line", string="history sale order line"
     )
     sale_order_line_id = fields.Many2one(
-        comodel_name='sale.order.line',
-        string='Sale order line',
+        comodel_name="sale.order.line", string="Sale order line"
     )
-    order_id = fields.Many2one(
-        related="sale_order_line_id.order_id",
-    )
-    partner_id = fields.Many2one(
-        related="sale_order_line_id.order_partner_id",
-    )
+    order_id = fields.Many2one(related="sale_order_line_id.order_id")
+    partner_id = fields.Many2one(related="sale_order_line_id.order_partner_id")
     sale_order_date_order = fields.Datetime(
-        related="sale_order_line_id.order_id.date_order",
+        related="sale_order_line_id.order_id.date_order"
     )
-    product_uom_qty = fields.Float(
-        related="sale_order_line_id.product_uom_qty",
-    )
-    price_unit = fields.Float(
-        related="sale_order_line_id.price_unit",
-    )
+    product_uom_qty = fields.Float(related="sale_order_line_id.product_uom_qty")
+    price_unit = fields.Float(related="sale_order_line_id.price_unit")
 
     @api.multi
     def action_set_price(self):
