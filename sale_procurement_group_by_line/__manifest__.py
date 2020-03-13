557dff9f54cec635b313cba66de8ab1a271ba304
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-13 16:02:08 +0100

    [MIG] sale_procurement_group_by_line: Migration to 13.0

--- sale_procurement_group_by_line/__manifest__.py
+++ sale_procurement_group_by_line/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sale Procurement Group by Line",
     "summary": "Base module for multiple procurement group by Sale order",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp,"
-    "Eficent,"
+    "ForgeFlow,"
     "Serpent Consulting Services Pvt. Ltd.,"
     "Odoo Community Association (OCA)",
     "category": "Warehouse",
