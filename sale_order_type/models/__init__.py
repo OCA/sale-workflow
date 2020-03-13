5bd421e17bc270f996487c88b4ef40757aa1977b
kong <rattapong.chok@gmail.com>
2020-03-13 16:42:48 +0700

    [MIG] sale_order_type: Migration to 13.0

--- sale_order_type/models/__init__.py
+++ sale_order_type/models/__init__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import sale_order_type
-from . import sale_order
+from . import sale
 from . import res_partner
-from . import account_invoice
+from . import account_move
