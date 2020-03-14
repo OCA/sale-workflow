f8db981142652ef56687eca11c09abb9b2749345
David Beal <david.beal@akretion.com>
2020-03-14 12:45:37 +0100

    FIX sale_isolated_quotation: fix uninstall hook

--- sale_isolated_quotation/hooks.py
+++ sale_isolated_quotation/hooks.py
@@ -4,6 +4,12 @@ import ast
 from odoo import api, SUPERUSER_ID
 
 
+ACTIONS = (
+    "sale.action_quotations_with_onboarding",
+    "sale.action_orders",
+)
+
+
 def post_init_hook(cr, registry):
     """ Set value for order_sequence on old records """
     cr.execute("""
@@ -17,12 +23,22 @@ def uninstall_hook(cr, registry):
     """ Restore sale.order action, remove context value """
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        for action_id in ['sale.action_quotations', 'sale.action_orders']:
+        for action_id in ACTIONS:
             action = env.ref(action_id)
             ctx = ast.literal_eval(action.context)
-            del ctx['order_sequence']
-            dom = ast.literal_eval(action.domain)
+            _cleanup_ctx(ctx)
+            dom = ast.literal_eval(action.domain or "{}")
             dom = [x for x in dom if x[0] != 'order_sequence']
-            if action_id == 'sale.action_quotations':
+            if action_id == "sale.action_orders":
                 dom.append(('state', 'not in', ('draft', 'sent', 'cancel')))
+            else:
+                ctx["search_default_my_quotation"] = True
+            dom = list(set(dom))
             action.write({'context': ctx, 'domain': dom})
+
+
+def _cleanup_ctx(ctx):
+    if "order_sequence" in ctx:
+        del ctx["order_sequence"]
+    if "default_order_sequence" in ctx:
+        del ctx["default_order_sequence"]
