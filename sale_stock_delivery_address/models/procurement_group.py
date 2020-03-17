ec88d46b793ed1aa78986b2007ed28c9f6c43083
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-17 16:11:46 +0100

    [IMP] sale_stock_delivery_address: black, isort

--- sale_stock_delivery_address/models/procurement_group.py
+++ sale_stock_delivery_address/models/procurement_group.py
@@ -8,8 +8,9 @@ class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
 
     @api.model
-    def run(self, product_id, product_qty, product_uom, location_id, name,
-            origin, values):
+    def run(
+        self, product_id, product_qty, product_uom, location_id, name, origin, values
+    ):
         sale_line_id = values.get("sale_line_id")
         if sale_line_id:
             sale_line = self.env["sale.order.line"].browse(sale_line_id)
@@ -19,6 +20,5 @@ class ProcurementGroup(models.Model):
                 group.partner_id = sale_line.dest_address_id
                 location_id = sale_line.dest_address_id.property_stock_customer
         return super(ProcurementGroup, self).run(
-            product_id, product_qty, product_uom, location_id, name,
-            origin, values,
+            product_id, product_qty, product_uom, location_id, name, origin, values,
         )
