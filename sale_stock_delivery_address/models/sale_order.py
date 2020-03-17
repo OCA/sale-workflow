faed20ea343821b03b52fd61d36a83401982eeeb
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-17 16:11:46 +0100

    [IMP] sale_stock_delivery_address: black, isort

--- sale_stock_delivery_address/models/sale_order.py
+++ sale_stock_delivery_address/models/sale_order.py
@@ -9,10 +9,10 @@ class SaleOrder(models.Model):
 
     @api.model
     def _prepare_procurement_group_by_line(self, line):
-        vals = super(SaleOrder, self)._prepare_procurement_group_by_line(
-            line)
+        vals = super(SaleOrder, self)._prepare_procurement_group_by_line(line)
         if line._get_procurement_group_key()[0] == 16 and line.dest_address_id:
-            name_extension = line.dest_address_id.name or \
-                line.dest_address_id.contact_address
-            vals['name'] = '/'.join([vals['name'], name_extension])
+            name_extension = (
+                line.dest_address_id.name or line.dest_address_id.contact_address
+            )
+            vals["name"] = "/".join([vals["name"], name_extension])
         return vals
