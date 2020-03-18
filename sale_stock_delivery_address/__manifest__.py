fcc8f9bbf123ebcd4a2a55132bcb46020d6dcf0e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-18 10:23:50 +0100

    [MIG] sale_stock_delivery_address: Migration to 13.0

--- sale_stock_delivery_address/__manifest__.py
+++ sale_stock_delivery_address/__manifest__.py
@@ -2,13 +2,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Sale Stock Sourcing Address",
-    "version": "12.0.1.0.0",
+    "name": "Sale Stock Delivery Address",
+    "version": "13.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow/",
     "category": "Sales Management",
     "license": "LGPL-3",
-    "depends": ["sale_stock", "sale_procurement_group_by_line",],
-    "data": ["views/sale_order_view.xml", "views/res_partner_view.xml",],
+    "depends": ["sale_stock", "sale_procurement_group_by_line"],
+    "data": ["views/sale_order_view.xml", "views/res_partner_view.xml"],
     "installable": True,
 }
