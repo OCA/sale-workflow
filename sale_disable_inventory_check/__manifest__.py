f2c82b15269a2cc61a5e422fd7076d3bf0711943
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-19 10:36:38 +0100

    [IMP] sale_disable_inventory_check: black, isort

--- sale_disable_inventory_check/__manifest__.py
+++ sale_disable_inventory_check/__manifest__.py
@@ -3,16 +3,14 @@
 {
     "name": "Sale Disable Inventory Check",
     "summary": "Disable warning 'Not enough inventory' when there isn't enough"
-               " product stock",
+    " product stock",
     "version": "12.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Nguyen Tan Phuc (komit-consulting.com), "
-              "Odoo Community Association (OCA)",
+    "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "sale_stock",
-    ],
+    "depends": ["sale_stock",],
 }
--- sale_disable_inventory_check/models/sale_order_line.py
+++ sale_disable_inventory_check/models/sale_order_line.py
@@ -8,9 +8,10 @@ from odoo.tools import config
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.onchange('product_uom_qty', 'product_uom', 'route_id')
+    @api.onchange("product_uom_qty", "product_uom", "route_id")
     def _onchange_product_id_check_availability(self):
-        if (config['test_enable'] and
-                not self.env.context.get('test_sale_disable_inventory_check')):
+        if config["test_enable"] and not self.env.context.get(
+            "test_sale_disable_inventory_check"
+        ):
             return super()._onchange_product_id_check_availability()
         return {}
