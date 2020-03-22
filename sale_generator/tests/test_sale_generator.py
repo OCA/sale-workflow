6787c7f5ba7ce7ab202da0b6c650d43bfa92ba5c
kevinkhao <kevin.khao@akretion.com.br>
2020-03-22 15:47:43 +0100

    fixup! Increase test coverage, minor translation string update

--- sale_generator/tests/test_sale_generator.py
+++ sale_generator/tests/test_sale_generator.py
@@ -9,6 +9,7 @@ class TestSaleGenerator(TransactionCase):
         super().setUp()
         self.partner1 = self.env.ref("base.res_partner_address_4")
         self.partner2 = self.env.ref("base.res_partner_address_27")
+        self.partner3 = self.env.ref("base.res_partner_10")
         self.demo_sale_order = self.env.ref("sale.sale_order_4")
         self.some_fields_to_compare_so = [
             "validity_date",
@@ -36,7 +37,17 @@ class TestSaleGenerator(TransactionCase):
                 line2 = so2.order_line[itr]
                 self.assertEqual(getattr(line1, field), getattr(line2, field))
 
-    def test_basic_generation(self):
+    def _helper_test_so_equality(self, base_SO, sale_orders):
+        for sale in sale_orders:
+            for field in self.some_fields_to_compare_so:
+                self.assertEqual(
+                    getattr(base_SO, field), getattr(sale, field)
+                )
+            for field, val in self.constant_values_for_generated_sos.items():
+                self.assertEqual(getattr(sale, field), val)
+            self._helper_test_order_line_equality(base_SO, sale)
+
+    def test_generation_workflow(self):
         sale_tmpl = self.demo_sale_order
         part1 = self.partner1
         part2 = self.partner2
@@ -48,24 +59,47 @@ class TestSaleGenerator(TransactionCase):
             "state": "draft",
             "company_id": self.ref("base.main_company"),
         }
-        sg = self.env["sale.generator"].create(vals)
+        sale_generator = self.env["sale.generator"].create(vals)
 
-        sg.button_generate_sale_orders()
-
+        # Initial generation of sale order on confirmation
+        sale_generator.button_generate_sale_orders()
         sales_generated = self.env["sale.order"].search(
-            [("generator_id", "=", sg.id)]
+            [("generator_id", "=", sale_generator.id)]
         )
         self.assertEqual(len(sales_generated), 2)
         for sale in sales_generated:
             self.assertEqual(sale.state, "draft")
+        self._helper_test_so_equality(self.demo_sale_order, sales_generated)
 
-        sg.action_confirm()
+        # Dynamic update of generated sale orders
+        sale_generator.partner_ids += self.partner3
+        sales_generated = self.env["sale.order"].search(
+            [("generator_id", "=", sale_generator.id)]
+        )
+        self.assertEqual(len(sales_generated), 3)
+        self._helper_test_so_equality(self.demo_sale_order, sales_generated)
 
-        for sale in sales_generated:
-            for field in self.some_fields_to_compare_so:
-                self.assertEqual(
-                    getattr(self.demo_sale_order, field), getattr(sale, field)
-                )
-            for field, val in self.constant_values_for_generated_sos.items():
-                self.assertEqual(getattr(sale, field), val)
-            self._helper_test_order_line_equality(self.demo_sale_order, sale)
+        sale_generator.action_confirm()
+        self._helper_test_so_equality(self.demo_sale_order, sales_generated)
+
+    def test_create_partner_from_generator(self):
+        sale_tmpl = self.demo_sale_order
+        part1 = self.partner1
+        part2 = self.partner2
+        vals = {
+            "name": "/",
+            "partner_ids": [(4, part1.id, 0), (4, part2.id, 0)],
+            "tmpl_sale_id": sale_tmpl.id,
+            "warehouse_id": self.env.ref("stock.stock_warehouse_shop0").id,
+            "state": "draft",
+            "company_id": self.ref("base.main_company"),
+        }
+        sale_generator = self.env["sale.generator"].create(vals)
+
+        # test special generator context: partner create, window closing
+        test_partner = self.env['res.partner'].with_context({'from_generator_id': sale_generator.id}).create({
+            'name': 'test partner'
+        })
+        self.assertEqual(test_partner.generator_id, sale_generator)
+        close_action = self.env['res.partner'].close_from_customer_wizard()
+        self.assertEqual(test_partner.generator_id, close_action['res_id'])
