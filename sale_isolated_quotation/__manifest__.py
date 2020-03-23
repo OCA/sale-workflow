bda32c71f9da283ae63eb8374a976a70f0de1d34
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-23 14:12:17 +0700

    [13.0][IMP] sale_isolated_quotation: black, isort

--- sale_isolated_quotation/__manifest__.py
+++ sale_isolated_quotation/__manifest__.py
@@ -2,17 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Isolated Quotation",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": ["sale_management"],
     "license": "AGPL-3",
     "images": [],
-    "data": [
-        "data/ir_sequence_data.xml",
-        "views/sale_views.xml",
-    ],
+    "data": ["data/ir_sequence_data.xml", "views/sale_views.xml"],
     "installable": True,
     "auto_install": False,
     "uninstall_hook": "uninstall_hook",
