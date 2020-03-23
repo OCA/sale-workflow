731bc85398eb2dc32a52610c7b78966c73d1d4f3
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-23 16:00:40 +0700

    [13.0][MIG] sale_isolated_quotation

--- sale_isolated_quotation/tests/test_sale_isolated_quotation.py
+++ sale_isolated_quotation/tests/test_sale_isolated_quotation.py
@@ -1,6 +1,11 @@
 # © 2017 Ecosoft (ecosoft.co.th).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import ast
+
+from odoo.exceptions import UserError
 from odoo.tests.common import TransactionCase
+
+from ..hooks import uninstall_hook
 
 
 class TestSaleIsolatedQuotation(TransactionCase):
@@ -18,6 +23,37 @@ class TestSaleIsolatedQuotation(TransactionCase):
         self.assertEqual(self.sale_order.state, "draft")
         self.assertEqual(self.sale_order.partner_id, self.partner)
         self.assertEqual(self.sale_order.quote_id, self.quotation)
+        with self.assertRaises(UserError):
+            self.sale_order.action_convert_to_order()
+
+    def test_uninstall_hook(self):
+        """"Test uninstall_hook"""
+        actions = [
+            "sale.action_quotations_with_onboarding",
+            "sale.action_orders",
+        ]
+        # Check context and domain in action before uninstall this module
+        for action_id in actions:
+            action = self.env.ref(action_id)
+            ctx = ast.literal_eval(action.context)
+            dom = ast.literal_eval(action.domain or "{}")
+            self.assertTrue("order_sequence" in ctx)
+            self.assertTrue("default_order_sequence" in ctx)
+            self.assertTrue("order_sequence" in map(lambda l: l[0], dom))
+        # Uninstall this module
+        uninstall_hook(self.cr, self.registry)
+        # Check context and domain in action after uninstall this module
+        for action_id in actions:
+            action = self.env.ref(action_id)
+            ctx = ast.literal_eval(action.context)
+            dom = ast.literal_eval(action.domain or "{}")
+            self.assertTrue("order_sequence" not in ctx)
+            self.assertTrue("default_order_sequence" not in ctx)
+            self.assertTrue("order_sequence" not in map(lambda l: l[0], dom))
+            if action_id == "sale.action_orders":
+                self.assertTrue("state" in map(lambda l: l[0], dom))
+            else:
+                self.assertTrue("search_default_my_quotation" in ctx)
 
     def setUp(self):
         super().setUp()
