5fff4ce2f681b4f2f547f7e7d36218e45cf0318e
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-03-27 12:54:47 +0100

    Add module sale_cutoff_time_weekday_delivery

---
+++ sale_partner_cutoff_delivery_window/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Sale Partner Cutoff Delivery Window",
+    "summary": "Schedule delivery orders according to cutoff and window preferences",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale_cutoff_time_delivery", "sale_partner_delivery_window"],
+    "auto_install": True,
+}
---
+++ sale_partner_cutoff_delivery_window/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds some unittests to ensure Method Resolution Order of
+`sale.order.line._prepare_procurement_values` for modules
+`sale_cutoff_time_delivery` and `sale_partner_delivery_window` is correct.
