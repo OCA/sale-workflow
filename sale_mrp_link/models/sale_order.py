a78f1e5702ff5ee35055020e4c30a4d069f0a0d8
Travis Waelbroeck <twaelbroeck@gmail.com>
2020-03-30 20:02:51 -0500

    [IMP] sale_mrp_link: black, isort, prettier

--- sale_mrp_link/models/sale_order.py
+++ sale_mrp_link/models/sale_order.py
@@ -5,11 +5,10 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
-    production_ids = fields.One2many('mrp.production', 'sale_order_id')
-    production_count = fields.Integer(
-        compute='_compute_production_count', store=True)
+    production_ids = fields.One2many("mrp.production", "sale_order_id")
+    production_count = fields.Integer(compute="_compute_production_count", store=True)
 
     @api.depends("production_ids")
     def _compute_production_count(self):
@@ -18,11 +17,12 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_view_production(self):
-        action = self.env.ref('mrp.mrp_production_action').read()[0]
+        action = self.env.ref("mrp.mrp_production_action").read()[0]
         if self.production_count > 1:
-            action['domain'] = [('id', 'in', self.production_ids.ids)]
+            action["domain"] = [("id", "in", self.production_ids.ids)]
         else:
-            action['views'] = [
-                (self.env.ref('mrp.mrp_production_form_view').id, 'form')]
-            action['res_id'] = self.production_ids.id
+            action["views"] = [
+                (self.env.ref("mrp.mrp_production_form_view").id, "form")
+            ]
+            action["res_id"] = self.production_ids.id
         return action
