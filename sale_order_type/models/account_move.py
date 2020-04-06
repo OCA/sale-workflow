d1bc652de282d8fd9b72eba4801a8be4523ac911
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-06 10:11:53 +0200

    [FIX+IMP] sale_order_type: New computed writable + migration fixes
    * Switch onchange on the main field to computed writable fields
    * Record rule is not up to v13
    * Added migration script for record rule
    * Proper payment term field name in invoice
    * Realign tests

--- sale_order_type/models/account_move.py
+++ sale_order_type/models/account_move.py
@@ -1,4 +1,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 
 from odoo import api, fields, models
 
@@ -6,26 +7,35 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _get_order_type(self):
-        return self.env["sale.order.type"].search([], limit=1)
-
     sale_type_id = fields.Many2one(
-        comodel_name="sale.order.type", string="Sale Type", default=_get_order_type
+        comodel_name="sale.order.type",
+        string="Sale Type",
+        compute="_compute_sale_type_id",
+        readonly=False,
+        store=True,
     )
 
-    @api.onchange("partner_id", "company_id")
-    def _onchange_partner_id(self):
-        res = super(AccountMove, self)._onchange_partner_id()
-        sale_type = (
-            self.partner_id.sale_type or self.partner_id.commercial_partner_id.sale_type
-        )
-        if sale_type:
-            self.sale_type_id = sale_type
-        return res
+    @api.depends("partner_id", "company_id")
+    def _compute_sale_type_id(self):
+        for record in self:
+            if not record.partner_id:
+                record.sale_type_id = self.env["sale.order.type"].search([], limit=1)
+            else:
+                sale_type = (
+                    record.partner_id.with_context(
+                        force_company=record.company_id.id
+                    ).sale_type
+                    or self.partner_id.commercial_partner_id.with_context(
+                        force_company=record.company_id.id
+                    ).sale_type
+                )
+                if sale_type:
+                    record.sale_type_id = sale_type
 
     @api.onchange("sale_type_id")
     def onchange_sale_type_id(self):
+        # TODO: To be changed to computed stored readonly=False if possible in v14?
         if self.sale_type_id.payment_term_id:
-            self.payment_term = self.sale_type_id.payment_term_id.id
+            self.invoice_payment_term_id = self.sale_type_id.payment_term_id.id
         if self.sale_type_id.journal_id:
             self.journal_id = self.sale_type_id.journal_id.id
--- sale_order_type/tests/test_sale_order_type.py
+++ sale_order_type/tests/test_sale_order_type.py
@@ -1,5 +1,6 @@
 # Copyright 2015 Oihane Crucelaegui - AvanzOSC
 # Copyright 2017 Pierre Faniel - Niboo SPRL (<https://www.niboo.be/>)
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import odoo.tests.common as common
@@ -29,7 +30,9 @@ class TestSaleOrderType(common.TransactionCase):
         self.warehouse = self.env["stock.warehouse"].create(
             {"name": "Warehouse Test", "code": "WT"}
         )
-        self.product = self.env.ref("product.product_product_4")
+        self.product = self.env["product.product"].create(
+            {"type": "service", "invoice_policy": "order", "name": "Test product"}
+        )
         self.immediate_payment = self.env.ref("account.account_payment_term_immediate")
         self.sale_pricelist = self.env.ref("product.list0")
         self.free_carrier = self.env.ref("account.incoterm_FCA")
@@ -56,11 +59,9 @@ class TestSaleOrderType(common.TransactionCase):
         }
         return {"partner_id": self.partner.id, "order_line": [(0, 0, sale_line_dict)]}
 
-    def test_sale_order_confirm(self):
+    def test_sale_order_flow(self):
         sale_type = self.sale_type
-        order_vals = self.get_sale_order_vals()
-        order = self.sale_order_model.create(order_vals)
-        order.onchange_partner_id()
+        order = self.sale_order_model.create(self.get_sale_order_vals())
         self.assertEqual(order.type_id, sale_type)
         order.onchange_type_id()
         self.assertEqual(order.warehouse_id, sale_type.warehouse_id)
@@ -68,37 +69,26 @@ class TestSaleOrderType(common.TransactionCase):
         self.assertEqual(order.payment_term_id, sale_type.payment_term_id)
         self.assertEqual(order.pricelist_id, sale_type.pricelist_id)
         self.assertEqual(order.incoterm, sale_type.incoterm_id)
-
         order.action_confirm()
+        invoice = order._create_invoices()
+        self.assertEqual(invoice.sale_type_id, sale_type)
+        self.assertEqual(invoice.journal_id, sale_type.journal_id)
 
-    def test_sale_order_onchange_partner(self):
+    def test_sale_order_change_partner(self):
         order = self.sale_order_model.new({"partner_id": self.partner.id})
-        order.onchange_partner_id()
         self.assertEqual(order.type_id, self.sale_type)
         order = self.sale_order_model.new({"partner_id": self.partner_child_1.id})
-        order.onchange_partner_id()
         self.assertEqual(order.type_id, self.sale_type)
 
     def test_invoice_onchange_type(self):
         sale_type = self.sale_type
         invoice = self.invoice_model.new({"sale_type_id": sale_type.id})
         invoice.onchange_sale_type_id()
-        self.assertEqual(invoice.payment_term, sale_type.payment_term_id.id)
+        self.assertEqual(invoice.invoice_payment_term_id, sale_type.payment_term_id)
         self.assertEqual(invoice.journal_id, sale_type.journal_id)
 
-    def test_invoice_onchange_partner(self):
+    def test_invoice_change_partner(self):
         invoice = self.invoice_model.new({"partner_id": self.partner.id})
-        invoice._onchange_partner_id()
         self.assertEqual(invoice.sale_type_id, self.sale_type)
         invoice = self.invoice_model.new({"partner_id": self.partner_child_1.id})
-        invoice._onchange_partner_id()
         self.assertEqual(invoice.sale_type_id, self.sale_type)
-
-    def test_prepare_invoice(self):
-        sale_type = self.sale_type
-        order_vals = self.get_sale_order_vals()
-        order_vals["type_id"] = sale_type.id
-        order = self.sale_order_model.create(order_vals)
-        invoice_vals = order._prepare_invoice()
-        self.assertEqual(invoice_vals["sale_type_id"], sale_type.id)
-        self.assertEqual(invoice_vals["journal_id"], sale_type.journal_id.id)
