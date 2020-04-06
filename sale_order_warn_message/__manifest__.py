7182bd84a9dca4491909f300d5e0d8ad15b994aa
mreficent <miquel.raich@eficent.com>
2020-04-06 19:43:34 +0200

    [ADD] sale_order_warn_message

---
+++ sale_order_warn_message/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Warn Message",
+    "summary": """
+        Add a popup warning on sale to ensure warning is populated""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "www.github.com/OCA/sale-workflow.git",
+    "depends": ["sale"],
+    "data": ["views/sale_order.xml"],
+}
