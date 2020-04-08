a0bb874b3aa6a12daa809ed54d5f6b35cf504e5b
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-04-08 13:04:08 +0530

    [13.0][MIG] sale_order_lot_selection

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Lot Selection",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "author": "Odoo Community Association (OCA), Agile Business Group",
     "website": "https://github.com/OCA/sale-workflow",
