6e48aa10683ebacbdc020be8ba7c3eabd8e66011
kevinkhao <kevin.khao@akretion.com.br>
2020-04-10 09:39:30 +0200

    [IMP] sale_partner_version: black

--- sale_partner_version/__manifest__.py
+++ sale_partner_version/__manifest__.py
@@ -2,18 +2,13 @@
 # Copyright 2018 Akretion - Benoît Guillot
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale Partner Version',
-    'version': '10.0.1.0.0',
-    'author': "Akretion, "
-              "Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
-    'category': 'Sale',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'sale',
-        'partner_address_version',
-    ],
-    'data': [
-    ],
+    "name": "Sale Partner Version",
+    "version": "10.0.1.0.0",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
+    "website": "http://www.akretion.com",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale", "partner_address_version"],
+    "data": [],
 }
