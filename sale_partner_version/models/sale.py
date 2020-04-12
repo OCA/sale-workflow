288a1b1535ecdf9e4e37d0876970fb7266d0cbf5
kevinkhao <kevin.khao@akretion.com.br>
2020-04-12 03:57:59 +0200

    [12.0][MIG] sale_partner_version: Migration to 12.0

--- sale_partner_version/models/sale.py
+++ sale_partner_version/models/sale.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion - Benoît Guillot
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
---
+++ sale_partner_version/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module uses the partner_address_version module to create a partner version when confirming a sale order.
