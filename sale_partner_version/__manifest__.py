d6fb0d9170df08fd5a2b4a7961b97a0064091ffc
kevinkhao <kevin.khao@akretion.com.br>
2020-04-12 03:57:59 +0200

    [12.0][MIG] sale_partner_version: Migration to 12.0

--- sale_partner_version/__manifest__.py
+++ sale_partner_version/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion - Benoît Guillot
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Partner Version",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Akretion, " "Odoo Community Association (OCA)",
-    "website": "http://www.akretion.com",
+    "website": "https://github.com/OCA/sale-workflow",
     "category": "Sale",
     "license": "AGPL-3",
     "installable": True,
