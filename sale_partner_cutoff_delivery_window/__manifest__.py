fa759ce5ab18131c871002ec458532ff2fec9f9b
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-04-15 12:16:52 +0200

    fixup! fixup! fixup! fixup! Add module sale_cutoff_time_weekday_delivery

--- sale_partner_cutoff_delivery_window/__manifest__.py
+++ sale_partner_cutoff_delivery_window/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
-    "name": "Sale Partner Cutoff Window Delivery",
+    "name": "Sale Partner Cutoff Delivery Window",
     "summary": "Schedule delivery orders according to cutoff and window preferences",
     "version": "13.0.1.0.0",
     "development_status": "Alpha",
