47242abf32bedf1e73f7bc3b5be25b0950b7069f
Harald Panten <harald.panten@sygel.es>
2020-05-07 17:10:30 +0200

    [MIG]portal_sale_personal_data_only: Migration to 13.0

--- portal_sale_personal_data_only/__manifest__.py
+++ portal_sale_personal_data_only/__manifest__.py
@@ -2,14 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Portal Sale Personal Data Only",
-    "description": "Allow portal users to see their own documents",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sale",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    "depends": ["sale",],
-    "data": ["data/portal_sale_security.xml",],
+    "depends": ["sale"],
+    "data": ["data/portal_sale_security.xml"],
     "installable": True,
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
