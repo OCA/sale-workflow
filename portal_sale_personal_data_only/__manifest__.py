46393fa140819a53725cad730fbb50c9a515c783
Harald Panten <harald.panten@sygel.es>
2020-05-07 17:01:04 +0200

    [IMP] portal_sale_personal_data_only: black, isort, prettier

--- portal_sale_personal_data_only/__manifest__.py
+++ portal_sale_personal_data_only/__manifest__.py
@@ -1,21 +1,16 @@
 # Copyright 2018-19 Tecnativa S.L. - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Portal Sale Personal Data Only',
-    'description': 'Allow portal users to see their own documents',
-    'version': '12.0.1.0.0',
-    'category': 'Sale',
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.github.com/OCA/sale-workflow',
+    "name": "Portal Sale Personal Data Only",
+    "description": "Allow portal users to see their own documents",
+    "version": "12.0.1.0.0",
+    "category": "Sale",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    'depends': [
-        'sale',
-    ],
-    'data': [
-        'data/portal_sale_security.xml',
-    ],
-    'installable': True,
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
+    "depends": ["sale",],
+    "data": ["data/portal_sale_security.xml",],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
 }
