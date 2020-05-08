4be6f939d41e1584a468968dd13583a99b4a2102
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-05-08 09:47:56 +0200

    [MIG] sale_fixed_discount: Migration to 13.0

--- sale_fixed_discount/__manifest__.py
+++ sale_fixed_discount/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-20 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Fixed Discount",
     "summary": "Allows to apply fixed amount discounts in sales orders.",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
