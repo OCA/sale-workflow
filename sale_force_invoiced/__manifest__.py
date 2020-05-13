e5396309e51aea1c444cc503ddcd372a36944780
mreficent <miquel.raich@eficent.com>
2020-05-13 09:16:30 +0200

    [IMP] sale_force_invoiced: black, isort, prettier

--- sale_force_invoiced/__manifest__.py
+++ sale_force_invoiced/__manifest__.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale Force Invoiced',
-    'summary': 'Allows to force the invoice status of the sales order to '
-               'Invoiced',
-    'version': '12.0.1.0.1',
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
-    'category': 'sale',
-    'license': 'AGPL-3',
-    'website': "https://github.com/OCA/sale-workflow",
-    'depends': ['sale'],
-    'data': [
-        'view/sale_view.xml'
-    ],
-    'installable': True,
+    "name": "Sale Force Invoiced",
+    "summary": "Allows to force the invoice status of the sales order to Invoiced",
+    "version": "12.0.1.0.1",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "category": "sale",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale"],
+    "data": ["view/sale_view.xml"],
+    "installable": True,
 }
