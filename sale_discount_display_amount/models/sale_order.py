c1d85b7cd32ec8de080e905c6fb9ef40ccdf0b89
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-08 16:37:33 +0200

    [FIX] sale_discount_display_amount: Guidelines in compute methods names
    If not, it can collide with other modules defining the same compute name, so let's
    follow the guidelines and name properly the method.

--- sale_discount_display_amount/models/sale_order.py
+++ sale_discount_display_amount/models/sale_order.py
@@ -9,20 +9,20 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     discount_total = fields.Monetary(
-        compute="_compute_discount",
+        compute="_compute_discount_total",
         string="Discount Subtotal",
         currency_field="currency_id",
         store=True,
     )
     price_total_no_discount = fields.Monetary(
-        compute="_compute_discount",
+        compute="_compute_discount_total",
         string="Subtotal Without Discount",
         currency_field="currency_id",
         store=True,
     )
 
     @api.depends("order_line.discount_total", "order_line.price_total_no_discount")
-    def _compute_discount(self):
+    def _compute_discount_total(self):
         for order in self:
             discount_total = sum(order.order_line.mapped("discount_total"))
             price_total_no_discount = sum(
