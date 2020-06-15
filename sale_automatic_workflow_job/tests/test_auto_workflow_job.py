569b0ace15c3da834d64aaef409a477aef4a22f8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-06-15 16:56:27 +0200

    Add sale_automatic_workflow_job
    Use Queue Jobs to process the Sales Automatic Workflow actions.
    
    The default behavior of the automatic workflow module is to use a
    scheduled action that searches all the record that need a workflow
    action and sequentially process all of them.
    
    It can hit some limits when the number of records is too high.
    
    This module keeps the scheduled action to search the records, but
    instead of directly executing the actions (confirm a sales order,
    create invoices for a sales order, validate invoices, ...), it
    creates one job per operation to do.
    
    It uses an identity key on the jobs so it will not create the same
    job for the same record and same operation twice.
    
    ~
    
    I needed to extract methods in `sale_automatic_workflow` in order
    to be able to execute them as jobs.
    
    A new decorator "job_auto_delay" (to eventually move in queue_job) makes
    these methods automatically delayed when called.

---
+++ sale_automatic_workflow_job/tests/test_auto_workflow_job.py
@@ -0,0 +1,107 @@
+# Copyright 2020 Camptocamp (https://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import tagged
+
+from odoo.addons.queue_job.job import identity_exact
+from odoo.addons.queue_job.tests.common import mock_with_delay
+from odoo.addons.sale_automatic_workflow.tests.test_automatic_workflow_base import (  # noqa
+    TestAutomaticWorkflowBase
+)
+
+
+@tagged('post_install', '-at_install')
+class TestAutoWorkflowJob(TestAutomaticWorkflowBase):
+
+    def setUp(self):
+        super().setUp()
+        workflow = self.create_full_automatic()
+        self.sale = self.create_sale_order(workflow)
+
+    def assert_job_delayed(self, delayable_cls, delayable, method_name, args):
+        # .with_delay() has been called once
+        self.assertEqual(delayable_cls.call_count, 1)
+        delay_args, delay_kwargs = delayable_cls.call_args
+        # .with_delay() has been called on self.env["automatic.workflow.job"]
+        self.assertEqual(delay_args, (self.env["automatic.workflow.job"],))
+        # .with_delay() with the following options
+        self.assertEqual(delay_kwargs.get("identity_key"), identity_exact)
+        # check what's passed to the job method
+        method = getattr(delayable, method_name)
+        self.assertEqual(method.call_count, 1)
+        delay_args, delay_kwargs = method.call_args
+        self.assertEqual(delay_args, args)
+        self.assertDictEqual(delay_kwargs, {})
+
+    def test_validate_sale_order(self):
+        with mock_with_delay() as (delayable_cls, delayable):
+            self.progress()  # run automatic workflow cron
+            self.assert_job_delayed(
+                delayable_cls,
+                delayable,
+                "_do_validate_sale_order",
+                (self.sale,)
+            )
+
+    def test_create_invoice(self):
+        self.sale.action_confirm()
+        # don't care about transfers in this test
+        self.sale.picking_ids.state = "done"
+        with mock_with_delay() as (delayable_cls, delayable):
+            self.progress()  # run automatic workflow cron
+            self.assert_job_delayed(
+                delayable_cls,
+                delayable,
+                "_do_create_invoice",
+                (self.sale,)
+            )
+
+    def test_validate_invoice(self):
+        self.sale.action_confirm()
+        # don't care about transfers in this test
+        self.sale.picking_ids.state = "done"
+        self.sale.action_invoice_create()
+        invoice = self.sale.invoice_ids
+        with mock_with_delay() as (delayable_cls, delayable):
+            self.progress()  # run automatic workflow cron
+            self.assert_job_delayed(
+                delayable_cls,
+                delayable,
+                "_do_validate_invoice",
+                (invoice,)
+            )
+
+    def test_validate_picking(self):
+        self.sale.action_confirm()
+        picking = self.sale.picking_ids
+        # disable invoice creation in this test
+        self.sale.workflow_process_id.create_invoice = False
+        with mock_with_delay() as (delayable_cls, delayable):
+            self.progress()  # run automatic workflow cron
+            self.assert_job_delayed(
+                delayable_cls,
+                delayable,
+                "_do_validate_picking",
+                (picking,)
+            )
+
+    def test_sale_done(self):
+        self.sale.action_confirm()
+        # don't care about transfers in this test
+        self.sale.picking_ids.state = "done"
+        self.sale.action_invoice_create()
+
+        # disable invoice validation for we don't care
+        # in this test
+        self.sale.workflow_process_id.validate_invoice = False
+        # activate the 'sale done' workflow
+        self.sale.workflow_process_id.sale_done = True
+
+        with mock_with_delay() as (delayable_cls, delayable):
+            self.progress()  # run automatic workflow cron
+            self.assert_job_delayed(
+                delayable_cls,
+                delayable,
+                "_do_sale_done",
+                (self.sale,)
+            )
