dcddd6d5b987473f60e7ea7b8dc89d9132052a84
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-06-18 15:55:44 +0200

    [IMP] rename module sale_order_min_qty to sale_restricted_qty

---
+++ sale_restricted_qty/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Akretion (<http://www.akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale order min quantity",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow/",
+    "license": "AGPL-3",
+    "depends": ["sale_management"],
+    "data": [
+        "views/product_category_views.xml",
+        "views/product_template_views.xml",
+        "views/product_product_views.xml",
+        "views/sale_views.xml",
+    ],
+    "pre_init_hook": "rename_module",
+    "installable": True,
+}
