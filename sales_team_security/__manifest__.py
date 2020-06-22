e6863ecf3e4caebab13f863e520fdc3ec1dcb8fb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-22 10:56:46 +0200

    [IMP] sales_team_security: Contacts permissions revamp
    Restrict contacts by code, removing the need of manually manipulating the existing
    record rules, that can also have some side effects.

--- sales_team_security/__manifest__.py
+++ sales_team_security/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2016-2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sales documents permissions by channels (teams)",
     "summary": "New group for seeing only sales channel's documents",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
