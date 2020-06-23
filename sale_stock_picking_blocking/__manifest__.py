9541b848ccea6b9ffc2ee7e12a8a9b548c4bdbcf
Riyaj Pathan <rjpathan19@gmail.com>
2020-06-23 19:31:07 +0530

    [IMP] sale_stock_picking_blocking: black, isort, prettier

--- sale_stock_picking_blocking/__manifest__.py
+++ sale_stock_picking_blocking/__manifest__.py
@@ -4,23 +4,21 @@
 
 {
     "name": "Sale Stock Picking Blocking",
-    "summary": "Allow you to block the creation of deliveries "
-               "from a sale order.",
+    "summary": "Allow you to block the creation of deliveries " "from a sale order.",
     "version": "12.0.1.0.1",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
     "depends": ["sale_stock"],
     "data": [
-        'security/ir.model.access.csv',
-        'security/sale_stock_picking_blocking_security.xml',
-        'data/sale_stock_picking_blocking_data.xml',
-        'views/sale_stock_picking_blocking_reason_view.xml',
-        'views/sale_order_view.xml',
-        'views/res_partner_view.xml',
+        "security/ir.model.access.csv",
+        "security/sale_stock_picking_blocking_security.xml",
+        "data/sale_stock_picking_blocking_data.xml",
+        "views/sale_stock_picking_blocking_reason_view.xml",
+        "views/sale_order_view.xml",
+        "views/res_partner_view.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
--- sale_stock_picking_blocking/models/sale_order.py
+++ sale_stock_picking_blocking/models/sale_order.py
@@ -2,7 +2,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -10,36 +10,40 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.multi
-    @api.constrains('delivery_block_id')
+    @api.constrains("delivery_block_id")
     def _check_not_auto_done(self):
-        auto_done = self.env['ir.default'].get(
-            'res.config.settings', 'auto_done_setting')
+        auto_done = self.env["ir.default"].get(
+            "res.config.settings", "auto_done_setting"
+        )
         if auto_done and any(so.delivery_block_id for so in self):
             raise ValidationError(
-                _('You cannot block a sale order with "auto_done_setting" '
-                  'active.'))
+                _('You cannot block a sale order with "auto_done_setting" ' "active.")
+            )
 
     delivery_block_id = fields.Many2one(
-        comodel_name='sale.delivery.block.reason', track_visibility='always',
-        string='Delivery Block Reason', readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+        comodel_name="sale.delivery.block.reason",
+        track_visibility="always",
+        string="Delivery Block Reason",
+        readonly=True,
+        states={"draft": [("readonly", False)], "sent": [("readonly", False)]},
+    )
 
     @api.multi
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_id(self):
         """Add the 'Default Delivery Block Reason' if set in the partner."""
         res = super(SaleOrder, self).onchange_partner_id()
         for so in self:
-            so.delivery_block_id = so.partner_id.default_delivery_block or \
-                False
+            so.delivery_block_id = so.partner_id.default_delivery_block or False
         return res
 
     @api.multi
     def action_remove_delivery_block(self):
         """Remove the delivery block and create procurements as usual."""
         for order in self.filtered(
-                lambda so: so.state == 'sale' or not so.delivery_block_id):
-            order.write({'delivery_block_id': False})
+            lambda so: so.state == "sale" or not so.delivery_block_id
+        ):
+            order.write({"delivery_block_id": False})
             order.order_line._action_launch_stock_rule()
         return True
 
@@ -47,8 +51,7 @@ class SaleOrder(models.Model):
     def copy(self, default=None):
         new_so = super(SaleOrder, self).copy(default=default)
         for so in new_so:
-            if (so.partner_id.default_delivery_block and not
-                    so.delivery_block_id):
+            if so.partner_id.default_delivery_block and not so.delivery_block_id:
                 so.delivery_block_id = so.partner_id.default_delivery_block
         return new_so
 
@@ -58,6 +61,7 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     def _action_launch_stock_rule(self):
-        return super(SaleOrderLine, self.filtered(
-            lambda line: not line.order_id.delivery_block_id)). \
-            _action_launch_stock_rule()
+        return super(
+            SaleOrderLine,
+            self.filtered(lambda line: not line.order_id.delivery_block_id),
+        )._action_launch_stock_rule()
