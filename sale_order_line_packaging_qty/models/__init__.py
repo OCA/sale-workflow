5eece31b79d5c6fec1c316fc7f6fb5679222f1c4
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-06-25 09:13:57 +0200

    [IMP][sale_order_line_packaging_qty] Sell product / unit or packaging
    **Context:**
    
    We want to be able to only sell by packaging some product (forbidden to
     sell by unit). Also, we want to choose which packaging is eligible to
     sell.
    
    **Usage**
    
    When I encode a sale order line, I must fill up the packaging and
    related quantity if the product is set as "Only sell by packaging".
    I cannot enter a quantity in the standard Odoo field quantity that
    is not a multiple of a sellable packaging
    (product.packaging can be sold = True).
    
    Is the product is not configured as "Only sell by packaging",
    then I am allowed to fill up a line with no packaging.
    
    On every packaging I can chose if it is sellable or not. By default it is.
    
    I can chose at product level if i restrict the quantity to sell
    to existing sellable packaging.
    
    **Dev**
    
    * Add a check box in product "Only sell by packaging multiple" (default False), aside of "Can be sold" in the header
    * Add a boolean on the packaging "Can be sold" (default True)
    * On the list of available packaging in a SO line, only display packaging with "can be sold = True"
    * When a SO line is encoded *with a product that can only be sold by packaging*:
        * The quantity field cannot contain a quantity that is not a multiple of a sellable packaging (can be sold = True) -> raise a warning (triggered on the on_change please, not create/write as we still want to be able to save it e.g. in case of an EDI order) -> "The quantity is not a multiple of a defined sellable packaging. Please complete the packaging first."
        * On create / write method, *if packaging and packaging quantity is empty*, fulfill the info if a multiple of an existing sellable packaging (can be sold = True) is found
            * e.g. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 400, I expect the system to complete the packaging with Karton and packaging quantity with 2
            * e.g. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 420, I expect the system to complete the packaging with box and packaging quantity with 42
            * e.g.. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 405, I expect the system to leave it empty
        * Make the message "You must define a package before setting a quantity of said package." a user warning and not a blocking point AND only raise this warning for product that have "Only sell by packaging multiple = True"

--- sale_order_line_packaging_qty/models/__init__.py
+++ sale_order_line_packaging_qty/models/__init__.py
@@ -1,1 +1,4 @@
+from . import product_packaging_type
+from . import product_template
+from . import product_product
 from . import sale_order_line
---
+++ sale_order_line_packaging_qty/models/product_packaging_type.py
@@ -0,0 +1,9 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductPackagingType(models.Model):
+    _inherit = "product.packaging.type"
+
+    can_be_sold = fields.Boolean(string="Can be sold", default=True,)
