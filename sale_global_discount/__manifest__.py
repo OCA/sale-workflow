f4ee2c22aae4f4e76540e62d5d176e54c6ec225d
david <david.vidal@tecnativa.com>
2020-06-30 07:21:11 +0200

    [FIX] sale_global_discount: wrong property name

--- sale_global_discount/__manifest__.py
+++ sale_global_discount/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Sale Global Discount',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.2',
     'category': 'Sales Management',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
