8a4183f88460ffc0847ad9b71accf724834d6b7d
Damien Crier <damien.crier@captivea.com>
2020-07-03 16:57:50 +0200

    [MIG] sale_automatic_workflow_payment_mode: Migration to 13.0

--- sale_automatic_workflow_payment_mode/models/sale_workflow_process.py
+++ sale_automatic_workflow_payment_mode/models/sale_workflow_process.py
@@ -1,7 +1,7 @@
 # © 2016 Camptocamp SA, Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleWorkflowProcess(models.Model):
@@ -9,19 +9,18 @@ class SaleWorkflowProcess(models.Model):
 
     @api.model
     def _default_payment_filter_id(self):
-        xmlid = ('sale_automatic_workflow_payment_mode.'
-                 'automatic_workflow_payment_filter')
+        xmlid = (
+            "sale_automatic_workflow_payment_mode." "automatic_workflow_payment_filter"
+        )
         try:
             return self.env.ref(xmlid)
         except ValueError:
-            return self.env['ir.filters'].browse()
+            return self.env["ir.filters"].browse()
 
     payment_filter_id = fields.Many2one(
-        'ir.filters',
-        string='Register Payment Invoice Filter',
+        "ir.filters",
+        string="Register Payment Invoice Filter",
         default=_default_payment_filter_id,
     )
     register_payment = fields.Boolean()
-    payment_filter_domain = fields.Text(
-        related='payment_filter_id.domain',
-    )
+    payment_filter_domain = fields.Text(related="payment_filter_id.domain")
