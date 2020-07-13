9c87598aa6026e2f140c7987217d46fc7d33d085
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-07-13 17:35:58 +0200

    sale_product_set: Define proper hook to prepare sale order line

--- sale_product_set/models/product_set_line.py
+++ sale_product_set/models/product_set_line.py
@@ -25,3 +25,14 @@ class ProductSetLine(models.Model):
     )
     sequence = fields.Integer(string="Sequence", required=True, default=0)
     discount = fields.Float(string="Discount (%)", digits="Discount", default=0.0)
+
+    def prepare_sale_order_line_values(self, order, quantity, max_sequence=0):
+        self.ensure_one()
+        return {
+            "order_id": order.id,
+            "product_id": self.product_id.id,
+            "product_uom_qty": self.quantity * quantity,
+            "product_uom": self.product_id.uom_id.id,
+            "sequence": max_sequence + self.sequence,
+            "discount": self.discount,
+        }
