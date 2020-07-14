d681ef3a35dd6d522d81d7f5002cbc71fb0f7600
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-07-14 15:24:03 +0200

    Apply pre-commit

--- sale_exception/__manifest__.py
+++ sale_exception/__manifest__.py
@@ -3,23 +3,21 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Sale Exception',
-    'summary': 'Custom exceptions on sale order',
-    'version': '13.0.1.1.0',
-    'category': 'Generic Modules/Sale',
-    'author': "Akretion, "
-              "Sodexis, "
-              "Camptocamp, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/sale-workflow',
-    'depends': ['sale', 'base_exception'],
-    'license': 'AGPL-3',
-    'data': [
-        'data/sale_exception_data.xml',
-        'wizard/sale_exception_confirm_view.xml',
-        'views/sale_view.xml',
+    "name": "Sale Exception",
+    "summary": "Custom exceptions on sale order",
+    "version": "13.0.1.1.0",
+    "category": "Generic Modules/Sale",
+    "author": "Akretion, "
+    "Sodexis, "
+    "Camptocamp, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale", "base_exception"],
+    "license": "AGPL-3",
+    "data": [
+        "data/sale_exception_data.xml",
+        "wizard/sale_exception_confirm_view.xml",
+        "views/sale_view.xml",
     ],
-    'demo': [
-        'demo/sale_exception_demo.xml',
-    ]
+    "demo": ["demo/sale_exception_demo.xml"],
 }
