77d9dbf785a8f5c4b7df7a5da743b3953febfc37
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-07-14 15:24:03 +0200

    sale_exception: apply pre-commit

--- sale_exception/models/sale.py
+++ sale_exception/models/sale.py
@@ -3,59 +3,56 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class ExceptionRule(models.Model):
-    _inherit = 'exception.rule'
+    _inherit = "exception.rule"
 
     model = fields.Selection(
         selection_add=[
-            ('sale.order', 'Sale order'),
-            ('sale.order.line', 'Sale order line'),
+            ("sale.order", "Sale order"),
+            ("sale.order.line", "Sale order line"),
         ]
     )
-    sale_ids = fields.Many2many(
-        'sale.order',
-        string="Sales")
+    sale_ids = fields.Many2many("sale.order", string="Sales")
 
 
 class SaleOrder(models.Model):
-    _inherit = ['sale.order', 'base.exception']
-    _name = 'sale.order'
-    _order = 'main_exception_id asc, date_order desc, name desc'
+    _inherit = ["sale.order", "base.exception"]
+    _name = "sale.order"
+    _order = "main_exception_id asc, date_order desc, name desc"
 
     @api.model
     def _exception_rule_eval_context(self, rec):
         res = super(SaleOrder, self)._exception_rule_eval_context(rec)
-        res['sale'] = rec
+        res["sale"] = rec
         return res
 
     @api.model
     def _reverse_field(self):
-        return 'sale_ids'
+        return "sale_ids"
 
     def detect_exceptions(self):
         all_exceptions = super(SaleOrder, self).detect_exceptions()
-        lines = self.mapped('order_line')
+        lines = self.mapped("order_line")
         all_exceptions += lines.detect_exceptions()
         return all_exceptions
 
     @api.model
     def test_all_draft_orders(self):
-        order_set = self.search([('state', '=', 'draft')])
+        order_set = self.search([("state", "=", "draft")])
         order_set.detect_exceptions()
         return True
 
     def _fields_trigger_check_exception(self):
-        return ['ignore_exception', 'order_line', 'state']
+        return ["ignore_exception", "order_line", "state"]
 
     @api.model
     def create(self, vals):
         record = super(SaleOrder, self).create(vals)
         check_exceptions = any(
-            field in vals for field
-            in self._fields_trigger_check_exception()
+            field in vals for field in self._fields_trigger_check_exception()
         )
         if check_exceptions:
             record.sale_check_exception()
@@ -64,21 +61,20 @@ class SaleOrder(models.Model):
     def write(self, vals):
         result = super(SaleOrder, self).write(vals)
         check_exceptions = any(
-            field in vals for field
-            in self._fields_trigger_check_exception()
+            field in vals for field in self._fields_trigger_check_exception()
         )
         if check_exceptions:
             self.sale_check_exception()
         return result
 
     def sale_check_exception(self):
-        orders = self.filtered(lambda s: s.state == 'sale')
+        orders = self.filtered(lambda s: s.state == "sale")
         if orders:
             orders._check_exception()
 
-    @api.onchange('order_line')
+    @api.onchange("order_line")
     def onchange_ignore_exception(self):
-        if self.state == 'sale':
+        if self.state == "sale":
             self.ignore_exception = False
 
     def action_confirm(self):
@@ -89,9 +85,7 @@ class SaleOrder(models.Model):
     def action_draft(self):
         res = super().action_draft()
         orders = self.filtered(lambda s: s.ignore_exception)
-        orders.write({
-            'ignore_exception': False,
-        })
+        orders.write({"ignore_exception": False})
         return res
 
     def _sale_get_lines(self):
@@ -100,4 +94,4 @@ class SaleOrder(models.Model):
 
     @api.model
     def _get_popup_action(self):
-        return self.env.ref('sale_exception.action_sale_exception_confirm')
+        return self.env.ref("sale_exception.action_sale_exception_confirm")
