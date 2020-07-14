f25e9756b2214caac4c034fc86134fbfdadc2535
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-07-14 15:24:03 +0200

    sale_exception: apply pre-commit

--- sale_exception/models/sale_order_line.py
+++ sale_exception/models/sale_order_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2019 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,24 +5,23 @@ from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = ['sale.order.line', 'base.exception.method']
-    _name = 'sale.order.line'
+    _inherit = ["sale.order.line", "base.exception.method"]
+    _name = "sale.order.line"
 
     ignore_exception = fields.Boolean(
-        related='order_id.ignore_exception',
-        store=True,
-        string="Ignore Exceptions")
+        related="order_id.ignore_exception", store=True, string="Ignore Exceptions"
+    )
 
     def _get_main_records(self):
-        return self.mapped('order_id')
+        return self.mapped("order_id")
 
     @api.model
     def _reverse_field(self):
-        return 'sale_ids'
+        return "sale_ids"
 
     def _detect_exceptions(self, rule):
         records = super(SaleOrderLine, self)._detect_exceptions(rule)
-        return records.mapped('order_id')
+        return records.mapped("order_id")
 
     @api.model
     def _exception_rule_eval_context(self, rec):
@@ -32,5 +30,5 @@ class SaleOrderLine(models.Model):
         # code during v13 migration. The record is already available in obj and
         # object variables and it is more than enough.
         res = super(SaleOrderLine, self)._exception_rule_eval_context(rec)
-        res['sale_line'] = rec
+        res["sale_line"] = rec
         return res
