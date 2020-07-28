477f390a7aa5e858535fd81958c953ebec30d98e
Jon Zaballa <jonzaballa@digital5.es>
2020-07-28 10:46:04 +0200

    [FIX] 11.0 Sale Fixed Discount
    Fix
    ===
    `_onchange_discount` method was shadowing the original _onchange_discount
    and all its field triggers had been replaced by `discount`. As the new method's
    trigger is the field `discount`, the name should be `_onchange_discount`.
    But the name is already taken. Hence `_onchange_discount2`. I'm not aware of
    a better naming convention in cases like this.
    
    Also, the way the method was implemented, it was impossible to change the
    discount manually if the pricelist had a discount, because that would
    trigger the `_onchange_discount`, call to super, and get the discount
    from the pricelist.
    
    Improve
    =======
    Override orginal `_onchange_discount` in order to get the value for the
    fixed discount in case the price rule method is `fixed`. It basically
    repeats what the original method does, so not very DRYish. I don't know
    if there is a better approach (I can only think of monkey path, but I
    don't like it either)

--- sale_fixed_discount/models/sale_order.py
+++ sale_fixed_discount/models/sale_order.py
@@ -38,16 +38,68 @@ class SaleOrderLine(models.Model):
         help="Fixed amount discount.")
 
     @api.onchange('discount')
-    def _onchange_discount(self):
-        res = super(SaleOrderLine, self)._onchange_discount()
+    def _onchange_discount2(self):
         if self.discount:
             self.discount_fixed = 0.0
-        return res
 
     @api.onchange('discount_fixed')
     def _onchange_discount_fixed(self):
         if self.discount_fixed:
             self.discount = 0.0
+
+    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty',
+                  'tax_id')
+    def _onchange_discount(self):
+        res = super(SaleOrderLine, self)._onchange_discount()
+
+        if not (self.product_id and self.product_uom and
+                self.order_id.partner_id and self.order_id.pricelist_id and
+                self.order_id.pricelist_id.discount_policy == 'without_discount' and
+                self.env.user.has_group('sale.group_discount_per_so_line')):
+            return res
+
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang,
+            partner=self.order_id.partner_id.id,
+            quantity=self.product_uom_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id,
+            fiscal_position=self.env.context.get('fiscal_position')
+        )
+        product_context = dict(self.env.context,
+                               partner_id=self.order_id.partner_id.id,
+                               date=self.order_id.date_order,
+                               uom=self.product_uom.id)
+
+        price, rule_id = self.order_id \
+            .pricelist_id \
+            .with_context(product_context) \
+            .get_product_price_rule(self.product_id,
+                                    self.product_uom_qty or 1.0,
+                                    self.order_id.partner_id)
+
+        rule = self.env['product.pricelist.item'].browse(rule_id)
+        if rule.compute_price == 'fixed':
+            new_list_price, currency_id = self.with_context(
+                product_context
+            )._get_real_price_currency(product,
+                                       rule_id, self.product_uom_qty,
+                                       self.product_uom,
+                                       self.order_id.pricelist_id.id)
+            if new_list_price != 0:
+                if self.order_id.pricelist_id.currency_id.id != currency_id:
+                    # we need new_list_price in the same currency as price,
+                    # which is in the SO's pricelist's currency
+                    new_list_price = self.env['res.currency'].browse(
+                        currency_id
+                    ).with_context(product_context).compute(
+                        new_list_price, self.order_id.pricelist_id.currency_id)
+                discount = new_list_price - price
+                if discount > 0:
+                    self.discount_fixed = discount
+                    self.discount = 0.0
+        return res
 
     @api.constrains('discount', 'discount_fixed')
     def _check_only_one_discount(self):
