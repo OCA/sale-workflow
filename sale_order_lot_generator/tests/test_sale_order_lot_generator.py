7f209ecf85338badb8e8c4039aa9dffbaa22ceba
David Beal <david.beal@akretion.com>
2020-08-19 12:32:18 +0200

    MIGR 12.0 sale_order_lot_generator

--- sale_order_lot_generator/tests/test_sale_order_lot_generator.py
+++ sale_order_lot_generator/tests/test_sale_order_lot_generator.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion, Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,26 +6,27 @@ import odoo.tests.common as test_common
 
 class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
     def setUp(self):
-        super(TestSaleOrderLotGenerator, self).setUp()
-        self.product_5b = self.env.ref("product.product_product_5b")
-        self.product_6 = self.env.ref("product.product_product_6")
-        self.product_5b.write(
+        super().setUp()
+        self.prd_flipover = self.env.ref("product.product_product_20")
+        self.prd_acoustic = self.env.ref("product.product_product_25")
+        self.prd_flipover.write(
             {"tracking": "lot", "type": "product", "auto_generate_prodlot": True}
         )
-        self.product_6.write(
+        self.prd_acoustic.write(
             {"tracking": "lot", "type": "product", "auto_generate_prodlot": True}
         )
 
     def test_sale_order_lot_generator(self):
         # create order
         self.order1 = self.env["sale.order"].create(
-            {"partner_id": self.env.ref("base.res_partner_1").id,}
+            # Lumber partner
+            {"partner_id": self.env.ref("base.res_partner_18").id}
         )
         self.sol1 = self.env["sale.order.line"].create(
             {
                 "name": "sol1",
                 "order_id": self.order1.id,
-                "product_id": self.product_5b.id,
+                "product_id": self.prd_flipover.id,
                 "product_uom_qty": 1,
             }
         )
@@ -39,15 +39,14 @@ class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
             {
                 "name": "sol1",
                 "order_id": self.order1.id,
-                "product_id": self.product_6.id,
+                "product_id": self.prd_acoustic.id,
                 "product_uom_qty": 1,
             }
         )
         lot_number = "%s-%03d" % (self.order1.name, 2)
         self.assertEqual(self.sol2.lot_id.name, lot_number)
-        picking = self.order1.picking_ids
-        for move in picking.move_lines:
-            if move.product_id.id == self.product_5b.id:
-                self.assertEqual(move.restrict_lot_id, self.sol1.lot_id)
-            if move.product_id.id == self.product_6.id:
-                self.assertEqual(move.restrict_lot_id, self.sol2.lot_id)
+        for line in self.order1.picking_ids.move_line_ids:
+            if line.product_id.id == self.prd_flipover.id:
+                self.assertEqual(line.lot_id, self.sol1.lot_id)
+            if line.product_id.id == self.prd_acoustic.id:
+                self.assertEqual(line.restrict_lot_id, self.sol2.lot_id)
