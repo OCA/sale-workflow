9359101205d41707526adb33eda0146e9436a3c9
David Beal <david.beal@akretion.com>
2020-08-19 12:32:18 +0200

    MIGR 12.0 sale_order_lot_generator

--- sale_order_lot_generator/models/product.py
+++ sale_order_lot_generator/models/product.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 #  @author Adrien CHAUSSENDE <adrien.chaussende@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -9,7 +8,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     auto_generate_prodlot = fields.Boolean(
-        string="Lot Generation Auto",
+        string="Auto Generate Lot",
         help="Forces to specifiy a Serial Number for all "
         "lines containing this product since the confirmation "
         "of the Sale Order",
