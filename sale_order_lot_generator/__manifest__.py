b9f5c99f82eefb7fd84f0dfb4928d2529f18f0bb
David Beal <david.beal@akretion.com>
2020-08-19 12:32:18 +0200

    MIGR 12.0 sale_order_lot_generator

--- sale_order_lot_generator/__manifest__.py
+++ sale_order_lot_generator/__manifest__.py
@@ -1,15 +1,15 @@
-# coding: utf-8
-#   @author Valentin CHEMIERE <valentin.chemiere@akretion.com>
+#   @author Valentin CHEMIERE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "sale_order_lot_generator",
-    "version": "10.0.0.0.1",
-    "author": "Akretion",
-    "website": "http://www.akretion.com",
+    "name": "Sale Order Lot Generator",
+    "version": "12.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "category": "Sale",
     "depends": ["sale_order_lot_selection"],
+    "maintainers": ["florian-dacosta", "mourad-ehm", "bealdav"],
     "data": ["views/product_view.xml"],
     "installable": True,
 }
