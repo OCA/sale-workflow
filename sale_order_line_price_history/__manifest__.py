48db87e933ed5b4a02e5e9804f8f128db1bcb93a
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-08-20 12:56:18 -0400

    [FIX] sale_order_line_price_history: set 'unit price' + 'discount'

--- sale_order_line_price_history/__manifest__.py
+++ sale_order_line_price_history/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order line price history",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "category": "Sales Management",
     "author": "Tecnativa,"
               "Odoo Community Association (OCA)",
