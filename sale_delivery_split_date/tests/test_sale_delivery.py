b3284719a1dcab8688ac7cbf30ed0a36c16cc604
SimoRubi <simone.rubino@agilebg.com>
2020-08-31 16:59:03 +0200

    [MIG] sale_delivery_split_date: Migration to 12.0 Note that sale_delivery_split_date is not compatible with sale_procurement_group_by_commitment_date

--- sale_delivery_split_date/tests/test_sale_delivery.py
+++ sale_delivery_split_date/tests/test_sale_delivery.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.tools import format_date
 from odoo.tests.common import TransactionCase
-from odoo import fields
 import datetime
 
 
@@ -14,11 +13,11 @@ class TestSaleDelivery(TransactionCase):
         customer = self.env.ref('base.res_partner_3')
         p1 = self.env.ref('product.product_product_16')
         p2 = self.env.ref('product.product_product_25')
-        today = datetime.datetime.now()
+        today = datetime.datetime(2020, 1, 1)
         self.dt1 = today + datetime.timedelta(days=9)
         self.dt2 = today + datetime.timedelta(days=10)
-        self.date_sooner = fields.Datetime.to_string(self.dt1)
-        self.date_later = fields.Datetime.to_string(self.dt2)
+        self.date_sooner = self.dt1
+        self.date_later = self.dt2
         self.so = self._create_sale_order(customer)
         self.so_line1 = self._create_sale_order_line(
             self.so, p1, 10, 100.0, self.dt1)
@@ -35,7 +34,7 @@ class TestSaleDelivery(TransactionCase):
             'order_id': sale.id,
             'price_unit': price,
             'product_uom_qty': qty,
-            'requested_date': date})
+            'commitment_date': date})
 
     def test_check_single_date(self):
         self.assertEqual(
@@ -46,21 +45,22 @@ class TestSaleDelivery(TransactionCase):
             len(self.so.picking_ids), 1,
             "There must be 1 picking for the SO when confirmed")
         self.assertEqual(
-            self.so.picking_ids[0].min_date[:10],
+            self.so.picking_ids[0].scheduled_date.date(),
             self.so.picking_ids[0].min_dt)
         self.assertEqual(
-            self.so.picking_ids[0].min_date, self.date_sooner,
+            self.so.picking_ids[0].scheduled_date, self.date_sooner,
             "The picking must be planned at the expected date")
         self.assertEqual(
             self.so_line1.procurement_group_id,
             self.so_line2.procurement_group_id,
             "The procurement group must be the same")
         self.assertIn(
-            self.date_sooner[:10], self.so_line1.procurement_group_id.name)
+            format_date(self.env, self.date_sooner.date()),
+            self.so_line1.procurement_group_id.name)
 
     def test_check_multiple_dates(self):
         # Change the date of the second line
-        self.so_line2.requested_date = self.dt2
+        self.so_line2.commitment_date = self.dt2
         self.assertEqual(
             len(self.so.picking_ids), 0,
             "There must not be pickings for the SO when draft")
@@ -68,32 +68,34 @@ class TestSaleDelivery(TransactionCase):
         self.assertEqual(
             len(self.so.picking_ids), 2,
             "There must be 2 pickings for the SO when confirmed")
-        sorted_pickings = self.so.picking_ids.sorted(lambda x: x.min_date)
+        sorted_pickings = self.so.picking_ids.sorted(lambda x: x.scheduled_date)
         self.assertEqual(
-            self.so.picking_ids[0].min_date[:10],
+            self.so.picking_ids[0].scheduled_date.date(),
             self.so.picking_ids[0].min_dt)
         self.assertEqual(
-            sorted_pickings[0].min_date, self.date_sooner,
+            sorted_pickings[0].scheduled_date, self.date_sooner,
             "The first picking must be planned at the soonest date")
         self.assertEqual(
-            self.so.picking_ids[1].min_date[:10],
+            self.so.picking_ids[1].scheduled_date.date(),
             self.so.picking_ids[1].min_dt)
         self.assertEqual(
-            sorted_pickings[1].min_date, self.date_later,
+            sorted_pickings[1].scheduled_date, self.date_later,
             "The second picking must be planned at the latest date")
         self.assertNotEqual(
             self.so_line1.procurement_group_id,
             self.so_line2.procurement_group_id,
             "The procurement group must be different")
         self.assertIn(
-            self.date_sooner[:10], self.so_line1.procurement_group_id.name)
+            format_date(self.env, self.date_sooner.date()),
+            self.so_line1.procurement_group_id.name)
         self.assertIn(
-            self.date_later[:10], self.so_line2.procurement_group_id.name)
+            format_date(self.env, self.date_later.date()),
+            self.so_line2.procurement_group_id.name)
 
     def test_check_same_dates(self):
         # Change the date of the second line by just adding 1 hour
         same_date = self.dt1 + datetime.timedelta(hours=1)
-        self.so_line2.requested_date = same_date
+        self.so_line2.commitment_date = same_date
         self.assertEqual(
             len(self.so.picking_ids), 0,
             "There must not be pickings for the SO when draft")
@@ -102,14 +104,15 @@ class TestSaleDelivery(TransactionCase):
             len(self.so.picking_ids), 1,
             "There must be only one picking for the SO when confirmed")
         self.assertEqual(
-            self.so.picking_ids[0].min_date[:10],
+            self.so.picking_ids[0].scheduled_date.date(),
             self.so.picking_ids[0].min_dt)
         self.assertEqual(
-            self.so.picking_ids.min_date, self.date_sooner,
+            self.so.picking_ids.scheduled_date, self.date_sooner,
             "The picking must be planned at the expected date")
         self.assertEqual(
             self.so_line1.procurement_group_id,
             self.so_line2.procurement_group_id,
             "The procurement group must be the same")
         self.assertIn(
-            self.date_sooner[:10], self.so_line1.procurement_group_id.name)
+            format_date(self.env, self.date_sooner.date()),
+            self.so_line1.procurement_group_id.name)
