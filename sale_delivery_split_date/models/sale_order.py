474b5e7afb6bea7d317f76a72e4f9f5959f9988a
SimoRubi <simone.rubino@agilebg.com>
2020-08-31 16:59:03 +0200

    [MIG] sale_delivery_split_date: Migration to 12.0 Note that sale_delivery_split_date is not compatible with sale_procurement_group_by_commitment_date

--- sale_delivery_split_date/models/sale_order.py
+++ sale_delivery_split_date/models/sale_order.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+from odoo.tools import format_date
 
 
 class SaleOrder(models.Model):
@@ -13,6 +13,6 @@ class SaleOrder(models.Model):
     def _prepare_procurement_group_by_line(self, line):
         vals = super(SaleOrder, self)._prepare_procurement_group_by_line(line)
         if line._get_procurement_group_key()[0] == 24:
-            if line.requested_date:
-                vals['name'] += '/' + line.requested_date[:10]
+            if line.commitment_date:
+                vals['name'] += '/' + format_date(line.env, line.commitment_date.date())
         return vals
---
+++ sale_delivery_split_date/readme/ROADMAP.rst
@@ -0,0 +1,1 @@
+* Incompatible with `sale_procurement_group_by_commitment_date <https://github.com/OCA/sale-workflow/tree/12.0/sale_procurement_group_by_commitment_date>`_
