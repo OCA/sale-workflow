99e8fdf2fff8702eaf1457e2ededce2116e2dd82
Kitti U <kittiu@gmail.com>
2020-09-03 16:58:33 +0700

    [IMP] : black, isort, prettier

--- sale_invoice_plan/wizard/sale_make_planned_invoice.py
+++ sale_invoice_plan/wizard/sale_make_planned_invoice.py
@@ -4,22 +4,24 @@ from odoo import api, models
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
-    _name = 'sale.make.planned.invoice'
-    _description = 'Wizard when create invoice by plan'
+    _name = "sale.make.planned.invoice"
+    _description = "Wizard when create invoice by plan"
 
     @api.multi
     def create_invoices_by_plan(self):
-        sale = self.env['sale.order'].browse(self._context.get('active_id'))
+        sale = self.env["sale.order"].browse(self._context.get("active_id"))
         sale.ensure_one()
-        MakeInvoice = self.env['sale.advance.payment.inv']
-        invoice_plans = self._context.get('all_remain_invoices') and \
-            sale.invoice_plan_ids.filtered(lambda l: not l.invoiced) or \
-            sale.invoice_plan_ids.filtered('to_invoice')
-        for plan in invoice_plans.sorted('installment'):
-            makeinv_wizard = {'advance_payment_method': 'all'}
-            if plan.invoice_type == 'advance':
-                makeinv_wizard['advance_payment_method'] = 'percentage'
-                makeinv_wizard['amount'] = plan.percent
+        MakeInvoice = self.env["sale.advance.payment.inv"]
+        invoice_plans = (
+            self._context.get("all_remain_invoices")
+            and sale.invoice_plan_ids.filtered(lambda l: not l.invoiced)
+            or sale.invoice_plan_ids.filtered("to_invoice")
+        )
+        for plan in invoice_plans.sorted("installment"):
+            makeinv_wizard = {"advance_payment_method": "all"}
+            if plan.invoice_type == "advance":
+                makeinv_wizard["advance_payment_method"] = "percentage"
+                makeinv_wizard["amount"] = plan.percent
             makeinvoice = MakeInvoice.create(makeinv_wizard)
             makeinvoice.with_context(invoice_plan_id=plan.id).create_invoices()
-        return {'type': 'ir.actions.act_window_close'}
+        return {"type": "ir.actions.act_window_close"}
