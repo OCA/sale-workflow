6691d8b8c711afd346da10f5e7ce041be3dd8934
Kitti U <kittiu@gmail.com>
2020-09-03 16:58:33 +0700

    [IMP] : black, isort, prettier

--- sale_invoice_plan/wizard/sale_make_invoice_advance.py
+++ sale_invoice_plan/wizard/sale_make_invoice_advance.py
@@ -4,13 +4,13 @@ from odoo import api, models
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
-    _inherit = 'sale.advance.payment.inv'
+    _inherit = "sale.advance.payment.inv"
 
     @api.multi
     def _create_invoice(self, order, so_line, amount):
         invoice = super()._create_invoice(order, so_line, amount)
-        invoice_plan_id = self._context.get('invoice_plan_id')
+        invoice_plan_id = self._context.get("invoice_plan_id")
         if invoice_plan_id:
-            plan = self.env['sale.invoice.plan'].browse(invoice_plan_id)
+            plan = self.env["sale.invoice.plan"].browse(invoice_plan_id)
             plan.invoice_ids += invoice
         return invoice
