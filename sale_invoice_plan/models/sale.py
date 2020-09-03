9623b8c05444571bcd2f518fb177bd1b1c66bd74
Kitti U <kittiu@gmail.com>
2020-09-03 17:04:04 +0700

    [13.0][MIG] sale_invoice_plan

--- sale_invoice_plan/models/sale.py
+++ sale_invoice_plan/models/sale.py
@@ -4,12 +4,10 @@ from dateutil.relativedelta import relativedelta
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.float_utils import float_compare, float_round as round
-
-from odoo.addons import decimal_precision as dp
+from odoo.tools.float_utils import float_compare, float_round
 
 
-class SaleOder(models.Model):
+class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     invoice_plan_ids = fields.One2many(
@@ -27,14 +25,18 @@ class SaleOder(models.Model):
         help="At least one invoice plan line pending to create invoice",
     )
 
-    @api.multi
     def _compute_ip_invoice_plan(self):
         for rec in self:
-            rec.ip_invoice_plan = (
-                rec.use_invoice_plan
-                and rec.invoice_plan_ids
-                and len(rec.invoice_plan_ids.filtered(lambda l: not l.invoiced))
-            )
+            has_invoice_plan = rec.use_invoice_plan and rec.invoice_plan_ids
+            to_invoice = rec.invoice_plan_ids.filtered(lambda l: not l.invoiced)
+            if rec.state == "sale" and has_invoice_plan and to_invoice:
+                if rec.invoice_status == "to invoice" or (
+                    rec.invoice_status == "no"
+                    and "advance" in to_invoice.mapped("invoice_type")
+                ):
+                    rec.ip_invoice_plan = True
+                    continue
+            rec.ip_invoice_plan = False
 
     @api.constrains("state")
     def _check_invoice_plan(self):
@@ -45,13 +47,11 @@ class SaleOder(models.Model):
                         _("Please fill percentage for all invoice plan lines")
                     )
 
-    @api.multi
     def action_confirm(self):
         if self.filtered(lambda r: r.use_invoice_plan and not r.invoice_plan_ids):
             raise UserError(_("Use Invoice Plan selected, but no plan created"))
         return super().action_confirm()
 
-    @api.multi
     def create_invoice_plan(
         self, num_installment, installment_date, interval, interval_type, advance
     ):
@@ -60,7 +60,7 @@ class SaleOder(models.Model):
         invoice_plans = []
         Decimal = self.env["decimal.precision"]
         prec = Decimal.precision_get("Product Unit of Measure")
-        percent = round(1.0 / num_installment * 100, prec)
+        percent = float_round(1.0 / num_installment * 100, prec)
         percent_last = 100 - (percent * (num_installment - 1))
         # Advance
         if advance:
@@ -92,7 +92,6 @@ class SaleOder(models.Model):
         self.write({"invoice_plan_ids": invoice_plans})
         return True
 
-    @api.multi
     def remove_invoice_plan(self):
         self.ensure_one()
         self.invoice_plan_ids.unlink()
@@ -110,18 +109,16 @@ class SaleOder(models.Model):
         next_date = fields.Date.to_string(next_date)
         return next_date
 
-    @api.multi
-    def action_invoice_create(self, grouped=False, final=False):
-        inv_ids = super().action_invoice_create(grouped=grouped, final=final)
+    def _create_invoices(self, grouped=False, final=False):
+        moves = super()._create_invoices(grouped=grouped, final=final)
         invoice_plan_id = self._context.get("invoice_plan_id")
         if invoice_plan_id:
             plan = self.env["sale.invoice.plan"].browse(invoice_plan_id)
-            invoices = self.env["account.invoice"].browse(inv_ids)
-            invoices.ensure_one()  # Expect 1 invoice for 1 invoice plan
-            plan._compute_new_invoice_quantity(invoices[0])
-            invoices[0].date_invoice = plan.plan_date
-            plan.invoice_ids += invoices
-        return inv_ids
+            moves.ensure_one()  # Expect 1 invoice for 1 invoice plan
+            plan._compute_new_invoice_quantity(moves[0])
+            moves.invoice_date = plan.plan_date
+            plan.invoice_move_ids += moves
+        return moves
 
 
 class SaleInvoicePlan(models.Model):
@@ -156,10 +153,10 @@ class SaleInvoicePlan(models.Model):
         store=True,
         index=True,
     )
-    installment = fields.Integer(string="Installment",)
-    plan_date = fields.Date(string="Plan Date", required=True,)
+    installment = fields.Integer(string="Installment")
+    plan_date = fields.Date(string="Plan Date", required=True)
     invoice_type = fields.Selection(
-        [("advance", "Advance"), ("installment", "Installment"),],
+        [("advance", "Advance"), ("installment", "Installment")],
         string="Type",
         required=True,
         default="installment",
@@ -171,14 +168,14 @@ class SaleInvoicePlan(models.Model):
     )
     percent = fields.Float(
         string="Percent",
-        digits=dp.get_precision("Product Unit of Measure"),
+        digits="Product Unit of Measure",
         help="This percent will be used to calculate new quantity",
     )
-    invoice_ids = fields.Many2many(
-        "account.invoice",
+    invoice_move_ids = fields.Many2many(
+        "account.move",
         relation="sale_invoice_plan_invoice_rel",
         column1="plan_id",
-        column2="invoice_id",
+        column2="move_id",
         string="Invoices",
         readonly=True,
     )
@@ -200,46 +197,44 @@ class SaleInvoicePlan(models.Model):
         )
     ]
 
-    @api.multi
     def _compute_to_invoice(self):
-        """ If any invoice is in draft/open/paid do not allow to create inv
-            Only if previous to_invoice is False, it is eligible to_invoice
+        """ If any invoice is in draft/open/paid do not allow to create inv.
+            Only if previous to_invoice is False, it is eligible to_invoice.
         """
-        for rec in self.sorted("installment"):
+        for rec in self:
             rec.to_invoice = False
-            if rec.sale_id.state != "sale":  # Not confirmed, no to_invoice
+        for rec in self.sorted("installment"):
+            if rec.state != "sale":  # Not confirmed, no to_invoice
                 continue
             if not rec.invoiced:
                 rec.to_invoice = True
                 break
 
-    @api.multi
     def _compute_invoiced(self):
         for rec in self:
-            invoiced = rec.invoice_ids.filtered(
-                lambda l: l.state in ("draft", "open", "paid")
+            invoiced = rec.invoice_move_ids.filtered(
+                lambda l: l.state in ("draft", "posted")
             )
             rec.invoiced = invoiced and True or False
 
-    @api.multi
     def _compute_last(self):
         for rec in self:
             last = max(rec.sale_id.invoice_plan_ids.mapped("installment"))
             rec.last = rec.installment == last
 
-    @api.multi
-    def _compute_new_invoice_quantity(self, invoice):
+    def _compute_new_invoice_quantity(self, invoice_move):
         self.ensure_one()
         if self.last:  # For last install, let the system do the calc.
             return
         percent = self.percent
-        for line in invoice.invoice_line_ids:
+        move = invoice_move.with_context({"check_move_validity": False})
+        for line in move.invoice_line_ids:
             assert (
                 len(line.sale_line_ids) >= 0
             ), "No matched order line for invoice line"
             order_line = fields.first(line.sale_line_ids)
-            if order_line.is_downpayment:
-                line.write({"quantity": -percent / 100})  # based on 1 unit
+            if order_line.is_downpayment:  # based on 1 unit
+                line.write({"quantity": -percent / 100})
             else:
                 plan_qty = order_line.product_uom_qty * (percent / 100)
                 prec = order_line.product_uom.rounding
@@ -252,4 +247,5 @@ class SaleInvoicePlan(models.Model):
                         % (plan_qty, line.quantity)
                     )
                 line.write({"quantity": plan_qty})
-        invoice.compute_taxes()
+        # Call this method to recompute dr/cr lines
+        move._move_autocomplete_invoice_lines_values()
--- sale_invoice_plan/wizard/sale_make_planned_invoice.py
+++ sale_invoice_plan/wizard/sale_make_planned_invoice.py
@@ -1,13 +1,12 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import api, models
+from odoo import models
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _name = "sale.make.planned.invoice"
     _description = "Wizard when create invoice by plan"
 
-    @api.multi
     def create_invoices_by_plan(self):
         sale = self.env["sale.order"].browse(self._context.get("active_id"))
         sale.ensure_one()
@@ -18,7 +17,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
             or sale.invoice_plan_ids.filtered("to_invoice")
         )
         for plan in invoice_plans.sorted("installment"):
-            makeinv_wizard = {"advance_payment_method": "all"}
+            makeinv_wizard = {"advance_payment_method": "delivered"}
             if plan.invoice_type == "advance":
                 makeinv_wizard["advance_payment_method"] = "percentage"
                 makeinv_wizard["amount"] = plan.percent
