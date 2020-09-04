7e50490fef6f053a1f7804ceac2eb47eb8dfb1ef
Renato Lima <renatonlima@gmail.com>
2020-09-04 16:26:01 -0300

    [FIX] pep8 and pylint

--- sale_cancel_reason/tests/test_sale_cancel_reason.py
+++ sale_cancel_reason/tests/test_sale_cancel_reason.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013 Guewen Baconnier, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo.tests.common import TransactionCase
 
 
