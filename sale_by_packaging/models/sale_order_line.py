1c6401ae43d2be648dd3725a8ca639994be1440d
Simone Orsi <simahawk@gmail.com>
2020-09-07 15:07:22 +0200

    sale_by_packaging: no pkging override if already set
    Before this change the packaging was always overridden
    hence the user had not control on the packaging to be used on SO line.

--- sale_by_packaging/models/sale_order_line.py
+++ sale_by_packaging/models/sale_order_line.py
@@ -122,6 +122,9 @@ class SaleOrderLine(models.Model):
         ):
             return super().write(vals)
         for line in self:
+            if line.product_packaging:
+                # don't touch it if set already
+                continue
             line_vals = vals.copy()
             line_vals.update(line._write_auto_assign_packaging(line_vals))
             super(SaleOrderLine, line).write(line_vals)
@@ -158,7 +161,8 @@ class SaleOrderLine(models.Model):
     def create(self, vals):
         """Auto assign packaging if needed"""
         # Fill the packaging if they are empty and the quantity is a multiple
-        vals.update(self._create_auto_assign_packaging(vals))
+        if not vals.get("product_packaging"):
+            vals.update(self._create_auto_assign_packaging(vals))
         return super().create(vals)
 
     @api.model
