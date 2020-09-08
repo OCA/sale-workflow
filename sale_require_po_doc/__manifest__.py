31946b191587a29d029769a823bbb33f966b64a9
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-09-09 02:17:40 +0530

    [ADD] Added new module to check sale documentation.

---
+++ sale_require_po_doc/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Orders Require PO or Sales Documentation",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "depends": [
+        "sale_management", "purchase"
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+        "views/sale_order_view.xml",
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
---
+++ sale_require_po_doc/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+The goal of this development is to create a field for validation to notate which customers require a purchase order in order to create an invoice.
