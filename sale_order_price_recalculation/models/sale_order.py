57fb623c65dc1b9150eef72ab9655a9f76f7bd56
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-09 19:33:20 +0200

    [MIG] sale_order_price_recalculation: Migration to 13.0

--- sale_order_price_recalculation/models/sale_order.py
+++ sale_order_price_recalculation/models/sale_order.py
@@ -1,5 +1,5 @@
 # Copyright 2014 Carlos Sánchez Cifuentes <csanchez@grupovermon.com>
-# Copyright 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# Copyright 2015-2020 Tecnativa - Pedro M. Baeza
 # Copyright 2015 Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
 # Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
@@ -10,26 +10,26 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.multi
-    def recalculate_prices(self):
+    @api.model
+    def _recompute_form_line_vals(self, fields):
+        """Helper method for writing in line certain recomputed by onchanges fields."""
         for line in self.mapped("order_line"):
-            dict = line._convert_to_write(line.read()[0])
+            vals = line._convert_to_write(line.read()[0])
             if "product_tmpl_id" in line._fields:
-                dict["product_tmpl_id"] = line.product_tmpl_id
-            line2 = self.env["sale.order.line"].new(dict)
+                vals["product_tmpl_id"] = line.product_tmpl_id
             # we make this to isolate changed values:
-            line2.product_uom_change()
-            line2._onchange_discount()
-            line.write(
-                {"price_unit": line2.price_unit, "discount": line2.discount,}
+            line2 = self.env["sale.order.line"].new(vals)
+            line2.env.add_to_compute(
+                line2.product_id._fields["price"], line2.product_id
             )
+            line2.product_id_change()
+            line2._onchange_discount()
+            line.write({field: line2[field] for field in fields})
+
+    def recalculate_prices(self):
+        self._recompute_form_line_vals(["price_unit", "discount"])
         return True
 
-    @api.multi
     def recalculate_names(self):
-        for line in self.mapped("order_line").filtered("product_id"):
-            # we make this to isolate changed values:
-            line2 = self.env["sale.order.line"].new({"product_id": line.product_id,})
-            line2.product_id_change()
-            line.name = line2.name
+        self._recompute_form_line_vals(["name"])
         return True
