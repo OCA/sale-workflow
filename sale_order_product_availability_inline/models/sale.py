0081a833c38d861e6b992a4403d9b4866c436d08
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-09-21 11:40:40 -0400

    [ADD] sale_order_product_availability_inline: new module
    Show product availability in sales order lines product drop-down.

---
+++ sale_order_product_availability_inline/models/sale.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange("product_id")
+    def product_id_change(self):
+        so_line = self
+        if self.env.context.get("so_product_stock_inline"):
+            so_line = self.with_context(so_product_stock_inline=False)
+        return super(SaleOrderLine, so_line).product_id_change()
