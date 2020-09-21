82efcbea81ee02ceb3fa28ac6466c217bbef2b63
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-09-21 11:40:40 -0400

    [ADD] sale_order_product_availability_inline: new module
    Show product availability in sales order lines product drop-down.

---
+++ sale_stock_picking_blocking/models/sale_stock_picking_blocking_reason.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleDeliveryBlockReason(models.Model):
+    _name = "sale.delivery.block.reason"
+    _description = "Sale Delivery Block Reason"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    sale_order_ids = fields.One2many(
+        comodel_name="sale.order",
+        inverse_name="delivery_block_id",
+        string="Sale Orders",
+        readonly=True,
+    )
