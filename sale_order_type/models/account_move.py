512b7e3f3a30207a4d54b241703d4d9872aa1b4e
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-23 00:58:44 +0200

    [FIX] sale_order_type: Use record instead of self

--- sale_order_type/models/account_move.py
+++ sale_order_type/models/account_move.py
@@ -29,7 +29,7 @@ class AccountMove(models.Model):
                     record.partner_id.with_context(
                         force_company=record.company_id.id
                     ).sale_type
-                    or self.partner_id.commercial_partner_id.with_context(
+                    or record.partner_id.commercial_partner_id.with_context(
                         force_company=record.company_id.id
                     ).sale_type
                 )
