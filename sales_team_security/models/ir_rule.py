921766e47822a0643217590814735acc296ee3af
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-24 22:08:59 +0200

    [IMP] : black, isort, prettier

--- sales_team_security/models/ir_rule.py
+++ sales_team_security/models/ir_rule.py
@@ -1,21 +1,23 @@
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models, tools, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api, models, tools
 from odoo.osv import expression
 from odoo.tools import config
 
 
 class IrRule(models.Model):
-    _inherit = 'ir.rule'
+    _inherit = "ir.rule"
 
     @api.model
     @tools.conditional(
-        'xml' not in config['dev_mode'],
+        "xml" not in config["dev_mode"],
         tools.ormcache(
-            'self._uid', 'model_name', 'mode',
-            'tuple(self._context.get(k) for k in self._compute_domain_keys())'
-        )
+            "self._uid",
+            "model_name",
+            "mode",
+            "tuple(self._context.get(k) for k in self._compute_domain_keys())",
+        ),
     )
     def _compute_domain(self, model_name, mode="read"):
         """Inject extra domain for restricting partners when the user
@@ -29,10 +31,10 @@ class IrRule(models.Model):
         if model_name == "res.partner" and user.id != SUPERUSER_ID:
             if user.has_group(group1) and not user.has_group(group3):
                 extra_domain = [
-                    '|',
-                    ('message_partner_ids', 'in', user.partner_id.ids),
-                    '|',
-                    ('id', '=', user.partner_id.id),
+                    "|",
+                    ("message_partner_ids", "in", user.partner_id.ids),
+                    "|",
+                    ("id", "=", user.partner_id.id),
                 ]
                 if user.has_group(group2):
                     extra_domain += [
