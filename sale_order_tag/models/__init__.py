47ac23d518904c7b34494c5d14aff0b308f91d70
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2020-09-24 17:30:51 -0500

    [ADD] sale_order_tag
    This module adds tags to sales order. The tags are for independant to sales orders and not related to the opportunities like the other tag field that exists.
    
    [IMP] domain fix and better readme description
    
    Update __manifest__.py
    
    Co-authored-by: Bhavesh Odedra <bodedra@opensourceintegrators.com>
    
    Update tag string
    
    Co-authored-by: Bhavesh Odedra <bodedra@opensourceintegrators.com>
    
    [FIX] Readme LINT
    
    [FIX] Reviewer suggestions
    
    - Removed plural on  saleordertag class
    - Moved menu item to proper xml
    - Added company_id field to tags with rule
    - Moved sequence to data
    - Additional code cleanup
    
    [FIX] Typo

---
+++ sale_order_tag/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020 Patrick Wilson <patrickraymondwilson@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import sale_order
+from . import sale_order_tag
---
+++ sale_order_tag/readme/USAGE.rst
@@ -0,0 +1,4 @@
+To use this module, you need to:
+
+#. Have Manager rights for Sales group to create tags.
+#. Go to the sales order form and add the necessary tags.
