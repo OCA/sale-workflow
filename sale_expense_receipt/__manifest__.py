3709a21fffddd50355508873aa4c3cfefe33fcb9
Khalid <m.khalid@serpentcs.com>
2020-09-30 21:17:18 +0530

    [ADD] sale_expense_receipt

---
+++ sale_expense_receipt/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Expense Receipt",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": """Open Source Integrators,
+        Serpent Consulting Services,
+        Odoo Community Association (OCA)""",
+    "summary": """This module allows you to attach all the receipts of the
+        expenses when invoicing a sale order and send them with the
+        invoice.""",
+    "category": "Sales",
+    "maintainers": ["Khalid-SerpentCS"],
+    "website": "https://github.com/OCA/hr",
+    "depends": [
+        "sale_expense"
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
---
+++ sale_expense_receipt/wizard/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import sale_make_invoice_advance
