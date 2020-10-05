4deeef328fe14c35ee2a25288d379a8dc661bb91
sergiocorato <sergiocorato@gmail.com>
2020-10-05 18:49:48 +0200

    [FIX] string duplicated

--- sale_double_validation/models/company.py
+++ sale_double_validation/models/company.py
@@ -12,13 +12,13 @@ class Company(models.Model):
             ('one_step', 'Confirm sale orders in one step'),
             ('two_step', 'Get 2 levels of approvals to confirm a sale order')
         ],
-        string="Levels of Approvals",
+        string="Levels of Sale Order Approvals",
         default='one_step',
         help="Provide a double validation mechanism for purchases"
     )
 
     so_double_validation_amount = fields.Monetary(
-        string='Double validation amount',
+        string='Sale Order double validation amount',
         default=5000,
         help="Minimum amount for which a double validation is required"
     )
