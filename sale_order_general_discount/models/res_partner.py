e7facc6ec9366ac409b02f21251f8030d4512925
Oriana Maita <o.maita@antilhue.com>
2020-10-07 13:17:27 -0300

    [IMP] sale_order_general_discount: black, isort, prettier

--- sale_order_general_discount/models/res_partner.py
+++ sale_order_general_discount/models/res_partner.py
@@ -2,14 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+
 from odoo.addons import decimal_precision as dp
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     sale_discount = fields.Float(
-        digits=dp.get_precision('Discount'),
-        string='Discount (%)',
+        digits=dp.get_precision("Discount"),
+        string="Discount (%)",
         company_dependent=True,
     )
