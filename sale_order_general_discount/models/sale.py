fa3c75b4ca12e4548ff94de2b05e9252b2de94a1
Oriana Maita <o.maita@antilhue.com>
2020-10-07 13:17:27 -0300

    [IMP] sale_order_general_discount: black, isort, prettier

--- sale_order_general_discount/models/sale.py
+++ sale_order_general_discount/models/sale.py
@@ -1,33 +1,35 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
-from odoo.addons import decimal_precision as dp
 from lxml import etree
+
+from odoo import api, fields, models
+
+from odoo.addons import decimal_precision as dp
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     general_discount = fields.Float(
-        digits=dp.get_precision('Discount'),
-        string='Discount (%)',
+        digits=dp.get_precision("Discount"), string="Discount (%)",
     )
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_id(self):
         super().onchange_partner_id()
         self.general_discount = self.partner_id.sale_discount
         return
 
-    @api.onchange('general_discount')
+    @api.onchange("general_discount")
     def onchange_general_discount(self):
-        self.mapped('order_line').update({
-            'discount': self.general_discount,
-        })
+        self.mapped("order_line").update(
+            {"discount": self.general_discount,}
+        )
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
-                        submenu=False):
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
         """The purpose of this is to write a context on "order_line" field
          respecting other contexts on this field.
          There is a PR (https://github.com/odoo/odoo/pull/26607) to odoo for
@@ -35,32 +37,31 @@ class SaleOrder(models.Model):
          in the field.
          """
         res = super(SaleOrder, self).fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar,
-            submenu=submenu,
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu,
         )
-        if view_type == 'form':
-            order_xml = etree.XML(res['arch'])
+        if view_type == "form":
+            order_xml = etree.XML(res["arch"])
             order_line_fields = order_xml.xpath("//field[@name='order_line']")
             if order_line_fields:
                 order_line_field = order_line_fields[0]
                 context = order_line_field.attrib.get("context", "{}").replace(
                     "{", "{'default_discount': general_discount, ", 1,
                 )
-                order_line_field.attrib['context'] = context
-                res['arch'] = etree.tostring(order_xml)
+                order_line_field.attrib["context"] = context
+                res["arch"] = etree.tostring(order_xml)
         return res
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.model
     def create(self, vals):
         """Apply general discount for sale order lines which are not created
         from sale order form view.
         """
-        if 'discount' not in vals and 'order_id' in vals:
-            sale_order = self.env['sale.order'].browse(vals['order_id'])
+        if "discount" not in vals and "order_id" in vals:
+            sale_order = self.env["sale.order"].browse(vals["order_id"])
             if sale_order.general_discount:
-                vals['discount'] = sale_order.general_discount
+                vals["discount"] = sale_order.general_discount
         return super().create(vals)
--- sale_order_general_discount/tests/test_sale_order_general_discount.py
+++ sale_order_general_discount/tests/test_sale_order_general_discount.py
@@ -1,39 +1,44 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import SavepointCase
 from lxml import etree
+
+from odoo.tests import SavepointCase
 
 
 class TestSaleOrderLineInput(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'Test',
-            'sale_discount': 10.0,
-        })
-        cls.product = cls.env['product.product'].create({
-            'name': 'test_product',
-            'type': 'service',
-        })
-        cls.order = cls.env['sale.order'].create({
-            'partner_id': cls.partner.id,
-            'order_line': [(0, 0, {
-                'name': cls.product.name,
-                'product_id': cls.product.id,
-                'product_uom_qty': 1,
-                'product_uom': cls.product.uom_id.id,
-                'price_unit': 1000.00,
-            })],
-            'pricelist_id': cls.env.ref('product.list0').id,
-        })
-        cls.View = cls.env['ir.ui.view']
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test", "sale_discount": 10.0,}
+        )
+        cls.product = cls.env["product.product"].create(
+            {"name": "test_product", "type": "service",}
+        )
+        cls.order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 1000.00,
+                        },
+                    )
+                ],
+                "pricelist_id": cls.env.ref("product.list0").id,
+            }
+        )
+        cls.View = cls.env["ir.ui.view"]
 
     def test_default_partner_discount(self):
         self.order.onchange_partner_id()
-        self.assertEqual(
-            self.order.general_discount, self.partner.sale_discount)
+        self.assertEqual(self.order.general_discount, self.partner.sale_discount)
 
     def test_sale_order_values(self):
         self.order.general_discount = 10
@@ -41,7 +46,7 @@ class TestSaleOrderLineInput(SavepointCase):
         self.assertEqual(self.order.order_line.price_reduce, 900.00)
 
     def _get_ctx_from_view(self, res):
-        order_xml = etree.XML(res['arch'])
+        order_xml = etree.XML(res["arch"])
         order_line_path = "//field[@name='order_line']"
         order_line_field = order_xml.xpath(order_line_path)[0]
         return order_line_field.attrib.get("context", "{}")
@@ -49,41 +54,45 @@ class TestSaleOrderLineInput(SavepointCase):
     def test_default_line_discount_value(self):
 
         res = self.order.fields_view_get(
-            view_id=self.env.ref('sale_order_general_discount.'
-                                 'sale_order_general_discount_form_view').id,
-            view_type='form')
+            view_id=self.env.ref(
+                "sale_order_general_discount." "sale_order_general_discount_form_view"
+            ).id,
+            view_type="form",
+        )
         ctx = self._get_ctx_from_view(res)
-        self.assertTrue('default_discount' in ctx)
-        view = self.View.create({
-            'name': "test",
-            'type': "form",
-            'model': 'sale.order',
-            'arch': """
+        self.assertTrue("default_discount" in ctx)
+        view = self.View.create(
+            {
+                "name": "test",
+                "type": "form",
+                "model": "sale.order",
+                "arch": """
                 <data>
                     <field name='order_line'
                         context="{'default_product_uom_qty': 3.0}">
                     </field>
                 </data>
-            """
-        })
-        res = self.order.fields_view_get(view_id=view.id, view_type='form')
+            """,
+            }
+        )
+        res = self.order.fields_view_get(view_id=view.id, view_type="form")
         ctx = self._get_ctx_from_view(res)
-        self.assertTrue('default_discount' in ctx)
+        self.assertTrue("default_discount" in ctx)
 
     def test_sale_order_line_wo_form_view(self):
         self.order.general_discount = 10
         vals = {
-            'name': self.product.name,
-            'product_id': self.product.id,
-            'product_uom_qty': 1,
-            'product_uom': self.product.uom_id.id,
-            'price_unit': 1000.00,
-            'order_id': self.order.id,
+            "name": self.product.name,
+            "product_id": self.product.id,
+            "product_uom_qty": 1,
+            "product_uom": self.product.uom_id.id,
+            "price_unit": 1000.00,
+            "order_id": self.order.id,
         }
-        order_line = self.env['sale.order.line'].create(vals)
+        order_line = self.env["sale.order.line"].create(vals)
         self.assertEqual(order_line.price_subtotal, 900.00)
         self.assertEqual(order_line.discount, 10)
-        vals['discount'] = 20
-        order_line2 = self.env['sale.order.line'].create(vals)
+        vals["discount"] = 20
+        order_line2 = self.env["sale.order.line"].create(vals)
         self.assertEqual(order_line2.price_subtotal, 800.00)
         self.assertEqual(order_line2.discount, 20)
