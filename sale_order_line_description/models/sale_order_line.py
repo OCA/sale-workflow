0ce575a791f9ce24d17d71a4cf05df11e3a846b0
Oriana Maita <o.maita@antilhue.com>
2020-10-08 10:08:03 -0300

    [IMP] sale_order_line_description: black, isort, prettier

--- sale_order_line_description/models/sale_order_line.py
+++ sale_order_line_description/models/sale_order_line.py
@@ -8,19 +8,20 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.multi
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         res = super(SaleOrderLine, self).product_id_change()
         if not self.product_id:  # pragma: no cover
             return res
-        if (self.user_has_groups(
-                'sale_order_line_description.'
-                'group_use_product_description_per_so_line') and
-                self.product_id.description_sale):
+        if (
+            self.user_has_groups(
+                "sale_order_line_description."
+                "group_use_product_description_per_so_line"
+            )
+            and self.product_id.description_sale
+        ):
             product = self.product_id
             if self.order_id.partner_id:
-                product = product.with_context(
-                    lang=self.order_id.partner_id.lang,
-                )
+                product = product.with_context(lang=self.order_id.partner_id.lang,)
             self.name = product.description_sale
         return res
