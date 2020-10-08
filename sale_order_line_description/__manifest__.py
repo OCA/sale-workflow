dcf372658837f631ce0da8a0089e4319db647889
Oriana Maita <o.maita@antilhue.com>
2020-10-08 10:08:03 -0300

    [IMP] sale_order_line_description: black, isort, prettier

--- sale_order_line_description/__manifest__.py
+++ sale_order_line_description/__manifest__.py
@@ -4,16 +4,10 @@
     "name": "Sale order line description",
     "version": "12.0.1.0.0",
     "category": "Sales Management",
-    "author": "Agile Business Group, "
-              "Odoo Community Association (OCA)",
+    "author": "Agile Business Group, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow/",
     "license": "AGPL-3",
-    "depends": [
-        "sale",
-    ],
-    "data": [
-        "security/sale_security.xml",
-        "views/res_config_settings_views.xml",
-    ],
+    "depends": ["sale",],
+    "data": ["security/sale_security.xml", "views/res_config_settings_views.xml",],
     "installable": True,
 }
