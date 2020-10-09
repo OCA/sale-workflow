9e10cc16e7ec4c9ebdd1f0c0a036e676e39342fc
François Honoré <francois.honore@acsone.eu>
2020-10-09 11:15:15 +0200

    [13.0][ADD] sale_by_packaging: add possibility to force the qty during SO line create/update

--- sale_by_packaging/models/product_packaging.py
+++ sale_by_packaging/models/product_packaging.py
@@ -9,3 +9,12 @@ class ProductPackaging(models.Model):
     can_be_sold = fields.Boolean(
         related="packaging_type_id.can_be_sold", readonly=True,
     )
+    force_sale_qty = fields.Boolean(
+        string="Force sale quantity",
+        help="Determine if during the creation of a sale order line, the "
+        "quantity should be forced with a multiple of the packaging.\n"
+        "Example:\n"
+        "You sell a product by packaging of 5 products.\n"
+        "When the user will put 3 as quantity, the system can force the "
+        "quantity to the superior unit (5 for this example).",
+    )
--- sale_by_packaging/tests/test_sale_only_by_packaging.py
+++ sale_by_packaging/tests/test_sale_only_by_packaging.py
@@ -1,7 +1,9 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import fields
 from odoo.exceptions import ValidationError
-from odoo.tests import SavepointCase
+from odoo.tests import Form, SavepointCase
+from odoo.tools import mute_logger
 
 
 class TestSaleProductByPackagingOnly(SavepointCase):
@@ -9,12 +11,14 @@ class TestSaleProductByPackagingOnly(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.SaleOrder = cls.env["sale.order"]
         cls.partner = cls.env.ref("base.res_partner_12")
         cls.product = cls.env.ref("product.product_product_9")
         cls.packaging = cls.env["product.packaging"].create(
             {"name": "Test packaging", "product_id": cls.product.id, "qty": 5.0}
         )
         cls.order = cls.env["sale.order"].create({"partner_id": cls.partner.id})
+        cls.precision = cls.env["decimal.precision"].precision_get("Product Price")
 
     def test_onchange_qty_is_pack_multiple(self):
         order_line = self.env["sale.order.line"].create(
@@ -108,3 +112,63 @@ class TestSaleProductByPackagingOnly(SavepointCase):
                     "product_uom_qty": 2,
                 }
             )
+
+    @mute_logger("odoo.tests.common.onchange")
+    def test_convert_packaging_qty(self):
+        """
+        Test if the function _convert_packaging_qty is correctly applied
+        during SO line create/edit and if qties are corrects.
+        :return:
+        """
+        self.product.sell_only_by_packaging = True
+        packaging = fields.first(self.product.packaging_ids)
+        # For this step, the qty is not forced on the packaging so nothing
+        # should happens if the qty doesn't match with packaging multiple.
+        with Form(self.SaleOrder) as sale_order:
+            sale_order.partner_id = self.partner
+            with sale_order.order_line.new() as so_line:
+                so_line.product_id = self.product
+                so_line.product_packaging = packaging
+                so_line.product_uom_qty = 12
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 12, places=self.precision
+                )
+                so_line.product_uom_qty = 10
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 10, places=self.precision
+                )
+                so_line.product_uom_qty = 36
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 36, places=self.precision
+                )
+        # Now force the qty on the packaging
+        packaging.force_sale_qty = True
+        with Form(self.SaleOrder) as sale_order:
+            sale_order.partner_id = self.partner
+            with sale_order.order_line.new() as so_line:
+                so_line.product_id = self.product
+                so_line.product_packaging = packaging
+                so_line.product_uom_qty = 12
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 15, places=self.precision
+                )
+                so_line.product_uom_qty = 10
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 10, places=self.precision
+                )
+                so_line.product_uom_qty = 8
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 10, places=self.precision
+                )
+                so_line.product_uom_qty = 11
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 15, places=self.precision
+                )
+                so_line.product_uom_qty = 208
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 210, places=self.precision
+                )
+                so_line.product_uom_qty = 209.98
+                self.assertAlmostEqual(
+                    so_line.product_uom_qty, 210, places=self.precision
+                )
