2b69db1918302aad68addd0d4d1f3288c84e948d
Sébastien BEAU <sebastien.beau@akretion.com>
2020-10-09 22:13:34 +0200

    [12.0][MIG] migrate module sale_delivery_state to V12

--- sale_delivery_state/__manifest__.py
+++ sale_delivery_state/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion (http://www.akretion.com).
 # @author Pierrick BRUN <pierrick.brun@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 {
     "name": "Sale delivery State",
     "summary": "Show the delivery state on the sale order",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Akretion, Odoo Community Association (OCA)",
