7a0f5aa18bbd6edb4bbe9a190784d4bfd1c5b1ac
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-10-09 15:28:54 +0200

    [MIG] sale_automatic_workflow_payment_mode: run pre-commit

--- sale_automatic_workflow_payment_mode/models/sale_workflow_process.py
+++ sale_automatic_workflow_payment_mode/models/sale_workflow_process.py
@@ -1,7 +1,7 @@
 # © 2016 Camptocamp SA, Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleWorkflowProcess(models.Model):
@@ -9,10 +9,7 @@ class SaleWorkflowProcess(models.Model):
 
     @api.model
     def _default_payment_filter_id(self):
-        xmlid = (
-            "sale_automatic_workflow_payment_mode."
-            "automatic_workflow_payment_filter"
-        )
+        xmlid = "sale_automatic_workflow_payment_mode.automatic_workflow_payment_filter"
         try:
             return self.env.ref(xmlid)
         except ValueError:
@@ -25,5 +22,5 @@ class SaleWorkflowProcess(models.Model):
     )
     register_payment = fields.Boolean(string="Register Payment")
     payment_filter_domain = fields.Text(
-        string="Payment Filter Domain", related="payment_filter_id.domain",
+        string="Payment Filter Domain", related="payment_filter_id.domain"
     )
