0185fa1df26533d9a76227e4b68559cce344a0f1
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-10-12 14:15:23 +0200

    [MIG] sale_order_line_price_history: Migration to 13.0

--- sale_order_line_price_history/__manifest__.py
+++ sale_order_line_price_history/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order line price history",
-    "version": "12.0.1.1.1",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow/",
     "license": "AGPL-3",
-    "depends": ["sale",],
-    "data": ["wizards/sale_order_line_price_history.xml", "views/sale_views.xml",],
+    "depends": ["sale"],
+    "data": ["wizards/sale_order_line_price_history.xml", "views/sale_views.xml"],
     "installable": True,
 }
