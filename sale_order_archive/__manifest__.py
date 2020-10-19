a1dd505d02b3ce1087bae50534c0ecbfa4d7f11b
Andrea <a.stirpe@onestein.nl>
2020-10-19 12:20:18 +0200

    [14.0][MIG] sale_order_archive

--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 Onestein (<https://www.onestein.eu>)
+# Copyright 2017-2020 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale_order.xml"],
