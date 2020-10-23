08c3c518f709b815b6093ede7518a0e1d766bcf5
João Marques <joao.marques@tecnativa.com>
2020-10-23 09:44:18 +0100

    [MIG] sale_stock_picking_note: Migration to 13.0
    TT25948

--- sale_stock_picking_note/__manifest__.py
+++ sale_stock_picking_note/__manifest__.py
@@ -3,12 +3,12 @@
 {
     "name": "Sale Stock Picking Note",
     "summary": "Add picking note in sale and purchase order",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": ["sale_stock",],
-    "data": ["views/sale_order_view.xml",],
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_view.xml"],
     "installable": True,
 }
