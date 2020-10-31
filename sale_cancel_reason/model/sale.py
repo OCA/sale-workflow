a74d1f01281cdb0c9f423937c1c3e2eb74c59e30
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-10-31 12:42:32 -0600

    [IMP] sale_cancel_reason: black, isort, prettier

--- sale_cancel_reason/model/sale.py
+++ sale_cancel_reason/model/sale.py
@@ -1,20 +1,21 @@
 # Copyright 2013 Guewen Baconnier, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     cancel_reason_id = fields.Many2one(
-        'sale.order.cancel.reason',
+        "sale.order.cancel.reason",
         string="Reason for cancellation",
         readonly=True,
-        ondelete="restrict")
+        ondelete="restrict",
+    )
 
 
 class SaleOrderCancelReason(models.Model):
-    _name = 'sale.order.cancel.reason'
-    _description = 'Sale Order Cancel Reason'
+    _name = "sale.order.cancel.reason"
+    _description = "Sale Order Cancel Reason"
 
-    name = fields.Char('Reason', required=True, translate=True)
+    name = fields.Char("Reason", required=True, translate=True)
