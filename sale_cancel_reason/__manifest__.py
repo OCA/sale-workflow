8b4b2994815f1040c43f0fbaf85ed418a2dd0344
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-10-31 12:42:32 -0600

    [IMP] sale_cancel_reason: black, isort, prettier

--- sale_cancel_reason/__manifest__.py
+++ sale_cancel_reason/__manifest__.py
@@ -3,21 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale Cancel Reason',
-    'version': '12.0.1.2.0',
-    'author': 'Camptocamp,'
-              'Odoo Community Association (OCA)',
-    'category': 'Sale',
-    'license': 'AGPL-3',
-    'complexity': 'normal',
-    'website': "https://github.com/OCA/sale-workflow",
-    'depends': ['sale'],
-    'data': [
-        'wizard/cancel_reason_view.xml',
-        'view/sale_view.xml',
-        'security/ir.model.access.csv',
-        'data/sale_order_cancel_reason.xml',
+    "name": "Sale Cancel Reason",
+    "version": "12.0.1.2.0",
+    "author": "Camptocamp," "Odoo Community Association (OCA)",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale"],
+    "data": [
+        "wizard/cancel_reason_view.xml",
+        "view/sale_view.xml",
+        "security/ir.model.access.csv",
+        "data/sale_order_cancel_reason.xml",
     ],
-    'auto_install': False,
-    'installable': True,
+    "auto_install": False,
+    "installable": True,
 }
