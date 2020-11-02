5d1a4c635a23bbef509386e7eec95b5f8992b784
Lois Rilo <lois.rilo@forgeflow.com>
2020-11-02 18:21:06 +0100

    [13.0][FIX] sale_stock_delivery_addres: order_partner_id not being stored.
    Include the related stored field `order_partner_id` in the views
    has the side effect of preventing it to be stored, even when it
    is invisible. ORM issues appart it is better to just remove it
    in this case, as the field is not needed anymore in the view by
    this module.

--- sale_stock_delivery_address/__manifest__.py
+++ sale_stock_delivery_address/__manifest__.py
@@ -8,6 +8,7 @@
     "website": "https://github.com/OCA/sale-workflow/",
     "category": "Sales Management",
     "license": "LGPL-3",
+    "development_status": "Production/Stable",
     "depends": ["sale_stock", "sale_procurement_group_by_line"],
     "data": ["views/sale_order_view.xml", "views/res_partner_view.xml"],
     "installable": True,
