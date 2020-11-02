5424851b330713acb15cb89e8c1a6f4bf24417aa
newtratip <tharathip.chaweewongphan@gmail.com>
2020-11-03 01:18:06 +0700

    [MIG] sale_commercial_partner: Migration to 14.0
    [UPD] Update sale_commercial_partner.pot

--- sale_commercial_partner/__manifest__.py
+++ sale_commercial_partner/__manifest__.py
@@ -1,11 +1,10 @@
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 {
     "name": "Sale Commercial Partner",
     "summary": "Add stored related field 'Commercial Entity' on sale orders",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
