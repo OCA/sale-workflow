761fea6baf3d5584f90da3660b4a76b10515e88d
newtratip <tharathip.chaweewongphan@gmail.com>
2020-11-03 19:24:51 +0700

    [IMP] sale_validity: black, isort, prettier

--- sale_validity/__manifest__.py
+++ sale_validity/__manifest__.py
@@ -2,10 +2,9 @@
 # © 2014-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 {
     "name": "Sale Validity",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "summary": "Set a default validity delay on quotations",
