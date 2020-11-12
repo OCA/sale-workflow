8ec02a4d072e7c319b93d1cc1f27105880bb9446
sergio-teruel <sergio.teruel@tecnativa.com>
2020-11-12 21:22:04 +0100

    [IMP] sale_order_product_recommendation: Allow to display price unit from listprice or from last sale order. TT26727

--- sale_order_product_recommendation/tests/test_recommendation_common.py
+++ sale_order_product_recommendation/tests/test_recommendation_common.py
@@ -9,26 +9,34 @@ class RecommendationCase(SavepointCase):
         super().setUpClass()
         # Make sure user has UoM activated for Forms to work
         cls.env.user.groups_id = [(4, cls.env.ref('uom.group_uom').id)]
+        cls.pricelist = cls.env['product.pricelist'].create({
+            'name': 'Pricelist for test',
+        })
         cls.partner = cls.env['res.partner'].create({
             'name': 'Mr. Odoo',
+            'property_product_pricelist': cls.pricelist.id
         })
         cls.product_obj = cls.env['product.product']
         cls.prod_1 = cls.product_obj.create({
             'name': 'Test Product 1',
             'type': 'service',
+            'list_price': 25.00,
         })
         cls.prod_2 = cls.product_obj.create({
             'name': 'Test Product 2',
             'type': 'service',
+            'list_price': 50.00,
         })
         cls.prod_3 = cls.product_obj.create({
             'name': 'Test Product 3',
             'type': 'service',
+            'list_price': 75.00,
         })
         # Create old sale orders to have searchable history
-        cls.env["sale.order"].create({
+        cls.order1 = cls.env["sale.order"].create({
             "partner_id": cls.partner.id,
             "state": "done",
+            "confirmation_date": "2020-11-15",
             "order_line": [
                 (0, 0, {
                     "product_id": cls.prod_1.id,
@@ -36,6 +44,7 @@ class RecommendationCase(SavepointCase):
                     "product_uom_qty": 25,
                     "qty_delivered_method": "manual",
                     "qty_delivered": 25,
+                    "price_unit": 24.50,
                 }),
                 (0, 0, {
                     "product_id": cls.prod_2.id,
@@ -43,6 +52,7 @@ class RecommendationCase(SavepointCase):
                     "product_uom_qty": 50,
                     "qty_delivered_method": "manual",
                     "qty_delivered": 50,
+                    "price_unit": 49.50,
                 }),
                 (0, 0, {
                     "product_id": cls.prod_3.id,
@@ -50,12 +60,14 @@ class RecommendationCase(SavepointCase):
                     "product_uom_qty": 100,
                     "qty_delivered_method": "manual",
                     "qty_delivered": 100,
+                    "price_unit": 74.50,
                 }),
             ],
         })
-        cls.env["sale.order"].create({
+        cls.order2 = cls.env["sale.order"].create({
             "partner_id": cls.partner.id,
             "state": "done",
+            "confirmation_date": "2020-11-10",
             "order_line": [
                 (0, 0, {
                     "product_id": cls.prod_2.id,
@@ -63,6 +75,7 @@ class RecommendationCase(SavepointCase):
                     "product_uom_qty": 50,
                     "qty_delivered_method": "manual",
                     "qty_delivered": 50,
+                    "price_unit": 89.00,
                 }),
             ],
         })
