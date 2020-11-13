ac3c3a4c0050c2cff83e051769bd893ca3b1b456
sergio-teruel <sergio.teruel@tecnativa.com>
2020-11-13 15:33:43 +0100

    [FIX] sale_order_product_recommendation: Get product image form product.product model instead of product.template in wizard kanban view. TT26749

--- sale_order_product_recommendation/__manifest__.py
+++ sale_order_product_recommendation/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Order Product Recommendation",
     "summary": "Recommend products to sell to customer based on history",
-    "version": "12.0.2.0.1",
+    "version": "12.0.2.0.2",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
