0c0a6eb7f33413136b3b55b96d4b8026d193709d
Kevin Khao <kevinkhao@gmail.com>
2020-11-23 16:05:20 +0100

    [14.0][MIG] sale_partner_version: Migration to 14.0

--- sale_partner_version/__manifest__.py
+++ sale_partner_version/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Partner Version",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sale",
--- sale_partner_version/models/sale.py
+++ sale_partner_version/models/sale.py
@@ -1,16 +1,15 @@
 # Copyright 2018 Akretion - Benoît Guillot
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.multi
     def action_confirm(self):
         address_fields = ["partner_shipping_id", "partner_invoice_id"]
         for order in self:
             for field in address_fields:
-                order[field] = order[field].get_address_version()
+                order[field] = order[field]._version_create()
         return super(SaleOrder, self).action_confirm()
