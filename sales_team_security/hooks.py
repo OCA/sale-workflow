9fefdee1d4e736f8df47fea4300fe33625b3e6f4
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-11-25 14:36:44 -0300

    [IMP] Implement uninstall hook to revert changes in res.groups

--- sales_team_security/hooks.py
+++ sales_team_security/hooks.py
@@ -1,5 +1,8 @@
 # Copyright 2018-2016 Tecnativa - Pedro M. Baeza
+# Copyright 2020 - Iván Todorovich
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, registry):
@@ -15,3 +18,16 @@ def post_init_hook(cr, registry):
         AND res_partner.parent_id = parent.id
         AND res_partner.team_id IS NULL"""
     )
+
+
+def uninstall_hook(cr, registry):  # pragma: no cover
+    """ At uninstall, revert changes made to record rules """
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        env.ref("sales_team.group_sale_salesman_all_leads").write(
+            {
+                "implied_ids": [
+                    (6, 0, [env.ref("sales_team.group_sale_salesman").id]),
+                ],
+            }
+        )
