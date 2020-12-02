72366a8b5c163403f023a061d5667f90cecc9745
Florian da Costa <florian.dacosta@akretion.com>
2020-12-02 20:29:30 +0100

    [REF] Refactore module making it depend on stock_restrict_lot The module was buggy, it would reserve stock if available, even if it had the wrong lot. Test added to show the case

--- sale_order_lot_selection/models/sale_order_line.py
+++ sale_order_lot_selection/models/sale_order_line.py
@@ -7,6 +7,12 @@ class SaleOrderLine(models.Model):
 
     lot_id = fields.Many2one("stock.production.lot", "Lot", copy=False)
 
+    def _prepare_procurement_values(self, group_id=False):
+        vals = super()._prepare_procurement_values(group_id=group_id)
+        if self.lot_id:
+            vals['restrict_lot_id'] = self.lot_id.id
+        return vals
+
     @api.onchange("product_id")
     def product_id_change(self):
         super().product_id_change()
