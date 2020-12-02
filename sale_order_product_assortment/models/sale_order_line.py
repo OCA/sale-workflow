34b601b51007b0742b2c8370e35dc7bf4c371bab
Carlos Roca <carlos.roca@tecnativa.com>
2020-12-02 12:14:07 +0100

    [ADD] sale_order_product_assortment: New module to use the product assortment This module allow to use the product assortment related to partners on sale orders.

---
+++ sale_order_product_assortment/models/sale_order_line.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+# We overwrite this class as a trick to repeat the product_id field with different
+# attributes on the view.
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_has_both_assortment_id = fields.Many2one(
+        related='product_id',
+        string="Product with whitelist and blacklist"
+    )
+    product_has_blacklist_assortment_id = fields.Many2one(
+        related='product_id',
+        string="Product with blacklist"
+    )
+
+    @api.onchange(
+        'product_has_both_assortment_id', 'product_has_blacklist_assortment_id')
+    def _onchange_product_secondary_fields(self):
+        if self.product_has_both_assortment_id:
+            self.product_id = self.product_has_both_assortment_id
+        if self.product_has_blacklist_assortment_id:
+            self.product_id = self.product_has_blacklist_assortment_id
