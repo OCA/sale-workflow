b0e55213bc924f12ec54641f8635920da86ec022
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-12-02 20:23:08 -0500

    [IMP] sale_order_product_availability_inline: set decimal precision

--- sale_order_product_availability_inline/__manifest__.py
+++ sale_order_product_availability_inline/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Product Availability Inline",
     "summary": "Show product availability in sales order line product " "drop-down.",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.1.1",
     "development_status": "Beta",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
