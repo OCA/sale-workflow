c9ef2a1706cefdf8893a18e4c021131d407f139b
Florian da Costa <florian.dacosta@akretion.com>
2020-12-03 14:09:29 +0100

    [MIG] sale_order_lot_generator: Migration to 14.0

--- sale_order_lot_generator/__manifest__.py
+++ sale_order_lot_generator/__manifest__.py
@@ -1,15 +1,14 @@
-#   @author Valentin CHEMIERE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Order Lot Generator",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "category": "Sale",
     "depends": ["sale_order_lot_selection"],
     "maintainers": ["florian-dacosta", "mourad-ehm", "bealdav"],
-    "data": ["views/product_view.xml"],
+    "data": ["views/product_template.xml"],
     "installable": True,
 }
--- sale_order_lot_generator/models/product_template.py
+++ sale_order_lot_generator/models/product_template.py
@@ -1,4 +1,3 @@
-#  @author Adrien CHAUSSENDE <adrien.chaussende@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
