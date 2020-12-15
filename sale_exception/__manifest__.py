911b1626604879e6600a4ae3f35eaf73f6d92f97
Simone Orsi <simahawk@gmail.com>
2020-12-15 15:27:20 +0100

    sale_exception: fix test class
    TestSaleOrder was useless and making sale order tests run twice.

--- sale_exception/__manifest__.py
+++ sale_exception/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Sale Exception",
     "summary": "Custom exceptions on sale order",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Generic Modules/Sale",
     "author": "Akretion, "
     "Sodexis, "
