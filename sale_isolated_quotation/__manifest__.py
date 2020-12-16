afb8203ca60c47a05d02507bf6139719e35e4cdf
Kevin.roche <kevin.roche@akretion.com>
2020-12-16 15:02:39 +0100

    [MIG] sale_isolated_quotation: Migration to 14.0

--- sale_isolated_quotation/__manifest__.py
+++ sale_isolated_quotation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Isolated Quotation",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
@@ -14,4 +14,5 @@
     "auto_install": False,
     "uninstall_hook": "uninstall_hook",
     "post_init_hook": "post_init_hook",
+    "maintainers": ["bealdav", "kittiu"],
 }
