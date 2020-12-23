52d3c8cb0ecfea30253cf8013e56e9fa04c62edc
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-23 17:16:45 +0100

    [MIG] sale_start_end_dates to v14

--- sale_start_end_dates/__manifest__.py
+++ sale_start_end_dates/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Sale Start End Dates",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "summary": "Adds start date and end date on sale order lines",
