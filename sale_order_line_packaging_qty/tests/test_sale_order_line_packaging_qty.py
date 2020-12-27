3321618c9036307e65766df78a3fc72aef74de68
Tran Thanh Phuc <phuc@trobz.com>
2020-12-27 19:19:50 +0700

    [MIG] sale_order_line_packaging_qty: Migration to 14.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
+++ sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo.exceptions import UserError
 from odoo.tests import SavepointCase
 
 
@@ -13,6 +14,7 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
         cls.packaging = cls.env["product.packaging"].create(
             {"name": "Test packaging", "product_id": cls.product.id, "qty": 5.0}
         )
+        cls.product_no_pckg = cls.env.ref("product.product_product_10")
 
     def test_product_packaging_qty(self):
         order = self.env["sale.order"].create({"partner_id": self.partner.id})
@@ -25,8 +27,43 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
             }
         )
         order_line.write({"product_packaging": self.packaging})
+        # This is what actually happened when user adds so line
+        order_line._onchange_product_uom_qty()
+        order_line._compute_product_packaging_qty()
         order_line._onchange_product_packaging()
+        order_line._onchange_product_packaging_qty()
         self.assertEqual(order_line.product_uom_qty, 5.0)
         self.assertEqual(order_line.product_packaging_qty, 1.0)
+
         order_line.write({"product_packaging_qty": 3.0})
+        order_line._onchange_product_packaging_qty()
         self.assertEqual(order_line.product_uom_qty, 15.0)
+
+        dozen = self.env.ref("uom.product_uom_dozen")
+        order_line.product_uom = dozen
+        order_line._compute_product_packaging_qty()
+        self.assertEqual(order_line.product_uom_qty, 180.0)
+
+        self.packaging.qty = 0
+        with self.assertRaises(UserError):
+            order_line.write({"product_packaging_qty": 0.0})
+
+        order_line.product_packaging = False
+        order_line._onchange_product_packaging()
+        self.assertEqual(order_line.product_packaging_qty, 0.0)
+
+    def test_product_packaging_qty_wo_packaging(self):
+        order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": order.id,
+                "product_id": self.product_no_pckg.id,
+                "product_uom": self.product_no_pckg.uom_id.id,
+                "product_uom_qty": 3.0,
+            }
+        )
+        order_line._compute_product_packaging_qty()
+        self.assertEqual(order_line.product_uom_qty, 3.0)
+        self.assertEqual(order_line.product_packaging_qty, 0.0)
+        with self.assertRaises(UserError):
+            order_line.write({"product_packaging_qty": 3.0})
