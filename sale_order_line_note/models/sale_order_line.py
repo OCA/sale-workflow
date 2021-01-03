1b9fc626ed19ff78143214557c789e26074644e1
Sébastien BEAU <sebastien.beau@akretion.com>
2021-01-03 22:43:35 +0100

    [ADD] add module for add note on sale order line

---
+++ sale_order_line_note/models/sale_order_line.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    note = fields.Text()
