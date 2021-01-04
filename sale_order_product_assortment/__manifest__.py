7ac472db644ea3efccf8dcc70d49008937c7a499
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-04 08:40:17 +0100

    [MIG] sale_order_product_assortment: Migration to v13

--- sale_order_product_assortment/__manifest__.py
+++ sale_order_product_assortment/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Sale Order Product Assortment",
-    "summary": "",
-    "version": "12.0.1.0.0",
+    "summary": "Module that allows to use the assortments on sale orders",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
