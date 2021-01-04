0db8305801cda4a711d9715945bce02054cb1d5a
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-04 08:40:17 +0100

    [MIG] sale_order_product_assortment: Migration to v13

--- sale_order_product_assortment/models/sale_order.py
+++ sale_order_product_assortment/models/sale_order.py
@@ -21,6 +21,11 @@ class SaleOrder(models.Model):
 
     @api.depends("partner_id")
     def _compute_product_assortment_ids(self):
+        # If we don't initialize the fields we get an error with NewId
+        self.whitelist_product_ids = self.env["product.product"]
+        self.blacklist_product_ids = self.env["product.product"]
+        self.has_whitelist = False
+        self.has_blacklist = False
         if self.partner_id:
             filters = self.env["ir.filters"].search(
                 [
@@ -44,9 +49,9 @@ class SaleOrder(models.Model):
                 self.has_whitelist = True
             if blacklist_products:
                 self.has_blacklist = True
-            self.whitelist_product_ids = self.env["product.product"].browse(
+            self.whitelist_product_ids = self.whitelist_product_ids.browse(
                 whitelist_products
             )
-            self.blacklist_product_ids = self.env["product.product"].browse(
+            self.blacklist_product_ids = self.blacklist_product_ids.browse(
                 blacklist_products
             )
