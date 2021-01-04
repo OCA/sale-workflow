7278d5b9b843cc18fa6946108a87c1d92a8aa298
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-04 08:22:17 +0100

    [IMP] sale_order_product_assortment: black, isort, prettier

--- sale_order_product_assortment/models/sale_order_line.py
+++ sale_order_product_assortment/models/sale_order_line.py
@@ -6,19 +6,18 @@ from odoo import api, fields, models
 # We overwrite this class as a trick to repeat the product_id field with different
 # attributes on the view.
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     product_has_both_assortment_id = fields.Many2one(
-        related='product_id',
-        string="Product with whitelist and blacklist"
+        related="product_id", string="Product with whitelist and blacklist"
     )
     product_has_blacklist_assortment_id = fields.Many2one(
-        related='product_id',
-        string="Product with blacklist"
+        related="product_id", string="Product with blacklist"
     )
 
     @api.onchange(
-        'product_has_both_assortment_id', 'product_has_blacklist_assortment_id')
+        "product_has_both_assortment_id", "product_has_blacklist_assortment_id"
+    )
     def _onchange_product_secondary_fields(self):
         if self.product_has_both_assortment_id:
             self.product_id = self.product_has_both_assortment_id
