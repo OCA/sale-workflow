daa2219f51090af7276698fb8faf87fba8dfd68e
Tran Thanh Phuc <phuc@trobz.com>
2021-01-11 22:04:23 +0700

    [MIG] sale_order_carrier_auto_assign: Migration to 14.0

--- sale_order_carrier_auto_assign/__manifest__.py
+++ sale_order_carrier_auto_assign/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Carrier Auto Assign",
     "summary": "Auto assign delivery carrier on sale order confirmation",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Operations/Inventory/Delivery",
     "website": "https://github.com/OCA/sale-workflow",
