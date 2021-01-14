6aee17febdb0ef30349005d4c4a69cb29788dc94
simonev <simone.vanin@agilebg.com>
2021-01-14 16:34:10 +0100

    [MIG][14.0] sale_order_line_description

--- sale_order_line_description/__manifest__.py
+++ sale_order_line_description/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order line description",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
     "author": "Agile Business Group, " "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/sale-workflow/",
+    "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["security/sale_security.xml", "views/res_config_settings_views.xml"],
