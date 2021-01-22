fae46d8c86943da16277c9c1c6db6644e49b4545
newtratip <tharathip.chaweewongphan@gmail.com>
2021-01-22 15:57:53 +0700

    [MIG] sale_order_type: Migration to 14.0

--- sale_order_type/migrations/14.0.1.0.0/post-migration.py
+++ sale_order_type/migrations/14.0.1.0.0/post-migration.py
@@ -7,5 +7,5 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "sale_order_type", "migrations/13.0.1.0.0/noupdate_changes.xml"
+        env.cr, "sale_order_type", "migrations/14.0.1.0.0/noupdate_changes.xml"
     )
