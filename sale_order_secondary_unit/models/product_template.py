ff645a552ccfe94490c8be8361c4be02f3cd0bed
Alexei Rivera <alexeirivera87@gmail.com>
2021-01-22 10:27:28 -0500

    [MIG] sale_order_secondary_unit: Migration to 14.0

--- sale_order_secondary_unit/models/product_template.py
+++ sale_order_secondary_unit/models/product_template.py
@@ -7,5 +7,8 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     sale_secondary_uom_id = fields.Many2one(
-        comodel_name="product.secondary.unit", string="Default secondary unit for sales"
+        comodel_name="product.secondary.unit",
+        string="Default secondary unit for sales",
+        help="In order to set a value, please first add at least one record"
+        " in 'Secondary Unit of Measure'",
     )
