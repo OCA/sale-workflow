5585406ebe4ac961ac59bbcb26d886d2e07abfb3
Alexei Rivera <alexeirivera87@gmail.com>
2021-01-22 10:27:28 -0500

    [MIG] sale_order_secondary_unit: Migration to 14.0

--- sale_order_secondary_unit/__manifest__.py
+++ sale_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Secondary Unit",
     "summary": "Sale product in a secondary unit",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
