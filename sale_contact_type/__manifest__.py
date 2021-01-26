19fb7b20d74d4a69af62e6b38f96914cab4e80cd
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-26 08:42:15 +0100

    [IMP] sale_contact_type: black, isort, prettier

--- sale_contact_type/__manifest__.py
+++ sale_contact_type/__manifest__.py
@@ -3,18 +3,14 @@
 
 
 {
-    'name': 'Sale Contact Type',
-    'version': '12.0.1.0.0',
-    'category': 'Sales',
-    'license': 'AGPL-3',
-    'summary': 'Define ordering contact type',
-    'depends': [
-        'sale_management',
-    ],
-    'author': 'Tecnativa, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'data': [
-        'views/sale_order.xml',
-    ],
-    'installable': True,
+    "name": "Sale Contact Type",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "summary": "Define ordering contact type",
+    "depends": ["sale_management"],
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "data": ["views/sale_order.xml"],
+    "installable": True,
 }
