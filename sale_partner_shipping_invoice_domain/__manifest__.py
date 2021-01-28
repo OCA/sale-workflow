6cdb99ecb312dfa1e8013cc10e768b33bcc5f453
Kevin Khao <kevinkhao@gmail.com>
2021-01-28 15:01:57 +0100

    [14.0][ADD] sale_partner_shipping_invoice_domain: add to 14.0

---
+++ sale_partner_shipping_invoice_domain/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Shipping and Invoice Domain",
+    "summary": """
+        Adds a sensible domain to the shipping
+        and invoicing addresses on sale form view""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale_stock", "sale_commercial_partner"],
+    "demo": [],
+}
---
+++ sale_partner_shipping_invoice_domain/models/sale_order.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Akretion France (http://www.akretion.com/)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    partner_invoice_id = fields.Many2one(
+        domain="['&', '|', "
+        "('company_id', '=', False), ('company_id', '=', company_id),"
+        "('id', 'child_of', commercial_partner_id)]"
+    )
+    partner_shipping_id = fields.Many2one(
+        domain="['&', '|', "
+        "('company_id', '=', False), ('company_id', '=', company_id),"
+        "('id', 'child_of', commercial_partner_id)]"
+    )
