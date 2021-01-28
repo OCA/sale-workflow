82fe3754f6775614e46f98c82dc87106cc36e343
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-01-28 16:19:32 -0300

    [IMP] pre-commit run -a

--- sale_order_general_discount/models/sale_order.py
+++ sale_order_general_discount/models/sale_order.py
@@ -26,13 +26,16 @@ class SaleOrder(models.Model):
         self, view_id=None, view_type="form", toolbar=False, submenu=False
     ):
         """The purpose of this is to write a context on "order_line" field
-         respecting other contexts on this field.
-         There is a PR (https://github.com/odoo/odoo/pull/26607) to odoo for
-         avoiding this. If merged, remove this method and add the attribute
-         in the field.
-         """
+        respecting other contexts on this field.
+        There is a PR (https://github.com/odoo/odoo/pull/26607) to odoo for
+        avoiding this. If merged, remove this method and add the attribute
+        in the field.
+        """
         res = super(SaleOrder, self).fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu,
+            view_id=view_id,
+            view_type=view_type,
+            toolbar=toolbar,
+            submenu=submenu,
         )
         if view_type == "form":
             order_xml = etree.XML(res["arch"])
@@ -40,7 +43,9 @@ class SaleOrder(models.Model):
             if order_line_fields:
                 order_line_field = order_line_fields[0]
                 context = order_line_field.attrib.get("context", "{}").replace(
-                    "{", "{'default_discount': general_discount, ", 1,
+                    "{",
+                    "{'default_discount': general_discount, ",
+                    1,
                 )
                 order_line_field.attrib["context"] = context
                 res["arch"] = etree.tostring(order_xml)
