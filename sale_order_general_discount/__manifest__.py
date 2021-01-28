33428156f76934c230e144cd00e716254f79a224
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-01-28 15:37:28 -0300

    [MIG] sale_order_general_discount: Migration to 14.0

--- sale_order_general_discount/__manifest__.py
+++ sale_order_general_discount/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order General Discount",
     "summary": "General discount per sale order",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
