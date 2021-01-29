eaf151afdfeaeaef07c60460b6b0a2ee69c3998f
Benoit <benoit.guillot@akretion.com>
2021-01-29 18:08:03 +0100

    [IMP] pricelist_tax: black, isort, prettier

--- pricelist_tax/models/sale.py
+++ pricelist_tax/models/sale.py
@@ -1,37 +1,35 @@
-# coding: utf-8
 # © 2018  Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from collections import defaultdict
 import logging
+from collections import defaultdict
 
-from odoo import models, api
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _map_exclude_tax(self):
-        """ return a dict
-            mtax[company_id or 0][tax amount]['include'|'exclude'] = tax_id
+        """return a dict
+        mtax[company_id or 0][tax amount]['include'|'exclude'] = tax_id
         """
         mtax = defaultdict(dict)
         prev_cpny = False
-        for tax in self.env['account.tax'].search(
-                [('type_tax_use', '=', 'sale')],
-                order='company_id ASC, price_include DESC'):
+        for tax in self.env["account.tax"].search(
+            [("type_tax_use", "=", "sale")], order="company_id ASC, price_include DESC"
+        ):
             cpny = tax.company_id
             if cpny != prev_cpny:
                 tamount = defaultdict(dict)
             if tax.price_include:
-                tamount[tax.amount].update({'include': tax.id})
+                tamount[tax.amount].update({"include": tax.id})
             else:
-                tamount[tax.amount].update({'exclude': tax.id})
+                tamount[tax.amount].update({"exclude": tax.id})
             if tax.amount in mtax[tax.company_id.id or 0]:
-                mtax[tax.company_id.id or 0][tax.amount].update(
-                    tamount[tax.amount])
+                mtax[tax.company_id.id or 0][tax.amount].update(tamount[tax.amount])
             else:
                 mtax[tax.company_id.id or 0][tax.amount] = tamount[tax.amount]
             prev_cpny = cpny
@@ -40,42 +38,47 @@ class SaleOrderLine(models.Model):
     @api.multi
     def _compute_tax_id(self):
         super(SaleOrderLine, self)._compute_tax_id()
-        map_tax = self.env['account.tax']._map_exclude_tax()
+        map_tax = self.env["account.tax"]._map_exclude_tax()
         for line in self:
             # If company_id is set, always filter taxes by the company
             taxes = line.product_id.taxes_id.filtered(
-                lambda r: not line.company_id or
-                r.company_id == line.company_id)
+                lambda r: not line.company_id or r.company_id == line.company_id
+            )
             if not line.order_id.pricelist_id.price_include_taxes:
                 # pricelist not include tax
-                taxes = self.env['account.tax']._get_substitute_taxes(
-                    self, taxes, map_tax)
-            fpos = (line.order_id.fiscal_position_id or
-                    line.order_id.partner_id.property_account_position_id)
-            line.tax_id = (fpos.map_tax(
-                taxes, line.product_id, line.order_id.partner_shipping_id)
-                if fpos else taxes)
+                taxes = self.env["account.tax"]._get_substitute_taxes(
+                    self, taxes, map_tax
+                )
+            fpos = (
+                line.order_id.fiscal_position_id
+                or line.order_id.partner_id.property_account_position_id
+            )
+            line.tax_id = (
+                fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id)
+                if fpos
+                else taxes
+            )
 
     def _get_substitute_taxes(self, taxes, map_tax):
         cpny = self.order_id.company_id.id or self.company_id.id or 0
         mytaxes = []
         for tax in taxes:
             if map_tax[cpny].get(tax.amount):
-                if map_tax[cpny][tax.amount].get('exclude'):
-                    mytaxes.append(map_tax[cpny][tax.amount]['exclude'])
+                if map_tax[cpny][tax.amount].get("exclude"):
+                    mytaxes.append(map_tax[cpny][tax.amount]["exclude"])
                 else:
                     mytaxes.append(tax.id)
             else:
                 mytaxes.append(tax.id)
-        return self.env['account.tax'].browse(mytaxes)
+        return self.env["account.tax"].browse(mytaxes)
 
     @api.multi
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         self._upd_onchange_ctx()
         return super(SaleOrderLine, self).product_id_change()
 
-    @api.onchange('product_uom', 'product_uom_qty')
+    @api.onchange("product_uom", "product_uom_qty")
     def product_uom_change(self):
         self._upd_onchange_ctx()
         return super(SaleOrderLine, self).product_uom_change()
--- pricelist_tax/models/tax.py
+++ pricelist_tax/models/tax.py
@@ -1,77 +1,79 @@
-# coding: utf-8
 # © 2018  Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-
 from collections import defaultdict
-from odoo import models, api, _
+
+from odoo import _, api, models
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
 class AccountTax(models.Model):
-    _inherit = 'account.tax'
+    _inherit = "account.tax"
 
     @api.model
     def _fix_tax_included_price(self, price, prod_taxes, line_taxes):
-        """ Override Odoo method to:
-            - use raw price from pricelist (if price exclude)
-              instead of recomputed price
-            - some checks/raises on unmanaged cases
+        """Override Odoo method to:
+        - use raw price from pricelist (if price exclude)
+          instead of recomputed price
+        - some checks/raises on unmanaged cases
         """
         self._check_unsupported_case(prod_taxes=prod_taxes)
         original = super(AccountTax, self)._fix_tax_included_price(
-            price, prod_taxes, line_taxes)
-        pricelist_id = self.env.context.get('pricelist')
+            price, prod_taxes, line_taxes
+        )
+        pricelist_id = self.env.context.get("pricelist")
         computed_price = False
         # Check if pricelist contains adhoc price
         if pricelist_id:
-            pricelist = self.env['product.pricelist'].browse(pricelist_id)
+            pricelist = self.env["product.pricelist"].browse(pricelist_id)
             if not pricelist.price_include_taxes:
                 computed_price = True
-                self._check_unsupported_case(
-                    line_taxes=line_taxes, pricelist=pricelist)
+                self._check_unsupported_case(line_taxes=line_taxes, pricelist=pricelist)
         if computed_price:
             return price
         return original
 
-    def _check_unsupported_case(
-            self, prod_taxes=None, line_taxes=None, pricelist=None):
+    def _check_unsupported_case(self, prod_taxes=None, line_taxes=None, pricelist=None):
         if prod_taxes:
             prod_taxes_exclude = [x for x in prod_taxes if not x.price_include]
             if prod_taxes_exclude:
                 raise UserError(
-                    _("Tax product '%s' is price exclude. "
-                      "You must switch to include ones."
-                      % prod_taxes_exclude[0].name))
+                    _(
+                        "Tax product '%s' is price exclude. "
+                        "You must switch to include ones." % prod_taxes_exclude[0].name
+                    )
+                )
         if line_taxes:
             line_taxes_include = [x for x in line_taxes if x.price_include]
             if line_taxes_include:
                 raise UserError(
-                    _("Tax with include price with pricelist b2b '%s' "
-                      "is not supported" % pricelist.name))
+                    _(
+                        "Tax with include price with pricelist b2b '%s' "
+                        "is not supported" % pricelist.name
+                    )
+                )
 
     def _map_exclude_tax(self):
-        """ return a dict
-            mtax[company_id or 0][tax amount]['include'|'exclude'] = tax_id
+        """return a dict
+        mtax[company_id or 0][tax amount]['include'|'exclude'] = tax_id
         """
         mtax = defaultdict(dict)
         prev_cpny = False
         for tax in self.search(
-                [('type_tax_use', '=', 'sale')],
-                order='company_id ASC, price_include DESC'):
+            [("type_tax_use", "=", "sale")], order="company_id ASC, price_include DESC"
+        ):
             cpny = tax.company_id
             if cpny != prev_cpny:
                 tamount = defaultdict(dict)
             if tax.price_include:
-                tamount[tax.amount].update({'include': tax.id})
+                tamount[tax.amount].update({"include": tax.id})
             else:
-                tamount[tax.amount].update({'exclude': tax.id})
+                tamount[tax.amount].update({"exclude": tax.id})
             if tax.amount in mtax[tax.company_id.id or 0]:
-                mtax[tax.company_id.id or 0][tax.amount].update(
-                    tamount[tax.amount])
+                mtax[tax.company_id.id or 0][tax.amount].update(tamount[tax.amount])
             else:
                 mtax[tax.company_id.id or 0][tax.amount] = tamount[tax.amount]
             prev_cpny = cpny
@@ -79,20 +81,20 @@ class AccountTax(models.Model):
 
     def _get_substitute_taxes(self, record, taxes, map_tax):
         # TODO shortify this code
-        if record._name == 'sale.order.line':
+        if record._name == "sale.order.line":
             cpny = record.order_id.company_id.id or self.company_id.id or 0
-        elif record._name == 'account.invoice.line':
+        elif record._name == "account.invoice.line":
             cpny = record.invoice_id.company_id.id or self.company_id.id or 0
         else:
-            raise("No other model supported than sale and invoice")
+            raise ("No other model supported than sale and invoice")
         # TODO end
         mytaxes = []
         for tax in taxes:
             if map_tax[cpny].get(tax.amount):
-                if map_tax[cpny][tax.amount].get('exclude'):
-                    mytaxes.append(map_tax[cpny][tax.amount]['exclude'])
+                if map_tax[cpny][tax.amount].get("exclude"):
+                    mytaxes.append(map_tax[cpny][tax.amount]["exclude"])
                 else:
                     mytaxes.append(tax.id)
             else:
                 mytaxes.append(tax.id)
-        return self.env['account.tax'].browse(mytaxes)
+        return self.env["account.tax"].browse(mytaxes)
