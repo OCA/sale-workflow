8f668cecee3cf2ff4547c20781afe5439cca610f
Benoit <benoit.guillot@akretion.com>
2021-01-29 18:08:03 +0100

    [MIG] pricelist_tax: Migration to 14.0

--- pricelist_tax/__manifest__.py
+++ pricelist_tax/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pricelist Tax",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sale",
     "author": "Akretion",
     "depends": [
