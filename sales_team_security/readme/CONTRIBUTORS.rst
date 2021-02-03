b7d32d8540f928bd04eb80eff63694c061271f87
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-03 13:50:56 +0100

    [IMP] sales_team_security: Auto-followers on main & invoice/shipping contacts + others
    - For avoiding access errors on this records that are related to sales flow
    - Sync salesman on parent change
    
    Co-Authored-By: Pedro M. Baeza <pedro.baeza@tecnativa.com>

--- sales_team_security/readme/CONTRIBUTORS.rst
+++ sales_team_security/readme/CONTRIBUTORS.rst
@@ -1,6 +1,7 @@
 * `Tecnativa <https://www.tecnativa.com>`__:
 
   * Pedro M. Baeza
+  * Víctor Martínez
 
 * `Guadaltech <https://www.guadaltech.es>`__:
 
--- sales_team_security/tests/test_sales_team_security.py
+++ sales_team_security/tests/test_sales_team_security.py
@@ -1,4 +1,5 @@
 # Copyright 2016-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from lxml import etree
@@ -17,6 +18,12 @@ class TestSalesTeamSecurity(common.SavepointCase):
         cls.partner = cls.env["res.partner"].create(
             {"name": "Test partner", "team_id": cls.team.id}
         )
+        cls.partner_child_1 = cls.env["res.partner"].create(
+            {"name": "Child 1", "parent_id": cls.partner.id}
+        )
+        cls.partner_child_2 = cls.env["res.partner"].create(
+            {"name": "Child 2", "parent_id": cls.partner.id, "type": "invoice"}
+        )
         cls.user = cls.env["res.users"].create(
             {
                 "login": "sales_team_security",
@@ -25,6 +32,7 @@ class TestSalesTeamSecurity(common.SavepointCase):
                 "sale_team_id": cls.team.id,
             }
         )
+        cls.user_partner = cls.user.partner_id
         cls.user2 = cls.env["res.users"].create(
             {
                 "login": "sales_team_security2",
@@ -32,6 +40,14 @@ class TestSalesTeamSecurity(common.SavepointCase):
                 "groups_id": [(4, cls.env.ref("sales_team.group_sale_salesman").id)],
                 "sale_team_id": cls.team.id,
             }
+        )
+        cls.user2_partner = cls.user2.partner_id
+
+    def _is_module_installed(self, name):
+        return bool(
+            self.env["ir.module.module"].search(
+                [("name", "=", name), ("state", "=", "installed")]
+            )
         )
 
     def test_onchange_parent_id(self):
@@ -58,6 +74,29 @@ class TestSalesTeamSecurity(common.SavepointCase):
         post_init_hook(self.env.cr, self.env.registry)
         contact.refresh()
         self.assertEqual(contact.team_id, self.partner.team_id)
+
+    def test_change_user_id_partner(self):
+        self.partner.write({"user_id": self.user.id})
+        self.assertIn(self.user_partner, self.partner.message_partner_ids)
+        self.assertNotIn(self.user_partner, self.partner_child_1.message_partner_ids)
+        self.assertIn(self.user_partner, self.partner_child_2.message_partner_ids)
+        # Change salesman
+        self.partner.write({"user_id": self.user2.id})
+        self.assertNotIn(self.user_partner, self.partner.message_partner_ids)
+        self.assertIn(self.user2_partner, self.partner.message_partner_ids)
+        self.assertNotIn(self.user_partner, self.partner_child_2.message_partner_ids)
+        self.assertIn(self.user2_partner, self.partner_child_2.message_partner_ids)
+
+    def test_change_user_id_partner_child_1(self):
+        self.partner_child_1.write({"user_id": self.user.id})
+        self.assertIn(self.user_partner, self.partner.message_partner_ids)
+        self.assertIn(self.user_partner, self.partner_child_2.message_partner_ids)
+        # Change salesman
+        self.partner_child_1.write({"user_id": self.user2.id})
+        self.assertNotIn(self.user_partner, self.partner.message_partner_ids)
+        self.assertIn(self.user2_partner, self.partner.message_partner_ids)
+        self.assertNotIn(self.user_partner, self.partner_child_2.message_partner_ids)
+        self.assertIn(self.user2_partner, self.partner_child_2.message_partner_ids)
 
     def test_partner_fields_view_get(self):
         res = self.env["res.partner"].fields_view_get(
@@ -103,7 +142,7 @@ class TestSalesTeamSecurity(common.SavepointCase):
         self._check_permission(False, self.team, True)
         self._check_permission(False, self.team2, False)
         self._check_permission(self.user, self.team, True)
-        self._check_permission(self.user, self.team2, False)
+        self._check_permission(self.user, self.team2, True)
         self._check_permission(self.user2, self.team2, False)
         self._check_permission(self.user2, self.team, True)
         # Add to group "See all leads"
