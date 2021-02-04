7690cb5819cf4025b0e72d9879111344d5204fb1
Mallory MARCOT <mmarcot@gmail.com>
2021-02-04 18:49:55 +0100

    [IMP] pep8 proof

--- sale_fixed_discount/models/sale_order.py
+++ sale_fixed_discount/models/sale_order.py
@@ -58,7 +58,8 @@ class SaleOrderLine(models.Model):
     @api.depends('price_unit', 'discount', 'discount_fixed')
     def _get_price_reduce(self):
         for line in self:
-            line.price_reduce = line.price_unit * (1.0 - line.discount / 100.0) - (line.discount_fixed or 0.0)
+            line.price_reduce = line.price_unit*(1.0-line.discount/100.0) - (
+                line.discount_fixed or 0.0)
 
     def _prepare_invoice_line(self, qty):
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
@@ -74,20 +75,27 @@ class SaleOrder(models.Model):
     def _amount_by_group(self):
         for order in self:
             currency = order.currency_id or order.company_id.currency_id
-            fmt = partial(formatLang, self.with_context(lang=order.partner_id.lang).env, currency_obj=currency)
+            fmt = partial(formatLang, self.with_context(
+                lang=order.partner_id.lang).env, currency_obj=currency)
             res = {}
             for line in order.order_line:
-                taxes = line.tax_id.compute_all(line.price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)['taxes']
+                taxes = line.tax_id.compute_all(
+                    line.price_reduce,
+                    quantity=line.product_uom_qty,
+                    product=line.product_id,
+                    partner=order.partner_shipping_id
+                )['taxes']
                 for tax in line.tax_id:
                     group = tax.tax_group_id
                     res.setdefault(group, {'amount': 0.0, 'base': 0.0})
                     for t in taxes:
-                        if t['id'] == tax.id or t['id'] in tax.children_tax_ids.ids:
+                        if t['id'] == tax.id or \
+                                t['id'] in tax.children_tax_ids.ids:
                             res[group]['amount'] += t['amount']
                             res[group]['base'] += t['base']
             res = sorted(res.items(), key=lambda l: l[0].sequence)
             order.amount_by_group = [(
-                l[0].name, l[1]['amount'], l[1]['base'],
-                fmt(l[1]['amount']), fmt(l[1]['base']),
+                ln[0].name, ln[1]['amount'], ln[1]['base'],
+                fmt(ln[1]['amount']), fmt(ln[1]['base']),
                 len(res),
-            ) for l in res]
+            ) for ln in res]
