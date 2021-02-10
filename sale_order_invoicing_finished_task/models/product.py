79536473a4536ef8db3f9dd718fe7bc5c64081e4
João Marques <joao.marques@tecnativa.com>
2021-02-10 11:30:48 +0000

    [IMP] sale_order_invoicing_finished_task: black, prettier, isort

--- sale_order_invoicing_finished_task/models/product.py
+++ sale_order_invoicing_finished_task/models/product.py
@@ -6,10 +6,9 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     invoicing_finished_task = fields.Boolean(
-        string='Invoicing control by task',
-        help='Invoice the order lines only when the task is set to '
-             'invoiceable',
+        string="Invoicing control by task",
+        help="Invoice the order lines only when the task is set to " "invoiceable",
     )
