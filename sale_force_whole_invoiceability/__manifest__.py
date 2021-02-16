d9b6deebafdf868fd2a6b95f0d4c3a16e162cff6
david <david.vidal@tecnativa.com>
2021-02-16 13:36:43 +0100

    [MIG] sale_force_whole_invoiceability: Migration to 13.0
    [UPD] Update sale_force_whole_invoiceability.pot
    
    [UPD] README.rst

--- sale_force_whole_invoiceability/__manifest__.py
+++ sale_force_whole_invoiceability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Force Whole Invoiceability",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/sale-workflow",
