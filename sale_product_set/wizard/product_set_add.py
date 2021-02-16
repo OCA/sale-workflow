eacc13f85a449b91d75e08e74fa1b8c15f0d79d4
Simone Orsi <simahawk@gmail.com>
2021-02-16 11:36:42 +0100

    sale_product_set: allow passing custom lines to wizard
    Now you can pass selected lines at creation via field value or ctx key.

--- sale_product_set/wizard/product_set_add.py
+++ sale_product_set/wizard/product_set_add.py
@@ -2,7 +2,7 @@
 # Copyright 2016-2020 Camptocamp SA
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models
 
 
 class ProductSetAdd(models.TransientModel):
@@ -21,6 +21,14 @@ class ProductSetAdd(models.TransientModel):
     product_set_id = fields.Many2one(
         "product.set", "Product set", required=True, ondelete="cascade"
     )
+    product_set_line_ids = fields.Many2many(
+        "product.set.line",
+        "Product set lines",
+        required=True,
+        ondelete="cascade",
+        compute="_compute_product_set_line_ids",
+        readonly=False,
+    )
     quantity = fields.Float(
         digits="Product Unit of Measure", required=True, default=1.0
     )
@@ -29,6 +37,25 @@ class ProductSetAdd(models.TransientModel):
         help="Enable this to not add new lines "
         "for products already included in SO lines.",
     )
+
+    @api.depends_context("product_set_add__set_line_ids")
+    @api.depends("product_set_id")
+    def _compute_product_set_line_ids(self):
+        line_ids = self.env.context.get("product_set_add__set_line_ids", [])
+        lines_from_ctx = self.env["product.set.line"].browse(line_ids)
+        for rec in self:
+            if rec.product_set_line_ids:
+                # Passed on creation
+                continue
+            lines = lines_from_ctx.filtered(
+                lambda x: x.product_set_id == rec.product_set_id
+            )
+            if lines:
+                # Use the ones from ctx but make sure they belong to the same set.
+                rec.product_set_line_ids = lines
+            else:
+                # Fallback to all lines from current set
+                rec.product_set_line_ids = rec.product_set_id.set_line_ids
 
     def _check_partner(self):
         """Validate order partner against product set's partner if any."""
@@ -85,7 +112,7 @@ class ProductSetAdd(models.TransientModel):
     def _get_lines(self):
         # hook here to take control on used lines
         so_product_ids = self.order_id.order_line.mapped("product_id").ids
-        for set_line in self.product_set_id.set_line_ids:
+        for set_line in self.product_set_line_ids:
             if self.skip_existing_products and set_line.product_id.id in so_product_ids:
                 continue
             yield set_line
