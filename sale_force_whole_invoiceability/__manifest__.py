4744b83088645063a167f22b4c4e2ab1a594b4c9
david <david.vidal@tecnativa.com>
2021-02-16 13:19:40 +0100

    [IMP] sale_force_whole_invoiceability: black, isort, prettier

--- sale_force_whole_invoiceability/__manifest__.py
+++ sale_force_whole_invoiceability/__manifest__.py
@@ -4,15 +4,10 @@
     "name": "Sale Force Whole Invoiceability",
     "version": "12.0.1.0.0",
     "category": "Sales",
-    "author": "Tecnativa,"
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    "depends": [
-        "sale",
-    ],
-    "data": [
-        "views/sale_order_views.xml",
-    ],
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
     "installable": True,
 }
--- sale_force_whole_invoiceability/models/sale_order.py
+++ sale_force_whole_invoiceability/models/sale_order.py
@@ -10,16 +10,18 @@ class SaleOrder(models.Model):
     def force_lines_to_invoice_policy_order(self):
         """Wrapper to launch the private method from UI"""
         if not self.user_has_groups("sales_team.group_sale_manager"):
-            raise UserError(_(
-                "Only Sales Managers are allowed to force the lines to invoice"
-            ))
+            raise UserError(
+                _("Only Sales Managers are allowed to force the lines to invoice")
+            )
         if self.state != "sale":
-            raise UserError(_(
-                "You can't perform this action over a sale order in this state"
-            ))
+            raise UserError(
+                _("You can't perform this action over a sale order in this state")
+            )
         if self.invoice_count:
-            raise UserError(_(
-                "You can't perform this action once the order has been "
-                "invoiced once already"
-            ))
+            raise UserError(
+                _(
+                    "You can't perform this action once the order has been "
+                    "invoiced once already"
+                )
+            )
         self._force_lines_to_invoice_policy_order()
