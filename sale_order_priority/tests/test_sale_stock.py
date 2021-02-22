880409846ab403232589a1bff6b843d36d4b94c0
mreficent <miquel.raich@eficent.com>
2021-02-22 14:29:40 +0100

    [IMP] sale_order_priority: black, isort, prettier

--- sale_order_priority/tests/test_sale_stock.py
+++ sale_order_priority/tests/test_sale_stock.py
@@ -4,33 +4,44 @@ from odoo.addons.sale.tests.test_sale_common import TestSale
 
 
 class TestSaleStock(TestSale):
-
     def test_sale_order_priority(self):
-        sale_order_priority = '3'
-        self.sale_order = self.env['sale.order'].create({
-            'partner_id': self.partner.id,
-            'priority': sale_order_priority,
-            'order_line': [(0, 0, {
-                'name': p.name,
-                'product_id': p.id,
-                'product_uom_qty': 2,
-                'product_uom': p.uom_id.id,
-                'price_unit': p.list_price})
-                for (_, p) in self.products.items()],
-        })
-        sale_order_line_priority = '2'
+        sale_order_priority = "3"
+        self.sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "priority": sale_order_priority,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": p.name,
+                            "product_id": p.id,
+                            "product_uom_qty": 2,
+                            "product_uom": p.uom_id.id,
+                            "price_unit": p.list_price,
+                        },
+                    )
+                    for (_, p) in self.products.items()
+                ],
+            }
+        )
+        sale_order_line_priority = "2"
         for sol in self.sale_order.order_line:
             # Test that the order's priority has been
             # correctly assigned to the order lines
             self.assertEqual(
-                sol.priority, sale_order_priority,
-                'Priority of order lines does not match')
+                sol.priority,
+                sale_order_priority,
+                "Priority of order lines does not match",
+            )
             sol.priority = sale_order_line_priority
         # Confirm the order and check the picking
         self.sale_order.action_confirm()
         # Test that the lines' priority has been
         # correctly assigned to the generated pickings
         self.assertEqual(
-            max(self.sale_order.picking_ids.mapped('priority')),
+            max(self.sale_order.picking_ids.mapped("priority")),
             sale_order_line_priority,
-            'Priority of generated picking does not match')
+            "Priority of generated picking does not match",
+        )
