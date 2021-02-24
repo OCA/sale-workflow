30a21eac9989cc20464c70b1ea9b529b1488610b
Quentin Groulard <quentin.groulard@acsone.eu>
2021-02-24 14:04:59 +0100

    [IMP] sale_promotion_rule: black, isort, prettier

--- sale_promotion_rule/__manifest__.py
+++ sale_promotion_rule/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com)
 # Benoît GUILLOT <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -9,17 +8,15 @@
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
-    'author': 'Akretion, ACSONE SA/NV, Odoo Community Association (OCA)',
+    "author": "Akretion, ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "sale",
-    ],
+    "depends": ["sale"],
     "data": [
-        'wizards/sale_order_add_coupon.xml',
-        'views/sale_order.xml',
-        'views/sale_promotion_rule.xml',
-        'security/ir.model.access.csv',
-    ]
+        "wizards/sale_order_add_coupon.xml",
+        "views/sale_order.xml",
+        "views/sale_promotion_rule.xml",
+        "security/ir.model.access.csv",
+    ],
 }
--- sale_promotion_rule/readme/ROADMAP.rst
+++ sale_promotion_rule/readme/ROADMAP.rst
@@ -1,4 +1,3 @@
 * Add "automatic" rule that are automatically applied without coupon (only coupon have been implmented)
 * Add gift support. (Only discount offer have been implemented)
 * Improve condition by using custom filter on partner
-
