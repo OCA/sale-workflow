aed4fbe900bc2bbb920b2ff45ee7e409fd9aae40
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-02-26 19:44:05 +0530

    [MIG][14.0] sale_require_po_doc.

--- sale_require_po_doc/models/sale_order.py
+++ sale_require_po_doc/models/sale_order.py
@@ -1,38 +1,46 @@
-# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     customer_need_po = fields.Boolean(
         related="partner_id.customer_need_po",
-        string='Customer Requires PO', readonly=True,
+        string="Customer Requires PO",
+        readonly=True,
     )
     sale_doc = fields.Text(
         related="partner_id.sale_doc",
-        string='Sales Require Documentation', readonly=True,
+        string="Sales Require Documentation",
+        readonly=True,
     )
     sale_document_option = fields.Selection(
-        [('no_need', 'No documentation needed for this Sale'),
-         ('done', 'Documentation required obtained and archived')],
-        string="Sale Documentation")
-    sale_document_note = fields.Text(
-        string='Sale Documentation Notes',
+        [
+            ("no_need", "No documentation needed for this Sale"),
+            ("done", "Documentation required obtained and archived"),
+        ],
+        string="Sale Documentation",
     )
+    sale_document_note = fields.Text(string="Sale Documentation Notes")
 
-    @api.multi
     def action_confirm(self):
         for order in self:
             if order.customer_need_po and not order.client_order_ref:
                 raise ValidationError(
-                    _("You can not confirm sale order without \
-                        Customer reference."))
+                    _(
+                        "You can not confirm sale order without \
+                    Customer reference."
+                    )
+                )
             if order.sale_doc and not order.sale_document_option:
                 raise ValidationError(
-                    _("You can not confirm sale order without \
-                        Sale Documentation."))
+                    _(
+                        "You can not confirm sale order without \
+                    Sale Documentation."
+                    )
+                )
         return super(SaleOrder, self).action_confirm()
