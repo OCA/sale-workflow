92a6938b58e702fe41403f53b03b4a4e80fed2bb
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-02-26 19:44:05 +0530

    [MIG][14.0] sale_require_po_doc.

--- sale_require_po_doc/__manifest__.py
+++ sale_require_po_doc/__manifest__.py
@@ -1,18 +1,15 @@
-# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sale Orders Require PO or Sales Documentation",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
     "depends": ["sale_management"],
-    "data": [
-        "views/res_partner_view.xml",
-        "views/sale_order_view.xml",
-    ],
+    "data": ["views/res_partner_view.xml", "views/sale_order_view.xml"],
     "auto_install": False,
     "application": False,
     "installable": True,
--- sale_require_po_doc/models/res_partner.py
+++ sale_require_po_doc/models/res_partner.py
@@ -1,19 +1,21 @@
-# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     customer_need_po = fields.Boolean(
-        string='Customer Requires PO', track_visibility="onchange",
+        string="Customer Requires PO",
+        tracking=True,
         help="A PO number will be required on the"
         " Sales Order field for Customer Reference",
     )
     sale_doc = fields.Text(
-        string='Sales Require Documentation', track_visibility="onchange",
+        string="Sales Require Documentation",
+        tracking=True,
         help="Details of the sales documentation required for this customer."
         " Sales documentation notes field will be required on Sales Orders"
         " This is complementary to requiring a PO number, and can be used"
