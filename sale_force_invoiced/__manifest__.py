3074ff9ff5369e770f5b1586c0c8f51f8f876a41
ferran-73 <ferran@studio73.es>
2021-03-05 13:55:44 +0100

    [MIG] sale_force_invoice: Migration 14.0

--- sale_force_invoiced/__manifest__.py
+++ sale_force_invoiced/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Force Invoiced",
     "summary": "Allows to force the invoice status of the sales order to " "Invoiced",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Eficent," "Odoo Community Association (OCA)",
     "category": "sale",
     "license": "AGPL-3",
--- sale_force_invoiced/model/sale_order.py
+++ sale_force_invoiced/model/sale_order.py
@@ -24,3 +24,26 @@ class SaleOrder(models.Model):
             lambda so: so.force_invoiced and so.invoice_status == "to invoice"
         ):
             order.invoice_status = "invoiced"
+        deposit_product_id = self.env["sale.advance.payment.inv"]._default_product_id()
+        line_invoice_status_all = [
+            (d["order_id"][0], d["invoice_status"])
+            for d in self.env["sale.order.line"].read_group(
+                [
+                    ("order_id", "in", self.ids),
+                    ("product_id", "!=", deposit_product_id.id),
+                ],
+                ["order_id", "invoice_status"],
+                ["order_id", "invoice_status"],
+                lazy=False,
+            )
+        ]
+        for order in self.filtered(
+            lambda so: not so.force_invoiced and so.invoice_status == "invoiced"
+        ):
+            line_invoice_status = [
+                d[1] for d in line_invoice_status_all if d[0] == order.id
+            ]
+            if any(
+                invoice_status == "to invoice" for invoice_status in line_invoice_status
+            ):
+                order.invoice_status = "to invoice"
