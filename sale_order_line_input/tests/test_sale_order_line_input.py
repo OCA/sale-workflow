42b12f369bca38d6b149efeeb09f5b5d0c7d3b9e
Rad0van <radovan@skolnik.info>
2021-03-08 14:08:14 +0100

    [IMP] sale_order_line_input: black, isort, prettier

--- sale_order_line_input/tests/test_sale_order_line_input.py
+++ sale_order_line_input/tests/test_sale_order_line_input.py
@@ -5,26 +5,26 @@ from odoo.tests import SavepointCase
 
 
 class TestSaleOrderLineInput(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.partner = cls.env['res.partner'].create({'name': 'Test'})
-        cls.product = cls.env['product.product'].create({
-            'name': 'test_product',
-            'type': 'service',
-        })
+        cls.partner = cls.env["res.partner"].create({"name": "Test"})
+        cls.product = cls.env["product.product"].create(
+            {"name": "test_product", "type": "service",}
+        )
 
     def test_sale_order_create_and_show(self):
-        line = self.env['sale.order.line'].create({
-            'order_partner_id': self.partner.id,
-            'product_id': self.product.id,
-            'price_unit': 190.50,
-            'product_uom': self.ref('product.decimal_product_uom'),
-            'product_uom_qty': 8.0,
-            'name': 'Test line description',
-        })
+        line = self.env["sale.order.line"].create(
+            {
+                "order_partner_id": self.partner.id,
+                "product_id": self.product.id,
+                "price_unit": 190.50,
+                "product_uom": self.ref("product.decimal_product_uom"),
+                "product_uom_qty": 8.0,
+                "name": "Test line description",
+            }
+        )
         self.assertTrue(line.order_id)
         action_dict = line.action_sale_order_form()
-        self.assertEquals(action_dict['res_id'], line.order_id.id)
-        self.assertEquals(action_dict['res_model'], 'sale.order')
+        self.assertEquals(action_dict["res_id"], line.order_id.id)
+        self.assertEquals(action_dict["res_model"], "sale.order")
