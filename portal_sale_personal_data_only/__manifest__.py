4bc12fb55bc18ffc239bae239e3ec6370df4e6b9
Kiril Vangelovski <kiril@lambda-is.com>
2021-03-09 14:58:08 +0100

    [13.0][FIX] portal_sale_personal_data_only: invalid domain in ir.rule

--- portal_sale_personal_data_only/__manifest__.py
+++ portal_sale_personal_data_only/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Portal Sale Personal Data Only",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Sale",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/sale-workflow",
