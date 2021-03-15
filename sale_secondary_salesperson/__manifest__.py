533e9d48447d6f0163621046b4c54d6309410812
Víctor Martínez <victor.martinez@tecnativa.com>
2021-03-15 09:42:17 +0100

    [MIG] sale_secondary_salesperson: Migration to 13.0

--- sale_secondary_salesperson/__manifest__.py
+++ sale_secondary_salesperson/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Secondary Salesperson",
     "category": "Sales",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["victoralmau"],
