aae796397b5b4a27e7490410f2c61d97362438d0
Eder Brito <britoederr@gmail.com>
2021-03-18 01:59:20 +0000

    cleanning pre-commit fails

--- sale_crm_set_won/models/res_config_settings.py
+++ sale_crm_set_won/models/res_config_settings.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 
 class ResConfigSettings(models.TransientModel):
 
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     crm_set_won = fields.Boolean(
         string="Set Opportunity as Won",
@@ -14,16 +14,16 @@ class ResConfigSettings(models.TransientModel):
 
     crm_set_won_option = fields.Selection(
         selection=[
-            ('set_100','Set Probability as 100%'),
-            ('choose_stage','Choose Stage'),
+            ("set_100", "Set Probability as 100%"),
+            ("choose_stage", "Choose Stage"),
         ],
         string="What To Do?",
         required="[('crm_set_won','=',True)]",
-        config_parameter='sale_crm_set_won.crm_set_won_option',
+        config_parameter="sale_crm_set_won.crm_set_won_option",
     )
 
     stage_id = fields.Many2one(
         comodel_name="crm.stage",
         string="Stage",
-        config_parameter='sale_crm_set_won.stage_id',
+        config_parameter="sale_crm_set_won.stage_id",
     )
