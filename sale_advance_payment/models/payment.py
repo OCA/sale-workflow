7201000d13452f0bc8463d0702aa9b4383a2331b
Omar (Comunitea) <omar@comunitea.com>
2021-03-25 17:12:36 +0100

    [ADD] sale_advance_payment: Register payments in advance

---
+++ sale_advance_payment/models/payment.py
@@ -0,0 +1,12 @@
+# Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class AccountPayment(models.Model):
+
+    _inherit = "account.payment"
+
+    sale_id = fields.Many2one('sale.order', "Sale", readonly=True,
+                              states={'draft': [('readonly', False)]})
