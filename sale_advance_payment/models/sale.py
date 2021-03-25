6f92c7f2bb210e45e1a2d9571bf08f7e18276e9d
Omar (Comunitea) <omar@comunitea.com>
2021-03-25 17:12:36 +0100

    [ADD] sale_advance_payment: Register payments in advance

---
+++ sale_advance_payment/models/sale.py
@@ -0,0 +1,34 @@
+# Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    @api.multi
+    def _get_payment_move_line_ids(self):
+        for sale in self:
+            sale.payment_line_ids = sale.account_payment_ids.\
+                mapped('move_line_ids').\
+                filtered(lambda x: x.account_id.internal_type == 'receivable')
+
+    @api.multi
+    def _get_amount_residual(self):
+        for sale in self:
+            advance_amount = 0.0
+            for line in sale.account_payment_ids:
+                if line.state != 'draft':
+                    advance_amount += line.amount
+            sale.amount_residual = sale.amount_total - advance_amount
+
+    account_payment_ids = fields.One2many('account.payment', 'sale_id',
+                                          string="Pay sale advanced",
+                                          readonly=True)
+    amount_residual = fields.Float('Residual amount', readonly=True,
+                                   compute="_get_amount_residual")
+    payment_line_ids = fields.Many2many('account.move.line',
+                                        string="Payment move lines",
+                                        compute="_get_payment_move_line_ids")
---
+++ sale_advance_payment/wizard/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import sale_advance_payment_wzd
