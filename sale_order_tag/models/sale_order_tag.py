4a7530e6f332110ce78494f8c67dcc02213a3b79
Joan <joan.segui@qubiq.es>
2021-03-26 16:44:54 +0100

    [IMP] sale_order_tag: black, isort, prettier

--- sale_order_tag/models/sale_order_tag.py
+++ sale_order_tag/models/sale_order_tag.py
@@ -12,12 +12,13 @@ class SaleOrderTag(models.Model):
     name = fields.Char(required=True)
     description = fields.Text(string="Description")
     color = fields.Integer(string="Color Index")
-    team_ids = fields.Many2many('crm.team', string="Sales Teams")
+    team_ids = fields.Many2many("crm.team", string="Sales Teams")
     active = fields.Boolean(string="Active", default=True)
-    company_id = fields.Many2one('res.company', string='Company')
+    company_id = fields.Many2one("res.company", string="Company")
     sequence = fields.Integer(
-        default=lambda self: self.env['ir.sequence'].next_by_code(
-            'sale.order.tag') or 0, required=True)
+        default=lambda self: self.env["ir.sequence"].next_by_code("sale.order.tag")
+        or 0,
+        required=True,
+    )
 
-    _sql_constraints = [("name_uniq", "unique (name)",
-                         "Tag name already exists!")]
+    _sql_constraints = [("name_uniq", "unique (name)", "Tag name already exists!")]
