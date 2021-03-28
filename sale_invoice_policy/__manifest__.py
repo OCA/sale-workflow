ea0194e7702acad9413402d6a283f064f2394f8e
Alexei Rivera <alexeirivera87@gmail.com>
2021-03-28 11:15:36 -0400

    [IMP] sale_invoice_policy: black, isort, prettier

--- sale_invoice_policy/__manifest__.py
+++ sale_invoice_policy/__manifest__.py
@@ -1,23 +1,20 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Sale invoice Policy",
-    'summary': """
+    "name": "Sale invoice Policy",
+    "summary": """
         Sales Management: let the user choose the invoice policy on the
         order""",
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'category': 'Sales Management',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale',
-        'sale_stock',
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales Management",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["sale", "sale_stock",],
+    "data": [
+        "wizards/res_config_view.xml",
+        "views/product_template_view.xml",
+        "views/sale_view.xml",
     ],
-    'data': [
-        'wizards/res_config_view.xml',
-        'views/product_template_view.xml',
-        'views/sale_view.xml',
-    ],
-    'post_init_hook': 'post_init_hook',
+    "post_init_hook": "post_init_hook",
 }
