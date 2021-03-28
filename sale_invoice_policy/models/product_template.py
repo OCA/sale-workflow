5f648adca1af2970c5f0d55d35065e981a978a50
Alexei Rivera <alexeirivera87@gmail.com>
2021-03-28 11:15:36 -0400

    [IMP] sale_invoice_policy: black, isort, prettier

--- sale_invoice_policy/models/product_template.py
+++ sale_invoice_policy/models/product_template.py
@@ -8,30 +8,30 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     default_invoice_policy = fields.Selection(
-        [('order', 'Ordered quantities'),
-         ('delivery', 'Delivered quantities')],
-        string='Default Invoicing Policy',
-        help='Ordered Quantity: Invoice based on the quantity the customer '
-             'ordered.\n'
-             'Delivered Quantity: Invoiced based on the quantity the vendor '
-             'delivered (time or deliveries).',
-        default='order')
+        [("order", "Ordered quantities"), ("delivery", "Delivered quantities")],
+        string="Default Invoicing Policy",
+        help="Ordered Quantity: Invoice based on the quantity the customer "
+        "ordered.\n"
+        "Delivered Quantity: Invoiced based on the quantity the vendor "
+        "delivered (time or deliveries).",
+        default="order",
+    )
 
     invoice_policy = fields.Selection(
         compute="_compute_invoice_policy",
         store=False,
         readonly=True,
-        search='_search_invoice_policy',
-        inverse='_inverse_invoice_policy',
+        search="_search_invoice_policy",
+        inverse="_inverse_invoice_policy",
     )
 
     @api.multi
     def _inverse_invoice_policy(self):
-        for template in self.filtered('invoice_policy'):
+        for template in self.filtered("invoice_policy"):
             template.default_invoice_policy = template.invoice_policy
 
     @api.multi
-    @api.depends('type', 'default_invoice_policy')
+    @api.depends("type", "default_invoice_policy")
     def _compute_invoice_policy(self):
         """
         Apply the invoice_policy given by context (if exist) otherwise use the
@@ -40,13 +40,13 @@ class ProductTemplate(models.Model):
         policy given by the context.
         :return:
         """
-        invoice_policy = self.env.context.get('invoice_policy')
+        invoice_policy = self.env.context.get("invoice_policy")
         for tmpl in self:
-            if tmpl.type != 'service' and invoice_policy:
+            if tmpl.type != "service" and invoice_policy:
                 tmpl.invoice_policy = invoice_policy
             else:
                 tmpl.invoice_policy = tmpl.default_invoice_policy
 
     @api.model
     def _search_invoice_policy(self, operator, value):
-        return [('default_invoice_policy', operator, value)]
+        return [("default_invoice_policy", operator, value)]
