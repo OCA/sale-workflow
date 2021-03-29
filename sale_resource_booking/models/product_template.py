b7981e956f6fbc4510caeaf81f7f81749bb4a157
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-03-29 11:41:34 +0100

    [ADD] sale_resource_booking: sell booking products
    This module extends the functionality of sale and resource bookings to support
    creating pending resource bookings automatically when confirming a sale order
    and to allow you to generate those sale orders easily from a resource booking
    type.
    
    @Tecnativa TT28202

---
+++ sale_resource_booking/models/product_template.py
@@ -0,0 +1,28 @@
+# Copyright 2021 Tecnativa - Jairo Llopis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    resource_booking_type_id = fields.Many2one(
+        "resource.booking.type",
+        string="Booking type",
+        index=True,
+        ondelete="restrict",
+        help="If set, one pending booking will be generated when sold.",
+    )
+    resource_booking_type_combination_rel_id = fields.Many2one(
+        "resource.booking.type.combination.rel",
+        string="Resource combination",
+        index=True,
+        ondelete="restrict",
+        domain="[('type_id', '=', resource_booking_type_id)]",
+        help=(
+            "If set, the booking will be created with this resource combination. "
+            "Otherwise, the combination will be assigned automatically later, "
+            "when the requester schedules the booking."
+        ),
+    )
