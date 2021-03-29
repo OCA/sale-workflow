8e5a40ec3f4119aef6017d4d789b37ed70aaafb0
Denis Roussel <denis.roussel@acsone.eu>
2021-03-29 09:44:12 +0200

    [IMP] sale_procurement_amendment: black, isort, prettier

--- sale_procurement_amendment/models/sale_order_line.py
+++ sale_procurement_amendment/models/sale_order_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -8,17 +7,13 @@ from odoo.tools import float_compare
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    to_be_procured = fields.Boolean(
-        compute='_compute_to_be_procured',
-    )
-    pickings_in_progress = fields.Boolean(
-        compute='_compute_pickings_in_progress'
-    )
+    to_be_procured = fields.Boolean(compute="_compute_to_be_procured",)
+    pickings_in_progress = fields.Boolean(compute="_compute_pickings_in_progress")
 
     @api.multi
-    @api.depends('order_id.picking_ids.can_be_amended')
+    @api.depends("order_id.picking_ids.can_be_amended")
     def _compute_pickings_in_progress(self):
         """
         Compute the picking in progress. That depends on picking
@@ -27,51 +22,54 @@ class SaleOrderLine(models.Model):
         :return:
         """
         for line in self.filtered(
-                lambda l: any(
-                    picking.state != 'cancel' and
-                    not picking.can_be_amended
-                    for picking in l.order_id.picking_ids)):
+            lambda l: any(
+                picking.state != "cancel" and not picking.can_be_amended
+                for picking in l.order_id.picking_ids
+            )
+        ):
             line.pickings_in_progress = True
 
     @api.multi
     @api.depends(
-        'procurement_ids',
-        'procurement_ids.state',
-        'procurement_ids.product_qty',
-        'product_uom_qty')
+        "procurement_ids",
+        "procurement_ids.state",
+        "procurement_ids.product_qty",
+        "product_uom_qty",
+    )
     def _compute_to_be_procured(self):
         """
         This compare the current procurement quantities (see Odoo
         _action_procurement_create function)
         :return:
         """
-        precision = self.env['decimal.precision'].precision_get(
-            'Product Unit of Measure')
+        precision = self.env["decimal.precision"].precision_get(
+            "Product Unit of Measure"
+        )
         for line in self:
             qty = 0.0
-            for proc in line.procurement_ids.filtered(
-                    lambda p: p.state != 'cancel'):
+            for proc in line.procurement_ids.filtered(lambda p: p.state != "cancel"):
                 qty += proc.product_qty
-            if float_compare(
-                    qty,
-                    line.product_uom_qty,
-                    precision_digits=precision) < 0:
+            if float_compare(qty, line.product_uom_qty, precision_digits=precision) < 0:
                 line.to_be_procured = True
 
     @api.multi
     def write(self, values):
-        precision = self.env['decimal.precision'].precision_get(
-            'Product Unit of Measure')
-        lines_lower = self.env['sale.order.line'].browse()
-        if 'product_uom_qty' in values:
+        precision = self.env["decimal.precision"].precision_get(
+            "Product Unit of Measure"
+        )
+        lines_lower = self.env["sale.order.line"].browse()
+        if "product_uom_qty" in values:
             lines_lower = self.filtered(
-                lambda l: l.state == 'sale' and
-                float_compare(
+                lambda l: l.state == "sale"
+                and float_compare(
                     l.product_uom_qty,
-                    values['product_uom_qty'],
-                    precision_digits=precision) > 0)
+                    values["product_uom_qty"],
+                    precision_digits=precision,
+                )
+                > 0
+            )
         res = super(SaleOrderLine, self).write(values)
-        if 'product_uom_qty' in values:
+        if "product_uom_qty" in values:
             # Quantities has changed
             # Check if procurement has to be created
             for line in lines_lower:
@@ -80,6 +78,6 @@ class SaleOrderLine(models.Model):
                     for procurement in line.procurement_ids:
                         procurement.cancel()
             self.filtered(
-                lambda line: line.state == 'sale' and
-                line.to_be_procured)._action_procurement_create()
+                lambda line: line.state == "sale" and line.to_be_procured
+            )._action_procurement_create()
         return res
