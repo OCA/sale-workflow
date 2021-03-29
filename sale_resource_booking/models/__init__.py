7178a0ade6e9532f7f050d8168ed623f81c001cf
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-03-29 11:41:34 +0100

    [ADD] sale_resource_booking: sell booking products
    This module extends the functionality of sale and resource bookings to support
    creating pending resource bookings automatically when confirming a sale order
    and to allow you to generate those sale orders easily from a resource booking
    type.
    
    @Tecnativa TT28202

---
+++ sale_resource_booking/models/__init__.py
@@ -0,0 +1,5 @@
+from . import product_template
+from . import resource_booking
+from . import resource_booking_type
+from . import sale_order
+from . import sale_order_line
---
+++ sale_resource_booking/readme/CONFIGURE.rst
@@ -0,0 +1,4 @@
+To enjoy all features from this module, you need these permissions:
+
+* Sales / User: Own Documents Only
+* Resource Booking / User
