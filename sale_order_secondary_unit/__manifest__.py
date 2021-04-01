fd29636b839a132800c0d4797fc815242f5a6bd3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-04-01 17:23:30 -0400

    [IMP] sale_order_secondary_unit: Add menu for secondary UoM

--- sale_order_secondary_unit/__manifest__.py
+++ sale_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Secondary Unit",
     "summary": "Sale product in a secondary unit",
-    "version": "15.0.1.1.1",
+    "version": "15.0.1.2.0",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
@@ -14,6 +14,7 @@
     "auto_install": True,
     "depends": ["sale", "product_secondary_unit"],
     "data": [
+        "views/product_secondary_unit_views.xml",
         "views/product_views.xml",
         "views/sale_order_views.xml",
         "report/sale_report_templates.xml",
