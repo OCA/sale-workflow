8f0003bb681fafdea3b6b120197908340c64efce
Mateu Griful <mateu.griful@forgeflow.com>
2021-04-12 17:45:03 +0200

    [ADD] sale_order_invoice_amount

---
+++ sale_order_invoice_amount/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2021 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Sale Order Invoice Amount",
+    "version": "13.0.1.0.1",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "summary": "Display the invoiced and uninvoiced total in the sale order",
+    "depends": ["sale"],
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
+}
