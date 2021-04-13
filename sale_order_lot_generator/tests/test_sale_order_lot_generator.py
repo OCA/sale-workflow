90fc46080f2de9579578c19fe0ef207a5e53ee73
Pierrick Brun <pierrick.brun@akretion.com>
2021-04-13 17:57:13 +0200

    [FIX] allow to create lines in redrafted orders

--- sale_order_lot_generator/tests/test_sale_order_lot_generator.py
+++ sale_order_lot_generator/tests/test_sale_order_lot_generator.py
@@ -8,8 +8,12 @@ class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
     def setUp(self):
         super().setUp()
         self.prd_flipover = self.env.ref("product.product_product_20")
+        self.prd_desk = self.env.ref("product.product_product_22")
         self.prd_acoustic = self.env.ref("product.product_product_25")
         self.prd_flipover.write(
+            {"tracking": "lot", "type": "product", "auto_generate_prodlot": True}
+        )
+        self.prd_desk.write(
             {"tracking": "lot", "type": "product", "auto_generate_prodlot": True}
         )
         self.prd_acoustic.write(
@@ -34,19 +38,35 @@ class TestSaleOrderLotGenerator(test_common.SingleTransactionCase):
         self.order1.action_confirm()
         lot_number = "%s-%03d" % (self.order1.name, 1)
         self.assertEqual(self.sol1.lot_id.name, lot_number)
-        # add second line after order confirm
+        # add second line after order redraft
+        self.order1.action_cancel()
+        self.order1.action_draft()
         self.sol2 = self.env["sale.order.line"].create(
             {
-                "name": "sol1",
+                "name": "sol2",
+                "order_id": self.order1.id,
+                "product_id": self.prd_desk.id,
+                "product_uom_qty": 1,
+            }
+        )
+        self.order1.action_confirm()
+        lot_number = "%s-%03d" % (self.order1.name, 2)
+        self.assertEqual(self.sol2.lot_id.name, lot_number)
+        # add third line after order confirm
+        self.sol3 = self.env["sale.order.line"].create(
+            {
+                "name": "sol3",
                 "order_id": self.order1.id,
                 "product_id": self.prd_acoustic.id,
                 "product_uom_qty": 1,
             }
         )
-        lot_number = "%s-%03d" % (self.order1.name, 2)
-        self.assertEqual(self.sol2.lot_id.name, lot_number)
+        lot_number = "%s-%03d" % (self.order1.name, 3)
+        self.assertEqual(self.sol3.lot_id.name, lot_number)
         for line in self.order1.picking_ids.move_line_ids:
             if line.product_id.id == self.prd_flipover.id:
                 self.assertEqual(line.lot_id, self.sol1.lot_id)
-            if line.product_id.id == self.prd_acoustic.id:
+            if line.product_id.id == self.prd_desk.id:
                 self.assertEqual(line.lot_id, self.sol2.lot_id)
+            if line.product_id.id == self.prd_acoustic.id:
+                self.assertEqual(line.lot_id, self.sol3.lot_id)
