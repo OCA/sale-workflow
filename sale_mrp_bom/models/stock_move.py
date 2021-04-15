9e2637dc082268fa7e10c8f8bce6ba590532ea18
Hai Lang <hailn@trobz.com>
2021-04-15 09:53:58 +0700

    [IMP] sale_mrp_bom: black, isort, prettier

--- sale_mrp_bom/models/stock_move.py
+++ sale_mrp_bom/models/stock_move.py
@@ -5,10 +5,10 @@ from odoo import models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     def _prepare_procurement_values(self):
         values = super()._prepare_procurement_values()
         if self.sale_line_id and self.sale_line_id.bom_id:
-            values['bom_id'] = self.sale_line_id.bom_id
+            values["bom_id"] = self.sale_line_id.bom_id
         return values
