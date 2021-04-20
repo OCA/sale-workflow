a791f56de2eced19b62eb319269b507f143fb5e4
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-04-20 15:12:35 -0300

    [MIG] sale_manual_delivery: Migration to 13.0

--- sale_manual_delivery/models/sale_order.py
+++ sale_manual_delivery/models/sale_order.py
@@ -1,8 +1,9 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools.translate import _
 
 
 class SaleOrder(models.Model):
@@ -11,38 +12,28 @@ class SaleOrder(models.Model):
     manual_delivery = fields.Boolean(
         string="Manual Delivery",
         default=False,
-        help="If Manual, the deliveries are not created at SO confirmation.\
-        You need to use the Create Delivery button in order to reserve and \
-        ship the goods.",
+        help="If enabled, the deliveries are not created at SO confirmation. "
+        "You need to use the Create Delivery button in order to reserve "
+        "and ship the goods.",
     )
 
     @api.onchange("team_id")
     def _onchange_team_id(self):
-        for so in self:
-            if so.team_id:
-                so.manual_delivery = so.team_id.manual_delivery
-            else:
-                so.manual_delivery = False
+        self.manual_delivery = self.team_id.manual_delivery
 
-    @api.multi
     def action_manual_delivery_wizard(self):
         self.ensure_one()
-        wizard = self.env["manual.delivery"].create({"carrier_id": self.carrier_id.id})
-        action = self.env.ref(
-            "sale_manual_delivery.action_wizard_manual_delivery"
-        ).read()[0]
-        action["res_id"] = wizard.id
+        action = self.env.ref("sale_manual_delivery.action_wizard_manual_delivery")
+        [action] = action.read()
+        action["context"] = {"default_carrier_id": self.carrier_id.id}
         return action
 
-    @api.multi
-    def toggle_manual(self):
-        self.ensure_one()
-        if isinstance(self.id, int):  # if already saved
-            if self.state not in ("draft", "sent"):
-                raise UserError(
-                    _(
-                        "You can only change to/from manual delivery"
-                        " in a quote, not a confirmed order"
-                    )
+    @api.constrains("manual_delivery")
+    def _check_manual_delivery(self):
+        if any(rec.state not in ["draft", "sent"] for rec in self):
+            raise UserError(
+                _(
+                    "You can only change to/from manual delivery"
+                    " in a quote, not a confirmed order"
                 )
-            self.manual_delivery = not self.manual_delivery
+            )
--- sale_manual_delivery/readme/DESCRIPTION.rst
+++ sale_manual_delivery/readme/DESCRIPTION.rst
@@ -1,5 +1,5 @@
 The goal of this module is to allow the manual creation of delivery slips. If activated,
-a sale order won't directly impact directly the stock. It will not make a reservation on the stock.
+a sale order won't directly impact the stock. It will not make a reservation on the stock.
 It allows the delivery and the impact on stock to be done manually when needed.
 The goal is to be used on long term projects where not all the material is shipped at once.
 As you make many shipments, every time you make a new delivery, you can choose an new carrier as well
