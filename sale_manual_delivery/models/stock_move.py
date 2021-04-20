bfd3103f7c487ba6150dbbe52438a9d93dda55a0
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-04-20 15:12:35 -0300

    [MIG] sale_manual_delivery: Migration to 13.0

--- sale_manual_delivery/models/stock_move.py
+++ sale_manual_delivery/models/stock_move.py
@@ -1,41 +1,38 @@
 # Copyright 2017 Denis Leemann, Camptocamp SA
+# Copyright 2021 Iván Todorovich, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method")
-
     def _get_new_picking_values(self):
-        res = super(StockMove, self)._get_new_picking_values()
-        if self.env.context.get("vals"):
-            vals = self.env.context.get("vals")
-            if vals.get("carrier_id"):
-                res["carrier_id"] = vals.get("carrier_id")
+        # Overload to set carrier_id from the manual delivery wizard
+        # Note: sale_manual_delivery is expected to be a manual.delivery record
+        res = super()._get_new_picking_values()
+        manual_delivery = self.env.context.get("sale_manual_delivery")
+        if manual_delivery:
+            if manual_delivery.partner_id:
+                res["partner_id"] = manual_delivery.partner_id.id
+            if manual_delivery.carrier_id:
+                res["carrier_id"] = manual_delivery.carrier_id.id
         return res
 
-    @api.multi
-    def _assign_picking(self):
-        # Redefine method to filter on date_expected (allows to group moves
-        # from the same manual delivery in one delivery)
-        if "manual_delivery" not in self.env.context:
-            return super(StockMove, self)._assign_picking()
-        Picking = self.env["stock.picking"]
-        self.recompute()
-
-        for move in self:
+    def _search_picking_for_assignation(self):
+        # Overload to filter carrier_id
+        # Note: sale_manual_delivery is expected to be a manual.delivery record
+        manual_delivery = self.env.context.get("sale_manual_delivery")
+        if manual_delivery:
+            # original domain used in super()
             domain = [
-                ("group_id", "=", move.group_id.id),
-                ("location_id", "=", move.location_id.id),
-                ("location_dest_id", "=", move.location_dest_id.id),
-                ("picking_type_id", "=", move.picking_type_id.id),
+                ("group_id", "=", self.group_id.id),
+                ("location_id", "=", self.location_id.id),
+                ("location_dest_id", "=", self.location_dest_id.id),
+                ("picking_type_id", "=", self.picking_type_id.id),
                 ("printed", "=", False),
-                ("min_date", "=", move.date_expected),
-                ("max_date", "=", move.date_expected),
-                ("carrier_id", "=", move.carrier_id.id),
+                ("immediate_transfer", "=", False),
                 (
                     "state",
                     "in",
@@ -48,8 +45,11 @@ class StockMove(models.Model):
                     ],
                 ),
             ]
-            picking = Picking.search(domain, limit=1)
-            if not picking.sale_id.manual_delivery:
-                picking = Picking.create(move._get_new_picking_values())
-            move.write({"picking_id": picking.id})
-        return True
+            # Filter on carrier
+            if manual_delivery.carrier_id:
+                domain += [
+                    ("carrier_id", "=", manual_delivery.carrier_id.id),
+                ]
+            return self.env["stock.picking"].search(domain, limit=1)
+        else:
+            return super()._search_picking_for_assignation()
