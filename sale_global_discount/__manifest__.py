c56492d575230278125b809b1185711f6b125f5b
david <david.vidal@tecnativa.com>
2021-04-20 13:55:29 +0200

    [MIG] sale_global_discount: Migration to 13.0
    [UPD] Update sale_global_discount.pot
    
    [UPD] README.rst

--- sale_global_discount/__manifest__.py
+++ sale_global_discount/__manifest__.py
@@ -3,12 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Global Discount",
-    "version": "12.0.1.2.0",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    "depends": ["account_global_discount"],
+    "depends": ["account_global_discount", "sale"],
     "data": ["views/sale_order_views.xml", "views/report_sale_order.xml"],
     "application": False,
     "installable": True,
