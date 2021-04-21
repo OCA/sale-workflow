b51dc9c38e78825d98ce6e420cbc2c5fb527631b
Denis Roussel <denis.roussel@acsone.eu>
2021-04-21 14:00:51 +0200

    [13.0][ADD] sale_order_line_chained_move

---
+++ sale_order_line_chained_move/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Line Chained Move",
+    "summary": """
+        This module adds a field on sale order line to get all related move lines""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["rousseldenis"],
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale_stock"],
+    "post_init_hook": "post_init_hook",
+}
