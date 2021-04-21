f8ff0d67832597c351afd1bad7ffdf0793e88e81
Denis Roussel <denis.roussel@acsone.eu>
2021-04-21 18:58:43 +0200

    [13.0][IMP] sale_order_line_chained_move: Add post_init_hook

---
+++ sale_order_line_chained_move/hooks.py
@@ -0,0 +1,24 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import SUPERUSER_ID, api
+
+
+def __fill_related(moves, line_id):
+    for move in moves:
+        move.related_sale_line_id = line_id
+        if move.move_orig_ids:
+            __fill_related(move.move_orig_ids, line_id)
+
+
+def _fill_in_related_sale_line(env):
+    """ Update related_sale_line_id on recursive moves
+    """
+    moves = env["stock.move"].search([("sale_line_id", "!=", False)])
+    for move in moves:
+        __fill_related(move.move_orig_ids, move.sale_line_id)
+
+
+def post_init_hook(cr, registry):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        _fill_in_related_sale_line(env)
