aa73f2954787bfd5a913929df7106bcd323206e7
Víctor Martínez <victor.martinez@tecnativa.com>
2021-05-04 10:39:02 +0200

    [ADD] sale_order_qty_change_no_recompute: Prevent recompute if only quantity has changed in sale order line

---
+++ sale_order_qty_change_no_recompute/tests/test_sale_order_qty_change.py
@@ -0,0 +1,33 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.tests.common import Form, SavepointCase
+
+
+class TestSaleOrderQtyChange(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product_1 = cls.env["product.product"].create(
+            {"name": "Test Product 1", "list_price": 25.00}
+        )
+        cls.product_2 = cls.env["product.product"].create(
+            {"name": "Test Product 2", "list_price": 30.00}
+        )
+        sale_form = Form(cls.env["sale.order"])
+        sale_form.partner_id = cls.env.ref("base.res_partner_12")
+        with sale_form.order_line.new() as cls.line_form:
+            cls.line_form.product_id = cls.product_1
+            cls.line_form.product_uom_qty = 1
+
+    def test_sale_line_misc(self):
+        self.assertEqual(self.line_form.price_unit, 25)
+        self.assertEqual(self.line_form.price_subtotal, 25)
+        self.line_form.price_unit = 10
+        self.assertEqual(self.line_form.price_unit, 10)
+        self.assertEqual(self.line_form.price_subtotal, 10)
+        self.line_form.product_uom_qty = 2
+        self.assertEqual(self.line_form.price_unit, 10)
+        self.assertEqual(self.line_form.price_subtotal, 20)
+        self.line_form.product_id = self.product_2
+        self.assertEqual(self.line_form.price_unit, 30)
+        self.assertEqual(self.line_form.price_subtotal, 60)
