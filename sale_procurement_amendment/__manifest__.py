3dbee85c1169215319640040fed1c4fd0ff2e4d5
Denis Roussel <denis.roussel@acsone.eu>
2021-05-05 19:18:14 +0200

    [14.0][MIG] sale_procurement_amendment

--- sale_procurement_amendment/__manifest__.py
+++ sale_procurement_amendment/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Allow to reflect confirmed sale lines quantity amendments
         to procurements""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "category": "Sales",
     "development_status": "Production/Stable",
