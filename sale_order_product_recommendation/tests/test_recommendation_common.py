437765a8d6fc72c1cd49d9e0a8f0eedfd93a07e1
Carlos Roca <carlos.roca@tecnativa.com>
2021-05-07 12:07:29 +0200

    [IMP] sale_order_product_recommendation: Added option to filter sale_orders by delivery address

--- sale_order_product_recommendation/tests/test_recommendation_common.py
+++ sale_order_product_recommendation/tests/test_recommendation_common.py
@@ -15,6 +15,12 @@ class RecommendationCase(SavepointCase):
         cls.partner = cls.env["res.partner"].create(
             {"name": "Mr. Odoo", "property_product_pricelist": cls.pricelist.id}
         )
+        cls.partner_delivery = cls.env["res.partner"].create(
+            {
+                "name": "Mr. Odoo Delivery",
+                "property_product_pricelist": cls.pricelist.id,
+            }
+        )
         cls.product_obj = cls.env["product.product"]
         cls.prod_1 = cls.product_obj.create(
             {"name": "Test Product 1", "type": "service", "list_price": 25.00}
@@ -26,11 +32,12 @@ class RecommendationCase(SavepointCase):
             {"name": "Test Product 3", "type": "service", "list_price": 75.00}
         )
         # Create old sale orders to have searchable history
+        # (Remember to change the dates if the tests fail)
         cls.order1 = cls.env["sale.order"].create(
             {
                 "partner_id": cls.partner.id,
                 "state": "done",
-                "date_order": "2020-11-15",
+                "date_order": "2021-05-05",
                 "order_line": [
                     (
                         0,
@@ -74,8 +81,9 @@ class RecommendationCase(SavepointCase):
         cls.order2 = cls.env["sale.order"].create(
             {
                 "partner_id": cls.partner.id,
+                "partner_shipping_id": cls.partner_delivery.id,
                 "state": "done",
-                "date_order": "2020-11-10",
+                "date_order": "2021-05-03",
                 "order_line": [
                     (
                         0,
