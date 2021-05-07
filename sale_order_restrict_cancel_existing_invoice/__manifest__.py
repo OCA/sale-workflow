7e02864dea9643ebf6c5b29649ad1fb3cffd289e
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-05-07 16:21:11 +0200

    [IMP] sale_order_restrict_cancel_existing_invoice: black, isort, prettier

--- sale_order_restrict_cancel_existing_invoice/__manifest__.py
+++ sale_order_restrict_cancel_existing_invoice/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Order Restrict Cancel Existing Invoice',
-    'summary': """
+    "name": "Sale Order Restrict Cancel Existing Invoice",
+    "summary": """
         Allows to restrict sale order cancelling if an invoice exists""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'maintainers': ['rousseldenis'],
-    'category': 'Sales',
-    'development_status': 'Production/Stable',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'depends': [
-        'sale',
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "category": "Sales",
+    "development_status": "Production/Stable",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": [
+        "sale",
     ],
 }
--- sale_order_restrict_cancel_existing_invoice/models/sale_order.py
+++ sale_order_restrict_cancel_existing_invoice/models/sale_order.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     confirmed_invoice_ids = fields.Many2many(
         comodel_name="account.invoice",
         compute="_compute_confirmed_invoice_ids",
-        help="Technical field in order to retrieve confirmed invoices"
+        help="Technical field in order to retrieve confirmed invoices",
     )
 
     def _get_confirmed_invoice_states(self):
@@ -28,12 +27,15 @@ class SaleOrder(models.Model):
     def _compute_confirmed_invoice_ids(self):
         for sale in self:
             sale.confirmed_invoice_ids = sale.invoice_ids.filtered(
-                lambda i, states=self._get_confirmed_invoice_states():
-                i.state in states)
+                lambda i, states=self._get_confirmed_invoice_states(): i.state in states
+            )
 
     def action_cancel(self):
         if any(sale.confirmed_invoice_ids for sale in self):
             raise UserError(
-                _("You cannot cancel a Sale Order for which a confirmed "
-                  "invoice exists!"))
+                _(
+                    "You cannot cancel a Sale Order for which a confirmed "
+                    "invoice exists!"
+                )
+            )
         return super(SaleOrder, self).action_cancel()
