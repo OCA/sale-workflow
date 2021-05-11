acf00b9311d4cb6907d4d3895d14b0c0a4989ed7
Quentin Groulard <quentin.groulard@acsone.eu>
2021-05-11 15:11:38 +0200

    [IMP] Add sale demo data
    Fix tests using demo data modified by other modules

--- sale_promotion_rule/__manifest__.py
+++ sale_promotion_rule/__manifest__.py
@@ -19,5 +19,5 @@
         "views/sale_promotion_rule.xml",
         "security/ir.model.access.csv",
     ],
-    "demo": ["demo/product.xml"],
+    "demo": ["demo/product.xml", "demo/sale_demo.xml"],
 }
