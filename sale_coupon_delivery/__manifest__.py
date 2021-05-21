5cb45f216ebec55ef040d0f7b0aebc4f28d50358
eLBati <lb@takobi.online>
2021-05-21 16:59:24 +0200

    Backporting sale_coupon_delivery to v12

--- sale_coupon_delivery/__manifest__.py
+++ sale_coupon_delivery/__manifest__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': "Sale Coupon Delivery",
     'summary': """Allows to offer free shippings in coupon reward""",
     'description': """Integrate coupon mechanism with shipping costs.""",
     'category': 'Sales/Sales',
-    'version': '1.0',
+    'version': '12.0.1.0.0',
     'depends': ['sale_coupon', 'delivery'],
     'data': [
     ],
