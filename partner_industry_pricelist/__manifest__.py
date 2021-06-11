34de4ee89b7e9f268a56700f3a6f479aa4a6da93
Luis Felipe Mileo <mileo@kmee.com.br>
2021-06-10 23:24:08 -0300

    [NEW] Partner Industry Pricelist

---
+++ partner_industry_pricelist/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Industry Pricelist',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/sale-workflow',
+    'depends': [
+        'sale',
+        'contacts',
+    ],
+    'data': [
+        'views/res_partner_industry.xml',
+    ],
+    'demo': [
+        'demo/res_partner_industry.xml',
+    ],
+}
