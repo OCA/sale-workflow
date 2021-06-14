6e80d0bb6094f7b86e84215afebec0a6b35fd5ca
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2021-06-14 13:41:59 +0530

    [FIX] Travis

--- sale_sourced_by_line/model/__init__.py
+++ sale_sourced_by_line/model/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2014 Camptocamp SA - Guewen Baconnier
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # © 2016 Serpent Consulting Services Pvt. Ltd.
