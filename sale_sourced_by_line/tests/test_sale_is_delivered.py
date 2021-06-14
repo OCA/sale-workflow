c7b67b1cc9e2a4e91cb5ae146d713141b5580600
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2021-06-14 13:41:59 +0530

    [FIX] Travis

--- sale_sourced_by_line/tests/test_sale_is_delivered.py
+++ sale_sourced_by_line/tests/test_sale_is_delivered.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Camptocamp SA - Yannick Vaucher
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -26,26 +25,24 @@ class TestSaleIsDelivered(TransactionCase):
     def test_sale_not_all_proc(self):
         """False, when one line with and one without procurement done"""
         self.sale_line1.procurement_group_id = self.proc_group1
-        self.proc1.state = 'done'
+        self.proc1.state = "done"
 
         self.assertFalse(self.sale.shipped)
 
     def test_sale_proc_and_service(self):
-        """True when, one line with procurement done and one line for service
-        """
+        """True when, one line with procurement done and one line for service"""
         self.sale_line1.procurement_group_id = self.proc_group1
-        self.proc1.state = 'done'
+        self.proc1.state = "done"
         self.sale_line2.product_id = self.service_product
 
         self.assertTrue(self.sale.shipped)
 
     def test_sale_partially_delivered(self):
-        """False when, all lines with procurement, one is partially delivered
-        """
+        """False when, all lines with procurement, one is partially delivered"""
         self.sale_line1.procurement_group_id = self.proc_group1
         self.sale_line2.procurement_group_id = self.proc_group2
-        self.proc1.state = 'done'
-        self.proc2.state = 'running'
+        self.proc1.state = "done"
+        self.proc2.state = "running"
 
         self.assertFalse(self.sale.shipped)
 
@@ -53,8 +50,8 @@ class TestSaleIsDelivered(TransactionCase):
         """True, when both line have a done procurement"""
         self.sale_line1.procurement_group_id = self.proc_group1
         self.sale_line2.procurement_group_id = self.proc_group2
-        self.proc1.state = 'done'
-        self.proc2.state = 'done'
+        self.proc1.state = "done"
+        self.proc2.state = "done"
 
         self.assertTrue(self.sale.shipped)
 
@@ -67,19 +64,18 @@ class TestSaleIsDelivered(TransactionCase):
 
         """
         super(TestSaleIsDelivered, self).setUp()
-        so = self.env['sale.order']
-        sol = self.env['sale.order.line']
-        product = self.env['product.product']
-        procurement = self.env['procurement.order']
-        procurement_group = self.env['procurement.group']
+        so = self.env["sale.order"]
+        sol = self.env["sale.order.line"]
+        product = self.env["product.product"]
+        procurement = self.env["procurement.order"]
+        procurement_group = self.env["procurement.group"]
         self.sale = so.new()
         self.sale_line1 = sol.new()
         self.sale_line2 = sol.new()
         self.sale_line1.order_id = self.sale
         self.sale_line2.order_id = self.sale
 
-        self.sale.order_line = sol.browse([self.sale_line1.id,
-                                           self.sale_line2.id])
+        self.sale.order_line = sol.browse([self.sale_line1.id, self.sale_line2.id])
 
         self.proc1 = procurement.new()
         self.proc_group1 = procurement_group.new()
@@ -89,4 +85,4 @@ class TestSaleIsDelivered(TransactionCase):
         self.proc_group2 = procurement_group.new()
         self.proc_group2.procurement_ids = self.proc2
 
-        self.service_product = product.new({'type': 'service'})
+        self.service_product = product.new({"type": "service"})
