d4325960ba8a927413acd7917ed0bc0f6f5977c1
dsolanki <dhara.solanki@initos.com>
2021-06-17 17:58:50 +0530

    [MIG] Migrate module sale_order_line_price_history to v14.

--- sale_order_line_price_history/__manifest__.py
+++ sale_order_line_price_history/__manifest__.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order line price history",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": [
+        "security/ir.model.access.csv",
         "wizards/sale_order_line_price_history.xml",
         "views/sale_views.xml",
         "views/assets.xml",
