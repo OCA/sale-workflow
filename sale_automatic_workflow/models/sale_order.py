1e0c648978ba92f0ebf2fbd8d42187b6d360bd64
sbejaoui <souheil.bejaoui@acsone.eu>
2021-06-30 15:04:55 +0200

    [14.0][FIX] - sale_automatic_workflow: fix method header

--- sale_automatic_workflow/models/sale_order.py
+++ sale_automatic_workflow/models/sale_order.py
@@ -63,14 +63,14 @@ class SaleOrder(models.Model):
             warning = {"title": _("Workflow Warning"), "message": workflow.warning}
             return {"warning": warning}
 
-    def _create_invoices(self, grouped=False, final=False):
+    def _create_invoices(self, grouped=False, final=False, date=None):
         for order in self:
             if not order.workflow_process_id.invoice_service_delivery:
                 continue
             for line in order.order_line:
                 if line.qty_delivered_method == "manual" and not line.qty_delivered:
                     line.write({"qty_delivered": line.product_uom_qty})
-        return super()._create_invoices(grouped=grouped, final=final)
+        return super()._create_invoices(grouped=grouped, final=final, date=date)
 
     def write(self, vals):
         if vals.get("state") == "sale" and vals.get("date_order"):
