e8863150bdf8018b59ed9e7cfdba3f4a198c447f
Simone Orsi <simahawk@gmail.com>
2021-07-08 10:22:49 +0200

    sale_delivery_state: refactor tests mock

--- sale_delivery_state/tests/test_delivery_state.py
+++ sale_delivery_state/tests/test_delivery_state.py
@@ -3,6 +3,8 @@
 # Copyright 2018 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import mock
+
 from odoo.tests import SavepointCase
 
 
@@ -14,6 +16,26 @@ class TestDeliveryState(SavepointCase):
         cls.order = cls.env.ref("sale_delivery_state.sale_order_1")
         cls.delivery_cost = cls.env["product.product"].create(
             {"name": "delivery", "type": "service"}
+        )
+
+    def _mock_delivery(self, delivery_prod=None):
+        delivery_prod = delivery_prod or self.delivery_cost
+        return mock.patch.object(
+            type(self.env["sale.order.line"]),
+            "_is_delivery",
+            lambda self: self.product_id == delivery_prod,
+        )
+
+    def _add_delivery_cost_line(self):
+        self.env["sale.order.line"].create(
+            {
+                "order_id": self.order.id,
+                "name": "Delivery cost",
+                "product_id": self.delivery_cost.id,
+                "product_uom_qty": 1,
+                "product_uom": self.env.ref("uom.product_uom_unit").id,
+                "price_unit": 10.0,
+            }
         )
 
     def test_no_delivery(self):
@@ -34,59 +56,38 @@ class TestDeliveryState(SavepointCase):
             line.qty_delivered = line.product_uom_qty
         self.assertEqual(self.order.delivery_state, "done")
 
-    def mock_delivery(self):
-        class DeliverySaleOrderLine(self.order.order_line._model_classes[0]):
-            def _is_delivery(self):
-                return True
-
-        for line in self.order.order_line:
-            if line.product_id == self.delivery_cost:
-                line.__class__ = DeliverySaleOrderLine
-
-    def add_delivery_cost_line(self):
-        self.env["sale.order.line"].create(
-            {
-                "order_id": self.order.id,
-                "name": "Delivery cost",
-                "product_id": self.delivery_cost.id,
-                "product_uom_qty": 1,
-                "product_uom": self.env.ref("uom.product_uom_unit").id,
-                "price_unit": 10.0,
-            }
-        )
-
     def test_no_delivery_delivery_cost(self):
-        self.add_delivery_cost_line()
-        self.mock_delivery()
-        self.assertEqual(self.order.delivery_state, "no")
+        self._add_delivery_cost_line()
+        with self._mock_delivery():
+            self.assertEqual(self.order.delivery_state, "no")
 
     def test_unprocessed_delivery_delivery_cost(self):
-        self.add_delivery_cost_line()
-        self.mock_delivery()
-        self.order.action_confirm()
-        self.assertEqual(self.order.delivery_state, "unprocessed")
+        self._add_delivery_cost_line()
+        with self._mock_delivery():
+            self.order.action_confirm()
+            self.assertEqual(self.order.delivery_state, "unprocessed")
 
     def test_partially_delivery_cost(self):
-        self.add_delivery_cost_line()
-        self.mock_delivery()
-        self.order.action_confirm()
-        self.order.order_line[0].qty_delivered = 2
-        self.assertEqual(self.order.delivery_state, "partially")
+        self._add_delivery_cost_line()
+        with self._mock_delivery():
+            self.order.action_confirm()
+            self.order.order_line[0].qty_delivered = 2
+            self.assertEqual(self.order.delivery_state, "partially")
 
     def test_forced_delivery_cost(self):
-        self.add_delivery_cost_line()
-        self.mock_delivery()
-        self.order.action_confirm()
-        self.order.order_line[0].qty_delivered = 2
-        self.order.force_delivery_state = True
-        self.assertEqual(self.order.delivery_state, "done")
+        self._add_delivery_cost_line()
+        with self._mock_delivery():
+            self.order.action_confirm()
+            self.order.order_line[0].qty_delivered = 2
+            self.order.force_delivery_state = True
+            self.assertEqual(self.order.delivery_state, "done")
 
     def test_delivery_done_delivery_cost(self):
-        self.add_delivery_cost_line()
-        self.mock_delivery()
-        self.order.action_confirm()
-        for line in self.order.order_line:
-            if line._is_delivery():
-                continue
-            line.qty_delivered = line.product_uom_qty
-        self.assertEqual(self.order.delivery_state, "done")
+        self._add_delivery_cost_line()
+        with self._mock_delivery():
+            self.order.action_confirm()
+            for line in self.order.order_line:
+                if line._is_delivery():
+                    continue
+                line.qty_delivered = line.product_uom_qty
+            self.assertEqual(self.order.delivery_state, "done")
