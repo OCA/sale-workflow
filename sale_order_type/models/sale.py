0ffd7cc23b728f15729258fbea9cdc42ba04b352
Carlos Serra-Toro <carlos.serra@bt-group.com>
2021-07-12 11:17:15 +0200

    [14.0][FIX] sale_order_type: sequence depending on date ranges
    The call to obtain the next sequence number for a sales order
    didn't take into account its date, so as a result the option
    to use a different sequence depending on the date was ignored.

--- sale_order_type/models/sale.py
+++ sale_order_type/models/sale.py
@@ -73,7 +73,9 @@ class SaleOrder(models.Model):
         if vals.get("name", "/") == "/" and vals.get("type_id"):
             sale_type = self.env["sale.order.type"].browse(vals["type_id"])
             if sale_type.sequence_id:
-                vals["name"] = sale_type.sequence_id.next_by_id()
+                vals["name"] = sale_type.sequence_id.next_by_id(
+                    sequence_date=vals["date_order"]
+                )
         return super(SaleOrder, self).create(vals)
 
     def write(self, vals):
@@ -88,7 +90,9 @@ class SaleOrder(models.Model):
                         and record.type_id.sequence_id != sale_type.sequence_id
                     ):
                         new_vals = vals.copy()
-                        new_vals["name"] = sale_type.sequence_id.next_by_id()
+                        new_vals["name"] = sale_type.sequence_id.next_by_id(
+                            sequence_date=vals.get("date_order")
+                        )
                         super(SaleOrder, record).write(new_vals)
                     else:
                         super(SaleOrder, record).write(vals)
