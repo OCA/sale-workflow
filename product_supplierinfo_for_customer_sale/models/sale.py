bbf9c2399e39e50d7d628a33cc967b90bab1aed2
Lois Rilo <lois.rilo@forgeflow.com>
2021-07-22 17:49:28 +0200

    [IMP] product_supplierinfo_for_customer_sale: black, isort, prettier

--- product_supplierinfo_for_customer_sale/models/sale.py
+++ product_supplierinfo_for_customer_sale/models/sale.py
@@ -6,42 +6,49 @@ from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     product_customer_code = fields.Char(
-        compute='_compute_product_customer_code',
-        string='Product Customer Code',
+        compute="_compute_product_customer_code", string="Product Customer Code",
     )
-    partner_is_customer = fields.Boolean(
-        related='order_id.partner_id.customer',
-    )
+    partner_is_customer = fields.Boolean(related="order_id.partner_id.customer",)
 
     @api.multi
-    @api.depends('product_id')
+    @api.depends("product_id")
     def _compute_product_customer_code(self):
         for line in self.filtered(lambda sol: sol.order_partner_id.customer):
             supplierinfo = self.get_customer_supplierinfo(line)
             line.product_customer_code = supplierinfo.product_code
 
     @api.multi
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         result = super(SaleOrderLine, self).product_id_change()
         for line in self.filtered(
-                lambda sol: sol.product_id.product_tmpl_id.customer_ids and
-                sol.order_id.pricelist_id.item_ids):
+            lambda sol: sol.product_id.product_tmpl_id.customer_ids
+            and sol.order_id.pricelist_id.item_ids
+        ):
             product = line.product_id
-            items = self.env['product.pricelist.item'].search([
-                ('pricelist_id', '=', line.order_id.pricelist_id.id),
-                ('compute_price', '=', 'formula'), ('base', '=', 'partner'),
-                '|', ('applied_on', '=', '3_global'),
-                '|', '&', ('categ_id', '=', product.categ_id.id),
-                ('applied_on', '=', '2_product_category'), '|',
-                '&', ('product_tmpl_id', '=', product.product_tmpl_id.id),
-                ('applied_on', '=', '1_product'), '&',
-                ('product_id', '=', product.id),
-                ('applied_on', '=', '0_product_variant'),
-            ])
+            items = self.env["product.pricelist.item"].search(
+                [
+                    ("pricelist_id", "=", line.order_id.pricelist_id.id),
+                    ("compute_price", "=", "formula"),
+                    ("base", "=", "partner"),
+                    "|",
+                    ("applied_on", "=", "3_global"),
+                    "|",
+                    "&",
+                    ("categ_id", "=", product.categ_id.id),
+                    ("applied_on", "=", "2_product_category"),
+                    "|",
+                    "&",
+                    ("product_tmpl_id", "=", product.product_tmpl_id.id),
+                    ("applied_on", "=", "1_product"),
+                    "&",
+                    ("product_id", "=", product.id),
+                    ("applied_on", "=", "0_product_variant"),
+                ]
+            )
             if items:
                 supplierinfo = self.get_customer_supplierinfo(line)
                 if supplierinfo and supplierinfo.min_qty:
@@ -53,15 +60,16 @@ class SaleOrderLine(models.Model):
         Search customerinfo for variant first, if it has not been found then
         search by product template
         """
-        customerinfo = self.env['product.customerinfo'].search(
-            [('name', '=', line.order_partner_id.id)] +
-            [
-                '|',
-                ('product_id', '=', line.product_id.id),
-                '&',
-                ('product_tmpl_id', '=', line.product_id.product_tmpl_id.id),
-                ('product_id', '=', False),
+        customerinfo = self.env["product.customerinfo"].search(
+            [("name", "=", line.order_partner_id.id)]
+            + [
+                "|",
+                ("product_id", "=", line.product_id.id),
+                "&",
+                ("product_tmpl_id", "=", line.product_id.product_tmpl_id.id),
+                ("product_id", "=", False),
             ],
             limit=1,
-            order='product_id, sequence, min_qty desc, price')
+            order="product_id, sequence, min_qty desc, price",
+        )
         return customerinfo
