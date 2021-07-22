da3b2280729427459a0c9dd03f36557f886e9537
sergio-teruel <sergio.teruel@tecnativa.com>
2021-07-22 23:08:45 +0200

    [ADD] sale_stock_last_date: New module TT31039

---
+++ sale_stock_last_date/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale Stock Last Date',
+    'summary': 'Displays last delivery date in sale order lines',
+    'version': '12.0.1.0.0',
+    'development_status': 'Beta',
+    'category': 'Sales',
+    'website': 'https://github.com/OCA/sale-workflow',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'sale_stock',
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+        'reports/sale_report_view.xml',
+    ],
+    'pre_init_hook': 'pre_init_hook',
+}
---
+++ sale_stock_last_date/reports/sale_report.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    last_delivery_date = fields.Datetime(
+        string='Last delivery date',
+    )
+
+    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+        fields = fields or {}
+        fields['last_delivery_date'] = ", l.last_delivery_date AS" \
+                                       " last_delivery_date"
+        groupby += ', l.last_delivery_date'
+        return super(SaleReport, self)._query(
+            with_clause, fields, groupby, from_clause
+        )
