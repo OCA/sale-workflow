1356b04e74106d05b04420fd2d9109150839d1d2
Chandresh Thakkar OSI <cthakkar@opensourceintegrators.com>
2021-07-22 19:23:55 +0530

    [MIG] Module migrated in v14.

--- sale_order_tag/__manifest__.py
+++ sale_order_tag/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Patrick Wilson <patrickraymondwilson@gmail.com>
+# Copyright 2021 Patrick Wilson <patrickraymondwilson@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,9 +7,9 @@
     "author": "Patrick Wilson, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sale",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
-    "depends": ["crm", "sale"],
+    "depends": ["crm", "sale_management"],
     "data": [
         "data/sale_order_tag_data.xml",
         "security/ir.model.access.csv",
