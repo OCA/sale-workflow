42bbab88a026a282e7b512343f4b528683f6224b
Chandresh Thakkar OSI <cthakkar@opensourceintegrators.com>
2021-07-22 19:23:55 +0530

    [MIG] Module migrated in v14.

--- sale_order_tag/models/__init__.py
+++ sale_order_tag/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Patrick Wilson <patrickraymondwilson@gmail.com>
+# Copyright 2021 Patrick Wilson <patrickraymondwilson@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import sale_order
