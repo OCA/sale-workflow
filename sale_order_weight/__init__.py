e0ad8b3630855a1e0b21abe2459dfce4d03a4ae0
Manuel Calero <manuelcalerosolis@gmail.com>
2021-07-27 11:33:51 +0200

    [ADD] sale_order_weight: New module to 13.0

---
+++ sale_order_weight/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021 Manuel Calero Solís (https://xtendoo.es)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import models
+from .post_init_hook import post_init_hook
---
+++ sale_order_weight/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 Manuel Calero Solís (https://xtendoo.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_sale_order_weight
