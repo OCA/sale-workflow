3bc10443e9721cb69e08fe89024fd325f32d3629
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-01 00:39:48 +0200

    [MIG] sale_rental from v12 to v14
    No more need to add users to the packaging group
    View of rental group is now possible on product.template form view

--- sale_rental/models/product.py
+++ sale_rental/models/product.py
@@ -1,6 +1,6 @@
-# Copyright 2014-2019 Akretion France (http://www.akretion.com)
+# Copyright 2014-2021 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
-# Copyright 2016-2019 Sodexis (http://sodexis.com)
+# Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
@@ -17,7 +17,7 @@ class ProductProduct(models.Model):
     # Link rented HW product -> rental service
     rental_service_ids = fields.One2many(
         'product.product', 'rented_product_id',
-        string='Rental Services')
+        string='Related Rental Services')
 
     @api.constrains('rented_product_id', 'must_have_dates', 'type', 'uom_id')
     def _check_rental(self):
@@ -39,3 +39,28 @@ class ProductProduct(models.Model):
                     raise ValidationError(_(
                         "The unit of measure of the rental product '%s' must "
                         "be 'Day'.") % product.name)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    rented_product_tmpl_id = fields.Many2one(
+        'product.template', compute='_compute_rented_product_tmpl_id',
+        string='Rented Product',
+        inverse='_set_rented_product_tmpl_id', store=True)
+    rental_service_tmpl_ids = fields.One2many(
+        'product.template', 'rented_product_tmpl_id',
+        string='Rental Services')
+
+    @api.depends('product_variant_ids', 'product_variant_ids.rented_product_id')
+    def _compute_rented_product_tmpl_id(self):
+        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+        for template in unique_variants:
+            template.rented_product_tmpl_id = template.product_variant_ids.rented_product_id.product_tmpl_id.id
+        for template in (self - unique_variants):
+            template.rented_product_tmpl_id = False
+
+    def _set_rented_product_tmpl_id(self):
+        for template in self:
+            if len(template.product_variant_ids) == 1:
+                template.product_variant_ids.rented_product_id = template.rented_product_tmpl_id.product_variant_ids[0].id
