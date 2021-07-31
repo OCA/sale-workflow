92ef3af1914c8aacdbf3a9d50654626248b78455
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-01 00:39:48 +0200

    [MIG] sale_rental from v12 to v14
    No more need to add users to the packaging group
    View of rental group is now possible on product.template form view

--- sale_rental/__manifest__.py
+++ sale_rental/__manifest__.py
@@ -1,15 +1,16 @@
-# Copyright 2014-2019 Akretion France (http://www.akretion.com)
+# Copyright 2014-2021 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
-# Copyright 2016-2019 Sodexis (http://sodexis.com)
+# Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Rental',
-    'version': '12.0.1.1.0',
+    'version': '14.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Manage Rental of Products',
     'author': 'Akretion, Sodexis, Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/sale-workflow',
     'depends': ['sale_start_end_dates', 'sale_stock', 'sales_team'],
     'data': [
@@ -22,7 +23,6 @@
         'wizard/create_rental_product_view.xml',
         'views/product.xml',
     ],
-    'post_init_hook': 'add_to_group_stock_packaging',
     'demo': ['demo/rental_demo.xml'],
     'installable': True,
 }
--- sale_rental/models/sale_order.py
+++ sale_rental/models/sale_order.py
@@ -1,13 +1,12 @@
-# Copyright 2014-2019 Akretion (http://www.akretion.com)
+# Copyright 2014-2021 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
-# Copyright 2016-2019 Sodexis (http://sodexis.com)
+# Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_compare
 from dateutil.relativedelta import relativedelta
-import odoo.addons.decimal_precision as dp
 import logging
 
 
@@ -19,17 +18,16 @@ class SaleOrder(models.Model):
 
     def action_cancel(self):
         """
-            In case cancelling a SO which sells rental product.
-            Picking in should be created manually
+        When the user cancels a rental extension, Odoo writes the initial
+        end date on the return picking
         """
-        res = super(SaleOrder, self).action_cancel()
+        res = super().action_cancel()
         for order in self:
             for line in order.order_line.filtered(
                     lambda l: l.rental_type == 'rental_extension' and
                     l.extension_rental_id):
                 initial_end_date = line.extension_rental_id.end_date
                 line.extension_rental_id.in_move_id.write({
-                    'date_expected': initial_end_date,
                     'date': initial_end_date,
                     })
         return res
@@ -42,16 +40,19 @@ class SaleOrderLine(models.Model):
     can_sell_rental = fields.Boolean(string='Can Sell from Rental')
     rental_type = fields.Selection([
         ('new_rental', 'New Rental'),
-        ('rental_extension', 'Rental Extension')], 'Rental Type',
+        ('rental_extension', 'Rental Extension')], string='Rental Type',
         readonly=True, states={'draft': [('readonly', False)]})
     extension_rental_id = fields.Many2one(
-        'sale.rental', string='Rental to Extend')
+        'sale.rental', string='Rental to Extend', check_company=True,
+        readonly=True, states={'draft': [('readonly', False)]})
     rental_qty = fields.Float(
         string='Rental Quantity',
-        digits=dp.get_precision('Product Unit of Measure'),
+        digits='Product Unit of Measure',
+        readonly=True, states={'draft': [('readonly', False)]},
         help="Indicate the number of items that will be rented.")
     sell_rental_id = fields.Many2one(
-        'sale.rental', string='Rental to Sell')
+        'sale.rental', string='Rental to Sell', check_company=True,
+        readonly=True, states={'draft': [('readonly', False)]})
 
     _sql_constraints = [(
         'rental_qty_positive',
@@ -68,7 +69,7 @@ class SaleOrderLine(models.Model):
                 if not line.extension_rental_id:
                     raise ValidationError(_(
                         "Missing 'Rental to Extend' on the sale order line "
-                        "with rental service %s") % line.product_id.name)
+                        "with rental service %s") % line.product_id.display_name)
 
                 if line.rental_qty != line.extension_rental_id.rental_qty:
                     raise ValidationError(_(
@@ -77,15 +78,15 @@ class SaleOrderLine(models.Model):
                         "quantity (%s) that is different from the quantity "
                         "of the original rental (%s). This is not supported.")
                         % (
-                        line.product_id.name,
+                        line.product_id.display_name,
                         line.rental_qty,
                         line.extension_rental_id.rental_qty))
             if line.rental_type in ('new_rental', 'rental_extension'):
                 if not line.product_id.rented_product_id:
                     raise ValidationError(_(
                         "On the 'new rental' sale order line with product "
-                        "'%s', we should have a rental service product !") % (
-                        line.product_id.name))
+                        "'%s', we should have a rental service product !")
+                        % line.product_id.display_name)
                 if line.product_uom_qty !=\
                         line.rental_qty * line.number_of_days:
                     raise ValidationError(_(
@@ -93,7 +94,7 @@ class SaleOrderLine(models.Model):
                         "the Product Quantity (%s) should be the "
                         "number of days (%s) "
                         "multiplied by the Rental Quantity (%s).") % (
-                        line.product_id.name, line.product_uom_qty,
+                        line.product_id.display_name, line.product_uom_qty,
                         line.number_of_days, line.rental_qty))
                 # the module sale_start_end_dates checks that, when we have
                 # must_have_dates, we have start + end dates
@@ -104,7 +105,7 @@ class SaleOrderLine(models.Model):
                         "you are trying to sell a rented product with a "
                         "quantity (%s) that is different from the rented "
                         "quantity (%s). This is not supported.") % (
-                        line.product_id.name,
+                        line.product_id.display_name,
                         line.product_uom_qty,
                         line.sell_rental_id.rental_qty))
 
@@ -118,7 +119,7 @@ class SaleOrderLine(models.Model):
             'group_id': group,
             'sale_line_id': self.id,
             'date_planned': self.start_date,
-            'route_ids': self.order_id.warehouse_id.rental_route_id,
+            'route_ids': self.route_id or self.order_id.warehouse_id.rental_route_id,
             'warehouse_id': self.order_id.warehouse_id or False,
             'partner_id': self.order_id.partner_shipping_id.id,
             }
@@ -126,13 +127,18 @@ class SaleOrderLine(models.Model):
 
     def _run_rental_procurement(self, vals):
         self.ensure_one()
-        self.env['procurement.group'].run(
-            self.product_id.rented_product_id, self.rental_qty,
+        procurements = [self.env['procurement.group'].Procurement(
+            self.product_id.rented_product_id,
+            self.rental_qty,
             self.product_id.rented_product_id.uom_id,
             self.order_id.warehouse_id.rental_out_location_id,
-            self.name, self.order_id.name, vals)
+            self.name,
+            self.order_id.name,
+            self.order_id.company_id,
+            vals)]
+        self.env['procurement.group'].run(procurements)
 
-    def _action_launch_stock_rule(self):
+    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
         errors = []
         for line in self:
             if (
@@ -164,7 +170,6 @@ class SaleOrderLine(models.Model):
                 end_datetime = fields.Datetime.to_datetime(
                     line.end_date)
                 line.extension_rental_id.in_move_id.write({
-                    'date_expected': end_datetime,
                     'date': end_datetime,
                     })
             elif line.sell_rental_id:
@@ -179,7 +184,7 @@ class SaleOrderLine(models.Model):
             raise UserError('\n'.join(errors))
 
         # call super() at the end, to make procurement_jit work
-        res = super(SaleOrderLine, self)._action_launch_stock_rule()
+        res = super()._action_launch_stock_rule(previous_product_uom_qty=previous_product_uom_qty)
         return res
 
     def _prepare_procurement_values(self, group_id=False):
@@ -187,7 +192,7 @@ class SaleOrderLine(models.Model):
             Overriding this function to changethe route
             on selling rental product
         """
-        vals = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
+        vals = super()._prepare_procurement_values(group_id=group_id)
         if self.sell_rental_id:
             vals.update({
                 'route_ids':
@@ -268,7 +273,7 @@ class SaleOrderLine(models.Model):
                     "The Rental Service of the Rental Extension you just "
                     "selected is '%s' and it's not the same as the "
                     "Product currently selected in this Sale Order Line.")
-                    % self.extension_rental_id.rental_product_id.name)
+                    % self.extension_rental_id.rental_product_id.display_name)
             initial_end_date = self.extension_rental_id.end_date
             self.start_date = initial_end_date + relativedelta(days=1)
             self.rental_qty = self.extension_rental_id.rental_qty
