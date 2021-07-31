30d31b738e80eff3074ace08d1efe231a4b92d85
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-01 00:42:48 +0200

    sale_rental: black, isort, etc.

--- sale_rental/models/stock_rule.py
+++ sale_rental/models/stock_rule.py
@@ -7,17 +7,18 @@ from odoo import fields, models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     def _push_prepare_move_copy_values(self, move_to_copy, new_date):
         """Inherit to write the end date of the rental on the return move"""
         res = super()._push_prepare_move_copy_values(move_to_copy, new_date)
-        location_id = res.get('location_id', False)
-        if location_id and\
-            location_id ==\
-            move_to_copy.warehouse_id.rental_out_location_id.id and\
-                move_to_copy.sale_line_id and\
-                move_to_copy.sale_line_id.rental_type == 'new_rental':
+        location_id = res.get("location_id", False)
+        if (
+            location_id
+            and location_id == move_to_copy.warehouse_id.rental_out_location_id.id
+            and move_to_copy.sale_line_id
+            and move_to_copy.sale_line_id.rental_type == "new_rental"
+        ):
             rental_end_date = move_to_copy.sale_line_id.end_date
-            res['date'] = fields.Datetime.to_datetime(rental_end_date)
+            res["date"] = fields.Datetime.to_datetime(rental_end_date)
         return res
