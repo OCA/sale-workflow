129d3035dad091e90e67feb728179e45c4108db2
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-01 00:42:48 +0200

    sale_rental: black, isort, etc.

--- sale_rental/models/stock_warehouse.py
+++ sale_rental/models/stock_warehouse.py
@@ -3,9 +3,10 @@
 # Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
 import logging
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 logger = logging.getLogger(__name__)
 
@@ -14,21 +15,30 @@ class StockWarehouse(models.Model):
     _inherit = "stock.warehouse"
 
     rental_view_location_id = fields.Many2one(
-        'stock.location', 'Parent Rental', check_company=True,
-        domain="[('usage', '=', 'view'), ('company_id', '=', company_id)]")
+        "stock.location",
+        "Parent Rental",
+        check_company=True,
+        domain="[('usage', '=', 'view'), ('company_id', '=', company_id)]",
+    )
     rental_in_location_id = fields.Many2one(
-        'stock.location', 'Rental In', check_company=True,
-        domain="[('usage', '=', 'internal'), ('company_id', '=', company_id)]")
+        "stock.location",
+        "Rental In",
+        check_company=True,
+        domain="[('usage', '=', 'internal'), ('company_id', '=', company_id)]",
+    )
     rental_out_location_id = fields.Many2one(
-        'stock.location', 'Rental Out', check_company=True,
-        domain="[('usage', '=', 'internal'), ('company_id', '=', company_id)]")
-    rental_allowed = fields.Boolean('Rental Allowed')
-    rental_route_id = fields.Many2one(
-        'stock.location.route', string='Rental Route')
+        "stock.location",
+        "Rental Out",
+        check_company=True,
+        domain="[('usage', '=', 'internal'), ('company_id', '=', company_id)]",
+    )
+    rental_allowed = fields.Boolean("Rental Allowed")
+    rental_route_id = fields.Many2one("stock.location.route", string="Rental Route")
     sell_rented_product_route_id = fields.Many2one(
-        'stock.location.route', string='Sell Rented Product Route')
+        "stock.location.route", string="Sell Rented Product Route"
+    )
 
-    @api.onchange('rental_allowed')
+    @api.onchange("rental_allowed")
     def _onchange_rental_allowed(self):
         if not self.rental_allowed:
             self.rental_view_location_id = False
@@ -39,172 +49,201 @@ class StockWarehouse(models.Model):
 
     def _get_rental_push_pull_rules(self):
         self.ensure_one()
-        route_obj = self.env['stock.location.route']
+        route_obj = self.env["stock.location.route"]
         try:
-            rental_route = self.env.ref('sale_rental.route_warehouse0_rental')
+            rental_route = self.env.ref("sale_rental.route_warehouse0_rental")
         except Exception:
-            rental_routes = route_obj.search([('name', '=', _('Rent'))])
+            rental_routes = route_obj.search([("name", "=", _("Rent"))])
             rental_route = rental_routes and rental_routes[0] or False
         if not rental_route:
             raise UserError(_("Can't find any generic 'Rent' route."))
         try:
             sell_rented_product_route = self.env.ref(
-                'sale_rental.route_warehouse0_sell_rented_product')
+                "sale_rental.route_warehouse0_sell_rented_product"
+            )
         except Exception:
             sell_rented_product_routes = route_obj.search(
-                [('name', '=', _('Sell Rented Product'))])
-            sell_rented_product_route =\
-                sell_rented_product_routes and sell_rented_product_routes[0]\
-                or False
+                [("name", "=", _("Sell Rented Product"))]
+            )
+            sell_rented_product_route = (
+                sell_rented_product_routes and sell_rented_product_routes[0] or False
+            )
         if not sell_rented_product_route:
-            raise UserError(_(
-                "Can't find any generic 'Sell Rented Product' route."))
+            raise UserError(_("Can't find any generic 'Sell Rented Product' route."))
         if not self.rental_in_location_id:
-            raise UserError(_(
-                "The Rental Input stock location is not set on the "
-                "warehouse %s") % self.name)
+            raise UserError(
+                _("The Rental Input stock location is not set on the " "warehouse %s")
+                % self.name
+            )
         if not self.rental_out_location_id:
-            raise UserError(_(
-                "The Rental Output stock location is not set on the "
-                "warehouse %s") % self.name)
+            raise UserError(
+                _("The Rental Output stock location is not set on the " "warehouse %s")
+                % self.name
+            )
         rental_pull_rule = {
-            'name': self._format_rulename(
-                self.rental_in_location_id,
-                self.rental_out_location_id, ''),
-            'location_src_id': self.rental_in_location_id.id,
-            'location_id': self.rental_out_location_id.id,
-            'route_id': rental_route.id,
-            'action': 'pull',
-            'picking_type_id': self.out_type_id.id,
-            'warehouse_id': self.id,
-            'company_id': self.company_id.id,
+            "name": self._format_rulename(
+                self.rental_in_location_id, self.rental_out_location_id, ""
+            ),
+            "location_src_id": self.rental_in_location_id.id,
+            "location_id": self.rental_out_location_id.id,
+            "route_id": rental_route.id,
+            "action": "pull",
+            "picking_type_id": self.out_type_id.id,
+            "warehouse_id": self.id,
+            "company_id": self.company_id.id,
         }
         rental_push_rule = {
-            'name': self._format_rulename(
-                self.rental_out_location_id,
-                self.rental_in_location_id, ''),
-            'location_src_id': self.rental_out_location_id.id,
-            'location_id': self.rental_in_location_id.id,
-            'route_id': rental_route.id,
-            'action': 'push',
-            'picking_type_id': self.in_type_id.id,
-            'warehouse_id': self.id,
-            'company_id': self.company_id.id,
+            "name": self._format_rulename(
+                self.rental_out_location_id, self.rental_in_location_id, ""
+            ),
+            "location_src_id": self.rental_out_location_id.id,
+            "location_id": self.rental_in_location_id.id,
+            "route_id": rental_route.id,
+            "action": "push",
+            "picking_type_id": self.in_type_id.id,
+            "warehouse_id": self.id,
+            "company_id": self.company_id.id,
         }
-        customer_loc = self.env.ref('stock.stock_location_customers')
+        customer_loc = self.env.ref("stock.stock_location_customers")
         sell_rented_product_pull_rule = {
-            'name': self._format_rulename(
-                self.rental_out_location_id, customer_loc, ''),
-            'location_src_id': self.rental_out_location_id.id,
-            'location_id': customer_loc.id,
-            'route_id': sell_rented_product_route.id,
-            'action': 'pull',
-            'picking_type_id': self.out_type_id.id,
-            'warehouse_id': self.id,
-            'company_id': self.company_id.id,
+            "name": self._format_rulename(
+                self.rental_out_location_id, customer_loc, ""
+            ),
+            "location_src_id": self.rental_out_location_id.id,
+            "location_id": customer_loc.id,
+            "route_id": sell_rented_product_route.id,
+            "action": "pull",
+            "picking_type_id": self.out_type_id.id,
+            "warehouse_id": self.id,
+            "company_id": self.company_id.id,
         }
         res = [
             rental_pull_rule,
             rental_push_rule,
             sell_rented_product_pull_rule,
-            ]
+        ]
         return res
 
     def _create_rental_locations(self):
-        slo = self.env['stock.location']
+        slo = self.env["stock.location"]
         for wh in self:
             # create stock locations
             if not wh.rental_view_location_id:
-                view_loc = slo.with_context(lang='en_US').search([
-                    ('name', 'ilike', 'Rental'),
-                    ('location_id', '=', wh.view_location_id.id),
-                    ('usage', '=', 'view'),
-                    ('company_id', '=', self.company_id.id),
-                    ], limit=1)
+                view_loc = slo.with_context(lang="en_US").search(
+                    [
+                        ("name", "ilike", "Rental"),
+                        ("location_id", "=", wh.view_location_id.id),
+                        ("usage", "=", "view"),
+                        ("company_id", "=", self.company_id.id),
+                    ],
+                    limit=1,
+                )
                 if not view_loc:
-                    view_loc = slo.with_context(lang='en_US').create({
-                        'name': 'Rental',
-                        'location_id': wh.view_location_id.id,
-                        'usage': 'view',
-                        'company_id': self.company_id.id,
-                    })
-                    slo.browse(view_loc.id).name = _('Rental')
+                    view_loc = slo.with_context(lang="en_US").create(
+                        {
+                            "name": "Rental",
+                            "location_id": wh.view_location_id.id,
+                            "usage": "view",
+                            "company_id": self.company_id.id,
+                        }
+                    )
+                    slo.browse(view_loc.id).name = _("Rental")
                     logger.debug(
-                        'New view rental stock location created ID %d',
-                        view_loc.id)
+                        "New view rental stock location created ID %d", view_loc.id
+                    )
                 wh.rental_view_location_id = view_loc.id
             if not wh.rental_in_location_id:
-                in_loc = slo.with_context(lang='en_US').search([
-                    ('name', 'ilike', 'Rental In'),
-                    ('location_id', '=', wh.rental_view_location_id.id),
-                    ('company_id', '=', self.company_id.id),
-                    ], limit=1)
+                in_loc = slo.with_context(lang="en_US").search(
+                    [
+                        ("name", "ilike", "Rental In"),
+                        ("location_id", "=", wh.rental_view_location_id.id),
+                        ("company_id", "=", self.company_id.id),
+                    ],
+                    limit=1,
+                )
                 if not in_loc:
-                    in_loc = slo.with_context(lang='en_US').create({
-                        'name': 'Rental In',
-                        'location_id': wh.rental_view_location_id.id,
-                        'company_id': self.company_id.id,
-                        })
-                    slo.browse(in_loc.id).name = _('Rental In')
+                    in_loc = slo.with_context(lang="en_US").create(
+                        {
+                            "name": "Rental In",
+                            "location_id": wh.rental_view_location_id.id,
+                            "company_id": self.company_id.id,
+                        }
+                    )
+                    slo.browse(in_loc.id).name = _("Rental In")
                     logger.debug(
-                        'New in rental stock location created ID %d',
-                        in_loc.id)
+                        "New in rental stock location created ID %d", in_loc.id
+                    )
                 wh.rental_in_location_id = in_loc.id
             if not wh.rental_out_location_id:
-                out_loc = slo.with_context(lang='en_US').search([
-                    ('name', 'ilike', 'Rental Out'),
-                    ('location_id', '=', wh.rental_view_location_id.id),
-                    ('company_id', '=', self.company_id.id),
-                    ], limit=1)
+                out_loc = slo.with_context(lang="en_US").search(
+                    [
+                        ("name", "ilike", "Rental Out"),
+                        ("location_id", "=", wh.rental_view_location_id.id),
+                        ("company_id", "=", self.company_id.id),
+                    ],
+                    limit=1,
+                )
                 if not out_loc:
-                    out_loc = slo.with_context(lang='en_US').create({
-                        'name': 'Rental Out',
-                        'location_id': wh.rental_view_location_id.id,
-                        'company_id': self.company_id.id,
-                        })
-                    slo.browse(out_loc.id).name = _('Rental Out')
+                    out_loc = slo.with_context(lang="en_US").create(
+                        {
+                            "name": "Rental Out",
+                            "location_id": wh.rental_view_location_id.id,
+                            "company_id": self.company_id.id,
+                        }
+                    )
+                    slo.browse(out_loc.id).name = _("Rental Out")
                     logger.debug(
-                        'New out rental stock location created ID %d',
-                        out_loc.id)
+                        "New out rental stock location created ID %d", out_loc.id
+                    )
                 wh.rental_out_location_id = out_loc.id
 
     def write(self, vals):
-        if 'rental_allowed' in vals:
-            rental_route = self.env.ref('sale_rental.route_warehouse0_rental')
+        if "rental_allowed" in vals:
+            rental_route = self.env.ref("sale_rental.route_warehouse0_rental")
             sell_rented_route = self.env.ref(
-                'sale_rental.route_warehouse0_sell_rented_product')
-            if vals.get('rental_allowed'):
+                "sale_rental.route_warehouse0_sell_rented_product"
+            )
+            if vals.get("rental_allowed"):
                 self._create_rental_locations()
-                self.write({
-                    'route_ids': [(4, rental_route.id)],
-                    'rental_route_id': rental_route.id,
-                    'sell_rented_product_route_id': sell_rented_route.id,
-                    })
-                rental_rules = self.env['stock.rule'].search(
-                    [('route_id', 'in',
-                      [rental_route.id, sell_rented_route.id]),
-                     ('active', '=', False)
-                     ])
+                self.write(
+                    {
+                        "route_ids": [(4, rental_route.id)],
+                        "rental_route_id": rental_route.id,
+                        "sell_rented_product_route_id": sell_rented_route.id,
+                    }
+                )
+                rental_rules = self.env["stock.rule"].search(
+                    [
+                        ("route_id", "in", [rental_route.id, sell_rented_route.id]),
+                        ("active", "=", False),
+                    ]
+                )
                 if rental_rules:
-                    rental_rules.write({'active': True})
+                    rental_rules.write({"active": True})
                 else:
                     for rule_vals in self._get_rental_push_pull_rules():
-                        self.env['stock.rule'].create(rule_vals)
+                        self.env["stock.rule"].create(rule_vals)
             else:
                 for wh in self:
-                    rules_to_archive = self.env['stock.rule'].search(
+                    rules_to_archive = self.env["stock.rule"].search(
                         [
-                            ('route_id', 'in', (
-                                wh.rental_route_id.id,
-                                wh.sell_rented_product_route_id.id)
-                             ),
-                            ('company_id', '=', wh.company_id.id),
+                            (
+                                "route_id",
+                                "in",
+                                (
+                                    wh.rental_route_id.id,
+                                    wh.sell_rented_product_route_id.id,
+                                ),
+                            ),
+                            ("company_id", "=", wh.company_id.id),
                         ]
                     )
-                    rules_to_archive.write({'active': False})
-                    wh.write({
-                        'route_ids': [(3, rental_route.id)],
-                        'rental_route_id': False,
-                        'sell_rented_product_route_id': False,
-                    })
+                    rules_to_archive.write({"active": False})
+                    wh.write(
+                        {
+                            "route_ids": [(3, rental_route.id)],
+                            "rental_route_id": False,
+                            "sell_rented_product_route_id": False,
+                        }
+                    )
         return super().write(vals)
--- sale_rental/wizard/create_rental_product.py
+++ sale_rental/wizard/create_rental_product.py
@@ -3,81 +3,89 @@
 # Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class CreateRentalProduct(models.TransientModel):
-    _name = 'create.rental.product'
-    _description = 'Create the Rental Service Product'
+    _name = "create.rental.product"
+    _description = "Create the Rental Service Product"
 
     @api.model
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
-        if self.env.context.get('active_model') == 'product.product':
-            hw_product = self.env['product.product'].browse(
-                self.env.context['active_id'])
-        elif self.env.context.get('active_model') == 'product.template':
-            hw_product_tmpl = self.env['product.template'].browse(
-                self.env.context['active_id'])
+        if self.env.context.get("active_model") == "product.product":
+            hw_product = self.env["product.product"].browse(
+                self.env.context["active_id"]
+            )
+        elif self.env.context.get("active_model") == "product.template":
+            hw_product_tmpl = self.env["product.template"].browse(
+                self.env.context["active_id"]
+            )
             assert len(hw_product_tmpl.product_variant_ids) == 1
             hw_product = hw_product_tmpl.product_variant_ids
         else:
             raise UserError(_("Wrong active_model. Should never happen."))
         hw_product = hw_product.with_context(display_default_code=False)
-        res.update({
-            'hw_product_id': hw_product.id,
-            'name': _('Rental of a %s') % hw_product.display_name,
-            })
+        res.update(
+            {
+                "hw_product_id": hw_product.id,
+                "name": _("Rental of a %s") % hw_product.display_name,
+            }
+        )
         if hw_product.default_code:
-            res['default_code'] = _('RENT-%s') % hw_product.default_code
+            res["default_code"] = _("RENT-%s") % hw_product.default_code
         return res
 
     hw_product_id = fields.Many2one(
-        'product.product', string='Product to Rent',
-        readonly=True, required=True)
-    name = fields.Char(string='Rental Service Name', required=True)
-    default_code = fields.Char(string='Default Code')
+        "product.product", string="Product to Rent", readonly=True, required=True
+    )
+    name = fields.Char(string="Rental Service Name", required=True)
+    default_code = fields.Char(string="Default Code")
     sale_price_per_day = fields.Float(
-        string='Rental Price per Day', required=True,
-        digits='Product Price', default=1.0)
+        string="Rental Price per Day",
+        required=True,
+        digits="Product Price",
+        default=1.0,
+    )
     categ_id = fields.Many2one(
-        'product.category', string='Product Category', required=True)
-    copy_image = fields.Boolean(string='Copy Product Image')
+        "product.category", string="Product Category", required=True
+    )
+    copy_image = fields.Boolean(string="Copy Product Image")
 
     @api.model
     def _prepare_rental_product(self):
-        day_uom_id = self.env.ref('uom.product_uom_day').id
+        day_uom_id = self.env.ref("uom.product_uom_day").id
         vals = {
-            'type': 'service',
-            'sale_ok': True,
-            'purchase_ok': False,
-            'uom_id': day_uom_id,
-            'uom_po_id': day_uom_id,
-            'list_price': self.sale_price_per_day,
-            'name': self.name,
-            'default_code': self.default_code,
-            'rented_product_id': self.hw_product_id.id,
-            'must_have_dates': True,
-            'categ_id': self.categ_id.id,
-            'invoice_policy': 'order',
+            "type": "service",
+            "sale_ok": True,
+            "purchase_ok": False,
+            "uom_id": day_uom_id,
+            "uom_po_id": day_uom_id,
+            "list_price": self.sale_price_per_day,
+            "name": self.name,
+            "default_code": self.default_code,
+            "rented_product_id": self.hw_product_id.id,
+            "must_have_dates": True,
+            "categ_id": self.categ_id.id,
+            "invoice_policy": "order",
         }
         if self.copy_image:
-            vals['image_1920'] = self.hw_product_id.image_1920
+            vals["image_1920"] = self.hw_product_id.image_1920
         return vals
 
     def create_rental_product(self):
         self.ensure_one()
-        pp_obj = self.env['product.product']
+        pp_obj = self.env["product.product"]
         #  check that a rental product doesn't already exists ?
         product = pp_obj.create(self._prepare_rental_product())
         action = {
-            'name': pp_obj._description,
-            'type': 'ir.actions.act_window',
-            'res_model': pp_obj._name,
-            'view_mode': 'form,tree,kanban',
-            'nodestroy': False,  # Close the wizard pop-up
-            'target': 'current',
-            'res_id': product.id,
+            "name": pp_obj._description,
+            "type": "ir.actions.act_window",
+            "res_model": pp_obj._name,
+            "view_mode": "form,tree,kanban",
+            "nodestroy": False,  # Close the wizard pop-up
+            "target": "current",
+            "res_id": product.id,
         }
         return action
