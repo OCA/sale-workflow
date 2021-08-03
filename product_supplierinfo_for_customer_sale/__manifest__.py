29df1b7b3ee817b6e518507341fd3130b6521423
Lois Rilo <lois.rilo@forgeflow.com>
2021-08-03 17:12:18 +0200

    [14.0][MIG] product_supplierinfo_for_customer_sale

--- product_supplierinfo_for_customer_sale/__manifest__.py
+++ product_supplierinfo_for_customer_sale/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo for Customer Sale",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Loads in every sale order line the customer code defined "
     "in the product",
     "author": "Agile Business Group,Vauxoo,Odoo Community Association (OCA)",
