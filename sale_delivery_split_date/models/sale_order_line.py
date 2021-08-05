d4a6b4ead0c34ba48ac83bb5c35d222dc21824b9
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-08-05 15:29:49 +0200

    [IMP] sale_delivery_split_date: black, isort, prettier

--- sale_delivery_split_date/models/sale_order_line.py
+++ sale_delivery_split_date/models/sale_order_line.py
@@ -6,11 +6,11 @@ from odoo import api, models
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.multi
     def _get_procurement_group_key(self):
-        """ Return a key with priority to be used to regroup lines in multiple
+        """Return a key with priority to be used to regroup lines in multiple
         procurement groups
         """
         priority = 24
