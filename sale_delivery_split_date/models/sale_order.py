5f14e6b6a7821f4bd02a0f568140372d348478ef
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-08-05 15:29:49 +0200

    [IMP] sale_delivery_split_date: black, isort, prettier

--- sale_delivery_split_date/models/sale_order.py
+++ sale_delivery_split_date/models/sale_order.py
@@ -7,12 +7,12 @@ from odoo.tools import format_date
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.model
     def _prepare_procurement_group_by_line(self, line):
         vals = super(SaleOrder, self)._prepare_procurement_group_by_line(line)
         if line._get_procurement_group_key()[0] == 24:
             if line.commitment_date:
-                vals['name'] += '/' + format_date(line.env, line.commitment_date.date())
+                vals["name"] += "/" + format_date(line.env, line.commitment_date.date())
         return vals
