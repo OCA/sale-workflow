abadd06c265e82027e14c9ca2031fd9b6496504e
Víctor Martínez <victor.martinez@tecnativa.com>
2021-08-09 16:54:20 +0200

    [IMP] sale_exception: Add Customer sale warning + product warning exceptions

--- sale_exception/tests/test_sale_exception.py
+++ sale_exception/tests/test_sale_exception.py
@@ -1,10 +1,11 @@
 # Copyright 2011 Akretion, Sodexis
 # Copyright 2018 Akretion
 # Copyright 2019 Camptocamp SA
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo.exceptions import ValidationError
-from odoo.tests import SavepointCase
+from odoo.tests import Form, SavepointCase
 
 
 class TestSaleException(SavepointCase):
@@ -45,8 +46,12 @@ class TestSaleException(SavepointCase):
         )
 
         # confirm quotation
+        exception = self.env.ref("sale_exception.excep_no_zip")
+        exception.active = True
         so1.action_confirm()
         self.assertTrue(so1.state == "draft")
+        so1.detect_exceptions()
+        self.assertTrue(so1.exception_ids.filtered(lambda x: x == exception))
         # test all draft so
         so2 = self.env["sale.order"].create(
             {
@@ -134,3 +139,36 @@ class TestSaleException(SavepointCase):
         ).create({"ignore": True})
         so_except_confirm.action_confirm()
         self.assertTrue(so1.ignore_exception)
+
+    def _create_sale_order(self, partner, product):
+        order_form = Form(self.env["sale.order"])
+        order_form.partner_id = partner
+        with order_form.order_line.new() as line_form:
+            line_form.product_id = product
+        return order_form.save()
+
+    def test_exception_partner_sale_warning(self):
+        exception = self.env.ref("sale_exception.exception_partner_sale_warning")
+        exception.active = True
+        partner = self.env.ref("base.res_partner_1")
+        sale_order = self._create_sale_order(
+            partner=partner, product=self.env.ref("product.product_product_6")
+        )
+        sale_order.action_confirm()
+        partner.sale_warn = "warning"
+        sale_order2 = sale_order.copy()
+        sale_order2.detect_exceptions()
+        self.assertTrue(sale_order2.exception_ids.filtered(lambda x: x == exception))
+
+    def test_exception_product_sale_warning(self):
+        exception = self.env.ref("sale_exception.exception_product_sale_warning")
+        exception.active = True
+        product = self.env.ref("product.product_product_6")
+        sale_order = self._create_sale_order(
+            partner=self.env.ref("base.res_partner_1"), product=product
+        )
+        sale_order.action_confirm()
+        product.sale_line_warn = "warning"
+        sale_order2 = sale_order.copy()
+        sale_order2.detect_exceptions()
+        self.assertTrue(sale_order2.exception_ids.filtered(lambda x: x == exception))
