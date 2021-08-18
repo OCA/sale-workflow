eb793e4bd908aaf72aa3cf1cd811769fe4dcea80
Víctor Martínez <victor.martinez@tecnativa.com>
2021-08-18 11:22:19 +0200

    [IMP] sale_stock_picking_note: Add Picking comments in sale orders to use in pickings + Add comments field to Delivery slip report
    [UPD] Update sale_stock_picking_note.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: sale-workflow-13.0/sale-workflow-13.0-sale_stock_picking_note
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-13-0/sale-workflow-13-0-sale_stock_picking_note/

--- sale_stock_picking_note/__manifest__.py
+++ sale_stock_picking_note/__manifest__.py
@@ -1,14 +1,19 @@
-# Copyright 2018 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Stock Picking Note",
     "summary": "Add picking note in sale and purchase order",
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["sale_stock"],
-    "data": ["views/sale_order_view.xml"],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/stock_picking_view.xml",
+        "views/report_deliveryslip.xml",
+    ],
     "installable": True,
 }
