81ddaa22a9c41c38a73d41768673292e21af1d17
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-09-03 21:45:42 +0200

    [OU-FIX] sale_automatic_workflow: Missing field transfer
    We need to pass automatic workflow reference from account.invoice to account.move.

--- sale_automatic_workflow/migrations/13.0.1.2.0/post-migration.py
+++ sale_automatic_workflow/migrations/13.0.1.2.0/post-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Camptocamp SA
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from openupgradelib import openupgrade  # pylint: disable=W7936
 
@@ -7,4 +8,12 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 def migrate(env, version):
     openupgrade.load_data(
         env.cr, "sale_automatic_workflow", "migrations/13.0.1.0.0/noupdate_changes.xml"
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move am
+        SET workflow_process_id = ai.workflow_process_id
+        FROM account_invoice ai
+        WHERE ai.id = am.old_invoice_id""",
     )
