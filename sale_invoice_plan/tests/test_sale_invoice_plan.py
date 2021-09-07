818d2db26756b4f55a530aee7648b6007f7f11de
sonhd <sonhd@trobz.com>
2021-09-07 10:49:11 +0700

    [MIG] sale_invoice_plan: Migration to 14.0

--- sale_invoice_plan/tests/test_sale_invoice_plan.py
+++ sale_invoice_plan/tests/test_sale_invoice_plan.py
@@ -1,12 +1,210 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+from collections import OrderedDict
+
 from odoo.exceptions import UserError, ValidationError
 from odoo.tests import Form
 
-from odoo.addons.sale.tests import test_sale_common
+from odoo.addons.sale.tests import common
 
 
-class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
+class TestSaleInvoicePlan(common.TestSaleCommon):
+    @classmethod
+    def setUpClass(cls):
+        super(TestSaleInvoicePlan, cls).setUpClass()
+        context_no_mail = {
+            "no_reset_password": True,
+            "mail_create_nosubscribe": True,
+            "mail_create_nolog": True,
+        }
+
+        # Create base account to simulate a chart of account
+        user_type_payable = cls.env.ref("account.data_account_type_payable")
+        cls.account_payable = cls.env["account.account"].create(
+            {
+                "code": "NC1110",
+                "name": "Test Payable Account",
+                "user_type_id": user_type_payable.id,
+                "reconcile": True,
+            }
+        )
+        user_type_receivable = cls.env.ref("account.data_account_type_receivable")
+        cls.account_receivable = cls.env["account.account"].create(
+            {
+                "code": "NC1111",
+                "name": "Test Receivable Account",
+                "user_type_id": user_type_receivable.id,
+                "reconcile": True,
+            }
+        )
+
+        Partner = cls.env["res.partner"].with_context(context_no_mail)
+        cls.partner_customer_usd = Partner.create(
+            {
+                "name": "Customer from the North",
+                "email": "customer.usd@north.com",
+                "property_account_payable_id": cls.account_payable.id,
+                "property_account_receivable_id": cls.account_receivable.id,
+            }
+        )
+
+        cls.sale_journal0 = cls.env["account.journal"].create(
+            {
+                "name": "Sale Journal",
+                "type": "sale",
+                "code": "SJT0",
+            }
+        )
+
+        cls.setUpClassicProducts()
+
+        sale_obj = cls.env["sale.order"]
+        # Create an SO for Service
+        cls.so_service = sale_obj.with_user(
+            cls.company_data["default_user_salesman"]
+        ).create(
+            {
+                "partner_id": cls.partner_customer_usd.id,
+                "partner_invoice_id": cls.partner_customer_usd.id,
+                "partner_shipping_id": cls.partner_customer_usd.id,
+                "use_invoice_plan": True,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product_order.name,
+                            "product_id": cls.product_order.id,
+                            "product_uom_qty": 1,
+                            "product_uom": cls.product_order.uom_id.id,
+                            "price_unit": cls.product_order.list_price,
+                        },
+                    )
+                ],
+                "pricelist_id": cls.env.ref("product.list0").id,
+            }
+        )
+        # Create an SO for product delivery
+        cls.so_product = sale_obj.with_user(
+            cls.company_data["default_user_salesman"]
+        ).create(
+            {
+                "partner_id": cls.partner_customer_usd.id,
+                "partner_invoice_id": cls.partner_customer_usd.id,
+                "partner_shipping_id": cls.partner_customer_usd.id,
+                "use_invoice_plan": True,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product_deliver.name,
+                            "product_id": cls.product_deliver.id,
+                            "product_uom_qty": 10,
+                            "product_uom": cls.product_deliver.uom_id.id,
+                            "price_unit": cls.product_deliver.list_price,
+                        },
+                    )
+                ],
+                "pricelist_id": cls.env.ref("product.list0").id,
+            }
+        )
+
+    @classmethod
+    def setUpClassicProducts(cls):
+        # Create an expense journal
+        user_type_income = cls.env.ref("account.data_account_type_direct_costs")
+        cls.account_income_product = cls.env["account.account"].create(
+            {
+                "code": "INCOME_PROD111",
+                "name": "Icome - Test Account",
+                "user_type_id": user_type_income.id,
+            }
+        )
+        # Create category
+        cls.product_category = cls.env["product.category"].create(
+            {
+                "name": "Product Category with Income account",
+                "property_account_income_categ_id": cls.account_income_product.id,
+            }
+        )
+        # Products
+        uom_unit = cls.env.ref("uom.product_uom_unit")
+        uom_hour = cls.env.ref("uom.product_uom_hour")
+        cls.product_order = cls.env["product.product"].create(
+            {
+                "name": "Zed+ Antivirus",
+                "standard_price": 235.0,
+                "list_price": 280.0,
+                "type": "consu",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "order",
+                "expense_policy": "no",
+                "default_code": "PROD_ORDER",
+                "service_type": "manual",
+                "taxes_id": False,
+                "categ_id": cls.product_category.id,
+            }
+        )
+        cls.service_deliver = cls.env["product.product"].create(
+            {
+                "name": "Cost-plus Contract",
+                "standard_price": 200.0,
+                "list_price": 180.0,
+                "type": "service",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "delivery",
+                "expense_policy": "no",
+                "default_code": "SERV_DEL",
+                "service_type": "manual",
+                "taxes_id": False,
+                "categ_id": cls.product_category.id,
+            }
+        )
+        cls.service_order = cls.env["product.product"].create(
+            {
+                "name": "Prepaid Consulting",
+                "standard_price": 40.0,
+                "list_price": 90.0,
+                "type": "service",
+                "uom_id": uom_hour.id,
+                "uom_po_id": uom_hour.id,
+                "invoice_policy": "order",
+                "expense_policy": "no",
+                "default_code": "PRE-PAID",
+                "service_type": "manual",
+                "taxes_id": False,
+                "categ_id": cls.product_category.id,
+            }
+        )
+        cls.product_deliver = cls.env["product.product"].create(
+            {
+                "name": "Switch, 24 ports",
+                "standard_price": 55.0,
+                "list_price": 70.0,
+                "type": "consu",
+                "uom_id": uom_unit.id,
+                "uom_po_id": uom_unit.id,
+                "invoice_policy": "delivery",
+                "expense_policy": "no",
+                "default_code": "PROD_DEL",
+                "service_type": "manual",
+                "taxes_id": False,
+                "categ_id": cls.product_category.id,
+            }
+        )
+
+        cls.product_map = OrderedDict(
+            [
+                ("prod_order", cls.product_order),
+                ("serv_del", cls.service_deliver),
+                ("serv_order", cls.service_order),
+                ("prod_del", cls.product_deliver),
+            ]
+        )
+
     def test_00_invoice_plan(self):
         # To create next invoice from SO
         ctx = {
@@ -32,7 +230,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
                     self.so_service.action_confirm()
             # Create Invocie Plan Installment
             plan.with_context(ctx).sale_create_invoice_plan()
-            self.assertEquals(
+            self.assertEqual(
                 len(self.so_service.invoice_plan_ids),
                 num_installment,
                 "Wrong number of installment created",
@@ -43,7 +241,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         make_wizard = self.env["sale.make.planned.invoice"].create({})
         make_wizard.with_context(ctx).create_invoices_by_plan()
         invoices = self.so_service.invoice_ids
-        self.assertEquals(len(invoices), 1, "Only 1 invoice should be created")
+        self.assertEqual(len(invoices), 1, "Only 1 invoice should be created")
 
     def test_01_invoice_plan(self):
         # To create all remaining invoice from SO
@@ -69,12 +267,12 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         make_wizard.with_context(ctx).create_invoices_by_plan()
         # Valid number of invoices
         invoices = self.so_service.invoice_ids
-        self.assertEquals(
+        self.assertEqual(
             len(invoices), num_installment, "Wrong number of invoice created"
         )
         # Valid total quantity of invoices
         quantity = sum(invoices.mapped("invoice_line_ids").mapped("quantity"))
-        self.assertEquals(quantity, 1, "Wrong number of total invoice quantity")
+        self.assertEqual(quantity, 1, "Wrong number of total invoice quantity")
 
     def test_02_invoice_plan_with_advance(self):
         # To create all remaining invoice from SO
@@ -90,7 +288,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         f.advance = True  # Advance
         plan = f.save()
         plan.with_context(ctx).sale_create_invoice_plan()
-        self.assertEquals(
+        self.assertEqual(
             len(self.so_service.invoice_plan_ids),
             num_installment + 1,
             "Wrong number of installment created",
@@ -101,7 +299,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         advance_line = self.so_service.invoice_plan_ids.filtered(
             lambda l: l.invoice_type == "advance"
         )
-        self.assertEquals(len(advance_line), 1, "No one advance line")
+        self.assertEqual(len(advance_line), 1, "No one advance line")
         # Add 10% to advance
         advance_line.percent = 10
         # Can confirm the SO after advance is filled
@@ -111,7 +309,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         wizard.with_context(ctx).create_invoices_by_plan()
         # Valid number of invoices, including advance
         invoices = self.so_service.invoice_ids
-        self.assertEquals(
+        self.assertEqual(
             len(invoices), num_installment + 1, "Wrong number of invoice created"
         )
         # Valid total quantity of invoices (exclude Advance line)
@@ -120,7 +318,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
             .filtered(lambda l: l.product_id == self.product_order)
             .mapped("quantity")
         )
-        self.assertEquals(quantity, 1, "Wrong number of total invoice quantity")
+        self.assertEqual(quantity, 1, "Wrong number of total invoice quantity")
 
     def test_03_unlink_invoice_plan(self):
         ctx = {"active_id": self.so_service.id, "active_ids": [self.so_service.id]}
@@ -152,7 +350,7 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         self.assertEqual(len(self.so_product.picking_ids), 1)
         pick = self.so_product.picking_ids[0]
         pick.move_ids_without_package.write({"quantity_done": 3.0})
-        pick.action_done()
+        pick._action_done()
         # Create invoice by plan
         wizard = self.env["sale.make.planned.invoice"].create({})
         with self.assertRaises(ValidationError) as e:
@@ -163,81 +361,10 @@ class TestSaleInvoicePlan(test_sale_common.TestCommonSaleNoChart):
         # Deliver all the rest and create invoice plan again
         pick = self.so_product.picking_ids.filtered(lambda l: l.state != "done")
         pick.mapped("move_ids_without_package").write({"quantity_done": 7.0})
-        pick.action_done()
+        pick._action_done()
         wizard = self.env["sale.make.planned.invoice"].create({})
         wizard.with_context(ctx).create_invoices_by_plan()
         # Valid total quantity of invoice = 10 units
         invoices = self.so_product.invoice_ids
         quantity = sum(invoices.mapped("invoice_line_ids").mapped("quantity"))
-        self.assertEquals(quantity, 10, "Wrong number of total invoice quantity")
-
-    @classmethod
-    def setUpClass(cls):
-        super(TestSaleInvoicePlan, cls).setUpClass()
-        cls.setUpClassicProducts()
-        cls.setUpUsers()
-        group_salemanager = cls.env.ref("sales_team.group_sale_manager")
-        group_salesman = cls.env.ref("sales_team.group_sale_salesman")
-        group_employee = cls.env.ref("base.group_user")
-        group_stock = cls.env.ref("stock.group_stock_user")
-        cls.user_manager.write(
-            {
-                "groups_id": [
-                    (6, 0, [group_salemanager.id, group_employee.id, group_stock.id])
-                ]
-            }
-        )
-        cls.user_employee.write(
-            {
-                "groups_id": [
-                    (6, 0, [group_salesman.id, group_employee.id, group_stock.id])
-                ]
-            }
-        )
-        sale_obj = cls.env["sale.order"]
-        # Create an SO for Service
-        cls.so_service = sale_obj.with_user(cls.user_employee).create(
-            {
-                "partner_id": cls.partner_customer_usd.id,
-                "partner_invoice_id": cls.partner_customer_usd.id,
-                "partner_shipping_id": cls.partner_customer_usd.id,
-                "use_invoice_plan": True,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product_order.name,
-                            "product_id": cls.product_order.id,
-                            "product_uom_qty": 1,
-                            "product_uom": cls.product_order.uom_id.id,
-                            "price_unit": cls.product_order.list_price,
-                        },
-                    )
-                ],
-                "pricelist_id": cls.env.ref("product.list0").id,
-            }
-        )
-        # Create an SO for product delivery
-        cls.so_product = sale_obj.with_user(cls.user_employee).create(
-            {
-                "partner_id": cls.partner_customer_usd.id,
-                "partner_invoice_id": cls.partner_customer_usd.id,
-                "partner_shipping_id": cls.partner_customer_usd.id,
-                "use_invoice_plan": True,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product_deliver.name,
-                            "product_id": cls.product_deliver.id,
-                            "product_uom_qty": 10,
-                            "product_uom": cls.product_deliver.uom_id.id,
-                            "price_unit": cls.product_deliver.list_price,
-                        },
-                    )
-                ],
-                "pricelist_id": cls.env.ref("product.list0").id,
-            }
-        )
+        self.assertEqual(quantity, 10, "Wrong number of total invoice quantity")
