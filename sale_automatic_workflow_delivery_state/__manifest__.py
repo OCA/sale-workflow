34dceebe9a9dedd92521e6a788f23774ddda7a55
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-09-16 09:33:06 +0200

    [FIX] sale_automatic_workflow_delivery_state version
    work around PyPI issue

--- sale_automatic_workflow_delivery_state/__manifest__.py
+++ sale_automatic_workflow_delivery_state/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sale Automatic Workflow Delivery State",
     "summary": "Glue module for sale_automatic_workflow and sale_delivery_state",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Akretion, Odoo Community Association (OCA)",
