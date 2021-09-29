38a045e2e1a4972f6e620075707e84a9f19587cf
Víctor Martínez <victor.martinez@tecnativa.com>
2021-09-29 14:56:07 +0200

    [IMP] partner_contact_sale_info_propagation: black, isort, prettier

--- partner_contact_sale_info_propagation/__manifest__.py
+++ partner_contact_sale_info_propagation/__manifest__.py
@@ -2,8 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner contact sale info propagation",
-    "summary": "Propagate Salesperson and Sales Channel "
-               "from Company to Contacts",
+    "summary": "Propagate Salesperson and Sales Channel " "from Company to Contacts",
     "version": "12.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
@@ -11,10 +10,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "sales_team",
-    ],
-    "data": [
-        "views/res_partner_view.xml",
-    ],
+    "depends": ["sales_team",],
+    "data": ["views/res_partner_view.xml",],
 }
--- partner_contact_sale_info_propagation/readme/DESCRIPTION.rst
+++ partner_contact_sale_info_propagation/readme/DESCRIPTION.rst
@@ -9,4 +9,3 @@ This module propagates Salesperson and Sales Channel from Company to Contacts
 * When the company changes the *Sales Channel*, it fills with the same
   *Sales Channel* all the contacts that don't have any or have the previous
   *Sales Channel* of the parent company.
-
