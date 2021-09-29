6a2ac1ae326c4c29627c98ca3d1c71a8782dac86
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-09-29 19:45:50 +0200

    [OU-FIX] sale_automatic_workflow: Adjust migration version
    So intermediate versions doesn't break

--- sale_automatic_workflow/migrations/13.0.1.0.0/post-migration.py
+++ sale_automatic_workflow/migrations/13.0.1.0.0/post-migration.py
@@ -7,7 +7,7 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "sale_automatic_workflow", "migrations/13.0.1.2.0/noupdate_changes.xml"
+        env.cr, "sale_automatic_workflow", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
     openupgrade.logged_query(
         env.cr,
