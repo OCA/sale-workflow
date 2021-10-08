12ad30751e12144bd16d3efd6f4edff642937078
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-10-08 06:29:04 +0200

    [IMP] sale_pwa_cache: Sync

--- sale_pwa_cache/static/src/js/worker/managers/sync.js
+++ sale_pwa_cache/static/src/js/worker/managers/sync.js
@@ -12,6 +12,49 @@ odoo.define("sale_pwa_cache.PWA.managers.Sync", function(require) {
      */
     SWSyncManager.include({
         /**
+         *
+         * @param {Object} record
+         * @param {Array} records_done
+         * @returns {Promise}
+         */
+        _canSaleOrderBeConfirmed: function(record, records_done) {
+            return new Promise(async (resolve, reject) => {
+                const rid = record.args[0].id;
+                try {
+                    for (const sync_record of records_done) {
+                        if (sync_record.model !== "sale.order.line") {
+                            continue;
+                        }
+                        let aid = false;
+                        if (sync_record.method === "create") {
+                            aid = sync_record.args[0].id;
+                        } else if (sync_record.method === "write") {
+                            aid = sync_record.args[0][0];
+                        } else if (sync_record.method === "unlink") {
+                            aid = sync_record.args[0][0][0];
+                        }
+                        if (aid) {
+                            const rec_line = await this._db.browse(
+                                sync_record.model,
+                                aid
+                            );
+                            if (
+                                !_.isEmpty(rec_line) &&
+                                rec_line.order_id === rid &&
+                                sync_record.failed
+                            ) {
+                                return reject();
+                            }
+                        }
+                    }
+                } catch (err) {
+                    return reject(err);
+                }
+                return resolve();
+            });
+        },
+
+        /**
          * @override
          */
         onSynchronizedRecords: function(records_done) {
@@ -31,6 +74,17 @@ odoo.define("sale_pwa_cache.PWA.managers.Sync", function(require) {
                         }
                         const rid = record.args[0].id;
                         try {
+                            await this._canSaleOrderBeConfirmed(record, records_done);
+                        } catch (err) {
+                            this._sendRecordFailToPages(
+                                record.id,
+                                `Can't confirm this sale order due to errors in the lines to synchronize. The record ID is ${rid}`
+                            );
+                            error_sale_ids.push(rid);
+                            continue;
+                        }
+                        let has_errors = false;
+                        try {
                             const [response] = await rpc.callJSonRpc(
                                 "sale.order",
                                 "action_confirm",
@@ -38,10 +92,18 @@ odoo.define("sale_pwa_cache.PWA.managers.Sync", function(require) {
                             );
                             const result = (await response.json()).result;
                             if (result !== true) {
+                                has_errors = true;
                                 error_sale_ids.push(rid);
                             }
                         } catch (err) {
+                            has_errors = true;
                             error_sale_ids.push(rid);
+                        }
+                        if (has_errors) {
+                            this._sendRecordFailToPages(
+                                record.id,
+                                `Can't confirm this sale order! The record ID is ${rid}`
+                            );
                         }
                     }
                     if (!_.isEmpty(error_sale_ids)) {
