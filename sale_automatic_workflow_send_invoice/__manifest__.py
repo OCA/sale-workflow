b39fb110f741f2412cb7ab4c55dddc5799a16ef4
Manuel Calero <manuelcalerosolis@gmail.com>
2021-10-08 20:28:36 +0200

    [ADD] sale_automatic_workflow_send_invoice: to 13.0

--- sale_automatic_workflow_send_invoice/__manifest__.py
+++ sale_automatic_workflow_send_invoice/__manifest__.py
@@ -6,10 +6,11 @@
     "version": "13.0.1.2.2",
     "category": "Sales Management",
     "license": "AGPL-3",
-    "author": "Xtendoo, " "Odoo Community Association (OCA)",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": ["sale_automatic_workflow"],
     "data": [
+        "views/account_move.xml",
         "views/sale_workflow_process_view.xml",
         "data/automatic_workflow_data.xml",
     ],
