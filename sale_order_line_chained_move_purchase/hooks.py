0cc5afb0a38a8fcc3fd25970501922c30819ccb2
Denis Roussel <denis.roussel@acsone.eu>
2021-10-13 10:39:17 +0200

    [14.0][FIX] sale_order_line_chained_move_purchase: Update post_init script

--- sale_order_line_chained_move_purchase/hooks.py
+++ sale_order_line_chained_move_purchase/hooks.py
@@ -1,20 +1,21 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
 from odoo import SUPERUSER_ID, api
 
 
+@openupgrade.logging()
 def _fill_in_related_sale_line(env):
     query = """
         UPDATE purchase_order_line pol
-            SET related_sale_line_id =
-                (SELECT related_sale_line_id
-                    FROM stock_move WHERE created_purchase_line_id = pol.id)
-            WHERE related_sale_line_id IS NULL
-            AND EXISTS (
-                SELECT 1 FROM stock_move
-                WHERE created_purchase_line_id = pol.id);
+            SET related_sale_line_id = sm.related_sale_line_id
+            FROM stock_move sm
+        WHERE pol.id = sm.created_purchase_line_id
+        AND pol.related_sale_line_id IS NULL
+        AND sm.related_sale_line_id IS NOT NULL
     """
-    env.cr.execute(query)
+    openupgrade.logged_query(env.cr, query)
 
 
 def post_init_hook(cr, registry):
