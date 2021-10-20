fb00b03021704426f67de8392416bbebdce6bbc8
Maksym Yankin <yankinmk@gmail.com>
2021-10-20 18:50:58 +0300

    [14.0][ADD] sale_amount_payment_link
    Reduce Amount to be paid while Payment Link is generated on Sale Order, depending on done Transactions.

---
+++ sale_amount_payment_link/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Amount Payment Link",
+    "summary": "Reduce Amount to be paid "
+    "while Payment Link is generated "
+    "on Sale Order, depending on done Transactions.",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "depends": ["sale"],
+    "website": "https://github.com/OCA/sale-workflow",
+    "installable": True,
+}
