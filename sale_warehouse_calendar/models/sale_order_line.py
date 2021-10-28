ab6c7ba62bd2ed861879af11bbaa5d5ffad5c50f
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-10-28 12:17:29 +0200

    fixup! [13.0] Add sale_warehouse_calendar

--- sale_warehouse_calendar/models/sale_order_line.py
+++ sale_warehouse_calendar/models/sale_order_line.py
@@ -41,6 +41,11 @@ class SaleOrderLine(models.Model):
             )
             # add back the security lead
             expected_date += td_security_lead
+            # This should be the delivery date.
+            # It is up to the carrier to determine the hour with the customer
+            expected_date = expected_date.replace(
+                hour=0, minute=0, second=0, microsecond=0
+            )
         return expected_date
 
     def _prepare_procurement_values(self, group_id=False):
@@ -58,7 +63,10 @@ class SaleOrderLine(models.Model):
             # Remove the workload that has been added by odoo
             date_planned -= td_workload
             # Add the workload, with respect to the wh calendar
-            res["date_planned"] = calendar.plan_days(
+            date_planned = calendar.plan_days(
                 workload_days, date_planned, compute_leaves=True
+            )
+            res["date_planned"] = date_planned.replace(
+                hour=0, minute=0, second=0, microsecond=0
             )
         return res
