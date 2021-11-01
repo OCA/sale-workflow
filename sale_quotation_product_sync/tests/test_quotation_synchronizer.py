ec6b09b56a96de981d8021899ec4114754185fbe
Denis Leemann <denis.leemann@camptocamp.com>
2021-11-01 11:34:11 +0100

    [IMP] : black, isort, prettier

--- sale_quotation_product_sync/tests/test_quotation_synchronizer.py
+++ sale_quotation_product_sync/tests/test_quotation_synchronizer.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.tests import common
 
 
@@ -7,47 +6,65 @@ class TestQuotationSynchronizer(common.SavepointCase):
     def setUpClass(cls):
         super(TestQuotationSynchronizer, cls).setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.any_customer = cls.env['res.partner'].search([
-            ('customer', '=', True),
-        ], limit=1)
+        cls.any_customer = cls.env["res.partner"].search(
+            [
+                ("customer", "=", True),
+            ],
+            limit=1,
+        )
         cls.template_default = cls.env.ref(
-            'website_quote.website_quote_template_default')
-        cls.product_2 = cls.env.ref('product.product_product_2')
-        cls.product_3 = cls.env.ref('product.product_product_3')
-        cls.template_default.write({
-            'quote_line': [
-                (0, 0, {
-                    'product_id': cls.product_3.id,
-                    'name': 'Sample Text',
-                    'product_uom_qty': 2.,
-                    'price_unit': 450.,
-                    'product_uom_id': cls.product_3.uom_id.id,
-                    'tested_sample': 'Sample3',
-                }),
-            ],
-            'options': [
-                (0, 0, {
-                    'product_id': cls.product_2.id,
-                    'name': cls.product_2.name,
-                    'quantity': 1,
-                    'price_unit': 38.25,
-                    'uom_id': cls.product_2.uom_id.id,
-                }),
-            ],
-        })
+            "website_quote.website_quote_template_default"
+        )
+        cls.product_2 = cls.env.ref("product.product_product_2")
+        cls.product_3 = cls.env.ref("product.product_product_3")
+        cls.template_default.write(
+            {
+                "quote_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product_3.id,
+                            "name": "Sample Text",
+                            "product_uom_qty": 2.0,
+                            "price_unit": 450.0,
+                            "product_uom_id": cls.product_3.uom_id.id,
+                            "tested_sample": "Sample3",
+                        },
+                    ),
+                ],
+                "options": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product_2.id,
+                            "name": cls.product_2.name,
+                            "quantity": 1,
+                            "price_unit": 38.25,
+                            "uom_id": cls.product_2.uom_id.id,
+                        },
+                    ),
+                ],
+            }
+        )
 
     def test_price_propagation(self):
-        order = self.env['sale.order'].create({
-            'partner_id': self.any_customer.id,
-            'template_id': self.template_default.id,
-        })
+        order = self.env["sale.order"].create(
+            {
+                "partner_id": self.any_customer.id,
+                "template_id": self.template_default.id,
+            }
+        )
         order.onchange_template_id()
         order_line_p3 = order.order_line.filtered(
-            lambda ol: ol.product_id == self.product_3)
+            lambda ol: ol.product_id == self.product_3
+        )
 
         order_copy = order.copy()
         order_copy_line_p3 = order_copy.order_line.filtered(
-            lambda ol: ol.product_id == self.product_3)
+            lambda ol: ol.product_id == self.product_3
+        )
         order_copy.action_confirm()
 
         self.assertEqual(order_line_p3.price_unit, 450.0)
@@ -57,9 +74,9 @@ class TestQuotationSynchronizer(common.SavepointCase):
 
         self.product_3.list_price = 500
         self.product_2.list_price = 50
-        wizard = self.env['sale.quotation.synchronizer'].create({
-            'product_ids': [(4, self.product_2.id), (4, self.product_3.id)]
-        })
+        wizard = self.env["sale.quotation.synchronizer"].create(
+            {"product_ids": [(4, self.product_2.id), (4, self.product_3.id)]}
+        )
         wizard.execute()
         # The price change should propagate to quotation template line
         self.assertEqual(self.template_default.quote_line.price_unit, 500.0)
--- sale_quotation_product_sync/wizards/sale_quotation_synchronizer.py
+++ sale_quotation_product_sync/wizards/sale_quotation_synchronizer.py
@@ -5,44 +5,44 @@ from odoo import api, fields, models
 
 class SaleQuotationSynchronizer(models.TransientModel):
 
-    _name = 'sale.quotation.synchronizer'
-    _description = 'Synchronize quotations from product'
+    _name = "sale.quotation.synchronizer"
+    _description = "Synchronize quotations from product"
 
-    product_ids = fields.Many2many('product.product')
+    product_ids = fields.Many2many("product.product")
 
     sync_sale_order_line = fields.Boolean(
-        string='Sync draft order lines', default=True,
+        string="Sync draft order lines",
+        default=True,
     )
     sync_sale_quote_line = fields.Boolean(
-        string='Sync quotation template lines', default=True,
+        string="Sync quotation template lines",
+        default=True,
     )
     sync_sale_quote_option = fields.Boolean(
-        string='Sync quotation template options', default=True,
+        string="Sync quotation template options",
+        default=True,
     )
 
     sync_price_unit = fields.Boolean(
-        string='Sync unit price', default=True, readonly=True,
+        string="Sync unit price",
+        default=True,
+        readonly=True,
     )
 
     def _sync_sale_order_lines(self, product):
-        sale_order_lines = self.env['sale.order.line'].search(
-            [
-                ('product_id', '=', product.id),
-                ('order_id.state', '=', 'draft')
-            ]
+        sale_order_lines = self.env["sale.order.line"].search(
+            [("product_id", "=", product.id), ("order_id.state", "=", "draft")]
         )
         for order_line in sale_order_lines:
             new_price = product.lst_price
             if order_line.product_uom != product.uom_id:
-                new_price = product.uom_id._compute_quantity(
-                    1, order_line.product_uom
-                )
-            order_line.write({'price_unit': new_price})
+                new_price = product.uom_id._compute_quantity(1, order_line.product_uom)
+            order_line.write({"price_unit": new_price})
 
     def _sync_sale_quote_lines(self, product):
-        sale_quote_lines = self.env['sale.quote.line'].search(
+        sale_quote_lines = self.env["sale.quote.line"].search(
             [
-                ('product_id', '=', product.id),
+                ("product_id", "=", product.id),
             ]
         )
         for quote_line in sale_quote_lines:
@@ -51,21 +51,19 @@ class SaleQuotationSynchronizer(models.TransientModel):
                 new_price = product.uom_id._compute_quantity(
                     1, quote_line.product_uom_id
                 )
-            quote_line.write({'price_unit': new_price})
+            quote_line.write({"price_unit": new_price})
 
     def _sync_sale_quote_options(self, product):
-        sale_quote_options = self.env['sale.quote.option'].search(
+        sale_quote_options = self.env["sale.quote.option"].search(
             [
-                ('product_id', '=', product.id),
+                ("product_id", "=", product.id),
             ]
         )
         for quote_option in sale_quote_options:
             new_price = product.lst_price
             if quote_option.uom_id != product.uom_id:
-                new_price = product.uom_id._compute_quantity(
-                    1, quote_option.uom_id
-                )
-            quote_option.write({'price_unit': new_price})
+                new_price = product.uom_id._compute_quantity(1, quote_option.uom_id)
+            quote_option.write({"price_unit": new_price})
 
     @api.multi
     def execute(self):
