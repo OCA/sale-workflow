7acf5515a34ebf622fa198d18d48f52218efd15a
moaadbourhim <moaad.bourhim@gmail.com>
2021-11-16 22:28:12 +0100

    [IMP] sale_order_line_date: black, isort, prettier

--- sale_order_line_date/__manifest__.py
+++ sale_order_line_date/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Sale Order Line Date",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.1",
     "summary": "Adds a commitment date to each sale order line.",
     "author": "OdooMRP team,"
     "AvanzOSC,"
