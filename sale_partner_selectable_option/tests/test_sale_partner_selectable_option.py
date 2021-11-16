6a1b6b95e931ea3e11874369a8f6f8fe8a6c494e
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-16 14:42:08 +0100

    [ADD] sale_partner_selectable_option: This module limits the partners to be selected in the sales orders. TT32528

---
+++ sale_partner_selectable_option/tests/test_sale_partner_selectable_option.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from lxml import etree
+
+from odoo.tests import common
+
+
+class TestSalePartnerSelectableOption(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_sale_order(self):
+        result = self.env["sale.order"].fields_view_get(
+            view_id=self.env.ref("sale.view_order_form").id, view_type="form",
+        )
+        doc = etree.XML(result["arch"])
+        field = doc.xpath("//field[@name='partner_id']")
+        domain = field[0].get("domain")
+        self.assertTrue("[('selectable_in_sales_orders', '=', True)]" in domain)
