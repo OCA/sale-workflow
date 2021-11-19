67c88afd0fb605814dce69fc9ffc3acd0bba12ee
manu <manuel.regidor@sygel.es>
2021-11-19 13:46:04 +0100

    [MIG]sale_order_warn_message: Migration to 14.0

--- sale_order_warn_message/__manifest__.py
+++ sale_order_warn_message/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sale Order Warn Message",
     "summary": """
         Add a popup warning on sale to ensure warning is populated""",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
