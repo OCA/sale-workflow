cd754aa2d7b1badfda6359788615da7a8375558d
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-29 10:56:40 +0100

    [IMP] sales_team_security: Auto-followers - Adapt to 14.0

--- sales_team_security/tests/test_sales_team_security.py
+++ sales_team_security/tests/test_sales_team_security.py
@@ -4,51 +4,17 @@
 
 from lxml import etree
 
-from odoo.tests import common
-
 from ..hooks import post_init_hook
+from .common import TestCommon
 
 
-class TestSalesTeamSecurity(common.SavepointCase):
+class TestSalesTeamSecurity(TestCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.team = cls.env["crm.team"].create({"name": "Test channel"})
-        cls.team2 = cls.env["crm.team"].create({"name": "Test channel 2"})
-        cls.partner = cls.env["res.partner"].create(
-            {"name": "Test partner", "team_id": cls.team.id}
-        )
-        cls.partner_child_1 = cls.env["res.partner"].create(
-            {"name": "Child 1", "parent_id": cls.partner.id}
-        )
-        cls.partner_child_2 = cls.env["res.partner"].create(
-            {"name": "Child 2", "parent_id": cls.partner.id, "type": "invoice"}
-        )
-        cls.user = cls.env["res.users"].create(
-            {
-                "login": "sales_team_security",
-                "name": "Test sales_team_security user",
-                "groups_id": [(4, cls.env.ref("sales_team.group_sale_salesman").id)],
-                "sale_team_id": cls.team.id,
-            }
-        )
         cls.user_partner = cls.user.partner_id
-        cls.user2 = cls.env["res.users"].create(
-            {
-                "login": "sales_team_security2",
-                "name": "Test sales_team_security user 2",
-                "groups_id": [(4, cls.env.ref("sales_team.group_sale_salesman").id)],
-                "sale_team_id": cls.team.id,
-            }
-        )
         cls.user2_partner = cls.user2.partner_id
-        cls.sale_order = cls.env["sale.order"].create(
-            {
-                "partner_id": cls.partner.id,
-                "user_id": cls.user.id,
-                "team_id": cls.team.id,
-            }
-        )
+        cls.record = cls.partner
 
     def test_onchange_parent_id(self):
         contact = self.env["res.partner"].create(
@@ -107,70 +73,17 @@ class TestSalesTeamSecurity(common.SavepointCase):
         self.assertTrue(xml_fields)
         self.assertTrue("default_team_id" in xml_fields[0].get("context", ""))
 
-    def _check_permission(self, record, salesman, team, expected):
-        record.write(
-            {
-                "user_id": salesman.id if salesman else salesman,
-                "team_id": team.id if team else team,
-            }
-        )
-        domain = [("id", "=", record.id)]
-        obj = self.env[record._name].with_user(self.user)
-        self.assertEqual(bool(obj.search(domain)), expected)
-
-    def _check_whole_permission_set(self, record, extra_checks=True):
-        self._check_permission(record, False, False, True)
-        self._check_permission(record, self.user, False, True)
-        self._check_permission(record, self.user2, False, False)
-        self._check_permission(record, False, self.team, True)
-        if extra_checks:
-            self._check_permission(record, False, self.team2, False)
-        self._check_permission(record, self.user, self.team, True)
-        self._check_permission(record, self.user, self.team2, True)
-        self._check_permission(record, self.user2, self.team2, False)
-        self._check_permission(record, self.user2, self.team, False)
-        # Add to group "Team manager"
-        self.user.groups_id = [
-            (4, self.env.ref("sales_team_security.group_sale_team_manager").id)
-        ]
-        self._check_permission(record, False, False, True)
-        self._check_permission(record, self.user, False, True)
-        self._check_permission(record, self.user2, False, True)
-        self._check_permission(record, False, self.team, True)
-        if extra_checks:
-            self._check_permission(record, False, self.team2, False)
-        self._check_permission(record, self.user, self.team, True)
-        self._check_permission(record, self.user, self.team2, True)
-        self._check_permission(record, self.user2, self.team2, False)
-        self._check_permission(record, self.user2, self.team, True)
-        # Add to group "See all leads"
-        self.user.groups_id = [
-            (4, self.env.ref("sales_team.group_sale_salesman_all_leads").id)
-        ]
-        self._check_permission(record, False, False, True)
-        self._check_permission(record, self.user, False, True)
-        self._check_permission(record, self.user2, False, True)
-        self._check_permission(record, False, self.team, True)
-        self._check_permission(record, False, self.team2, True)
-        self._check_permission(record, self.user, self.team, True)
-        self._check_permission(record, self.user, self.team2, True)
-        self._check_permission(record, self.user2, self.team2, True)
-        self._check_permission(record, self.user2, self.team, True)
-        # Regular internal user
-        if extra_checks:
-            self.user.groups_id = [(6, 0, [self.env.ref("base.group_user").id])]
-            self._check_permission(record, False, False, True)
-            self._check_permission(record, self.user, False, True)
-            self._check_permission(record, self.user2, False, True)
-            self._check_permission(record, False, self.team, True)
-            self._check_permission(record, False, self.team2, True)
-            self._check_permission(record, self.user, self.team, True)
-            self._check_permission(record, self.user, self.team2, True)
-            self._check_permission(record, self.user2, self.team2, True)
-            self._check_permission(record, self.user2, self.team, True)
-
     def test_partner_permissions(self):
-        self._check_whole_permission_set(self.partner)
+        self._check_whole_permission_set()
 
-    def test_sale_order_permissions(self):
-        self._check_whole_permission_set(self.sale_order, extra_checks=False)
+    def test_partner_permissions_subscription(self):
+        self.check_permission_subscribe = True
+        self._check_permission(self.user2, False, True)
+
+    def test_partner_permissions_own_partner(self):
+        self.user.partner_id.write({"user_id": self.user2.id})
+        domain = [("id", "in", self.user.partner_id.ids)]
+        Partner = self.env["res.partner"].with_user(self.user)
+        # Make sure the acces is not due to the subscription
+        self.partner.message_unsubscribe(partner_ids=self.user.partner_id.ids)
+        self.assertEqual(bool(Partner.search(domain)), True)
