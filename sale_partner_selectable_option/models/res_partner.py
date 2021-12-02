b9f1def3e3990082981b1c4e7e4af1f8f94cdbf1
Víctor Martínez <victor.martinez@tecnativa.com>
2021-12-02 14:34:05 +0100

    [IMP] sale_partner_selectable_option: Change position field. TT33223

--- sale_partner_selectable_option/models/res_partner.py
+++ sale_partner_selectable_option/models/res_partner.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    selectable_in_sales_orders = fields.Boolean(
-        string="Selectable in sales orders", default=True
-    )
+    sale_selectable = fields.Boolean(string="Selectable in sale orders", default=True)
