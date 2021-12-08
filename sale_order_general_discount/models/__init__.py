58ea350923c0d01dfa1c672c4af4ec23031c4761
Pierre Verkest <pierreverkest84@gmail.com>
2021-12-08 16:39:46 +0100

    [16.0][IMP] sale_order_general_discount: apply discount only on general discount apply product option

--- sale_order_general_discount/models/__init__.py
+++ sale_order_general_discount/models/__init__.py
@@ -1,4 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import product_product
+from . import product_template
 from . import res_partner
 from . import sale_order
 from . import sale_order_line
