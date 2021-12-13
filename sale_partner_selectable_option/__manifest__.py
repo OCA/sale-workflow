ca957919699eb6e9f1a4b51255b409a39018be2b
Víctor Martínez <victor.martinez@tecnativa.com>
2021-12-13 15:01:14 +0100

    [FIX] sale_partner_selectable_option: Improve domain in partner_id field from sale order. TT33223

--- sale_partner_selectable_option/__manifest__.py
+++ sale_partner_selectable_option/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Partner Selectable Option",
-    "version": "13.0.2.1.0",
+    "version": "13.0.2.2.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["sale"],
-    "data": ["views/res_partner_view.xml", "views/sale_order_view.xml"],
+    "data": ["views/res_partner_view.xml"],
     "installable": True,
     "maintainers": ["victoralmau"],
 }
