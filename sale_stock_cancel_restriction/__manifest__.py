0538e18eb56d5e2d91227631d9be974c2ea0b1e7
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-12-13 13:17:40 -0500

    [ADD] sale_stock_cancel_restriction: new module.

---
+++ sale_stock_cancel_restriction/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Stock Cancel Restriction",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "development_status": "Production/Stable",
+    "depends": ["sale_stock"],
+    "installable": True,
+}
