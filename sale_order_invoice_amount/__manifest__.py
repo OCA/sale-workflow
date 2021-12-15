5c505208e3767d64071c20634b47b6423fa9978f
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2021-12-15 10:04:35 +0530

    [MIG] sale_order_invoice_amount: Migration to 14.0

--- sale_order_invoice_amount/__manifest__.py
+++ sale_order_invoice_amount/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Invoice Amount",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
