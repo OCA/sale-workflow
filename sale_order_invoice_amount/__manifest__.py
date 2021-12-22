446449da9eba5c8a82b5e943405335aa02466009
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2021-12-22 14:12:51 +0530

    [MIG] sale_order_invoice_amount: Migration to 15.0

--- sale_order_invoice_amount/__manifest__.py
+++ sale_order_invoice_amount/__manifest__.py
@@ -3,13 +3,18 @@
 
 {
     "name": "Sale Order Invoice Amount",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
     "license": "LGPL-3",
     "summary": "Display the invoiced and uninvoiced total in the sale order",
-    "depends": ["sale"],
+    "depends": ["sale", "account"],
     "data": ["views/sale_order_view.xml"],
     "installable": True,
+    "assets": {
+        "web.assets_qweb": [
+            "sale_order_invoice_amount/static/src/xml/**/*",
+        ],
+    },
 }
