f984497f000f0033e3c8e3428e82083b5b964756
emagdalenaC2i <emagdalena@c2i.es>
2022-01-01 23:58:12 +0100

    [MIG] sale_product_category_menu : Migration to 15.0

--- sale_product_category_menu/__manifest__.py
+++ sale_product_category_menu/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Product Category Menu",
     "summary": "Shows 'Product Categories' menu item in Sales",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Sygel, Odoo Community Association (OCA)",
