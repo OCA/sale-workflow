61fe0c31f1ddb9da8dcb9c9bdb6e38e886836197
Damien Crier <damien.crier@camptocamp.com>
2022-01-12 11:02:25 +0100

    [14.0][ADD] add sale_quick and sale_quick_seasonality

---
+++ sale_quick/models/sale_order.py
@@ -0,0 +1,65 @@
+# Copyright 2022 Camptocamp SA
+# @author: Damien Crier <damien.crier@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _name = "sale.order"
+    _inherit = ["sale.order", "product.mass.addition"]
+
+    def _get_context_add_products(self):
+        return {
+            "search_default_filter_to_sale": 1,
+            "quick_access_rights_sale": 1,
+        }
+
+    def _get_domain_add_products(self):
+        return []
+
+    def add_product(self):
+        self.ensure_one()
+        res = self._common_action_keys()
+        res["context"].update(self._get_context_add_products())
+        domain = self._get_domain_add_products()
+        if domain:
+            res["domain"] = domain
+        commercial = self.partner_id.commercial_partner_id.name
+        res["name"] = "🔙 {} ({})".format(_("Product Variants"), commercial)
+        res["view_id"] = (self.env.ref("sale_quick.product_tree_view4sale").id,)
+        return res
+
+    def _get_quick_line(self, product):
+        result = self.order_line.filtered(lambda rec: rec.product_id == product)
+
+        nr_lines = len(result.ids)
+        if nr_lines > 1:
+            raise ValidationError(
+                _(
+                    "Must have only 1 line per product for mass addition, but "
+                    "there are %s lines for the product %s"
+                    % (nr_lines, product.display_name),
+                )
+            )
+        return result
+
+    def _get_quick_line_qty_vals(self, product):
+        return {
+            "product_uom_qty": product.qty_to_process,
+            "product_uom": product.quick_uom_id.id,
+        }
+
+    def _complete_quick_line_vals(self, vals, lines_key=""):
+        # This params are need for playing correctly the onchange
+        vals.update(
+            {
+                "order_id": self.id,
+                "order_partner_id": self.partner_id.id,
+            }
+        )
+        return super()._complete_quick_line_vals(vals, lines_key="order_line")
+
+    def _add_quick_line(self, product, lines_key=""):
+        return super()._add_quick_line(product, lines_key="order_line")
