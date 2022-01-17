e7018b6630364f6c3447f6bbfa88f49d3004b331
Kiril Vangelovski <kiril@lambda-is.com>
2022-01-17 18:24:14 +0100

    [MIG] sale_rental: Migration to 15.0
    Standard migration + add some tests

--- sale_rental/models/__init__.py
+++ sale_rental/models/__init__.py
@@ -3,4 +3,4 @@ from . import sale_order
 from . import sale_rental
 from . import stock_warehouse
 from . import stock_rule
-from . import stock_inventory
+from . import stock_quant
--- sale_rental/models/stock_quant.py
+++ sale_rental/models/stock_quant.py
@@ -3,18 +3,15 @@
 # Copyright 2016-2021 Sodexis (http://sodexis.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class StockInventory(models.Model):
-    _inherit = "stock.inventory"
+    _inherit = "stock.quant"
 
+    @api.model
     def create_demo_and_validate(self):
-        silo = self.env["stock.inventory.line"]
-        demo_inv = self.env.ref("sale_rental.rental_inventory")
         rental_in_loc = self.env.ref("stock.warehouse0").rental_in_location_id
-        demo_inv.write({"location_ids": [(6, 0, [rental_in_loc.id])]})
-        demo_inv.action_start()
         products = [
             ("product.consu_delivery_01", 56),
             ("product.product_product_20", 46),
@@ -22,13 +19,10 @@ class StockInventory(models.Model):
         ]
         for (product_xmlid, qty) in products:
             product = self.env.ref(product_xmlid)
-            silo.create(
+            self.with_context(inventory_mode=True).create(
                 {
                     "product_id": product.id,
-                    "product_uom_id": product.uom_id.id,
-                    "inventory_id": demo_inv.id,
-                    "product_qty": qty,
+                    "inventory_quantity": qty,
                     "location_id": rental_in_loc.id,
                 }
-            )
-        demo_inv.action_validate()
+            ).action_apply_inventory()
