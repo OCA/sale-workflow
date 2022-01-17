08d6cde305bb6b8b7e6c722c6ba82d8ce886d4a6
jcoux <julien.coux@camptocamp.com>
2022-01-17 17:04:50 +0100

    [14.0][FIX] Fix sale_automatic_workflow_payment_mode migration
    Parts of `sale_automatic_workflow_payment_mode` have been merged into `sale_automatic_workflow`.
    But these parts have been not removed from `sale_automatic_workflow_payment_mode`.
    
    This PR clean both modules after this merge.
    
    Originals PR of the merge: https://github.com/OCA/sale-workflow/pull/1789

--- sale_automatic_workflow_payment_mode/__manifest__.py
+++ sale_automatic_workflow_payment_mode/__manifest__.py
@@ -10,9 +10,7 @@
     "depends": ["sale_automatic_workflow", "account_payment_sale"],  # oca/bank-payment
     "website": "https://github.com/OCA/sale-workflow",
     "data": [
-        "data/automatic_workflow_data.xml",
         "views/account_payment_mode_views.xml",
-        "views/sale_workflow_process_view.xml",
     ],
     "installable": True,
     "auto_install": True,
--- sale_automatic_workflow_payment_mode/models/automatic_workflow_job.py
+++ sale_automatic_workflow_payment_mode/models/automatic_workflow_job.py
@@ -3,10 +3,7 @@
 
 import logging
 
-from odoo import api, fields, models
-from odoo.tools.safe_eval import safe_eval
-
-from odoo.addons.sale_automatic_workflow.models.automatic_workflow_job import savepoint
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
@@ -14,65 +11,28 @@ _logger = logging.getLogger(__name__)
 class AutomaticWorkflowJob(models.Model):
     _inherit = "automatic.workflow.job"
 
-    @api.model
-    def run_with_workflow(self, sale_wkf):
-        workflow_domain = [("workflow_process_id", "=", sale_wkf.id)]
-        res = super().run_with_workflow(sale_wkf)
-        if sale_wkf.register_payment:
-            self._register_payments(
-                safe_eval(sale_wkf.payment_filter_id.domain) + workflow_domain
+    def _prepare_dict_account_payment(self, invoice):
+        values = super()._prepare_dict_account_payment(invoice)
+        payment_mode = invoice.payment_mode_id
+        values.update(
+            {
+                "payment_reference": invoice.payment_reference or invoice.name,
+                "payment_method_id": payment_mode.payment_method_id.id,
+                "journal_id": payment_mode.fixed_journal_id.id,
+            }
+        )
+        return values
+
+    def _register_payment_invoice(self, invoice):
+        payment_mode = invoice.payment_mode_id
+
+        if not payment_mode.fixed_journal_id:
+            _logger.debug(
+                "Unable to Register Payment for invoice %s: "
+                "Payment mode %s must have fixed journal",
+                invoice.id,
+                payment_mode.id,
             )
-        return res
+            return
 
-    @api.model
-    def _register_payments(self, payment_filter):
-        invoice_obj = self.env["account.move"]
-        invoices = invoice_obj.search(payment_filter)
-        _logger.debug("Invoices to Register Payment: %s", invoices.ids)
-
-        domain = [
-            ("account_internal_type", "in", ("receivable", "payable")),
-            ("reconciled", "=", False),
-        ]
-        for invoice in invoices:
-            partner_type = (
-                invoice.move_type in ("out_invoice", "out_refund")
-                and "customer"
-                or "supplier"
-            )
-            payment_mode = invoice.payment_mode_id
-
-            if not payment_mode.fixed_journal_id:
-                _logger.debug(
-                    "Unable to Register Payment for invoice %s: "
-                    "Payment mode %s must have fixed journal",
-                    invoice.id,
-                    payment_mode.id,
-                )
-                continue
-
-            with savepoint(self.env.cr):
-                payment = self.env["account.payment"].create(
-                    {
-                        "amount": invoice.amount_residual,
-                        "date": fields.Date.context_today(self),
-                        "payment_reference": invoice.payment_reference or invoice.name,
-                        "partner_id": invoice.partner_id.id,
-                        "partner_type": partner_type,
-                        "payment_type": payment_mode.payment_type,
-                        "payment_method_id": payment_mode.payment_method_id.id,
-                        "journal_id": payment_mode.fixed_journal_id.id,
-                    }
-                )
-                payment.action_post()
-
-            if payment.state != "posted":
-                continue
-
-            payment_lines = payment.line_ids.filtered_domain(domain)
-            lines = invoice.line_ids
-            for account in payment_lines.account_id:
-                (payment_lines + lines).filtered_domain(
-                    [("account_id", "=", account.id), ("reconciled", "=", False)]
-                ).reconcile()
-        return
+        return super()._register_payment_invoice(invoice)
