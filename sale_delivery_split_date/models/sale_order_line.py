cf4cef53787f7ce0ef8292c2340d6073f3a8da38
Alfredo Zamora <alfredo.zamora@agilebg.com>
2022-01-18 08:48:57 +0100

    [MIG] sale_delivery_split_date: Migration to 14.0

--- sale_delivery_split_date/models/sale_order_line.py
+++ sale_delivery_split_date/models/sale_order_line.py
@@ -1,14 +1,20 @@
 # Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
+from odoo.tools import format_date
 
 
 class SaleOrderLine(models.Model):
-
     _inherit = "sale.order.line"
 
-    @api.multi
+    def _prepare_procurement_group_vals(self):
+        vals = super(SaleOrderLine, self)._prepare_procurement_group_vals()
+        if self._get_procurement_group_key()[0] == 24:
+            if self.commitment_date:
+                vals["name"] += "/" + format_date(self.env, self.commitment_date.date())
+        return vals
+
     def _get_procurement_group_key(self):
         """Return a key with priority to be used to regroup lines in multiple
         procurement groups
@@ -21,3 +27,9 @@ class SaleOrderLine(models.Model):
                 # group by date instead of datetime
                 return (priority, self.commitment_date.date())
         return key
+
+    def _prepare_procurement_values(self, group_id=False):
+        vals = super(SaleOrderLine, self)._prepare_procurement_values(group_id=group_id)
+        if self.commitment_date:
+            vals.update({"date_planned": self.commitment_date})
+        return vals
