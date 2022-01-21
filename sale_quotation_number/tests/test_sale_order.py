02891714e79a841aa484818d9b3b366060101e51
sergio-teruel <sergio.teruel@tecnativa.com>
2022-01-21 01:48:14 +0100

    [FIX] sale_quotation_number: Compatibility with sale_order_revision

--- sale_quotation_number/tests/test_sale_order.py
+++ sale_quotation_number/tests/test_sale_order.py
@@ -2,14 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import UserError
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestSaleOrder(TransactionCase):
-    def setUp(self, *args, **kwargs):
-        super(TestSaleOrder, self).setUp()
-        self.sale_order_model = self.env["sale.order"]
-        company = self.env.company
+class TestSaleOrder(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.sale_order_model = cls.env["sale.order"]
+        company = cls.env.company
         company.keep_name_so = False
 
     def test_enumeration(self):
@@ -57,9 +58,12 @@ class TestSaleOrder(TransactionCase):
 
     def test_copy_with_origin(self):
         origin = "origin"
-        order1 = self.sale_order_model.create(
-            {"origin": origin, "partner_id": self.env.ref("base.res_partner_1").id}
-        )
+        vals = {"origin": origin, "partner_id": self.env.ref("base.res_partner_1").id}
+        # This test fails randomly if sale_order_revision is installed so I
+        # apply this trick
+        if "unrevisioned_name" in self.sale_order_model:
+            vals["unrevisioned_name"] = "URunrevisioned_name_01"
+        order1 = self.sale_order_model.create(vals)
         order_copy = order1.copy()
 
         self.assertEqual(", ".join([origin, order1.name]), order_copy.origin)
