190d4ea0c1c707be1f6d4abfccffac49ba196ab6
Bernat Puig Font <bernat.puig@forgeflow.com>
2022-01-28 16:39:18 +0100

    [15.0][FIX] sale_order_line_date: Refactor code

--- sale_order_line_date/models/sale_order_line.py
+++ sale_order_line_date/models/sale_order_line.py
@@ -6,6 +6,8 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
+from datetime import timedelta
+
 from odoo import fields, models
 
 
@@ -14,22 +16,25 @@ class SaleOrderLine(models.Model):
 
     commitment_date = fields.Datetime("Delivery Date")
 
-    def write(self, vals):
-        # Force commitment date only if all lines are on the same sale order
-        if len(self.mapped("order_id")) == 1:
-            for line in self:
-                if (
-                    not line.commitment_date
-                    and line.order_id.commitment_date
-                    and "commitment_date" not in vals
-                ):
-                    vals.update({"commitment_date": line.order_id.commitment_date})
-                    break
-        return super(SaleOrderLine, self).write(vals)
-
     def _prepare_procurement_values(self, group_id=False):
         vals = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
         # has ensure_one already
         if self.commitment_date:
+            vals.update(
+                {
+                    "date_planned": self.commitment_date
+                    - timedelta(days=self.order_id.company_id.security_lead)
+                }
+            )
             vals.update({"date_deadline": self.commitment_date})
         return vals
+
+    def _expected_date(self):
+        """
+        The only intention of this method is to call
+        _onchange_expected_date() when a sale order line
+        Delivery Date is changed and SO was already confirmed.
+        """
+        res = super(SaleOrderLine, self)._expected_date()
+        self.order_id._onchange_expected_date()
+        return res
