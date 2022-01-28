366ed146595c10d04019b4a0045fd3c62ff0278a
Bernat Puig Font <bernat.puig@forgeflow.com>
2022-01-28 16:39:18 +0100

    [15.0][FIX] sale_order_line_date: Refactor code

--- sale_order_line_date/tests/test_sale_order_line_date.py
+++ sale_order_line_date/tests/test_sale_order_line_date.py
@@ -19,22 +19,20 @@ class TestSaleOrderLineDates(TransactionCase):
         price = 100.0
         qty = 5
         product_id = self.env.ref("product.product_product_7")
-        today = fields.Datetime.now()
-        dt1 = today + datetime.timedelta(days=9)
-        dt2 = today + datetime.timedelta(days=10)
-        self.dt3 = today + datetime.timedelta(days=3)
-        self.sale1 = self._create_sale_order(customer, dt2)
+        self.today = fields.Datetime.now()
+        self.dt1 = self.today + datetime.timedelta(days=9)
+        self.dt2 = self.today + datetime.timedelta(days=10)
+        self.dt3 = self.today + datetime.timedelta(days=3)
+        self.sale1 = self._create_sale_order(customer, None)
         self.sale_line1 = self._create_sale_order_line(
-            self.sale1, product_id, qty, price, dt1
+            self.sale1, product_id, qty, price, None
         )
         self.sale_line2 = self._create_sale_order_line(
-            self.sale1, product_id, qty, price, dt2
+            self.sale1, product_id, qty, price, None
         )
         self.sale_line3 = self._create_sale_order_line(
             self.sale1, product_id, qty, price, None
         )
-        self.sale_line2.write({"commitment_date": dt2})
-        self.sale1.action_confirm()
 
     def _create_sale_order(self, customer, date):
         sale = self.env["sale.order"].create(
@@ -43,6 +41,7 @@ class TestSaleOrderLineDates(TransactionCase):
                 "partner_invoice_id": customer.id,
                 "partner_shipping_id": customer.id,
                 "commitment_date": date,
+                "picking_policy": "direct",
             }
         )
         return sale
@@ -60,10 +59,38 @@ class TestSaleOrderLineDates(TransactionCase):
         )
         return sale_line
 
-    def test_on_change_commitment_date(self):
-        """True when the commitment date in the sale_order_line
-        matches the commitment date in the sale order"""
+    def test_01_so_quotation_commitment_date(self):
+        """Test if commitment date is correct in SO quotation"""
+        self.assertEqual(self.sale1.commitment_date, False)
+        self.sale_line1.write({"commitment_date": self.dt3})
+        self.sale1._onchange_expected_date()
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+        self.sale_line2.write({"commitment_date": self.dt2})
+        self.sale1._onchange_expected_date()
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+        self.sale1.picking_policy = "one"
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+
+    def test_02_so_commitment_date(self):
+        """Test if commitment date is correct in SO quotation"""
+        self.sale1.action_confirm()
+        self.assertEqual(self.sale1.commitment_date, False)
+        self.sale_line1.write({"commitment_date": self.dt3})
+        self.sale1._onchange_expected_date()
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+        self.sale_line2.write({"commitment_date": self.dt2})
+        self.sale1._onchange_expected_date()
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+        self.sale1.picking_policy = "one"
+        self.assertEqual(self.sale1.commitment_date, self.dt3)
+
+    def test_03_on_change_so_commitment_date(self):
+        """Test if changing Delivery Date has repercussion in SO lines"""
+        self.sale_line1.write({"commitment_date": self.dt1})
+        self.sale_line2.write({"commitment_date": self.dt2})
+        self.sale_line3.write({"commitment_date": self.dt3})
+        self.sale1.action_confirm()
         self.sale1.write({"commitment_date": self.dt3})
-        result = self.sale1._onchange_commitment_date()
-        for line in result["value"]["order_line"]:
-            self.assertEqual(line[2]["commitment_date"], self.dt3)
+        self.assertEqual(self.sale_line1.commitment_date, self.dt1)
+        self.assertEqual(self.sale_line2.commitment_date, self.dt2)
+        self.assertEqual(self.sale_line3.commitment_date, self.dt3)
