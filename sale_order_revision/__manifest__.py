8407a3d2e453cdc4fdd058ed2b4653f83af10d53
Víctor Martínez <victor.martinez@tecnativa.com>
2022-02-03 09:40:38 +0100

    [IMP] sale_order_revision: Tests are added to check that the addon works as expected. TT33433

--- sale_order_revision/__manifest__.py
+++ sale_order_revision/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Sale order revisions",
     "summary": "Keep track of revised quotations",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Sale Management",
     "author": "Agile Business Group,"
     "Dreambits,"
