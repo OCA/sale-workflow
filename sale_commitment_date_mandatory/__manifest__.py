58d2becfca42d29ee634266514b476287d9633fc
Ruchir Shukla <ruchir@bizzappdev.com>
2022-02-15 14:37:24 +0530

    [MIG] sale_commitment_date_mandatory : Migrate sale_commitment_date_mandatory to v15.

--- sale_commitment_date_mandatory/__manifest__.py
+++ sale_commitment_date_mandatory/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale commitment date mandatory",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "summary": "Set commitment data mandatory and don't allow"
--- sale_commitment_date_mandatory/readme/CONTRIBUTORS.rst
+++ sale_commitment_date_mandatory/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Camptocamp
 * Damien Crier <damien.crier@camptocamp.com>
+* Ruchir Shukla <ruchir@bizzappdev.com>
