a9d295ad1791a06d4145866fb471bc9f701a610f
Ruchir Shukla <ruchir@bizzappdev.com>
2022-02-15 15:57:27 +0530

    [MIG] sale_order_archive : Migrate sale_order_archive to v15.

--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale_order.xml"],
