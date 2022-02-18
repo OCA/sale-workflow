07aeecc1be6a09c236d1a4edf156557b83b22d92
dani <dani.xtendoo@gmail.com>
2022-02-18 13:17:08 +0100

    [ADD] sale_order_line_last_price: black, isort

---
+++ sale_order_line_last_price/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale order line last price",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Tecnativa," "Odoo Community Association (OCA)," "Xtendoo",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": ["sale_order_line_price_history"],
+    "data": [
+                "wizards/sale_order_line_last_price.xml",
+    ],
+    "installable": True,
+}
