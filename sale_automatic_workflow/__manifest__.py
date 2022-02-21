74e84330c04d8430e28e4064746e94cc5a94f747
david <david.vidal@tecnativa.com>
2022-02-21 17:36:53 +0100

    [FIX] sale_automatic_workflow: active model context
    For some invoicing processes the active model context is important. For
    example:
    
    https://github.com/OCA/bank-payment/blob/13.0/account_payment_partner/models/account_move.py#L161
    
    TT34678

--- sale_automatic_workflow/__manifest__.py
+++ sale_automatic_workflow/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Sale Automatic Workflow",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Akretion, "
