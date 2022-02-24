88d1268a99d3d5d9ee4ed4616db7b1dbdfd818da
François Honoré <francois.honore@acsone.eu>
2022-02-24 17:45:52 +0100

    [13.0][FIX] sale_order_line_packaging_qty: fix decimal precision to check if qty packaged must be reset + unit test

--- sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
+++ sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
@@ -8,10 +8,16 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.precision = cls.env["decimal.precision"].precision_get(
+            "Product Unit of Measure"
+        )
         cls.partner = cls.env.ref("base.res_partner_12")
         cls.product = cls.env.ref("product.product_product_9")
         cls.packaging = cls.env["product.packaging"].create(
             {"name": "Test packaging", "product_id": cls.product.id, "qty": 5.0}
+        )
+        cls.packaging2 = cls.env["product.packaging"].create(
+            {"name": "Test packaging", "product_id": cls.product.id, "qty": 2.2}
         )
 
     def test_product_packaging_qty(self):
@@ -24,9 +30,54 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
                 "product_uom_qty": 3.0,
             }
         )
-        order_line.write({"product_packaging": self.packaging})
+        order_line.write({"product_packaging": self.packaging.id})
         order_line._onchange_product_packaging()
-        self.assertEqual(order_line.product_uom_qty, 5.0)
-        self.assertEqual(order_line.product_packaging_qty, 1.0)
-        order_line.write({"product_packaging_qty": 3.0})
-        self.assertEqual(order_line.product_uom_qty, 15.0)
+        self.assertAlmostEqual(
+            order_line.product_uom_qty, self.packaging.qty, places=self.precision
+        )
+        self.assertAlmostEqual(
+            order_line.product_packaging_qty, 1.0, places=self.precision
+        )
+        pack_qty = 3.0
+        order_line.write({"product_packaging_qty": pack_qty})
+        order_line._compute_product_packaging_qty()
+        self.assertAlmostEqual(
+            order_line.product_uom_qty,
+            self.packaging.qty * pack_qty,
+            places=self.precision,
+        )
+        self.assertAlmostEqual(
+            order_line.product_packaging_qty, pack_qty, places=self.precision
+        )
+
+    def test_product_packaging_qty_float(self):
+        order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": order.id,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+                "product_uom_qty": 7,
+            }
+        )
+        order_line.write({"product_packaging": self.packaging2.id})
+        order_line._onchange_product_packaging()
+        self.assertAlmostEqual(
+            order_line.product_uom_qty, self.packaging2.qty, places=self.precision
+        )
+        self.assertAlmostEqual(
+            order_line.product_packaging_qty, 1.0, places=self.precision
+        )
+        pack_qty = 7
+        order_line.write({"product_packaging_qty": pack_qty})
+        # Via interface, this compute is triggered and could cause some issue.
+        # So simulate it.
+        order_line._compute_product_packaging_qty()
+        self.assertAlmostEqual(
+            order_line.product_uom_qty,
+            self.packaging2.qty * pack_qty,
+            places=self.precision,
+        )
+        self.assertAlmostEqual(
+            order_line.product_packaging_qty, pack_qty, places=self.precision
+        )
