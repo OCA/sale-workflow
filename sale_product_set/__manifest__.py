dbc78b271b7cca7b73842c6cdd702440c2c3d399
Víctor Martínez <victor.martinez@tecnativa.com>
2022-03-11 08:31:39 +0100

    [FIX] sale_product_set: Change toggle_active button to web_ribbon widget in form view.
    TT34555

--- sale_product_set/__manifest__.py
+++ sale_product_set/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Sale",
     "license": "AGPL-3",
     "author": "Anybox, Odoo Community Association (OCA)",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.4.0",
     "website": "https://github.com/OCA/sale-workflow",
     "summary": "Sale product set",
     "depends": ["sale", "sale_management", "onchange_helper"],
