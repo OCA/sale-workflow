7beafa4eaf7273ac26bd9e7750e9403a0466b7bb
david <david.vidal@tecnativa.com>
2022-04-19 12:20:40 +0200

    [ADD] sale_order_pricelist_tracking: New module
    TT35444

---
+++ sale_order_pricelist_tracking/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Track sale order pricelist changes",
+    "version": "13.0.1.0.0",
+    "category": "Partner Management",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/partner-contact",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [],
+    "installable": True,
+}
---
+++ sale_order_pricelist_tracking/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to track manual pricelist changes in the sales order in the chatter.
