b1523eecf6d05a8b3c5ebd4df7fd9fd37eb0c293
Ilyas <irazor147@gmail.com>
2022-04-21 12:57:59 +0500

    [14.0][TEST] sale_amount_payment_link sale_invoice_plan  Fix tests

--- sale_invoice_plan/tests/test_sale_invoice_plan.py
+++ sale_invoice_plan/tests/test_sale_invoice_plan.py
@@ -3,11 +3,12 @@
 from collections import OrderedDict
 
 from odoo.exceptions import UserError, ValidationError
-from odoo.tests import Form
+from odoo.tests import Form, tagged
 
 from odoo.addons.sale.tests import common
 
 
+@tagged("post_install")
 class TestSaleInvoicePlan(common.TestSaleCommon):
     @classmethod
     def setUpClass(cls):
