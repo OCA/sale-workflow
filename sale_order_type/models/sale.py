f5f178afa3a46109c6527ed90231ff9f8b61e406
matiasperalta1 <matias_peralta1@hotmail.com>
2022-04-22 17:21:22 -0300

    [FIX][Backport 13.0] sale_order_type: When creating a sale order, the "Type of order" changes

--- sale_order_type/models/sale.py
+++ sale_order_type/models/sale.py
@@ -50,24 +50,23 @@ class SaleOrder(models.Model):
 
     @api.depends("partner_id", "company_id")
     def _compute_sale_type_id(self):
-        for record in self:
-            if not record.partner_id:
-                record.type_id = self.env["sale.order.type"].search(
-                    [("company_id", "in", [record.company_id.id, False])], limit=1
-                )
-            else:
-                sale_type = (
-                    record.partner_id.with_context(
-                        force_company=record.company_id.id
-                    ).sale_type
-                    or record.partner_id.commercial_partner_id.with_context(
-                        force_company=record.company_id.id
-                    ).sale_type
-                )
-                if sale_type:
-                    record.type_id = sale_type
-                else:  # HACK: Avoid CacheMiss when no sale_type is set
-                    record.type_id = record.type_id
+        if "compute_sale_type_id" not in self.env.context:
+            for record in self:
+                if not record.partner_id:
+                    record.type_id = self.env["sale.order.type"].search(
+                        [("company_id", "in", [self.env.company.id, False])], limit=1
+                    )
+                else:
+                    sale_type = (
+                        record.partner_id.with_context(
+                            force_company=record.company_id.id
+                        ).sale_type
+                        or record.partner_id.commercial_partner_id.with_context(
+                            force_company=record.company_id.id
+                        ).sale_type
+                    )
+                    if sale_type:
+                        record.type_id = sale_type
 
     @api.onchange("type_id")
     def onchange_type_id(self):
@@ -108,10 +107,14 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, vals):
         if vals.get("name", "/") == "/" and vals.get("type_id"):
-            next_sequence = self._get_next_sequence(vals)
-            if next_sequence:
-                vals["name"] = next_sequence
-        return super(SaleOrder, self).create(vals)
+            sale_type = self.env["sale.order.type"].browse(vals["type_id"])
+            if sale_type.sequence_id:
+                vals["name"] = sale_type.sequence_id.next_by_id(
+                    sequence_date=vals.get("date_order")
+                )
+        return super(SaleOrder, self.with_context(compute_sale_type_id=False)).create(
+            vals
+        )
 
     def write(self, vals):
         """A sale type could have a different order sequence, so we could
