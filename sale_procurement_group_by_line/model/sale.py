89a20e2c6f8a04eb4f9f90268465d7e012d2157e
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2022-04-29 13:51:09 +0200

    [FIX] sale_procurement_group_by_line: fix fill previous_product_uom_qty
    Before the fix, when new quantities are added into an already confirmed
    line, we are writing in the previous_product_uom_qty only the new qty that
    has been processed, not the total amount that has actually been
    processed for the line.
    
    Due to this, when we call super() on _action_launch_stock_rule, the new
    quantities are procured again, as we have not really updated the amount
    in previous_product_uom_qty including what we have already procured
    before calling super().

--- sale_procurement_group_by_line/model/sale.py
+++ sale_procurement_group_by_line/model/sale.py
@@ -85,7 +85,6 @@ class SaleOrderLine(models.Model):
 
             values = line._prepare_procurement_values(group_id=group_id)
             product_qty = line.product_uom_qty - qty
-            product_qty_uom = product_qty
 
             procurement_uom = line.product_uom
             quant_uom = line.product_id.uom_id
@@ -116,7 +115,7 @@ class SaleOrderLine(models.Model):
                 self.env["procurement.group"].run(procurements)
                 # We store the procured quantity in the UoM of the line to avoid
                 # duplicated procurements, specially for dropshipping and kits.
-                previous_product_uom_qty[line.id] = product_qty_uom
+                previous_product_uom_qty[line.id] = line.product_uom_qty
             except UserError as error:
                 errors.append(error.name)
         if errors:
