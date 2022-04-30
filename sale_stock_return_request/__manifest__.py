03dc95c44e63d90d079cb23c41df3db8001f3b5c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-04-30 15:11:34 +0200

    [FIX] sale_*: Adjust development status
    Can't depend on lower status.

--- sale_stock_return_request/__manifest__.py
+++ sale_stock_return_request/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "12.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/sale-workflow",
-    "development_status": "Production/Stable",
+    "development_status": "Beta",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "maintainers": [
