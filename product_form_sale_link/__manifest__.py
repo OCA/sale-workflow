79976e1bab413494f8ff156cc91f2a30d257a1e3
Miquel Raïch <miquel.raich@forgeflow.com>
2022-05-13 16:04:21 +0200

    [FIX] product_form_sale_link: assure compatibility with sale_stock
    The button 'action_view_sales' is replaced in sale_stock, so it cannot be referenced.

--- product_form_sale_link/__manifest__.py
+++ product_form_sale_link/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Form Sale Link",
     "summary": """
         Adds a button on product forms to access Sale Lines""",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "license": "AGPL-3",
     "category": "Sales",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
