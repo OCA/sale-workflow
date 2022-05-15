224f2651daf97f13035f1c1de409bd395e723454
Sébastien BEAU <sebastien.beau@akretion.com>
2022-05-15 18:15:48 +0200

    sale_order_pricelist_tax: refactor code to make it cleaner

--- sale_order_pricelist_tax/models/tax.py
+++ sale_order_pricelist_tax/models/tax.py
@@ -2,9 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-from collections import defaultdict
 
-from odoo import _, api, models
+from odoo import _, api, models, tools
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
@@ -20,43 +19,33 @@ class AccountTax(models.Model):
           instead of recomputed price
         - some checks/raises on unmanaged cases
         """
-        fixed_price = super(AccountTax, self)._fix_tax_included_price(
-            price, prod_taxes, line_taxes
-        )
         pricelist_id = self.env.context.get("pricelist")
-        skip_fixing_price = False
-        # Check if pricelist contains adhoc price
         if pricelist_id:
-            self._check_product_sale_tax_include(prod_taxes)
+            prod_taxes._ensure_price_include()
             pricelist = self.env["product.pricelist"].browse(pricelist_id)
             if not pricelist.price_include_taxes:
-                self._check_sale_line_tax_exclude(line_taxes, pricelist)
+                line_taxes._ensure_price_exclude(pricelist)
                 # we skip odoo logic as the pricelist is tax excluded
                 # with tax excluded on line so we do not have anything to remove
-                skip_fixing_price = True
+                return price
             elif all(line_taxes.mapped("price_include")):
                 # we skip odoo logic as the pricelist is tax included
                 # and the taxe line are tax included
-                skip_fixing_price = True
-        if skip_fixing_price:
-            return price
-        return fixed_price
+                return price
+        return super()._fix_tax_included_price(price, prod_taxes, line_taxes)
 
-    def _check_product_sale_tax_include(self, prod_taxes):
-        prod_taxes_exclude = prod_taxes.filtered(
-            lambda t: t.type_tax_use == "sale" and not t.price_include
-        )
-        if prod_taxes_exclude:
+    def _ensure_price_include(self):
+        taxes_exclude = self.filtered(lambda s: not s.price_include)
+        if taxes_exclude:
             raise UserError(
                 _(
                     "Tax product '%s' is price exclude. "
-                    "You must switch to include ones." % prod_taxes_exclude[0].name
+                    "You must switch to include ones." % taxes_exclude[0].name
                 )
             )
 
-    def _check_sale_line_tax_exclude(self, line_taxes, pricelist):
-        line_taxes_include = line_taxes.filtered(lambda t: t.price_include)
-        if line_taxes_include:
+    def _ensure_price_exclude(self, pricelist):
+        if any(self.mapped("price_include")):
             raise UserError(
                 _(
                     "Tax with include price with pricelist b2b '%s' "
@@ -64,45 +53,40 @@ class AccountTax(models.Model):
                 )
             )
 
-    def _map_exclude_tax(self):
-        """return a dict
-        mtax[company_id or 0][tax amount]['include'|'exclude'] = tax_id
-        """
-        mtax = defaultdict(dict)
-        prev_cpny = False
-        for tax in self.search(
-            [("type_tax_use", "=", "sale")], order="company_id ASC, price_include DESC"
-        ):
-            cpny = tax.company_id
-            if cpny != prev_cpny:
-                tamount = defaultdict(dict)
-            if tax.price_include:
-                tamount[tax.amount].update({"include": tax.id})
-            else:
-                tamount[tax.amount].update({"exclude": tax.id})
-            if tax.amount in mtax[tax.company_id.id or 0]:
-                mtax[tax.company_id.id or 0][tax.amount].update(tamount[tax.amount])
-            else:
-                mtax[tax.company_id.id or 0][tax.amount] = tamount[tax.amount]
-            prev_cpny = cpny
-        return mtax
+    def get_equivalent_tax_exc(self):
+        taxes = self.browse(False)
+        for record in self:
+            taxes |= self.browse(self._get_equivalent_tax_exc_id(record.id))
+        return taxes
 
-    def _get_substitute_taxes(self, record, taxes, map_tax):
-        # TODO shortify this code
-        if record._name == "sale.order.line":
-            cpny = record.order_id.company_id.id or self.company_id.id or 0
-        elif record._name == "account.move.line":
-            cpny = record.move_id.company_id.id or self.company_id.id or 0
+    @tools.ormcache("tax_inc_id")
+    def _get_equivalent_tax_exc_id(self, tax_inc_id):
+        tax_inc = self.browse(tax_inc_id)
+        tax_inc._ensure_price_include()
+        tax = self.search(
+            [
+                ("price_include", "=", False),
+                ("company_id", "=", tax_inc.company_id.id),
+                ("type_tax_use", "=", tax_inc.type_tax_use),
+                ("amount", "=", tax_inc.amount),
+            ]
+        )
+        if not tax:
+            raise UserError(
+                _("Equivalent price exclude tax for '%s' is missing" % tax_inc.name)
+            )
         else:
-            raise UserError(_("No other model supported than sale and invoice"))
-        # TODO end
-        mytaxes = []
-        for tax in taxes:
-            if map_tax[cpny].get(tax.amount):
-                if map_tax[cpny][tax.amount].get("exclude"):
-                    mytaxes.append(map_tax[cpny][tax.amount]["exclude"])
-                else:
-                    mytaxes.append(tax.id)
-            else:
-                mytaxes.append(tax.id)
-        return self.env["account.tax"].browse(mytaxes)
+            return tax.id
+
+    def write(self, vals):
+        self.clear_caches()
+        return super().write(vals)
+
+    @api.model_create_multi
+    def create(self, list_vals):
+        self.clear_caches()
+        return super().create(list_vals)
+
+    def unlink(self):
+        self.clear_caches()
+        return super().unlink()
