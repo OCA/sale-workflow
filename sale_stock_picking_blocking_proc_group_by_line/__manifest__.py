0e8a5d1e948e555a83c0f3cdc6242975e3b83b0f
Juany Davila <juanyadavilap@gmail.com>
2022-05-18 13:51:36 -0500

    [MIG] sale_stock_picking_blocking_proc_group_by_line: Migration to 14.0

--- sale_stock_picking_blocking_proc_group_by_line/__manifest__.py
+++ sale_stock_picking_blocking_proc_group_by_line/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2017-18 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Stock Picking Blocking Procurement Group By Line",
     "summary": "Module that allows module sale_stock_picking_blocking to work "
     "with sale_procurement_group_by_line",
-    "version": "11.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
     "depends": [
--- sale_stock_picking_blocking_proc_group_by_line/models/sale_order.py
+++ sale_stock_picking_blocking_proc_group_by_line/models/sale_order.py
@@ -1,14 +1,12 @@
-# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# Copyright 2017-18 ForgeFlow S.L.(http://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.multi
     def _action_launch_procurement_rule(self):
         return super(
             SaleOrderLine,
