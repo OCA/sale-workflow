8d620c4e9a3ed2d607bcc2b8a654e020d701bf33
manu <manuel.regidor@sygel.es>
2022-06-01 13:48:23 +0200

    [MIG]sale_discount_display_amount: Migration to 15.0

--- sale_discount_display_amount/__manifest__.py
+++ sale_discount_display_amount/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         This addon intends to display the amount of the discount computed on
         sale_order_line and sale_order level""",
-    "version": "14.0.1.0.3",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
--- sale_discount_display_amount/tests/test_discount_display_amount.py
+++ sale_discount_display_amount/tests/test_discount_display_amount.py
@@ -19,8 +19,21 @@ class TestDiscountDisplay(TransactionCase):
 
         first_line = so.order_line[0]
         first_line.discount = 10
-
         self.assertAlmostEqual(first_line.price_total_no_discount, 35.36)
         self.assertAlmostEqual(first_line.discount_total, 3.53)
         self.assertAlmostEqual(so.discount_total, 3.53)
         self.assertAlmostEqual(so.price_total_no_discount, 35.36)
+
+    def test_sale_without_discount_value(self):
+        product2 = self.env["product.product"].create(
+            {"name": "Product TEST", "type": "consu"}
+        )
+        customer2 = self.env["res.partner"].create(
+            {"name": "Customer TEST", "is_company": False, "email": "test@tes.ttest"}
+        )
+        so2 = self.env["sale.order"].create({"partner_id": customer2.id})
+        self.env["sale.order.line"].create(
+            {"order_id": so2.id, "product_id": product2.id, "price_unit": 30.75}
+        )
+        first_line = so2.order_line[0]
+        self.assertEqual(first_line.price_total_no_discount, first_line.price_total)
