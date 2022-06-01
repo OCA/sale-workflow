d6d322fd1fb07bcb56ced1f51c155800b878509f
manu <manuel.regidor@sygel.es>
2022-06-01 13:48:23 +0200

    [MIG]sale_discount_display_amount: Migration to 15.0

--- sale_discount_display_amount/models/sale_order.py
+++ sale_discount_display_amount/models/sale_order.py
@@ -21,11 +21,7 @@ class SaleOrder(models.Model):
         store=True,
     )
 
-    @api.model
-    def _get_compute_discount_total_depends(self):
-        return ["order_line.discount_total", "order_line.price_total_no_discount"]
-
-    @api.depends(lambda self: self._get_compute_discount_total_depends())
+    @api.depends("order_line.discount_total", "order_line.price_total_no_discount")
     def _compute_discount_total(self):
         for order in self:
             discount_total = sum(order.order_line.mapped("discount_total"))
