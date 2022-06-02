6a2f846fa8d23e00748d5cb13535092951eb395e
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-06-02 23:42:36 +0200

    [MIG] sale_stock_cancel_restriction: Migration to 15.0

--- sale_stock_cancel_restriction/__manifest__.py
+++ sale_stock_cancel_restriction/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Stock Cancel Restriction",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales Management",
