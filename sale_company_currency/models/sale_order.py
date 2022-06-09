2753b24d90b973d67b1f5ef450e6dda70a07434c
Alexei Rivera <alexeirivera87@gmail.com>
2022-06-09 15:28:55 -0400

    [IMP] sale_company_currency: black, isort, prettier

--- sale_company_currency/models/sale_order.py
+++ sale_company_currency/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,30 +5,30 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     company_currency_id = fields.Many2one(
-        'res.currency',
-        related='company_id.currency_id',
+        "res.currency",
+        related="company_id.currency_id",
         string="Company Currency",
-        readonly=True
+        readonly=True,
     )
     amount_total_curr = fields.Monetary(
         string="Total Amount",
         readonly=True,
-        help='Sale Order Amount in the company Currency',
+        help="Sale Order Amount in the company Currency",
         compute="_compute_amount_company",
-        currency_id='company_currency_id',
+        currency_id="company_currency_id",
     )
 
     @api.multi
-    @api.depends('amount_total')
+    @api.depends("amount_total")
     def _compute_amount_company(self):
         for so in self:
-            if so.state in ('sale', 'done'):
+            if so.state in ("sale", "done"):
                 so_date = so.confirmation_date
             else:
                 so_date = so.date_order
-            so.amount_total_curr = so.currency_id.with_context(
-                date=so_date).compute(
-                so.amount_total, so.company_id.currency_id)
+            so.amount_total_curr = so.currency_id.with_context(date=so_date).compute(
+                so.amount_total, so.company_id.currency_id
+            )
