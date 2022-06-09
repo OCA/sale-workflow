c466609e2a3710af8b299f7bb4fbf56373842c2c
Alexei Rivera <alexeirivera87@gmail.com>
2022-06-09 15:28:55 -0400

    [IMP] sale_company_currency: black, isort, prettier

--- sale_company_currency/__manifest__.py
+++ sale_company_currency/__manifest__.py
@@ -1,18 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Company currency in sale orders",
-    'version': "10.0.1.0.0",
-    'author': "Camptocamp, "
-              "Odoo Community Association (OCA) ",
-    'website': "https://odoo-community.org/",
-    'category': "Sale",
-    'license': "AGPL-3",
-    'depends': ["sale"],
-    'data': [
-        "views/sale_order_view.xml"
-    ],
-    'installable': True,
+    "name": "Company currency in sale orders",
+    "version": "10.0.1.0.0",
+    "author": "Camptocamp, " "Odoo Community Association (OCA) ",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
 }
