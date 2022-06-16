90aed7f0a3fa9162a61a09d2156c527b46a2e7d1
Dan Tillinghast <dan@compassfoundation.io>
2022-06-16 16:32:36 -0400

    [IMP] sale_order_action_invoice_create_hook: black, isort, prettier

--- sale_order_action_invoice_create_hook/model/sale_order.py
+++ sale_order_action_invoice_create_hook/model/sale_order.py
@@ -1,13 +1,13 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
 from odoo.tools import float_is_zero
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.model
     def _get_invoice_group_key(self, order):
@@ -27,7 +27,6 @@ class SaleOrder(models.Model):
 
     @api.model_cr
     def _register_hook(self):
-
         def new_action_invoice_create(self, grouped=False, final=False):
             """
             Create the invoice associated to the SO.
@@ -36,9 +35,8 @@ class SaleOrder(models.Model):
             :param final: if True, refunds will be generated if necessary
             :returns: list of created invoices
             """
-            if not hasattr(self, '_get_invoice_group_key'):
-                return self.action_invoice_create_original(grouped=grouped,
-                                                           final=final)
+            if not hasattr(self, "_get_invoice_group_key"):
+                return self.action_invoice_create_original(grouped=grouped, final=final)
             invoices = {}
             references = {}
 
@@ -47,9 +45,10 @@ class SaleOrder(models.Model):
             self._get_draft_invoices(invoices, references)
             # END HOOK
 
-            inv_obj = self.env['account.invoice']
-            precision = self.env['decimal.precision']. \
-                precision_get('Product Unit of Measure')
+            inv_obj = self.env["account.invoice"]
+            precision = self.env["decimal.precision"].precision_get(
+                "Product Unit of Measure"
+            )
 
             # START HOOK
             # As now from the beginning there can be invoices related to that
@@ -62,13 +61,13 @@ class SaleOrder(models.Model):
                 # We only want to create sections that
                 # have at least one invoiceable line
                 pending_section = None
-                for line in order.order_line.sorted(
-                        key=lambda l: l.qty_to_invoice < 0):
-                    if line.display_type == 'line_section':
+                for line in order.order_line.sorted(key=lambda l: l.qty_to_invoice < 0):
+                    if line.display_type == "line_section":
                         pending_section = line
                         continue
-                    if line.display_type != 'line_note' and float_is_zero(
-                            line.qty_to_invoice, precision_digits=precision):
+                    if line.display_type != "line_note" and float_is_zero(
+                        line.qty_to_invoice, precision_digits=precision
+                    ):
                         continue
                     # START HOOK
                     # Allow to check if a line should not be invoiced
@@ -79,8 +78,9 @@ class SaleOrder(models.Model):
                     # Add more flexibility in grouping key fields
                     # WAS: group_key = order.id if grouped
                     # else (order.partner_invoice_id.id, order.currency_id.id)
-                    group_key = order.id if grouped else \
-                        self._get_invoice_group_line_key(line)
+                    group_key = (
+                        order.id if grouped else self._get_invoice_group_line_key(line)
+                    )
                     # 'invoice' must be always instantiated
                     # respecting the old logic
                     if group_key in invoices:
@@ -99,28 +99,28 @@ class SaleOrder(models.Model):
                         invoice = invoices[group_key]
                         # END HOOK
                         vals = {}
-                        if order.name not in invoice. \
-                                origin.split(', '):
-                            vals['origin'] = \
-                                invoice.origin + ', ' + order.name
-                        if order.client_order_ref and order.client_order_ref \
-                                not in invoice.name.split(', ') \
-                                and order.client_order_ref != invoice.name:
-                            vals['name'] = \
-                                invoice.name + ', ' + \
-                                order.client_order_ref
+                        if order.name not in invoice.origin.split(", "):
+                            vals["origin"] = invoice.origin + ", " + order.name
+                        if (
+                            order.client_order_ref
+                            and order.client_order_ref not in invoice.name.split(", ")
+                            and order.client_order_ref != invoice.name
+                        ):
+                            vals["name"] = invoice.name + ", " + order.client_order_ref
                         invoice.write(vals)
-                    if line.qty_to_invoice > 0 or \
-                            (line.qty_to_invoice < 0 and final) or \
-                            line.display_type == 'line_note':
+                    if (
+                        line.qty_to_invoice > 0
+                        or (line.qty_to_invoice < 0 and final)
+                        or line.display_type == "line_note"
+                    ):
                         if pending_section:
                             pending_section.invoice_line_create(
-                                invoices[group_key].id,
-                                pending_section.qty_to_invoice
+                                invoices[group_key].id, pending_section.qty_to_invoice
                             )
                             pending_section = None
-                        line.invoice_line_create(invoices[group_key].id,
-                                                 line.qty_to_invoice)
+                        line.invoice_line_create(
+                            invoices[group_key].id, line.qty_to_invoice
+                        )
                         # START HOOK
                         # Change to true if new lines are added
                         new_lines = True
@@ -133,19 +133,18 @@ class SaleOrder(models.Model):
             # WAS: if not invoices:
             # Check if new lines have been added in order to determine whether
             # there are invoice lines or not
-            if not new_lines and \
-                    not self.env.context.get('no_check_lines', False):
-                raise UserError(_('There is no invoicable line.'))
+            if not new_lines and not self.env.context.get("no_check_lines", False):
+                raise UserError(_("There is no invoicable line."))
             # END HOOK
             self._modify_invoices(invoices)
 
             for invoice in invoices.values():
                 invoice.compute_taxes()
                 if not invoice.invoice_line_ids:
-                    raise UserError(_('There is no invoicable line.'))
+                    raise UserError(_("There is no invoicable line."))
                 # If invoice is negative, do a refund invoice instead
                 if invoice.amount_untaxed < 0:
-                    invoice.type = 'out_refund'
+                    invoice.type = "out_refund"
                     for line in invoice.invoice_line_ids:
                         line.quantity = -line.quantity
                 # Use additional field helper function (for account extensions)
@@ -161,18 +160,19 @@ class SaleOrder(models.Model):
                 # To keep the payment terms set on the SO
                 invoice.payment_term_id = so_payment_term_id
                 invoice.message_post_with_view(
-                    'mail.message_origin_link',
-                    values={'self': invoice, 'origin': references[invoice]},
-                    subtype_id=self.env.ref('mail.mt_note').id)
+                    "mail.message_origin_link",
+                    values={"self": invoice, "origin": references[invoice]},
+                    subtype_id=self.env.ref("mail.mt_note").id,
+                )
             return [inv.id for inv in invoices.values()]
 
-        self._patch_method('action_invoice_create', new_action_invoice_create)
+        self._patch_method("action_invoice_create", new_action_invoice_create)
 
         return super(SaleOrder, self)._register_hook()
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _prepare_invoice(self):
         return self.order_id._prepare_invoice()
