56c27d46125c67a00c1c469adccca16c78163182
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-06-16 15:33:17 +0200

    [IMP] product_supplierinfo_for_customer_sale: Add menu entry to access all product.customerinfo
    [UPD] Update product_supplierinfo_for_customer_sale.pot
    
    product_supplierinfo_for_customer_sale 15.0.1.1.0

--- product_customerinfo_sale/__manifest__.py
+++ product_customerinfo_sale/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo for Customer Sale",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "summary": "Loads in every sale order line the customer code defined "
     "in the product",
     "author": "Agile Business Group,Vauxoo,Odoo Community Association (OCA)",
@@ -12,6 +12,10 @@
     "category": "Sales Management",
     "license": "AGPL-3",
     "depends": ["sale", "product_supplierinfo_for_customer"],
-    "data": ["security/ir.model.access.csv", "views/sale_view.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_customerinfo_views.xml",
+        "views/sale_view.xml",
+    ],
     "installable": True,
 }
