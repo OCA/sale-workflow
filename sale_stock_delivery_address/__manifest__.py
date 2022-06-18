ed7a90524eeaf5599d45a37998542c60c54e4da5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-06-18 16:42:20 +0200

    [FIX] sale_stock_delivery_address: Downgrade to beta
    To comply with the maturity level constraint of its dependency.

--- sale_stock_delivery_address/__manifest__.py
+++ sale_stock_delivery_address/__manifest__.py
@@ -8,6 +8,7 @@
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales Management",
     "license": "AGPL-3",
+    "development_status": "Beta",
     "depends": [
         "sale_stock",
         "sale_procurement_group_by_line",
