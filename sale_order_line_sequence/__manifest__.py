31c948c2fe1ac8346963a1fd80af3d8ee349e4c9
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-21 17:36:25 +0200

    [UPD] Eficent -> ForgeFlow

--- sale_order_line_sequence/__manifest__.py
+++ sale_order_line_sequence/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,7 +6,7 @@
     "name": "Sale Order Line Sequence",
     "summary": "Propagates SO line sequence to invoices and stock picking.",
     "version": "13.0.1.0.0",
-    "author": "Eficent, Serpent CS, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Serpent CS, Odoo Community Association (OCA)",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
