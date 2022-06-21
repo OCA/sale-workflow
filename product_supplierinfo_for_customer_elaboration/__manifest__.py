b5e0e1ffe7c34ffc0f222021b43c7919ef36e190
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-06-21 08:54:57 +0200

    [ADD] product_supplierinfo_for_customer_elaboration: New module to set default elaborations

---
+++ product_supplierinfo_for_customer_elaboration/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Supplierinfo for Customers Elaboration",
+    "summary": "Allows to define default elaborations and elaboration notes on product"
+    " customerinfos",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": ["product_supplierinfo_for_customer_sale", "sale_elaboration"],
+    "data": ["views/product_views.xml"],
+    "installable": True,
+}
