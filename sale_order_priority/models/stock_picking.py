7b250a38818ef6175b6d2881971eabcf13f86dcc
Juany Davila <juanyadavilap@gmail.com>
2022-06-27 23:52:09 -0500

    [MIG] sale_order_priority: Migration to 15.0

--- sale_order_priority/models/stock_picking.py
+++ sale_order_priority/models/stock_picking.py
@@ -4,8 +4,8 @@ from odoo import models
 class Picking(models.Model):
     _inherit = "stock.picking"
 
-    def action_assign(self):
-        res = super(Picking, self).action_assign()
-        picking_priority = self.env.context.get("sale_priority")
-        self.priority = picking_priority if picking_priority else 0
+    def action_confirm(self):
+        res = super().action_confirm()
+        picking_priority = self.env.context.get("sale_priority", False)
+        self.write({"priority": picking_priority or 0})
         return res
