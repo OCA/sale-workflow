34a2efbf0c03cbc5b8d87d6577bb1430f7d0f093
SilvioC2C <silvio.gregorini@camptocamp.com>
2022-07-07 17:17:15 +0200

    [MIG] sale_invoice_no_mail: Migration to 14.0

--- sale_invoice_no_mail/__manifest__.py
+++ sale_invoice_no_mail/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Invoice No Mail",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
     "author": "Camptocamp SA," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
