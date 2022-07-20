162865cd52bb0369182adcefecbaae3d73b9b39a
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-07-20 12:55:48 +0530

    [MIG] sale_shipping_info_helper: Migration to 15.0

--- sale_shipping_info_helper/__manifest__.py
+++ sale_shipping_info_helper/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Sale shipping info helper",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
