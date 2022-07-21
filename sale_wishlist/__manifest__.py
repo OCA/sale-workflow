4331fb1550a0005fe3498440fef55384bfe141bd
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-07-21 10:05:20 +0530

    [MIG] sale_wishlist: Migration to 15.0
    [UPD] Update sale_wishlist.pot
    
    [UPD] README.rst

--- sale_wishlist/__manifest__.py
+++ sale_wishlist/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sale Wishlist",
     "summary": """
         Handle sale wishlist for partners""",
-    "version": "14.0.1.0.2",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
