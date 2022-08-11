5a5f43246b83534e72ab95367a16bf22931fce29
Karl Southern <karl@theangryangel.co.uk>
2022-08-11 09:56:07 +0100

    [MIG] sale_order_line_menu: Migration to 12.0

--- sale_order_line_menu/__manifest__.py
+++ sale_order_line_menu/__manifest__.py
@@ -3,17 +3,15 @@
 
 {
     "name": "Sale Order Line Menu",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "summary": "Adds a Sale Order Lines Menu",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "depends": ["sale"],
     "category": "Sales/Sales",
-    "data": [
-        "views/sale_order_line_views.xml",
-    ],
+    "data": ["views/sale_order_line_views.xml"],
     "installable": True,
-    "maintainer": "dreispt",
+    "maintainers": ["dreispt"],
     "development_status": "Beta",
 }
