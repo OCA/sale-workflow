c3349863382e1a5b24160839e0a701c5115f00c9
Aritz Olea <ao@landoo.es>
2022-08-12 12:11:14 +0200

    [MIG] sale_cancel_reason: Migration to 14.0

--- sale_cancel_reason/wizard/cancel_reason.py
+++ sale_cancel_reason/wizard/cancel_reason.py
@@ -1,4 +1,5 @@
 # © 2013 Guewen Baconnier, Camptocamp SA
+# © 2022 Landoo Sistemas de Informacion SL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
@@ -7,21 +8,12 @@ class SaleOrderCancel(models.TransientModel):
 
     """Ask a reason for the sale order cancellation."""
 
-    _name = "sale.order.cancel"
-    _description = __doc__
+    _inherit = "sale.order.cancel"
 
     reason_id = fields.Many2one(
         "sale.order.cancel.reason", string="Reason", required=True
     )
 
-    def confirm_cancel(self):
-        self.ensure_one()
-        act_close = {"type": "ir.actions.act_window_close"}
-        sale_ids = self._context.get("active_ids")
-        if sale_ids is None:
-            return act_close
-        assert len(sale_ids) == 1, "Only 1 sale ID expected"
-        sale = self.env["sale.order"].browse(sale_ids)
-        sale.cancel_reason_id = self.reason_id.id
-        sale.action_cancel()
-        return act_close
+    def action_cancel(self):
+        self.order_id.cancel_reason_id = self.reason_id
+        return super(SaleOrderCancel, self).action_cancel()
