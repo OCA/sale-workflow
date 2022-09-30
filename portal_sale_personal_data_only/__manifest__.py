80cee8b7d94a342d627fb908f3b96e7e5b67d844
Miquel Raïch <miquel.raich@forgeflow.com>
2022-09-30 11:46:10 +0200

    [FIX] pre-commit, pylint

--- portal_sale_personal_data_only/__manifest__.py
+++ portal_sale_personal_data_only/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Portal Sale Personal Data Only',
-    'description': 'Allow portal users to see their own documents',
+    'summary': 'Allow portal users to see their own documents',
     'version': '10.0.1.0.1',
     'category': 'Sale',
     'author': 'Tecnativa,'
