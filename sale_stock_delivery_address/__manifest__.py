1c0eedb88bf889db84a74992ca5999914bfef091
Denis Roussel <denis.roussel@acsone.eu>
2022-09-30 08:44:06 +0200

    [FIX] sale_stock_delivery_address: Swith to AGPL du to sale_procurement_group_by_line dependency

--- sale_stock_delivery_address/__manifest__.py
+++ sale_stock_delivery_address/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 ForgeFlow S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Sale Stock Sourcing Address",
@@ -7,7 +7,7 @@
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales Management",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": [
         "sale_stock",
         "sale_procurement_group_by_line",
