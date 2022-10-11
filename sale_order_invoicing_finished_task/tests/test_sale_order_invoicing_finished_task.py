9c9e23ba2aeb62b28b21b117893ee95ff8388100
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2022-10-11 21:25:10 +0200

    [FIX] Update deprecated unit tests

--- sale_order_invoicing_finished_task/tests/test_sale_order_invoicing_finished_task.py
+++ sale_order_invoicing_finished_task/tests/test_sale_order_invoicing_finished_task.py
@@ -5,7 +5,7 @@ from odoo.exceptions import ValidationError
 from odoo.tests import common
 
 
-class TestInvoicefinishedTask(common.SavepointCase):
+class TestInvoicefinishedTask(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
