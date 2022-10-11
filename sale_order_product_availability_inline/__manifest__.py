8c5ea9a5f2d73c1053cbae4ff61abc6e837e85d5
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2022-10-11 21:25:10 +0200

    [FIX] Update deprecated unit tests

--- sale_order_product_availability_inline/__manifest__.py
+++ sale_order_product_availability_inline/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Product Availability Inline",
     "summary": "Show product availability in sales order line product " "drop-down.",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "development_status": "Mature",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
