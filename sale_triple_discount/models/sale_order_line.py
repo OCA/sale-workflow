4267247dbe3f45299c3375e2f0c3d5fc6cb70556
manu <manuel.regidor@sygel.es>
2022-10-19 15:37:39 +0200

    [MIG]sale_triple_discount: Migration to 15.0
    [UPD] Update sale_triple_discount.pot
    
    [UPD] README.rst

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -11,6 +11,26 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    discount2 = fields.Float(
+        string="Disc. 2 (%)",
+        digits="Discount",
+        default=0.0,
+    )
+    discount3 = fields.Float(
+        string="Disc. 3 (%)",
+        digits="Discount",
+        default=0.0,
+    )
+    discounting_type = fields.Selection(
+        selection=[("additive", "Additive"), ("multiplicative", "Multiplicative")],
+        default="multiplicative",
+        required=True,
+        help="Specifies whether discounts should be additive "
+        "or multiplicative.\nAdditive discounts are summed first and "
+        "then applied.\nMultiplicative discounts are applied sequentially.\n"
+        "Multiplicative discounts are default",
+    )
+
     def _get_final_discount(self):
         self.ensure_one()
         if self.discounting_type == "additive":
@@ -19,13 +39,13 @@ class SaleOrderLine(models.Model):
             return self._multiplicative_discount()
         else:
             raise ValidationError(
-                _("Sale order line %s has unknown discounting type %s")
-                % (self.name, self.discounting_type)
+                _("Sale order line %(name)s has unknown discounting type %(dic_type)s")
+                % {"name": self.name, "disc_type": self.discounting_type}
             )
 
     def _additive_discount(self):
         self.ensure_one()
-        discount = sum(getattr(self, x) or 0.0 for x in self._discount_fields())
+        discount = sum(self[x] or 0.0 for x in self._discount_fields())
         if discount <= 0:
             return 0
         elif discount >= 100:
@@ -46,68 +66,37 @@ class SaleOrderLine(models.Model):
     @api.depends("discount2", "discount3", "discounting_type")
     def _compute_amount(self):
         prev_values = self.triple_discount_preprocess()
-        super()._compute_amount()
+        res = super()._compute_amount()
         self.triple_discount_postprocess(prev_values)
-
-    discount2 = fields.Float(
-        string="Disc. 2 (%)",
-        digits="Discount",
-        default=0.0,
-    )
-    discount3 = fields.Float(
-        string="Disc. 3 (%)",
-        digits="Discount",
-        default=0.0,
-    )
-    discounting_type = fields.Selection(
-        string="Discounting type",
-        selection=[("additive", "Additive"), ("multiplicative", "Multiplicative")],
-        default="multiplicative",
-        required=True,
-        help="Specifies whether discounts should be additive "
-        "or multiplicative.\nAdditive discounts are summed first and "
-        "then applied.\nMultiplicative discounts are applied sequentially.\n"
-        "Multiplicative discounts are default",
-    )
+        return res
 
     _sql_constraints = [
         (
             "discount2_limit",
             "CHECK (discount2 <= 100.0)",
-            "Discount 2 must be lower than 100%.",
+            "Discount 2 must be lower or equal than 100%.",
         ),
         (
             "discount3_limit",
             "CHECK (discount3 <= 100.0)",
-            "Discount 3 must be lower than 100%.",
+            "Discount 3 must be lower or equal than 100%.",
         ),
     ]
-
-    def _get_triple_discount(self):
-        """Get the discount that is equivalent to the subsequent application
-        of discount, discount2 and discount3"""
-        discount_factor = 1.0
-        for discount in [self.discount, self.discount2, self.discount3]:
-            discount_factor *= (100.0 - discount) / 100.0
-        return 100.0 - (discount_factor * 100.0)
 
     def _prepare_invoice_line(self, **kwargs):
         res = super()._prepare_invoice_line(**kwargs)
         res.update({"discount2": self.discount2, "discount3": self.discount3})
         return res
 
-    @api.depends("discount2", "discount3")
-    def _get_price_reduce(self):
-        prev_values = self.triple_discount_preprocess()
-        super()._get_price_reduce()
-        self.triple_discount_postprocess(prev_values)
-
     def triple_discount_preprocess(self):
-        """Save the values of the discounts in a dictionary,
+        """Prepare data for post processing.
+
+        Save the values of the discounts in a dictionary,
         to be restored in postprocess.
         Resetting discount2 and discount3 to 0.0 avoids issues if
         this method is called multiple times.
         Updating the cache provides consistency through recomputations."""
+
         prev_values = dict()
         self.invalidate_cache(
             fnames=["discount", "discount2", "discount3"], ids=self.ids
@@ -130,7 +119,9 @@ class SaleOrderLine(models.Model):
     @api.model
     def triple_discount_postprocess(self, prev_values):
         """Restore the discounts of the lines in the dictionary prev_values.
+
         Updating the cache provides consistency through recomputations."""
+
         self.invalidate_cache(
             fnames=["discount", "discount2", "discount3"],
             ids=[line.id for line in list(prev_values.keys())],
