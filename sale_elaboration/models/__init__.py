768ea81e41a97b3bf1de045e50683b8add3c3409
sergio-teruel <sergio.teruel@tecnativa.com>
2022-10-23 09:38:53 +0200

    [FIX] sale_elaboration: Fix delivery slip report. Add config setting to allow users display elaboration notes on delivery slips

--- sale_elaboration/models/__init__.py
+++ sale_elaboration/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import product
 from . import product_elaboration
+from . import res_config_settings
 from . import sale_order
 from . import stock_picking
