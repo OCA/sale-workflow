a262a8c218085b22958b9dc0e0abc4b0c2e8dfc9
Kiril Vangelovski <kiril@lambda-is.com>
2022-10-25 13:02:37 +0200

    [IMP] sale_global_discount: black, isort, prettier

--- sale_global_discount/models/sale_order.py
+++ sale_global_discount/models/sale_order.py
@@ -1,10 +1,7 @@
 # Copyright 2020 Tecnativa - David Vidal
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from functools import partial
-
 from odoo import _, api, exceptions, fields, models
-from odoo.tools.misc import formatLang
 
 
 class SaleOrder(models.Model):
