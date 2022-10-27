8d3afc5ead2c95df248e3b15ed25ec83297e76a7
augusto-weiss <awe@adhoc.com.ar>
2022-10-27 14:37:29 -0300

    [MIG] sale_order_type: Migration to 16.0

--- sale_order_type/wizards/sale_make_invoice_advance.py
+++ sale_order_type/wizards/sale_make_invoice_advance.py
@@ -7,10 +7,8 @@ from odoo import models
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
 
-    def _prepare_invoice_values(self, order, name, amount, so_line):
-        res = super(SaleAdvancePaymentInv, self)._prepare_invoice_values(
-            order, name, amount, so_line
-        )
+    def _prepare_invoice_values(self, order, so_line):
+        res = super(SaleAdvancePaymentInv, self)._prepare_invoice_values(order, so_line)
         if order.type_id.journal_id:
             res["journal_id"] = order.type_id.journal_id.id
         if order.type_id:
