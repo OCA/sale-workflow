5d0116e8a5e0a6c0da21167ea8f24f835ed1addc
k.joel <joelkabamba35@gmail.com>
2022-11-11 23:59:36 +0100

    [MIG] partner_sale_pivot: Migration to 16.0

--- partner_sale_pivot/__manifest__.py
+++ partner_sale_pivot/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner Sale Pivot",
     "summary": "Sales analysis from customer form view",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
