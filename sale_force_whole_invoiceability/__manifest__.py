9759d8e81ce36543432374a143db07db6a6b00fd
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-14 12:00:13 +0100

    [MIG] sale_force_whole_invoiceability: Migration to 15.0
    TT37312

--- sale_force_whole_invoiceability/__manifest__.py
+++ sale_force_whole_invoiceability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Force Whole Invoiceability",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sales",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
