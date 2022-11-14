7d31ee8f869c17e86c2ff8a425b9cb15f93659d6
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-14 12:00:13 +0100

    [MIG] sale_force_whole_invoiceability: Migration to 15.0
    TT37312

--- sale_force_whole_invoiceability/tests/test_sale_force_whole.py
+++ sale_force_whole_invoiceability/tests/test_sale_force_whole.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.exceptions import UserError
-from odoo.tests import Form, common
+from odoo.tests import Form, common, new_test_user, users
 
 
-class SaleOrderForceInvoiceability(common.SavepointCase):
+class SaleOrderForceInvoiceability(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -14,10 +14,16 @@ class SaleOrderForceInvoiceability(common.SavepointCase):
         cls.products |= cls.env["product.product"].create(
             {"name": "test product 2", "invoice_policy": "delivery"}
         )
-        cls.user_demo = cls.env.ref("base.user_demo")
-        cls.group_sales_manager = cls.env.ref("sales_team.group_sale_manager")
-        # Prevent demo user being in sales manager group
-        cls.group_sales_manager.write({"users": [(3, cls.user_demo.id)]})
+        new_test_user(
+            cls.env,
+            login="test-sale-user",
+            groups="sales_team.group_sale_salesman_all_leads",
+        )
+        new_test_user(
+            cls.env,
+            login="test-sale-manager",
+            groups="sales_team.group_sale_manager",
+        )
         sale = Form(cls.env["sale.order"])
         sale.partner_id = cls.env["res.partner"].create({"name": "Mr. Odoo"})
         for product in cls.products:
@@ -26,34 +32,32 @@ class SaleOrderForceInvoiceability(common.SavepointCase):
                 order_line.product_uom_qty = 5
         cls.sale_order = sale.save()
 
+    @users("admin", "test-sale-manager")
     def test_01_force_invoiceability_draft(self):
         """Try to force invoiceability"""
         # The order is still not in `sale` mode so we can't force it
+        self.sale_order = self.env["sale.order"].browse(self.sale_order.id)
         with self.assertRaises(UserError) as cm:
             self.sale_order.force_lines_to_invoice_policy_order()
         self.assertEqual(
             "You can't perform this action over a sale order in this state",
-            cm.exception.name,
+            cm.exception.args[0],
         )
 
+    @users("test-sale-user")
     def test_02_force_invoiceability_no_permissions(self):
         # Only users with sales manager permission can force the lines
+        self.sale_order = self.env["sale.order"].browse(self.sale_order.id)
         self.sale_order.action_confirm()
         with self.assertRaises(UserError) as cm:
-            self.sale_order.with_user(
-                self.user_demo.id
-            ).force_lines_to_invoice_policy_order()
+            self.sale_order.force_lines_to_invoice_policy_order()
         self.assertEqual(
             "Only Sales Managers are allowed to force the lines to invoice",
-            cm.exception.name,
+            cm.exception.args[0],
         )
-        # Once we grant permission everything goes ok
-        self.group_sales_manager.users |= self.user_demo
-        self.sale_order.with_user(
-            self.user_demo.id
-        ).force_lines_to_invoice_policy_order()
 
     def test_03_force_invoiceable_lines(self):
+        self.sale_order = self.env["sale.order"].browse(self.sale_order.id)
         self.sale_order.action_confirm()
         # Only the product with *Ordered quantities* policy has its line
         # quantities `to invoice`
@@ -81,5 +85,9 @@ class SaleOrderForceInvoiceability(common.SavepointCase):
         self.assertEqual(
             "You can't perform this action once the order has been invoiced "
             "once already",
-            cm.exception.name,
+            cm.exception.args[0],
         )
+
+    @users("test-sale-manager")
+    def test_03_force_invoiceable_lines_extra(self):
+        self.test_03_force_invoiceable_lines()
