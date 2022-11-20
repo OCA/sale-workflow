f99d3d2eed0c7d4a8e940592cd245ae4cd395354
Rad0van <radovan@skolnik.info>
2022-11-20 22:38:44 +0100

    [MIG] sale_fixed_discount: Migration to 15.0

--- sale_fixed_discount/models/sale_order.py
+++ sale_fixed_discount/models/sale_order.py
@@ -1,6 +1,8 @@
 # Copyright 2017-20 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import json
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -55,7 +57,50 @@ class SaleOrderLine(models.Model):
             line.update(vals[line])
         return res
 
-    def _prepare_invoice_line(self):
+    def _prepare_invoice_line(self, **optional_values):
         res = super(SaleOrderLine, self)._prepare_invoice_line()
         res.update({"discount_fixed": self.discount_fixed})
         return res
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.depends(
+        "order_line.tax_id", "order_line.price_unit", "amount_total", "amount_untaxed"
+    )
+    def _compute_tax_totals_json(self):
+        def compute_taxes(order_line):
+            real_price = order_line.price_unit * (
+                1 - (order_line.discount or 0.0) / 100.0
+            ) - (order_line.discount_fixed or 0.0)
+            twicked_price = real_price / (1 - (order_line.discount or 0.0) / 100.0)
+            price = (
+                order_line.price_unit * (1 - (order_line.discount or 0.0) / 100.0)
+                if not order_line.discount_fixed
+                else twicked_price
+            )
+            order = order_line.order_id
+            return order_line.tax_id._origin.compute_all(
+                price,
+                order.currency_id,
+                order_line.product_uom_qty,
+                product=order_line.product_id,
+                partner=order.partner_shipping_id,
+            )
+
+        account_move = self.env["account.move"]
+        for order in self:
+            tax_lines_data = (
+                account_move._prepare_tax_lines_data_for_totals_from_object(
+                    order.order_line, compute_taxes
+                )
+            )
+            tax_totals = account_move._get_tax_totals(
+                order.partner_id,
+                tax_lines_data,
+                order.amount_total,
+                order.amount_untaxed,
+                order.currency_id,
+            )
+            order.tax_totals_json = json.dumps(tax_totals)
