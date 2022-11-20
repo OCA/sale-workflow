3928f722fe802a37c20e117a4f2865354e0e2bda
Rad0van <radovan@skolnik.info>
2022-11-20 22:38:44 +0100

    [MIG] sale_fixed_discount: Migration to 15.0

--- sale_fixed_discount/__manifest__.py
+++ sale_fixed_discount/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Fixed Discount",
     "summary": "Allows to apply fixed amount discounts in sales orders.",
-    "version": "13.0.1.1.1",
+    "version": "15.0.1.1.1",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
