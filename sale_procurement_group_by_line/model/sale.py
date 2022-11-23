6d197aa9b30f94ac8ba3693642cc9ea217e6cc2c
Miquel Raïch <miquel.raich@forgeflow.com>
2022-11-23 17:26:07 +0100

    [FIX] sale_procurement_group_by_line: adapt to v14

--- sale_procurement_group_by_line/model/sale.py
+++ sale_procurement_group_by_line/model/sale.py
@@ -3,22 +3,20 @@
 # © 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 from odoo.tools.float_utils import float_compare
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    @api.model
-    def _prepare_procurement_group_by_line(self, line):
-        """Hook to be able to use line data on procurement group"""
-        return {"name": line.order_id.name}
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
+
+    procurement_group_id = fields.Many2one(
+        "procurement.group", "Procurement group", copy=False
+    )
+
+    def _get_procurement_group(self):
+        super()._get_procurement_group()
+        return self.procurement_group_id or False
 
     def _get_procurement_group_key(self):
         """Return a key with priority to be used to regroup lines in multiple
@@ -34,7 +32,7 @@ class SaleOrderLine(models.Model):
         precision = self.env["decimal.precision"].precision_get(
             "Product Unit of Measure"
         )
-        errors = []
+        procurements = []
         groups = {}
         if not previous_product_uom_qty:
             previous_product_uom_qty = {}
@@ -49,9 +47,10 @@ class SaleOrderLine(models.Model):
             ):
                 continue
 
+            group_id = line._get_procurement_group()
+
             # Group the sales order lines with same procurement group
             # according to the group key
-            group_id = line.procurement_group_id or False
             for order_line in line.order_id.order_line:
                 g_id = order_line.procurement_group_id or False
                 if g_id:
@@ -60,14 +59,7 @@ class SaleOrderLine(models.Model):
                 group_id = groups.get(line._get_procurement_group_key())
 
             if not group_id:
-                vals = line.order_id._prepare_procurement_group_by_line(line)
-                vals.update(
-                    {
-                        "move_type": line.order_id.picking_policy,
-                        "sale_id": line.order_id.id,
-                        "partner_id": line.order_id.partner_shipping_id.id,
-                    }
-                )
+                vals = line._prepare_procurement_group_vals()
                 group_id = self.env["procurement.group"].create(vals)
             else:
                 # In case the procurement group is already created and the
@@ -92,33 +84,23 @@ class SaleOrderLine(models.Model):
             product_qty, procurement_uom = line_uom._adjust_uom_quantities(
                 product_qty, quant_uom
             )
-
-            try:
-                procurements = []
-                procurements.append(
-                    self.env["procurement.group"].Procurement(
-                        line.product_id,
-                        product_qty,
-                        procurement_uom,
-                        line.order_id.partner_shipping_id.property_stock_customer,
-                        line.name,
-                        line.order_id.name,
-                        line.order_id.company_id,
-                        values,
-                    )
+            procurements.append(
+                self.env["procurement.group"].Procurement(
+                    line.product_id,
+                    product_qty,
+                    procurement_uom,
+                    line.order_id.partner_shipping_id.property_stock_customer,
+                    line.name,
+                    line.order_id.name,
+                    line.order_id.company_id,
+                    values,
                 )
-                self.env["procurement.group"].run(procurements)
-                # We store the procured quantity in the UoM of the line to avoid
-                # duplicated procurements, specially for dropshipping and kits.
-                previous_product_uom_qty[line.id] = line.product_uom_qty
-            except UserError as error:
-                errors.append(error.args[0])
-        if errors:
-            raise UserError("\n".join(errors))
-        return super()._action_launch_stock_rule(
-            previous_product_uom_qty=previous_product_uom_qty
-        )
-
-    procurement_group_id = fields.Many2one(
-        "procurement.group", "Procurement group", copy=False
-    )
+            )
+            # We store the procured quantity in the UoM of the line to avoid
+            # duplicated procurements, specially for dropshipping and kits.
+            previous_product_uom_qty[line.id] = line.product_uom_qty
+        if procurements:
+            self.env["procurement.group"].run(procurements)
+        return super(
+            SaleOrderLine, self.with_context(sale_group_by_line=True)
+        )._action_launch_stock_rule(previous_product_uom_qty=previous_product_uom_qty)
