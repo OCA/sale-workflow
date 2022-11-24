a11f9051ea156f394728f5cf766f0c71b643cda3
Florian da Costa <florian.dacosta@akretion.com>
2022-11-24 21:12:56 +0100

    [MIG][16.0] Migrate sale_order_lot_selection to v16

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Lot Selection",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "author": "Odoo Community Association (OCA), Agile Business Group",
     "website": "https://github.com/OCA/sale-workflow",
