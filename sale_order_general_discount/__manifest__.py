2d9fa50d97d57831331ff1bfdca522e6369c7577
Stefan <stefan.ungureanu@tecnativa.com>
2022-11-24 13:00:08 +0100

    [MIG] sale_order_general_discount: Migration to 15.0

--- sale_order_general_discount/__manifest__.py
+++ sale_order_general_discount/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order General Discount",
     "summary": "General discount per sale order",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
