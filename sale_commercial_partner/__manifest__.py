5cb5e8f12172c4f388810644eab06b3a486eb2cb
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-12 23:25:38 +0100

    [MIG] sale_commercial_partner to v16

--- sale_commercial_partner/__manifest__.py
+++ sale_commercial_partner/__manifest__.py
@@ -1,16 +1,18 @@
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sale Commercial Partner",
     "summary": "Add stored related field 'Commercial Entity' on sale orders",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
     "license": "AGPL-3",
     "contributors": ["Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>"],
     "depends": ["sale"],
-    "data": ["views/sale_views.xml"],
+    "data": ["views/sale_order.xml", "views/sale_report.xml"],
     "installable": True,
 }
