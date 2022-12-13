f4130ced16c4fea95d3bc17f471bf8481445525b
Carlos Dauden <carlos.dauden@tecnativa.com>
2022-12-13 18:10:13 +0100

    [FIX] sale_planner_calendar: Use same domain in Sales Follower Orders and Sales Follower Order Lines TT40792

---
+++ sale_planner_calendar/migrations/15.0.1.0.1/post-migration.py
@@ -0,0 +1,10 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "sale_planner_calendar", "migrations/15.0.1.0.1/noupdate_changes.xml"
+    )
