8f8ea741ba18cc8fb96108f19d2e5ca6885c8b0a
Kiril Vangelovski <kiril@lambda-is.com>
2022-12-22 22:55:45 +0100

    [FIX] sale_global_discount: compute the taxes widget with global discounts applied

--- sale_global_discount/models/sale_order.py
+++ sale_global_discount/models/sale_order.py
@@ -2,6 +2,7 @@
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from functools import partial
+import json
 
 from odoo import _, api, exceptions, fields, models
 from odoo.tools.misc import formatLang
@@ -144,3 +145,36 @@ class SaleOrder(models.Model):
                 {"global_discount_ids": [(6, 0, self.global_discount_ids.ids)]}
             )
         return invoice_vals
+
+    def _compute_tax_totals_json(self):
+        """OVERRIDEN: add global discount in the tax calculation."""
+        super()._compute_tax_totals_json()
+
+        def compute_taxes(order_line):
+            price = order_line.price_unit * (1 - (order_line.discount or 0.0) / 100.0)
+            discounts = order_line.order_id.global_discount_ids.mapped("discount")
+            price = self.get_discounted_global(price, discounts.copy())
+            order = order_line.order_id
+            return order_line.tax_id._origin.compute_all(
+                price,
+                order.currency_id,
+                order_line.product_uom_qty,
+                product=order_line.product_id,
+                partner=order.partner_shipping_id,
+            )
+
+        account_move = self.env["account.move"]
+        for order in self.filtered("global_discount_ids"):
+            tax_lines_data = (
+                account_move._prepare_tax_lines_data_for_totals_from_object(
+                    order.order_line, compute_taxes
+                )
+            )
+            tax_totals = account_move._get_tax_totals(
+                order.partner_id,
+                tax_lines_data,
+                order.amount_total,
+                order.amount_untaxed,
+                order.currency_id,
+            )
+            order.tax_totals_json = json.dumps(tax_totals)
