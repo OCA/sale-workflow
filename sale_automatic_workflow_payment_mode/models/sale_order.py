117c58859dd2b664876155ccfa665351c405acef
Florian da Costa <florian.dacosta@akretion.com>
2022-12-30 19:45:41 +0100

    [16.0][MIG] Migrate sale_automatic_workflow_payment_mode to v16

--- sale_automatic_workflow_payment_mode/models/sale_order.py
+++ sale_automatic_workflow_payment_mode/models/sale_order.py
@@ -1,13 +1,18 @@
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.onchange("payment_mode_id")
-    def onchange_payment_mode_set_workflow(self):
-        if self.payment_mode_id.workflow_process_id:
-            self.workflow_process_id = self.payment_mode_id.workflow_process_id
+    workflow_process_id = fields.Many2one(
+        compute="_compute_workflow_process_id", store=True, readonly=False
+    )
+
+    @api.depends("payment_mode_id")
+    def _compute_workflow_process_id(self):
+        for sale in self:
+            if sale.payment_mode_id.workflow_process_id:
+                sale.workflow_process_id = sale.payment_mode_id.workflow_process_id.id
