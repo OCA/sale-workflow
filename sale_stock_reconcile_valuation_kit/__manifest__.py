cb48214d9de16093f14bd7e10d1c0e826a2c6318
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2023-01-06 19:51:24 +0100

    [ADD] sale_stock_reconcile_valuation_kit

---
+++ sale_stock_reconcile_valuation_kit/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ForgeFlow, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Sale Stock Reconcile Valuation Kit",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales Management",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "depends": [
+        "sale_stock",
+        "account_move_line_sale_info",  # OCA/account-financial-tools
+    ],
+    "installable": True,
+}
