7123539060b612fd1b96624071e111b0cb33db5c
sergio-teruel <sergio.teruel@tecnativa.com>
2023-01-11 05:54:09 +0100

    [IMP] sale_order_secondary_unit: Compatibility with product variants TT40323

--- sale_order_secondary_unit/tests/test_sale_order_secondary_unit.py
+++ sale_order_secondary_unit/tests/test_sale_order_secondary_unit.py
@@ -1,6 +1,6 @@
 # Copyright 2018-2020 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import TransactionCase, tagged
+from odoo.tests import Form, TransactionCase, tagged
 
 
 @tagged("post_install", "-at_install")
@@ -11,11 +11,19 @@ class TestSaleOrderSecondaryUnit(TransactionCase):
         cls.product_uom_kg = cls.env.ref("uom.product_uom_kgm")
         cls.product_uom_gram = cls.env.ref("uom.product_uom_gram")
         cls.product_uom_unit = cls.env.ref("uom.product_uom_unit")
+        cls.price_list = cls.env["product.pricelist"].create(
+            {"name": "price list for test"}
+        )
         cls.product = cls.env["product.product"].create(
             {
                 "name": "test",
                 "uom_id": cls.product_uom_kg.id,
                 "uom_po_id": cls.product_uom_kg.id,
+            }
+        )
+        # Set secondary uom on product template
+        cls.product.product_tmpl_id.write(
+            {
                 "secondary_uom_ids": [
                     (
                         0,
@@ -34,27 +42,14 @@ class TestSaleOrderSecondaryUnit(TransactionCase):
         )
         cls.product.sale_secondary_uom_id = cls.secondary_unit.id
         cls.partner = cls.env["res.partner"].create({"name": "test - partner"})
-        so = cls.env["sale.order"].new(
-            {
-                "partner_id": cls.partner.id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product.name,
-                            "product_id": cls.product.id,
-                            "product_uom_qty": 1,
-                            "product_uom": cls.product.uom_id.id,
-                            "price_unit": 1000.00,
-                        },
-                    )
-                ],
-                "pricelist_id": cls.env.ref("product.list0").id,
-            }
-        )
-        so.onchange_partner_id()
-        cls.order = cls.env["sale.order"].create(so._convert_to_write(so._cache))
+        with Form(cls.env["sale.order"]) as order_form:
+            order_form.partner_id = cls.partner
+            order_form.pricelist_id = cls.price_list
+            with order_form.order_line.new() as line_form:
+                line_form.product_id = cls.product
+                line_form.product_uom_qty = 1
+                line_form.price_unit = 1000.00
+        cls.order = order_form.save()
 
     def test_onchange_secondary_uom(self):
         self.order.order_line.write(
@@ -99,6 +94,8 @@ class TestSaleOrderSecondaryUnit(TransactionCase):
         self.assertEqual(self.order.order_line.product_uom_qty, 17)
 
     def test_secondary_uom_unit_price(self):
+        # Remove secondary uom in sale line to do a complete test of secondary price
+        self.order.order_line.secondary_uom_id = False
         self.assertEqual(self.order.order_line.secondary_uom_unit_price, 0)
         self.order.order_line.update(
             {"secondary_uom_id": self.secondary_unit.id, "product_uom_qty": 2}
