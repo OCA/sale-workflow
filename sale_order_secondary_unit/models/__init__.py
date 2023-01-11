05e439602e70654e122b930aa91093b162aea6f8
sergio-teruel <sergio.teruel@tecnativa.com>
2023-01-11 05:54:09 +0100

    [IMP] sale_order_secondary_unit: Compatibility with product variants TT40323

--- sale_order_secondary_unit/models/__init__.py
+++ sale_order_secondary_unit/models/__init__.py
@@ -1,7 +1,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from . import product
+||||||| parent of c0e2baecd ([IMP] sale_order_secondary_unit: Compatibility with product variants)
+=======
+from . import product
+>>>>>>> c0e2baecd ([IMP] sale_order_secondary_unit: Compatibility with product variants)
 from . import product_template
 ||||||| parent of d16be0e27 ([FIX] code refactor)
 from . import product_template
