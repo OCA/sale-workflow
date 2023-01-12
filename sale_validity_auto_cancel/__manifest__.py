43864d04291315c315bd0013e7041ef2c5bc972d
Juany Davila <juanyadavilap@gmail.com>
2023-01-12 07:46:15 -0500

    [MIG] sale_valitidy_auto_cancel: migration to 14.0

--- sale_validity_auto_cancel/__manifest__.py
+++ sale_validity_auto_cancel/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Validity Auto-Cancel",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales",
     "license": "LGPL-3",
     "summary": "Automatically cancel quotations after validity period.",
