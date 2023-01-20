8e464a8abf34c618904d20709679f35a029ad83b
ferran-73 <ferran@studio73.es>
2023-01-20 08:54:05 +0100

    [MIG] sale_global_discount: 16.0

--- sale_global_discount/__manifest__.py
+++ sale_global_discount/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Global Discount",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
