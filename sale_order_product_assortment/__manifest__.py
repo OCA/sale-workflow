8860532341fb5e2689301d7f2b66e7ab27e52f01
Robrecht Colson <robrecht.colson@gumbys.be>
2023-02-03 13:42:45 +0100

    [MIG] sale_order_product_assortment: Migration to 16.0

--- sale_order_product_assortment/__manifest__.py
+++ sale_order_product_assortment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Order Product Assortment",
     "summary": "Module that allows to use the assortments on sale orders",
-    "version": "15.0.2.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
@@ -12,6 +12,10 @@
     "maintainers": ["CarlosRoca13"],
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["sale", "product_assortment", "base_view_inheritance_extension"],
+    "depends": [
+        "sale",
+        "product_assortment",
+        "web_domain_field",
+    ],
     "data": ["views/sale_order_view.xml"],
 }
--- sale_order_product_assortment/models/sale_order.py
+++ sale_order_product_assortment/models/sale_order.py
@@ -1,38 +1,38 @@
 # Copyright 2020 Tecnativa - Carlos Roca
 # Copyright 2023 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import json
+
 from odoo import api, fields, models
-from odoo.osv import expression
+from odoo.osv.expression import AND
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    allowed_product_ids = fields.Many2many(
-        comodel_name="product.product",
-        string="Allowed Products",
-        compute="_compute_product_assortment_ids",
-    )
-    has_allowed_products = fields.Boolean(compute="_compute_product_assortment_ids")
+    has_allowed_products = fields.Boolean(compute="_compute_allowed_product_domain")
+    allowed_product_domain = fields.Char(compute="_compute_allowed_product_domain")
 
     @api.depends("partner_id", "partner_shipping_id", "partner_invoice_id")
-    def _compute_product_assortment_ids(self):
-        self.allowed_product_ids = False
-        self.has_allowed_products = False
+    def _compute_allowed_product_domain(self):
         partner_field = (
             self.env["ir.config_parameter"]
             .sudo()
             .get_param("sale_order_product_assortment.partner_field", "partner_id")
         )
-        partner = self[partner_field]
-        product_domain = []
-        if partner:
-            for ir_filter in partner.applied_assortment_ids:
-                product_domain = expression.AND(
-                    [product_domain, ir_filter._get_eval_domain()]
-                )
-            if product_domain:
-                self.allowed_product_ids = self.env["product.product"].search(
-                    product_domain
-                )
-                self.has_allowed_products = True
+        filters_partner_domain = self.env["ir.filters"].search(
+            [("is_assortment", "=", True)]
+        )
+        for rec in self:
+            rec.allowed_product_domain = json.dumps([(1, "=", 1)])
+            rec.has_allowed_products = False
+
+            domains = []
+            for ir_filter in filters_partner_domain.filtered(
+                lambda f, partner=rec[partner_field]: partner.id
+                in f.all_partner_ids.ids
+            ):
+                domains.append(ir_filter._get_eval_domain())
+            if domains:
+                rec.has_allowed_products = True
+                rec.allowed_product_domain = json.dumps(AND(domains))
