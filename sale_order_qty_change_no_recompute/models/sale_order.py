7adc17e982727fd1429cf73da209c69a93ba29f4
Jakub Bordáš <jakub.bordas@slovanet.net>
2023-02-07 17:01:49 +0100

    [MIG] sale_order_qty_change_no_recompute: Migration to 16.0

--- sale_order_qty_change_no_recompute/models/sale_order.py
+++ sale_order_qty_change_no_recompute/models/sale_order.py
@@ -2,28 +2,35 @@
 # Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, fields, models
 from odoo.tools import config
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _onchange_eval(self, field_name, onchange, result):
-        """Remove the trigger for the undesired onchange method with this field.
+    quantity_skip = fields.Boolean(default=False)
 
-        We have to act at this place, as `_onchange_methods` is defined as a
-        property, and thus it can't be inherited due to the conflict of
-        inheritance between Python and Odoo ORM, so we can consider this as a HACK.
+    def _onchange_eval(self, field_name, onchange, result):
+        """If only product quantity is changed, we need to set
+        field quantity_skip to True. This is done to avoid
+        computing price unit every time when product quantity
+        is changed.
         """
+
         ctx = self.env.context
-        if field_name in {"product_uom_qty", "product_uom"} and (
+        if field_name in ("product_uom_qty", "product_uom") and (
             not config["test_enable"]
             or (config["test_enable"] and ctx.get("prevent_onchange_quantity", False))
         ):
-            cls = type(self)
-            for method in self._onchange_methods.get(field_name, ()):
-                if method == cls.product_uom_change:
-                    self._onchange_methods[field_name].remove(method)
-                    break
+            self.quantity_skip = True
         return super()._onchange_eval(field_name, onchange, result)
+
+    @api.depends("product_id", "product_uom", "product_uom_qty")
+    def _compute_price_unit(self):
+        for line in self:
+            if line.quantity_skip:
+                line.quantity_skip = False
+                continue
+            # Run default computing method
+            return super(SaleOrderLine, line)._compute_price_unit()
--- sale_order_qty_change_no_recompute/tests/test_sale_order_qty_change.py
+++ sale_order_qty_change_no_recompute/tests/test_sale_order_qty_change.py
@@ -13,12 +13,10 @@ class TestSaleOrderQtyChange(TransactionCase):
         cls.product_2 = cls.env["product.product"].create(
             {"name": "Test Product 2", "list_price": 30.00, "taxes_id": False}
         )
-        pricelist = cls.env["product.pricelist"].create({"name": "Test pricelist"})
         sale_form = Form(
             cls.env["sale.order"].with_context(prevent_onchange_quantity=True)
         )
         sale_form.partner_id = cls.env.ref("base.res_partner_12")
-        sale_form.pricelist_id = pricelist
         with sale_form.order_line.new() as cls.line_form:
             cls.line_form.product_id = cls.product_1
             cls.line_form.product_uom_qty = 1
