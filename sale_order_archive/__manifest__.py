28956f18409b03f5f7c1eda12123fb0ea65e2e79
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-02-14 15:33:37 +0000

    [MIG] sale_order_archive: Migration to 16.0

--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale_order.xml"],
