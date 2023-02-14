bb34f6551c7a0930ea99a0d27dfc4afeba9b4ce0
Juany Davila <juanyadavilap@gmail.com>
2023-02-14 17:07:36 -0500

    [MIG] sale_stock_picking_blocking_proc_group_by_line: migration to 15.0

--- sale_stock_picking_blocking_proc_group_by_line/__manifest__.py
+++ sale_stock_picking_blocking_proc_group_by_line/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Sale Stock Picking Blocking Procurement Group By Line",
     "summary": "Module that allows module sale_stock_picking_blocking to work "
     "with sale_procurement_group_by_line",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
--- sale_stock_picking_blocking_proc_group_by_line/models/sale_order.py
+++ sale_stock_picking_blocking_proc_group_by_line/models/sale_order.py
@@ -7,8 +7,8 @@ from odoo import models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _action_launch_procurement_rule(self):
+    def _action_launch_stock_rule(self):
         return super(
             SaleOrderLine,
             self.filtered(lambda line: not line.order_id.delivery_block_id),
-        )._action_launch_procurement_rule()
+        )._action_launch_stock_rule()
