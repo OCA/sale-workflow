50676fdf8858b9e7d08fed501f919565a1c97428
sergio-teruel <sergio.teruel@tecnativa.com>
2023-02-27 14:24:04 +0100

    [FIX] sale_planner_calendar: Do not send invitation attendees emails when reassign user to calendar.event TT41889

--- sale_planner_calendar/__manifest__.py
+++ sale_planner_calendar/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale planner calendar",
     "summary": "Sale planner calendar",
-    "version": "15.0.1.4.0",
+    "version": "15.0.1.4.1",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
