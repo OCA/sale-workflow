087aac0e7e832ed7b38802c765b971c95c48319f
Carmen Rondon Regalado <crondon@archeti.com>
2023-03-07 15:56:48 -0500

    [MIG] sale_delivery_split_date: Migration to 16.0

--- sale_delivery_split_date/__manifest__.py
+++ sale_delivery_split_date/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Delivery Split Date",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Sale Deliveries split by date",
     "category": "Sales Management",
     "license": "AGPL-3",
