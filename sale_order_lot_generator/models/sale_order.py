9342e62db74b444f665b66ffe65a541d8af088eb
Florian da Costa <florian.dacosta@akretion.com>
2023-03-10 21:05:21 +0100

    [FIX] Stop deleting lot on sale order cancelation A lot may have happen, and it is possible to have done some MO, received some PO using this lot, maybe there is even stock on hand with this lot and should not delete it or it is lost everywhere.

--- sale_order_lot_generator/models/sale_order.py
+++ sale_order_lot_generator/models/sale_order.py
@@ -35,10 +35,3 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         self.generate_lot()
         return super().action_confirm()
-
-    def action_cancel(self):
-        res = super().action_cancel()
-        for sale in self:
-            for line in sale.order_line:
-                line.lot_id.unlink()
-        return res
