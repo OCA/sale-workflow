4cffa2c2af64853e1575e32034fc74783becd2cb
nguyenhk <nguyenhk@trobz.com>
2023-03-16 09:53:51 +0700

    [MIG] sale_manual_delivery: Migration to 16.0

--- sale_manual_delivery/models/crm_team.py
+++ sale_manual_delivery/models/crm_team.py
@@ -8,7 +8,6 @@ class CrmTeam(models.Model):
     _inherit = "crm.team"
 
     manual_delivery = fields.Boolean(
-        string="Manual Delivery",
         help="If enabled, the deliveries are not created at SO confirmation. "
         "You need to use the Create Delivery button in order to reserve and "
         "ship the goods.",
---
+++ sale_manual_delivery/readme/CREDITS.rst
@@ -0,0 +1,1 @@
+The migration of this module from 14.0 to 16.0 was financially supported by Camptocamp
