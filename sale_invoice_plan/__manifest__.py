43397b00c056c68038b601294b8c0931f1dbcc63
Murtuza Saleh <msaleh@opensourceintegrators.com>
2023-03-28 16:42:09 +0530

    [MIG] sale_invoice_plan: Migrate to 16.0

--- sale_invoice_plan/__manifest__.py
+++ sale_invoice_plan/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sales Invoice Plan",
     "summary": "Add to sales order, ability to manage future invoice plan",
-    "version": "15.0.1.3.2",
+    "version": "16.0.1.0.0",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/sale-workflow",
--- sale_invoice_plan/models/sale.py
+++ sale_invoice_plan/models/sale.py
@@ -45,14 +45,14 @@ class SaleOrder(models.Model):
         for rec in self:
             has_invoice_plan = rec.use_invoice_plan and rec.invoice_plan_ids
             to_invoice = rec.invoice_plan_ids.filtered(lambda l: not l.invoiced)
+            invoice_plan_process = False
             if rec.state == "sale" and has_invoice_plan and to_invoice:
-                if rec.invoice_status == "to invoice" or (
-                    rec.invoice_status == "no"
-                    and "advance" in to_invoice.mapped("invoice_type")
-                ):
-                    rec.invoice_plan_process = True
-                    continue
-            rec.invoice_plan_process = False
+                if rec.invoice_status in [
+                    "to invoice",
+                    "no",
+                ] and "advance" in to_invoice.mapped("invoice_type"):
+                    invoice_plan_process = True
+            rec.invoice_plan_process = invoice_plan_process
 
     @api.constrains("invoice_plan_ids")
     def _check_invoice_plan_total_percent(self):
@@ -135,12 +135,13 @@ class SaleOrder(models.Model):
 
     def _create_invoices(self, grouped=False, final=False, date=None):
         moves = super()._create_invoices(grouped=grouped, final=final, date=date)
-        invoice_plan_id = self._context.get("invoice_plan_id")
-        if invoice_plan_id:
-            plan = self.env["sale.invoice.plan"].browse(invoice_plan_id)
+        if self.use_invoice_plan:
+            plan = self.env["sale.invoice.plan"].search(
+                [("sale_id", "=", self.id)], limit=1
+            )
             for move in moves:
                 plan._compute_new_invoice_quantity(move)
                 move.invoice_date = plan.plan_date
-                move._onchange_invoice_date()
+                move._compute_date()
             plan.invoice_move_ids += moves
         return moves
