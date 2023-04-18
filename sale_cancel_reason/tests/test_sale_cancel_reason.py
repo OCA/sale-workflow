23f829c801c5beeb6e1a88d2528329f4b6749df7
Hugo <hugo.cordoba@factorlibre.com>
2023-04-18 17:14:53 +0200

    [UPD]sale_cancel_reason change setUp to setUpClass

--- sale_cancel_reason/tests/test_sale_cancel_reason.py
+++ sale_cancel_reason/tests/test_sale_cancel_reason.py
@@ -5,10 +5,27 @@ from odoo.tests.common import TransactionCase
 
 
 class TestSaleCancelReason(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        SaleOrder = cls.env["sale.order"]
+        CancelReason = cls.env["sale.order.cancel.reason"]
+        cls.reason = CancelReason.create({"name": "Canceled for tests"})
+        cls.partner = cls.env.ref("base.res_partner_2")
+        cls.product = cls.env.ref("product.product_product_7")
+        cls.sale_order = SaleOrder.create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (0, 0, {"product_id": cls.product.id, "product_uom_qty": 8})
+                ],
+            }
+        )
+
     def test_sale_order_cancel_reason(self):
         """
-        - Cancel a sales order with the wizard asking for the reason
-        - Then the sale order should be canceled and the reason stored
+        - Cancel a sales order with the wizard asking for the reason.
+        - Then the sale order should be canceled and the reason stored.
         """
         SaleOrderCancel = self.env["sale.order.cancel"]
         wizard = SaleOrderCancel.create(
@@ -21,19 +38,3 @@ class TestSaleCancelReason(TransactionCase):
             self.sale_order.state, "cancel", "the sale order should be canceled"
         )
         self.assertEqual(self.sale_order.cancel_reason_id.id, self.reason.id)
-
-    def setUp(self):
-        super(TestSaleCancelReason, self).setUp()
-        SaleOrder = self.env["sale.order"]
-        CancelReason = self.env["sale.order.cancel.reason"]
-        self.reason = CancelReason.create({"name": "Canceled for tests"})
-        self.partner = self.env.ref("base.res_partner_2")
-        self.product = self.env.ref("product.product_product_7")
-        self.sale_order = SaleOrder.create(
-            {
-                "partner_id": self.partner.id,
-                "order_line": [
-                    (0, 0, {"product_id": self.product.id, "product_uom_qty": 8})
-                ],
-            }
-        )
