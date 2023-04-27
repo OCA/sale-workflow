75872b2601fb363f19db662715242b4bc4f5aebe
Yadier A. De Quesada <yadier.dequesada@tecnativa.com>
2023-04-27 12:08:33 -0400

    [MIG] sale_partner_selectable_option: Migration to 16.0

--- sale_partner_selectable_option/models/sale_order.py
+++ sale_partner_selectable_option/models/sale_order.py
@@ -1,6 +1,5 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from lxml import etree
 
 from odoo import api, models
@@ -10,15 +9,8 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.model
-    def fields_view_get(
-        self, view_id=None, view_type="form", toolbar=False, submenu=False
-    ):
-        res = super().fields_view_get(
-            view_id=view_id,
-            view_type=view_type,
-            toolbar=toolbar,
-            submenu=submenu,
-        )
+    def get_view(self, view_id=None, view_type="form", **options):
+        res = super().get_view(view_id, view_type, **options)
         if view_type == "form":
             order_xml = etree.XML(res["arch"])
             partner_id_fields = order_xml.xpath("//field[@name='partner_id']")
