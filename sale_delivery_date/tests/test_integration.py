a06c85b306046e4e43c37d6925584f02cabf6a11
MmeQuignon <matthieu.mequignon@camptocamp.com>
2023-05-12 08:58:28 +0200

    [FIX] sale_delivery_date: fix tests

--- sale_delivery_date/tests/test_integration.py
+++ sale_delivery_date/tests/test_integration.py
@@ -13,6 +13,7 @@ SATURDAY = "2021-08-21"
 SUNDAY = "2021-08-22"
 NEXT_MONDAY = "2021-08-23"
 NEXT_TUESDAY = "2021-08-24"
+NEXT_WEDNESDAY = "2021-08-25"
 NEXT_THURSDAY = "2021-08-26"
 NEXT_FRIDAY = "2021-08-27"
 # NOTE: the following dates are UTC, with 'Europe/Paris' we are GMT+2
@@ -31,10 +32,16 @@ class TestSaleDeliveryDate(Common):
     @freeze_time(THURSDAY_AFTER_CUTOFF)
     def test_order_on_thursday_after_cutoff_to_deliver_on_workdays(self):
         """Order confirmed after cut-off time on Thursday to deliver on workdays."""
+        # next cutoff friday @ 7:00
+        # next end work friday @ 15:00
+        # + security_lead saturday @ 15:00
+        # next window = monday @ 15:00
+        #
         order = self.order_warehouse_cutoff
         order.action_confirm()
         picking = order.picking_ids
         self.assertEqual(str(picking.scheduled_date.date()), FRIDAY)
+        # self.assertEqual(str(picking.date_deadline.date()), FRIDAY)
         self.assertEqual(str(order.expected_date.date()), NEXT_MONDAY)
 
     @freeze_time(THURSDAY_BEFORE_CUTOFF)
@@ -67,6 +74,16 @@ class TestSaleDeliveryDate(Common):
     @freeze_time(FRIDAY_AFTER_CUTOFF)
     def test_order_on_friday_after_cutoff_to_deliver_on_friday(self):
         """Order confirmed after cut-off time on Friday to deliver on friday."""
+        # now = 2021-08-20 08:30:00
+        # Apply cutoff : 2021-08-21 08:00:00
+        # next_working_day: 2021-08-23 08:00:00
+        # apply workload: 2021-08-23 17:00:00
+        # apply security_lead: 2021-08-24 00:00:00
+        # apply time window: 2021-08-27 08:00:00 (delivery_date)
+        # ====
+        # deduct security_lead: 2021-08-26 23:59:59 (TODO should be time.maxed)
+        # previous end attendance: 2021-08-26 17:00:00
+        # latest start work: 2021-08-26 08:00:00
         self._set_partner_time_window_to_friday(self.customer_warehouse_cutoff)
         order = self.order_warehouse_cutoff
         order.action_confirm()
