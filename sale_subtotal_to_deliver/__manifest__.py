81d97818103eeabc1e3b91236a3c2978d4eebca4
SergiCForgeFlow <sergi.casau@forgeflow.com>
2023-05-22 09:33:35 +0200

    [13.0][ADD]Sale Subtotal to Deliver / Delivered quantities added

---
+++ sale_subtotal_to_deliver/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Subtotal To Deliver",
+    "summary": "Allows to gain an economic perspective of the sale orders that have quantities "
+    "pending to receive.",
+    "version": "13.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": ["views/views.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+}
