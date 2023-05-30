4461599594200c692fbc42a2d196be408a2acc49
matiasperalta1 <matias_peralta1@hotmail.com>
2023-05-30 10:27:01 -0300

    [MIG] sale_order_carrier_auto_assign: Migration to 16.0
    [UPD] Update sale_order_carrier_auto_assign.pot
    
    [BOT] post-merge updates

--- sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
+++ sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
@@ -12,8 +12,8 @@ class TestSaleOrderCarrierAutoAssign(TransactionCase):
         cls.env = cls.env(context=dict(test_context, tracking_disable=True))
         cls.partner = cls.env.ref("base.res_partner_2")
         product = cls.env.ref("product.product_product_9")
-        cls.normal_delivery_carrier = cls.env.ref("delivery.normal_delivery_carrier")
-        cls.normal_delivery_carrier.fixed_price = 10
+        cls.delivery_local_delivery = cls.env.ref("delivery.delivery_local_delivery")
+        cls.delivery_local_delivery.fixed_price = 10
         sale_order_form = Form(cls.env["sale.order"])
         sale_order_form.partner_id = cls.partner
         with sale_order_form.order_line.new() as line_form:
@@ -22,14 +22,14 @@ class TestSaleOrderCarrierAutoAssign(TransactionCase):
 
     def test_sale_order_carrier_auto_assign(self):
         self.assertEqual(
-            self.partner.property_delivery_carrier_id, self.normal_delivery_carrier
+            self.partner.property_delivery_carrier_id, self.delivery_local_delivery
         )
         self.assertFalse(self.sale_order.carrier_id)
         self.sale_order.action_confirm()
         self.assertEqual(self.sale_order.state, "sale")
-        self.assertEqual(self.sale_order.carrier_id, self.normal_delivery_carrier)
+        self.assertEqual(self.sale_order.carrier_id, self.delivery_local_delivery)
         delivery_line = self.sale_order.order_line.filtered(lambda l: l.is_delivery)
-        delivery_rate = self.normal_delivery_carrier.rate_shipment(self.sale_order)
+        delivery_rate = self.delivery_local_delivery.rate_shipment(self.sale_order)
         self.assertEqual(delivery_line.price_unit, delivery_rate["carrier_price"])
 
     def test_sale_order_carrier_auto_assign_no_carrier(self):
