3b7c1c610758325c28715f23e58d68321c3cb0ba
AaronHForgeFlow <ahenriquez@eficent.com>
2023-06-06 12:58:08 +0200

    [ADD] sale_fully_invoiced

---
+++ sale_fully_invoiced/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Sales Fully Invoiced",
+    "summary": "Useful filters in Sales to know the actual status of invoices.",
+    "author": "Forgeflow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "version": "15.0.1.0.0",
+    "depends": ["sale"],
+    "data": ["views/sale_views.xml"],
+    "installable": True,
+}
