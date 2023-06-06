8213f29d8e479a5a948832b5ef604c8d132abb22
Maxime Franco <maxime.franco@acsone.eu>
2023-06-06 15:22:42 +0200

    [IMP] sale_timesheet_project_manual - pre-commit stuff

--- sale_timesheet_project_manual/__manifest__.py
+++ sale_timesheet_project_manual/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sale Timesheet Project Manual',
-    'summary': """
+    "name": "Sale Timesheet Project Manual",
+    "summary": """
         Allows to create the project/tasks before the sale confirmation""",
-    'version': '11.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'depends': [
-        'sale_timesheet',
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": [
+        "sale_timesheet",
     ],
-    'data': [
-        'views/sale_order.xml',
+    "data": [
+        "views/sale_order.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
--- sale_timesheet_project_manual/models/sale_order.py
+++ sale_timesheet_project_manual/models/sale_order.py
@@ -1,37 +1,33 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     action_project_manual_allowed = fields.Boolean(
-        compute='_compute_action_project_manual_allowed'
+        compute="_compute_action_project_manual_allowed"
     )
 
     @api.multi
     @api.depends(
-        'state',
-        'order_line.is_service',
-        'order_line.product_id.service_tracking',
+        "state",
+        "order_line.is_service",
+        "order_line.product_id.service_tracking",
     )
     def _compute_action_project_manual_allowed(self):
         for rec in self:
-            rec.action_project_manual_allowed = (
-                rec.state in ('draft', 'sent') and
-                any([
-                    line.is_service and
-                    line.product_id.service_tracking in (
-                        'task_global_project',
-                        'project_only',
-                        'task_new_project'
-                    )
+            rec.action_project_manual_allowed = rec.state in ("draft", "sent") and any(
+                [
+                    line.is_service
+                    and line.product_id.service_tracking
+                    in ("task_global_project", "project_only", "task_new_project")
                     for line in rec.order_line
-                ])
+                ]
             )
 
     @api.multi
@@ -43,8 +39,11 @@ class SaleOrder(models.Model):
     def action_project_manual_allowed_check(self):
         for rec in self:
             if not rec.action_project_manual_allowed:
-                raise ValidationError(_(
-                    "You can anticipate the project creation only for "
-                    "draft quotations which contain service with timesheet "
-                    "generation. (SO: %s)"
-                ) % rec.display_name)
+                raise ValidationError(
+                    _(
+                        "You can anticipate the project creation only for "
+                        "draft quotations which contain service with timesheet "
+                        "generation. (SO: %s)"
+                    )
+                    % rec.display_name
+                )
