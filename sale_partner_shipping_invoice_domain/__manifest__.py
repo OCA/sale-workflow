f79138f95b2aa8bb8990e9e2f2bd193fb5812440
Kev-Roche <kevin.roche@akretion.com>
2023-06-12 16:50:06 +0200

    [IMP] dynamic domain for shipping and delivery partner in sale.order

--- sale_partner_shipping_invoice_domain/__manifest__.py
+++ sale_partner_shipping_invoice_domain/__manifest__.py
@@ -6,10 +6,13 @@
     "summary": """
         Adds a sensible domain to the shipping
         and invoicing addresses on sale form view""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
-    "depends": ["sale_stock", "sale_commercial_partner"],
+    "depends": ["sale_stock", "sale_commercial_partner", "web_domain_field"],
+    "data": [
+        "views/sale_order.xml",
+    ],
     "demo": [],
 }
