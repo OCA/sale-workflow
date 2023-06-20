98d5d3fc2003f7a0455ae05a107c0825e8bcaa5d
Alexis de Lattre <alexis.delattre@akretion.com>
2023-06-20 12:17:27 +0200

    sale_start_end_dates: onchange -> compute methods
    Improve and update some strings
    Update FR translation

--- sale_start_end_dates/tests/test_sale_start_end_dates.py
+++ sale_start_end_dates/tests/test_sale_start_end_dates.py
@@ -11,8 +11,12 @@ class TestSaleStartEndDates(TransactionCase):
     def setUp(self):
         super().setUp()
         self.partner = self.env.ref("base.res_partner_3")
-        self.product_id = self.env.ref("product.product_product_7")
-        self.product_id.must_have_dates = True
+        self.product_id = self.env.ref(
+            "account_invoice_start_end_dates.product_insurance_contract_demo"
+        )
+        self.assertTrue(self.product_id.must_have_dates)
+        self.product_no_dates = self.env.ref("product.product_product_7")
+        self.assertFalse(self.product_no_dates.must_have_dates)
         self.default_start_date = datetime.datetime.now()
         self.default_end_date = self.default_start_date + datetime.timedelta(days=9)
         self.so = self.env["sale.order"].create(
@@ -23,26 +27,23 @@ class TestSaleStartEndDates(TransactionCase):
                 "default_start_date": self.default_start_date,
                 "default_end_date": self.default_end_date,
                 "pricelist_id": self.env.ref("product.list0").id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": self.product_id.name,
-                            "product_id": self.product_id.id,
-                            "product_uom_qty": 2,
-                            "product_uom": self.product_id.uom_id.id,
-                            "price_unit": self.product_id.list_price,
-                            "start_date": self.default_start_date,
-                            "end_date": self.default_end_date,
-                        },
-                    )
-                ],
+            }
+        )
+        self.order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": self.so.id,
+                "name": self.product_id.display_name,
+                "product_id": self.product_id.id,
+                "product_uom_qty": 2,
+                "product_uom": self.product_id.uom_id.id,
+                "price_unit": self.product_id.list_price,
+                "start_date": self.default_start_date,
+                "end_date": self.default_end_date,
             }
         )
         self.so.action_confirm()
 
-    def test_default_start_date_change(self):
+    def test_default_start_end_date_constraint(self):
         with self.assertRaises(ValidationError):
             self.so.write(
                 {
@@ -50,77 +51,44 @@ class TestSaleStartEndDates(TransactionCase):
                     "default_end_date": self.default_start_date,
                 }
             )
-        self.so.default_start_date_change()
 
-    def test_default_end_date_change(self):
-        with self.assertRaises(ValidationError):
-            self.so.write(
-                {
-                    "default_start_date": self.default_end_date,
-                    "default_end_date": self.default_start_date,
-                }
-            )
-        self.so.default_end_date_change()
-
-    def test_start_end_dates_product_id_change(self):
-        if self.so.default_end_date and self.so.default_end_date:
-            self.so.order_line.start_end_dates_product_id_change()
-            self.so.order_line.start_date_change()
-            self.so.order_line.end_date_change()
-        self.product_id.must_have_dates = False
-        self.so.order_line.start_end_dates_product_id_change()
-
-    def test_start_end_dates_product_id(self):
-        self.product_id.must_have_dates = False
-        self.so.default_start_date = self.so.default_end_date = False
-        self.so.order_line.start_end_dates_product_id_change()
-
-    def test_end_date_change(self):
-        self.product_id.must_have_dates = False
-        self.so.order_line.write(
-            {"start_date": self.default_end_date, "end_date": self.default_start_date}
+    def test_compute_start_end_date(self):
+        new_line = self.env["sale.order.line"].create(
+            {
+                "product_id": self.product_id.id,
+                "name": self.product_id.display_name,
+                "product_uom_qty": 2,
+                "product_uom": self.product_id.uom_id.id,
+                "order_id": self.so.id,
+            }
         )
-        self.so.order_line.end_date_change()
+        self.assertEqual(new_line.start_date, self.so.default_start_date)
+        self.assertEqual(new_line.end_date, self.so.default_end_date)
 
-    def test_start_date_change(self):
-        self.product_id.must_have_dates = False
-        self.so.order_line.write(
-            {"start_date": self.default_end_date, "end_date": self.default_start_date}
+    def test_compute_product_no_dates(self):
+        new_line = self.env["sale.order.line"].create(
+            {
+                "product_id": self.product_no_dates.id,
+                "name": self.product_no_dates.display_name,
+                "product_uom_qty": 2,
+                "product_uom": self.product_no_dates.uom_id.id,
+                "order_id": self.so.id,
+            }
         )
-        self.so.order_line.start_date_change()
-
-    def test_constrains_end_dates(self):
-        with self.assertRaises(ValidationError):
-            self.so.order_line.end_date = False
-
-    def test_constrains_start_date(self):
-        with self.assertRaises(ValidationError):
-            self.so.order_line.start_date = False
-
-    def test_constrains_greater_st_date(self):
-        with self.assertRaises(ValidationError):
-            self.so.order_line.write(
-                {
-                    "start_date": self.default_end_date,
-                    "end_date": self.default_start_date,
-                }
-            )
+        self.assertFalse(new_line.start_date)
+        self.assertFalse(new_line.end_date)
 
     def test_compute_number_of_days(self):
-        self.assertEqual(self.so.order_line[0].number_of_days, 10)
+        self.assertEqual(self.order_line.number_of_days, 10)
 
     def test_inverse_number_of_days(self):
-        self.so.order_line[0].number_of_days = 1
-        self.assertEqual(
-            self.so.order_line[0].start_date, self.so.order_line[0].end_date
-        )
-        self.so.order_line[0].number_of_days = -1
-        self.assertEqual(self.so.order_line[0].number_of_days, 1)
+        self.order_line.number_of_days = 1
+        self.assertEqual(self.order_line.start_date, self.order_line.end_date)
+        self.order_line.number_of_days = -1
+        self.assertEqual(self.order_line.number_of_days, 1)
 
     def test_prepare_invoice_line(self):
-        invoice_line_vals = self.so.order_line._prepare_invoice_line()
+        invoice_line_vals = self.order_line._prepare_invoice_line()
         self.assertEqual(invoice_line_vals["product_id"], self.product_id.id)
-        self.assertEqual(
-            invoice_line_vals["start_date"], self.so.order_line[0].start_date
-        )
-        self.assertEqual(invoice_line_vals["end_date"], self.so.order_line[0].end_date)
+        self.assertEqual(invoice_line_vals["start_date"], self.order_line.start_date)
+        self.assertEqual(invoice_line_vals["end_date"], self.order_line.end_date)
