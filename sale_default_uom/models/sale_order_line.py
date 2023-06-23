c62f88d501e11580d6d107098d9bd6e1660d42df
sistecem <info@sistecem.com>
2023-06-23 18:47:06 -0400

    [IMP] Option to base the sale_price on sale_uom_id
    The default sale price was based on uom_id
    An option to based on sale_uom_id ( when this is set ) is created with a boolean tick box.
    Making it easier and more accurate on decimals, useful on big quantities and rare uom Factors.

--- sale_default_uom/models/sale_order_line.py
+++ sale_default_uom/models/sale_order_line.py
@@ -12,3 +12,20 @@ class SaleOrderLine(models.Model):
                 rec.product_uom = rec.product_id.product_tmpl_id.sale_uom_id
 
         return res
+
+
+    @api.onchange("product_uom", "product_uom_qty")
+    def product_uom_change(self):
+        res = super(SaleOrderLine, self).product_uom_change()
+        if (
+            self.product_id.list_price_base_sale_uom
+            and self.product_id.product_tmpl_id.sale_uom_id
+            and self.product_id.product_tmpl_id.sale_uom_id
+            != self.product_id.product_tmpl_id.uom_id
+        ):
+            if self.product_id.product_tmpl_id.sale_uom_id.uom_type == "bigger":
+                factorUom = 1 / self.product_id.product_tmpl_id.sale_uom_id.factor_inv
+            else:
+                factorUom = self.product_id.product_tmpl_id.sale_uom_id.factor
+            self.price_unit = self.price_unit * factorUom
+        return res
