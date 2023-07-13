06103238df412f8768145ea3b32f7eedf57b3826
OriolMForgeFlow <oriol.miranda@forgeflow.com>
2023-07-13 10:31:40 +0200

    [MIG] sale_stock_line_sequence: Migration to 14.0

--- sale_stock_line_sequence/models/sale_order.py
+++ sale_stock_line_sequence/models/sale_order.py
@@ -15,18 +15,16 @@ class SaleOrder(models.Model):
                     for ptype in order.order_line.mapped("product_id.type")
                 ]
             ):
-                pickings = order.picking_ids.filtered(
-                    lambda x: x.state not in ("done", "cancel")
-                )
-                if pickings:
-                    picking = pickings[0]
-                    order_lines = order.order_line.filtered(
-                        lambda l: l.product_id.type in ["product", "consu"]
-                    )
-                    for move, line in zip(
-                        sorted(picking.move_lines, key=lambda m: m.id), order_lines
-                    ):
-                        move.write({"sequence": line.sequence})
+                for picking in order.picking_ids:
+                    for move in picking.move_lines:
+                        if move.sale_line_id.display_type:
+                            continue
+                        move.write({"sequence": move.sale_line_id.visible_sequence})
+
+    def action_confirm(self):
+        res = super().action_confirm()
+        self._update_moves_sequence()
+        return res
 
     def write(self, line_values):
         res = super(SaleOrder, self).write(line_values)
