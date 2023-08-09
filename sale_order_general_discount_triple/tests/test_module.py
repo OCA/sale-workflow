169d54647d3d996aeeb525071d4ecbd40fecc21d
RabbitJon-S73 <roger@studio73.es>
2023-08-09 14:46:51 +0200

    [IMP] sale_order_general_discount_triple: Add pricelist discount
    Pricelist discount was overide by general discount functionality. With this change, its up to the user to choose in which discount wants to apply general and pricelist disounts.

--- sale_order_general_discount_triple/tests/test_module.py
+++ sale_order_general_discount_triple/tests/test_module.py
@@ -2,26 +2,43 @@ from odoo.tests.common import Form, TransactionCase
 
 
 class TestModule(TransactionCase):
-    def test_action_result(self):
-        setting_obj = self.env["res.config.settings"]
-        sale_order_obj = self.env["sale.order"]
-        partner_obj = self.env["res.partner"]
-        product_obj = self.env["product.product"]
-
-        product_form = Form(product_obj)
-        product_form.name = "Test Product"
-        product = product_form.save()
-
-        partner_form = Form(partner_obj)
-        partner_form.name = "Test Partner"
-        partner = partner_form.save()
-        setting_form = Form(setting_obj)
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test Partner", "sale_discount": 10}
+        )
+        cls.product = cls.env.ref("product.product_product_4")
+        cls.pricelist = cls.env["product.pricelist"].create(
+            {
+                "name": "Test multi-currency",
+                "discount_policy": "without_discount",
+                "currency_id": cls.env.ref("base.USD").id,
+                "item_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "applied_on": "3_global",
+                            "compute_price": "percentage",
+                            "percent_price": 20,
+                        },
+                    ),
+                ],
+            }
+        )
+        setting_form = Form(cls.env["res.config.settings"])
         setting_form.general_discount = "discount2"
+        setting_form.pricelist_discount = "discount"
+        setting_form.group_discount_per_so_line = True
         setting_form.save().set_values()
 
-        with Form(sale_order_obj) as sale_order:
-            sale_order.partner_id = partner
-            sale_order.general_discount = 5
-            with sale_order.order_line.new() as line:
-                line.product_id = product
-                self.assertEqual(sale_order.general_discount, 5)
+    def test_action_result(self):
+        sale_form = Form(self.env["sale.order"])
+        sale_form.partner_id = self.partner
+        sale_form.pricelist_id = self.pricelist
+        with sale_form.order_line.new() as line:
+            line.product_id = self.product
+        sale = sale_form.save()
+        self.assertEqual(sale.order_line.discount2, 10)
+        self.assertEqual(sale.order_line.discount, 20)
