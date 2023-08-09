f100e45e798c5077abf3f2e4fa9c62cf46724d41
RabbitJon-S73 <roger@studio73.es>
2023-08-09 14:46:03 +0200

    [FIX] sale_order_general_discount_triple: make all discounts computed
    Discount behaviuour in v16.0 is to be computed but in this module it was changed. Here we are using compute_discount method to archive the functionallity and also converting the remaining two discounts to computed fields.

--- sale_order_general_discount_triple/models/sale_order_line.py
+++ sale_order_general_discount_triple/models/sale_order_line.py
@@ -4,51 +4,46 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    discount = fields.Float(
-        string="Discount (%)",
-        digits="Discount",
-        compute=False,
-        store=True,
-        readonly=False,
-        default=0.0,
-    )
+    discount2 = fields.Float(compute="_compute_discount2", store=True, readonly=False)
+    discount3 = fields.Float(compute="_compute_discount3", store=True, readonly=False)
 
-    @api.model
-    def default_get(self, fields):
-        vals = super().default_get(fields)
-        general_discount = (
+    @api.depends("product_id", "product_uom", "product_uom_qty")
+    def _compute_discount(self):
+        general_discount = self._get_discount_field_position("general_discount")
+        if "discount" != general_discount:
+            self.update({"discount": 0.0})
+            return
+        for line in self:
+            if general_discount == "discount":
+                line.update({"discount": line.order_id.partner_id.sale_discount})
+        return
+
+    @api.depends("product_id", "product_uom", "product_uom_qty")
+    def _compute_discount2(self):
+        general_discount = self._get_discount_field_position("general_discount")
+        if "discount2" != general_discount:
+            self.update({"discount2": 0.0})
+            return
+        for line in self:
+            if general_discount == "discount2":
+                line.update({"discount2": line.order_id.partner_id.sale_discount})
+
+    @api.depends("product_id", "product_uom", "product_uqom_qty")
+    def _compute_discount3(self):
+        general_discount = self._get_discount_field_position("general_discount")
+        if "discount3" != general_discount:
+            self.update({"discount3": 0.0})
+            return
+        for line in self:
+            if general_discount == "discount3":
+                line.update({"discount3": line.order_id.partner_id.sale_discount})
+
+
+    def _get_discount_field_position(self, field_name):
+        return (
             self.env["ir.config_parameter"]
             .sudo()
             .get_param(
-                "sale_order_general_discount_triple.general_discount", "discount"
+                "sale_order_general_discount_triple.{}".format(field_name), "discount"
             )
         )
-        value = vals.get("discount") or 0.0
-        vals.update(
-            {
-                "discount": 0.0,
-                "discount2": 0.0,
-                "discount3": 0.0,
-            }
-        )
-
-        vals.update(
-            {
-                general_discount: value,
-            }
-        )
-        return vals
-
-    @api.model
-    def create(self, vals):
-        sale_order = self.env["sale.order"].browse(vals["order_id"])
-        general_discount = (
-            self.env["ir.config_parameter"]
-            .sudo()
-            .get_param(
-                "sale_order_general_discount_triple.general_discount", "discount"
-            )
-        )
-        if general_discount:
-            vals[general_discount] = sale_order.general_discount
-        return super().create(vals)
