54735114cd9829efe60eb8a308648ca74b7ca12b
François Honoré <francois.honore@acsone.eu>
2023-08-11 11:24:54 +0200

    [MIG] sale_order_end_user: Migration to 16.0

--- sale_order_end_user/__manifest__.py
+++ sale_order_end_user/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Allows to define the end user in sale orders if there is an
         intermediate between shipping and this end user""",
-    "version": "10.0.1.1.0",
+    "version": "16.0.1.1.0",
     "license": "AGPL-3",
     "development_status": "Beta",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
