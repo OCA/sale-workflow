f630c795b119c17b3f7195658dee7d15f2bd4f75
François Honoré <francois.honore@acsone.eu>
2023-08-11 11:17:06 +0200

    [IMP] sale_order_end_user: pre-commit stuff

--- sale_order_end_user/models/sale_order.py
+++ sale_order_end_user/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,12 +6,12 @@ from odoo import fields, models
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     end_user_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='End User',
+        comodel_name="res.partner",
+        string="End User",
         index=True,
-        domain=[('type', '=', 'end_user')],
-        ondelete='restrict',
+        domain=[("type", "=", "end_user")],
+        ondelete="restrict",
     )
