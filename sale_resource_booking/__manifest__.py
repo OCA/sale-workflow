9d027897aa912a41e8586d5c57760004873ae488
Henrik Norlin <henrik.norl@gmail.com>
2023-08-29 12:47:35 +0200

    [MIG] sale_resource_booking: Migration to 16.0
    [FIX] sale_resource_booking: Migration to 16.0

--- sale_resource_booking/__manifest__.py
+++ sale_resource_booking/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sell resource bookings",
     "summary": "Link resource bookings with sales",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Appointments",
     "website": "https://github.com/OCA/sale-workflow",
