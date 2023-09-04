330f56a1e125f5485411a2c3d7a31725d29236a5
matteo.tognini <matteo.tognini@nextev.it>
2023-09-04 16:26:06 +0200

    [IMP]sale_order_general_discount: add condition base on flag inside context, to avoiding add multiple discount

--- sale_order_general_discount/models/sale_order_line.py
+++ sale_order_general_discount/models/sale_order_line.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models
 
 
@@ -17,7 +18,11 @@ class SaleOrderLine(models.Model):
         """Apply general discount for sale order lines which are not created
         from sale order form view.
         """
-        if "discount" not in vals and "order_id" in vals:
+        if (
+            not self.env.context.get("no_propagate_general_discount")
+            and "discount" not in vals
+            and "order_id" in vals
+        ):
             sale_order = self.env["sale.order"].browse(vals["order_id"])
             if sale_order.general_discount:
                 vals["discount"] = sale_order.general_discount
