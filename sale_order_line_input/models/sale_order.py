45933410a2f0c0d4615c05277cae1b0e34e40927
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-09-13 07:33:52 -0300

    [MIG] sale_order_line_input: Migration to 16.0

--- sale_order_line_input/models/sale_order.py
+++ sale_order_line_input/models/sale_order.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -7,10 +8,7 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    pricelist_id = fields.Many2one(
-        related="order_id.pricelist_id",
-        readonly=True,
-    )
+    pricelist_id = fields.Many2one(related="order_id.pricelist_id")
     force_company_id = fields.Many2one(
         comodel_name="res.company",
         string="Forced company",
@@ -24,12 +22,7 @@ class SaleOrderLine(models.Model):
     def _compute_force_company_id(self):
         """Related company is not computed already when we click create new line"""
         for line in self:
-            line.force_company_id = (
-                line.order_id.company_id
-                # Is not necessary use browse here
-                or self.env.context.get("force_company")
-                or self.env.company
-            )
+            line.force_company_id = line.order_id.company_id or self.env.company
 
     @api.onchange("force_company_id")
     def _onchange_force_company_id(self):
@@ -44,9 +37,7 @@ class SaleOrderLine(models.Model):
         if not self.order_partner_id or self.order_id:
             return
         SaleOrder = self.env["sale.order"]
-        new_so = SaleOrder.new(
-            {"partner_id": self.order_partner_id, "company_id": self.force_company_id}
-        )
+        new_so = SaleOrder.new({"partner_id": self.order_partner_id})
         for onchange_method in new_so._onchange_methods["partner_id"]:
             onchange_method(new_so)
         order_vals = new_so._convert_to_write(new_so._cache)
