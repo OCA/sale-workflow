21c4e3abb6870d619111eadb1473a92610d3c634
Christian Santamaria <christian.santamaria@solvos.es>
2023-09-20 11:06:34 +0200

    sale_order_partner_no_autofollow: fix res.config.settings parameter
    s

--- sale_order_partner_no_autofollow/models/res_config_settings.py
+++ sale_order_partner_no_autofollow/models/res_config_settings.py
@@ -5,6 +5,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     partner_disable_autofollow = fields.Boolean(
-        config_parameter="sale_order_partner_no_autofollow.disable_partner_autofollow",
+        config_parameter="sale_order_partner_no_autofollow.partner_disable_autofollow",
         string="Customer disable autofollow",
     )
