5d289723a276fd226d46d969fd55103e99c579ad
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-09-22 17:06:53 +0200

    [IMP] sale_order_line_chained_move: do not always merge propagated moves

--- sale_order_line_chained_move/models/stock_move.py
+++ sale_order_line_chained_move/models/stock_move.py
@@ -1,11 +1,10 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-
     _inherit = "stock.move"
 
     related_sale_line_id = fields.Many2one(
@@ -24,3 +23,10 @@ class StockMove(models.Model):
                 }
             )
         return res
+
+    @api.model
+    def _prepare_merge_moves_distinct_fields(self):
+        distinct_fields = super()._prepare_merge_moves_distinct_fields()
+        if not self or any(move.rule_id.preserve_separate_so_lines for move in self):
+            distinct_fields.append("related_sale_line_id")
+        return distinct_fields
