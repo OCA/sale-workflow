4cc3dcf766650a74424cf752fcbdf870c9b28a8f
matthieu.saison <matthieu.saison@akretion.br.com>
2023-09-22 18:25:11 +0200

    base module

---
+++ sale_probability_amount/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Sale Probability Amount",
+    "summary": "add a win probability on quotation",
+    "version": "14.0.1.0.1",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale_company_currency"],
+    "data": ["views/sale_views.xml", "data/ir_config_parameter.xml"],
+}
