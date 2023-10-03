24be3f2ffde5483f513b24705e92c424aad7c525
Simone Rubino <simone.rubino@aion-tech.it>
2023-10-03 17:47:54 +0200

    [MIG] sale_restricted_qty: Migration to 16.0

--- sale_restricted_qty/__manifest__.py
+++ sale_restricted_qty/__manifest__.py
@@ -1,18 +1,16 @@
 # Copyright 2019 Akretion (<http://www.akretion.com>)
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sale order min quantity",
-    "version": "14.0.1.1.1",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "author": "Akretion, Odoo Community Association (OCA)",
     "contributors": ["Ashish Hirpara"],
     "maintainers": ["ashishhirapara"],
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
-    "external_dependencies": {
-        "python": ["openupgradelib"],
-    },
     "depends": ["sale_management"],
     "data": [
         "views/product_category_views.xml",
@@ -20,6 +18,5 @@
         "views/product_product_views.xml",
         "views/sale_views.xml",
     ],
-    "pre_init_hook": "rename_module",
     "installable": True,
 }
