ed7f9ec6e4ffd041dc2bd3c1b322934aaf26af36
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-10-05 12:41:38 +0200

    [FIX] sale_commercial_partner: add development_status
    TT44347

--- sale_commercial_partner/__manifest__.py
+++ sale_commercial_partner/__manifest__.py
@@ -6,6 +6,7 @@
     "name": "Sale Commercial Partner",
     "summary": "Add stored related field 'Commercial Entity' on sale orders",
     "version": "16.0.1.0.0",
+    "development_status": "Production/Stable",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/sale-workflow",
