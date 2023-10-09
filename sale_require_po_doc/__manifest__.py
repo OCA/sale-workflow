1b2cc996efdef829b1959061866bffd0cff55c86
AlexPForgeFlow <alex.paris@forgeflow.com>
2023-10-09 12:59:03 +0200

    [MIG] sale_require_po_doc: Migration to 16.0

--- sale_require_po_doc/__manifest__.py
+++ sale_require_po_doc/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Orders Require PO or Sales Documentation",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
