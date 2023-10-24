e2a26df2abf41cc647ddd9349b4efa097cb40d02
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 15:02:31 +0200

    [IMP] sale_exception: test performance improvement - Include context keys for avoiding mail operations overhead.

--- sale_exception/tests/test_sale_exception.py
+++ sale_exception/tests/test_sale_exception.py
@@ -12,7 +12,16 @@ class TestSaleException(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
 
     def test_sale_order_exception(self):
         self.sale_exception_confirm = self.env["sale.exception.confirm"]
