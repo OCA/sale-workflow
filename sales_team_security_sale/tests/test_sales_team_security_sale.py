741afc0448e35913ce49e860175a85c4fc1178d4
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 15:39:46 +0200

    [IMP] sales_team_security_sale: test performance improvement - Include context keys for avoiding mail operations overhead.

--- sales_team_security_sale/tests/test_sales_team_security_sale.py
+++ sales_team_security_sale/tests/test_sales_team_security_sale.py
@@ -8,6 +8,16 @@ class TestSalesTeamSecuritySale(TestCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.record = cls.env["sale.order"].create(
             {
                 "partner_id": cls.partner.id,
