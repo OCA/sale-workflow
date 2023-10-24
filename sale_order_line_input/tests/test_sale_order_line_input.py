7bb985d4b11e4263e29717f8aeee7993a737dab7
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 15:05:48 +0200

    [IMP] sale_order_line_input: test performance improvement - Include context keys for avoiding mail operations overhead.

--- sale_order_line_input/tests/test_sale_order_line_input.py
+++ sale_order_line_input/tests/test_sale_order_line_input.py
@@ -8,6 +8,16 @@ class TestSaleOrderLineInput(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.partner = cls.env["res.partner"].create({"name": "Test"})
         cls.product = cls.env["product.product"].create(
             {"name": "test_product", "type": "service"}
