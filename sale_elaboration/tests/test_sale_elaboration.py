6a1b14e879048b94912a1a8d682b30836da60010
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 15:00:42 +0200

    [IMP] sale_elaboration: test performance improvement - Include context keys for avoiding mail operations overhead.

--- sale_elaboration/tests/test_sale_elaboration.py
+++ sale_elaboration/tests/test_sale_elaboration.py
@@ -8,6 +8,16 @@ class TestSaleElaboration(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.Elaboration = cls.env["product.elaboration"]
         cls.product = cls.env["product.product"].create(
             {"name": "test", "tracking": "none", "list_price": 1000}
