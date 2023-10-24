0e9df1972abba80bcb5838cb7f05cd4e58d07bdf
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 12:03:11 +0200

    [IMP] product_supplierinfo_for_customer_elaboration: test performance improvement - Include context keys for avoiding mail operations overhead.

--- product_customerinfo_elaboration/tests/test_customerinfo_elaboration.py
+++ product_customerinfo_elaboration/tests/test_customerinfo_elaboration.py
@@ -8,6 +8,16 @@ class TestCustomerinfoElaboration(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.customer0 = cls.env["res.partner"].create({"name": "Test partner 0"})
         cls.customer1 = cls.env["res.partner"].create(
             {"name": "Test partner 1", "parent_id": cls.customer0.id}
