5f27c401e8be2657a4e76136b1f76a736d4a357a
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-24 15:34:58 +0200

    [IMP] sale_tier_validation: test performance improvement - Include context keys for avoiding mail operations overhead.

--- sale_tier_validation/tests/test_tier_validation.py
+++ sale_tier_validation/tests/test_tier_validation.py
@@ -9,6 +9,16 @@ class TestSaleTierValidation(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         # Get sale order model
         cls.so_model = cls.env.ref("sale.model_sale_order")
 
