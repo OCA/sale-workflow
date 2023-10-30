2088f491dc2eb4b822018e89434ef3e8d3f281b3
manu <manuel.regidor@sygel.es>
2023-10-30 14:10:25 +0100

    [MIG]sale_order_warn_message: Migration to 15.0

--- sale_order_warn_message/tests/test_sale_order_warn_message.py
+++ sale_order_warn_message/tests/test_sale_order_warn_message.py
@@ -1,10 +1,10 @@
 # Copyright 2020 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestSaleOrderWarnMessage(SavepointCase):
+class TestSaleOrderWarnMessage(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
