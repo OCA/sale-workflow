eb01b94b06f1f57a9ce7ca9def5342b0a7e0e960
PauBForgeFlow <pau.bosch@forgeflow.com>
2023-11-02 09:40:26 +0100

    [MIG] sale_stock_reconcile_valuation_kit: Migration to 16.0

--- sale_stock_reconcile_valuation_kit/__manifest__.py
+++ sale_stock_reconcile_valuation_kit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Stock Reconcile Valuation Kit",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales Management",
