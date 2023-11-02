f4db3185d1056c2aacf0b39dd6c98da435c62785
Simone Rubino <simone.rubino@aion-tech.it>
2023-11-02 10:17:00 +0100

    [IMP] sale_pricelist_triple_discount: pre-commit execution

--- sale_pricelist_triple_discount/model/pricelist.py
+++ sale_pricelist_triple_discount/model/pricelist.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import fields, models
 import openerp.addons.decimal_precision as dp
+from openerp import fields, models
 
 
 class ProductPricelistItem(models.Model):
-    _inherit = 'product.pricelist.item'
+    _inherit = "product.pricelist.item"
 
     discount2 = fields.Float(
-        'Discount 2 (%)',
-        digits_compute=dp.get_precision('Discount'),
+        "Discount 2 (%)",
+        digits_compute=dp.get_precision("Discount"),
         help="Second discount applied on a sale order line.",
-        default=0.0
+        default=0.0,
     )
 
     discount3 = fields.Float(
-        'Discount 3 (%)',
-        digits_compute=dp.get_precision('Discount'),
+        "Discount 3 (%)",
+        digits_compute=dp.get_precision("Discount"),
         help="Third discount applied on a sale order line.",
-        default=0.0
+        default=0.0,
     )
