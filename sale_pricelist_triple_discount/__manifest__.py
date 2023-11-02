580dec54a4187944301b951fa87464fd53559df0
Simone Rubino <simone.rubino@aion-tech.it>
2023-11-02 10:17:00 +0100

    [MIG] sale_pricelist_triple_discount: Migration to 16.0

--- sale_pricelist_triple_discount/__manifest__.py
+++ sale_pricelist_triple_discount/__manifest__.py
@@ -1,19 +1,21 @@
 #  Copyright (c) 2015 credativ ltd (<http://www.credativ.co.uk>)
 #  Copyright (c) 2020 Simone Rubino - Agile Business Group
+#  Copyright 2023 Simone Rubino - Aion Tech
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Pricelist Triple Discount",
-    "version": "8.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sale",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow"
-    "8.0/sale_pricelist_triple_discount",
+    "16.0/sale_pricelist_triple_discount",
     "license": "AGPL-3",
     "depends": [
-        "sale_pricelist_discount",
         "sale_triple_discount",
     ],
-    "data": ["view/pricelist_view.xml"],
+    "data": [
+        "view/product_pricelist_item_views.xml",
+    ],
     "auto_install": False,
     "installable": True,
 }
