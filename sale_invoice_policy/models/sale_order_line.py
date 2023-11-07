95e5c83004e55837b68654bb8f7e99b5f84e2983
Alejandro Ji Cheung <alejandro.jicheung@factorlibre.com>
2023-11-07 08:38:08 +0100

    [FIX] sale_invoice_policy: re-structure module so that there is no need to change the original invoice_policy field

--- sale_invoice_policy/models/sale_order_line.py
+++ sale_invoice_policy/models/sale_order_line.py
@@ -5,35 +5,42 @@ from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
-
     _inherit = "sale.order.line"
+
+    def _get_required_order_lines_by_invoice_policy(self, invoice_policy):
+        so_lines = self.filtered(
+            lambda x, p=invoice_policy: x.order_id.invoice_policy == p
+            and x.order_id.invoice_policy_required
+        ).with_prefetch()
+        return so_lines
+
+    def _set_invoice_policy_on_product_templates(self, invoice_policy):
+        product_templates = self.mapped("product_template_id").filtered(
+            lambda p: p.invoice_policy != invoice_policy
+            and p.detailed_type != "service"
+        )
+        if product_templates:
+            product_templates.write({"invoice_policy": invoice_policy})
 
     @api.depends(
         "qty_invoiced",
         "qty_delivered",
         "product_uom_qty",
-        "order_id.state",
+        "state",
         "order_id.invoice_policy",
     )
     def _compute_qty_to_invoice(self):
         invoice_policies = set(self.mapped("order_id.invoice_policy"))
         line_by_id = {line.id: line for line in self}
-        done_lines = self.env["sale.order.line"].browse()
+        done_lines = self.env["sale.order.line"]
         for invoice_policy in invoice_policies:
-            so_lines = (
-                self.with_context(invoice_policy=invoice_policy)
-                .filtered(lambda x, p=invoice_policy: x.order_id.invoice_policy == p)
-                .with_prefetch()
-            )
+            so_lines = self._get_required_order_lines_by_invoice_policy(invoice_policy)
             if so_lines:
                 done_lines |= so_lines
+                so_lines._set_invoice_policy_on_product_templates(invoice_policy)
                 super(SaleOrderLine, so_lines)._compute_qty_to_invoice()
                 for line in so_lines:
-                    # due to the change of context in compute methods,
-                    # assign the value in the modified context to self
                     line_by_id[line.id].qty_to_invoice = line.qty_to_invoice
-        # Not to break function if (it could not happen) some records
-        # were not in so_lines
         super(SaleOrderLine, self - done_lines)._compute_qty_to_invoice()
         return True
 
@@ -48,21 +55,14 @@ class SaleOrderLine(models.Model):
     def _compute_invoice_status(self):
         invoice_policies = set(self.mapped("order_id.invoice_policy"))
         line_by_id = {line.id: line for line in self}
-        done_lines = self.env["sale.order.line"].browse()
+        done_lines = self.env["sale.order.line"]
         for invoice_policy in invoice_policies:
-            so_lines = (
-                self.with_context(invoice_policy=invoice_policy)
-                .filtered(lambda x, p=invoice_policy: x.order_id.invoice_policy == p)
-                .with_prefetch()
-            )
-            done_lines |= so_lines
+            so_lines = self._get_required_order_lines_by_invoice_policy(invoice_policy)
             if so_lines:
+                done_lines |= so_lines
+                so_lines._set_invoice_policy_on_product_templates(invoice_policy)
                 super(SaleOrderLine, so_lines)._compute_invoice_status()
                 for line in so_lines:
-                    # due to the change of context in compute methods,
-                    # assign the value in the modified context to self
                     line_by_id[line.id].invoice_status = line.invoice_status
-        # Not to break function if (it could not happen) some records
-        # were not in so_lines
         super(SaleOrderLine, self - done_lines)._compute_invoice_status()
         return True
