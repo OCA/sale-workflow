abfe3369aa7a0fb599e999c19cd0e88bfe15ad91
Sébastien BEAU <sebastien.beau@akretion.com>
2023-11-10 15:48:54 +0100

    sale_delivery_state: refactor code to reuse native field

---
+++ sale_stock_delivery_state/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Stock Delivery State",
+    "summary": "Change the way to compute the delivery state",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": " Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale_stock",
+        "sale_delivery_state",
+    ],
+    "data": [],
+    "demo": [],
+}
