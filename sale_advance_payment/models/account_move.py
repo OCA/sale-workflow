23006d02c1645b5d2d2c4f9f28fd4e64c7d76c48
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2023-11-20 15:18:20 +0100

    [MIG] sale_advance_payment: Migration to 16.0

--- sale_advance_payment/models/account_move.py
+++ sale_advance_payment/models/account_move.py
@@ -1,8 +1,6 @@
 # Copyright 2022 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import json
-
 from odoo import models
 
 
@@ -13,13 +11,10 @@ class AccountMove(models.Model):
         # Automatic reconciliation of payment when invoice confirmed.
         res = super(AccountMove, self).action_post()
         sale_order = self.mapped("line_ids.sale_line_ids.order_id")
-        if (
-            sale_order
-            and json.loads(self.invoice_outstanding_credits_debits_widget) is not False
-        ):
-            json_invoice_outstanding_data = json.loads(
-                self.invoice_outstanding_credits_debits_widget
-            ).get("content", [])
+        if sale_order and self.invoice_outstanding_credits_debits_widget is not False:
+            json_invoice_outstanding_data = (
+                self.invoice_outstanding_credits_debits_widget.get("content", [])
+            )
             for data in json_invoice_outstanding_data:
                 if data.get("move_id") in sale_order.account_payment_ids.move_id.ids:
                     self.js_assign_outstanding_line(line_id=data.get("id"))
