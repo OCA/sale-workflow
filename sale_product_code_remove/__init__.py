e6fb1ce8058332c71dba38942fdebfa85bcb7e3c
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-11-23 17:15:53 -0600

    [ADD] sale_product_code_remove: new module to remove default code in sale order line description

---
+++ sale_product_code_remove/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2023 Jarsa, (<https://www.jarsa.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+from . import models
---
+++ sale_product_code_remove/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Removes default code brackets from the beginning of products and product descriptions on sale order lines.
