bfb0fd253fd91008316c1c5ff9bdfe695eccfe9e
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-11-28 13:31:14 +0000

    [MIG] sale_product_set_layout: Migration to 16.0
    Translated using Weblate (Italian)
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: sale-workflow-16.0/sale-workflow-16.0-sale_product_set
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-16-0/sale-workflow-16-0-sale_product_set/it/
    
    [UPD] Update sale_product_set.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: sale-workflow-16.0/sale-workflow-16.0-sale_product_set
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-16-0/sale-workflow-16-0-sale_product_set/
    
    Translated using Weblate (Italian)
    
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: sale-workflow-16.0/sale-workflow-16.0-sale_product_set
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-16-0/sale-workflow-16-0-sale_product_set/it/
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: sale-workflow-16.0/sale-workflow-16.0-sale_product_set
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-16-0/sale-workflow-16-0-sale_product_set/es/

--- sale_product_set/tests/test_product_set.py
+++ sale_product_set/tests/test_product_set.py
@@ -184,3 +184,35 @@ class TestProductSet(common.TransactionCase):
         wizard.add_set()
         self.assertEqual(products, self.so.order_line.product_id)
         self.assertEqual(len(self.so.order_line), 3)
+
+    def test_add_set_section(self):
+        so = self.env.ref("sale.sale_order_6")
+        base_line_ids = so.order_line
+        count_lines = len(so.order_line)
+        product_set_with_section = self.env.ref("product_set.product_set_services")
+        so_set = self.product_set_add.create(
+            {
+                "product_set_id": product_set_with_section.id,
+                "quantity": 2,
+                "order_id": so.id,
+            }
+        )
+        so_set.add_set()
+        self.assertEqual(len(so.order_line), count_lines + 3)
+        products_in_set = product_set_with_section.set_line_ids.filtered(
+            lambda a: a.product_id
+        ).mapped("product_id")
+        # Check lines with products
+        for line in so.order_line.filtered(
+            lambda a: a.id not in base_line_ids.ids and a.product_id
+        ):
+            self.assertFalse(line.display_type)
+            self.assertTrue(line.product_id.id in products_in_set.ids)
+        # Test sections
+        for line in so.order_line.filtered(
+            lambda a: a.id not in base_line_ids.ids and not a.product_id
+        ):
+            self.assertEqual(
+                line.display_type,
+                "line_section",
+            )
