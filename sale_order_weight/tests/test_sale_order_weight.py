aa387b555503154241920d6c6d437c2a55854b33
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-11-28 13:29:58 +0100

    [REF] sale_order_weight : simplify test code. Remove call of _onchange_weight. (by setting correct value in the creation of the sale order lines)

--- sale_order_weight/tests/test_sale_order_weight.py
+++ sale_order_weight/tests/test_sale_order_weight.py
@@ -7,7 +7,7 @@ from odoo.tests import common
 class TestSaleOrderWeight(common.TransactionCase):
 
     def setUp(self):
-        super(TestSaleOrderWeight, self).setUp()
+        super().setUp()
         self.partner = self.env.ref('base.res_partner_1')
         self.product_1 = self.env.ref('product.product_product_4')
         self.product_1.weight = 5.0  # * 2 = 10 kg
@@ -20,6 +20,7 @@ class TestSaleOrderWeight(common.TransactionCase):
             'order_id': self.sale_order.id,
             'name': self.product_1.name,
             'product_id': self.product_1.id,
+            'unit_weight': self.product_1.weight,
             'product_uom_qty': 2,
             'product_uom': self.product_1.uom_id.id,
         })
@@ -27,20 +28,18 @@ class TestSaleOrderWeight(common.TransactionCase):
             'order_id': self.sale_order.id,
             'name': self.product_2.name,
             'product_id': self.product_2.id,
+            'unit_weight': self.product_2.weight,
             'product_uom_qty': 3,
             'product_uom': self.product_2.uom_id.id,
         })
 
     def test_sale_order_weight(self):
-        self.sale_order.action_confirm()
-        self.sale_order_line_1._onchange_weight()
-        self.sale_order_line_2._onchange_weight()
         self.assertEqual(self.sale_order.total_ordered_weight, 22.0)
+        self.sale_order_line_1.product_uom_qty = 1
+        self.assertEqual(self.sale_order.total_ordered_weight, 17.0)
 
     def test_sale_delivered_weight(self):
         self.sale_order.action_confirm()
-        self.sale_order_line_1._onchange_weight()
-        self.sale_order_line_2._onchange_weight()
         picking_1 = self.sale_order.picking_ids
         self.assertEqual(len(picking_1.move_lines), 2)
         picking_1.action_confirm()
@@ -49,9 +48,6 @@ class TestSaleOrderWeight(common.TransactionCase):
         self.assertEqual(self.sale_order.total_delivered_weight, 9.0)
 
     def test_sale_order_recalculate_weight(self):
-        self.sale_order.action_confirm()
-        self.sale_order_line_1._onchange_weight()
-        self.sale_order_line_2._onchange_weight()
         self.product_1.weight = 10.0  # * 2 = 20 kg
         self.sale_order.recalculate_weight()
         self.assertEqual(self.sale_order.total_ordered_weight, 32.0)
