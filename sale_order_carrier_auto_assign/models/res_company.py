25bfb63c1970b6915f81b0a6cc7f1a5e268edaaf
Telmo Santos <telmo.santos@camptocamp.com>
2023-12-07 07:59:16 +0100

    [IMP] sale_order_carrier_auto_assigni: add settings

---
+++ sale_order_carrier_auto_assign/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    carrier_auto_assign = fields.Boolean()
