2cd6f1bb792c8785a4156d3a788e9d1d2f8d4272
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-12-12 12:53:14 +0100

    [FIX] sale_stock_return_request: Exclude section and notes from _get_returnable_lines
    If not, you get the following error:
    
    AssertionError: precision_rounding must be positive, got 0.0
    
    TT46472

--- sale_stock_return_request/__manifest__.py
+++ sale_stock_return_request/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Stock Return Request",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.1.1",
     "category": "Stock",
     "website": "https://github.com/OCA/sale-workflow",
     "development_status": "Beta",
