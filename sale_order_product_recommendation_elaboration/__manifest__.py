7ec88764b73083f5da90be86db8d6b8e25a7d201
Jairo Llopis <jairo@moduon.team>
2023-12-15 11:03:27 +0000

    [ADD] sale_order_product_recommendation_elaboration
    New moduule to include elaborations when generating or accepting sale order product recommendations.
    
    @moduon MT-4472

---
+++ sale_order_product_recommendation_elaboration/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Moduon Team S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "Sale order product recommendation with elaborations",
+    "summary": (
+        "Include elaborations when generating or accepting sale order "
+        "product recommendations"
+    ),
+    "version": "16.0.1.0.1",
+    "development_status": "Alpha",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["rafaelbn", "yajo"],
+    "license": "AGPL-3",
+    "depends": [
+        "sale_order_product_recommendation",
+        "sale_elaboration",
+    ],
+    "data": [
+        "wizards/sale_order_recommendation_view.xml",
+    ],
+}
