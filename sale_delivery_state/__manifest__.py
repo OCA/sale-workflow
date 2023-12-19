ad4c714617735d15b9c5391681c2c82f32fa3d4f
manu <manuel.regidor@sygel.es>
2023-12-19 11:32:05 +0100

    [IMP]sale_delivery_state: Skip sale order lines for delivery state

--- sale_delivery_state/__manifest__.py
+++ sale_delivery_state/__manifest__.py
@@ -14,6 +14,7 @@
     "depends": ["sale"],
     "data": [
         "views/sale_order_views.xml",
+        "views/res_config_settings_views.xml",
     ],
     "demo": [
         "demo/sale_demo.xml",
