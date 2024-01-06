f89051d69026cf7112774825be7e52ab36bfb9df
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-01-06 14:00:44 +0100

    [IMP] sale_order_secondary_unit: Require secondary_uom_id when secondary_uom_qty is filled

--- sale_order_secondary_unit/__manifest__.py
+++ sale_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Secondary Unit",
     "summary": "Sale product in a secondary unit",
-    "version": "15.0.2.0.1",
+    "version": "15.0.2.1.0",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
