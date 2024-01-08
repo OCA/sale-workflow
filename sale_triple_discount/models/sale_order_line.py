4f592900e39df032b729e40716244f953c10f507
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-01-08 21:15:23 +0100

    sale_triple_discount: Consolidate discount in std field
    Until now, the triple discount feature did use the standard Odoo field
    as the first discount field, adding only extra fields for the second
    and the third discount. This implied we had to override any function
    using discount to consider the other discounts properly.
    
    By adding an extra field discount1 to store the first discount, it allows
    to redefine the standard discount field to a computed field that will
    consolidate the triple discount from the other fields, and avoid the
    need to redefine anything relying on the discount field as it will
    already consider the triple discounts.

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -13,6 +13,15 @@ from odoo.exceptions import ValidationError
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    discount = fields.Float(
+        string="Total discount",
+        readonly=True,
+    )
+    discount1 = fields.Float(
+        string="Disc. 1 (%)",
+        digits="Discount",
+        default=0.0,
+    )
     discount2 = fields.Float(
         string="Disc. 2 (%)",
         digits="Discount",
@@ -64,15 +73,27 @@ class SaleOrderLine(models.Model):
 
     @api.model
     def _discount_fields(self):
-        return ["discount", "discount2", "discount3"]
+        return ["discount1", "discount2", "discount3"]
 
-    @api.depends("discount2", "discount3", "discounting_type")
-    def _compute_amount(self):
-        with self._aggregated_discount() as lines:
-            res = super(SaleOrderLine, lines)._compute_amount()
-        return res
+    # pylint: disable=W8110
+    @api.depends("discount1", "discount2", "discount3", "discounting_type")
+    def _compute_discount(self):
+        super()._compute_discount()
+        for line in self:
+            # Reset discount if not done in super
+            if not (
+                line.order_id.pricelist_id
+                and line.order_id.pricelist_id.discount_policy == "without_discount"
+            ):
+                line.discount = 0.0
+            line.discount += line._get_final_discount()
 
     _sql_constraints = [
+        (
+            "discount1_limit",
+            "CHECK (discount1 <= 100.0)",
+            "Discount 1 must be lower or equal than 100%.",
+        ),
         (
             "discount2_limit",
             "CHECK (discount2 <= 100.0)",
@@ -91,51 +112,14 @@ class SaleOrderLine(models.Model):
         more discount fields to the invoice lines
         """
         res = super()._prepare_invoice_line(**kwargs)
-        res.update({"discount2": self.discount2, "discount3": self.discount3})
+        if self.discounting_type == "multiplicative":
+            res.update(
+                {
+                    "discount1": self.discount1,
+                    "discount2": self.discount2,
+                    "discount3": self.discount3,
+                }
+            )
+        else:
+            res.update({"discount1": self.discount})
         return res
-
-    @contextmanager
-    def _aggregated_discount(self):
-        """A context manager to temporarily change the discount value on the
-        records and restore it after the context is exited. It temporarily
-        changes the discount value to the aggregated discount value so that
-        methods that depend on the discount value will use the aggregated
-        discount value instead of the original one.
-        """
-        discount_field = self._fields["discount"]
-        # Protect discount field from triggering recompute of totals. We don't want
-        # to invalidate the cache to avoid to flush the records to the database.
-        # This is safe because we are going to restore the original value at the end
-        # of the method.
-        with self.env.protecting([discount_field], self):
-            old_values = {}
-            for line in self:
-                old_values[line.id] = line.discount
-                aggregated_discount = line._get_final_discount()
-                line.update({"discount": aggregated_discount})
-            yield self.with_context(discount_is_aggregated=True)
-            for line in self:
-                if line.id not in old_values:
-                    continue
-                line.with_context(
-                    restoring_triple_discount=True,
-                ).update({"discount": old_values[line.id]})
-
-    def _convert_to_tax_base_line_dict(self):
-        self.ensure_one()
-        discount = (
-            self.discount
-            if self.env.context.get("discount_is_aggregated")
-            else self._get_final_discount()
-        )
-        return self.env["account.tax"]._convert_to_tax_base_line_dict(
-            self,
-            partner=self.order_id.partner_id,
-            currency=self.order_id.currency_id,
-            product=self.product_id,
-            taxes=self.tax_id,
-            price_unit=self.price_unit,
-            quantity=self.product_uom_qty,
-            discount=discount,
-            price_subtotal=self.price_subtotal,
-        )
