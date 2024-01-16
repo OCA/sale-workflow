bb619a801bb08d5c555e5f9d8858e6ea7092f51d
Dmytro Pavlov <dmitriy.paulov@gmail.com>
2024-01-16 19:35:40 +0200

    [16.0][ADD] sale_optional_product_quantity: add option quantity computation
    [16.0][ADD] sale_optional_product_quantity: add unmerged dep
    
    [16.0][FIX] sale_optional_product_quantity: fix default quantity assignment
    
    [16.0][FIX] sale_optional_product_quantity: fix quantity issue
    
    [FIX] sale_optional_product_quantity: fix logic according to requirements
    
    [FIX] sale_optional_product_quantity: add only options without configurator
    
    [REM] sale_optional_product_quantity: remove merged dependency

---
+++ sale_optional_product_quantity/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Optional Product Quantity",
+    "version": "16.0.1.0.0",
+    "category": "Sales Management",
+    "summary": "Use optional product quantities in quotations and sales orders",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": [
+        "product_optional_product_quantity",
+        "sale_management",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ sale_optional_product_quantity/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2024 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import sale_order_option
+from . import sale_order_line
+from . import sale_order
