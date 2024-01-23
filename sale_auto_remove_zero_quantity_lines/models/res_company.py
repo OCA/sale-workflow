d8762b3e3a45936625203e52637bfe8c3ff35d72
Telmo Santos <telmo.santos@camptocamp.com>
2024-01-23 10:08:37 +0100

    [ADD] sale_auto_remove_zero_quantity_lines

---
+++ sale_auto_remove_zero_quantity_lines/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Camptocamp (<https://www.camptocamp.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    sale_auto_remove_zero_quantity_lines = fields.Boolean()
---
+++ sale_auto_remove_zero_quantity_lines/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_sale_auto_remove_zero_quantity_lines
