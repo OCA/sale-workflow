8cb3a3bae4b4450b6092c9caf09ce40dcb7d3908
Daniel Reis <dreis@opensourceintegrators.com>
2024-01-27 09:13:24 +0000

    [ADD] sale_order_revision_replace_stock: new Sale Order revisions replace previous ones and keep Delivery info

---
+++ sale_order_revision_replace_stock/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale order revisions keep Deliveries",
+    "summary": "New revisions keep the link to Delivery Orders",
+    "version": "14.0.1.0.0",
+    "category": "Sale Management",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": ["sale_order_revision_replace", "stock"],
+    "installable": True,
+    "auto_install": True,
+}
