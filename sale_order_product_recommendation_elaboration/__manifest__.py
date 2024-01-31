bb27ba5556d3f5408695d6590de58aa1ad0d55b2
Jairo Llopis <jairo@moduon.team>
2024-01-31 09:48:38 +0000

    [FIX] sale_order_product_recommendation: AccessError when getting recommendations
    A simple salesperson couldn't access recommendations after https://github.com/OCA/sale-workflow/pull/2913 was merged.
    
    It turns out that these tests were not using the testing user they created, although it seemed like so.
    
    Now I'm reducing the permissions for the user that runs the tests, and thus can fix the issue and get it tested.
    
    @moduon MT-4941

--- sale_order_product_recommendation_elaboration/__manifest__.py
+++ sale_order_product_recommendation_elaboration/__manifest__.py
@@ -7,7 +7,7 @@
         "Include elaborations when generating or accepting sale order "
         "product recommendations"
     ),
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "development_status": "Alpha",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
