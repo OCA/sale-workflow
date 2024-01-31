a1429dc3c5982925b8a93cbfcde7aa073e5711cd
Florian Mounier <paradoxxx.zero@gmail.com>
2024-01-31 14:31:46 +0100

    [ADD] sale_order_qty_to_invoice_cancelled

---
+++ sale_order_qty_to_invoice_cancelled/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Qty to Invoice For Cancelled",
+    "summary": "This module sets the sale order quantity to invoice to "
+    "the quantity invoiced when the sale order is cancelled on lines with "
+    "order as invoice_policy.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["sale"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+}
