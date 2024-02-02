9f86dd2db18f563e726c2e9148d081fe275ba565
chien <chien@trobz.com>
2024-02-02 15:30:21 +0700

    [MIG] sale_exception: Migration to 17.0

--- sale_exception/__manifest__.py
+++ sale_exception/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Sale Exception",
     "summary": "Custom exceptions on sale order",
-    "version": "16.0.1.2.0",
+    "version": "17.0.1.0.0",
     "category": "Generic Modules/Sale",
     "author": "Akretion, "
     "Sodexis, "
