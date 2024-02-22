0c03e447c9087e041756d5525f1412d7f2168f50
FSchiepers <96426358+FSchiepers@users.noreply.github.com>
2024-02-22 10:50:52 +0100

    [17.0][MIG] Sale Discount Display Amount: Migration to 17.0

---
+++ sale_discount_display_amount/models/res_config_settings.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    display_discount_with_tax = fields.Boolean(
+        string="Show the Discount with TAX",
+        help='Check this field to show the Discount with TAX',
+        related="company_id.display_discount_with_tax",
+        readonly=False,
+    )
+    
