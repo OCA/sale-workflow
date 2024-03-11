e8460af1465d9097aa905b601ecff11402916460
ntsirintanis <ntsirintanis@therp.nl>
2024-03-11 10:50:43 +0100

    [ADD] sale_forecast

---
+++ sale_forecast/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Therp BV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Forecast",
+    "summary": "Sale forecast report",
+    "version": "16.0.1.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "depends": ["stock_demand_estimate", "stock_demand_estimate_matrix"],
+    "license": "AGPL-3",
+}
