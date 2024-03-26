ec851a8afbe993b7922b627638814c456f3f72ff
Nils Coenen <74965194+NICO-SOLUTIONS@users.noreply.github.com>
2024-03-26 10:57:57 +0100

    [MIG] sale_product_set: Migration to 17.0

--- sale_product_set/__manifest__.py
+++ sale_product_set/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Sale",
     "license": "AGPL-3",
     "author": "Anybox, Odoo Community Association (OCA)",
-    "version": "16.0.3.0.0",
+    "version": "17.0.1.0.0",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": ["sale", "sale_management", "product_set"],
     "data": [
