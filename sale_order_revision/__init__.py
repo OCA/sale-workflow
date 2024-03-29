467ef62e31160442037d95d9627a3c2c9c806145
siemenv <siemen.vandervoort@dynapps.be>
2024-03-29 08:48:01 +0100

    [MIG] sale_order_revision: Migration to 17.0

--- sale_order_revision/__init__.py
+++ sale_order_revision/__init__.py
@@ -2,8 +2,8 @@
 from . import models
 
 
-def populate_unrevisioned_name(cr, registry):
-    cr.execute(
+def populate_unrevisioned_name(env):
+    env.cr.execute(
         "UPDATE sale_order "
         "SET unrevisioned_name = name "
         "WHERE unrevisioned_name is NULL"
