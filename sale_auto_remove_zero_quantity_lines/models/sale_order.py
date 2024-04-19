c79b5f78ee396051b74ea9ba88bf97b5d14a16eb
Tran Anh Tuan <anhtuan@trobz.com>
2024-04-19 11:10:42 +0700

    [IMP] sale_auto_remove_zero_quantity_lines: Remove empty note in sale order

--- sale_auto_remove_zero_quantity_lines/models/sale_order.py
+++ sale_auto_remove_zero_quantity_lines/models/sale_order.py
@@ -14,14 +14,15 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         for order in self:
             if order._should_auto_remove_zero_quantity_lines():
-                zero_lines = order.order_line.filtered(
-                    lambda line: line.product_id and line.product_uom_qty == 0
+                zero_or_empty_lines = order.order_line.filtered(
+                    lambda line: (line.product_id and line.product_uom_qty == 0)
+                    or (line.display_type == "line_note" and not line.name.strip())
                 )
-                if zero_lines:
+                if zero_or_empty_lines:
                     body = _(
-                        "Some sale order lines with zero quantities were removed upon "
-                        "confirmation."
+                        "Some lines with zero quantities or empty notes were "
+                        "removed upon confirmation."
                     )
                     order.message_post(body=body)
-                    zero_lines.unlink()
+                    zero_or_empty_lines.unlink()
         return super().action_confirm()
