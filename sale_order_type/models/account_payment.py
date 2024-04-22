d2f67750e9fed64827bce9d2c8ae6694d75113d1
cesar <cesar@Trescloud.myguest.virtualbox.org>
2024-04-22 15:31:40 -0500

    [REF][CL] Refactoring

--- sale_order_type/models/account_payment.py
+++ sale_order_type/models/account_payment.py
@@ -1,20 +1,22 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import api, models
 
 
 class AccountPayment(models.Model):
-    _inherit = 'account.payment'
+    _inherit = "account.payment"
 
-    @api.depends('available_partner_bank_ids', 'journal_id')
+    @api.depends("available_partner_bank_ids", "journal_id")
     def _compute_partner_bank_id(self):
-        '''
+        """
         If the selected account is in the available accounts then the change is not made.
-        '''
+        """
         partner_bank_by_pay = {}
         for payment in self:
-                partner_bank_by_pay[payment.id] = payment.partner_bank_id
+            partner_bank_by_pay[payment.id] = payment.partner_bank_id
         res = super(AccountPayment, self)._compute_partner_bank_id()
         for payment in self:
-            if partner_bank_by_pay.get(payment.id, False) and partner_bank_by_pay.get(payment.id) in payment.available_partner_bank_ids:
+            if (partner_bank_by_pay.get(payment.id, False) 
+                and partner_bank_by_pay.get(payment.id) 
+                in payment.available_partner_bank_ids):
                 payment.partner_bank_id = partner_bank_by_pay.get(payment.id)
         return res
+    
