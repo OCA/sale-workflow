267de6bd4be440a3e9807aaf89701b2e1c6e580d
Christopher Rogos <crogos@gmail.com>
2024-04-24 16:24:11 +0000

    [MIG] sale_order_qty_change_no_recompute: Migration to 17.0

--- sale_order_qty_change_no_recompute/__manifest__.py
+++ sale_order_qty_change_no_recompute/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Qty change no recompute",
     "summary": "Prevent recompute if only quantity has changed in sale order line",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sale Workflow",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
