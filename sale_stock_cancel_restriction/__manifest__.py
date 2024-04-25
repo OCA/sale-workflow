72fe7dca3b51d465bbda7ed38c1b84db0555c289
Deriman-Alonso <d.alonso@binhex.cloud>
2024-04-25 09:29:29 +0100

    [IMP] sale_stock_cancel_restriction: pre-commit auto fixes

--- sale_stock_cancel_restriction/__manifest__.py
+++ sale_stock_cancel_restriction/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Stock Cancel Restriction",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales Management",
