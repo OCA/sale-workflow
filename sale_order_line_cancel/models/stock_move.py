8e92f13bd646d996111dc8e3bee4501b8b4ec8f5
Jacques-Etienne Baudoux <je@bcim.be>
2024-05-08 13:13:28 +0200

    [IMP] sale_order_line_cancel: support kits

--- sale_order_line_cancel/models/stock_move.py
+++ sale_order_line_cancel/models/stock_move.py
@@ -1,4 +1,5 @@
 # Copyright 2023 ACSONE SA/NV
+# Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -13,8 +14,14 @@ class StockMove(models.Model):
             lambda m: m.sale_line_id and m.state not in ("done", "cancel")
         )
         res = super()._action_cancel()
-        for rec in sale_moves:
-            if rec.state != "cancel":
-                continue
-            rec.sale_line_id.product_qty_canceled = rec.product_uom_qty
+        sale_lines = sale_moves.filtered(lambda m: m.state == "cancel").sale_line_id
+        sale_lines._update_qty_canceled()
         return res
+
+    def _action_done(self, cancel_backorder=False):
+        moves_todo = super()._action_done(cancel_backorder=cancel_backorder)
+        if cancel_backorder and moves_todo:
+            # _action_cancel is called before marking as done, so the hook on
+            # _action_cancel will not be triggered. Call it now
+            self.sale_line_id._update_qty_canceled()
+        return moves_todo
