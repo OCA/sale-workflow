6d99dd94e1ca3f5b6d398b10f980ee0bd74d6d27
Deriman-Alonso <d.alonso@binhex.cloud>
2024-05-08 13:26:46 +0100

    [MIG] sale_partner_selectable_option: Migration to 17.0

--- sale_partner_selectable_option/__manifest__.py
+++ sale_partner_selectable_option/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Partner Selectable Option",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
