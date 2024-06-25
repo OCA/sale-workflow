2acf9984cfb445240ab481453497712bcb7ee228
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-06-25 12:57:41 +0200

    sale_pricelist_from_commitment_date: avoid calling several times super()

--- sale_pricelist_from_commitment_date/models/sale_order_line.py
+++ sale_pricelist_from_commitment_date/models/sale_order_line.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+from odoo.tools import groupby
 
 
 class SaleOrderLine(models.Model):
@@ -15,10 +16,12 @@ class SaleOrderLine(models.Model):
         "order_id.pricelist_id.price_based_on_delivery_date",
     )
     def _compute_price_unit(self):
-        for line in self:
-            date = line.order_id._get_pricelist_date()
-            line = line.with_context(force_pricelist_date=date)
-            super(SaleOrderLine, line)._compute_price_unit()
+        for order, lines in groupby(self, key=lambda line: line.order_id):
+            date = order._get_pricelist_date()
+            lines_ = (
+                self.browse().concat(*lines).with_context(force_pricelist_date=date)
+            )
+            super(SaleOrderLine, lines_)._compute_price_unit()
         return True
 
     @api.depends(
@@ -29,8 +32,10 @@ class SaleOrderLine(models.Model):
         "order_id.pricelist_id.price_based_on_delivery_date",
     )
     def _compute_pricelist_item_id(self):
-        for line in self:
-            date = line.order_id._get_pricelist_date()
-            line = line.with_context(force_pricelist_date=date)
-            super(SaleOrderLine, line)._compute_pricelist_item_id()
+        for order, lines in groupby(self, key=lambda line: line.order_id):
+            date = order._get_pricelist_date()
+            lines_ = (
+                self.browse().concat(*lines).with_context(force_pricelist_date=date)
+            )
+            super(SaleOrderLine, lines_)._compute_pricelist_item_id()
         return True
