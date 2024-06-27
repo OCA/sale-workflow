e7d546a6bc701f3b19849d9b603f4009dcc36925
Mikheil Barnaveli <barnaveli@hmg.software>
2024-06-27 14:15:22 +0400

    [MIG] Sale_Stock_Delivery_Address: Migration to 17.0 and making requested changes

---
+++ sale_stock_delivery_address/models/sale_order.py
@@ -0,0 +1,18 @@
+# Copyright 2020-22 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.model
+    def _prepare_procurement_group_by_line(self, line):
+        vals = super(SaleOrder, self)._prepare_procurement_group_by_line(line)
+        if line._get_procurement_group_key()[0] == 16 and line.dest_address_id:
+            name_extension = (
+                line.dest_address_id.name or line.dest_address_id.contact_address
+            )
+            vals["name"] = "/".join([vals["name"], name_extension])
+        return vals
