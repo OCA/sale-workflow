ce037bf2e253f4b4d64626867773560812be5768
Renato Lima <renatonlima@gmail.com>
2024-07-02 17:16:55 -0300

    Added amount_untaxed_curr and amount_tax

--- sale_company_currency/models/sale_order.py
+++ sale_company_currency/models/sale_order.py
@@ -14,6 +14,22 @@ class SaleOrder(models.Model):
         readonly=True,
         store=True,
     )
+    amount_untaxed_curr = fields.Monetary(
+        string="Total Amount Untaxed",
+        readonly=True,
+        help="Sale Order Amount untaxed in the company Currency",
+        compute="_compute_amount_company",
+        currency_field="company_currency_id",
+        store=True,
+    )
+    amount_tax_curr = fields.Monetary(
+        string="Total Amount Tax",
+        readonly=True,
+        help="Sale Order Amount taxes in the company Currency",
+        compute="_compute_amount_company",
+        currency_field="company_currency_id",
+        store=True,
+    )
     amount_total_curr = fields.Monetary(
         string="Total Amount",
         readonly=True,
@@ -27,7 +43,13 @@ class SaleOrder(models.Model):
     def _compute_amount_company(self):
         for order in self:
             if order.currency_id.id == order.company_id.currency_id.id:
+                to_untaxed = order.amount_untaxed
+                to_tax = order.amount_tax
                 to_amount = order.amount_total
             else:
+                to_untaxed = order.amount_untaxed / order.currency_rate
+                to_tax = order.amount_tax / order.currency_rate
                 to_amount = order.amount_total / order.currency_rate
+            order.amount_untaxed_curr = to_untaxed
+            order.amount_tax_curr = to_tax
             order.amount_total_curr = to_amount
