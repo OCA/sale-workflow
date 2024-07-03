da3be3edabe139ede192a1289b9292ebe25586e3
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-07-03 11:48:35 +0200

    fixup! fixup! fixup! sale_pricelist_from_commitment_date: enable feature on pricelists

--- sale_pricelist_from_commitment_date/__manifest__.py
+++ sale_pricelist_from_commitment_date/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Pricelist From Commitment Date",
     "summary": "Use sale order commitment date to compute line price from pricelist",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Camptocamp, Odoo Community Association (OCA)",
