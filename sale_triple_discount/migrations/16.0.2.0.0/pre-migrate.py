fb51c098f082c4cf0972e299cf60168c93dd352d
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-07-12 18:06:10 +0200

    Fix migration script import

--- sale_triple_discount/migrations/16.0.2.0.0/pre-migrate.py
+++ sale_triple_discount/migrations/16.0.2.0.0/pre-migrate.py
@@ -1,10 +1,11 @@
 # Copyright 2024 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-import openupgrade
+from openupgradelib import openupgrade
 
 
-def migrate_discount_to_discount1():
+def migrate_discount_to_discount1(env):
     openupgrade.add_fields(
+        env,
         [
             (
                 "discount1",
@@ -18,6 +19,7 @@ def migrate_discount_to_discount1():
         ]
     )
     openupgrade.logged_query(
+        env.cr,
         """
         UPDATE sale_order_line
         SET discount1 = discount;
@@ -25,5 +27,6 @@ def migrate_discount_to_discount1():
     )
 
 
-def migrate(cr, version):
-    migrate_discount_to_discount1()
+@openupgrade.migrate()
+def migrate(env, version):
+    migrate_discount_to_discount1(env)
