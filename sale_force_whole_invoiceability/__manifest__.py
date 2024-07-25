6de5101e62951ba42d54ef9e1ff324d32d090d77
Roger <roger.sans@sygel.es>
2024-07-25 17:41:23 +0200

    [MIG]sale_force_whole_invoiceability: Migration to 17.0

--- sale_force_whole_invoiceability/__manifest__.py
+++ sale_force_whole_invoiceability/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Force Whole Invoiceability",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sales",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
