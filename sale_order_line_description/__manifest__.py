39b0faa32a92002a17f9a86c65f9ca573e1b8fbe
Laszlo Krajnikovszkij <laszlo@laszlo.is>
2024-07-29 12:26:19 +0200

    [IMP] sale_order_line_description: pre-commit stuff

--- sale_order_line_description/__manifest__.py
+++ sale_order_line_description/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale order line description",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "author": "Agile Business Group, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
