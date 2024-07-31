81d59214927854ec6037a59d5cb2a0da3eeef1c9
Kev-Roche <kevin.roche@akretion.com>
2024-07-31 15:30:22 +0200

    [IMP] fixes from review

--- sale_warehouse_rule/models/sale_warehouse_rule.py
+++ sale_warehouse_rule/models/sale_warehouse_rule.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleWarehouseRule(models.Model):
@@ -10,17 +10,8 @@ class SaleWarehouseRule(models.Model):
     _description = "Sale Warehouse Rule"
 
     warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Warehouse")
-    company_id = fields.Many2one(comodel_name="res.company", string="Company")
-    product_id = fields.Many2one(domain="[('id', 'in', available_product_ids)]")
-    available_product_ids = fields.Many2many(
-        comodel_name="product.product",
-        string="Available Products",
-        compute="_compute_available_product_ids",
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        default=lambda self: self.env.company,
     )
-
-    @api.depends("product_tmpl_id")
-    def _compute_available_product_ids(self):
-        for rec in self:
-            rec.available_product_ids = self.env["product.product"].search(
-                [("product_tmpl_id", "=", self.product_tmpl_id._origin.id)]
-            )
