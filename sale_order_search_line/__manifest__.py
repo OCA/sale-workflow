f31572bf35f3ac51fe44b7b219883b777a08a6fe
Aungkokolin1997 <lin@quartile.co>
2024-08-21 08:42:09 +0000

    [ADD] sale_order_search_line

---
+++ sale_order_search_line/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Search Line",
+    "version": "16.0.1.0.0",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
