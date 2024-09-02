05e392f17be755abdc4748144e16ce0ff55491ad
sergio-teruel <sergio.teruel@tecnativa.com>
2024-09-02 11:47:03 +0200

    [FIX] sale_elaboration, product_supplierinfo_for_customer_elaboration: Set elaboration note with a onchange instead of compute method

--- sale_elaboration/tests/test_sale_elaboration.py
+++ sale_elaboration/tests/test_sale_elaboration.py
@@ -120,6 +120,14 @@ class TestSaleElaboration(AccountTestInvoicingCommon):
         self.order.order_line.elaboration_ids = self.elaboration_b
         self.assertEqual(self.order.order_line.elaboration_note, "Some details")
 
+    def test_sale_elaboration_change(self):
+        order_form = Form(self.order)
+        with order_form.order_line.edit(0) as line:
+            line.elaboration_ids.clear()
+            line.elaboration_ids.add(self.elaboration_b)
+        order_form.save()
+        self.assertEqual(self.order.order_line.elaboration_note, "Elaboration B")
+
     def test_sale_elaboration(self):
         self.order.action_confirm()
         self.order.picking_ids.move_ids.quantity = 10.0
