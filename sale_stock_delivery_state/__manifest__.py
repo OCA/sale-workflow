07643da19d9439d882016d08d8d662b7bfff6afd
andrea <ast@360erp.com>
2024-09-25 13:08:22 +0200

    [MIG] sale_stock_delivery_state: migrate to V17

--- sale_stock_delivery_state/__manifest__.py
+++ sale_stock_delivery_state/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Sale Stock Delivery State",
     "summary": "Change the way to compute the delivery state",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/sale-workflow",
--- sale_stock_delivery_state/hooks.py
+++ sale_stock_delivery_state/hooks.py
@@ -3,11 +3,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-from odoo import SUPERUSER_ID, api
-
-
-def post_init_hook(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        sales = env["sale.order"].search([])
-        sales._compute_oca_delivery_status()
+def post_init_hook(env):
+    sales = env["sale.order"].search([])
+    sales._compute_oca_delivery_status()
