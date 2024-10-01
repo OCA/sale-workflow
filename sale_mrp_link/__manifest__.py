1a1e0176ac4da4ede0ad8fa4299a5764f1ec7c5f
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-10-01 10:36:53 +0200

    [IMP] sale_mrp_link: pre-commit stuff

--- sale_mrp_link/__manifest__.py
+++ sale_mrp_link/__manifest__.py
@@ -3,22 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale MRP Link',
-    'summary': 'Show manufacturing orders generated from sales order',
-    'version': '12.0.1.0.1',
-    'development_status': 'Production/Stable',
-    'category': 'Sales Management',
-    'website': 'https://github.com/OCA/sale-workflow',
-    'author': 'Akretion, Agile Business Group, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'sale_mrp',
+    "name": "Sale MRP Link",
+    "summary": "Show manufacturing orders generated from sales order",
+    "version": "12.0.1.0.1",
+    "development_status": "Production/Stable",
+    "category": "Sales Management",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Akretion, Agile Business Group, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_mrp",
     ],
-    'data': [
-        'views/mrp_production.xml',
-        'views/sale_order.xml',
+    "data": [
+        "views/mrp_production.xml",
+        "views/sale_order.xml",
     ],
 }
--- sale_mrp_link/models/mrp_production.py
+++ sale_mrp_link/models/mrp_production.py
@@ -6,28 +6,29 @@ from odoo import api, fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     sale_order_id = fields.Many2one(
-        comodel_name='sale.order', string='Source Sale Order')
+        comodel_name="sale.order", string="Source Sale Order"
+    )
 
     @api.model
     def create(self, values):
-        if 'origin' in values:
+        if "origin" in values:
             # Checking first if this comes from a 'sale.order'
-            sale_id = self.env['sale.order'].search([
-                ('name', '=', values['origin'])
-            ], limit=1)
+            sale_id = self.env["sale.order"].search(
+                [("name", "=", values["origin"])], limit=1
+            )
             if sale_id:
-                values['sale_order_id'] = sale_id.id
+                values["sale_order_id"] = sale_id.id
                 if sale_id.client_order_ref:
-                    values['origin'] = sale_id.client_order_ref
+                    values["origin"] = sale_id.client_order_ref
             else:
                 # Checking if this production comes from a route
-                production_id = self.env['mrp.production'].search([
-                    ('name', '=', values['origin'])
-                ])
+                production_id = self.env["mrp.production"].search(
+                    [("name", "=", values["origin"])]
+                )
                 # If so, use the 'sale_order_id' from the parent production
-                values['sale_order_id'] = production_id.sale_order_id.id
+                values["sale_order_id"] = production_id.sale_order_id.id
 
         return super(MrpProduction, self).create(values)
