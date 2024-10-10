cd90a4220e9ce64a07371b40c47dba2199375195
Pablo Calvo Espinar <pablo.calvo@factorlibre.com>
2024-10-10 12:55:55 +0200

    [IMP] sale_loyalty_exclude: restrict returned programs if loyalty_exclusions
    * Inherit get_claimable_rewards and try_apply_program to restrict the returned rewards
    if the product have the check loyalty_exclude active in order to hide the claim button
    in the website.

--- sale_loyalty_exclude/models/sale_order.py
+++ sale_loyalty_exclude/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Camptocamp (<https://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import _, models
 
 
 class SaleOrder(models.Model):
@@ -14,3 +14,22 @@ class SaleOrder(models.Model):
         self.ensure_one()
         lines = self.order_line.filtered(lambda line: line.product_id.loyalty_exclude)
         return lines | super()._get_no_effect_on_threshold_lines()
+
+    def _has_all_products_loyalty_exclude(self):
+        """Check if all order lines have loyalty_exclude set to True."""
+        for line in self.order_line:
+            if not line.product_id.loyalty_exclude:
+                return False
+        return True
+
+    def _get_claimable_rewards(self, forced_coupons=None):
+        if self._has_all_products_loyalty_exclude():
+            return {}
+        else:
+            return super()._get_claimable_rewards(forced_coupons=forced_coupons)
+
+    def _try_apply_program(self, program, coupon=None):
+        if self._has_all_products_loyalty_exclude():
+            return {"error": _("This code is invalid.")}
+        else:
+            return super()._try_apply_program(program, coupon=coupon)
