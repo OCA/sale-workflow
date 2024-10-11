20e74fc15589ddd8bca7ae95af11e33074ee3508
Henry Backman <henry.backman@camptocamp.com>
2024-10-11 14:14:23 +0200

    [FIX] sale_discount_display_amount: config views fixes
    fix issues with config settings views after forward porting

--- sale_discount_display_amount/models/res_config_settings.py
+++ sale_discount_display_amount/models/res_config_settings.py
@@ -5,11 +5,14 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     display_discount_with_tax = fields.Boolean(
-        Name="Show the Discount with TAX",
+        string="Show the Discount with TAX",
         help="Check this field to show the Discount with TAX",
         related="company_id.display_discount_with_tax",
         readonly=False,
     )
     report_total_without_discount = fields.Boolean(
-        related="company_id.report_total_without_discount", readonly=False
+        string="Report Total Without Discount",
+        help='Display "Total without discount" in report',
+        related="company_id.report_total_without_discount",
+        readonly=False,
     )
