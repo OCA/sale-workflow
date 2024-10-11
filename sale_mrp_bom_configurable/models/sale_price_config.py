b1f02e7382a9b4d1aa000b7688956589563d450b
Francois Poizat <francois.poizat@gmail.com>
2024-10-11 16:19:40 +0200

    Fix computation of included category or products

--- sale_mrp_bom_configurable/models/sale_price_config.py
+++ sale_mrp_bom_configurable/models/sale_price_config.py
@@ -102,17 +102,18 @@ class SalePriceConfigLine(models.Model):
             case "include_product_or_category":
                 lines = input_line.create_bom_line_data()
                 res_id = self.included_product_or_category
+                price = 0.0
                 for line in lines:
                     if res_id._name == "product.category" and line[
                         "product_tmpl_id"
-                    ].filtered_domain([("categ_id", "child_of", res_id.id)]):
-                        return line["product_tmpl_id"].list_price * line["product_qty"]
+                    ].filtered_domain(["|", ("categ_id", "child_of", res_id.id), ("categ_id", "=", res_id.id)]):
+                        price += line["product_tmpl_id"].list_price * line["product_qty"]
                     if (
                         res_id._name == "product.template"
                         and line["product_tmpl_id"].id == res_id.id
                     ):
-                        return line["product_tmpl_id"].list_price * line["product_qty"]
-                return 0.0
+                        price += line["product_tmpl_id"].list_price * line["product_qty"]
+                return price
             case "factor":
                 factor = input_line[self.target_field.name]
                 return factor * self.amount
