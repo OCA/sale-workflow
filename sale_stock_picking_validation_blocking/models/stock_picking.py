efd81bf7c550be67d074f51ce3f1d5ca76126f86
Kevin Khao <kevinkhao@gmail.com>
2024-10-21 13:22:57 +0300

    [17.0][MIG] sale_stock_picking_validation_blocking: Migration to 17.0

--- sale_stock_picking_validation_blocking/models/stock_picking.py
+++ sale_stock_picking_validation_blocking/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -14,18 +14,10 @@ class StockPicking(models.Model):
         string="Validation Blocked by SO",
     )
 
-    @api.depends("state", "is_locked", "validation_blocked_by_so")
-    def _compute_show_validate(self):
-        for picking in self:
-            if picking.validation_blocked_by_so:
-                picking.show_validate = False
-            else:
-                super(StockPicking, picking)._compute_show_validate()
-
     def button_validate(self):
         for picking in self:
             if picking.validation_blocked_by_so:
                 raise ValidationError(
                     _("Validation is blocked by SO for picking %s" % picking.name)
                 )
-        return super(StockPicking, self).button_validate()
+        return super().button_validate()
