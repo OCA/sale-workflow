d69b4bad94c6f716b770845c2edd903b4f647da9
Franco Leyes <lef@adhoc.com.ar>
2024-11-14 13:17:53 -0300

    [MIG] sale_order_lot_selection: Migration to 18.0

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sale Order Lot Selection",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "author": "Odoo Community Association (OCA), Agile Business Group",
     "website": "https://github.com/OCA/sale-workflow",
