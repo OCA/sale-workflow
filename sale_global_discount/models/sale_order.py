c3f0ce936e6c74456fe22047467cd6f2538785ec
miguel-S73 <miguel@studio73.es>
2024-11-19 13:40:23 +0100

    [17.0][MIG] sale_global_discount: Migration to 17.0

--- sale_global_discount/models/sale_order.py
+++ sale_global_discount/models/sale_order.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, exceptions, fields, models
+from odoo.tools.misc import formatLang
 
 
 class SaleOrder(models.Model):
@@ -14,6 +15,9 @@ class SaleOrder(models.Model):
         domain="[('discount_scope', '=', 'sale'), "
         "('account_id', '!=', False), '|', "
         "('company_id', '=', company_id), ('company_id', '=', False)]",
+        compute="_compute_global_discount_ids",
+        store=True,
+        readonly=False,
     )
     # HACK: Looks like UI doesn't behave well with Many2many fields and
     # negative groups when the same field is shown. In this case, we want to
@@ -68,7 +72,7 @@ class SaleOrder(models.Model):
             return True
         taxes_keys = {}
         for line in self.order_line.filtered(
-            lambda l: not l.display_type and l.product_id
+            lambda _line: not _line.display_type and _line.product_id
         ):
             if not line.tax_id:
                 raise exceptions.UserError(
@@ -135,20 +139,83 @@ class SaleOrder(models.Model):
         return res
 
     def _compute_tax_totals(self):
-        return super(
-            SaleOrder, self.with_context(from_tax_calculation=True)
-        )._compute_tax_totals()
+        res = super()._compute_tax_totals()
+        for order in self:
+            amount_discount = 0.0
+            amount_discount_by_tax = {}
+            total_discount_tax = 0.0
+            for line in order.order_line:
+                if (
+                    line.display_type
+                    or not line.product_id
+                    or line.product_id.bypass_global_discount
+                ):
+                    continue
+                for gbl_disc in order.global_discount_ids:
+                    amount_discount += line.price_subtotal * gbl_disc.discount / 100
+                    for tax in line.tax_id:
+                        amount_discount_by_tax.setdefault(tax.tax_group_id.id, 0.0)
+                        discounted_tax = tax.compute_all(
+                            line.price_subtotal * gbl_disc.discount / 100,
+                            line.order_id.currency_id,
+                            1.0,
+                            product=line.product_id,
+                            partner=line.order_id.partner_shipping_id,
+                        )["taxes"][0]["amount"]
+                        amount_discount_by_tax[tax.tax_group_id.id] += discounted_tax
+                        total_discount_tax += discounted_tax
+            currency = order.currency_id
+            amount_untaxed = order.tax_totals["amount_untaxed"] - amount_discount
+            amount_total = (
+                order.tax_totals["amount_total"] - amount_discount - total_discount_tax
+            )
+            for tax_dict in order.tax_totals["groups_by_subtotal"].values():
+                for tax in tax_dict:
+                    tax["tax_group_base_amount"] = amount_untaxed
+                    tax["formatted_tax_group_base_amount"] = formatLang(
+                        self.env, amount_untaxed, currency_obj=currency
+                    )
+                    tax["tax_group_amount"] -= amount_discount_by_tax.get(
+                        tax["tax_group_id"], 0.0
+                    )
+                    tax["formatted_tax_group_amount"] = formatLang(
+                        self.env, tax["tax_group_amount"], currency_obj=currency
+                    )
+            order.tax_totals.update(
+                {
+                    "amount_untaxed": amount_untaxed,
+                    "formatted_amount_untaxed": formatLang(
+                        self.env, amount_untaxed, currency_obj=currency
+                    ),
+                    "amount_total": amount_total,
+                    "formatted_amount_total": formatLang(
+                        self.env, amount_total, currency_obj=currency
+                    ),
+                }
+            )
+            for subtotal in order.tax_totals["subtotals"]:
+                subtotal["amount"] = amount_untaxed
+                subtotal["formatted_amount"] = formatLang(
+                    self.env, amount_untaxed, currency_obj=currency
+                )
+        return res
 
-    @api.onchange("partner_id")
-    def onchange_partner_id_set_gbl_disc(self):
-        self.global_discount_ids = (
-            self.partner_id.customer_global_discount_ids.filtered(
-                lambda d: d.company_id == self.company_id
-            )
-            or self.partner_id.commercial_partner_id.customer_global_discount_ids.filtered(
-                lambda d: d.company_id == self.company_id
-            )
-        )
+    @api.depends("partner_id", "company_id")
+    def _compute_global_discount_ids(self):
+        for order in self:
+            commercial = order.partner_id.commercial_partner_id
+            commercial_global_disc = commercial.customer_global_discount_ids
+            partner_global_disc = order.partner_id.customer_global_discount_ids
+            discounts = self.env["global.discount"]
+            _discounts = self.env["global.discount"]
+            if partner_global_disc:
+                _discounts = partner_global_disc
+            else:
+                _discounts = commercial_global_disc
+            for discount in _discounts:
+                if discount.company_id == order.company_id:
+                    discounts |= discount
+            order.global_discount_ids = discounts
 
     def _prepare_invoice(self):
         invoice_vals = super()._prepare_invoice()
