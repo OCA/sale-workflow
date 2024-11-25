c62b8075dd71693df819a1180ab53b51eea87256
Laurent Mignonn (ACSONE) <laurent.mignon@acsone.eu>
2024-11-25 11:24:45 +0100

    [IMP] sale_stock_prebook: Prevent qty done on reservation move

--- sale_stock_prebook/models/stock_move.py
+++ sale_stock_prebook/models/stock_move.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockMove(models.Model):
@@ -8,6 +9,16 @@ class StockMove(models.Model):
 
     used_for_sale_reservation = fields.Boolean(default=False)
 
-    def _action_assign(self):
+    @api.constrains("used_for_sale_reservation", "quantity_done")
+    def _check_used_for_sale_reservation(self):
+        for move in self:
+            if move.used_for_sale_reservation and move.quantity_done:
+                raise ValidationError(
+                    _(
+                        "You cannot set a quantity done on a move used for sale reservation"
+                    )
+                )
+
+    def _action_assign(self, force_qty=None):
         new_self = self.filtered(lambda m: not m.used_for_sale_reservation)
-        return super(StockMove, new_self)._action_assign()
+        return super(StockMove, new_self)._action_assign(force_qty=force_qty)
