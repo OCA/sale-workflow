4d3c6455b9463af400cc1f06dc9a945ba84d0dd2
Raura <jcolljeyce@gmail.com>
2024-12-02 12:12:47 +0100

    [MIG] sale_force_whole_invoiceability: Migration to 18.0

--- sale_force_whole_invoiceability/models/sale_order.py
+++ sale_force_whole_invoiceability/models/sale_order.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import _, models
+from odoo import models
 from odoo.exceptions import UserError
 
 
@@ -9,17 +9,17 @@ class SaleOrder(models.Model):
 
     def force_lines_to_invoice_policy_order(self):
         """Wrapper to launch the private method from UI"""
-        if not self.user_has_groups("sales_team.group_sale_manager"):
+        if not self.env.user.has_group("sales_team.group_sale_manager"):
             raise UserError(
-                _("Only Sales Managers are allowed to force the lines to invoice")
+                self.env._("Only Sales Managers are allowed to force the lines to invoice")
             )
         if self.state != "sale":
             raise UserError(
-                _("You can't perform this action over a sale order in this state")
+                self.env._("You can't perform this action over a sale order in this state")
             )
         if self.invoice_count:
             raise UserError(
-                _(
+                self.env._(
                     "You can't perform this action once the order has been "
                     "invoiced once already"
                 )
