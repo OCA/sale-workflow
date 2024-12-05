dd654c125dd6f5929f2e8881eb59166ed1d72321
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-12-05 14:30:03 +0100

    [IMP] sale_planner_calendar: Add reorder options in kanban view

--- sale_planner_calendar/__manifest__.py
+++ sale_planner_calendar/__manifest__.py
@@ -36,7 +36,8 @@
             "sale_planner_calendar/static/src/xml/sale_planner_calendar_event_sales.xml",
             "sale_planner_calendar/static/src/xml/activity_menu_view.xml",
             "sale_planner_calendar/static/src/scss/sale_planner_calendar.scss",
-            "sale_planner_calendar/static/src/js/*.js",
+            "sale_planner_calendar/static/src/js/*.esm.js",
+            "sale_planner_calendar/static/src/xml/move_kanban_record_widget.xml",
         ],
     },
 }
