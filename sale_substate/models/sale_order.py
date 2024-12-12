573122a8889a14fdb3884577c3584bc48f681510
len <len@lambdao.dev>
2024-12-12 11:31:12 +0100

    [FIX] sale_substate: fix inheritance order
    With the current order, the super to _track_template from the mixin was never called.
    Which was a good thing since it was also buggy;
    however the override in this model was removed on the assumption that it was.

--- sale_substate/models/sale_order.py
+++ sale_substate/models/sale_order.py
@@ -13,5 +13,5 @@ class BaseSubstateType(models.Model):
 
 
 class SaleOrder(models.Model):
-    _inherit = ["sale.order", "base.substate.mixin"]
+    _inherit = ["base.substate.mixin", "sale.order"]  # order matters
     _name = "sale.order"
