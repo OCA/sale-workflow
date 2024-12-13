5c4a3597546f8674c422d8c49fec1caa515418a5
Heliconia Solutions <oca@heliconia.in>
2024-12-13 12:24:04 +0530

    [MIG] sale_order_archive: Migration to 18.0

--- sale_order_archive/__manifest__.py
+++ sale_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale_order.xml"],
