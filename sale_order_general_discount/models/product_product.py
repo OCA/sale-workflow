c8a82470f70f9ff909bdb0e09850095fedc8df95
Heliconia Solutions <oca@heliconia.in>
2024-12-13 14:52:38 +0530

    [MIG] sale_order_general_discount: Migration to 18.0

--- sale_order_general_discount/models/product_product.py
+++ sale_order_general_discount/models/product_product.py
@@ -7,7 +7,6 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     bypass_general_discount = fields.Boolean(
-        string="Don't apply general discount",
         help="If this checkbox is not ticked, it means changing general discount on "
         "sale order will impact sale order lines with this related product.",
     )
