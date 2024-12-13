cced66807196e9d8f08bcf88ddcec0e3311f1949
Heliconia Solutions <oca@heliconia.in>
2024-12-13 12:24:04 +0530

    [MIG] sale_order_archive: Migration to 18.0

--- sale_order_archive/models/sale_order.py
+++ sale_order_archive/models/sale_order.py
@@ -1,18 +1,20 @@
 # Copyright 2017-2020 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, fields, models
+from odoo import fields, models
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    active = fields.Boolean(default=True)
+    active = fields.Boolean(copy=False, default=True)
 
     def toggle_active(self):
         if self.filtered(
             lambda so: not so.locked and so.state != "cancel" and so.active
         ):
-            raise UserError(_("Only 'Locked' or 'Canceled' orders can be archived"))
+            raise UserError(
+                self.env._("Only 'Locked' or 'Canceled' orders can be archived")
+            )
         return super().toggle_active()
