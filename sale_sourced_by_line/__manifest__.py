06c4873ca637afe9916b9c0bcdb9555786c7fe8d
Ajay Javiya <ajaviya@odoogap.com>
2024-12-17 10:11:00 +0530

    [MIG] sale_sourced_by_line: Migration to 18.0
    [FIX] pylint error

--- sale_sourced_by_line/__manifest__.py
+++ sale_sourced_by_line/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Sale Sourced by Line",
     "summary": "Multiple warehouse source locations for Sale order",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp,"
     "Eficent,"
     "SerpentCS,"
