a860b10b71297ccb977b5d0da6563b3c95f2f501
Ruchir Shukla <ruchir@bizzappdev.com>
2024-12-23 11:11:57 +0530

    [MIG] sale_manual_delivery: Migration to 18.0

--- sale_manual_delivery/__manifest__.py
+++ sale_manual_delivery/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Sale",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "website": "https://github.com/OCA/sale-workflow",
     "summary": "Create manually your deliveries",
     "depends": ["stock_delivery", "sale_stock", "sales_team"],
