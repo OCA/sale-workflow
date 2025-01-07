b041d7e41bfa6003ddb242b5bf9a72db6d815f6f
tafaRU <alex.comba@agilebg.com>
2025-01-07 17:25:04 +0100

    [MIG] sale_order_line_description: Migration to 16.0

--- sale_order_line_description/models/sale_order_line.py
+++ sale_order_line_description/models/sale_order_line.py
@@ -1,28 +1,27 @@
-# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2013 Agile Business Group sagl (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.onchange("product_id")
-    def product_id_change(self):
-        res = super(SaleOrderLine, self).product_id_change()
-        if not self.product_id:  # pragma: no cover
-            return res
-        if (
-            self.user_has_groups(
-                "sale_order_line_description."
-                "group_use_product_description_per_so_line"
-            )
-            and self.product_id.description_sale
-        ):
-            product = self.product_id
-            if self.order_id.partner_id:
-                product = product.with_context(
-                    lang=self.order_id.partner_id.lang,
+    def _compute_name(self):
+        res = super()._compute_name()
+        for line in self:
+            if not line.product_id:
+                continue
+            if (
+                line.user_has_groups(
+                    "sale_order_line_description."
+                    "group_use_product_description_per_so_line"
                 )
-            self.name = product.description_sale
+                and line.product_id.description_sale
+            ):
+                product = line.product_id
+                partner = line.order_id.partner_id
+                if partner:
+                    product = product.with_context(lang=partner.lang)
+                line.name = product.description_sale
         return res
