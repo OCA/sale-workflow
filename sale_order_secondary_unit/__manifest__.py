e5bb3638da9ab15a0f29ded016603fc39e86b0a6
Matias Peralta <matias_peralta1@hotmail.com>
2025-01-15 14:24:49 -0300

    [MIG] sale_order_secondary_unit: Migration to 18.0

--- sale_order_secondary_unit/__manifest__.py
+++ sale_order_secondary_unit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Order Secondary Unit",
     "summary": "Sale product in a secondary unit",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
