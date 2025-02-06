ba38e26d6696ee1fc03ba7f888229823fc11a682
Denis Roussel <denis.roussel@acsone.eu>
2025-02-06 16:31:54 +0100

    [IMP] sale_partner_address_restrict: Use computed domain

--- sale_partner_address_restrict/models/sale_order.py
+++ sale_partner_address_restrict/models/sale_order.py
@@ -1,9 +1,31 @@
-from odoo import _, api, models
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    partner_address_restriction_domain = fields.Binary(
+        compute="_compute_partner_address_restriction_domain",
+        help="This is the computed domain to filter delivery and invoicing addresses.",
+    )
+
+    @api.depends("partner_id", "company_id.sale_partner_address_restriction")
+    def _compute_partner_address_restriction_domain(self):
+        for activated, activated_sales in self.partition(
+            lambda sale: sale.company_id.sale_partner_address_restriction
+        ).items():
+            if not activated:
+                activated_sales.partner_address_restriction_domain = [(1, "=", 1)]
+                continue
+            for company, sales in activated_sales.partition("company_id").items():
+                for sale in sales:
+                    sale.partner_address_restriction_domain = [
+                        ("commercial_partner_id", "=", sale.partner_id.id),
+                        "|",
+                        ("company_id", "=", False),
+                        ("company_id", "=", company.id),
+                    ]
 
     @api.constrains("partner_id", "partner_invoice_id", "partner_shipping_id")
     def _check_partner_addresses(self):
