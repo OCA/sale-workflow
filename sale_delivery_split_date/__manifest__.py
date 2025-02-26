0833027a86bbaf1fd749ea4f707253f6a8188915
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2025-02-26 16:43:05 +0100

    [MIG] sale_delivery_split_date: Migration to 18.0

--- sale_delivery_split_date/__manifest__.py
+++ sale_delivery_split_date/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Delivery Split Date",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Sale Deliveries split by date",
     "category": "Sales Management",
     "license": "AGPL-3",
