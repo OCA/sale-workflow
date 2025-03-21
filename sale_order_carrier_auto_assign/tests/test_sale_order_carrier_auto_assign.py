4c06c56fd9d5ecddc61b9a24baa2915c838f4662
Víctor Martínez <victor.martinez@tecnativa.com>
2025-03-21 10:28:51 +0100

    [IMP] sale_order_carrier_auto_assign: Add delivery_auto_refresh changes
    TT52316
    
    [UPD] Update sale_order_carrier_auto_assign.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: sale-workflow-17.0/sale-workflow-17.0-sale_order_carrier_auto_assign
    Translate-URL: https://translation.odoo-community.org/projects/sale-workflow-17-0/sale-workflow-17-0-sale_order_carrier_auto_assign/

--- sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
+++ sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
@@ -1,47 +1,83 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo.tests import Form, TransactionCase
+from odoo.tests import Form
 
-from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestSaleOrderCarrierAutoAssign(TransactionCase):
+class TestSaleOrderCarrierAutoAssign(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env["base"].with_context(**DISABLED_MAIL_CONTEXT).env
         cls.settings = cls.env["res.config.settings"].create({})
-        cls.settings.carrier_auto_assign = True
-        cls.settings.set_values()
-
-        cls.partner = cls.env.ref("base.res_partner_2")
-        product = cls.env.ref("product.product_product_9")
+        cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
+        cls.product = cls.env.ref("product.product_product_9")
         cls.delivery_local_delivery = cls.env.ref("delivery.delivery_local_delivery")
         cls.delivery_local_delivery.fixed_price = 10
         cls.delivery_local_delivery.free_over = False
+
+    @classmethod
+    def _create_sale_order(cls):
         sale_order_form = Form(cls.env["sale.order"])
         sale_order_form.partner_id = cls.partner
         with sale_order_form.order_line.new() as line_form:
-            line_form.product_id = product
-        cls.sale_order = sale_order_form.save()
+            line_form.product_id = cls.product
+        return sale_order_form.save()
 
-    def test_sale_order_carrier_auto_assign(self):
+    def test_partner_property_delivery_carrier_id(self):
         self.assertEqual(
             self.partner.property_delivery_carrier_id, self.delivery_local_delivery
         )
-        self.assertFalse(self.sale_order.carrier_id)
-        self.sale_order.action_confirm()
-        self.assertEqual(self.sale_order.state, "sale")
-        self.assertEqual(self.sale_order.carrier_id, self.delivery_local_delivery)
-        delivery_line = self.sale_order.order_line.filtered(
-            lambda line: line.is_delivery
-        )
-        delivery_rate = self.delivery_local_delivery.rate_shipment(self.sale_order)
+
+    def test_sale_order_carrier_auto_assign(self):
+        self.settings.carrier_auto_assign = True
+        self.settings.set_values()
+        sale_order = self._create_sale_order()
+        self.assertFalse(sale_order.carrier_id)
+        sale_order.action_confirm()
+        self.assertEqual(sale_order.state, "sale")
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+        delivery_line = sale_order.order_line.filtered(lambda line: line.is_delivery)
+        delivery_rate = self.delivery_local_delivery.rate_shipment(sale_order)
         self.assertEqual(delivery_line.price_unit, delivery_rate["carrier_price"])
 
     def test_sale_order_carrier_auto_assign_no_carrier(self):
+        self.settings.carrier_auto_assign = True
+        self.settings.carrier_on_create = True
+        self.settings.set_values()
         self.partner.property_delivery_carrier_id = False
-        self.assertFalse(self.sale_order.carrier_id)
-        self.sale_order.action_confirm()
-        self.assertEqual(self.sale_order.state, "sale")
-        self.assertFalse(self.sale_order.carrier_id)
+        sale_order = self._create_sale_order()
+        self.assertFalse(sale_order.carrier_id)
+        sale_order.action_confirm()
+        self.assertEqual(sale_order.state, "sale")
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_onchange(self):
+        self.settings.carrier_on_create = True
+        self.settings.set_values()
+        sale_order = self._create_sale_order()
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_create(self):
+        self.settings.carrier_on_create = True
+        self.settings.set_values()
+        sale_order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        self.assertEqual(sale_order.carrier_id, self.delivery_local_delivery)
+
+    def test_sale_order_carrier_auto_assign_disabled(self):
+        self.settings.carrier_on_create = False
+        self.settings.set_values()
+        sale_order = self._create_sale_order()
+        self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_carrier_auto_assign_carrier_already_set(self):
+        self.settings.carrier_on_create = True
+        self.settings.set_values()
+        carrier = self.env.ref("delivery.delivery_carrier")
+        sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "carrier_id": carrier.id,
+            }
+        )
+        self.assertEqual(sale_order.carrier_id, carrier)
