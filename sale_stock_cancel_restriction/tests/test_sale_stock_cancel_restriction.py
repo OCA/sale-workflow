e41df12e48c2ec2e60391722df149d58a6fac9a6
Mmequignon <matthieu@fwzte.xyz>
2025-03-26 16:24:12 +0100

    [MIG] sale_stock_cancel_restriction: Migration to 14.0

--- sale_stock_cancel_restriction/tests/test_sale_stock_cancel_restriction.py
+++ sale_stock_cancel_restriction/tests/test_sale_stock_cancel_restriction.py
@@ -2,18 +2,26 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import UserError
-from odoo.tests import Form, TransactionCase
+from odoo.tests import Form, tagged
+from odoo.tests.common import SavepointCase
 
 
-class TestSaleStockCancelRestriction(TransactionCase):
+@tagged("-at_install", "post_install")
+class TestSaleStockCancelRestriction(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.product = cls.env["product.product"].create(
             {"name": "Product test", "type": "product"}
         )
         cls.partner = cls.env["res.partner"].create({"name": "Partner test"})
         cls.warehouse = cls.env.ref("stock.warehouse0")
+        # Disable sale_cancel_confirm for this test, as it'll return
+        # a wizard instead of cancelling the SO, which isnt' std behavior
+        cls.env["ir.config_parameter"].sudo().set_param(
+            "sale.order.cancel_confirm_disable", "True"
+        )
 
     @classmethod
     def _create_sale_order(cls):
@@ -32,7 +40,7 @@ class TestSaleStockCancelRestriction(TransactionCase):
         """
         sale_order = self._create_sale_order()
         picking = sale_order.picking_ids
-        picking.move_ids.quantity_done = 2
+        picking.move_lines.quantity_done = 2
         picking.button_validate()
         with self.assertRaises(UserError):
             sale_order.action_cancel()
@@ -46,13 +54,10 @@ class TestSaleStockCancelRestriction(TransactionCase):
         pick_picking = sale_order.picking_ids.filtered(
             lambda p: p.picking_type_id.code == "internal"
         )
-        pick_picking.move_ids.quantity_done = 2
+        pick_picking.move_lines.quantity_done = 2
         pick_picking.button_validate()
-        wizz = sale_order.action_cancel()
-        self.assertEqual(
-            wizz["res_model"],
-            "sale.order.cancel",
-        )
+        # This shouldn't raise an error
+        sale_order.action_cancel()
 
     def test_cancel_sale_order_restrict_undelivered_shipped(self):
         # Enable restrict_sale_cancel_after_delivery, and multi step delivery.
@@ -64,13 +69,13 @@ class TestSaleStockCancelRestriction(TransactionCase):
         pick_picking = sale_order.picking_ids.filtered(
             lambda p: p.picking_type_id.code == "internal"
         )
-        pick_picking.move_ids.quantity_done = 2
+        pick_picking.move_lines.quantity_done = 2
         pick_picking.button_validate()
         # Deliver 2 units
         ship_picking = sale_order.picking_ids.filtered(
             lambda p: p.picking_type_id.code == "outgoing"
         )
-        ship_picking.move_ids.quantity_done = 2
+        ship_picking.move_lines.quantity_done = 2
         ship_picking.button_validate()
         with self.assertRaises(UserError):
             sale_order.action_cancel()
@@ -80,8 +85,6 @@ class TestSaleStockCancelRestriction(TransactionCase):
         model 'sale.order.cancel
         """
         sale_order = self._create_sale_order()
-        wizz = sale_order.action_cancel()
-        self.assertEqual(
-            wizz["res_model"],
-            "sale.order.cancel",
-        )
+        sale_order.action_cancel()
+        self.assertEqual(sale_order.state, "cancel")
+        self.assertEqual(sale_order.picking_ids.state, "cancel")
