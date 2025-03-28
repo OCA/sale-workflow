01724a7932d87093fcc007682eb3a085ceb8cc08
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-03-28 17:41:47 +0100

    [REF] sale_discount_display_amount: reduce migration duration. Rational: for the time being, computation is done for all the line of an order, if the order contains a discounted line. Now, it is done only for discounted lines

--- sale_discount_display_amount/migrations/12.0.1.2.0/post-compute-subtotal-columns.py
+++ sale_discount_display_amount/migrations/12.0.1.2.0/post-compute-subtotal-columns.py
@@ -7,21 +7,20 @@ _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    _logger.info("Compute discount columns")
     env = Environment(cr, SUPERUSER_ID, {})
 
+    _logger.info("Compute discount columns sale_order_line.price_subtotal_no_discount")
     query = """
-    update sale_order_line
-    set
-        price_subtotal_no_discount = price_subtotal
-    where discount = 0.0
+    UPDATE sale_order_line
+    SET price_subtotal_no_discount = price_subtotal
+    WHERE discount = 0.0
     """
     cr.execute(query)
 
+    _logger.info("Compute discount columns sale_order.price_subtotal_no_discount")
     query = """
-    update sale_order
-    set
-        price_subtotal_no_discount = amount_untaxed
+    UPDATE sale_order
+    SET price_subtotal_no_discount = amount_untaxed
     """
     cr.execute(query)
 
@@ -32,5 +31,10 @@ def migrate(cr, version):
     cr.execute(query)
     order_ids = cr.fetchall()
 
+    _logger.info("Search all orders with discounted lines...")
     orders = env["sale.order"].search([("id", "in", order_ids)])
-    orders.mapped("order_line")._compute_discount()
+    _logger.info(f"Found {len(orders)} orders.")
+    for i, order in enumerate(orders, start=1):
+        lines = order.mapped("order_line").filtered(lambda x: x.discount)
+        _logger.info(f"{i} / {len(orders)}. order #{order.id} computing order lines data for {len(lines)} lines.")
+        lines._compute_discount()
