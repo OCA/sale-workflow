19ec4c533a2f2debf8e921da42dbe5be5f9a61f6
sbejaoui <souheil.bejaoui@acsone.eu>
2025-03-29 13:02:56 +0100

    [REF] sale_triple_discount: Consolidate discount in std field
    inspired by https://github.com/OCA/account-invoicing/pull/1638

--- sale_triple_discount/__manifest__.py
+++ sale_triple_discount/__manifest__.py
@@ -16,4 +16,5 @@
     "depends": ["sale", "account_invoice_triple_discount"],
     "data": ["views/sale_order_report.xml", "views/sale_order_view.xml"],
     "installable": True,
+    "post_init_hook": "post_init_hook",
 }
