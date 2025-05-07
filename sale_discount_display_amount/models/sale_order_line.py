b5f4346a12a48c95d35fd055ce8670e6b3471993
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-05-07 11:39:00 +0200

    [FIX] sale_discount_display_amount: fix onchange peformance
    Before this change, when the onchange was triggered on the sale order
    following the update of value on a line, odoo replied by marking all the
    lines as dirty, which was not necessary. This was causing a performance
    issue and unpredictable behavior since the following call to onchange
    will include into the list of dirty lines all the lines even if they
    were not modified.
    
    This issue was initiated by a wrong declaration/implementation of the
    compute method for the fields `discount_total` and
    `price_total_no_discount`. The computation of these fields was initially
    done by the native `_compute_amount` method of the sale order line. But
    in fact, it depends on the price_total and discount fields of the sale
    order line. As a result, into the onchange mechanism, when these fields
    were recomputed, the system wrongly marked all the lines as dirty since
    the value of a field that was not marked as a dependency of the compute
    method and not provided into the snapshot info of lines from the UI was
    changed into the onchange process.
    
    By declaring the fields `discount_total` and `price_total_no_discount`
    as dependency of the fields `price_total` we ensure that the onchange
    mechanim will pre-compute the value into the initial snapshot of the
    lines (since the value is not provided into the information of the lines
    but some values changed in the lines required to recompute this value) and
    will not detect any changes at the end of the onchange process when
    comparing the initial and final snapshot of the lines.
    
    Prior to this change, the `price_total` was read from the database and
    when the recompute of the `discount_total` and `price_total_no_discount`
    was done, the system detected a change int the value of the field
    `price_total` that was not expected since this field was a declared
    dependency of these two fields. As a result, the system marked all the lines
    as dirty...

--- sale_discount_display_amount/models/sale_order_line.py
+++ sale_discount_display_amount/models/sale_order_line.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
@@ -9,13 +10,13 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     discount_total = fields.Monetary(
-        compute="_compute_amount",
+        compute="_compute_discount_total",
         string="Discount Subtotal",
         store=True,
         precompute=True,
     )
     price_total_no_discount = fields.Monetary(
-        compute="_compute_amount",
+        compute="_compute_discount_total",
         string="Subtotal Without Discount",
         store=True,
         precompute=True,
@@ -23,32 +24,54 @@ class SaleOrderLine(models.Model):
 
     def _update_discount_display_fields(self):
         for line in self:
-            line.price_total_no_discount = 0
-            line.discount_total = 0
+            price_total_no_discount = 0.0
+            discount_total = 0.0
+            currency = line.order_id.currency_id
             if not line.discount:
-                line.price_total_no_discount = line.price_total
-                continue
-            price = line.price_unit
-            taxes = line.tax_id.compute_all(
-                price,
-                line.order_id.currency_id,
-                line.product_uom_qty,
-                product=line.product_id,
-                partner=line.order_id.partner_shipping_id,
-            )
+                price_total_no_discount = line.price_total
+            else:
+                price = line.price_unit
+                taxes = line.tax_id.compute_all(
+                    price,
+                    currency,
+                    line.product_uom_qty,
+                    product=line.product_id,
+                    partner=line.order_id.partner_shipping_id,
+                )
 
-            price_total_no_discount = taxes["total_included"]
-            discount_total = price_total_no_discount - line.price_total
+                price_total_no_discount = taxes["total_included"]
+                discount_total = price_total_no_discount - line.price_total
+            if (
+                float_compare(
+                    line.discount_total,
+                    discount_total,
+                    precision_rounding=currency.rounding,
+                )
+                != 0
+            ):
+                line.discount_total = discount_total
+            if (
+                float_compare(
+                    line.price_total_no_discount,
+                    price_total_no_discount,
+                    precision_rounding=currency.rounding,
+                )
+                != 0
+            ):
+                line.price_total_no_discount = price_total_no_discount
 
-            line.update(
-                {
-                    "discount_total": discount_total,
-                    "price_total_no_discount": price_total_no_discount,
-                }
-            )
-
-    @api.depends("product_uom_qty", "discount", "price_unit", "tax_id")
-    def _compute_amount(self):
-        res = super(SaleOrderLine, self)._compute_amount()
+    @api.depends(
+        "discount",
+        "price_total",
+        "product_uom_qty",
+        "product_id",
+        "tax_id",
+        "price_unit",
+        "order_id.currency_id",
+        "order_id.partner_shipping_id",
+    )
+    def _compute_discount_total(self):
+        # we should move the logic from the _update_discount_display_fields
+        # to the _compute_discount_total method but to ensure the backward
+        # compatibility we keep the logic in both methods TODO in V19
         self._update_discount_display_fields()
-        return res
