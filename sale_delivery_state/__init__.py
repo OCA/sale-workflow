2fc05f70bef2eaa702febf75d4e5edb1d71aae66
Simone Orsi <simahawk@gmail.com>
2025-05-14 11:21:45 +0200

    sale_delivery_state: bring post init hook from sale_stock_delivery_state

--- sale_delivery_state/__init__.py
+++ sale_delivery_state/__init__.py
@@ -1,2 +1,2 @@
 from . import models
-from .hooks import pre_init_hook
+from .hooks import pre_init_hook, post_init_hook
--- sale_delivery_state/__manifest__.py
+++ sale_delivery_state/__manifest__.py
@@ -20,4 +20,5 @@
         "demo/sale_demo.xml",
     ],
     "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
 }
