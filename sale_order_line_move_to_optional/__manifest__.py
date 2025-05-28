7cfbf6f5be41de6d0b35bea6a21c135cb30f34cc
matteo.tognini <matteo.tognini@nextev.it>
2025-05-28 12:23:22 +0200

    [ADD]sale_order_line_move_to_optional: move sale order line to optional

---
+++ sale_order_line_move_to_optional/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Nextev Srl
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Line move to Optional",
+    "summary": "Move sale order line to optional products.",
+    "author": "Nextev Srl, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "version": "16.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_management",
+    ],
+    "data": [
+        "views/sale_order.xml",
+    ],
+}
---
+++ sale_order_line_move_to_optional/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module enables moving sale order lines to the optional products section,
+while removing them from the main order lines.
