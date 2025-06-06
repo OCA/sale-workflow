beea4095fbcc96b98b039d3270c663f2224ddd8c
Jacques-Etienne Baudoux <je@bcim.be>
2025-06-06 15:07:46 +0200

    [FIX] sale_invoice_policy: external dependency
    pre init hook depends on openupgradelib

--- sale_invoice_policy/__manifest__.py
+++ sale_invoice_policy/__manifest__.py
@@ -11,6 +11,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale_stock", "base_partition"],
+    "external_dependencies": {"python": ["openupgradelib"]},
     "data": [
         "views/res_config_settings_view.xml",
         "views/sale_view.xml",
