cac8f33af1bc1c6dbedd1fa2109fd748fe812dbc
Alejandro Parrales <alejandro17parrales@gmail.com>
2025-07-04 11:05:44 -0500

    [MIG] sale_order_line_tag: Migration to 18.0

--- sale_order_line_tag/__manifest__.py
+++ sale_order_line_tag/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Line Tag",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "summary": "Add tags to classify sales order line reasons",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
