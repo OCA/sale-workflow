85215f92f358fcbfa73e47a155dfa6e60175dc91
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-07-23 07:47:00 +0200

    [ADD] sale_semaphore: New module to be alerted of unit prices set on sale order

---
+++ sale_semaphore/__manifest__.py
@@ -0,0 +1,33 @@
+# Copyright 2025 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Semaphore",
+    "summary": "Adds a semaphore for commercial purposes",
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/account_invoice_report_views.xml",
+        "views/account_move_views.xml",
+        "views/product_category_views.xml",
+        "views/product_views.xml",
+        "views/sale_order_views.xml",
+        "views/sale_report_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "assets": {
+        "web.assets_backend": [
+            "sale_semaphore/static/src/semaphore/semaphore.esm.js",
+            "sale_semaphore/static/src/semaphore/semaphore.scss",
+        ],
+        "web.assets_qweb": [
+            "sale_semaphore/static/src/semaphore/semaphore.xml",
+        ],
+    },
+    "pre_init_hook": "pre_init_hook",
+}
