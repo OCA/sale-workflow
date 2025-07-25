065a00e39c84578654bb747976a650798526cfc8
Arnau <arnau.cruz@forgeflow.com>
2025-07-25 11:42:45 +0200

    [MIG] sale_stock_line_sequence: Migration to 18.0

--- sale_stock_line_sequence/__manifest__.py
+++ sale_stock_line_sequence/__manifest__.py
@@ -3,8 +3,9 @@
 
 {
     "name": "Sale Stock Order Line Sequence",
-    "summary": "Glue Module for Sale Order Line Sequence and Stock Picking Line Sequence",
-    "version": "16.0.1.0.0",
+    "summary": "Glue Module for Sale Order Line Sequence "
+    "and Stock Picking Line Sequence",
+    "version": "18.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",
