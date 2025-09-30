983f873cfe427ece6fa67fc4cc629d090d40f671
eduardo <eduardo.ezerouali@tecnativa.com>
2025-09-30 06:51:42 +0000

    [ADD] sale_stock_warehouse_partner: New module

---
+++ sale_stock_warehouse_partner/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Tecnativa - Eduardo Ezerouali
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Sale Stock Warehouse Partner ",
+    "summary": "Allows to set a warehouse to a partner",
+    "version": "15.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA), Odoo SA",
+    "license": "AGPL-3",
+    "depends": ["sale_stock"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
---
+++ sale_stock_warehouse_partner/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2025 Tecnativa - Eduardo Ezerouali
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from . import test_sale_stock_warehouse_partner
