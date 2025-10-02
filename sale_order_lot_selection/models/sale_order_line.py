882ff501470a41b0d0834eb50872a0d11874c241
Víctor Martínez <victor.martinez@tecnativa.com>
2025-10-02 11:54:34 +0200

    [IMP] sale_order_lot_selection: Hide the lot_id field if the product does not have tracking defined

--- sale_order_lot_selection/models/sale_order_line.py
+++ sale_order_lot_selection/models/sale_order_line.py
@@ -4,6 +4,15 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    def _selection_product_tracking(self):
+        return self.env["product.product"].fields_get(
+            allfields=["tracking"],
+        )["tracking"]["selection"]
+
+    product_tracking = fields.Selection(
+        selection=_selection_product_tracking,
+        compute="_compute_product_tracking",
+    )
     lot_id = fields.Many2one(
         "stock.lot",
         "Lot",
@@ -19,6 +28,11 @@ class SaleOrderLine(models.Model):
         if self.lot_id:
             vals["restrict_lot_id"] = self.lot_id.id
         return vals
+
+    @api.depends("product_id")
+    def _compute_product_tracking(self):
+        for sol in self:
+            sol.product_tracking = sol.product_id.tracking or sol.product_tracking
 
     @api.depends("product_id")
     def _compute_lot_id(self):
