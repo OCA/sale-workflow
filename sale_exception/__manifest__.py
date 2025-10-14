3c91d847261197dd7560bd2b0f1a7466d20a4ec0
Franco Leyes <lef@adhoc.com.ar>
2025-10-14 16:50:37 -0300

    [IMP] sale_exception: pre-commit execution

--- sale_exception/__manifest__.py
+++ sale_exception/__manifest__.py
@@ -7,10 +7,7 @@
     "summary": "Custom exceptions on sale order",
     "version": "18.0.2.1.0",
     "category": "Generic Modules/Sale",
-    "author": "Akretion, "
-    "Sodexis, "
-    "Camptocamp, "
-    "Odoo Community Association (OCA)",
+    "author": "Akretion, Sodexis, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "depends": ["sale", "base_exception"],
     "license": "AGPL-3",
