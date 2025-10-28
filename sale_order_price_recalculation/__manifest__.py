d3143fee632d9e355c0343dcae59fd1c34969b30
Bhavesh Heliconia <oca@heliconia.in>
2025-10-28 13:15:31 +0530

    [IMP] sale_order_price_recalculation: pre-commit auto fixes

--- sale_order_price_recalculation/__manifest__.py
+++ sale_order_price_recalculation/__manifest__.py
@@ -11,10 +11,7 @@
     "version": "18.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
-    "author": "AvanzOSC,"
-    "Grupo Vermon,"
-    "Tecnativa,"
-    "Odoo Community Association (OCA)",
+    "author": "AvanzOSC,Grupo Vermon,Tecnativa,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
