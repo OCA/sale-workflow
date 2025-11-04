28e979199b395c59c9563a142417510a3e185b5a
Víctor Martínez <victor.martinez@tecnativa.com>
2025-11-04 14:04:30 +0100

    [IMP] sale_order_lot_selection: Be able to select only available lots
    Example use case:
    - Create 5 lots (Lot 1, Lot 2, Lot 3, Lot 4, Lot 5) for a product
    - Define stock only in lots: Lot 1, Lot 2, and Lot 5
    - Only lots with stock should be selectable in the sales order line.
    
    TT57112

--- sale_order_lot_selection/models/sale_order_line.py
+++ sale_order_lot_selection/models/sale_order_line.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
@@ -13,9 +14,11 @@ class SaleOrderLine(models.Model):
         selection=_selection_product_tracking,
         compute="_compute_product_tracking",
     )
+    domain_lot_id = fields.Binary(compute="_compute_domain_lot_id")
     lot_id = fields.Many2one(
         "stock.lot",
         "Lot",
+        domain="domain_lot_id",
         copy=False,
         compute="_compute_lot_id",
         store=True,
@@ -33,6 +36,35 @@ class SaleOrderLine(models.Model):
     def _compute_product_tracking(self):
         for sol in self:
             sol.product_tracking = sol.product_id.tracking or sol.product_tracking
+
+    @api.depends("product_id", "product_uom_qty", "warehouse_id")
+    def _compute_domain_lot_id(self):
+        dp = self.env["decimal.precision"].precision_get("Product Unit of Measure")
+        for sol in self:
+            domain = []
+            if sol.product_id and sol.product_tracking != "none":
+                # Only available lots should be displayed. In pickings, the
+                # corresponding stock.quant record is selected directly, so we
+                # use the same filters that are used.
+                quants = self.env["stock.quant"].search(
+                    [
+                        ("product_id", "=", sol.product_id.id),
+                        ("quantity", ">=", sol.product_uom_qty),
+                        ("lot_id", "!=", False),
+                        ("location_id.usage", "=", "internal"),
+                        ("location_id.warehouse_id", "=", sol.warehouse_id.id),
+                    ]
+                )
+                available_quants = quants.filtered(
+                    lambda x, qty=sol.product_uom_qty: float_compare(
+                        x.available_quantity,
+                        qty,
+                        precision_digits=dp,
+                    )
+                    >= 0
+                )
+                domain = [("id", "in", available_quants.mapped("lot_id").ids)]
+            sol.domain_lot_id = domain
 
     @api.depends("product_id")
     def _compute_lot_id(self):
