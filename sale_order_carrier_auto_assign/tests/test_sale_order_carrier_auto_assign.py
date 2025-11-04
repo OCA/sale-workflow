715f9ecdb0b4db1ee15115a085c0699d00a4e69f
Simone Orsi <simahawk@gmail.com>
2025-11-04 11:44:05 +0100

    sale_order_carrier_auto_assign: add test for form onchange

--- sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
+++ sale_order_carrier_auto_assign/tests/test_sale_order_carrier_auto_assign.py
@@ -28,10 +28,20 @@ class TestSaleOrderCarrierAutoAssignCommon(BaseCommon):
         cls.delivery_local_delivery = cls.env.ref("delivery.delivery_local_delivery")
         cls.delivery_local_delivery.fixed_price = 10
         cls.delivery_local_delivery.free_over = False
+        cls.delivery_local_delivery2 = cls.delivery_local_delivery.copy(
+            {"name": "New delivery"}
+        )
         cls.partner = cls.env["res.partner"].create(
             {
                 "name": "Test partner",
                 "property_delivery_carrier_id": cls.delivery_local_delivery.id,
+            }
+        )
+        cls.partner_delivery = cls.env["res.partner"].create(
+            {
+                "name": "Test partner delivery",
+                "parent_id": cls.partner.id,
+                "property_delivery_carrier_id": cls.delivery_local_delivery2.id,
             }
         )
 
@@ -139,6 +149,20 @@ class TestSaleOrderCarrierAutoAssignOnCreate(TestSaleOrderCarrierAutoAssignCommo
     def test_sale_order_carrier_auto_assign_all_service(self):
         sale_order = self.env["sale.order"].create({"partner_id": self.partner.id})
         self.assertFalse(sale_order.carrier_id)
+
+    def test_sale_order_add_delivery_carrier_on_partner_change(self):
+        # enable to change delivery address
+        self.env.user.groups_id += self.env.ref(
+            "account.group_delivery_invoice_address"
+        )
+        sale_order_form = Form(self.env["sale.order"])
+        # no lines yet, won't do anything
+        sale_order_form.partner_id = self.partner
+        with sale_order_form.order_line.new() as line_form:
+            line_form.product_id = self.product_storable
+        sale_order_form.partner_shipping_id = self.partner_delivery
+        so = sale_order_form.save()
+        self.assertEqual(so.carrier_id, self.delivery_local_delivery2)
 
 
 class TestSaleOrderCarrierAutoAssignOnConfirm(TestSaleOrderCarrierAutoAssignCommon):
