1617bc5392c601584b64f36d6af4daea79e6f20c
Víctor Martínez <victor.martinez@tecnativa.com>
2025-11-12 16:41:26 +0100

    [IMP] sale_order_lot_selection: Define the appropriate lot data in stock moves
    Example use case:
    - Product with Lot A and Lot B.
    - SO with Lot A linked.
    - The SO is confirmed and the OUT is generated with Lot A linked in the stock move
    - The SO is modified to define Lot B.
    - The OUT stock move is changed to Lot B.
    
    TT57112

--- sale_order_lot_selection/models/__init__.py
+++ sale_order_lot_selection/models/__init__.py
@@ -1,1 +1,2 @@
 from . import sale_order_line
+from . import stock_move
--- sale_order_lot_selection/models/sale_order_line.py
+++ sale_order_lot_selection/models/sale_order_line.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
@@ -18,6 +20,7 @@ class SaleOrderLine(models.Model):
         "Lot",
         copy=False,
         compute="_compute_lot_id",
+        inverse="_inverse_lot_id",
         store=True,
         readonly=False,
         precompute=True,
@@ -39,3 +42,19 @@ class SaleOrderLine(models.Model):
         for sol in self:
             if sol.product_id != sol.lot_id.product_id:
                 sol.lot_id = False
+
+    def _inverse_lot_id(self):
+        for item in self.filtered(
+            lambda x: x.product_id and x.product_tracking != "none" and x.move_ids
+        ):
+            moves = item.move_ids.filtered(lambda x: x.restrict_lot_id)
+            if any(move.state == "done" for move in moves):
+                raise ValidationError(
+                    self.env._(
+                        "You can't modify the Lot/Serial number "
+                        "because some stock move has already been done."
+                    )
+                )
+            pending_moves = moves.filtered(lambda x: x.state != "cancel")
+            if pending_moves:
+                pending_moves._set_restrict_lot_id_from_sol(item.lot_id)
