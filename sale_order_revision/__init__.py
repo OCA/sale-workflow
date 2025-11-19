a540bff185e1ee6043c7a6461a640a5c1d1e427e
Paula Marfull <paula.marfull@forgeflow.com>
2025-11-19 11:28:56 +0100

    [IMP] sale_order_revision: pre-commit auto fixes

--- sale_order_revision/__init__.py
+++ sale_order_revision/__init__.py
@@ -4,7 +4,5 @@ from . import models
 
 def populate_unrevisioned_name(env):
     env.cr.execute(
-        "UPDATE sale_order "
-        "SET unrevisioned_name = name "
-        "WHERE unrevisioned_name is NULL"
+        "UPDATE sale_order SET unrevisioned_name = name WHERE unrevisioned_name is NULL"
     )
