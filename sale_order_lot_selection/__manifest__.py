1f10af08ec3c713f8f104fa7120867b9558ff352
Víctor Martínez <victor.martinez@tecnativa.com>
2025-12-01 16:44:28 +0100

    [IMP] sale_order_lot_selection: Add lot_id to sale report
    TT57124

--- sale_order_lot_selection/__manifest__.py
+++ sale_order_lot_selection/__manifest__.py
@@ -6,7 +6,11 @@
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
     "depends": ["sale_stock", "stock_restrict_lot"],
-    "data": ["security/ir.model.access.csv", "views/sale_order_views.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_views.xml",
+        "reports/sale_report_views.xml",
+    ],
     "demo": ["demo/sale_demo.xml"],
     "maintainers": ["bodedra"],
     "installable": True,
