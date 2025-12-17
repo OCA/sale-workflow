94746944257af8b173dcf9cd1d51b4485468bfd9
David Vidal <david@moduon.team>
2025-12-17 12:27:54 +0100

    [MIG] sale_stock_product_recommendation: Migration to 18.0
    MT-11833

--- sale_stock_product_recommendation/__manifest__.py
+++ sale_stock_product_recommendation/__manifest__.py
@@ -4,15 +4,13 @@
 {
     "name": "Sale Order Product Recommendation Stock",
     "summary": "Display stock info when recommending products to sell",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/Sales",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Moduon, Odoo Community Association (OCA)",
     "maintainers": ["yajo", "rafaelbn"],
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
     "auto_install": True,
     "depends": ["sale_stock", "sale_order_product_recommendation"],
     "data": [
--- sale_stock_product_recommendation/readme/CREDITS.md
+++
@@ -1,3 +0,0 @@
-The development of this module has been financially supported by:
-
-- [Moduon](https://www.moduon.team/)
