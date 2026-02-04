cad577a7c1813f9c042dd1fef45fd88d9aaa4ac5
Maksym Yankin <yankinmk@gmail.com>
2026-02-04 14:46:30 +0200

    [ADD] sale_product_multiple_qty

---
+++ sale_product_multiple_qty/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Product Multiple Qty",
+    "summary": "Allows defining a sales quantity multiple on products.",
+    "version": "19.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-workflow",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale"],
+    "maintainers": ["yankinmax"],
+    "data": [
+        # Views
+        "views/product_view.xml",
+    ],
+}
