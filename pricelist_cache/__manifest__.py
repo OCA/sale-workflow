aa54ab6c43dace58c671ffc53e0ceaa9a64cf687
ralf-mens <155630025+ralf-mens@users.noreply.github.com>
2026-02-09 12:39:35 +0100

    [IMP] Dutch translation and fixed view
    - Added dutch translation
    - Fixed button position on pricelist view

--- pricelist_cache/__manifest__.py
+++ pricelist_cache/__manifest__.py
@@ -6,7 +6,7 @@
         Provide a new model to cache price lists and update it,
         to make it easier to retrieve them.
     """,
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Hidden",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
