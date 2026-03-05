eacb7b13d67d52ed856d2834ca43783112833a7c
Guillaume MASSON <guillaume.masson@akretion.com>
2026-03-04 16:00:50 -1000

    [MIG] sale_probability_amount: Migration to 18.0

--- sale_probability_amount/__manifest__.py
+++ sale_probability_amount/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Sale Probability Amount",
     "summary": "add a win probability on quotation",
-    "version": "14.0.1.0.1",
+    "version": "18.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "license": "AGPL-3",
