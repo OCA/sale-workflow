cb30993c19f46dacd72b34da41a76f8607538fb3
Guillaume MASSON <guillaume.masson@akretion.com>
2026-03-04 15:10:41 -1000

    [MIG] sale_partner_shipping_invoice_domain: Migration to 18.0

--- sale_partner_shipping_invoice_domain/__manifest__.py
+++ sale_partner_shipping_invoice_domain/__manifest__.py
@@ -1,16 +1,15 @@
 # Copyright 2021 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "Sale Shipping and Invoice Domain",
     "summary": """
         Adds a sensible domain to the shipping
         and invoicing addresses on sale form view""",
-    "version": "14.0.1.0.2",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
-    "depends": ["sale_stock", "sale_commercial_partner", "web_domain_field"],
+    "depends": ["sale_stock", "sale_commercial_partner", "web"],
     "data": [
         "views/sale_order.xml",
     ],
