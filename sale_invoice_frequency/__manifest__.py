0003151b40d4140bc70c11124f384d2226093541
Maksym Yankin <yankinmk@gmail.com>
2026-03-10 17:32:53 +0200

    [19.0][MIG] 19.0-mig-sale_invoice_frequency: Migration to 19.0

--- sale_invoice_frequency/__manifest__.py
+++ sale_invoice_frequency/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Invoice Frequency",
     "summary": "Define the invoice frequency for customers",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "development_status": "Beta",
     "category": "Sales/Sales",
     "website": "https://github.com/OCA/sale-workflow",
