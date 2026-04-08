c6ba272447908dd69a9f3cc61c02ce1c7fe8ba23
Guillaume MASSON <guillaume.masson@akretion.com>
2026-04-07 14:08:32 -1000

    [FIX] sale_partner_primeship : freeze time in tests to avoid errors due to DST

--- sale_partner_primeship/__manifest__.py
+++ sale_partner_primeship/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Sale Partner Primeship",
     "summary": """Allow you to manage time limited prime memberships
     and prime membership activation products.""",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "author": "Akretion, Odoo Community Association (OCA)",
     "maintainers": ["nayatec", "paradoxxxzero"],
     "website": "https://github.com/OCA/sale-workflow",
