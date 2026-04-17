750fcedf594c8a82a0ae50350968143ed7b56ee8
Andrii9090-tecnativa <andrii.kompaniiets@tecnativa.com>
2026-04-17 10:51:45 +0200

    [MIG] sale_elaboration_brand: Migration to 18.0

--- sale_elaboration_brand/__manifest__.py
+++ sale_elaboration_brand/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product brand in sale elaboration report",
     "summary": "Show product brand in sale elaboration report",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
