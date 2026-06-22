39ccecb9c5b24d8a0f9516da05ba4b527254d29f
Souheil Bejaoui <souheil.bejaoui@acsone.eu>
2026-06-22 18:10:01 +0200

    [ADD] sale_pricelist_fixed_price_no_discount: lock standard and triple discounts

---
+++ sale_pricelist_fixed_price_no_discount/models/sale_order_line.py
@@ -0,0 +1,45 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    fixed_price_pricelist_rule = fields.Boolean(
+        compute="_compute_fixed_price_pricelist_rule"
+    )
+
+    @api.depends("pricelist_item_id")
+    def _compute_fixed_price_pricelist_rule(self):
+        for line in self:
+            line.fixed_price_pricelist_rule = line._has_fixed_price_pricelist_rule()
+
+    def _has_fixed_price_pricelist_rule(self):
+        self.ensure_one()
+        return self.pricelist_item_id.compute_price == "fixed"
+
+    @api.depends("pricelist_item_id")
+    def _compute_discount(self):
+        res = super()._compute_discount()
+        for line in self:
+            if line._has_fixed_price_pricelist_rule():
+                line.discount = 0.0
+        return res
+
+    @api.constrains("discount", "product_id", "product_uom", "product_uom_qty")
+    def _check_no_discount_on_fixed_price_pricelist_rule(self):
+        precision = self.env["decimal.precision"].precision_get("Discount")
+        for line in self:
+            if line.fixed_price_pricelist_rule and not float_is_zero(
+                line.discount, precision_digits=precision
+            ):
+                raise ValidationError(
+                    _(
+                        "Discounts are not allowed when the applied pricelist rule "
+                        "uses a fixed price."
+                    )
+                )
---
+++ sale_pricelist_fixed_price_no_discount/tests/test_sale_pricelist_fixed_price_no_discount.py
@@ -0,0 +1,69 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import Command
+from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
+
+
+class TestSalePricelistFixedPriceNoDiscount(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env.ref("base.res_partner_1")
+        cls.product = cls.env["product.product"].create(
+            {"name": "Fixed Price Product", "list_price": 100.0}
+        )
+        cls.pricelist = cls.env["product.pricelist"].create(
+            {
+                "name": "Fixed Price Pricelist",
+                "item_ids": [
+                    Command.create(
+                        {
+                            "applied_on": "1_product",
+                            "product_tmpl_id": cls.product.product_tmpl_id.id,
+                            "compute_price": "fixed",
+                            "fixed_price": 80.0,
+                            "base": "list_price",
+                        }
+                    )
+                ],
+            }
+        )
+        cls.order = cls.env["sale.order"].create(
+            {"partner_id": cls.partner.id, "pricelist_id": cls.pricelist.id}
+        )
+
+    def test_fixed_price_pricelist_rule_sets_technical_boolean(self):
+        line = self.env["sale.order.line"].create(
+            {
+                "order_id": self.order.id,
+                "product_id": self.product.id,
+                "product_uom_qty": 1.0,
+            }
+        )
+
+        self.assertTrue(line.fixed_price_pricelist_rule)
+
+    def test_fixed_price_pricelist_rule_rejects_discount_on_create(self):
+        with self.assertRaises(ValidationError):
+            self.env["sale.order.line"].create(
+                {
+                    "order_id": self.order.id,
+                    "product_id": self.product.id,
+                    "product_uom_qty": 1.0,
+                    "discount": 10.0,
+                }
+            )
+
+    def test_fixed_price_pricelist_rule_rejects_discount_on_write(self):
+        line = self.env["sale.order.line"].create(
+            {
+                "order_id": self.order.id,
+                "product_id": self.product.id,
+                "product_uom_qty": 1.0,
+            }
+        )
+
+        with self.assertRaises(ValidationError):
+            line.write({"discount": 10.0})
