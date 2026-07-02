5b9b51bb4c4634a1230a571087c8e8dc133c7722
Marc Gimeno <marc.gimeno@forgeflow.com>
2026-07-02 13:29:00 +0200

    [ADD] sale_order_line_route_visible: show Routes column by default on sale order lines

---
+++ sale_order_line_route_visible/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Line Route Visible",
+    "summary": "Show the Routes column by default on sale order lines",
+    "version": "19.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Sales Management",
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["MarcGForgeFlow"],
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
