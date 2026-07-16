13d2e9f992fc67fc8fb56e3ca95be026e9bcb925
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-07-16 17:56:53 +0530

    [19.0][MIG]sale_line_name_option: Migration to 19.0

--- sale_line_name_option/__manifest__.py
+++ sale_line_name_option/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/res_config_settings_views.xml"],
