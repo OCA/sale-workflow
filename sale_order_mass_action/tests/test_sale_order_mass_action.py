99da98f95289cf22048e7f195707afb91a064de6
sergio-teruel <sergio.teruel@tecnativa.com>
2026-07-27 23:19:33 +0200

    [15.0][MIG] sale_order_mass_action: Migration to 15.0
    sale_order_mass_action was never migrated to 15.0 upstream (it went
    straight from 14.0 to 16.0). Port the module as-is from 14.0:
    
    - Bump manifest version to 15.0.1.0.0.
    - Replace SavepointCase (deprecated since 15.0, merged into
      TransactionCase) with TransactionCase in tests.
    - Regenerate README/pot for the 15.0 branch, add Tecnativa as
      contributor.

--- sale_order_mass_action/tests/test_sale_order_mass_action.py
+++ sale_order_mass_action/tests/test_sale_order_mass_action.py
@@ -1,10 +1,10 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests import Form
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestSaleOrderMassAction(SavepointCase):
+class TestSaleOrderMassAction(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
