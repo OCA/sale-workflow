8d4044ca465c3a02dcd915bb2acd6fd271059fd9
Denis Roussel <denis.roussel@acsone.eu>
2026-08-25 14:22:27 +0200

    [18.0][ADD] sale_order_invoice_tree_customer_ref

---
+++ sale_order_invoice_tree_customer_ref/models/account_move.py
@@ -0,0 +1,13 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    customer_reference = fields.Char(
+        string="Customer Code",
+        related="partner_id.ref",
+    )
