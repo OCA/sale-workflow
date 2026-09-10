9ab68db0cb6aca886d3f8ebffdbc3ba7720d1745
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-09-10 12:41:36 +0530

    [MIG]sale_partner_shipping_default_partner_invoice: Migration to 19.0

--- sale_partner_shipping_default_partner_invoice/tests/test_shipping_default_partner_invoice.py
+++ sale_partner_shipping_default_partner_invoice/tests/test_shipping_default_partner_invoice.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
-from odoo.tests import TransactionCase
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestSalePartnerShippingDefaultPartnerInvoice(TransactionCase):
+class TestSalePartnerShippingDefaultPartnerInvoice(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
