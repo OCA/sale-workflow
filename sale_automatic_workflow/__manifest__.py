4a4d5dc7b145373c359111b1cbcfc3fc9957868c
Andrea Stirpe <and.stirpe@gmail.com>
2026-09-15 17:33:42 +0200

    [FIX] sale_automatic_workflow: do not register zero amount payments

--- sale_automatic_workflow/__manifest__.py
+++ sale_automatic_workflow/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Sale Automatic Workflow",
-    "version": "18.0.1.2.0",
+    "version": "18.0.1.3.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Akretion, "
